e21cb041b64f883168a82da2e10fce26a4ee66c7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-06 20:27:52 +0800

    libedit: update 20251016-3.1_1 bottle.

--- Formula/lib/libedit.rb
+++ Formula/lib/libedit.rb
@@ -14,12 +14,7 @@ class Libedit < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "2957d416dfdc8c58a1dd666669d3c7d1330ba15ededc2438a4de3059ca8ec20b"
-    sha256 cellar: :any,                 arm64_sequoia: "510068e4f21502b89b2c659b098f0f1a3adfa6e30e7cd77ec5389e78038026d9"
-    sha256 cellar: :any,                 arm64_sonoma:  "ff578f10a3f9bd4c183f97578b0e92893d27a1fdcdd3093ddb7bff44d4e61416"
-    sha256 cellar: :any,                 sonoma:        "a875f674adab029105a001dda7a43dbe44544887d73caad16a48cf7c51cacc4e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e61d1403b27ad966a5aafaafafd2db88aace8c717d57aa561df5f7b6d1f1fc69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0e22aa9e7d09e78d35b2a97e87747271a8651f7eabe4d0e424756939718dfe2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d18c7bff6ed40d310654942ff3e1136b8f465ce6b5eeae832ca4e0a251f04d08"
   end
 
   keg_only :provided_by_macos
