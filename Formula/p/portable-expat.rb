f0ebe725e8dc6ef5a5fea8d5a6a130ccf38955eb
hqzing <hqzing@outlook.com>
2026-05-06 19:45:33 +0800

    portable-expat 2.7.5 (new formula)

---
+++ Formula/p/portable-expat.rb
@@ -0,0 +1,74 @@
+require File.expand_path("../../Abstract/portable-formula", __dir__)
+
+class PortableExpat < PortableFormula
+  desc "XML 1.0 parser"
+  homepage "https://libexpat.github.io/"
+  url "https://github.com/libexpat/libexpat/releases/download/R_2_7_5/expat-2.7.5.tar.lz"
+  sha256 "975e76ab8a5625190bd04a577fa9efc246798c25b8ec665acb6f7951be8b0646"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^\D*?(\d+(?:[._]\d+)*)$/i)
+    strategy :github_latest do |json, regex|
+      json["tag_name"]&.scan(regex)&.map { |match| match[0].tr("_", ".") }
+    end
+  end
+
+  head do
+    url "https://github.com/libexpat/libexpat.git", branch: "master"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "docbook2x" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    cd "expat" if build.head?
+    system "autoreconf", "-fiv" if build.head?
+    args = ["--mandir=#{man}"]
+    args << "--with-docbook" if build.head?
+    args << "--disable-shared"
+    system "./configure", *std_configure_args, *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <stdio.h>
+      #include "expat.h"
+
+      static void XMLCALL my_StartElementHandler(
+        void *userdata,
+        const XML_Char *name,
+        const XML_Char **atts)
+      {
+        printf("tag:%s|", name);
+      }
+
+      static void XMLCALL my_CharacterDataHandler(
+        void *userdata,
+        const XML_Char *s,
+        int len)
+      {
+        printf("data:%.*s|", len, s);
+      }
+
+      int main()
+      {
+        static const char str[] = "<str>Hello, world!</str>";
+        int result;
+
+        XML_Parser parser = XML_ParserCreate("utf-8");
+        XML_SetElementHandler(parser, my_StartElementHandler, NULL);
+        XML_SetCharacterDataHandler(parser, my_CharacterDataHandler);
+        result = XML_Parse(parser, str, sizeof(str), 1);
+        XML_ParserFree(parser);
+
+        return result;
+      }
+    C
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lexpat", "-o", "test"
+    assert_equal "tag:str|data:Hello, world!|", shell_output("./test")
+  end
+end
