07b86b67dd28631e07aaebe547653fd56b7f1d43
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-06 18:41:29 +0800

    gmp: update 6.3.0 bottle.

--- Formula/g/gmp.rb
+++ Formula/g/gmp.rb
@@ -15,18 +15,7 @@ class Gmp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "db8075f389ca0a9f9aba54762c93760db66ef92ee989f4a32b5e13b4b987339c"
-    sha256 cellar: :any,                 arm64_sequoia:  "6683d73d6677d28e1e8d1b92d6ebfbc068c1d33e19b79114a22a648a99ba5991"
-    sha256 cellar: :any,                 arm64_sonoma:   "78e4f40cba6419cf7e2d81e9c945d1e93744511bd5230bdfac1b69ed894914b4"
-    sha256 cellar: :any,                 arm64_ventura:  "98c163edfbe7bdc0c14f88d7d34fa2764ecb9cab9f749600b861012700603260"
-    sha256 cellar: :any,                 arm64_monterey: "2115b33b8b4052f91ffb85e476c7fc0388cf4e614af1ce6453b35e6d25473911"
-    sha256 cellar: :any,                 tahoe:          "3f2654a4c29cd2b8605bea7a9473d6984ecbefa6413662b0d976f1457099f24d"
-    sha256 cellar: :any,                 sequoia:        "d1192da68b2618652f4be0dd9f56b18d2d276481440ae241ce9cc17be0450e07"
-    sha256 cellar: :any,                 sonoma:         "e8410d92339535174e9f4a5eccc403301b70c7287f2f9a87f064a9aa2e21b54b"
-    sha256 cellar: :any,                 ventura:        "83ec5443c018c02036d88ae0dc8dc4237b3b38eb76a3cdd82148e7f841ffd39f"
-    sha256 cellar: :any,                 monterey:       "b04023f65b8c79c45798a4bfd97fdbeb10f1bf9e8416e22e8eeedbd9b2a8c102"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "5f23ebfcde217dbc697dc961d103ca602445a5775c2ed19a29eaec98f4cfa3c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3dca3544faca889c7389a5fdbd2b5b00582c34a4e14607033573ad3b06ca7882"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3a123def7337b7939e1c63f80096a83c0f75b39762a043082e73f7f9149f8bb1"
   end
 
   depends_on "autoconf" => :build
