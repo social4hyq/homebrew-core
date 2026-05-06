a684ac2773a5606c2bb0330c3cbae56bff836dfb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-06 19:38:25 +0800

    portable-libyaml: add 0.2.5 bottle.

--- Formula/p/portable-libyaml.rb
+++ Formula/p/portable-libyaml.rb
@@ -11,6 +11,10 @@ class PortableLibyaml < PortableFormula
     formula "libyaml"
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4cd04c04e33ede866363363b63ce25c3ce12b578b0598402abd2145ee22d9aa4"
+  end
+
   def install
     system "./configure", *portable_configure_args,
                           "--disable-dependency-tracking",
