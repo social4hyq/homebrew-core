1c205b7a4f1ca035b6a885dd05bd36ae5bac5f7a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-06 20:19:48 +0800

    libngtcp2: update 1.22.1 bottle.

--- Formula/lib/libngtcp2.rb
+++ Formula/lib/libngtcp2.rb
@@ -9,12 +9,7 @@ class Libngtcp2 < Formula
   head "https://github.com/ngtcp2/ngtcp2.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "9b5ecd7bfaa9480d125e9dd5bf96ab2cde59557e9a8797f04ec6ee1af66ad9b5"
-    sha256 cellar: :any,                 arm64_sequoia: "ea867de81e1a2fbb1a8f8c76bbffbca05db53fede0b3da50f0ba7c9f1541158a"
-    sha256 cellar: :any,                 arm64_sonoma:  "c05e1d6ea7186865188cabe53b5e1f79afee3646fdaa408998ea7bdceb5414be"
-    sha256 cellar: :any,                 sonoma:        "6ef4ea330fbbbbfe6a94ad85d7bad8a4f78586bbe91f2824756c3a3eb0d2e48c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "275827e764af82be486645dd0e4e27409f8b2c4883f68c58fdeb1fbbddc2f3aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6cb712caac1c531346ad50dd0b40c30b7ba768aa5e3d332da82673f3dd16793a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f9563578d3686421d4010340e19c8c5c5df368f2224e4fe4fd4ef0d8d6daeb3d"
   end
 
   depends_on "pkgconf" => :build
