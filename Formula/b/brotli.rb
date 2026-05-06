c9934e2220b08c76344b697f283238b2bf1fa340
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-06 20:15:24 +0800

    brotli: update 1.2.0 bottle.

--- Formula/b/brotli.rb
+++ Formula/b/brotli.rb
@@ -15,12 +15,7 @@ class Brotli < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "71ffd30ccd74a9f28cfb210c21fcdc1aa5fec79e23ad2264b6dbceb73437601f"
-    sha256 cellar: :any,                 arm64_sequoia: "b8e388c96be5a63a46e0335c390fecb20dda7adb1368a2ba0209b38cae17c708"
-    sha256 cellar: :any,                 arm64_sonoma:  "46b1cc7ef8ba325e56f7363de1068a84449dac940187dd2290a75e7008b3b4e1"
-    sha256 cellar: :any,                 sonoma:        "c9d272f3ac33b730d548da14ca323e8b4e0185273a6c85b0f12a4bbcd9f684af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "485e0a6a74702cbe57819b32177bfb81e0195a71f52a16bab84b03529a672cf3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d97618daaa84b66bc783888297f1765eb6f60e6a78e9b7c2ba0406889c159c05"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ffd1d8347cd46bb8b7e7d3ee583939bc80b132555c83a0570ac143beacde7886"
   end
 
   depends_on "cmake" => :build
