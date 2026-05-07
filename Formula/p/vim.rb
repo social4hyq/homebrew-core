d99efe3c67fd65436bfe91cb38577bbbd11b31b1
hqzing <hqzing@outlook.com>
2026-05-07 11:50:11 +0800

    vim 9.2.0400 (new formula)

---
+++ Formula/p/vim.rb
@@ -0,0 +1,108 @@
+class Vim < Formula
+  desc "Vi 'workalike' with many additional features"
+  homepage "https://www.vim.org/"
+  # vim should only be updated every 50 releases on multiples of 50
+  url "https://github.com/vim/vim/archive/refs/tags/v9.2.0400.tar.gz"
+  sha256 "e99d7d926326103267840feac5a4a8f73bd7546f56cae8dd8dec59e00008cc76"
+  license "Vim"
+  compatibility_version 1
+  head "https://github.com/vim/vim.git", branch: "master"
+
+  # The Vim repository contains thousands of tags and the `Git` strategy isn't
+  # ideal in this context. This is an exceptional situation, so this checks the
+  # first 50 tags using the GitHub API (to minimize data transfer).
+  livecheck do
+    url "https://api.github.com/repos/vim/vim/tags?per_page=50"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json.map do |tag|
+        match = tag["name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+    throttle 50
+  end
+
+  bottle do
+    sha256 arm64_tahoe:   "65c8ed4235f0a5630a97529dc6ff09d14f03b9d9726e32504f10607a194f5f44"
+    sha256 arm64_sequoia: "92576d91b09b37a49019874d141ebed5e56a1c2987304bc0a15113d9646112f0"
+    sha256 arm64_sonoma:  "1467fa6b9c89cebe13f3e1209c85e10aff39874e38f216fb76d762a19a7e6de4"
+    sha256 sonoma:        "8e5fd8f1cbc5a4007ddaf9dd4210443695fa7cdfa3192c8839933de32aaf9327"
+    sha256 arm64_linux:   "d2186cfff725f21c7e2d66b33e3b2158b9102a5d4934c7f498866bbaf7af1e2e"
+    sha256 x86_64_linux:  "5e700683e9519f7b859ebea39af9450570759a48f52d93dcf6eaa77ad25a199c"
+  end
+
+  depends_on "gettext" => :build
+  depends_on "libsodium"
+  depends_on "lua@5.4" # Lua 5.5 doesn't work for now, see https://github.com/vim/vim/issues/19639
+  depends_on "ncurses"
+  depends_on "python@3.14"
+  depends_on "ruby"
+
+  uses_from_macos "perl"
+
+  depends_on "gettext"
+
+  conflicts_with "ex-vi", because: "vim and ex-vi both install bin/ex and bin/view"
+  conflicts_with "macvim", because: "vim and macvim both install vi* binaries"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@3.14"].opt_libexec/"bin"
+
+    # https://github.com/Homebrew/homebrew-core/pull/1046
+    ENV.delete("SDKROOT")
+
+    # vim doesn't require any Python package, unset PYTHONPATH.
+    ENV.delete("PYTHONPATH")
+
+    ENV.append_to_cflags "-mllvm -enable-constraint-elimination=0" if DevelopmentTools.clang_build_version == 1600
+
+    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
+    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
+    # system vimscript files. We specify the normal installation prefix
+    # when calling "make install".
+    # Homebrew will use the first suitable Perl & Ruby in your PATH if you
+    # build from source. Please don't attempt to hardcode either.
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
+                          "--mandir=#{man}",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--with-compiledby=Homebrew",
+                          "--enable-cscope",
+                          "--enable-terminal",
+                          "--enable-perlinterp",
+                          "--enable-rubyinterp",
+                          "--enable-python3interp",
+                          "--disable-gui",
+                          "--without-x",
+                          "--enable-luainterp",
+                          "--with-lua-prefix=#{Formula["lua@5.4"].opt_prefix}"
+    system "make"
+    # Parallel install could miss some symlinks
+    # https://github.com/vim/vim/issues/1031
+    ENV.deparallelize
+    # If stripping the binaries is enabled, vim will segfault with
+    # statically-linked interpreters like ruby
+    # https://github.com/vim/vim/issues/114
+    system "make", "install", "prefix=#{prefix}", "STRIP=#{which "true"}"
+    bin.install_symlink "vim" => "vi"
+
+    inreplace share/"vim/vim92/defaults.vim" do |s|
+      s.gsub! "set mouse=a", "set mouse="
+      s.gsub! "set mouse=nvi", "set mouse="
+    end
+  end
+
+  test do
+    (testpath/"commands.vim").write <<~VIM
+      :python3 import vim; vim.current.buffer[0] = 'hello python3'
+      :wq
+    VIM
+    system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+    assert_equal "hello python3", File.read("test.txt").chomp
+    assert_match "+gettext", shell_output("#{bin}/vim --version")
+    assert_match "+sodium", shell_output("#{bin}/vim --version")
+  end
+end
