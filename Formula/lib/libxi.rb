0495f023edf0558bbfa934e8e5098627702e57fc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-07 15:10:07 +0800

    libxi: update 1.8.2 bottle.

--- Formula/lib/libxi.rb
+++ Formula/lib/libxi.rb
@@ -7,16 +7,7 @@ class Libxi < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "02cb7bb24d30cd4ec0c6a4911e5a01d0c7d174753752671a31669048585f0c69"
-    sha256 cellar: :any,                 arm64_sequoia:  "57806e4096680d7fdbfeceea4e2aa04c3d179846b0ba00c9db8daf0319e6f40d"
-    sha256 cellar: :any,                 arm64_sonoma:   "474b22f2f4b4bf6945a3ce00400213838ba390542393cdba26c807f39694401b"
-    sha256 cellar: :any,                 arm64_ventura:  "e21e8ceae47d1b3ebabdcc9be472620b3f6f1c07aa320c3b9eb42d71d6c0d0fc"
-    sha256 cellar: :any,                 arm64_monterey: "72f13ecbdacac0c3c7479459e7f89d339c43d3f26d04cd8ff7fab7ab75938376"
-    sha256 cellar: :any,                 sonoma:         "912b8268b30dae048b52740eefbacda1b9767dc2db9469a2d34c395e0752c2b0"
-    sha256 cellar: :any,                 ventura:        "7a214e9759c6d33f6554c7941f9fb5a08c2805e514a93053f087899df79a0eae"
-    sha256 cellar: :any,                 monterey:       "e1621d00c64c524f3e511f93197e6323ae6c8f63d370730b8cc204ff1cd6c5e7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "b2b86357aea40aebb92de04c90134a812a6f0f4ead1b0188f4f60b1d1d3846b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6839d93e4dc8d53688515849ec4ec2d830946d6d26f6cfcca6402390bc0f1867"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5caad6f8b02e892021f0a4116cc60acf594b1a19a03abf84190ea3093a0b7749"
   end
 
   depends_on "pkgconf" => :build
