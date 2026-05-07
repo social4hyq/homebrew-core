c11d7e20b987d32946d387e12d9cdf9177c2a4b3
hqzing <hqzing@outlook.com>
2026-05-07 11:36:10 +0800

    bash 5.3.9 (new formula)

---
+++ Formula/b/bash.rb
@@ -0,0 +1,151 @@
+class Bash < Formula
+  desc "Bourne-Again SHell, a UNIX command interpreter"
+  homepage "https://www.gnu.org/software/bash/"
+  license "GPL-3.0-or-later"
+  compatibility_version 1
+  head "https://git.savannah.gnu.org/git/bash.git", branch: "master"
+
+  stable do
+    url "https://ftpmirror.gnu.org/gnu/bash/bash-5.3.tar.gz"
+    mirror "https://ftp.gnu.org/gnu/bash/bash-5.3.tar.gz"
+    mirror "https://mirrors.kernel.org/gnu/bash/bash-5.3.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.3.tar.gz"
+    sha256 "0d5cd86965f869a26cf64f4b71be7b96f90a3ba8b3d74e27e8e9d9d5550f31ba"
+    version "5.3.9"
+
+    # Add new patches using this format:
+    #
+    # patch_checksum_pairs = %w[
+    #   001 <checksum for 5.3.1>
+    #   002 <checksum for 5.3.2>
+    #   ...
+    # ]
+
+    patch_checksum_pairs = %w[
+      001 1f608434364af86b9b45c8b0ea3fb3b165fb830d27697e6cdfc7ac17dee3287f
+      002 e385548a00130765ec7938a56fbdca52447ab41fabc95a25f19ade527e282001
+      003 f245d9c7dc3f5a20d84b53d249334747940936f09dc97e1dcb89fc3ab37d60ed
+      004 9591d245045529f32f0812f94180b9d9ce9023f5a765c039b852e5dfc99747d0
+      005 cca1ef52dbbf433bc98e33269b64b2c814028efe2538be1e2c9a377da90bc99d
+      006 29119addefed8eff91ae37fd51822c31780ee30d4a28376e96002706c995ff10
+      007 c0976bbfffa1453c7cfdd62058f206a318568ff2d690f5d4fa048793fa3eb299
+      008 097cd723cbfb8907674ac32214063a3fd85282657ec5b4e544d2c0f719653fb4
+      009 eee30fe78a4b0cb2fe20e010e00308899cfc613e0774ebb3c8557a1552f24f8c
+    ]
+
+    patch_checksum_pairs.each_slice(2) do |p, checksum|
+      patch :p0 do
+        url "https://ftpmirror.gnu.org/gnu/bash/bash-5.3-patches/bash53-#{p}"
+        mirror "https://ftp.gnu.org/gnu/bash/bash-5.3-patches/bash53-#{p}"
+        mirror "https://mirrors.kernel.org/gnu/bash/bash-5.3-patches/bash53-#{p}"
+        mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.3-patches/bash53-#{p}"
+        sha256 checksum
+      end
+    end
+  end
+
+  # We're not using `url :stable` here because we need `url` to be a string
+  # when we use it in the `strategy` block.
+  livecheck do
+    url "https://ftpmirror.gnu.org/gnu/bash/?C=M&O=D"
+    regex(/href=.*?bash[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    strategy :gnu do |page, regex|
+      # Match versions from files
+      versions = page.scan(regex)
+                     .flatten
+                     .uniq
+                     .map { |v| Version.new(v) }
+                     .sort
+      next versions if versions.blank?
+
+      # Assume the last-sorted version is newest
+      newest_version = versions.last
+
+      # Simply return the found versions if there isn't a patches directory
+      # for the "newest" version
+      patches_directory = page.match(%r{href=.*?(bash[._-]v?#{newest_version.major_minor}[._-]patches/?)["' >]}i)
+      next versions if patches_directory.blank?
+
+      # Fetch the page for the patches directory
+      patches_page = Homebrew::Livecheck::Strategy.page_content(URI.join(@url, patches_directory[1]).to_s)
+      next versions if patches_page[:content].blank?
+
+      # Generate additional major.minor.patch versions from the patch files in
+      # the directory and add those to the versions array
+      patches_page[:content].scan(/href=.*?bash[._-]?v?\d+(?:\.\d+)*[._-]0*(\d+)["' >]/i).each do |match|
+        versions << "#{newest_version.major_minor}.#{match[0]}"
+      end
+
+      versions
+    end
+  end
+
+  no_autobump! because: :incompatible_version_format
+
+  bottle do
+    sha256 arm64_tahoe:   "075fd682409ff27a9acbbe88a1456b12826634a1669c0c99fe4d76fda574e0ef"
+    sha256 arm64_sequoia: "99ad5486305387ebc826b15f2c8ac0eb5e0c221ee03bc2b8c74902f6478d8627"
+    sha256 arm64_sonoma:  "6de5e01ff6246712ee5f78dbe2828bdffffd4bd91539963dfa6d0eb10b0dcd0f"
+    sha256 sonoma:        "16d2c963e646d30f259382a3b20c6f959b5f9c69dce11e6e62fc7a6648752612"
+    sha256 arm64_linux:   "f1dab753e73b8ba1d4569214f7d47c4e6f34f1e37a4272855143b4ee7346b157"
+    sha256 x86_64_linux:  "7f7a002baad948b82cd8e1e0a89ef8816fb3433fe6e914ac2d0ab4f50c90202f"
+  end
+
+  # System ncurses lacks functionality
+  # https://github.com/Homebrew/homebrew-core/issues/158667
+  depends_on "ncurses"
+  depends_on "readline"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  def bash_loadables_path
+    [
+      "#{HOMEBREW_PREFIX}/lib/bash",
+      # Stock Bash paths; keep them for backwards compatibility.
+      "/usr/local/lib/bash",
+      "/usr/lib/bash",
+      "/opt/local/lib/bash",
+      "/usr/pkg/lib/bash",
+      "/opt/pkg/lib/bash",
+      ".",
+    ].uniq.join(":")
+  end
+
+  def install
+    # When built with SSH_SOURCE_BASHRC, bash will source ~/.bashrc when
+    # it's non-interactively from sshd.  This allows the user to set
+    # environment variables prior to running the command (e.g. PATH).  The
+    # /bin/bash that ships with macOS defines this, and without it, some
+    # things (e.g. git+ssh) will break if the user sets their default shell to
+    # Homebrew's bash instead of /bin/bash.
+    ENV.append_to_cflags "-DSSH_SOURCE_BASHRC"
+
+    ENV.append_to_cflags "-DDEFAULT_LOADABLE_BUILTINS_PATH='\"#{bash_loadables_path}\"'"
+
+    # Avoid crashes on macOS 15.0-15.4.
+    ENV["bash_cv_func_strchrnul_works"] = "no" if OS.mac? && MacOS.version <= :sequoia
+
+    system "./configure", "--prefix=#{prefix}", "--with-curses", "--with-installed-readline", "--with-bash-malloc=no"
+    system "make", "install"
+
+    (include/"bash/builtins").install lib/"bash/loadables.h"
+    pkgshare.install lib.glob("bash/Makefile*")
+  end
+
+  def caveats
+    "DEFAULT_LOADABLE_BUILTINS_PATH: #{bash_loadables_path}"
+  end
+
+  test do
+    assert_equal "hello", shell_output("#{bin}/bash -c 'echo -n hello'")
+    # If the following assertion breaks, then it's likely the configuration of `DEFAULT_LOADABLE_BUILTINS_PATH`
+    # is broken. Changing the test below will probably hide that breakage.
+    assert_equal "csv is a shell builtin\n", shell_output("#{bin}/bash -c 'enable csv; type csv'")
+
+    return if OS.linux? || MacOS.version > :sequoia
+
+    refute_match "U _strchrnul", shell_output("nm #{bin}/bash")
+  end
+end
