80267b21edd37e7d6f5c93079f7ef1012045024a
hqzing <hqzing@outlook.com>
2026-05-08 12:05:22 +0800

    jpeg-turbo 3.1.4.1 (new formula)

---
+++ Formula/j/jpeg-turbo.rb
@@ -0,0 +1,74 @@
+class JpegTurbo < Formula
+  desc "JPEG image codec that aids compression and decompression"
+  homepage "https://www.libjpeg-turbo.org/"
+  url "https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/3.1.4.1/libjpeg-turbo-3.1.4.1.tar.gz"
+  sha256 "ecae8008e2cc9ade2f2c1bb9d5e6d4fb73e7c433866a056bd82980741571a022"
+  license all_of: [
+    "IJG", # libjpeg API library and programs
+    "Zlib", # libjpeg-turbo SIMD source code
+    "BSD-3-Clause", # TurboJPEG API library and programs
+  ]
+  compatibility_version 1
+  head "https://github.com/libjpeg-turbo/libjpeg-turbo.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "9a0e281d4e1b3047460f4d0d7e291790a136c32677532fd62bea6bb6428acec6"
+    sha256 cellar: :any,                 arm64_sequoia: "ec354654375dbeeba9dd637ecbfafb7c3e1132138bbcbf8a0beb08ec1e4dad4b"
+    sha256 cellar: :any,                 arm64_sonoma:  "42f79e8bea4e55f60ff202617c82527bae17eec9a2575d57833fe510634d360d"
+    sha256 cellar: :any,                 sonoma:        "0412ff07188c22bc04957ae45382873aef059fa3efee3ae52c974c27cbec70fa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c20b1d8af8574deef518f29e7711a330aaa34dd2e0c2fbab2ac5ad81c5fb1764"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2cdefb3a7bdb35b19658d014d7659e6f47f472edb4e88269bb3db7fe45c0dcff"
+  end
+
+  depends_on "cmake" => :build
+
+  on_intel do
+    # Required only for x86 SIMD extensions.
+    depends_on "nasm" => :build
+  end
+
+  # These conflict with `jpeg`, which is now keg-only.
+  link_overwrite "bin/cjpeg", "bin/djpeg", "bin/jpegtran", "bin/rdjpgcom", "bin/wrjpgcom"
+  link_overwrite "include/jconfig.h", "include/jerror.h", "include/jmorecfg.h", "include/jpeglib.h"
+  link_overwrite "lib/libjpeg.dylib", "lib/libjpeg.so", "lib/libjpeg.a", "lib/pkgconfig/libjpeg.pc"
+  link_overwrite "share/man/man1/cjpeg.1", "share/man/man1/djpeg.1", "share/man/man1/jpegtran.1",
+                 "share/man/man1/rdjpgcom.1", "share/man/man1/wrjpgcom.1"
+
+  def install
+    args = ["-DWITH_JPEG8=1", "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,#{rpath}"]
+    if Hardware::CPU.arm? && OS.mac?
+      if MacOS.version >= :ventura
+        # https://github.com/libjpeg-turbo/libjpeg-turbo/issues/709
+        args += ["-DFLOATTEST8=fp-contract",
+                 "-DFLOATTEST12=fp-contract"]
+      elsif MacOS.version == :monterey
+        # https://github.com/libjpeg-turbo/libjpeg-turbo/issues/734
+        args << "-DFLOATTEST12=no-fp-contract"
+      end
+    end
+    args += std_cmake_args
+
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "ctest", "--test-dir", "build", "--rerun-failed", "--output-on-failure", "--parallel", ENV.make_jobs
+    system "cmake", "--install", "build"
+
+    # Avoid rebuilding dependents that hard-code the prefix.
+    inreplace [lib/"pkgconfig/libjpeg.pc", lib/"pkgconfig/libturbojpeg.pc"],
+              prefix, opt_prefix
+  end
+
+  test do
+    system bin/"jpegtran", "-crop", "1x1",
+                           "-transpose",
+                           "-perfect",
+                           "-outfile", "out.jpg",
+                           test_fixtures("test.jpg")
+    assert_path_exists testpath/"out.jpg"
+  end
+end
