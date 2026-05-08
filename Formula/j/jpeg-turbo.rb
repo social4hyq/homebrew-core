74b30229a716cbe5e36faef6fb17e501d03d3b31
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-08 12:10:39 +0800

    jpeg-turbo: update 3.1.4.1 bottle.

--- Formula/j/jpeg-turbo.rb
+++ Formula/j/jpeg-turbo.rb
@@ -17,12 +17,7 @@ class JpegTurbo < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "9a0e281d4e1b3047460f4d0d7e291790a136c32677532fd62bea6bb6428acec6"
-    sha256 cellar: :any,                 arm64_sequoia: "ec354654375dbeeba9dd637ecbfafb7c3e1132138bbcbf8a0beb08ec1e4dad4b"
-    sha256 cellar: :any,                 arm64_sonoma:  "42f79e8bea4e55f60ff202617c82527bae17eec9a2575d57833fe510634d360d"
-    sha256 cellar: :any,                 sonoma:        "0412ff07188c22bc04957ae45382873aef059fa3efee3ae52c974c27cbec70fa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c20b1d8af8574deef518f29e7711a330aaa34dd2e0c2fbab2ac5ad81c5fb1764"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2cdefb3a7bdb35b19658d014d7659e6f47f472edb4e88269bb3db7fe45c0dcff"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e06f7b29b035d7a7118655e133e80ff7144eab519a5f0948b4ab7d8ef932c41a"
   end
 
   depends_on "cmake" => :build
