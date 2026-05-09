be9bea4fcfaaef3f0066264e20529e933f45a012
hqzing <hqzing@outlook.com>
2026-05-09 07:23:59 +0000

    docutils 0.22.4 (new formula)

---
+++ Formula/d/docutils.rb
@@ -0,0 +1,44 @@
+class Docutils < Formula
+  include Language::Python::Virtualenv
+
+  desc "Text processing system for reStructuredText"
+  homepage "https://docutils.sourceforge.io"
+  url "https://files.pythonhosted.org/packages/ae/b6/03bb70946330e88ffec97aefd3ea75ba575cb2e762061e0e62a213befee8/docutils-0.22.4.tar.gz"
+  sha256 "4db53b1fde9abecbb74d91230d32ab626d94f6badfc575d6db9194a49df29968"
+  license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "858cbe0370c24a44e6225f3209232b5378ec399c3d5ecd9c0adc9511d4ee1cc2"
+  end
+
+  depends_on "python@3.14"
+
+  def install
+    virtualenv_install_with_resources
+    bin.glob("*.py") do |f|
+      bin.install_symlink f => f.basename(".py")
+    end
+  end
+
+  test do
+    (testpath/"README.txt").write <<~EOS
+      .. include:: docs/header0.txt
+
+      =========================
+      README: Docutils
+      =========================
+
+      :Author: David Goodger
+      :Contact: goodger@python.org
+      :Date: $Date: 2023-05-09 20:32:19 +0200 (Di, 09. Mai 2023) $
+      :Web site: https://docutils.sourceforge.io/
+      :Copyright: This document has been placed in the public domain.
+
+      .. contents::
+    EOS
+
+    mkdir_p testpath/"docs"
+    touch testpath/"docs/header0.txt"
+    system bin/"rst2man", testpath/"README.txt"
+  end
+end
