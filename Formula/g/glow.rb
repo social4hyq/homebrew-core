700874b83a11b448479df26fe46d3d531777a9ef
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 16:41:36 +0800

    glow: update 2.1.2 bottle.

--- Formula/g/glow.rb
+++ Formula/g/glow.rb
@@ -7,12 +7,7 @@ class Glow < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "efafef80669a8776fd80e6126ee3f874dde5cf1ae25260c90ab843543f56c0a7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "efafef80669a8776fd80e6126ee3f874dde5cf1ae25260c90ab843543f56c0a7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "efafef80669a8776fd80e6126ee3f874dde5cf1ae25260c90ab843543f56c0a7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5147e48f551c32ef0753e98927895c8468ca97a4896b641423640d2d27381058"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d45a6d2d331f5409907eab72abfaa9268edf1eb6a9dbf4d75a31347c258d29d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c0bb657054bb32edd7f3823268f2f76b5871edbd731769088eb7e8a30ae2356"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ac1040b2e1851988db3a8144349510afba9d82b97be3f18eb30aef09860c0f91"
   end
 
   depends_on "go" => :build
