8ebaecd4ae17c210585a7d68fe2d8b14776a01f1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 17:18:53 +0800

    hello: update 2.12.3 bottle.

--- Formula/h/hello.rb
+++ Formula/h/hello.rb
@@ -6,14 +6,7 @@ class Hello < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1d542fba541d655b431c64e07b0a2f020fa1b47cf7213bddec73d51f6b1246e4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce9f4dd817532429fb3b890a901da44fa66f0540e242321f40fa12231f5ffa60"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c2d2936b723280b7ea2fd38d977fbce0c20919e3a483229d0b4f4c96800e4d7c"
-    sha256 cellar: :any_skip_relocation, tahoe:         "d3c167221f723235eed887d3116ba656ecb4d2c824e4a04037bf63dca96ce227"
-    sha256 cellar: :any_skip_relocation, sequoia:       "d0ec57cc9a6a0ed8d035d8f19073d83f5dba21007c0d2815333d4908d40f98a8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c55ce0232ee793fee24cea7dfce44f323e9b006eb49e6ff381678614e22aa3b5"
-    sha256                               arm64_linux:   "1758c42f89b579961610910892bf56d9d637614a4bd505265ebce6e09240e397"
-    sha256                               x86_64_linux:  "babafecd3b1c532d4e799de46f943f6384b5d78beffb52c9f4730e30ad374a70"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "895b21c57ce40a73ad02896b90ed7a4f6653fcdb5a804e8927e94a9823929af9"
   end
 
   def install
