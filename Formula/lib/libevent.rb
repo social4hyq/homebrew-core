e95246e4e9cb4fda7e1081b69d0dbf55fb4a4163
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 16:53:14 +0800

    libevent: update 2.1.12_1 bottle.

--- Formula/lib/libevent.rb
+++ Formula/lib/libevent.rb
@@ -13,18 +13,7 @@ class Libevent < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "c442ba9d640780f91257a853002b25840439b9c6b1c568864111901b760d328e"
-    sha256 cellar: :any,                 arm64_sequoia:  "65fc7c61fec0f5ae0c5dfc8fc7e3b6b0507d3f1c7c308a332802541f00334963"
-    sha256 cellar: :any,                 arm64_sonoma:   "38a3eb3510a7e0cd4096e4592d0095c562eb1bbad572d951f1923009a14ad702"
-    sha256 cellar: :any,                 arm64_ventura:  "a75d453a7fe2aba1eaba334621b7bd9f0ff6f9e1f04aa400565f68711a9f6db4"
-    sha256 cellar: :any,                 arm64_monterey: "a24d682548fb7cb11c127932240cced5d6fdb16feaaa6dc2ab3a7f0833e5df2e"
-    sha256 cellar: :any,                 arm64_big_sur:  "0c3deccd564c0ed001cb3613ddc10d7e46e78deb5f8882fde74f8935557d5cba"
-    sha256 cellar: :any,                 sonoma:         "5d54f13cd93d87185bd7bb592cb945d1f64cac3e88d1e46c2fb5d9ea538d9623"
-    sha256 cellar: :any,                 ventura:        "79a1036d3428c6ad8325803912e9ff0f74b8a8202908ae8594959c27e998c90b"
-    sha256 cellar: :any,                 monterey:       "d0557018f19021fb4675a20d9cefda5e13646558c276ab7b4f01f96144b432f8"
-    sha256 cellar: :any,                 big_sur:        "042923957c025a4298465d320a63db6127414644fde58fcdc0d29e8c28fd2993"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "2ebd43e299f937313fc4dc17c21a7446b53d5a46d7dad29922a763df1761acac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83ef4ce689a91f6fca013d6b4b0b2fcda3706080f8e0cccd056a3d94d6bc0f17"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "80c9e196e7deb8fe81d8c518683c1220fb17a15ef51772c2d45f97cc5c5efb6f"
   end
 
   depends_on "autoconf" => :build
