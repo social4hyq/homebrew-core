bfbf5733c2323246c75b6ea1ac35369113cf7893
lanfungcing <lanfq.26@qq.com>
2026-05-09 06:25:06 +0000

    doctest 2.5.2 (new formula)

---
+++ Formula/d/doctest.rb
@@ -0,0 +1,39 @@
+class Doctest < Formula
+  desc "Feature-rich C++11/14/17/20/23 single-header testing framework"
+  homepage "https://github.com/doctest/doctest"
+  url "https://github.com/doctest/doctest/archive/refs/tags/v2.5.2.tar.gz"
+  sha256 "9189960c2bbbc4f3382ce0773b2bb5f13e3afd8fed47f55f193e11e85a4f9854"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "327aba0814a710bea52f932aec32c4843540b648b99a3c4c638c7d8ef9aec576"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DDOCTEST_WITH_TESTS=OFF", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+      #include <doctest/doctest.h>
+      TEST_CASE("Basic") {
+        int x = 1;
+        SUBCASE("Test section 1") {
+          x = x + 1;
+          REQUIRE(x == 2);
+        }
+        SUBCASE("Test section 2") {
+          REQUIRE(x == 1);
+        }
+      }
+    CPP
+
+    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    system "./test"
+  end
+end
