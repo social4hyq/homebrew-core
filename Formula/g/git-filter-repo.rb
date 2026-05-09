db640caa727fdf16bf110ff96f90c7769cc10d81
lanfungcing <lanfq.26@qq.com>
2026-05-09 08:29:57 +0000

    git-filter-repo 2.47.0 (new formula)

---
+++ Formula/g/git-filter-repo.rb
@@ -0,0 +1,41 @@
+class GitFilterRepo < Formula
+  include Language::Python::Shebang
+
+  desc "Quickly rewrite git repository history"
+  homepage "https://github.com/newren/git-filter-repo"
+  url "https://github.com/newren/git-filter-repo/releases/download/v2.47.0/git-filter-repo-2.47.0.tar.xz"
+  sha256 "4662cbe5918196a9f1b5b3e1211a32e61cff1812419c21df4f47c5439f09e902"
+  license "MIT"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "774b68744bca239b25b7e82693204f198c6ab1224ded38d8dd33ec1c23c245d8"
+  end
+
+  uses_from_macos "git"
+  uses_from_macos "python"
+
+  def install
+    rewrite_shebang detected_python_shebang(use_python_from_path: true), "git-filter-repo"
+    bin.install "git-filter-repo"
+    man1.install "Documentation/man1/git-filter-repo.1"
+  end
+
+  test do
+    system bin/"git-filter-repo", "--version"
+
+    system "git", "init"
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "BrewTestBot@example.com"
+
+    touch "foo"
+    system "git", "add", "foo"
+    system "git", "commit", "-m", "foo"
+    # Use --force to accept non-fresh clone run:
+    # Aborting: Refusing to overwrite repo history since this does not look like a fresh clone.
+    # (expected freshly packed repo)
+    system bin/"git-filter-repo", "--path-rename=foo:bar", "--force"
+
+    assert_path_exists testpath/"bar"
+  end
+end
