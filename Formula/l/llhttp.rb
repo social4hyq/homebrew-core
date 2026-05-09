5eefb00aac84c035e41fba86a26e8a6d2278107b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 11:38:11 +0800

    llhttp: update 9.4.1 bottle.

--- Formula/l/llhttp.rb
+++ Formula/l/llhttp.rb
@@ -7,12 +7,7 @@ class Llhttp < Formula
   compatibility_version 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "008c27ccda18513f7b62a6ad142774acecc300cd6eeac9f5aac8fef748495ae0"
-    sha256 cellar: :any,                 arm64_sequoia: "2e8334042c1afd61dcbdde7e24472889cf323cbec3d251b669ba9f5e79476bee"
-    sha256 cellar: :any,                 arm64_sonoma:  "5cc1eba5deaab8a20bf1ba35469db8d693f6cf75726e0d88ae0c4170190df389"
-    sha256 cellar: :any,                 sonoma:        "9d0428367d420235836b13a4d9db987a55c341b4e8eeea4756824827c85c8c41"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "44cc7ec6552fbbb70d026809c0c2a88caacad11e7eeeba9a9c02b1aa6cb86ac3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b6fa60dfe50e4abe067f2295ffea6c4668fbfdec401edd73c6e16d70b9a8c6de"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3f22ec69f661b117072b59a36e8e915c858e4fdb123c0b132d1778473d94a8f1"
   end
 
   depends_on "cmake" => :build
