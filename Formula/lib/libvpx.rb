18614e55f927b9f27ef801cdf00e31ab768a16d0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 16:17:37 +0800

    libvpx: update 1.16.0 bottle.

--- Formula/lib/libvpx.rb
+++ Formula/lib/libvpx.rb
@@ -8,12 +8,7 @@ class Libvpx < Formula
   head "https://chromium.googlesource.com/webm/libvpx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "38d28c66d270e2d9b1826f16ec4721b6532a1c59c9ace22811cca3aac43b06ab"
-    sha256 cellar: :any,                 arm64_sequoia: "4f72f10109e23239ba65d4e7c1eb9086a4b2cd2875f784c8f9d79d713106675d"
-    sha256 cellar: :any,                 arm64_sonoma:  "dbc1b002c52c40ae7d892801b81be82cc1666cd7952273037efc8027a5126eba"
-    sha256 cellar: :any,                 sonoma:        "198eadc026da61065d4f3c626c89bdc770c9109311e0b59db4f5ab0487345dea"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ecd53d77a7fa7af9ae5878f5f52483be94eb6ea08264059fcc59707909c41dcb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "980eac19f80348cc5e5f27f06410a3d724b86727ec6f128355c7ee1dd4b3b09d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e5f8fae29b2a482dd67ee1b8b72b1235d93f8754b4d568aa138d4aae26fea248"
   end
 
   on_intel do
