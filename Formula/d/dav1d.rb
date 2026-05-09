e4187e99d722288559e3c72e624f6d105ebdf520
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 17:33:22 +0800

    dav1d: update 1.5.3 bottle.

--- Formula/d/dav1d.rb
+++ Formula/d/dav1d.rb
@@ -7,12 +7,7 @@ class Dav1d < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "cda8a6b7d3184cb2a083b8629ae5f9b00f8e04b36da71d8f56f149cbdeebfdbf"
-    sha256 cellar: :any,                 arm64_sequoia: "9502a86f722756284734b724206d21783a0863406462abf8d43fe52d5232bad5"
-    sha256 cellar: :any,                 arm64_sonoma:  "fe0db93877e6734a127c1cb8dd98293d2016b830fd2ec36fb8985e36e92d7611"
-    sha256 cellar: :any,                 sonoma:        "8697d509b54358c4ad2b8a370841ee5244431d2d85c2883bb7624f60c59ec7b6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "89f1cacc3d5d6de20cd85b098e0abed0eaf552dbda07cb3715cf06decf5c1fcc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8dae4c806758642d9e2dc6d064b0f62dd7b692c951c2bba171394ac94e69a8d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e979912353b4ac962459c16473859bdab102ddccf28083176c5b670930c2bf25"
   end
 
   depends_on "meson" => :build
