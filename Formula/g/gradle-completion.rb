b2714a4c459cc391160fe407a6fd612446901682
lanfungcing <lanfq.26@qq.com>
2026-05-09 08:54:47 +0000

    gradle-completion 9.5.0 (new formula)

---
+++ Formula/g/gradle-completion.rb
@@ -0,0 +1,23 @@
+class GradleCompletion < Formula
+  desc "Bash and Zsh completion for Gradle"
+  homepage "https://gradle.org/"
+  url "https://github.com/gradle/gradle-completion/archive/refs/tags/v9.5.0.tar.gz"
+  sha256 "59354e68f2f0f38a542ea0010be32c14aacfd6184523f209a5e31481964566c8"
+  license "MIT"
+  compatibility_version 1
+  head "https://github.com/gradle/gradle-completion.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "396b01159d221b4b92518a142a17a293703bf9d371f8ce11ef0d378b3b157a65"
+  end
+
+  def install
+    bash_completion.install "gradle-completion.bash" => "gradle"
+    zsh_completion.install "_gradle" => "_gradle"
+  end
+
+  test do
+    assert_match "-F _gradle",
+      shell_output("bash -c 'source #{bash_completion}/gradle && complete -p gradle'")
+  end
+end
