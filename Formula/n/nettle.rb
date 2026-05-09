e96121b60aa4eeccfd915d4459ec3e1fd96cadd8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-10 06:04:18 +0800

    nettle 4.0

--- Formula/n/nettle.rb
+++ Formula/n/nettle.rb
@@ -1,9 +1,9 @@
 class Nettle < Formula
   desc "Low-level cryptographic library"
   homepage "https://www.lysator.liu.se/~nisse/nettle/"
-  url "https://ftpmirror.gnu.org/gnu/nettle/nettle-3.10.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/nettle/nettle-3.10.2.tar.gz"
-  sha256 "fe9ff51cb1f2abb5e65a6b8c10a92da0ab5ab6eaf26e7fc2b675c45f1fb519b5"
+  url "https://ftpmirror.gnu.org/gnu/nettle/nettle-4.0.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/nettle/nettle-4.0.tar.gz"
+  sha256 "3addbc00da01846b232fb3bc453538ea5468da43033f21bb345cb1e9073f5094"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
   compatibility_version 1
 
