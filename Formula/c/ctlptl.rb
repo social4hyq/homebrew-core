daf6fc468ba39e23e00c42237a7bb3335663275b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 15:11:48 +0800

    ctlptl: update 0.9.3 bottle.

--- Formula/c/ctlptl.rb
+++ Formula/c/ctlptl.rb
@@ -7,12 +7,7 @@ class Ctlptl < Formula
   head "https://github.com/tilt-dev/ctlptl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cf53047d8143436c909cf232df8d6f45f8385b8652c5048a44adba479a480195"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b557bc10a760fbd1f8ebd4e1bd6fc13739361878fbab37802de0c430d8d4f66"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c4837c14340eb4925a2ca6d0d7058c84208c996ad75bf1d898512b434d2718c5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a09d4c257850d99a380a9d8dbcb429bbb943391a1676be9768fc572096272e07"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "89fed1d8bd8e74377ba293ee6429d62b974e6629c22d040a5b863c714335bddd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "842be208e40f2e2a8e7d2484f78287ed5d6c3d2dbcc9cbf8a432e5c7782bd24e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "748f621639e9111acb6f6993e63c8966e3624f0cd0d1c7a27e1e5a2350fcace2"
   end
 
   depends_on "go" => :build
