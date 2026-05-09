387a56b1673c2b535954042e0c1a5d94cbeb1354
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 15:41:15 +0800

    svt-av1: update 4.1.0 bottle.

--- Formula/s/svt-av1.rb
+++ Formula/s/svt-av1.rb
@@ -8,12 +8,7 @@ class SvtAv1 < Formula
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "ef9741a91c2155055fa3dd9682776ac0c59014e0757ea92c789c6d0592273b86"
-    sha256 cellar: :any,                 arm64_sequoia: "dfd452b9d5f752a679a6f810fcdb3906b6884dbc8a279abe558dc61c9bfc3c8a"
-    sha256 cellar: :any,                 arm64_sonoma:  "b3f0cea562b0fcc454889b6aa6f1453be515faf87600021de6a4351c3e6018bf"
-    sha256 cellar: :any,                 sonoma:        "1fcfdbcd821d7267e82e58445931d5b8e6c17d40b4a3a44ff32f715c1b1edcc6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "58cb3abfa77fcdf297a32ba2715a53fd2cd60571ca776632401d6334ac03c0ee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ebe93ba0e92ec1fcfff2bb9b360e09442a90c91aaa7b5dcd2392c74d04a8406"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ea0d2264948cb062b0c5efbaf86038108c33962442e329c91602573267c0430a"
   end
 
   depends_on "cmake" => :build
