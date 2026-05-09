8992831a53615f7a8b818f56122709a508dd8f2b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-09 17:35:08 +0800

    hwloc: update 2.13.0 bottle.

--- Formula/h/hwloc.rb
+++ Formula/h/hwloc.rb
@@ -12,12 +12,7 @@ class Hwloc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "9f8b4882a8b58c18900e03bb2ecadabc828ba3a50978fa33e4d9c8f4c83fe674"
-    sha256 cellar: :any,                 arm64_sequoia: "ad9de3dc933720a5e61ef567bee28793b5c283055d51a79e984133b37cc0d635"
-    sha256 cellar: :any,                 arm64_sonoma:  "841bca31067f7ae880010f9abacfc9bb59f2d675d51afdfebd768b06b1cbb7de"
-    sha256 cellar: :any,                 sonoma:        "2232073d9ed0f9d2783309ac0963b0042b2b9db381699a5b023cee251be0c86a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "147461c3c53fb69fa1985ede4a7a1da3d658c675f6d380d51788b7d29c457fa5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35e4c018c3758ea1f9a6124ea34c1832a71b34b97c5fdab3fe25343f9f2f5fb4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "90f96e5e1931c2405f7bcbc43d09d0f7d1d8d259ea99e5a3efab01777acc554c"
   end
 
   head do
