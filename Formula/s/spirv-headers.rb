043ce0bb78ba0329ed5923e67ea44ac13550152a
hqzing <hqzing@outlook.com>
2026-05-09 07:21:06 +0000

    spirv-headers 1.4.341.0 (new formula)

---
+++ Formula/s/spirv-headers.rb
@@ -0,0 +1,43 @@
+class SpirvHeaders < Formula
+  desc "Headers for SPIR-V"
+  homepage "https://github.com/KhronosGroup/SPIRV-Headers"
+  url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/vulkan-sdk-1.4.341.0.tar.gz"
+  sha256 "cab0a654c4917e16367483296b44cdb1d614e3120c721beafcd37e3a8580486c"
+  license "MIT"
+  head "https://github.com/KhronosGroup/SPIRV-Headers.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^(?:vulkan[._-])?sdk[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "efb9f1b78eb2c873093671ddcace25f80d411a5dc4eb53c83f96e8a433bfc6ec"
+  end
+
+  depends_on "cmake" => [:build, :test]
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "tests"
+  end
+
+  test do
+    cp pkgshare/"tests/example.cpp", testpath
+
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.14)
+
+      add_library(SPIRV-Headers-example
+                  ${CMAKE_CURRENT_SOURCE_DIR}/example.cpp)
+      target_include_directories(SPIRV-Headers-example
+                  PRIVATE ${SPIRV-Headers_SOURCE_DIR}/include)
+    CMAKE
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+  end
+end
