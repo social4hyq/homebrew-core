d93b6eaaf7efee0a149b79b1a00d1c5ff3e6a11f
lanfungcing <lanfq.26@qq.com>
2026-05-11 07:35:42 +0000

    pipx 1.12.0 (new formula)

---
+++ Formula/p/pipx.rb
@@ -0,0 +1,71 @@
+class Pipx < Formula
+  include Language::Python::Virtualenv
+
+  desc "Execute binaries from Python packages in isolated environments"
+  homepage "https://pipx.pypa.io"
+  url "https://files.pythonhosted.org/packages/6b/b8/dcf992895f73b29d66ba21ad8032c68fd6d3ff02ac4bb9357d3a7369eb02/pipx-1.12.0.tar.gz"
+  sha256 "a25ae54944c116b7dc53440aab71975f29f4f9fe4bc478cf9ddb123bed3e00f5"
+  license "MIT"
+  head "https://github.com/pypa/pipx.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "cc1d2afd2d26517058563da939cbaab8bb4b3ec7a97c2cde78f455d761ef3cb8"
+  end
+
+  depends_on "python@3.14"
+
+  resource "argcomplete" do
+    url "https://files.pythonhosted.org/packages/38/61/0b9ae6399dd4a58d8c1b1dc5a27d6f2808023d0b5dd3104bb99f45a33ff6/argcomplete-3.6.3.tar.gz"
+    sha256 "62e8ed4fd6a45864acc8235409461b72c9a28ee785a2011cc5eb78318786c89c"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/bb/63/f9e1ea081ce35720d8b92acde70daaedace594dc93b693c869e0d5910718/click-8.3.3.tar.gz"
+    sha256 "398329ad4837b2ff7cbe1dd166a4c0f8900c3ca3a218de04466f38f6497f18a2"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/d7/f1/e7a6dd94a8d4a5626c03e4e99c87f241ba9e350cd9e6d75123f992427270/packaging-26.2.tar.gz"
+    sha256 "ff452ff5a3e828ce110190feff1178bb1f2ea2281fa2075aadb987c2fb221661"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/9f/4a/0883b8e3802965322523f0b200ecf33d31f10991d0401162f4b23c698b42/platformdirs-4.9.6.tar.gz"
+    sha256 "3bfa75b0ad0db84096ae777218481852c0ebc6c727b3168c1b9e0118e458cf0a"
+  end
+
+  resource "userpath" do
+    url "https://files.pythonhosted.org/packages/d5/b7/30753098208505d7ff9be5b3a32112fb8a4cb3ddfccbbb7ba9973f2e29ff/userpath-1.9.2.tar.gz"
+    sha256 "6c52288dab069257cc831846d15d48133522455d4677ee69a9781f11dbefd815"
+  end
+
+  def python3
+    deps.map(&:to_formula)
+        .find { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    # Avoid Cellar path reference, which is only good for one version.
+    inreplace "src/pipx/interpreter.py",
+              "DEFAULT_PYTHON = _get_sys_executable()",
+              "DEFAULT_PYTHON = '#{python3.opt_libexec/"bin/python"}'"
+
+    virtualenv_install_with_resources
+
+    generate_completions_from_executable(libexec/"bin/register-python-argcomplete", "pipx",
+                                         shell_parameter_format: :arg)
+
+    # Build an `:all` bottle by replacing comments
+    site_packages = libexec/Language::Python.site_packages("python3")
+    file = site_packages/"argcomplete-#{resource("argcomplete").version}.dist-info/METADATA"
+    inreplace file, "/opt/homebrew/bin/bash", "$HOMEBREW_PREFIX/bin/bash"
+  end
+
+  test do
+    assert_match "PIPX_HOME", shell_output("#{bin}/pipx --help")
+    system bin/"pipx", "install", "csvkit"
+    assert_path_exists testpath/".local/bin/csvjoin"
+    system bin/"pipx", "uninstall", "csvkit"
+    refute_match "csvjoin", shell_output("#{bin}/pipx list")
+  end
+end
