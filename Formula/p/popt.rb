55789455c5fabbe92772972ea692f992cc9f1ea3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 15:43:56 +0800

    popt: update 1.19 bottle.

--- Formula/p/popt.rb
+++ Formula/p/popt.rb
@@ -17,19 +17,7 @@ class Popt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "365d143d0d38003c60c1d6fb6d399ab4361690a64467d46a8f27c2f50d2dcfb6"
-    sha256 cellar: :any,                 arm64_sequoia:  "4ff5f0c8c34f510a7336b16f42a6e058d028ff2025e9b01093b294be84d90bb2"
-    sha256 cellar: :any,                 arm64_sonoma:   "2fb8801217b8bcddc5c15974478b7fd89992868fdd6b09360835e7b6f0cef791"
-    sha256 cellar: :any,                 arm64_ventura:  "9cabf84985466e8531cff42433a8df6b16668222537544b0295dab0cef292e53"
-    sha256 cellar: :any,                 arm64_monterey: "1154aeb3aedee17c3dddb8f7896f4b5f6b4d7d9dc5334fd1011fb96768788e9c"
-    sha256 cellar: :any,                 arm64_big_sur:  "36a746fdc0e913f77421aebbde75099112fb452beeca9d5420cd1d3907802fa6"
-    sha256 cellar: :any,                 sonoma:         "0eab78dae2d0e5dab0d54f51c43fa8fbbfd777628078c4318622ae9416f6dbcd"
-    sha256 cellar: :any,                 ventura:        "4826c003aae6f5407fc0f7e9db8ae2a8e8aad55fab65e1556d7a8db300af8110"
-    sha256 cellar: :any,                 monterey:       "7d6cc173811a3aa97adae35f9c0a759acec73e0bc2c948fdb012f2691b4aef3d"
-    sha256 cellar: :any,                 big_sur:        "92d031d6010ce339beb5d179471ac14b76e887cba2d8cd7699c41aa1d76bcee5"
-    sha256 cellar: :any,                 catalina:       "bab861fc16b94cf46a1a438503ea5e7a602a2c750be14706210e83f63f4abaaa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "5b137c17f2231710bf51430cb36d013b38bfa8ddef947adcf7a000d7ece9660c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eaf6aef4cf756e5ad7d75b04d51efb181e673be05bbc40053123b7e8f975db76"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cb7b9a53fc16c53dbe5b6c388b2525abb538ea912a751027b22c95a80cab4699"
   end
 
   def install
