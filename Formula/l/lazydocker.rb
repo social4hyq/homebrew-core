facd13f83a2db0d45b64d474d64c81fe997f29ad
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 10:48:32 +0800

    lazydocker: update 0.25.2 bottle.

--- Formula/l/lazydocker.rb
+++ Formula/l/lazydocker.rb
@@ -7,12 +7,7 @@ class Lazydocker < Formula
   head "https://github.com/jesseduffield/lazydocker.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8522a4229dea2b63f88c29a5772f5ba7321bd8637c6dee299e266a14c29cffe5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8522a4229dea2b63f88c29a5772f5ba7321bd8637c6dee299e266a14c29cffe5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8522a4229dea2b63f88c29a5772f5ba7321bd8637c6dee299e266a14c29cffe5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "68b8d5ae390f69c33aed50d4089b4d7a8087e27110b172e065f8e84efea08584"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6266094262f876ff6e9209f067d25587d1845904c98de4fa478106a8e942d32f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "491c528838ce7c9209659601a4aa742785fde0b8a22b7dd4c556d6952ecbcb58"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "03e1ce0ce7e2c09c39d44f82ae1910a95f43a06f9e11a17afab66b3cce873a33"
   end
 
   depends_on "go" => :build
