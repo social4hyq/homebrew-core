321122b837626a60faa955cd236e6c1cd1a41744
hqzing <hqzing@outlook.com>
2026-05-11 20:00:22 +0800

    python@3.14: enable-shared

--- Formula/p/python@3.14.rb
+++ Formula/p/python@3.14.rb
@@ -4,7 +4,7 @@ class PythonAT314 < Formula
   url "https://www.python.org/ftp/python/3.14.4/Python-3.14.4.tgz"
   sha256 "b4c059d5895f030e7df9663894ce3732bfa1b32cd3ab2883980266a45ce3cb3b"
   license "Python-2.0"
-  revision 1
+  revision 2
   compatibility_version 1
 
   livecheck do
@@ -147,7 +147,7 @@ class PythonAT314 < Formula
     cflags         = []
     cflags_nodist  = ["-I#{HOMEBREW_PREFIX}/include"]
     ldflags        = ["-lintl"]
-    ldflags_nodist = ["-L#{HOMEBREW_PREFIX}/lib", "-Wl,-rpath,#{HOMEBREW_PREFIX}/lib"]
+    ldflags_nodist = ["-L#{HOMEBREW_PREFIX}/lib", "-Wl,-rpath,#{HOMEBREW_PREFIX}/lib", "-Wl,-z,global"]
     cppflags       = ["-I#{HOMEBREW_PREFIX}/include"]
 
     if OS.mac?
@@ -162,6 +162,7 @@ class PythonAT314 < Formula
       # Avoid linking to libgcc https://mail.python.org/pipermail/python-dev/2012-February/116205.html
       args << "MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}"
     else
+      args << "--enable-shared"
       args << "--with-dbmliborder="
     end
 
