594f6ac3fef94c092b8a09244ef9a47cb191e0a5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 14:24:18 +0800

    libgcrypt: update 1.12.2 bottle.

--- Formula/lib/libgcrypt.rb
+++ Formula/lib/libgcrypt.rb
@@ -12,12 +12,7 @@ class Libgcrypt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a28d181fc09720c699b5fefe62d41b3c75ad8ae73a080c989faa2acf6925d06d"
-    sha256 cellar: :any,                 arm64_sequoia: "8f05385898305e7f3f8717aead5162962b7266d1f25f8eb6b1f1aaf5ced87139"
-    sha256 cellar: :any,                 arm64_sonoma:  "562d16658a0786b0be1ae3bb764ce4e66d9215d0ca0007d8df0c800fe192ab2b"
-    sha256 cellar: :any,                 sonoma:        "72ec986ea68038405a419bfe5f646aa4b71f0e214517873d154b300b4ce71fbc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d7154aabeee4d72b91369b7f7b9bfecd25c793fb4e4fcfadfc41b3f23197e012"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "941be22c944a367fc59d0dac07bce43ee16b3ea8b634389813bd9d2337a01926"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "caf686b739b3d251b0fba85c49a2cf7a77cf54b1b71cb87017c6704a99a200f3"
   end
 
   depends_on "libgpg-error"
