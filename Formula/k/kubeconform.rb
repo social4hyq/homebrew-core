0be7564c5e1ac21c79b680861bd418f12f36eef4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 14:13:20 +0800

    kubeconform: update 0.7.0 bottle.

--- Formula/k/kubeconform.rb
+++ Formula/k/kubeconform.rb
@@ -7,14 +7,7 @@ class Kubeconform < Formula
   head "https://github.com/yannh/kubeconform.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9fc0bd5fff9f4711f940b5e715bbd3a29bc6b95af8b03f93593c45cf35eb2e90"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "daf8891c42d9a174f5b407e5a54ad7a18ec39e1413386738ad44a7f4a0e66257"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "daf8891c42d9a174f5b407e5a54ad7a18ec39e1413386738ad44a7f4a0e66257"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "daf8891c42d9a174f5b407e5a54ad7a18ec39e1413386738ad44a7f4a0e66257"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c4a2a5d5b68ca69d05ce2ead6c5e67e282c4a25bd31bee697ff0d8356ad38324"
-    sha256 cellar: :any_skip_relocation, ventura:       "c4a2a5d5b68ca69d05ce2ead6c5e67e282c4a25bd31bee697ff0d8356ad38324"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "be5bff53c729946857a4eeb647d3ec19ea7d40ef99365c3995dda6ac19e66034"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b430e93e88f9c3182f8e7a7c511c220e3e469564b4413d600815fc72d336bdb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "864c6d64ee2b3d7971844f0ddfafe2cf7949934805d12fa3bd376c70a036b490"
   end
 
   depends_on "go" => :build
