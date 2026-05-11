106b1804a3e0a3c3ba93cc0bfa9375b39d2b40a6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 10:49:18 +0800

    little-cms2: update 2.19 bottle.

--- Formula/l/little-cms2.rb
+++ Formula/l/little-cms2.rb
@@ -19,12 +19,7 @@ class LittleCms2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "09a863e549255993de3f73d705328b1202417dff324edd30c703b77fcee09b01"
-    sha256 cellar: :any,                 arm64_sequoia: "be9135a585a60f84ec283d97e7a89bec9338e525679b637b171092338c5c28a0"
-    sha256 cellar: :any,                 arm64_sonoma:  "27689360239a2d3e1b69f14d2f981852c7cacc3bbd1fbdfba499a059cf69b551"
-    sha256 cellar: :any,                 sonoma:        "213896d19f9b7a0baad9c5a0b5daebe4f3d901a93c85de24f299c01ab1dadede"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6db1e7a93012fc49569cedc5772f9a2cab3ed4411c7f1015652b3b5d132740b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8df57239d5b52a119832c5b042c5a9fda56caf1692ac1377d517cb6bfc578ed8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "25476ba204dc80994617d065d24d4d6cddb2b762be4a1486992becf2a57e2807"
   end
 
   depends_on "jpeg-turbo"
