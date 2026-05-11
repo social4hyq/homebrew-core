629e103c574da52dbb4675560d653b164999d547
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 10:38:40 +0800

    jsoncpp: update 1.9.7 bottle.

--- Formula/j/jsoncpp.rb
+++ Formula/j/jsoncpp.rb
@@ -30,12 +30,7 @@ class Jsoncpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5beaf350688283483e3f7c82726a8d11ec6cc88b389051916b09e21850a2b498"
-    sha256 cellar: :any,                 arm64_sequoia: "a71f80781827c5677bafdfeb474711cf3b5b48a3250adf99bbbbd49d07dd15c7"
-    sha256 cellar: :any,                 arm64_sonoma:  "362ddd256b7f2bd664bed9375c3697a7ffe11a19bc120f1c496fec56316b1534"
-    sha256 cellar: :any,                 sonoma:        "5f5d30dac714b5dfd17f4de27349692cd949f0144ccb3780860015b87a275b98"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9b8d1661e7e6a7b5c6307c97ae08aea55e19bcb0587afe220e5c38a70f64a3f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7df8d9a070ee27066fb2287e2a48218ffe143ccc5bf607a701143a57dae242d3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9fa6ee6e51e146a5ee9963cd68e1fd523dc8dd24530bf4eaaea10635b315fec7"
   end
 
   # NOTE: Do not change this to use CMake, because the CMake build is deprecated.
