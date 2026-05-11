81cb518c3abe8edb5ccb0f66e0f717b1c37161c6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 10:44:02 +0800

    k9s: update 0.50.18 bottle.

--- Formula/k/k9s.rb
+++ Formula/k/k9s.rb
@@ -13,12 +13,7 @@ class K9s < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "640c74ca18c9ccfbf9fc9f903223fb8b8acaefc07ed36e50c82324311b2dce95"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd8d2d9b3d1514a4ae71f441f42974e699bda7bbce9640621802a55b5b7b6ed6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f6b82ca069e6ff73a5ab7cb6732f1e64abfbc127a10958c9ccd55d0740d94110"
-    sha256 cellar: :any_skip_relocation, sonoma:        "48a6671360c2266758916b585748e18dfd2c359631c344cb0e005f4068d9be62"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "346696b92754e55d665b5c55ba3cca18b28b1c2696bbc331674b6502e5415b0b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3752c9e54adb8cc77cf821ef3e952a89acac090681135bb2e42836515a2e20b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c9f94cb9786e00bc081b49a7f7b207b0f5112a22be4b16d61e3c4cf2df26dfbc"
   end
 
   depends_on "go" => :build
