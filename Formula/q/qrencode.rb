ec24df4c8c83d42a5058bddbcd2ae56bbd73a0d6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-11 11:43:14 +0800

    qrencode: update 4.1.1 bottle.

--- Formula/q/qrencode.rb
+++ Formula/q/qrencode.rb
@@ -8,15 +8,7 @@ class Qrencode < Formula
   head "https://github.com/fukuchi/libqrencode.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "43022662b77fc3e4181eb5b0d8e671f6208c9217c307073c2e76d4e5c98ea9f9"
-    sha256 cellar: :any,                 arm64_sequoia: "3fec9ad635eb185c133cca4e3ab3e8ab7c2094453c462c457b1e5624960bca35"
-    sha256 cellar: :any,                 arm64_sonoma:  "74e41b86ddec16b100ac17a756235f377b0e7b8990af8e37a66571b204db40c8"
-    sha256 cellar: :any,                 arm64_ventura: "01e05ae6c40b460ebc0dbba2fe76a4310ee538a692e7712dda77c07e18789ed8"
-    sha256 cellar: :any,                 sonoma:        "b9af340917ff148b24e48c24e5313423e16df56a99a7a612d0b23af59e2394b9"
-    sha256 cellar: :any,                 ventura:       "6995e471fbd7e8f9c3c507e7576ba53a5636fb329a049f61f54494b7b5d9fe2c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a598b96cd3fa15c9c028d7c6d86f884ece57ac86feca9939e862b17a14143bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "181bcd364cef78e8be564a42859f3b431febc429fc3db940d5c06bb572493999"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9d2660e29d100b3477393c3e4aa8d495d0dc2b4c14c9ff255015747b30e6aacc"
   end
 
   depends_on "cmake" => :build
