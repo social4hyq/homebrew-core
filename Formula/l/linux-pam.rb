9b914b3e9e7e9810d7088a9d0b12fa71214a5771
hqzing <hqzing@outlook.com>
2026-05-12 10:01:20 +0800

    linux-pam 1.7.2 (new formula)

---
+++ Formula/l/linux-pam.rb
@@ -0,0 +1,42 @@
+class LinuxPam < Formula
+  desc "Pluggable Authentication Modules for Linux"
+  homepage "https://github.com/linux-pam/linux-pam"
+  url "https://github.com/linux-pam/linux-pam/releases/download/v1.7.2/Linux-PAM-1.7.2.tar.xz"
+  sha256 "3d86b6383fb5fd9eb9578d2cd47d92801191f4bf3f9bc61419bfefc8aa1e531a"
+  license any_of: ["BSD-3-Clause", "GPL-1.0-only"]
+  head "https://github.com/linux-pam/linux-pam.git", branch: "master"
+
+  bottle do
+    sha256 arm64_linux:  "3e541f42f80d3d7f993b99967fb4a148d48bc036d6bcf975969eb102aa26cdd0"
+    sha256 x86_64_linux: "1320fdd62af4198e52f35271742e0b91db573f4429edffad6c44dcc0cf169663"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "libnsl"
+  depends_on "libtirpc"
+  depends_on "libxcrypt"
+  depends_on "gettext"
+  depends_on :linux
+
+  def install
+    ENV.append "CFLAGS", "-Dgetspnam(x)=((void*)0)"
+    ENV.append "CFLAGS", "-Dsetspent()=((void)0)"
+    ENV.append "CFLAGS", "-Dendspent()=((void)0)"
+    ENV.append "CFLAGS", "-Dfgetspent(x)=((void*)0)"
+    ENV.append "CFLAGS", "-Dfgetpwent(x)=((void*)0)"
+    ENV.append "CFLAGS", "-Dputspent(x,y)=0"
+    ENV.append "LDFLAGS", "-lintl"
+
+    system "meson", "setup", "build", "--sysconfdir=#{etc}", "-Dvendordir=#{pkgshare}/security",
+"-Dsecuredir=#{lib}/security", "-Ddocs=disabled", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    assert_match "Usage: #{sbin}/mkhomedir_helper <username>",
+                 shell_output("#{sbin}/mkhomedir_helper 2>&1", 14)
+  end
+end
