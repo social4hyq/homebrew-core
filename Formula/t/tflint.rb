ba913154104ed760e7238eec364041fe63deeb83
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-12 14:31:32 +0800

    tflint: update 0.61.0 bottle.

--- Formula/t/tflint.rb
+++ Formula/t/tflint.rb
@@ -9,12 +9,7 @@ class Tflint < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "74880fb58c8251cc4cef72ddf8bfdb4eee0910c5d9f480f9c8bbf796c7bd2564"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "74880fb58c8251cc4cef72ddf8bfdb4eee0910c5d9f480f9c8bbf796c7bd2564"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "74880fb58c8251cc4cef72ddf8bfdb4eee0910c5d9f480f9c8bbf796c7bd2564"
-    sha256 cellar: :any_skip_relocation, sonoma:        "19ef66af3e849610ac2664876527efe4783457d8ec64d61d373963aa51204da0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d757d8cce83666f2715232cf720dff1036cd5861081995f9463e660b5c9bdcf2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14a28d44e653de43ef61c1a5b9c041c612aa1833965a9767af85d49b5fcfb492"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3d2b3aadd1b58d31c36fc833be14cc00616f21460b8ddb43819c0d302d9b7c94"
   end
 
   depends_on "go" => :build
