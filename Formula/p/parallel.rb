eba3ce88dea2dd93574d2ad58b477457ad557b90
lanfungcing <lanfq.26@qq.com>
2026-05-12 03:38:26 +0000

    parallel 20260422 (new formula)

---
+++ Formula/p/parallel.rb
@@ -0,0 +1,42 @@
+class Parallel < Formula
+  desc "Shell command parallelization utility"
+  homepage "https://savannah.gnu.org/projects/parallel/"
+  url "https://ftpmirror.gnu.org/gnu/parallel/parallel-20260422.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/parallel/parallel-20260422.tar.bz2"
+  sha256 "664cf165d66ea217b2f49cd96a7865ecf90c9d0616599cc2aba8cad481d907bb"
+  license "GPL-3.0-or-later"
+  version_scheme 1
+  head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(/GNU Parallel v?(\d{6,8}).*? released/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "514fca782195bb2c7c60434865a46e4a8b6e97acbc79b4ee711d66f60d71476d"
+  end
+
+  conflicts_with "moreutils", because: "both install a `parallel` executable"
+
+  def install
+    ENV.append_path "PATH", bin
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+    bash_completion.install share/"bash-completion/completions/parallel"
+  end
+
+  def caveats
+    <<~EOS
+      To use the --csv option, the Perl Text::CSV module has to be installed.
+      You can install it via:
+        perl -MCPAN -e'install Text::CSV'
+    EOS
+  end
+
+  test do
+    assert_equal "test\ntest\n",
+                 shell_output("#{bin}/parallel --will-cite echo ::: test test")
+  end
+end
