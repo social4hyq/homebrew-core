e7d93f36a2c6bb56fcd414a7379a36c7387a7657
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-12 14:21:45 +0800

    supabase: update 2.98.2 bottle.

--- Formula/s/supabase.rb
+++ Formula/s/supabase.rb
@@ -12,12 +12,7 @@ class Supabase < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "54cd02ecedd9274ca8922272d16d9d69d6294bcceb00c1f7a3b85ba8ae4f4576"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b4dfbf64e70a0fcc0af75e1b70dd8f3d71810e10e78c5757e85ef545c15951b5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "42afbb69378c052476b1a6f458ab46270c9b3f0429a9e1bd09e97c08e2c8c55a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1d8185631e5ed4024b913b8b154b05e2e195e1fa81f7872f288393dd02b2640d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "99429f74c085915e57562e41de410bd8fe29c07f3b1a42b764fe6048556bb6bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "588d2eeb1db8edc4e595290d0a08928548b6cf8e47d771a57f86f831dd6ce633"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4c6bf641d91826aaafa6ef0c38d40d19d73b63ea9357adedae29919fc331f856"
   end
 
   depends_on "go" => :build
