eb24bd76a05f53862202b7a21d26998f5438c8ad
lanfungcing <lanfq.26@qq.com>
2026-05-12 03:45:29 +0000

    ruby-build 20260512 (new formula)

---
+++ Formula/r/ruby-build.rb
@@ -0,0 +1,27 @@
+class RubyBuild < Formula
+  desc "Install various Ruby versions and implementations"
+  homepage "https://rbenv.org/man/ruby-build.1"
+  url "https://github.com/rbenv/ruby-build/archive/refs/tags/v20260512.tar.gz"
+  sha256 "fc4aedf0f808a164c8a38a6315bb92047e01ab0152c488c270f3adb0b147a61c"
+  license "MIT"
+  compatibility_version 1
+  head "https://github.com/rbenv/ruby-build.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "da4f5bfcec0fa24cdcbbc092fc01cb4622f647038d5e352d3b3992069df028cc"
+  end
+
+  depends_on "autoconf"
+  depends_on "libyaml"
+  depends_on "openssl@3"
+  depends_on "pkgconf"
+  depends_on "readline"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_match "2.0.0", shell_output("#{bin}/ruby-build --definitions")
+  end
+end
