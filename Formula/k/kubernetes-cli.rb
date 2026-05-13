f19787a608785db47a48c4ab3e45459b4c845f75
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-13 20:46:15 +0800

    kubernetes-cli: update 1.36.1 bottle.

--- Formula/k/kubernetes-cli.rb
+++ Formula/k/kubernetes-cli.rb
@@ -13,7 +13,7 @@ class KubernetesCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8a0d15957af49641b13e344f8433c9f3b621b667d97633c03dd90dc3978a33c6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a09f06af18cc98dd29ea6d575e811cbd9bfb3d433e1e2c1c6820e39a730c05bd"
   end
 
   depends_on "go" => :build
