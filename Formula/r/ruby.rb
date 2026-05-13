def55c194b01bfc2cc4379f2d0b41e941893f970
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-13 15:35:56 +0800

    ruby: update 4.0.1 bottle.

--- Formula/r/ruby.rb
+++ Formula/r/ruby.rb
@@ -32,7 +32,8 @@ class Ruby < Formula
   no_autobump! because: "this is a critical package so an auto bump might break Homebrew usability."
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a4b409ba3dcfb6a96cf16f4f5a3ce45f0c770a79c240d8b5d8f260f686b0335c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b5c8b481b0ec5e6ece9e82d4d3d3cfaac8a803c4b9637c40f3c3ee72873b1481"
   end
 
   keg_only :provided_by_macos
