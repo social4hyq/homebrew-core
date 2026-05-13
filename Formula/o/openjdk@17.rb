26462936f6cb9aa23d13128ee3e70365e35bc194
hqzing <hqzing@outlook.com>
2026-05-13 15:28:27 +0800

    openjdk@17: update patch

--- Formula/o/openjdk@17.rb
+++ Formula/o/openjdk@17.rb
@@ -4,6 +4,7 @@ class OpenjdkAT17 < Formula
   url "https://github.com/openjdk/jdk17u/archive/refs/tags/jdk-17.0.19-ga.tar.gz"
   sha256 "b165f0dd120f4455904b76cf87dd9352fd23f88c2e9a33c2532fabacc3cca962"
   license "GPL-2.0-only" => { with: "Classpath-exception-2.0" }
+  revision 1
   compatibility_version 1
 
   livecheck do
@@ -51,8 +52,7 @@ class OpenjdkAT17 < Formula
   end
 
   patch do
-    url "https://raw.gitcode.com/Harmonybrew/homebrew-core/raw/main/Patches/openjdk@17/0001-support-ohos.patch"
-    sha256 "bce5e3d437b0c8bc2ac2f2c30d5d76a29242780c61054a4439305c917a65a8ea"
+    file "Patches/openjdk@17/0001-support-ohos.patch"
   end
 
   def install
@@ -80,7 +80,6 @@ class OpenjdkAT17 < Formula
       --without-version-opt
       --without-version-pre
       --with-zlib=system
-      --enable-ohos=yes
       --enable-headless-only=yes
       --with-copyright-year=2026
       --with-toolchain-type=clang
