39c71332b38ac9ab90803732285a8520b8dff16f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-14 11:21:31 +0800

    gnu-time: update 1.10 bottle.

--- Formula/g/gnu-time.rb
+++ Formula/g/gnu-time.rb
@@ -7,12 +7,7 @@ class GnuTime < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ae9f7fbf6e56886f0c126322f5a4ad47eb61d14b02d2b37fa51491ccd8cc7ec"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6d643fbbc1f1362553e5b23bf1faab2b9469a95194bd56eb3cec2e8c43cdab7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b3b7ce7272013335803ac82b23ae9ac078b0085da7f593dd92f4ad1e00c62254"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d5d6f1dd8e8a3f2582fae4eea0684558c56dd7d8146e60013ea6e37b83933a00"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c9784f415917d7e4aa363ec7d0e89456e493c39a7e49113745a872b195029d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b675fc8cb2b3c659f05eee9aebdf2a7487c62934374f82198af666556d3c8d6e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ded7342519b730175bf6f568618b603e6042979cb2d5db05ad8d79c9ceae0830"
   end
 
   uses_from_macos "ruby" => :test
