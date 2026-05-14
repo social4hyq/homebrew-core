9b1ce94211522d49ead8ae66e9735ba915832b88
hqzing <hqzing@outlook.com>
2026-05-14 12:13:51 +0800

    python@3.13: move gettext and util-linux into 'on_linux' block

--- Formula/p/python@3.13.rb
+++ Formula/p/python@3.13.rb
@@ -17,11 +17,9 @@ class PythonAT313 < Formula
   end
 
   depends_on "pkgconf" => :build
-  depends_on "gettext"
   depends_on "mpdecimal"
   depends_on "openssl@3"
   depends_on "sqlite"
-  depends_on "util-linux"
   depends_on "xz"
 
   uses_from_macos "bzip2"
@@ -32,6 +30,8 @@ class PythonAT313 < Formula
   uses_from_macos "unzip"
 
   on_linux do
+    depends_on "gettext"
+    depends_on "util-linux"
     depends_on "zlib-ng-compat"
   end
 
