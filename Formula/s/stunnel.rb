31d3f8e0eae133ee0af497ba90e2227ec7985f1b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-14 15:10:18 +0800

    stunnel: update 5.78 bottle.

--- Formula/s/stunnel.rb
+++ Formula/s/stunnel.rb
@@ -11,12 +11,7 @@ class Stunnel < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "39db1e422f499c370d2ca6d4a3ea522a5e2c5ba449613143e60333a4e942a55e"
-    sha256 cellar: :any,                 arm64_sequoia: "5437506a5e4e9fe0b6226aed2f58367e676200179a9e2b91529b02523069ecc7"
-    sha256 cellar: :any,                 arm64_sonoma:  "55cb2685b0e6aa8d25de1a4c22dcbc2a73b44f2d0a6b16595fc1c356ee2430f4"
-    sha256 cellar: :any,                 sonoma:        "a7834dd9314261f37ae181e7241491a41a5f5619f3182f0cde97e550fda776d1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3fdcd7912a0134981263f8155793cc49350847767d830cc88130e00c9bce1c54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28b5b3eddb6e07d8435d38e96c6e72f04cf0977c396dcb6f2eea204a552dc157"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ba634e996894c463bc870613ca0d1a0be6b95797b8523db4171bb0c53f6de2b0"
   end
 
   depends_on "openssl@3"
