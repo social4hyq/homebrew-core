a30c7a5cf4beb86dde5e50b91dccd9f6899c0c2a
lanfungcing <lanfq.26@qq.com>
2026-05-14 02:05:23 +0000

    stunnel 5.78 (new formula)

---
+++ Formula/s/stunnel.rb
@@ -0,0 +1,98 @@
+class Stunnel < Formula
+  desc "SSL tunneling program"
+  homepage "https://www.stunnel.org/"
+  url "https://www.stunnel.org/downloads/stunnel-5.78.tar.gz"
+  sha256 "8727e53bb8b7528f850327a2a149158422c02183bc120d1d733cc65b1e2c349d"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://www.stunnel.org/downloads.html"
+    regex(/href=.*?stunnel[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "39db1e422f499c370d2ca6d4a3ea522a5e2c5ba449613143e60333a4e942a55e"
+    sha256 cellar: :any,                 arm64_sequoia: "5437506a5e4e9fe0b6226aed2f58367e676200179a9e2b91529b02523069ecc7"
+    sha256 cellar: :any,                 arm64_sonoma:  "55cb2685b0e6aa8d25de1a4c22dcbc2a73b44f2d0a6b16595fc1c356ee2430f4"
+    sha256 cellar: :any,                 sonoma:        "a7834dd9314261f37ae181e7241491a41a5f5619f3182f0cde97e550fda776d1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3fdcd7912a0134981263f8155793cc49350847767d830cc88130e00c9bce1c54"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28b5b3eddb6e07d8435d38e96c6e72f04cf0977c396dcb6f2eea204a552dc157"
+  end
+
+  depends_on "openssl@3"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}",
+                          "--mandir=#{man}",
+                          "--disable-libwrap",
+                          "--disable-systemd",
+                          "--with-ssl=#{Formula["openssl@3"].opt_prefix}"
+    system "make", "install"
+
+    # This programmatically recreates pem creation used in the tools Makefile
+    # which would usually require interactivity to resolve.
+    cd "tools" do
+      system "dd", "if=/dev/urandom", "of=stunnel.rnd", "bs=256", "count=1"
+      system "#{Formula["openssl@3"].opt_bin}/openssl", "req",
+        "-new", "-x509",
+        "-days", "365",
+        "-rand", "stunnel.rnd",
+        "-config", "openssl.cnf",
+        "-out", "stunnel.pem",
+        "-keyout", "stunnel.pem",
+        "-sha256",
+        "-subj", "/C=PL/ST=Mazovia Province/L=Warsaw/O=Stunnel Developers/OU=Provisional CA/CN=localhost/"
+      chmod 0600, "stunnel.pem"
+      (etc/"stunnel").install "stunnel.pem"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      A bogus SSL server certificate has been installed to:
+        #{etc}/stunnel/stunnel.pem
+
+      This certificate will be used by default unless a config file says otherwise!
+      Stunnel will refuse to load the sample configuration file if left unedited.
+
+      In your stunnel configuration, specify a SSL certificate with
+      the "cert =" option for each service.
+
+      To use Stunnel with Homebrew services, make sure to set "foreground = yes" in
+      your Stunnel configuration.
+    EOS
+  end
+
+  service do
+    run [opt_bin/"stunnel"]
+  end
+
+  test do
+    user = if OS.mac?
+      "nobody"
+    else
+      ENV["USER"]
+    end
+    (testpath/"tstunnel.conf").write <<~EOS
+      cert = #{etc}/stunnel/stunnel.pem
+      foreground = yes
+
+      setuid = #{user}
+      setgid = #{user}
+
+      [pop3s]
+      accept  = 995
+      connect = 110
+
+      [imaps]
+      accept  = 993
+      connect = 143
+    EOS
+
+    assert_match "successful", pipe_output("timeout 1s #{bin}/stunnel #{testpath}/tstunnel.conf 2>&1")
+  end
+end
