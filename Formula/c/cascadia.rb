d87dfecb471e0e2f66c0423db8e2ededd453354b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 01:18:58 +0800

    cascadia: update 1.5.0 bottle.

--- Formula/c/cascadia.rb
+++ Formula/c/cascadia.rb
@@ -7,12 +7,7 @@ class Cascadia < Formula
   head "https://github.com/suntong/cascadia.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d9da8a8796edfe3ae5b6d9c71d48bc856588bc9152cf6fec79f3eac3f53b636"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d9da8a8796edfe3ae5b6d9c71d48bc856588bc9152cf6fec79f3eac3f53b636"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2d9da8a8796edfe3ae5b6d9c71d48bc856588bc9152cf6fec79f3eac3f53b636"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1e86283adf0af56ae887df6c55967a7b03a62544b8f1b914e4b1d7d1b5ef57a4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "11f89dc57dc214953fd42f1651546cabf331f4fa5d06c5e5ff91b62f7e5ad998"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a08db3834543b4f96b07377a34ca4203e5e7c1b6dbe73674017aded43b91e6b1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "11055c42afa140222a9ae7c5a4f0ab562fb365102e00efd9fabee6cc39154e05"
   end
 
   depends_on "go" => :build
