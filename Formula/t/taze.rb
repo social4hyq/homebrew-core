0ebc83b2fee34cfee5c763bd993f2051d294a822
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 17:21:06 +0800

    taze: update 19.11.0 bottle.

--- Formula/t/taze.rb
+++ Formula/t/taze.rb
@@ -7,7 +7,7 @@ class Taze < Formula
   head "https://github.com/antfu-collective/taze.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cd059548b0921facb066368c38b2a78b9ff6415b727112bb50ed3c2cb6785c6e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "230fa8f3e82f30a994c93fb5e9cd4f2686b0f01406033ddbc7b9bda9a431c9b8"
   end
 
   depends_on "node"
