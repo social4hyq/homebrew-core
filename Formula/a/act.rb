f6dce3c7efcf75b148891563d258295e05defe91
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 17:09:16 +0800

    act: update 0.2.88 bottle.

--- Formula/a/act.rb
+++ Formula/a/act.rb
@@ -7,12 +7,7 @@ class Act < Formula
   head "https://github.com/nektos/act.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b5b9a3ba9ddf9782ccebf0c930bb7a1f1f2e9f2760d157a07c9c03861f376f5d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe57421127da1ba68e2789b2dc45c9a361d0a366669f43eb1e7614fd4c689576"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "848ff696dc99eec087e91d7a5bb5256cb7a3f33996f4613f77167ea306b8a040"
-    sha256 cellar: :any_skip_relocation, sonoma:        "df61f544da978a0051c1f018f078b7f6ce93e13f30f375e5b9420d12006dd780"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6b42a43205b7b0764e2d186b148c5153fca9e1ce68e109d8b75c35cfa17d1637"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b46df2998362f3e4d101a7d28c3682460d5a75fc31a4b8f231c7b3ff653aae8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bfa843bc3916c35b7d99b712d7b6743ea05fe6ae9d6e42902212da67e895715a"
   end
 
   depends_on "go" => :build
