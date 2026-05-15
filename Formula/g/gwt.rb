d657fb387f2c9bea8fab11f2daa92c55e6262593
hqzing <hqzing@outlook.com>
2026-05-15 13:22:06 +0000

    gwt 2.13.0 (new formula)

---
+++ Formula/g/gwt.rb
@@ -0,0 +1,31 @@
+class Gwt < Formula
+  desc "Google web toolkit"
+  homepage "https://www.gwtproject.org/"
+  url "https://github.com/gwtproject/gwt/releases/download/2.13.0/gwt-2.13.0.zip"
+  sha256 "43ca936765a432d5e6fb7ce746caf9c9a7d6d47abb955da3f03742d7a61b31d3"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "61e592697a9d5ce46b49729d46788ef4b4ee119d7c40dcfa094bdbcc0f17f389"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm Dir["*.cmd"] # remove Windows cmd files
+    libexec.install Dir["*"]
+
+    (bin/"i18nCreator").write_env_script libexec/"i18nCreator", Language::Java.overridable_java_home_env
+    (bin/"webAppCreator").write_env_script libexec/"webAppCreator", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    system bin/"webAppCreator", "sh.brew.test"
+    assert_path_exists testpath/"src/sh/brew/test.gwt.xml"
+  end
+end
