fce82521e926bc09bf357fb4153062c8e14e9f30
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 16:30:07 +0800

    yo: update 7.0.1 bottle.

--- Formula/y/yo.rb
+++ Formula/y/yo.rb
@@ -7,12 +7,7 @@ class Yo < Formula
   head "https://github.com/yeoman/yo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5b7987d0debf1e8b671ef5e14dbf896466c1cd55baefdb61c803f43f36ebc103"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b7987d0debf1e8b671ef5e14dbf896466c1cd55baefdb61c803f43f36ebc103"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b7987d0debf1e8b671ef5e14dbf896466c1cd55baefdb61c803f43f36ebc103"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5b7987d0debf1e8b671ef5e14dbf896466c1cd55baefdb61c803f43f36ebc103"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ac90a9d6b066420569c8568fc7a06372c3bba0d5d97ec019edc139eca8753599"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac90a9d6b066420569c8568fc7a06372c3bba0d5d97ec019edc139eca8753599"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5f64aff6ca0f568b519d56b322667f08f47c91ec0837e71c2d9bf922947f8427"
   end
 
   depends_on "node"
