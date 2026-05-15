788266902ffe67d009e3e6ba05552207fa8fb8b9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 23:08:29 +0800

    shfmt: update 3.13.1 bottle.

--- Formula/s/shfmt.rb
+++ Formula/s/shfmt.rb
@@ -7,12 +7,7 @@ class Shfmt < Formula
   head "https://github.com/mvdan/sh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bf8528e33b4d5c4d8572fa23f45a2dbe3935b54a21b522cc0e4059e5a70b4fcb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf8528e33b4d5c4d8572fa23f45a2dbe3935b54a21b522cc0e4059e5a70b4fcb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf8528e33b4d5c4d8572fa23f45a2dbe3935b54a21b522cc0e4059e5a70b4fcb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b7d542832f29366a897f46c3659e06e190ebf27458d1e8260b410d185cd6e6dc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0e3ee9253b76dc169b3be3a9d24dd15caebdcc1c82eebe1c2de549fdd87f4067"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e78519471a3c6a1f082c944277d3e97ee4cab817f7a8d92ee69c635a85cf3d3f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "79aeeb4fbc16923debd8d6b7f664050f47ac18f97d22a291084221c2b5b60bfc"
   end
 
   depends_on "go" => :build
