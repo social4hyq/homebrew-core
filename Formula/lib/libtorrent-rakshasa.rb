fa32a9fadab11e4c1a2c1aeac053339f3730810e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 01:30:17 +0800

    libtorrent-rakshasa: update 0.16.11 bottle.

--- Formula/lib/libtorrent-rakshasa.rb
+++ Formula/lib/libtorrent-rakshasa.rb
@@ -11,12 +11,7 @@ class LibtorrentRakshasa < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "0c4138afcf161df70bbe73e27eb85544ef7398143f44b4543dee9bb2f3f011d5"
-    sha256 cellar: :any,                 arm64_sequoia: "c5b294fac3ed526a83b55b4a9d932065dd652a3b1b4f740710eeef2fea065c9f"
-    sha256 cellar: :any,                 arm64_sonoma:  "25db9a0301001ede2bdda8c0c0f4dcf1906be7099cdfdea171f5353ce63fd045"
-    sha256 cellar: :any,                 sonoma:        "65f7de5c6a71f360a481795d264acbcc5ae4486e46acde5e248465b9ed4f768e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d5c890408c17634d6cee9dc25953eae2ee609c21be4aed3a70a8aac71ef44a07"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76774fdcf1c39b5ec79253dc5054dade569ffe00d6eda9478274f56f9319b448"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5141b05bb209d69c55588c76f66050995c30fffa3e32fdea37539ac2eac152f4"
   end
 
   depends_on "autoconf" => :build
