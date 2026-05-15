4cab99f3be270b8e68ef713fe9be9f2337f1a5f3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 00:37:17 +0800

    kamal-proxy: update 0.9.2 bottle.

--- Formula/k/kamal-proxy.rb
+++ Formula/k/kamal-proxy.rb
@@ -7,12 +7,7 @@ class KamalProxy < Formula
   head "https://github.com/basecamp/kamal-proxy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "478c74971a4fa286300ae1880211ffe67d9f11b03967de22b126da6e7b5e7018"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0978c00a32b8646ada7c683f15017ad06e6e5deeff9ae951819ac6f034c165e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb7443d081fea5fec0f4d28da1c91af52e6bda2fb5c5f311f33b40c8aa7edc17"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bf864dd9aea86ee2f33e469fa39c48ba1124170e37b030f8a35b577b6ed53478"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "72c88174507d978e9ffa83473089ec872a2aeb535258f84b00b39d73c42a2b86"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6eb60afa749bd752d782ccd3557b4306f1bce197e195bac901b0fe17887a6684"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "006e936ac6c87835bfca3424d3085ada67be135f4fbf17380cfa402cf2fbd79f"
   end
 
   depends_on "go" => :build
