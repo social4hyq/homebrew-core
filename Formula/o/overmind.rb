18c5237ed149afaf51222c85e65f808280070fac
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 01:15:41 +0800

    overmind: update 2.5.1 bottle.

--- Formula/o/overmind.rb
+++ Formula/o/overmind.rb
@@ -7,16 +7,7 @@ class Overmind < Formula
   head "https://github.com/DarthSim/overmind.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "7e75f7028eb9c455f06ca7d1bbfdc0dd56a1b93b50f957a2b43ae25850737eca"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "63ad87a46c888c096259183252addcd8ef53d521bf1a5e36fa3ef79b4a1c526a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "170345488efff9664a6e6f1c203e9f252f2a6b36f3b19a574a69e2a504ff88d0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2cefab99b19b428357d41d4f27186c68a149c6b67dd317ba71e7cc985e3ab824"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5b5121a20d14591466ebfa082329d902ced52c30e7c257bebe0c6141717c29ca"
-    sha256 cellar: :any_skip_relocation, sonoma:         "75d9f3f07a21c2bd979f50fd520bec07a4221fecf0e70afe2e167d91ca263360"
-    sha256 cellar: :any_skip_relocation, ventura:        "d10e4a237a80de7a6119f79ab10304623e7cb5dd42d74e5feb1382bd74642862"
-    sha256 cellar: :any_skip_relocation, monterey:       "8f39c51a41a651b932147937384d1091dd0b86ae15a040bd6dbd96c2db5f3cf4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "905ba5269700b1a1484869ab1a8450752ff42a38f54a070a6cb55dceac025dc3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba8a085f0aae6ddeec87817674bc44a085c2ae884e0a90568b23d9e37c5a3cee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c8b86f8772069bff581b26c5e8b0616956aef22835c1b6423b052b5087f4ab3b"
   end
 
   depends_on "go" => :build
