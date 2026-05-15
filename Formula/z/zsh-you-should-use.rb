ae910d4b92db5a22b141aa13e9759919fa63f690
hqzing <hqzing@outlook.com>
2026-05-15 13:22:23 +0000

    zsh-you-should-use 1.11.1 (new formula)

---
+++ Formula/z/zsh-you-should-use.rb
@@ -0,0 +1,33 @@
+class ZshYouShouldUse < Formula
+  desc "ZSH plugin that reminds you to use existing aliases for commands you just typed"
+  homepage "https://github.com/MichaelAquilina/zsh-you-should-use"
+  url "https://github.com/MichaelAquilina/zsh-you-should-use/archive/refs/tags/1.11.1.tar.gz"
+  sha256 "4d884aed12f1301f4620888e322c406c215b719e49385aa3088dc84c8ed383ed"
+  license "GPL-3.0-only"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "01d4355f8d2ac4b7030007a49e4c96f37277569d3e76aa8791ffa60988d2a873"
+  end
+
+  uses_from_macos "zsh"
+
+  def install
+    pkgshare.install "you-should-use.plugin.zsh"
+  end
+
+  def caveats
+    <<~EOS
+      To activate You Should Use, add the following to your .zshrc:
+
+        source #{HOMEBREW_PREFIX}/share/zsh-you-should-use/you-should-use.plugin.zsh
+
+      You will also need to restart your terminal for this change to take effect.
+    EOS
+  end
+
+  test do
+    assert_match version.to_s,
+      shell_output("zsh -c '. #{pkgshare}/you-should-use.plugin.zsh && " \
+                   "echo $YSU_VERSION'")
+  end
+end
