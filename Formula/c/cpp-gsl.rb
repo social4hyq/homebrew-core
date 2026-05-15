c5dc2825e7147c10a3bba02376915cd3a549990a
hqzing <hqzing@outlook.com>
2026-05-15 07:34:23 +0000

    cpp-gsl 4.2.1 (new formula)

---
+++ Formula/c/cpp-gsl.rb
@@ -0,0 +1,33 @@
+class CppGsl < Formula
+  desc "Microsoft's C++ Guidelines Support Library"
+  homepage "https://github.com/Microsoft/GSL"
+  url "https://github.com/Microsoft/GSL/archive/refs/tags/v4.2.1.tar.gz"
+  sha256 "d959f1cb8bbb9c94f033ae5db60eaf5f416be1baa744493c32585adca066fe1f"
+  license "MIT"
+  head "https://github.com/Microsoft/GSL.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9a2345b4b473bab384699e4373258d94f7cd324e147b80304075048a015b6cc1"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DGSL_TEST=false", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <gsl/gsl>
+      int main() {
+        gsl::span<int> z;
+        return 0;
+      }
+    CPP
+
+    system ENV.cxx, "test.cpp", "-o", "test", "-std=c++14"
+    system "./test"
+  end
+end
