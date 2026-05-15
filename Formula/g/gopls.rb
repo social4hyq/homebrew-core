c6db37bc5aa22b9c7eb66410aa88d95263ef731d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 16:15:12 +0800

    gopls: update 0.21.1 bottle.

--- Formula/g/gopls.rb
+++ Formula/g/gopls.rb
@@ -13,12 +13,7 @@ class Gopls < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e1991fd339294634d7b6576de9f101e71b5fe1ab78cf0de2ef38fc56be60e1f2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e1991fd339294634d7b6576de9f101e71b5fe1ab78cf0de2ef38fc56be60e1f2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1991fd339294634d7b6576de9f101e71b5fe1ab78cf0de2ef38fc56be60e1f2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "63c6dfdd1cc1c2927888bac68f8bb6bb40e283530ef065046206e303888941f7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "adc320a44e5a73f55f31c9f78b93ea4e11925b19aefb1b3f9f1979325174f8cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d3ca52daa50fd77434b69220279c28ad44d6c4b0f627c612cf09e38246d9d59"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "99b12345cc7b63d89d26a1e6673787f5b05bd179e62bd56ae239835b861027ea"
   end
 
   depends_on "go" => :build
