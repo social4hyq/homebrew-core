fd0c9a8c72a011c6f265531ca5c8b2893ed54335
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 01:28:55 +0800

    bedtools: update 2.31.1 bottle.

--- Formula/b/bedtools.rb
+++ Formula/b/bedtools.rb
@@ -6,13 +6,7 @@ class Bedtools < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "1e609e7589f355b608db522a0ea0f645093f68d2b103919fe0a45dc155b305a5"
-    sha256 cellar: :any,                 arm64_sequoia: "f46a8643bf1b9c29b01d1fd3a7e842adbb707bb167fe939cf35f9516edda9844"
-    sha256 cellar: :any,                 arm64_sonoma:  "d11daa75a18bab575cef615a79d679d710cf6213c1cfc060cb7b0c55976e0612"
-    sha256 cellar: :any,                 sonoma:        "f3f23539a6d31e0c7d0576084965996aaeecfd61a6c055163fc0214fc9869ca6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cd4c766589bc8b4f73d33846a7b895536071694917d559e551776e6ba48c5d0e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55200ba7e7f92b5491db32eb807a08718ccd330bbc5d3fbc702e561bc5abc89a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "664a01a850471aef5ed2aca7c5a82b8f5f6ee0262bce7e8bc07dabf98e97ea06"
   end
 
   depends_on "xz"
