40d94f5de083f9e2292f4af0cbdec5a7b84b388c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 17:05:00 +0800

    jvgrep: update 5.8.16 bottle.

--- Formula/j/jvgrep.rb
+++ Formula/j/jvgrep.rb
@@ -7,12 +7,7 @@ class Jvgrep < Formula
   head "https://github.com/mattn/jvgrep.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2c33ed09adb2427fbfdf4e954d18f857efe45cea3b708f570d60b1d92101dac6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c33ed09adb2427fbfdf4e954d18f857efe45cea3b708f570d60b1d92101dac6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2c33ed09adb2427fbfdf4e954d18f857efe45cea3b708f570d60b1d92101dac6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "462610c9365f48db395b8640036e607dc4c2d90d747e46e48bc3d4947a299d6b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eb6ce31a89ff7bda2f71292ba30cc60fcf54a00d0dfbf920f7c7e07f4d9fe79f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "186486fa0c0488ab1bedc0b8471774334e180ca053532f9c97cdd653dc347ef0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2665d99d2dd9c55922c50a7cc58a1b226117032bfdf6402138e6eae00fd2c932"
   end
 
   depends_on "go" => :build
