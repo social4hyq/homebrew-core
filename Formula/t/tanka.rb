8974b18431f55cdd46ff01be722e8a8b2529ca67
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:47:25 +0800

    tanka: update 0.37.2 bottle.

--- Formula/t/tanka.rb
+++ Formula/t/tanka.rb
@@ -12,12 +12,7 @@ class Tanka < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "92807134801b160539860d0cbf6a81723826ffd1556a092e95c8aef2c074ba6a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92807134801b160539860d0cbf6a81723826ffd1556a092e95c8aef2c074ba6a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92807134801b160539860d0cbf6a81723826ffd1556a092e95c8aef2c074ba6a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "89b38c2afccefa36bc52d0746cf7448735ea5637169b507662b28b29a840205d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb10b7c031c80d6059df02634c82919b4a95f9095af27c0edcf447588baf60fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "985347cb468d43ab645d2451179afe54dc756995157c882844b9ae4927790657"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9c39d1e71ebcfa4b6424afb11f8256e9de84248f68b3dad370775eb1d0e05bbb"
   end
 
   depends_on "go" => :build
