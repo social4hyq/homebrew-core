f9fd23473d60c0ccb45a8cb7b4046892516a7d98
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 21:53:35 +0800

    buildozer: update 8.5.1 bottle.

--- Formula/b/buildozer.rb
+++ Formula/b/buildozer.rb
@@ -7,12 +7,7 @@ class Buildozer < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd9592a02db22ff8243d99081cca1da48e4f4fc4664bffd41b16dce39cb8d465"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd9592a02db22ff8243d99081cca1da48e4f4fc4664bffd41b16dce39cb8d465"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd9592a02db22ff8243d99081cca1da48e4f4fc4664bffd41b16dce39cb8d465"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1b4609634a3f90b20b3104e6ed459ae0cadec118b659afff986ad132aa27cb51"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8263d5556d0dca20e40d99d0efb1befdc14dbec44683a1b9be36cfd6d701e055"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "625424997cc35a8cf391d02c61788fc1c2b41d32e9a59ac63d24ebdfd33e4a28"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "aad771236ecd80e71a876606dff135e898fc9bb25cda486b45db6e1b1c96e5b5"
   end
 
   depends_on "go" => :build
