d80803d72ffc056339003f1db17ebbf8660d9bfa
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 16:56:04 +0800

    cog: update 0.19.3 bottle.

--- Formula/c/cog.rb
+++ Formula/c/cog.rb
@@ -12,12 +12,7 @@ class Cog < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5b9c9c40573ef25f63690ed2ff6c32742fee00c8c497008a2f5cb57df3455976"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fb959864120834d220313b392cf1c14d1f3b08bde16324e40dd4132339affee5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c2e7f2ad1b230389251e00a308bf505f5a3903c0e99e2a72e51402f5a02bc0c7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2644ae68dd30a3b35da80640954a37927e87a8f3ddf5210657c7b58fa9c59ee6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "744a32c531c9cc58bdb81a1a96bf1bb710d0a84ceb72a19d1cba2e9f3db48213"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "589dbb85de44712f37047542d26440daa4fda030fa8541f1ba6b21bbdd50d7d8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5e55852b7f18d43413e7c8f9ca0be783cd53bcedb92b8a33e055504d46841b9b"
   end
 
   depends_on "go" => :build
