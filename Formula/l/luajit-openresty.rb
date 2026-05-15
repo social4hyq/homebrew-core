b570a2e89a739776fa3b21f3e9e0d6364d5ad663
hqzing <hqzing@outlook.com>
2026-05-15 07:31:43 +0000

    luajit-openresty 2.1-20260415 (new formula)

---
+++ Formula/l/luajit-openresty.rb
@@ -0,0 +1,81 @@
+class LuajitOpenresty < Formula
+  desc "OpenResty's Branch of LuaJIT 2"
+  homepage "https://github.com/openresty/luajit2"
+  url "https://github.com/openresty/luajit2/archive/refs/tags/v2.1-20260415.tar.gz"
+  sha256 "b4477219ae6fea510a371027c494ad0939114d1dff5e84049369a8a59f1c86ab"
+  license "MIT"
+  version_scheme 1
+  head "https://github.com/openresty/luajit2.git", branch: "v2.1-agentzh"
+
+  # The latest LuaJIT release is unstable (2.1.0-beta3, from 2017-05-01) and
+  # OpenResty is making releases using the latest LuaJIT Git commits. With this
+  # in mind, the regex below is very permissive and will match any tags
+  # starting with a numeric version, ensuring that we match unstable versions.
+  # We should consider restricting the regex to stable versions if it ever
+  # becomes feasible in the future.
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:[.-]\d+)+[^{}]*)/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "11a758f6839bddeb7d4a56ebc1576e05a58c21edd86029cf1281a8e609f23fb8"
+    sha256 cellar: :any,                 arm64_sequoia: "312fc483a58579f33eaf8ac52dd7d8f7e605159e940c18602e89a0c9344242f5"
+    sha256 cellar: :any,                 arm64_sonoma:  "cd00e1aa4f7e0d273dcc5762f7aa514ef657355366cc95e4431e44bf9ed5f151"
+    sha256 cellar: :any,                 sonoma:        "6a417b53dc69a8457de6555936002e0780874186a2aa7cc5de77d866bb84c6cd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cf69845307577b7de81a4baeaa193f88828dac58597678b92f2d2fffaf9a441a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d12ccf03922b3e24e8b7063a641e419ba43f6983ee4967fcd3cd89cbab0b35d"
+  end
+
+  keg_only "it conflicts with the LuaJIT formula"
+
+  def install
+    # 1 - Override the hardcoded gcc.
+    # 2 - Remove the "-march=i686" so we can set the march in cflags.
+    # Both changes should persist and were discussed upstream.
+    inreplace "src/Makefile" do |f|
+      f.change_make_var! "CC", ENV.cc
+      f.change_make_var! "CCOPT_x86", ""
+    end
+
+    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+    # is not set then it's forced to 10.4, which breaks compile on Mojave.
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s if OS.mac?
+
+    # Fix for clang >= 16, see https://github.com/LuaJIT/LuaJIT/issues/1266
+    ENV.append "LDFLAGS", "-Wl,-no_deduplicate" if DevelopmentTools.clang_build_version >= 1600
+
+    args = %W[
+      PREFIX=#{prefix}
+      XCFLAGS=-DLUAJIT_ENABLE_GC64
+    ]
+
+    system "make", "amalg", *args
+    system "make", "install", *args
+
+    # LuaJIT doesn't automatically symlink unversioned libraries:
+    # https://github.com/Homebrew/homebrew/issues/45854.
+    lib.install_symlink lib/"libluajit-5.1.dylib" => "libluajit.dylib"
+    lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
+
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+    end
+
+    # Having an empty Lua dir in lib/share can mess with other Homebrew Luas.
+    %W[#{lib}/lua #{share}/lua].each { |d| rm_r(d) }
+  end
+
+  test do
+    system bin/"luajit", "-e", <<~EOS
+      local ffi = require("ffi")
+      ffi.cdef("int printf(const char *fmt, ...);")
+      ffi.C.printf("Hello %s!\\n", "#{ENV["USER"]}")
+    EOS
+  end
+end
