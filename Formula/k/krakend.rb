e3c1153af154f1e53af09bca7a4fa56ad6f01cd0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:55:37 +0800

    krakend: update 2.13.5 bottle.

--- Formula/k/krakend.rb
+++ Formula/k/krakend.rb
@@ -7,12 +7,7 @@ class Krakend < Formula
   head "https://github.com/krakend/krakend-ce.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "af4743c2dacd672028bc035b4357d13679f455fecf625c00ba7dc17fa39d226b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ef3153a98a9d4fceb25e83d6e057c96f0263df9a8bf6fc0cafbdb4076b65e28a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "396e424f3066711b41dc2f4fb0557a27485b2f5f10cf24e40a8dbd15b42622d4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6f41c9e7f5a5bafcc6f42a655eb550897d9cc6dfa64309eda1f7928ac0924d1c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "571d7260df04e048e8f4cb703a3b8b1cabe51f2d2cd82bfadd6e4471ba889dd7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6992ce9f8432116ea6771390251174400352a9b5f58386f6de756464fd536b74"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9b66aa93d0ff972956b5b36ddfbf96b82f2b92e89830c8d8efb32fa2340fdacf"
   end
 
   depends_on "go" => :build
