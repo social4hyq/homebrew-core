d09c23b25b64dc708cf6c7d6eb4d397349957f33
hqzing <hqzing@outlook.com>
2026-05-15 13:16:10 +0000

    stepci 2.8.2 (new formula)

---
+++ Formula/s/stepci.rb
@@ -0,0 +1,48 @@
+class Stepci < Formula
+  desc "API Testing and Monitoring made simple"
+  homepage "https://stepci.com"
+  url "https://registry.npmjs.org/stepci/-/stepci-2.8.2.tgz"
+  sha256 "0ba4ed74a5f51414b0ed86651e37a1b5e6af4e027187bedfc94dbd2176793178"
+  license "MPL-2.0"
+
+  bottle do
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, all: "98104a3592dd43f41522aae67073e1eed4344638c7501aa6617af89ee207d163"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    # https://docs.stepci.com/legal/privacy.html
+    ENV["STEPCI_DISABLE_ANALYTICS"] = "1"
+
+    (testpath/"workflow.yml").write <<~YAML
+      version: "1.1"
+      name: Status Check
+      env:
+        host: example.com
+      tests:
+        example:
+          steps:
+            - name: GET request
+              http:
+                url: https://${{env.host}}
+                method: GET
+                check:
+                  status: /^20/
+    YAML
+
+    expected = <<~EOS
+      Tests: 0 failed, 1 passed, 1 total
+      Steps: 0 failed, 0 skipped, 1 passed, 1 total
+    EOS
+    assert_match expected, shell_output("#{bin}/stepci run workflow.yml")
+
+    assert_match version.to_s, shell_output("#{bin}/stepci --version")
+  end
+end
