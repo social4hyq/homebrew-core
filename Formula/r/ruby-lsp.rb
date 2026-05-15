112d496d1b818dcfb8c4b3e0ba4fc23a41aceb11
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:50:56 +0800

    ruby-lsp: update 0.26.9 bottle.

--- Formula/r/ruby-lsp.rb
+++ Formula/r/ruby-lsp.rb
@@ -7,12 +7,7 @@ class RubyLsp < Formula
   head "https://github.com/Shopify/ruby-lsp.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "70e906ca86ab7d544c64e5c5c8ff5cb6c753330da919149c0da47f4a607efd3c"
-    sha256 cellar: :any,                 arm64_sequoia: "c892b43193f13f0ac62991a8f14a0226e688392f41d86426c5707f34e6e9dffa"
-    sha256 cellar: :any,                 arm64_sonoma:  "660ea01b712675baea507d0658d4f8fee09d6d72a4331f478c895afc3a2c41de"
-    sha256 cellar: :any,                 sonoma:        "eb7487772e9435ffd6ee57a12d01f8f94795aac8a329dafe05b2baff9f4dfb4b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5e67d8ebb4b705aed8fcd747dd643076a22fc47b926e0d3ca98cc45319233039"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f249b8e92c21ff6e78972874d89a087b23741e78b94fbf526fd0922702ef3926"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a7b5ac42e38c0b73709163968b027be0c409225481f213da2f903835800e7ca3"
   end
 
   depends_on "ruby"
