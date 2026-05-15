3bb0f288a314e3cb14ef18e2fb96700ea8b2021b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:48:14 +0800

    vcfanno: update 0.3.8 bottle.

--- Formula/v/vcfanno.rb
+++ Formula/v/vcfanno.rb
@@ -6,12 +6,7 @@ class Vcfanno < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d29c909e614e5a767766f43b76608bf766f482905a919c43f69102d03997b0e4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d29c909e614e5a767766f43b76608bf766f482905a919c43f69102d03997b0e4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d29c909e614e5a767766f43b76608bf766f482905a919c43f69102d03997b0e4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5bb2e06c97b02e96b5e850ee7ee89b1d60a874dabb18f9093bed3ffefe7c4c29"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a0999dffcc6f95492b65caa0adfa9eb8e91300c080900644d41e58bd02d03e51"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23b91271b2887d670d2d71511bca43fcdb90415f2338fece6cfc94b9b87bd21a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dab804a5813a01fdf891f1d45c374a731e2e807249b7e014fb5bb91748e5ccf2"
   end
 
   depends_on "go" => :build
