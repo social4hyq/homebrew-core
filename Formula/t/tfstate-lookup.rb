19d9224b489bc99d5aaae692eb282dcc2db2cf2f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:20:13 +0800

    tfstate-lookup: update 1.11.0 bottle.

--- Formula/t/tfstate-lookup.rb
+++ Formula/t/tfstate-lookup.rb
@@ -6,12 +6,7 @@ class TfstateLookup < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "52ecbf8e2fc461cf8b0d831e4ec2fa744d9e6e62c1e390c6e6b76b0c5e59ecdd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "52ecbf8e2fc461cf8b0d831e4ec2fa744d9e6e62c1e390c6e6b76b0c5e59ecdd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "52ecbf8e2fc461cf8b0d831e4ec2fa744d9e6e62c1e390c6e6b76b0c5e59ecdd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5433d18708b3010f6dad7d13646032021ce422ef9c5fac69deecbad80ac66491"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cd38299a8e27120296d3e63b508dcf6f613d01c318af130573f711a1f7ae390e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f1aab88211780c3f07c3b6f10766adedfb4152c2adebb870347e869df2c7744"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3ae3d493d7516e44aff3dea2edc1b3638ededc83add0e271bbd46a949fe47536"
   end
 
   depends_on "go" => :build
