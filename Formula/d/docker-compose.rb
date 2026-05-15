074dfe39734eb366d44da2470cc7f81fd008a3a9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 16:31:32 +0800

    docker-compose: update 5.1.3 bottle.

--- Formula/d/docker-compose.rb
+++ Formula/d/docker-compose.rb
@@ -15,12 +15,7 @@ class DockerCompose < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dc0828bedbb05f3f4b043aca1a0c0331e7ee2abef03e0ef8cfce40ed85cdf6d1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cfc46d3774dbf745bdd7ecf96e404e732782acb34abd031713dd2df211d8766e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99f5dee9c43dcd51761dad12b5509d6aa5532e38ebb534c36b4b584e71f0a4f0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f10c692d1458b20b5dd1ec7d02d04acf135e6001b4122fea94c80f1794da943a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fd87074ef519d79b103bc7d9851ee9ed95cf3fe1457d410a3a0f154c5dd92e7f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7755df3eed96e1e19e841ce30ef3d06fba19c439fca77ea9175a9967fd7e554b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "53d46d375667b3e8694602b9bf415b6772283b7e782fdf5701d87af73244605a"
   end
 
   depends_on "go" => :build
