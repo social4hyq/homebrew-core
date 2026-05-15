811850413b797599c5b4c6811676a17f60c6dadf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 17:00:08 +0800

    render: update 2.17.0 bottle.

--- Formula/r/render.rb
+++ Formula/r/render.rb
@@ -7,12 +7,7 @@ class Render < Formula
   head "https://github.com/render-oss/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5b887056a36922c19185cbe37bcda1a2b84cce701613b33feade8da42b881b9d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b887056a36922c19185cbe37bcda1a2b84cce701613b33feade8da42b881b9d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b887056a36922c19185cbe37bcda1a2b84cce701613b33feade8da42b881b9d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6c20b8f35f1dc00cb40046aa4a485dfa79df3a7525334a5175ae13c25f0d9a7c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba2d54850fd8f7d13d51a8b8ab6209d50d66fd81afc11a491a0745199650ffa9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6d5bf7b78d353fa5aa0ffda91417a873189cc3f0b69d8350f67e3740376abe4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0be7b85c0eac4da29a0d92f176ceb8fef52decac35491a455c72deb0f110e881"
   end
 
   depends_on "go" => :build
