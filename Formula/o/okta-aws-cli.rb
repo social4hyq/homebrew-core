ae4731d4384d84797b77855f92c9e742d2f194c5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:02:56 +0800

    okta-aws-cli: update 2.6.0 bottle.

--- Formula/o/okta-aws-cli.rb
+++ Formula/o/okta-aws-cli.rb
@@ -12,12 +12,7 @@ class OktaAwsCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5e7ed56735b06b45c7c08503291102b4d933b825d960142280a4956f64ea4d93"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e7ed56735b06b45c7c08503291102b4d933b825d960142280a4956f64ea4d93"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e7ed56735b06b45c7c08503291102b4d933b825d960142280a4956f64ea4d93"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2b8257c2a91a3bb3665b238f94270b7e76e90757cea404191ef3b79ff04bcb40"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2725d92b1b5bb4d9c8e53ae37c62364d850032419d64577623470e62dd269673"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c38c00fe03b0d4944ba3ca2e094400242088bcc601dd460f4ba5602938ac9d2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4e327a5d5ae26dc18f861621ea8996e781410c1aeace34ba90912481c0bb4d9f"
   end
 
   depends_on "go" => :build
