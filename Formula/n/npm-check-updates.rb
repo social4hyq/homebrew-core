c8b554b713cfa1226aec03a1321ca181d190f158
hqzing <hqzing@outlook.com>
2026-05-15 13:17:45 +0000

    npm-check-updates 22.2.0 (new formula)

---
+++ Formula/n/npm-check-updates.rb
@@ -0,0 +1,43 @@
+class NpmCheckUpdates < Formula
+  desc "Find newer versions of dependencies than what your package.json allows"
+  homepage "https://github.com/raineorshine/npm-check-updates"
+  url "https://registry.npmjs.org/npm-check-updates/-/npm-check-updates-22.2.0.tgz"
+  sha256 "f36bccaf85d763b6f4f8d97c20daa12fe885bce9381753312f274f4166f7a948"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2a189f392015be95be9b088bf977802b69c993627e39883ac6c28d785b0b3de7"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    test_package_json = testpath/"package.json"
+    test_package_json.write <<~JSON
+      {
+        "dependencies": {
+          "express": "1.8.7",
+          "lodash": "3.6.1"
+        }
+      }
+    JSON
+
+    system bin/"ncu", "-u"
+
+    # Read the updated package.json to get the new dependency versions
+    updated_package_json = JSON.parse(test_package_json.read)
+    updated_express_version = updated_package_json["dependencies"]["express"]
+    updated_lodash_version = updated_package_json["dependencies"]["lodash"]
+
+    # Assert that both dependencies have been updated to higher versions
+    assert Gem::Version.new(updated_express_version) > Gem::Version.new("1.8.7"),
+      "Express version not updated as expected"
+    assert Gem::Version.new(updated_lodash_version) > Gem::Version.new("3.6.1"),
+      "Lodash version not updated as expected"
+  end
+end
