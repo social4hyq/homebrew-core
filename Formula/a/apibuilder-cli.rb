04643733bbdcdd81c42a117ab89949df005cb661
hqzing <hqzing@outlook.com>
2026-05-15 07:33:12 +0000

    apibuilder-cli 0.2.4 (new formula)

---
+++ Formula/a/apibuilder-cli.rb
@@ -0,0 +1,29 @@
+class ApibuilderCli < Formula
+  desc "Command-line interface to generate clients for api builder"
+  homepage "https://www.apibuilder.io"
+  url "https://github.com/apicollective/apibuilder-cli/archive/refs/tags/0.2.4.tar.gz"
+  sha256 "80291a48e1637c24ac691b5e56061264b31342d35a560a05a6fcabfb847a46fd"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "37089a9067afbb17f49925411efa1abfb9c00773976eeabe881562bb9b249084"
+  end
+
+  uses_from_macos "ruby"
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    (testpath/"config").write <<~EOS
+      [default]
+      token = abcd1234
+    EOS
+
+    assert_match "Profile default:",
+                 shell_output("#{bin}/read-config --path config")
+    assert_match "Could not find apibuilder configuration directory",
+                 shell_output("#{bin}/apibuilder", 1)
+  end
+end
