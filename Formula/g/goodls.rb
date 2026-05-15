2896bcb6b04ff75f7b8b68f33af3d985025f6ce9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 23:03:58 +0800

    goodls: update 2.0.6 bottle.

--- Formula/g/goodls.rb
+++ Formula/g/goodls.rb
@@ -7,14 +7,7 @@ class Goodls < Formula
   head "https://github.com/tanaikech/goodls.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bb271f1eea59804e392908cff1ac2387179f00e0e9a02fdc8bf51e6ed464f14d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dcaf66196cb83773ee01ecbafb2db92b9640641a90ba38388ea60904240beb4c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dcaf66196cb83773ee01ecbafb2db92b9640641a90ba38388ea60904240beb4c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "dcaf66196cb83773ee01ecbafb2db92b9640641a90ba38388ea60904240beb4c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "16161f75d9eabbe1263b7115aedd206dc84581c7e20f6775ad2d3f51ae355f2c"
-    sha256 cellar: :any_skip_relocation, ventura:       "16161f75d9eabbe1263b7115aedd206dc84581c7e20f6775ad2d3f51ae355f2c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9480958e7d8d1901b2b4f21204a54a53562d089f4298d36521201cdb03c7ce0f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "590b91e949cadbf80fdeaca615297fa72ed5afd864fa762fc584d49368958390"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5cba50f45e2dedeeebf3551285cb2ba630a3ae3f76f0f8f30a95af2f43e3d080"
   end
 
   depends_on "go" => :build
