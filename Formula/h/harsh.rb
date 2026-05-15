f7a390c9fefd59d5f807b72d92f3b3d79eeaf830
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 01:25:40 +0800

    harsh: update 0.14.3 bottle.

--- Formula/h/harsh.rb
+++ Formula/h/harsh.rb
@@ -7,12 +7,7 @@ class Harsh < Formula
   head "https://github.com/wakatara/harsh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3542f5e35b58bf8d97a93234be085b36fe6409d7fd54c99350dd2ad63bfb413e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3542f5e35b58bf8d97a93234be085b36fe6409d7fd54c99350dd2ad63bfb413e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3542f5e35b58bf8d97a93234be085b36fe6409d7fd54c99350dd2ad63bfb413e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c7487ea2137ffea2d10694b2e2ae9068edd63de8f9e984272f3bcb87f0c6049d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "45f5141a8ff967102b0357a7a6075ea123af6382fd9d6dd0263fdb86dd07446d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad4e78b485a0b7f93767dd9cc8002e22e90dcdc875d32045192d90f0cde37840"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "519ecb79f4706def2afd91ba8926a62849ef0c2683915842b173dd35355d6bc7"
   end
 
   depends_on "go" => :build
