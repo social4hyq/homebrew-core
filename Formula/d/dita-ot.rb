20e783872f445b15ca08ed517bec0396672d9e5d
hqzing <hqzing@outlook.com>
2026-05-15 16:04:32 +0000

    dita-ot 4.4 (new formula)

---
+++ Formula/d/dita-ot.rb
@@ -0,0 +1,34 @@
+class DitaOt < Formula
+  desc "DITA Open Toolkit is an implementation of the OASIS DITA specification"
+  homepage "https://www.dita-ot.org/"
+  url "https://github.com/dita-ot/dita-ot/releases/download/4.4/dita-ot-4.4.zip"
+  sha256 "598b9d405ed88112abb08a41189d750584e7eece86e89e97787777dea19401a0"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b3cc98aec54980cbeecbb3a2054744ec4e15cd61bc8c7585b711d14796439ee2"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm(Dir["bin/*.bat", "config/env.bat", "startcmd.*"])
+    libexec.install Dir["*"]
+    (bin/"dita").write_env_script libexec/"bin/dita", JAVA_HOME: Formula["openjdk"].opt_prefix
+
+    # Build an `:all` bottle by removing doc file.
+    rm libexec/"docsrc/topics/installing-via-homebrew.dita"
+  end
+
+  test do
+    system bin/"dita", "--input=#{libexec}/docsrc/site.ditamap",
+                       "--format=html5",
+                       "--output=#{testpath}/out"
+    assert_path_exists testpath/"out/index.html"
+  end
+end
