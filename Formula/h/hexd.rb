a412249d5e3c0cff7db54610199f69f17e8a1180
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-15 22:44:25 +0800

    hexd: update 1.1.0 bottle.

--- Formula/h/hexd.rb
+++ Formula/h/hexd.rb
@@ -6,14 +6,7 @@ class Hexd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "78fb673214aece803927239e51d3b92a134d2dc71bc13bfec30a41cf49f8ef2c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05ec65d79e0eae2ee7291cbb5fc523358b97e0f93a5b5cab039d02b27aa39230"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6258b4701ae5d49c39ee122ad9988d9647141a3c62ae49923dacc5086617c1f5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "542333622e6a776e913cd72d820671ae3da80180673f3f6ce93e911816f04b56"
-    sha256 cellar: :any_skip_relocation, sonoma:        "45a53e8aad1758074bbffb8e618a19c25645547d883d5f4ccf62cd2ac312658e"
-    sha256 cellar: :any_skip_relocation, ventura:       "dacdc00f2ad09c469316c672491b47d4242bf119eea23ed4de2d361518a3b7f8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a4f51eef0ca22a531e73b1b6bc75ae06ed9e987fee0d94a612121651577e22a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b62eca23b33b5257e4fb33ec61b9301700d7c732c2efe2037952fdc3b9e8cf8d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cf2706df5e517361d171173e0811f34471ffc014502c2e387858aaee8de9dbce"
   end
 
   def install
