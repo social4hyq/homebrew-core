0e8eae50d4ed3a04e22dd13090b1a319fc1b1a5c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 19:01:21 +0800

    syncthing: update 2.1.0 bottle.

--- Formula/s/syncthing.rb
+++ Formula/s/syncthing.rb
@@ -12,12 +12,7 @@ class Syncthing < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "874d8e5ba83d6c779294781d0b206737375b02f2cb4fabde54bf8e7846c157b2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "64908bac7b488755b10944b620bccc3351cec30e1212c7a1e0d354b334481afa"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f042622c100b9c2955d36c9af294241b538bfc60e8f204eefdb6dfc3d180806"
-    sha256 cellar: :any_skip_relocation, sonoma:        "99210a70877cd490de9a89adcf3393f1d120d3898b8d73d659be12a2b03cd3b8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "548868f971b346de6048d33e75e5ffe4d821eb7c7f6ee7718f9378f9c44857bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "626e608a788743cad9e5f68cc4e44a553a7f6b69c7d5f965c43f0cc3dc83df66"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "42f5948b92e3fc24c255dad42c7e743ddc2d3d647ce977dac21770ceeeff5716"
   end
 
   depends_on "go" => :build
