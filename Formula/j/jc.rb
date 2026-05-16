d9c023b71f53a69f03bcc7a0699f54e36c66a8bc
hqzing <hqzing@outlook.com>
2026-05-16 21:51:57 +0000

    jc 1.25.6 (new formula)

---
+++ Formula/j/jc.rb
@@ -0,0 +1,45 @@
+class Jc < Formula
+  include Language::Python::Virtualenv
+
+  desc "Serializes the output of command-line tools to structured JSON output"
+  homepage "https://github.com/kellyjonbrazil/jc"
+  url "https://files.pythonhosted.org/packages/7c/fa/f174bfb1ecfd3bdc6259b2de32052f09cccba11c9a1b733aedf618fadd05/jc-1.25.6.tar.gz"
+  sha256 "27f58befc7ae0a4c63322926c5f1ec892e3eac4a065eff3b07cfe420a6924a07"
+  license "MIT"
+  revision 1
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "d8b4e6e1d9832668d75f25d26a43235d6dafeb70a2f84786971dac4664415d5d"
+  end
+
+  depends_on "libyaml"
+  depends_on "python@3.14"
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "ruamel-yaml" do
+    url "https://files.pythonhosted.org/packages/c7/3b/ebda527b56beb90cb7652cb1c7e4f91f48649fbcd8d2eb2fb6e77cd3329b/ruamel_yaml-0.19.1.tar.gz"
+    sha256 "53eb66cd27849eff968ebf8f0bf61f46cdac2da1d1f3576dd4ccee9b25c31993"
+  end
+
+  resource "xmltodict" do
+    url "https://files.pythonhosted.org/packages/19/70/80f3b7c10d2630aa66414bf23d210386700aa390547278c789afa994fd7e/xmltodict-1.0.4.tar.gz"
+    sha256 "6d94c9f834dd9e44514162799d344d815a3a4faec913717a9ecbfa5be1bb8e61"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    man1.install "man/jc.1"
+    generate_completions_from_executable(bin/"jc", "--bash-comp", shells: [:bash], shell_parameter_format: :none)
+    generate_completions_from_executable(bin/"jc", "--zsh-comp", shells: [:zsh], shell_parameter_format: :none)
+  end
+
+  test do
+    assert_equal "[{\"header1\":\"data1\",\"header2\":\"data2\"}]\n",
+                  pipe_output("#{bin}/jc --csv", "header1, header2\n data1, data2")
+  end
+end
