08bbd9d6f584c2fdcf528bd408dfe4c398672bb9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 16:44:34 +0800

    vvenc: update 1.14.0 bottle.

--- Formula/v/vvenc.rb
+++ Formula/v/vvenc.rb
@@ -7,12 +7,7 @@ class Vvenc < Formula
   head "https://github.com/fraunhoferhhi/vvenc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "1bf0c02468ebe04b2da19e96db653dc600c403f323121b2985ea1beec6ac8dac"
-    sha256 cellar: :any,                 arm64_sequoia: "aafba28d027d7b04b59c291684f7e61abc9c234afa12c1341c78382d767e8725"
-    sha256 cellar: :any,                 arm64_sonoma:  "c9c91cf6a4d48ba248e7760c6d6e02f64c63158a87b4f2613c271bdd26be73ce"
-    sha256 cellar: :any,                 sonoma:        "7a092b2d4b10f9d3624620250b6f7e3b7b84e2b33c55c5c42ff97025c6580fdd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5783986556c6ba47a1eb3b4378bd8fba822a0f58ccfb7e3caa19021aabde8039"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32293a4895fb8b742751771b247662fb7e2abec5142a7467c03fd6d00627135e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "367aabd82f320c9c9b05f8c9798bfd6a943d2ea44ac7568356fca41b00ea8f62"
   end
 
   depends_on "cmake" => :build
