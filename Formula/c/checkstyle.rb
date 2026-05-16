135ccf631f6a45025ff898a661bcef06690f35b5
hqzing <hqzing@outlook.com>
2026-05-16 19:38:44 +0000

    checkstyle 13.4.2 (new formula)

---
+++ Formula/c/checkstyle.rb
@@ -0,0 +1,33 @@
+class Checkstyle < Formula
+  desc "Check Java source against a coding standard"
+  homepage "https://checkstyle.sourceforge.io/"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-13.4.2/checkstyle-13.4.2-all.jar"
+  sha256 "35dbdb99710781b6706222380b31a47b6d2f178dd47649e7dd6525cc15ae0cce"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "275b7bdea8bb97ec534c7f6847f1f94b9e49f74a8c1837d5e9d046f3ee31c54f"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "checkstyle-#{version}-all.jar"
+    bin.write_jar_script libexec/"checkstyle-#{version}-all.jar", "checkstyle"
+  end
+
+  test do
+    path = testpath/"foo.java"
+    path.write "public class Foo{ }\n"
+
+    output = shell_output("#{bin}/checkstyle -c /sun_checks.xml #{path}", 2)
+    errors = output.lines.select { |line| line.start_with?("[ERROR] #{path}") }
+    assert_match "#{path}:1:17: '{' is not preceded with whitespace.", errors.join(" ")
+    assert_equal errors.size, $CHILD_STATUS.exitstatus
+  end
+end
