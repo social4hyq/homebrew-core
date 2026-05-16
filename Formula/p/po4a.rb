c0dcd544b6f327160ab543206e7394278d8f7c84
hqzing <hqzing@outlook.com>
2026-05-16 09:42:35 +0000

    po4a 0.74 (new formula)

---
+++ Formula/p/po4a.rb
@@ -0,0 +1,180 @@
+require "language/perl"
+
+class Po4a < Formula
+  include Language::Perl::Shebang
+
+  desc "Documentation translation maintenance tool"
+  homepage "https://po4a.org"
+  url "https://github.com/mquinson/po4a/archive/refs/tags/v0.74.tar.gz"
+  sha256 "6e390eb7707501a86f2e648d78fddb0d211d1e8699aa1ee201176e9f966a798b"
+  license "GPL-2.0-or-later"
+  revision 1
+  head "https://github.com/mquinson/po4a.git", branch: "master"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "08b9a793f7b589dc09de4e2a9973fb4a944c2814e87bfbdd42a3b2344ffab49e"
+    sha256 cellar: :any,                 arm64_sequoia: "5ccc2e17840df2c7e6c1296c7d594adec3543b3b230819bf6164b0ea384e8bfb"
+    sha256 cellar: :any,                 arm64_sonoma:  "af2dada6c7c576abb28bfab0db2b74f06f476376c7e75515d72b2d88323ceb7f"
+    sha256 cellar: :any,                 sonoma:        "3a6cc9bd1e076edb447eada1df96fadcd0c4b0f74f23302e548dd8f6f1e6647b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "09e05f340a3e093c01436be844d9c5998ecc5e5740deb3daf8af71d56c80ef8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5927670f77918b411a03fa7ed9200ae7e9cd07076dbdfea62a414848bff525e9"
+  end
+
+  depends_on "docbook-xsl" => :build
+  depends_on "gettext"
+
+  uses_from_macos "libxslt"
+  uses_from_macos "perl"
+
+  # Resources for following Perl modules and their recursive dependencies:
+  # * Locale::gettext
+  # * Pod::Parser
+  # * SGMLS
+  # * Syntax::Keyword::Try -> XS::Parse::Keyword::Builder (build) -> ...
+  # * Term::ReadKey
+  # * Text::WrapI18N -> Text::CharWidth
+  # * Unicode::GCString -> MIME::Charset
+  # * YAML::Tiny
+
+  on_linux do
+    resource "Module::Build" do
+      url "https://cpan.metacpan.org/authors/id/L/LE/LEONT/Module-Build-0.4234.tar.gz"
+      sha256 "66aeac6127418be5e471ead3744648c766bd01482825c5b66652675f2bc86a8f"
+    end
+
+    resource "Class::Inspector" do
+      url "https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/Class-Inspector-1.36.tar.gz"
+      sha256 "cc295d23a472687c24489d58226ead23b9fdc2588e522f0b5f0747741700694e"
+    end
+
+    resource "File::ShareDir" do
+      url "https://cpan.metacpan.org/authors/id/R/RE/REHSACK/File-ShareDir-1.118.tar.gz"
+      sha256 "3bb2a20ba35df958dc0a4f2306fc05d903d8b8c4de3c8beefce17739d281c958"
+    end
+
+    resource "Term::ReadKey" do
+      url "https://cpan.metacpan.org/authors/id/J/JS/JSTOWE/TermReadKey-2.38.tar.gz"
+      sha256 "5a645878dc570ac33661581fbb090ff24ebce17d43ea53fd22e105a856a47290"
+    end
+
+    resource "YAML::Tiny" do
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/YAML-Tiny-1.76.tar.gz"
+      sha256 "a8d584394cf069bf8f17cba3dd5099003b097fce316c31fb094f1b1c171c08a3"
+    end
+  end
+
+  resource "Locale::gettext" do
+    url "https://cpan.metacpan.org/authors/id/P/PV/PVANDRY/Locale-gettext-1.07.tar.gz"
+    sha256 "909d47954697e7c04218f972915b787bd1244d75e3bd01620bc167d5bbc49c15"
+  end
+
+  resource "Pod::Parser" do
+    url "https://cpan.metacpan.org/authors/id/M/MA/MAREKR/Pod-Parser-1.67.tar.gz"
+    sha256 "5deccbf55d750ce65588cd211c1a03fa1ef3aaa15d1ac2b8d85383a42c1427ea"
+  end
+
+  resource "SGMLS" do
+    url "https://cpan.metacpan.org/authors/id/R/RA/RAAB/SGMLSpm-1.1.tar.gz"
+    sha256 "550c9245291c8df2242f7e88f7921a0f636c7eec92c644418e7d89cfea70b2bd"
+  end
+
+  resource "Text::CharWidth" do
+    url "https://cpan.metacpan.org/authors/id/K/KU/KUBOTA/Text-CharWidth-0.04.tar.gz"
+    sha256 "abded5f4fdd9338e89fd2f1d8271c44989dae5bf50aece41b6179d8e230704f8"
+  end
+
+  resource "Text::WrapI18N" do
+    url "https://cpan.metacpan.org/authors/id/K/KU/KUBOTA/Text-WrapI18N-0.06.tar.gz"
+    sha256 "4bd29a17f0c2c792d12c1005b3c276f2ab0fae39c00859ae1741d7941846a488"
+  end
+
+  resource "Unicode::GCString" do
+    url "https://cpan.metacpan.org/authors/id/N/NE/NEZUMI/Unicode-LineBreak-2019.001.tar.gz"
+    sha256 "486762e4cacddcc77b13989f979a029f84630b8175e7fef17989e157d4b6318a"
+  end
+
+  resource "MIME::Charset" do
+    url "https://cpan.metacpan.org/authors/id/N/NE/NEZUMI/MIME-Charset-1.013.1.tar.gz"
+    sha256 "1bb7a6e0c0d251f23d6e60bf84c9adefc5b74eec58475bfee4d39107e60870f0"
+  end
+
+  resource "ExtUtils::CChecker" do
+    url "https://cpan.metacpan.org/authors/id/P/PE/PEVANS/ExtUtils-CChecker-0.12.tar.gz"
+    sha256 "8b87d145337dec1ee754d30871d0b105c180ad4c92c7dc0c7fadd76cec8c57d3"
+  end
+
+  resource "XS::Parse::Keyword::Builder" do
+    url "https://cpan.metacpan.org/authors/id/P/PE/PEVANS/XS-Parse-Keyword-0.49.tar.gz"
+    sha256 "76c5ed142abba1f1df2335849681c83d83cc0842fe854af71081d2c411efb0bb"
+  end
+
+  resource "Syntax::Keyword::Try" do
+    url "https://cpan.metacpan.org/authors/id/P/PE/PEVANS/Syntax-Keyword-Try-0.31.tar.gz"
+    sha256 "7bc6242d746378982a599b34de35f07d3decc9e09d5646f8fa3b87f459414a4a"
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    ENV.prepend_path "PERL5LIB", libexec/"lib"
+    ENV["PERL_MM_USE_DEFAULT"] = "1"
+
+    resources.each do |r|
+      r.stage do
+        # Workaround for macOS perl as MakeMaker can only search libraries in perl compile-time paths
+        # Issue ref: https://github.com/Perl-Toolchain-Gang/ExtUtils-MakeMaker/issues/277
+        if OS.mac? && r.name == "Locale::gettext"
+          inreplace "Makefile.PL", '$libs = "-lintl"', "$libs = \"-L#{Formula["gettext"].opt_lib} -lintl\""
+        end
+
+        if File.exist?("Makefile.PL")
+          system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}", "NO_MYMETA=1"
+          system "make", "install"
+        else
+          system "perl", "Build.PL", "--install_base", libexec
+          system "./Build"
+          system "./Build", "install"
+        end
+      end
+    end
+
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    system "perl", "Build.PL", "--install_base", libexec
+    system "./Build"
+    system "./Build", "install"
+
+    shell_scripts = %w[po4a-display-man po4a-display-pod]
+
+    %w[msguntypot po4a po4a-display-man po4a-display-pod
+       po4a-gettextize po4a-translate po4a-normalize po4a-updatepo].each do |cmd|
+      rewrite_shebang detected_perl_shebang, libexec/"bin"/cmd unless shell_scripts.include? cmd
+
+      (bin/cmd).write_env_script(libexec/"bin"/cmd, PERL5LIB: ENV["PERL5LIB"])
+    end
+
+    man1.install Dir[libexec/"man/man1/{msguntypot.1p.gz,po4a*}"]
+    man3.install Dir[libexec/"man/man3/Locale::Po4a::*"]
+    man7.install Dir[libexec/"man/man7/*"]
+  end
+
+  test do
+    # LaTeX
+
+    (testpath/"en.tex").write <<~'TEX'
+      \documentclass[a4paper]{article}
+      \begin{document}
+      Hello from Homebrew!
+      \end{document}
+    TEX
+
+    system bin/"po4a-updatepo", "-f", "latex", "-m", "en.tex", "-p", "latex.pot"
+    assert_match "Hello from Homebrew!", (testpath/"latex.pot").read
+
+    # Markdown
+
+    (testpath/"en.md").write("Hello from Homebrew!")
+    system bin/"po4a-updatepo", "-f", "text", "-m", "en.md", "-p", "text.pot"
+    assert_match "Hello from Homebrew!", (testpath/"text.pot").read
+  end
+end
