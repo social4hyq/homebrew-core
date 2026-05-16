63468dd0f986bd90c26878cff63b7544a77ca57b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 15:35:51 +0800

    sysstat: update 12.7.9 bottle.

--- Formula/s/sysstat.rb
+++ Formula/s/sysstat.rb
@@ -7,8 +7,7 @@ class Sysstat < Formula
   head "https://github.com/sysstat/sysstat.git", branch: "master"
 
   bottle do
-    sha256 arm64_linux:  "3043eefebf552d698f327d67e02fd1c5e712dc098dca13504977101a02965d58"
-    sha256 x86_64_linux: "78ff08b5505015157b91169a5363cbbdaa455c7c1dcafedb1c79e58305b25dc3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b7b9f2fd9c6af11c1d2fd483ee218bf9f111363ced28df10cf71cf91b30770b0"
   end
 
   depends_on :linux
