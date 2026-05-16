94affa9f3217b9918a0a1b2a0bce298623ad4c53
hqzing <hqzing@outlook.com>
2026-05-16 04:22:37 +0000

    basedpyright 1.39.4 (new formula)

---
+++ Formula/b/basedpyright.rb
@@ -0,0 +1,31 @@
+class Basedpyright < Formula
+  desc "Pyright fork with various improvements and built-in pylance features"
+  homepage "https://github.com/DetachHead/basedpyright"
+  url "https://registry.npmjs.org/basedpyright/-/basedpyright-1.39.4.tgz"
+  sha256 "88da55e7993d21d6a11e8918c21fac658626a5e222a31dd0018c1f8468cfdfe5"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7fdab8c9c2ea974b93bbd3ab2b8d189c6ba34a317c8d8c6c37ee8942791639eb"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/pyright" => "basedpyright"
+    bin.install_symlink libexec/"bin/pyright-langserver" => "basedpyright-langserver"
+
+    # Remove empty folder to make :all bottle
+    rm_r libexec/"lib/node_modules/basedpyright/node_modules" if OS.mac?
+  end
+
+  test do
+    (testpath/"broken.py").write <<~PYTHON
+      def wrong_types(a: int, b: int) -> str:
+          return a + b
+    PYTHON
+    output = shell_output("#{bin}/basedpyright broken.py 2>&1", 1)
+    assert_match "error: Type \"int\" is not assignable to return type \"str\"", output
+  end
+end
