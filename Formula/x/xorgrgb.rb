a34f94b86486d364f441cf340130eb251f1e1209
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 13:25:46 +0800

    xorgrgb: update 1.1.1 bottle.

--- Formula/x/xorgrgb.rb
+++ Formula/x/xorgrgb.rb
@@ -6,12 +6,7 @@ class Xorgrgb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "159d5fec59b9e636d32f19ea34afbede62f66570ccdf032640bcec65bab8dd8a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35bf31cee3ecc5346e8e702941b781afeb6ac9e3e41723094a47ab4863429fbb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f3467d30599e1855fc62b23db5b4dbe709b593c90b9fef775ea86141bd6c05d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f7638605023153225dc39fb4322ef00ec039531ca1ba13f12ff26f2dbf8b17d6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "625580b1d2ca0c33fa1aaea94a23cf29945e8d9a9de97286ea728369b5a8ff6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d3a5e626941b7b150c0e39f5101b8320d637f20ac0f03f6532d7f671aa2f001"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0343869afd7bd2f71c2ae3d5da508f7af650a4fe4fbe802293508ea036500695"
   end
 
   depends_on "pkgconf" => :build
