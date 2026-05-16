ade144657d96b907feae102c7510f6d6b1130a1b
hqzing <hqzing@outlook.com>
2026-05-16 16:13:59 +0000

    rake-completion 1.0.3 (new formula)

---
+++ Formula/r/rake-completion.rb
@@ -0,0 +1,25 @@
+class RakeCompletion < Formula
+  desc "Bash completion for Rake"
+  homepage "https://github.com/mernen/completion-ruby"
+  url "https://github.com/mernen/completion-ruby/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "cbcd002bba2a43730cff54f5386565917913d9dec16dcd89345fbe298fe4316b"
+  license "MIT"
+  head "https://github.com/mernen/completion-ruby.git", branch: "main"
+
+  livecheck do
+    formula "ruby-completion"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "5004cd20889962fc9ce3a921f56e27cb5a28e0f88fde57ae1428e28e0dbf89bb"
+  end
+
+  def install
+    bash_completion.install "completion-rake" => "rake"
+  end
+
+  test do
+    assert_match "-F __rake",
+      shell_output("bash -c 'source #{bash_completion}/rake && complete -p rake'")
+  end
+end
