9067ed583330a9312a109b9e6a7ec3740294b8ec
hqzing <hqzing@outlook.com>
2026-05-16 16:14:40 +0000

    detekt 1.23.8 (new formula)

---
+++ Formula/d/detekt.rb
@@ -0,0 +1,38 @@
+class Detekt < Formula
+  desc "Static code analysis for Kotlin"
+  homepage "https://github.com/detekt/detekt"
+  url "https://github.com/detekt/detekt/releases/download/v1.23.8/detekt-cli-1.23.8-all.jar"
+  sha256 "2ce2ff952e150baf28a29cda70a363b0340b3e81a55f43e51ec5edffc3d066c1"
+  license "Apache-2.0"
+
+  livecheck do
+    url :homepage
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "37152acb3b7195f62c6e972c3deb2d8423a59f907e1e3452a94628c6e4023ea0"
+  end
+
+  depends_on "openjdk@21"
+
+  def install
+    libexec.install "detekt-cli-#{version}-all.jar"
+    bin.write_jar_script libexec/"detekt-cli-#{version}-all.jar", "detekt", java_version: "21"
+  end
+
+  test do
+    # generate default config for testing
+    system bin/"detekt", "--generate-config"
+    assert_match "empty-blocks:", File.read(testpath/"detekt.yml")
+
+    (testpath/"input.kt").write <<~KOTLIN
+      fun main() {
+
+      }
+    KOTLIN
+
+    shell_output("#{bin}/detekt --input input.kt --report txt:output.txt --config #{testpath}/detekt.yml", 2)
+    assert_equal "EmptyFunctionBlock", shell_output("cat output.txt").slice(/\w+/)
+  end
+end
