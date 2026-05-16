151f0557911dc5eb18760e47fc7ad7c463a9ba02
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 22:24:41 +0800

    linkerd: update 2.14.10 bottle.

--- Formula/l/linkerd.rb
+++ Formula/l/linkerd.rb
@@ -13,16 +13,7 @@ class Linkerd < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "92f89d28c0b857ea8f76583f7ad385ac68156aa36e8a1a144f960779f0a85d4d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "38c1c491271ea2f1e76a50c01f919f24528b409311a6c5e0b5801b8d7b4df78e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4dd8a49fcb56e1eab62825fa310ffe6966d9aac27ebf4356b0f2be56ccb7d2ca"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ecd3045e2d699c0bb1a16619ec5b045fae1d92293a3276c754354c3d201cc2aa"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5e275b3c5a026354bc0db5683f6044b75e551dafc0d40dd70144a9bc374b61c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a805f780856c04434e1cf56bf9722ff2d72e78ccbd4cd2bfec4b4667dca06084"
-    sha256 cellar: :any_skip_relocation, ventura:        "e5c1ac2ba88949aa2e98ca4e39d5532a33d14cfad608d8776bb2f10e47cd4f8e"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d982f58ff4958cc8a9cfabb65325d72dceb0c01c3b2c6200473868fb526a650"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "77bc80d5bd80767167b7895ab1afb69da5b45ef309cbd29db4956208ed8af55a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c1695ba96f330ebb2cfe3267a5e3cb4e77b092410ac9dac2465a977317d00945"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3f7437df2498443cecbf6c526d785df492ef5cd243b06cf7bf13e24b3b16d464"
   end
 
   depends_on "go" => :build
