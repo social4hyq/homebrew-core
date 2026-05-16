7cc96b29d231a1eff4ab70a2723e0947fffaab4d
hqzing <hqzing@outlook.com>
2026-05-16 04:16:39 +0000

    tbb 2023.0.0 (new formula)

---
+++ Formula/t/tbb.rb
@@ -0,0 +1,106 @@
+class Tbb < Formula
+  desc "Rich and complete approach to parallelism in C++"
+  homepage "https://uxlfoundation.github.io/oneTBB/"
+  url "https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v2023.0.0.tar.gz"
+  sha256 "f8767b971ec6aea25dde58ae0f593e94e7aa75a739a86f67967012f69e2199b1"
+  license "Apache-2.0"
+  compatibility_version 1
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "00e4867e969892148347ae14b779209cac8de44c69227b86749ff4840f6184e8"
+    sha256 cellar: :any,                 arm64_sequoia: "939acac1ca4fad1bdee9ef1d03a15833c865f261025dfaf1628b9c70745ad0f7"
+    sha256 cellar: :any,                 arm64_sonoma:  "35e95645a813d1d889347ecbfc7b27e79ca5bb0fba3dc339d527068217f70410"
+    sha256 cellar: :any,                 sonoma:        "ae17b9ec825d97023639168e5cd62ab5074063b54b089a169ce5e02fc771a7c0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a6dd633794722403f96b0e41d16310d4099844902509ef7db431e016dd23343b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "519c5cc0a84ff581b4fa78684d4a15ba6d87daa5f381e3a1b8930dc6eca8daef"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.14" => [:build, :test]
+  depends_on "swig" => :build
+  depends_on "hwloc"
+
+  def python3
+    "python3.14"
+  end
+
+  def install
+    site_packages = Language::Python.site_packages(python3)
+    tbb_site_packages = prefix/site_packages/"tbb"
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath},-rpath,#{rpath(source: tbb_site_packages)}"
+
+    args = %W[
+      -DTBB_TEST=OFF
+      -DTBB4PY_BUILD=ON
+      -DPYTHON_EXECUTABLE=#{which(python3)}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build/shared",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    *args, *std_cmake_args
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
+
+    system "cmake", "-S", ".", "-B", "build/static",
+                    "-DBUILD_SHARED_LIBS=OFF",
+                    *args, *std_cmake_args
+    system "cmake", "--build", "build/static"
+    lib.install buildpath.glob("build/static/*/libtbb*.a")
+  end
+
+  test do
+    # The glob that installs these might fail,
+    # so let's check their existence.
+    assert_path_exists lib/"libtbb.a"
+    assert_path_exists lib/"libtbbmalloc.a"
+
+    (testpath/"cores-types.cpp").write <<~CPP
+      #include <cstdlib>
+      #include <tbb/task_arena.h>
+
+      int main() {
+          const auto numa_nodes = tbb::info::numa_nodes();
+          const auto size = numa_nodes.size();
+          const auto type = numa_nodes.front();
+          return size != 1 || type != tbb::task_arena::automatic ? EXIT_SUCCESS : EXIT_FAILURE;
+      }
+    CPP
+
+    system ENV.cxx, "cores-types.cpp", "-std=c++14", "-DTBB_PREVIEW_TASK_ARENA_CONSTRAINTS_EXTENSION=1",
+                                      "-L#{lib}", "-ltbb", "-o", "core-types"
+    system "./core-types"
+
+    (testpath/"sum1-100.cpp").write <<~CPP
+      #include <iostream>
+      #include <tbb/blocked_range.h>
+      #include <tbb/parallel_reduce.h>
+
+      int main()
+      {
+        auto total = tbb::parallel_reduce(
+          tbb::blocked_range<int>(0, 100),
+          0.0,
+          [&](tbb::blocked_range<int> r, int running_total)
+          {
+            for (int i=r.begin(); i < r.end(); ++i) {
+              running_total += i + 1;
+            }
+
+            return running_total;
+          }, std::plus<int>()
+        );
+
+        std::cout << total << std::endl;
+        return 0;
+      }
+    CPP
+
+    system ENV.cxx, "sum1-100.cpp", "-std=c++14", "-L#{lib}", "-ltbb", "-o", "sum1-100"
+    assert_equal "5050", shell_output("./sum1-100").chomp
+
+    system python3, "-c", "import tbb"
+  end
+end
