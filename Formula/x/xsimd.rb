ee3605cda4df68a665a467206de31ab7da28d1b9
hqzing <hqzing@outlook.com>
2026-05-16 03:04:31 +0000

    xsimd 14.2.0 (new formula)

---
+++ Formula/x/xsimd.rb
@@ -0,0 +1,51 @@
+class Xsimd < Formula
+  desc "Modern, portable C++ wrappers for SIMD intrinsics"
+  homepage "https://xsimd.readthedocs.io/en/latest/"
+  url "https://github.com/xtensor-stack/xsimd/archive/refs/tags/14.2.0.tar.gz"
+  sha256 "21e841ab684b05331e81e7f782431753a029ef7b7d9d6d3ddab837e7782a40ee"
+  license "BSD-3-Clause"
+  compatibility_version 1
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2bc310b5c4877504470689b912f1deb2d0a1a3611ed06ce54106faa517ed1b75"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_TESTS=OFF", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <vector>
+      #include <type_traits>
+
+      #include "xsimd/config/xsimd_macros.hpp"
+      #include "xsimd/memory/xsimd_alignment.hpp"
+
+      using namespace xsimd;
+
+      struct mock_container {};
+
+      int main(void) {
+        using u_vector_type = std::vector<double>;
+        using a_vector_type = std::vector<double, xsimd::default_allocator<double>>;
+
+        using u_vector_align = container_alignment_t<u_vector_type>;
+        using a_vector_align = container_alignment_t<a_vector_type>;
+        using mock_align = container_alignment_t<mock_container>;
+
+        if(!std::is_same<u_vector_align, unaligned_mode>::value) abort();
+        if(!std::is_same<a_vector_align, aligned_mode>::value) abort();
+        if(!std::is_same<mock_align, unaligned_mode>::value) abort();
+        return 0;
+      }
+    CPP
+
+    system ENV.cxx, "test.cpp", "-std=c++14", "-I#{include}", "-o", "test"
+    system "./test"
+  end
+end
