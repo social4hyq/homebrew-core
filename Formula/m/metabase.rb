74bd542aac0943cf067a1d281e7d052730d4def5
hqzing <hqzing@outlook.com>
2026-05-16 10:30:55 +0000

    metabase 0.61.1 (new formula)

---
+++ Formula/m/metabase.rb
@@ -0,0 +1,53 @@
+class Metabase < Formula
+  desc "Business intelligence report server"
+  homepage "https://www.metabase.com/"
+  url "https://downloads.metabase.com/v0.61.1/metabase.jar"
+  sha256 "8504490d4f61007cf806480315df6b24477fdce7de8224c0d2ccbaa7946e3455"
+  license "AGPL-3.0-only"
+
+  # The first-party download page only provides an unversioned link to the
+  # latest OSS jar file. We check the "latest" GitHub release, as the release
+  # body text contains a versioned link to the OSS jar file.
+  livecheck do
+    url :head
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7a6f52269b8a2d3fb6e11321530f6ec279bd236f15987b1db4a74e3d65d765f3"
+  end
+
+  head do
+    url "https://github.com/metabase/metabase.git", branch: "master"
+
+    depends_on "leiningen" => :build
+    depends_on "node" => :build
+    depends_on "yarn" => :build
+  end
+
+  depends_on "openjdk"
+
+  def install
+    if build.head?
+      system "./bin/build"
+      libexec.install "target/uberjar/metabase.jar"
+    else
+      libexec.install "metabase.jar"
+    end
+
+    bin.write_jar_script libexec/"metabase.jar", "metabase"
+  end
+
+  service do
+    run opt_bin/"metabase"
+    keep_alive true
+    require_root true
+    working_dir var/"metabase"
+    log_path var/"metabase/server.log"
+    error_log_path File::NULL
+  end
+
+  test do
+    system bin/"metabase", "migrate", "up"
+  end
+end
