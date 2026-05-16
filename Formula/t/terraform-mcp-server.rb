9a4e3fbde5c066f1f6d190df39f712105dcaf016
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 17:08:26 +0800

    terraform-mcp-server: update 0.5.2 bottle.

--- Formula/t/terraform-mcp-server.rb
+++ Formula/t/terraform-mcp-server.rb
@@ -7,12 +7,7 @@ class TerraformMcpServer < Formula
   head "https://github.com/hashicorp/terraform-mcp-server.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f87bfc41fc5020874084a58bf6ddeed58afaf4f00b5871461474efc371a17b79"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f87bfc41fc5020874084a58bf6ddeed58afaf4f00b5871461474efc371a17b79"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f87bfc41fc5020874084a58bf6ddeed58afaf4f00b5871461474efc371a17b79"
-    sha256 cellar: :any_skip_relocation, sonoma:        "86890a2cebd7a1eec8a11ce4a36282863908b957ccef7c65587c5daf03c5f74c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "54f11910c8d624a3039b9b568092535eb01cb18064297c845dd12445415527e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26ca63aa090762f9b1d0e5eb7fcad6fea08c2d0df1c8390bc5600d024bf97c62"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ce0553415d4339ac1dc3e4e96b24f05b215d176815d67a58a42a35460aa26be9"
   end
 
   depends_on "go" => :build
