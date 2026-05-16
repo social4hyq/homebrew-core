70308af7a469d0e99fec34c1a5ae7e4403244b87
hqzing <hqzing@outlook.com>
2026-05-16 16:41:33 +0000

    certigo 1.18.0 (new formula)

---
+++ Formula/c/certigo.rb
@@ -0,0 +1,57 @@
+class Certigo < Formula
+  desc "Utility to examine and validate certificates in a variety of formats"
+  homepage "https://github.com/square/certigo"
+  url "https://github.com/square/certigo/archive/refs/tags/v1.18.0.tar.gz"
+  sha256 "f532bc215b8f57af6bd823d16b6ef2d57499a24f949722d06a2d1c8ea64df225"
+  license "Apache-2.0"
+  head "https://github.com/square/certigo.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "44862825248945f87685110c1d1f17c8747c1ea7324291851092ce1c305cb420"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b90746920cb04d611b1f63ffd5a21a90402a256d0a4b5dbdb2cd44f7fc41bbb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3dd0367a4272f7ed14fdd728a6c662702976bf2a5033c1a433b91ae53e69b819"
+    sha256 cellar: :any_skip_relocation, sonoma:        "18d5ebad42f5b96207b884102cfc401c086848f002645413216ac512a3134152"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a01187764b521a5ecebb086618c825f08fe2657fec32067ecb0f7f4a028e7b0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6870ecd9f505c65d452c8b3a9450077a204eaeeeaf88300b4ada945d312a87ad"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "./build"
+    bin.install "bin/certigo"
+
+    generate_completions_from_executable(bin/"certigo", shell_parameter_format: "--completion-script-",
+                                                        shells:                 [:bash, :zsh])
+  end
+
+  test do
+    (testpath/"test.crt").write <<~EOS
+      -----BEGIN CERTIFICATE-----
+      MIIDLDCCAhQCCQCa74bQsAj2/jANBgkqhkiG9w0BAQsFADBYMQswCQYDVQQGEwJV
+      UzELMAkGA1UECBMCQ0ExEDAOBgNVBAoTB2NlcnRpZ28xEDAOBgNVBAsTB2V4YW1w
+      bGUxGDAWBgNVBAMTD2V4YW1wbGUtZXhwaXJlZDAeFw0xNjA2MTAyMjE0MTJaFw0x
+      NjA2MTEyMjE0MTJaMFgxCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEQMA4GA1UE
+      ChMHY2VydGlnbzEQMA4GA1UECxMHZXhhbXBsZTEYMBYGA1UEAxMPZXhhbXBsZS1l
+      eHBpcmVkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs6JY7Hm/NAsH
+      3nuMOOSBno6WmwsTYEw3hk4eyprWiI/NpoiaiZVCGahT8NAKqLDW5t9vgKz6c4ff
+      i5/aQ2scichq3QS7pELAYlS4b+ey3dA6hj62MOTTO4Ad5bFbbRZG+Mdm2Ayvl6eV
+      6catQhMvxt7aIoY9+bodyIYC1zZVqwQ5sOT+CPLDnxK+GvhoyD2jL/XwZplWiIVL
+      oX6eEpKIo/QtB6mSU216F/PuAzl/BJond+RzF9mcxJjdZYZlhwT8+o8oXEMI4vEf
+      3yzd+zB/mjuxDJR2iw3bSL+zZr2GV/CsMLG/jmvbpIuyI/p5eTy0alz+iHOiyeCN
+      9pgD6jyonwIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAMUuv/zVYniJ94GdOVcNJ/
+      bL3CxR5lo6YB04S425qsVrmOex3IQBL1fUduKSSxh5nF+6nzhRzRrDzp07f9pWHL
+      ZHt6rruVhE1Eqt7TKKCtZg0d85lmx5WddL+yWc5cI1UtCohB9+iZDPUBUR9RcszQ
+      dGD9PmxnPc9soEcQw/3iNffhMMpLRhPaRW9qtJU8wk16DZunWR8E0Oeq42jVTnb4
+      ZiD1Idajj0tj/rT5/M1K/ZLEiOzXVpo/+l/+hoXw9eVnRa2nBwjoiZ9cMuGKUpHm
+      YSv7SyFevNwDwcxcAq6uVitKi0YCqHiNZ7Ye3/BGRDUFpK2IASUo8YbXYNyA/6nu
+      -----END CERTIFICATE-----
+    EOS
+    system bin/"certigo", "dump", "test.crt"
+  end
+end
