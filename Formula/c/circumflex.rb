3c310459c6d9bc374ee12290781504fb9dae6798
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 16:31:57 +0800

    circumflex: update 4.1.1 bottle.

--- Formula/c/circumflex.rb
+++ Formula/c/circumflex.rb
@@ -7,12 +7,7 @@ class Circumflex < Formula
   head "https://github.com/bensadeh/circumflex.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "82a4d31a5eb00cf571a421d8ad04afe3bba3a61a3d2488e37ab6ce165dcf7777"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "82a4d31a5eb00cf571a421d8ad04afe3bba3a61a3d2488e37ab6ce165dcf7777"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82a4d31a5eb00cf571a421d8ad04afe3bba3a61a3d2488e37ab6ce165dcf7777"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7acb0988fa0e0e38e9b99eaf036e3027126bff2830981330ef33ea503272936f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f7a0c4809eb6ae9f352745078e68639baaf561635b4df4519497beec0d377710"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "266a0692420b33841d22f764b43f2b1eb28a9b6ec9adfe72a4b5178c1a82f6b4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "30e19f734d49dd8a00e75bfa28ac6f7ff7ba4cfde13a44717da68713bed09668"
   end
 
   depends_on "go" => :build
