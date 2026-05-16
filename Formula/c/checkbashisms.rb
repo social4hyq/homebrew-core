7d5935669348618d273b66d31e2f8b92a4e13565
hqzing <hqzing@outlook.com>
2026-05-16 19:21:47 +0000

    checkbashisms 2.26.8 (new formula)

---
+++ Formula/c/checkbashisms.rb
@@ -0,0 +1,40 @@
+class Checkbashisms < Formula
+  desc "Checks for bashisms in shell scripts"
+  homepage "https://launchpad.net/ubuntu/+source/devscripts/"
+  url "https://deb.debian.org/debian/pool/main/d/devscripts/devscripts_2.26.8.tar.xz"
+  sha256 "3b97d88d14302761ad42f83d0a86a5148fd106df997802b8f72388b44f3d6d5f"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://deb.debian.org/debian/pool/main/d/devscripts/"
+    regex(/href=.*?devscripts[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3f24a13ec015cc38acb252a67ac420e99c04485d05548a26b50ae261d973ec89"
+  end
+
+  def install
+    inreplace "scripts/checkbashisms.pl" do |s|
+      s.gsub! "###VERSION###", version.to_s
+      s.gsub! "#!/usr/bin/perl", "#!/usr/bin/perl -T"
+    end
+
+    bin.install "scripts/checkbashisms.pl" => "checkbashisms"
+    man1.install "scripts/checkbashisms.1"
+  end
+
+  test do
+    (testpath/"test.sh").write <<~SHELL
+      #!/bin/sh
+
+      if [[ "home == brew" ]]; then
+        echo "dog"
+      fi
+    SHELL
+    expected = <<~EOS
+      (alternative test command ([[ foo ]] should be [ foo ])):
+    EOS
+    assert_match expected, shell_output("#{bin}/checkbashisms #{testpath}/test.sh 2>&1", 1)
+  end
+end
