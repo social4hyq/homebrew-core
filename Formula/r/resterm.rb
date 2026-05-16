b16c90970d3a792b4df9cf1f3c6cd7d87940e810
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 13:22:43 +0800

    resterm: update 0.39.3 bottle.

--- Formula/r/resterm.rb
+++ Formula/r/resterm.rb
@@ -7,12 +7,7 @@ class Resterm < Formula
   head "https://github.com/unkn0wn-root/resterm.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aecc87fba0fc2a780f966e9b6c8c4d262663735ddb31ef2b3e66279cb728211a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aecc87fba0fc2a780f966e9b6c8c4d262663735ddb31ef2b3e66279cb728211a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aecc87fba0fc2a780f966e9b6c8c4d262663735ddb31ef2b3e66279cb728211a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "dea3332578e8cf303a8ac8a44d1c2119052f33f7f3657b6977acf95ecd26b803"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c3b0137f17589fc3dfcfbe925ee6e6a198f87906fde4af5931b358b3d729a1a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1009b5bf1cd44c7f8a071238110dfc85254624600b2be0705bacd8fe31fc81c7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e2098bb6e3013d54637605650f83923b8b2585dbdfd7a2e3062f6edd8a25cad2"
   end
 
   depends_on "go" => :build
