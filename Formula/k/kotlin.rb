92b30f7f87204d98fe79f6c3ed685527deeaa079
hqzing <hqzing@outlook.com>
2026-05-16 19:11:27 +0000

    kotlin 2.3.21 (new formula)

---
+++ Formula/k/kotlin.rb
@@ -0,0 +1,42 @@
+class Kotlin < Formula
+  desc "Statically typed programming language for the JVM"
+  homepage "https://kotlinlang.org/"
+  url "https://github.com/JetBrains/kotlin/releases/download/v2.3.21/kotlin-compiler-2.3.21.zip"
+  sha256 "a8cfc1d62cd4d0de4d04f42575e40135bd620588c17d568a20eb9c7c259af14f"
+  license "Apache-2.0"
+
+  # Upstream maintains multiple major/minor versions and the "latest" release
+  # may be for a lower version, so we have to check multiple releases to
+  # identify the highest version.
+  livecheck do
+    url :stable
+    strategy :github_releases
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "82fb66db104e3cc1dd607b4046db9ff5f721b9bb54858f47b4c5f296d131d726"
+  end
+
+  depends_on "openjdk"
+
+  conflicts_with cask: "kotlin-native"
+
+  def install
+    libexec.install "bin", "build.txt", "lib"
+    rm Dir[libexec/"bin/*.bat"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+    prefix.install "license"
+  end
+
+  test do
+    (testpath/"test.kt").write <<~KOTLIN
+      fun main(args: Array<String>) {
+        println("Hello World!")
+      }
+    KOTLIN
+
+    system bin/"kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system bin/"kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
+  end
+end
