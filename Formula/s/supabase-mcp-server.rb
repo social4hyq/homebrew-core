a389666bb05051a3226bf60ceadd2d03e564023e
hqzing <hqzing@outlook.com>
2026-05-16 04:20:58 +0000

    supabase-mcp-server 0.8.1 (new formula)

---
+++ Formula/s/supabase-mcp-server.rb
@@ -0,0 +1,31 @@
+class SupabaseMcpServer < Formula
+  desc "MCP Server for Supabase"
+  homepage "https://supabase.com/docs/guides/getting-started/mcp"
+  url "https://registry.npmjs.org/@supabase/mcp-server-supabase/-/mcp-server-supabase-0.8.1.tgz"
+  sha256 "a6976a1ce688e5e8e61ecc52b7abd60f689c0721b3163fdf8187f0e8fbfee4d9"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a3f5f227a739ac3b832948e77dbe1c8b9cc3169b7266dbe781c6a31a3f6a3213"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/mcp-server-supabase" => "supabase-mcp-server"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/supabase-mcp-server --version")
+
+    ENV["SUPABASE_ACCESS_TOKEN"] = "test-token"
+
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    assert_match "Lists all Supabase projects for the user", pipe_output(bin/"supabase-mcp-server", json, 0)
+  end
+end
