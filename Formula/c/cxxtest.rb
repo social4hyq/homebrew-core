9c6b9b67fb676b50959aecfcd4947b2f4d73343f
hqzing <hqzing@outlook.com>
2026-05-16 20:37:39 +0000

    cxxtest 4.4 (new formula)

---
+++ Formula/c/cxxtest.rb
@@ -0,0 +1,45 @@
+class Cxxtest < Formula
+  include Language::Python::Virtualenv
+
+  desc "C++ unit testing framework similar to JUnit, CppUnit and xUnit"
+  homepage "https://github.com/CxxTest/cxxtest"
+  url "https://github.com/CxxTest/cxxtest/releases/download/4.4/cxxtest-4.4.tar.gz"
+  mirror "https://deb.debian.org/debian/pool/main/c/cxxtest/cxxtest_4.4.orig.tar.gz"
+  sha256 "1c154fef91c65dbf1cd4519af7ade70a61d85a923b6e0c0b007dc7f4895cf7d8"
+  license "LGPL-3.0-only"
+  revision 3
+
+  bottle do
+    rebuild 6
+    sha256 cellar: :any_skip_relocation, all: "aa6fedcd740d5f396b372bfae35f71164499faf2b62c1737befbcada3ea4d7f8"
+  end
+
+  depends_on "python@3.14"
+
+  def install
+    venv = virtualenv_create(libexec, "python3.14")
+    venv.pip_install_and_link buildpath/"python"
+
+    include.install "cxxtest"
+    doc.install Dir["doc/*"]
+  end
+
+  test do
+    testfile = testpath/"MyTestSuite1.hpp"
+    testfile.write <<~CPP
+      #include <cxxtest/TestSuite.h>
+
+      class MyTestSuite1 : public CxxTest::TestSuite {
+      public:
+          void testAddition(void) {
+              TS_ASSERT(1 + 1 > 1);
+              TS_ASSERT_EQUALS(1 + 1, 2);
+          }
+      };
+    CPP
+
+    system bin/"cxxtestgen", "--error-printer", "-o", testpath/"runner.cpp", testfile
+    system ENV.cxx, "-o", testpath/"runner", testpath/"runner.cpp"
+    system testpath/"runner"
+  end
+end
