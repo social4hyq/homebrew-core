aef5cb660dd16517b4fcf4b39ced2acd645931e1
hqzing <hqzing@outlook.com>
2026-05-16 10:05:53 +0000

    django-completion 6.0.5 (new formula)

---
+++ Formula/d/django-completion.rb
@@ -0,0 +1,26 @@
+class DjangoCompletion < Formula
+  desc "Bash completion for Django"
+  homepage "https://www.djangoproject.com/"
+  url "https://github.com/django/django/archive/refs/tags/6.0.5.tar.gz"
+  sha256 "eac18f294b4cda3dd88ab03ec8fed70f2ab755b7d846d9e5030685c887ecef4c"
+  license "BSD-3-Clause"
+  head "https://github.com/django/django.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e71ebfb97c43fa97b15c04fd8ccb1a76a02ccc8467b116ef07005d57f35c02be"
+  end
+
+  def install
+    bash_completion.install "extras/django_bash_completion" => "django"
+  end
+
+  test do
+    assert_match "-F _django_completion",
+      shell_output("bash -c 'source #{bash_completion}/django && complete -p django-admin'")
+  end
+end
