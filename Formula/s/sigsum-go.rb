f0841a9119eab9a99d6e28c09d522e9ab5377e17
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 16:32:56 +0800

    sigsum-go: update 0.14.0 bottle.

--- Formula/s/sigsum-go.rb
+++ Formula/s/sigsum-go.rb
@@ -6,12 +6,7 @@ class SigsumGo < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "20a4e48e4609e9c78c83dcd16ca748a151ed15490239912ff5013659dff52462"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "20a4e48e4609e9c78c83dcd16ca748a151ed15490239912ff5013659dff52462"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20a4e48e4609e9c78c83dcd16ca748a151ed15490239912ff5013659dff52462"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f473d19be0693d6a03ff19138f843f93f4d2fdcc249d47984949fa776a074e25"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "232269807e1fc5b04a19886da2b183c50f8ffeba85d64df951f34a65a79fc0bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8bdd2858cb28d6b06b525a74c10a209061003f71bd49cde615f8a933c5bd1851"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2789641637bef2e76d00d4ab78d4d56105e7c6141361a31fceb5f18b22fb9438"
   end
 
   depends_on "go" => :build
