e02d6ac9c33321894036f69aba9c228ff883afc9
hqzing <hqzing@outlook.com>
2026-05-16 16:36:28 +0000

    ptpython 3.0.32 (new formula)

---
+++ Formula/p/ptpython.rb
@@ -0,0 +1,56 @@
+class Ptpython < Formula
+  include Language::Python::Virtualenv
+
+  desc "Advanced Python REPL"
+  homepage "https://github.com/prompt-toolkit/ptpython"
+  url "https://files.pythonhosted.org/packages/b6/8c/7e904ceeb512b4530c7ca1d918d3565d694a1fa7df337cdfc36a16347d68/ptpython-3.0.32.tar.gz"
+  sha256 "11651778236de95c582b42737294e50a66ba4a21fa01c0090ea70815af478fe0"
+  license "BSD-3-Clause"
+  revision 1
+  head "https://github.com/prompt-toolkit/ptpython.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ad1dcf5f065450ae165fdaccb42449caf5203e884253d70b4b4dcaf1cd5b68ac"
+  end
+
+  depends_on "python@3.14"
+
+  resource "appdirs" do
+    url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
+    sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
+  end
+
+  resource "jedi" do
+    url "https://files.pythonhosted.org/packages/72/3a/79a912fbd4d8dd6fbb02bf69afd3bb72cf0c729bb3063c6f4498603db17a/jedi-0.19.2.tar.gz"
+    sha256 "4770dc3de41bde3966b02eb84fbcf557fb33cce26ad23da12c742fb50ecb11f0"
+  end
+
+  resource "parso" do
+    url "https://files.pythonhosted.org/packages/81/76/a1e769043c0c0c9fe391b702539d594731a4362334cdf4dc25d0c09761e7/parso-0.8.6.tar.gz"
+    sha256 "2b9a0332696df97d454fa67b81618fd69c35a7b90327cbe6ba5c92d2c68a7bfd"
+  end
+
+  resource "prompt-toolkit" do
+    url "https://files.pythonhosted.org/packages/a1/96/06e01a7b38dce6fe1db213e061a4602dd6032a8a97ef6c1a862537732421/prompt_toolkit-3.0.52.tar.gz"
+    sha256 "28cde192929c8e7321de85de1ddbe736f1375148b02f2e17edd840042b1be855"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/35/a2/8e3becb46433538a38726c948d3399905a4c7cabd0df578ede5dc51f0ec2/wcwidth-0.6.0.tar.gz"
+    sha256 "cdc4e4262d6ef9a1a57e018384cbeb1208d8abbc64176027e2c2455c81313159"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"test.py").write "print(2+2)\n"
+    assert_equal "4", shell_output("#{bin}/ptpython test.py").chomp
+  end
+end
