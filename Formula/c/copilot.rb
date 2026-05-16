27b903cdaf05c47f4de390349c0af728889dab2f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 13:14:14 +0800

    copilot: update 1.34.1 bottle.

--- Formula/c/copilot.rb
+++ Formula/c/copilot.rb
@@ -12,14 +12,7 @@ class Copilot < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6956e0e2fd50ea21db1b36dc6a23a1577c302f659b4b8d1d48d08b0c74df87a6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13faf4886169d8f186deaf7635b8c970f23155a7cc871f1b002ff7cd6c4c3cad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "612ed50072ab9efa44768a26c50204ae34f6239eaebf4943420f3974346e5099"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6386f436762921c82276a71a96f3cb039c19feb6686cd1f0425ead33221ad52f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f6bd3082d39efbe86dda980e542f93186871646ec9ac23b951f7683d3422f6c9"
-    sha256 cellar: :any_skip_relocation, ventura:       "1ec11fa87d97cc7efe7093979126094c6f67ca0f4834df821d60431a2d92e91a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c72c7b0fdb9e03d19111e4eb6b77e4146779cd18a42a2820666fd5f73e6c4d0e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fbed548a000b6f038d80263542e7f794a6ceaae95ecfd3c2cc321bcf9d89d5bf"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e2e804bcff76711cd3c893741ea52f0bd20e042e5d667738e5d36e181809e282"
   end
 
   depends_on "go" => :build
