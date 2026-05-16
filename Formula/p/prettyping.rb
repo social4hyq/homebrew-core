2472f6464f6f5903baffd8c6987a7e64161a527a
hqzing <hqzing@outlook.com>
2026-05-16 17:37:43 +0000

    prettyping 1.1.0 (new formula)

---
+++ Formula/p/prettyping.rb
@@ -0,0 +1,19 @@
+class Prettyping < Formula
+  desc "Wrapper to colorize and simplify ping's output"
+  homepage "https://denilsonsa.github.io/prettyping/"
+  url "https://github.com/denilsonsa/prettyping/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "e8484492e3c704b2460a00b0e417a07ad7112b5f4ad9a211931ee031fe64b4b6"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "34b060c257cb158f76339e83dc74f498fce30be734f9d007b203f7f95699d5e2"
+  end
+
+  def install
+    bin.install "prettyping"
+  end
+
+  test do
+    system bin/"prettyping", "-c", "3", "127.0.0.1"
+  end
+end
