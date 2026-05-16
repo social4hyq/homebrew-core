d5efb90c8bc154c032c0206e9d7a1cf0806198eb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 19:21:58 +0800

    jinx: update 1.3.10 bottle.

--- Formula/j/jinx.rb
+++ Formula/j/jinx.rb
@@ -7,18 +7,7 @@ class Jinx < Formula
   head "https://github.com/JamesBoer/Jinx.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "1b79fa4e492e378b7efebc2117fc5842ce1dcd57180e332a2eb6ce00b1915c16"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "640fd3707ccc9262904729a97da5789a22e9cb46c658b7d20aecbb323749a1a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "24313e091b9222029e7d5e6e4aea87ef70e20facef9a6b82e0a0d4abfffcc511"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "76c5986afd50a5bbe9ee092ab25dfe8633ae63e1895b5ee90107f508a6297673"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "362834274bbb963b081203c47ece5ccbae44ab6959177d293f2d6af86b2063bf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2cc147cfcb7b0769a0570f011806266e8762eea26cc751fc49df4749f5e73de8"
-    sha256 cellar: :any_skip_relocation, sonoma:         "3353d9402dd94c01eaaa937bcb58590824f144027f54fadacdaa4584b6f03979"
-    sha256 cellar: :any_skip_relocation, ventura:        "6f9d56b84c16029fbec98ffe69d6fcf84e83effbeabde98cd2c3a553cbb366ff"
-    sha256 cellar: :any_skip_relocation, monterey:       "cafc3ac794c99f4e0a74380927681bd35eb465eb22d940a99cee23aae2f8ea61"
-    sha256 cellar: :any_skip_relocation, big_sur:        "eb75b3db57ce5a1349190419c5e1f7e880c74c0d0f8ae5485a708739358592ff"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "56ecdc57ff29b8f45c0539ebbcef8608581644a619014cb6058c8d01b8aa3422"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca7c15cf146241f441cf37fd695dbae391da90bdb5cf6b3fcf7d3c1f05be2270"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "db77fe0233126c703c68639a6a50f7c3e03badd26ec7c8e374a4c81c85cdfa77"
   end
 
   depends_on "cmake" => :build
