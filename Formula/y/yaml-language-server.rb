eda49cdc455de9a6e48d81bbcae64d3e8df48f87
hqzing <hqzing@outlook.com>
2026-05-16 10:09:17 +0000

    yaml-language-server 1.23.0 (new formula)

---
+++ Formula/y/yaml-language-server.rb
@@ -0,0 +1,40 @@
+class YamlLanguageServer < Formula
+  desc "Language Server for Yaml Files"
+  homepage "https://github.com/redhat-developer/yaml-language-server"
+  url "https://registry.npmjs.org/yaml-language-server/-/yaml-language-server-1.23.0.tgz"
+  sha256 "f0b6ec7f169507e9fa31667d0440446d23901b48005d859d99e3de5adef216cf"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "1740b6bae51a6357477c5cc4fc96a2bbd126c9efc8abcf382041a6c906324752"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3(bin/"yaml-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
