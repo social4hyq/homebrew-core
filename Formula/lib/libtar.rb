dbf91f8453f6fc716c9aae7cfa580c97cad050af
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 23:07:46 +0800

    libtar: update 1.2.20 bottle.

--- Formula/lib/libtar.rb
+++ Formula/lib/libtar.rb
@@ -7,13 +7,7 @@ class Libtar < Formula
   license "NCSA"
 
   bottle do
-    rebuild 3
-    sha256 cellar: :any,                 arm64_tahoe:   "23ceb454b2f6611082364f55c92c573111df507f73b3d19ca69555bc4ebac48a"
-    sha256 cellar: :any,                 arm64_sequoia: "d4e8e07ef8f56b82f2770a4b7dbbe05b1e82fd2c79bd96e7fd951a9122ed960a"
-    sha256 cellar: :any,                 arm64_sonoma:  "98e649e4ddedb2869148b221f35a5a2cebe6f03482ae073518454d34e2779421"
-    sha256 cellar: :any,                 sonoma:        "6f63b617e34b6a198feafb7330800d8a47960cf2bdcd412cc1bcdad98fb07108"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3cfe7b8ddcc43ac4ff3271b9bcc59589799c70700ad2a231f8ee645213aa8e0c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d636db798faefdeba582d4b33c6ee41f659a46c7e99cd9b5aab9eb4af1b10b37"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "78adac27671f5cf1eee666f64a825d52de7fd4f2ca7063f7a9950f2158e12202"
   end
 
   depends_on "autoconf" => :build
