b833c80d4861fa66c0af3e1903537ecdf48b3265
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 20:28:00 +0800

    secp256k1: update 0.7.1 bottle.

--- Formula/s/secp256k1.rb
+++ Formula/s/secp256k1.rb
@@ -6,12 +6,7 @@ class Secp256k1 < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "da2fa73c7222a2b0ecd611814895c92fe175fd59b4a2e60fb9886993c549b842"
-    sha256 cellar: :any,                 arm64_sequoia: "07ae325f7e07a797ff61e73a6b785ef5066855459990f568604ae387d5154175"
-    sha256 cellar: :any,                 arm64_sonoma:  "329d9345dc3d88b81a2206b921cfd3b0f55241d878e639ce4f2e49b5f1ba5506"
-    sha256 cellar: :any,                 sonoma:        "c6a1dfbc6bfaec13225ebc3a979b131dcce74b475afebc60bb8993dd59087112"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "684efba291dd63c1bae72882e46db5aa6a94b052ea45db027bc776d964544fe1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f11de3494970a0d0e9b4206ea0a8d5eea0fc119efcd3927e309fed714944c19c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a35772bdef8410c1ce4d7546026368958c265bf02b7cfa1a4dad8cd919247990"
   end
 
   depends_on "autoconf" => [:build]
