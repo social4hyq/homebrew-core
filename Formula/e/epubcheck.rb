a672503163e5d6669eca9ce591fbfb3c363ede42
hqzing <hqzing@outlook.com>
2026-05-16 04:39:00 +0000

    epubcheck 5.3.0 (new formula)

---
+++ Formula/e/epubcheck.rb
@@ -0,0 +1,23 @@
+class Epubcheck < Formula
+  desc "Validate EPUB files, version 2.0 and later"
+  homepage "https://github.com/w3c/epubcheck"
+  url "https://github.com/w3c/epubcheck/releases/download/v5.3.0/epubcheck-5.3.0.zip"
+  sha256 "6c07e68584b2e2ce2f89fe06e1246dfead3eb36b46b340e7d93524f29dcff6c5"
+  license "BSD-3-Clause"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0299f18ea40b9b12c948221009f646286bb9cfad887ddd4fc10ccf578ae5262e"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    jarname = "epubcheck.jar"
+    libexec.install jarname, "lib"
+    bin.write_jar_script libexec/jarname, "epubcheck"
+  end
+
+  test do
+    assert_match "No errors or warnings detected", shell_output("#{bin}/epubcheck #{test_fixtures("test.epub")}")
+  end
+end
