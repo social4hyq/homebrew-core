f20707e134003782037a776369d61f6ceb48d380
hqzing <hqzing@outlook.com>
2026-05-16 03:17:12 +0000

    sf 2.134.6 (new formula)

---
+++ Formula/s/sf.rb
@@ -0,0 +1,34 @@
+class Sf < Formula
+  desc "Command-line toolkit for Salesforce development"
+  homepage "https://developer.salesforce.com/tools/salesforcecli"
+  url "https://registry.npmjs.org/@salesforce/cli/-/cli-2.134.6.tgz"
+  sha256 "eb9a9a3bea6cc4226f335efff40726472ca1e96912c491a58206e1f7d5f7bb41"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://registry.npmjs.org/@salesforce/cli/latest"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ac3c653e10c3eb0d96d999ec47c467b51b104d890102193e935218d98c254180"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    system bin/"sf", "project", "generate", "-n", "projectname", "-t", "empty"
+    assert_path_exists testpath/"projectname"
+    assert_path_exists testpath/"projectname/config/project-scratch-def.json"
+    assert_path_exists testpath/"projectname/README.md"
+    assert_path_exists testpath/"projectname/sfdx-project.json"
+    assert_path_exists testpath/"projectname/.forceignore"
+  end
+end
