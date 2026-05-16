1645a693ab336641d4de188dab9c453b13f883db
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 19:15:49 +0800

    at-spi2-core: update 2.60.3 bottle.

--- Formula/a/at-spi2-core.rb
+++ Formula/a/at-spi2-core.rb
@@ -7,12 +7,7 @@ class AtSpi2Core < Formula
   compatibility_version 1
 
   bottle do
-    sha256 arm64_tahoe:   "b9be04f22a465846f51415d3698cc182d9cde683aeb764799ea3c8a47b71b347"
-    sha256 arm64_sequoia: "a28331162db0e7e90ef2547aa6636d89bf8303722697d8c62637975f2a302174"
-    sha256 arm64_sonoma:  "32fe759b2132992b62f1dc4255487d7dd6341ac521807ab879f0a9aea6557b0f"
-    sha256 sonoma:        "9b7af1c29c8b1d796e0165e16b7005d1ccffdbf6c81df931102e2fd2e5904401"
-    sha256 arm64_linux:   "f68c7930a954c03e3aee73e1b9507409763baf109e360f36b1ca047d9c45f511"
-    sha256 x86_64_linux:  "47ac126cb94a6ce42684374c2cf85bcce70c6bbd0841c4954b4803ad808da79e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9579c4a022781cf17e25a07d4fa89138f648199f5875d1517bb879b4d32b32cf"
   end
 
   depends_on "gettext" => :build
