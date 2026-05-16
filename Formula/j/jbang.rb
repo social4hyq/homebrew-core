eedd74bbe87d3aea16b4c008cb1314d36ea832ba
hqzing <hqzing@outlook.com>
2026-05-16 18:42:20 +0000

    jbang 0.138.0 (new formula)

---
+++ Formula/j/jbang.rb
@@ -0,0 +1,30 @@
+class Jbang < Formula
+  desc "Tool to create, edit and run self-contained source-only Java programs"
+  homepage "https://jbang.dev/"
+  url "https://github.com/jbangdev/jbang/releases/download/v0.138.0/jbang-0.138.0.zip"
+  sha256 "3c9fb9ac5823ae5ab9d136dec08b896e2a0f6f6b313689b21f43c511b7c34b85"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "26175bc715435de7cb9300e9e5c30c4b9ebc0e72f9153957e6379d71fe7c941d"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    inreplace "#{libexec}/bin/jbang", /^abs_jbang_dir=.*/, "abs_jbang_dir=#{libexec}/bin"
+    bin.install_symlink libexec/"bin/jbang"
+  end
+
+  test do
+    system bin/"jbang", "init", "--template=cli", testpath/"hello.java"
+    assert_match "hello made with jbang", (testpath/"hello.java").read
+    assert_match version.to_s, shell_output("#{bin}/jbang --version 2>&1")
+  end
+end
