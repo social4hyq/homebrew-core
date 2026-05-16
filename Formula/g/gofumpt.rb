54f63a693f6b3ae04fe5fd548d8945e1dc53e8d2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 20:17:14 +0800

    gofumpt: update 0.10.0_1 bottle.

--- Formula/g/gofumpt.rb
+++ Formula/g/gofumpt.rb
@@ -8,12 +8,7 @@ class Gofumpt < Formula
   head "https://github.com/mvdan/gofumpt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e850602bcc582dd3614973dd3b21ab9ded99480fc731c3e9f545dc47abb2c699"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e850602bcc582dd3614973dd3b21ab9ded99480fc731c3e9f545dc47abb2c699"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e850602bcc582dd3614973dd3b21ab9ded99480fc731c3e9f545dc47abb2c699"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c17286157c80bddfe79f1773365b13f46e64bcda6decdb2d809b5f22140eadb5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a70db059495d952b95cf72e49c5d5976f1a213d6dcab0380bd540910ce0c80f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fac66368cb5d80b7a4b807e9204b1e56e5e2d5668e411668b4a2ae6c920f67cf"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5b3b30580ce0477f0ab71b5d07c00e87b2750ac1b1c4f5a7378f12e13e8262d6"
   end
 
   depends_on "go"
