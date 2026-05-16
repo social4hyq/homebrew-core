2739a14932e6375b90c28fe2897991120c068ce7
hqzing <hqzing@outlook.com>
2026-05-16 03:25:13 +0000

    jtbl 1.6.0 (new formula)

---
+++ Formula/j/jtbl.rb
@@ -0,0 +1,41 @@
+class Jtbl < Formula
+  include Language::Python::Virtualenv
+
+  desc "Convert JSON and JSON Lines to terminal, CSV, HTTP, and markdown tables"
+  homepage "https://github.com/kellyjonbrazil/jtbl"
+  url "https://files.pythonhosted.org/packages/9e/7c/b21f3383ca611b56dbc281081cca73a24274c3f39654e7fe196d73a67af6/jtbl-1.6.0.tar.gz"
+  sha256 "7de0cb08ebb2b3a0658229a8edd4204c6944cbd9e3e04724a9ea235a61c115a5"
+  license "MIT"
+
+  bottle do
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, all: "72309a9ade71950be9675e9b57c336177ffd5a84b058634d0b4bdcb6466b7e9a"
+  end
+
+  depends_on "python@3.14"
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/ec/fe/802052aecb21e3797b8f7902564ab6ea0d60ff8ca23952079064155d1ae1/tabulate-0.9.0.tar.gz"
+    sha256 "0095b12bf5966de529c0feb1fa08671671b3368eec77d7ef7ab114be2c068b3c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    man1.install "man/jtbl.1"
+
+    # Build an `:all` bottle
+    packages = libexec/Language::Python.site_packages("python3")
+    inreplace packages/"jtbl-#{version}.dist-info/METADATA", "/usr/local/Cellar", "#{HOMEBREW_PREFIX}/Cellar"
+  end
+
+  test do
+    assert_match <<~EOS, pipe_output(bin/"jtbl", "[{\"a\":1,\"b\":1},{\"a\":2,\"b\":2}]")
+        a    b
+      ---  ---
+        1    1
+        2    2
+    EOS
+
+    assert_match version.to_s, shell_output("#{bin}/jtbl --version 2>&1", 1)
+  end
+end
