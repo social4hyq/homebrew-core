2036b1bd2df83b9fbb75c84495f4963bab8f672f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 12:46:50 +0800

    wal-g: update 3.0.8 bottle.

--- Formula/w/wal-g.rb
+++ Formula/w/wal-g.rb
@@ -6,12 +6,7 @@ class WalG < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "b51f4142cdcea4c4a43cc6f8f1773f69523cbe72b5283e6e30d8a3a9a6835c21"
-    sha256 cellar: :any,                 arm64_sequoia: "d8df29e624956a3d1ea567bb5b1f11d6a331c2bda4c1ca9521043b1860a4a786"
-    sha256 cellar: :any,                 arm64_sonoma:  "1784ead186775c161c507026e4166b6df1aeaa17f0122ebefc81505b04eb5cc6"
-    sha256 cellar: :any,                 sonoma:        "ce4e3e6f90e8ce94b9649f623020565b95836cde1b7d6a8e63c32626b5456262"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f4038b929fb7ab597d0c1ecf1eb828097e4ed7b35d5f2d4d3bc671fed33e9da7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35fd271cdc3f2c241e2ba68b7e8c44f1ce49f54293e27c9fc0ac139689df45cd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "851c6c7983f865780c507f013fcbfc4da2993fe2537b7441b76f2cd9eccbeb4b"
   end
 
   depends_on "go" => :build
