ebcea5c68f6f55499db1736bbe6d7faae098254a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 13:25:10 +0800

    packetq: update 1.7.3 bottle.

--- Formula/p/packetq.rb
+++ Formula/p/packetq.rb
@@ -11,13 +11,7 @@ class Packetq < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "29f2ffe806704b6ccf4a3b36ed2a03229aec5ab347638c02bc048b1869c269b8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0616b69581ac77fc31dc4a4d4a3d9258421ee38538997e38eca9a66a62d10339"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "175c53c989f9b926e7ddcf382517cec08f015ab1635938659ef8045f69e37e0d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "26f2629870ea59d9bc6673411af50187b30cd4cee701fadf1557e6c949b43749"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1d955362f371fe590e3a44a3fb66eef99bf88782410e6c7ee4c3b9c3b62c5d68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5afff996605124f10306b5a479c3b619b35f1719b262a939f4a02d3528a29800"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d8bd43b3d8590c7eebdb00a70e684fb8436a515d43a1b05577d64e383d29b97c"
   end
 
   on_linux do
