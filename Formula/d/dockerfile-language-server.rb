11b12ed802bb1ca75db6f18d95bf61b5283daa61
hqzing <hqzing@outlook.com>
2026-05-16 10:50:01 +0000

    dockerfile-language-server 0.15.0 (new formula)

---
+++ Formula/d/dockerfile-language-server.rb
@@ -0,0 +1,40 @@
+class DockerfileLanguageServer < Formula
+  desc "Language server for Dockerfiles powered by Node, TypeScript, and VSCode"
+  homepage "https://github.com/rcjsuen/dockerfile-language-server"
+  url "https://registry.npmjs.org/dockerfile-language-server-nodejs/-/dockerfile-language-server-nodejs-0.15.0.tgz"
+  sha256 "32ee93b98f43d8d60273019b7ed12a263593ba3a2308442ca7449d49676f480d"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7e63595a7c55824a185f84aeef01ba0c0ad797c217fcbabd23a0990614ff7795"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/docker-langserver", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
