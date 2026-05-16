3e6aebb980a244e1dd7afd5f2203f5550446be7e
hqzing <hqzing@outlook.com>
2026-05-16 09:46:18 +0000

    ktlint 1.8.0 (new formula)

---
+++ Formula/k/ktlint.rb
@@ -0,0 +1,32 @@
+class Ktlint < Formula
+  desc "Anti-bikeshedding Kotlin linter with built-in formatter"
+  homepage "https://ktlint.github.io/"
+  url "https://github.com/pinterest/ktlint/releases/download/1.8.0/ktlint-1.8.0.zip"
+  sha256 "3722801dd119b96a2fbeda0b9d66f173994f249998c87bcf2274b51977aa8f77"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "6ceb6042ff9dd6daab3eb7bb071ecca7c5227ab327a11e4a7e8bddcea27f029f"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "bin/ktlint"
+    (libexec/"ktlint").chmod 0755
+    (bin/"ktlint").write_env_script libexec/"ktlint", Language::Java.java_home_env
+  end
+
+  test do
+    (testpath/"Main.kt").write <<~KOTLIN
+      fun main( )
+    KOTLIN
+
+    (testpath/"Out.kt").write <<~KOTLIN
+      fun main()
+    KOTLIN
+
+    system bin/"ktlint", "-F", "Main.kt"
+    assert_equal shell_output("cat Main.kt"), shell_output("cat Out.kt")
+  end
+end
