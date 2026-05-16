de0fbf2b2ec44a3190f1566431c45bdc8e96b008
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 13:16:40 +0800

    ronn-ng: update 0.10.1_2 bottle.

--- Formula/r/ronn-ng.rb
+++ Formula/r/ronn-ng.rb
@@ -7,13 +7,7 @@ class RonnNg < Formula
   revision 2
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cde2a1921cd09603374d28ee44d9c470f0b187add7b9d9eb0b526d7dbe5a3305"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cde2a1921cd09603374d28ee44d9c470f0b187add7b9d9eb0b526d7dbe5a3305"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cde2a1921cd09603374d28ee44d9c470f0b187add7b9d9eb0b526d7dbe5a3305"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e81143c3c8d224765b7db5c39a07caa7c2d7d2c9a291064f99186a1b6a7578d5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "43cce7ff99b681bb070ad22d1b9ee17a4c2093c79921cd5211095242a05222a1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4fd9f3b447f24382a9d97e7a1aa2cfdda53eed5ad75f19b0e6d2ae6e210938e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d2864c5e7c4cb7e35aaed55d7763c78fecadbb0b17e93c924f5fa7724c3763e6"
   end
 
   depends_on "ruby"
