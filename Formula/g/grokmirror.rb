f9cf26dd8da9d5d0164b4db4c7facb9d55ae2fb4
hqzing <hqzing@outlook.com>
2026-05-16 16:01:51 +0000

    grokmirror 2.0.12 (new formula)

---
+++ Formula/g/grokmirror.rb
@@ -0,0 +1,64 @@
+class Grokmirror < Formula
+  include Language::Python::Virtualenv
+
+  desc "Framework to smartly mirror git repositories"
+  homepage "https://github.com/mricon/grokmirror"
+  url "https://files.pythonhosted.org/packages/26/91/af8831185ef4e5bef5d210039ab67abdc8c27a09a585d3963a10cf774789/grokmirror-2.0.12.tar.gz"
+  sha256 "5264b6b2030bcb48ff5610173dacaba227b77b6ed39b17fc473bed91d4eb218b"
+  license "GPL-3.0-or-later"
+  revision 6
+  head "https://github.com/mricon/grokmirror.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7721daa695a1cfd49c59cf5977c81ec9114e41180370926979c8d5b6bf998f24"
+  end
+
+  depends_on "certifi"
+  depends_on "python@3.14"
+
+  pypi_packages exclude_packages: "certifi"
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
+    sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/05/b1/efac073e0c297ecf2fb33c346989a529d4e19164f1759102dee5953ee17e/idna-3.14.tar.gz"
+    sha256 "466d810d7a2cc1022bea9b037c39728d51ae7dad40d480fc9b7d7ecf98ba8ee3"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/43/b8/7a707d60fea4c49094e40262cc0e2ca6c768cca21587e34d3f705afec47e/requests-2.34.0.tar.gz"
+    sha256 "7d62fe92f50eb82c529b0916bb445afa1531a566fc8f35ffdc64446e771b856a"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    mkdir "repos/repo" do
+      system "git", "init"
+      system "git", "config", "user.name", "BrewTestBot"
+      system "git", "config", "user.email", "BrewTestBot@test.com"
+      (testpath/"repos/repo/test").write "foo"
+      system "git", "add", "test"
+      system "git", "commit", "-m", "Initial commit"
+      system "git", "config", "--bool", "core.bare", "true"
+      mv testpath/"repos/repo/.git", testpath/"repos/repo.git"
+    end
+    rm_r(testpath/"repos/repo")
+
+    system bin/"grok-manifest", "-m", testpath/"manifest.js.gz", "-t", testpath/"repos"
+    system "gzip", "-d", testpath/"manifest.js.gz"
+    refs = Utils.safe_popen_read("git", "--git-dir", testpath/"repos/repo.git", "show-ref")
+    manifest = JSON.parse (testpath/"manifest.js").read
+    assert_equal Digest::SHA1.hexdigest(refs), manifest["/repo.git"]["fingerprint"]
+  end
+end
