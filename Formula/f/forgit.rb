31ecaf0669422f677ec6f0f0b0649da7288420ce
hqzing <hqzing@outlook.com>
2026-05-16 16:44:39 +0000

    forgit 26.05.0 (new formula)

---
+++ Formula/f/forgit.rb
@@ -0,0 +1,47 @@
+class Forgit < Formula
+  desc "Interactive git commands in the terminal"
+  homepage "https://github.com/wfxr/forgit"
+  url "https://github.com/wfxr/forgit/releases/download/26.05.0/forgit-26.05.0.tar.gz"
+  sha256 "15b5c23db79dda43c315405f90a95ad4f88eae89de00173ea84164da0702cdad"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a23875e22127316471d2ca34cb0d69fec32324f74a320746618bd057de5f54ba"
+  end
+
+  depends_on "fzf"
+
+  def install
+    bin.install "bin/git-forgit"
+    bash_completion.install "completions/git-forgit.bash" => "git-forgit"
+    zsh_completion.install "completions/_git-forgit" => "_git-forgit"
+    fish_completion.install "completions/git-forgit.fish"
+    inreplace "forgit.plugin.zsh", 'FORGIT="$FORGIT_INSTALL_DIR', "FORGIT=\"#{opt_prefix}"
+    inreplace "conf.d/forgit.plugin.fish",
+              'set -x FORGIT "$FORGIT_INSTALL_DIR/bin/git-forgit"',
+              "set -x FORGIT \"#{opt_prefix}/bin/git-forgit\""
+    pkgshare.install "conf.d/forgit.plugin.fish"
+    pkgshare.install "forgit.plugin.zsh"
+    pkgshare.install_symlink "forgit.plugin.zsh" => "forgit.plugin.sh"
+  end
+
+  def caveats
+    <<~EOS
+      A shell plugin has been installed to:
+        #{opt_pkgshare}/forgit.plugin.zsh
+        #{opt_pkgshare}/forgit.plugin.sh
+        #{opt_pkgshare}/forgit.plugin.fish
+    EOS
+  end
+
+  test do
+    system "git", "init"
+    (testpath/"foo").write "bar"
+    system "git", "forgit", "add", "foo"
+  end
+end
