9d7d3397daf4c6fb10035a3adecfb598dece5d26
hqzing <hqzing@outlook.com>
2026-05-16 21:28:01 +0000

    lynis 3.1.6 (new formula)

---
+++ Formula/l/lynis.rb
@@ -0,0 +1,39 @@
+class Lynis < Formula
+  desc "Security and system auditing tool to harden systems"
+  homepage "https://cisofy.com/lynis/"
+  url "https://github.com/CISOfy/lynis/archive/refs/tags/3.1.6.tar.gz"
+  sha256 "2a31e3e028d6b9f0e8f502402ad6752b069f9082893514c8c023962f5786b3b6"
+  license "GPL-3.0-only"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8d48f78fa1aeceaff1403302071e842a97603f36bcce6073d5cdcc20a044d177"
+  end
+
+  def install
+    inreplace "lynis" do |s|
+      s.gsub! 'tINCLUDE_TARGETS="/usr/local/include/lynis ' \
+              '/usr/local/lynis/include /usr/share/lynis/include ./include"',
+              %Q(tINCLUDE_TARGETS="#{include}")
+      s.gsub! 'tPLUGIN_TARGETS="/usr/local/lynis/plugins ' \
+              "/usr/local/share/lynis/plugins /usr/share/lynis/plugins " \
+              '/etc/lynis/plugins ./plugins"',
+              %Q(tPLUGIN_TARGETS="#{prefix}/plugins")
+      s.gsub! 'tDB_TARGETS="/usr/local/share/lynis/db /usr/local/lynis/db ' \
+              '/usr/share/lynis/db ./db"',
+              %Q(tDB_TARGETS="#{prefix}/db")
+    end
+    inreplace "include/functions" do |s|
+      s.gsub! 'tPROFILE_TARGETS="/usr/local/etc/lynis /etc/lynis ' \
+              '/usr/local/lynis ."',
+              %Q(tPROFILE_TARGETS="#{prefix}")
+    end
+
+    prefix.install "db", "include", "plugins", "default.prf"
+    bin.install "lynis"
+    man8.install "lynis.8"
+  end
+
+  test do
+    assert_match "lynis", shell_output("#{bin}/lynis --invalid 2>&1", 64)
+  end
+end
