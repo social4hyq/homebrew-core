189e5caaadb3ea11ed57479b70f7f63980ce3fdf
hqzing <hqzing@outlook.com>
2026-05-16 04:24:12 +0000

    wiremock-standalone 3.13.2 (new formula)

---
+++ Formula/w/wiremock-standalone.rb
@@ -0,0 +1,38 @@
+class WiremockStandalone < Formula
+  desc "Simulator for HTTP-based APIs"
+  homepage "https://wiremock.org/docs/running-standalone/"
+  url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/3.13.2/wiremock-standalone-3.13.2.jar"
+  sha256 "d097b19bd483c5038479b13a5c71e9faf8f2f5106584f0c120a7770ab0bdb367"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "038c521d40a77423d0e9b7c8ab50f19e05972835263eb0d00ca9a3d311c0541b"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "wiremock-standalone-#{version}.jar"
+    bin.write_jar_script libexec/"wiremock-standalone-#{version}.jar", "wiremock"
+  end
+
+  test do
+    port = free_port
+
+    wiremock = spawn "#{bin}/wiremock", "-port", port.to_s
+
+    loop do
+      Utils.popen_read("curl", "-s", "http://localhost:#{port}/__admin/", "-X", "GET")
+      break if $CHILD_STATUS.exitstatus.zero?
+    end
+
+    system "curl", "-s", "http://localhost:#{port}/__admin/shutdown", "-X", "POST"
+
+    Process.wait(wiremock)
+  end
+end
