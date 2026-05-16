c435fb4b34505b048f99c440ec0e24953b24108f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 15:10:24 +0800

    libre: update 4.8.0_1 bottle.

--- Formula/lib/libre.rb
+++ Formula/lib/libre.rb
@@ -7,12 +7,7 @@ class Libre < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "16d4d005c638f6cbb34f4481c8e4d952fe868e6633465d915265cca126a481be"
-    sha256 cellar: :any,                 arm64_sequoia: "71953001f6328485ee71e6ae9fd0ff2da5307cff2392d7e477854e08e08ecd2c"
-    sha256 cellar: :any,                 arm64_sonoma:  "55a88b72ae863c98a292da62c2ed2ed6458047b3f5f8ed2bc348fd7afb1c2b7a"
-    sha256 cellar: :any,                 sonoma:        "cd2dd2c9a91e7a974818ba0ab5e59d05b0beca3a326bdd32b6c3e0c987672684"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cc15c0506ad2a6a909acfcb00a859f880312d7af20700845bf7ec778609e9959"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ee605805e7a8bc175adcb3015ce521c371f2a6023d75dc0fbf4669e3c2e269a9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c1ace301a02389fccea6c3455989f09bc7921a0451526a55a93f1938bca06017"
   end
 
   depends_on "cmake" => :build
