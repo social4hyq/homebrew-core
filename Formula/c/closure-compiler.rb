0f331397ff03e2255c385ef8e034d1ec4832ea51
hqzing <hqzing@outlook.com>
2026-05-16 16:47:12 +0000

    closure-compiler 20260513 (new formula)

---
+++ Formula/c/closure-compiler.rb
@@ -0,0 +1,36 @@
+class ClosureCompiler < Formula
+  desc "JavaScript optimizing compiler"
+  homepage "https://developers.google.com/closure/compiler"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20260513/closure-compiler-v20260513.jar"
+  sha256 "32a56dba28f29b49f7e584d201956cb966afaa32dcc329266d63f650597a402d"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/"
+    regex(/href=.*?v?(\d{8})/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9819e63dfe809d8dc57f56c79a5c57fed412abc9dee012b2ec46dda77e98b5e9"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_jar_script libexec/"closure-compiler-v#{version}.jar", "closure-compiler"
+  end
+
+  test do
+    (testpath/"test.js").write <<~JS
+      (function(){
+        var t = true;
+        return t;
+      })();
+    JS
+    system bin/"closure-compiler",
+           "--js", testpath/"test.js",
+           "--js_output_file", testpath/"out.js"
+    assert_equal (testpath/"out.js").read.chomp, "(function(){return!0})();"
+  end
+end
