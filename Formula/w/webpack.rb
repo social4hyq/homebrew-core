4f21767102ef839acf47cbeb25f7945e5d6f26ee
hqzing <hqzing@outlook.com>
2026-05-16 04:27:39 +0000

    webpack 5.106.2 (new formula)

---
+++ Formula/w/webpack.rb
@@ -0,0 +1,56 @@
+require "json"
+
+class Webpack < Formula
+  desc "Bundler for JavaScript and friends"
+  homepage "https://webpack.js.org/"
+  url "https://registry.npmjs.org/webpack/-/webpack-5.106.2.tgz"
+  sha256 "8437b31d3948448a87c282d00621c5b2a411ab7d280b8df5cd8d5733f636e5a9"
+  license "MIT"
+  head "https://github.com/webpack/webpack.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4cf6e79ea0e53c03e22e3aac4aca08ee389ca0752902711fc855647c85dd3dd1"
+  end
+
+  depends_on "node"
+
+  resource "webpack-cli" do
+    url "https://registry.npmjs.org/webpack-cli/-/webpack-cli-7.0.2.tgz"
+    sha256 "aff76cea56dd87feb531c2fa0681c1672abd2bf0955d368986c1ad21741cbe82"
+  end
+
+  def install
+    (buildpath/"node_modules/webpack").install Dir["*"]
+    buildpath.install resource("webpack-cli")
+
+    cd buildpath/"node_modules/webpack" do
+      system "npm", "install", *std_npm_args(prefix: false), "--force"
+    end
+
+    # declare webpack as a bundledDependency of webpack-cli
+    pkg_json = JSON.parse(File.read("package.json"))
+    pkg_json["dependencies"]["webpack"] = version
+    pkg_json["bundleDependencies"] = ["webpack"]
+    File.write("package.json", JSON.pretty_generate(pkg_json))
+
+    system "npm", "install", *std_npm_args
+
+    bin.install_symlink libexec.glob("bin/*")
+    bin.install_symlink libexec/"bin/webpack-cli" => "webpack"
+  end
+
+  test do
+    (testpath/"index.js").write <<~JS
+      function component() {
+        const element = document.createElement('div');
+        element.innerHTML = 'Hello webpack';
+        return element;
+      }
+
+      document.body.appendChild(component());
+    JS
+
+    system bin/"webpack", "bundle", "--mode=production", testpath/"index.js"
+    assert_match 'const e=document.createElement("div");', (testpath/"dist/main.js").read
+  end
+end
