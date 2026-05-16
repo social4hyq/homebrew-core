e65df45d10ff48291d13b1b4a8204f1fdd08575a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 21:10:15 +0800

    tfschema: update 0.7.10 bottle.

--- Formula/t/tfschema.rb
+++ Formula/t/tfschema.rb
@@ -7,12 +7,7 @@ class Tfschema < Formula
   head "https://github.com/minamijoyo/tfschema.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3b174c50761ec68cf29cd2d1f617d7b08d038c94ab2fdf44d2ffeac851290788"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3b174c50761ec68cf29cd2d1f617d7b08d038c94ab2fdf44d2ffeac851290788"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3b174c50761ec68cf29cd2d1f617d7b08d038c94ab2fdf44d2ffeac851290788"
-    sha256 cellar: :any_skip_relocation, sonoma:        "718b9d42ac16fdd4bf8cbe77398586299b4cdd903181385325b4abedbcb2504f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "40855957a6ad1ea3b2af91adec960953ed55c06edca96494e35970a7b375d2e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d6a3969a16779afb6cc0526f1d880f65e27a266621467311f7c75c70d1484c3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bb2e6864ff05a25ea2d3daa72e15154f0c05f4852924fa5cbea9a3f3ecc484fe"
   end
 
   depends_on "go" => :build
