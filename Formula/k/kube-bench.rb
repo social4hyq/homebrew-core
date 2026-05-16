82146265e6ce836ceb0d8a7c87a26f097544f0df
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-16 21:15:31 +0800

    kube-bench: update 0.15.4 bottle.

--- Formula/k/kube-bench.rb
+++ Formula/k/kube-bench.rb
@@ -7,12 +7,7 @@ class KubeBench < Formula
   head "https://github.com/aquasecurity/kube-bench.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3e2bdcb0b559dd28002eb5ca5c6450c7337d26e3751295b38cc6e7faaf649143"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3e2bdcb0b559dd28002eb5ca5c6450c7337d26e3751295b38cc6e7faaf649143"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3e2bdcb0b559dd28002eb5ca5c6450c7337d26e3751295b38cc6e7faaf649143"
-    sha256 cellar: :any_skip_relocation, sonoma:        "04961fbdfb0b6b6a9b151d1a3698f6a2319cc533d834a7bc8bfb857d5fa4b512"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca75b851e9ae8224bae5a2ca9f3760389008a99290b7df41cac8c131a81da597"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a5e444797f3b71250785d501c89c3797c7f58a22f879306100ff19776f159aa"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f8c435e72806a83402c631151ec1800ec667a3bea27cf86e95448a48c6fcb30e"
   end
 
   depends_on "go" => :build
