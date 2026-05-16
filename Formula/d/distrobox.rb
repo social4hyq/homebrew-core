542a0116f45ebaf23d0edd666099de553ba98310
hqzing <hqzing@outlook.com>
2026-05-16 19:06:03 +0000

    distrobox 1.8.2.5 (new formula)

---
+++ Formula/d/distrobox.rb
@@ -0,0 +1,30 @@
+class Distrobox < Formula
+  desc "Use any Linux distribution inside your terminal"
+  homepage "https://distrobox.privatedns.org/"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.8.2.5.tar.gz"
+  sha256 "0c3bc4785ee3be3b89f93abb7cc0a9f60e56989e81319af140a4b60403b18f80"
+  license "GPL-3.0-only"
+  head "https://github.com/89luca89/distrobox.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7c51ad4da1192d6346f7099ba55a1c3e18e0c67853833568c86d9bd200f3be6f"
+  end
+
+  depends_on :linux
+
+  def install
+    system "./install", "--prefix", prefix
+  end
+
+  def caveats
+    <<~EOS
+      Distrobox requires one of podman or docker. Do
+        brew install podman
+      or consult the documentation for details.
+    EOS
+  end
+
+  test do
+    system bin/"distrobox-create", "--dry-run"
+  end
+end
