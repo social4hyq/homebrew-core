e1229b649c281ad03d3f3761655191c0f2135966
hqzing <hqzing@outlook.com>
2026-05-16 03:17:39 +0000

    usb.ids 2025.12.13 (new formula)

---
+++ Formula/u/usb.ids.rb
@@ -0,0 +1,25 @@
+class UsbIds < Formula
+  desc "Repository of vendor, device, subsystem and device class IDs used in USB devices"
+  homepage "http://www.linux-usb.org/usb-ids.html"
+  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2025.12.13.orig.tar.xz"
+  sha256 "492edc99d85a25dca388930c304e85543ef5913b63b5f2fdfa2c213d77709784"
+  license any_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
+  compatibility_version 1
+
+  livecheck do
+    url "https://deb.debian.org/debian/pool/main/u/usb.ids/"
+    regex(/href=.*?usb\.ids[._-]v?(\d+(?:\.\d+)+)\.orig\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7b58097d679f8c948a5cada0a504a346d2062b24ba66b74d0eae71133e641c95"
+  end
+
+  def install
+    (share/"misc").install "usb.ids"
+  end
+
+  test do
+    assert_match "Version: #{version}", File.read(share/"misc/usb.ids", encoding: "ISO-8859-1")
+  end
+end
