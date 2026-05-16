c2a931d0c57d3e3342f4cf9195117e56f499381d
hqzing <hqzing@outlook.com>
2026-05-16 19:57:48 +0000

    python-build 1.5.0 (new formula)

---
+++ Formula/p/python-build.rb
@@ -0,0 +1,38 @@
+class PythonBuild < Formula
+  include Language::Python::Virtualenv
+
+  desc "Simple, correct PEP 517 build frontend"
+  homepage "https://github.com/pypa/build"
+  url "https://files.pythonhosted.org/packages/78/e0/df5e171f685f82f37b12e1f208064e24244911079d7b767447d1af7e0d70/build-1.5.0.tar.gz"
+  sha256 "302c22c3ba2a0fd5f3911918651341ebb3896176cbdec15bd421f80b1afc7647"
+  license "MIT"
+  head "https://github.com/pypa/build.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "688253f58de7668d9423fe472d8702854cfe10d1d5a1dac02c8aa95315289df1"
+  end
+
+  depends_on "python@3.14"
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/d7/f1/e7a6dd94a8d4a5626c03e4e99c87f241ba9e350cd9e6d75123f992427270/packaging-26.2.tar.gz"
+    sha256 "ff452ff5a3e828ce110190feff1178bb1f2ea2281fa2075aadb987c2fb221661"
+  end
+
+  resource "pyproject-hooks" do
+    url "https://files.pythonhosted.org/packages/e7/82/28175b2414effca1cdac8dc99f76d660e7a4fb0ceefa4b4ab8f5f6742925/pyproject_hooks-1.2.0.tar.gz"
+    sha256 "1e859bd5c40fae9448642dd871adf459e5e2084186e8d2c2a79a824c970da1f8"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    stable.stage do
+      system bin/"pyproject-build"
+      assert_path_exists Pathname.pwd/"dist/build-#{stable.version}.tar.gz"
+      assert_path_exists Pathname.pwd/"dist/build-#{stable.version}-py3-none-any.whl"
+    end
+  end
+end
