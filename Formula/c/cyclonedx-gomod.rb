c178fd6abbfd7f5698ac76a32f76f23c1c0eb71f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 16:33:38 +0800

    cyclonedx-gomod: update 1.10.0 bottle.

--- Formula/c/cyclonedx-gomod.rb
+++ Formula/c/cyclonedx-gomod.rb
@@ -7,12 +7,7 @@ class CyclonedxGomod < Formula
   head "https://github.com/CycloneDX/cyclonedx-gomod.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dd7b3ce89ec8361b5c32558c62582a62f21f3574fcfd4d7f39ddfeffaa89ceef"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dd7b3ce89ec8361b5c32558c62582a62f21f3574fcfd4d7f39ddfeffaa89ceef"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd7b3ce89ec8361b5c32558c62582a62f21f3574fcfd4d7f39ddfeffaa89ceef"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8c59b92233c8beba9f3a9ac8f3ee90d86239eef28e3db002f0c30566d57787c0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d20cbef64f5f8167a18d0edd98d5f9780df07032683c1ddf0c6882791f1c367b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0264d8f7838c67048cfebe524bad1caf684ccde316a8140300d15c01d41d696d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "277c422f3bb0503fd46cab431ef36dcf739c237c7eda7ccec7ed3481863a9294"
   end
 
   depends_on "go" => [:build, :test]
