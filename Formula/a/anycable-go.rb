88341f2e1fbf942c36914be2d887f6447d2078e6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 12:58:15 +0800

    anycable-go: update 1.6.13 bottle.

--- Formula/a/anycable-go.rb
+++ Formula/a/anycable-go.rb
@@ -12,12 +12,7 @@ class AnycableGo < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eefb3596c0ca76dab65b8f1cc4b95038abcdc701ac819f9c5dca4d6f58630c4c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eefb3596c0ca76dab65b8f1cc4b95038abcdc701ac819f9c5dca4d6f58630c4c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eefb3596c0ca76dab65b8f1cc4b95038abcdc701ac819f9c5dca4d6f58630c4c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e04ed9c7d7a852680f63f4ef7014caf29dd2043124bfa408ee748fc3fc00a35d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "02d1500c7ff019af19d2c4483320daa7bd39e2e80f67e4849d22c3f0bb1ef5b8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f4893f0a7ecc46a1a031447cc662f5e494dcba085273d489a8369b70259563a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "323e6ebecd24236ba891e6650702d508e0b1e5b13e04080f536b0c3608b4c052"
   end
 
   depends_on "go" => :build
