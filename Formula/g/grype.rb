d1bc42286169a7d6c7714606029ae1fe8df1c10b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:36:00 +0800

    grype: update 0.112.0 bottle.

--- Formula/g/grype.rb
+++ Formula/g/grype.rb
@@ -7,12 +7,7 @@ class Grype < Formula
   head "https://github.com/anchore/grype.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "513e05ea46524b3973b801d7ff3b0a364e9212d62565b3fb44f0fa4ec66b9f87"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0cc0b4bce7279b02217420e6da6fc525f9256c8dd64de57b19d740c18025c6b7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0801736cec0f4fe04c213ce9e7a0b588bc48014edaebc6b245c200e8046bdc0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "67a7f4dee63d1ca0833534d26f5606ba51707989ca3b843c376fb6e8e8f21846"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c111c10c8a8d21699fba83b03fd34e7ec2c103985a15b2b284224f0074eb7fbc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32afcb923562e4b728db60bcf89724f2060b9b93663e57d55764f85cb0324b8b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bd2e3229a50684bbd67aa708e7327c9cf1d4a66d72ae6172d9df04b9ba1103bb"
   end
 
   depends_on "go" => :build
