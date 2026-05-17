b71ce976f2506a94887aa6c2e67abd77db24ffc0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:55:50 +0800

    revive: update 1.15.0 bottle.

--- Formula/r/revive.rb
+++ Formula/r/revive.rb
@@ -8,12 +8,7 @@ class Revive < Formula
   head "https://github.com/mgechev/revive.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4337a14af0e46666de7a19a6c2466a23228d4e62670cb52a7d89c62f9815ca2c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4337a14af0e46666de7a19a6c2466a23228d4e62670cb52a7d89c62f9815ca2c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4337a14af0e46666de7a19a6c2466a23228d4e62670cb52a7d89c62f9815ca2c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6fb205ea00722af30605e0b0fa2c1fb0a5dc5b5a5374ff34296f3aa6b5711c30"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba3a985bd2e8ff34933a604a32dbd64fe29d70abc64c1fb3fe7f46894371c213"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e579ed73be078aecb76280cefb87bb80c966a98f255d499cbfb05c874754730f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "709be8d4abbffc50a46cadfc2b7999ea4f171031223724620075d7fc331665b9"
   end
 
   depends_on "go" => [:build, :test]
