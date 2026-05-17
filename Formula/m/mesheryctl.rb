6f71341d3937181c3f39d823541cae98f0674156
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 23:51:15 +0800

    mesheryctl: update 1.0.20 bottle.

--- Formula/m/mesheryctl.rb
+++ Formula/m/mesheryctl.rb
@@ -10,12 +10,7 @@ class Mesheryctl < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ae622be1e7fcc33aa2d09dadf181ab92e2e54456acf34d7fa6a57ce929a9f154"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32ff8500b626933cb523567425fe2bfbed69fa558cafeba3d15b92e5fc7d861e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "857dda70c0805c94a5f59497b774181eca3480aeff96926c7342c456e6f1e998"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9a7d49d67305d40dccc3a5bedb37064e67ab4ce376d36a2f22db5edf08316c92"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "58c75d630917a389639ce89da9770c61172d8096699231e3f177302436ea92d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a5c4c5f1d9447e71fa804323e4ab02b5eec40ba12b3e7c5f900ee17d4187b4bd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7fad1ddeffc0a66bc434a4968ec843c7d50cf776879916a8711894eabcb909c0"
   end
 
   depends_on "go" => :build
