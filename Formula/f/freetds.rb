c520117bb1f4e4fd1b5ac54243c427108a8fdf16
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 17:47:58 +0800

    freetds: update 1.5.18 bottle.

--- Formula/f/freetds.rb
+++ Formula/f/freetds.rb
@@ -12,12 +12,7 @@ class Freetds < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "04255031703dd87bc594f1bba35bd507eb75ab0a7df0b1fa85fc64d6a8798791"
-    sha256 arm64_sequoia: "4227980af018668e860120a319066f8c701823156821d806de69d089198ed2c3"
-    sha256 arm64_sonoma:  "955417a5ee2d8fc5259ae59549f78bf9bd7f81b2f188caf4545740137236ffee"
-    sha256 sonoma:        "6fcee5bc09ff8ffec162a2d6ba547fa7f5930381303e6dfacd8b0f2534412796"
-    sha256 arm64_linux:   "7296b8bd471f270f419e4ce39d713b2a7961bca5eba6df0777f56e4bbc869456"
-    sha256 x86_64_linux:  "79349cab9e40ae1e20506398940d6765caa81602c9a57f7cb7874d9e55b288a8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d5ba911e6431591432f40b8a912fd5c62ac6206a4bba186b7550d2fa0a02e58e"
   end
 
   head do
