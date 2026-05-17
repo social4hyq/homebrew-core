e83cb2b033f57343d1c8ec47e4b1bac42a75990f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 01:34:11 +0800

    recoverpy: update 2.3.0_1 bottle.

--- Formula/r/recoverpy.rb
+++ Formula/r/recoverpy.rb
@@ -10,7 +10,7 @@ class Recoverpy < Formula
   head "https://github.com/PabloLec/recoverpy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0523109c1179d7d4260ea6471464388ea95dbb13554568e8985116629abf4ef9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d7b172ca58b149b764409f74e2d9c150af399a37797303210f5b12b189f7dcbc"
   end
 
   depends_on :linux
