ce4ea05438e1bc13d552557e1214824431487856
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 23:43:22 +0800

    node@24: update 24.15.0_1 bottle.

--- Formula/n/node@24.rb
+++ Formula/n/node@24.rb
@@ -13,7 +13,7 @@ class NodeAT24 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "093caee610c60e65fbf7e90ef984a028ed3b4a8cc2d5e6d98725f6f14bea2bfe"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c1cbcc646ee17861ae1c4570b5595ae7e9ed40a218c6f002abbfb8f6d520027b"
   end
 
   keg_only :versioned_formula
