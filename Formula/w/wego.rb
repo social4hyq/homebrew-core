cacd199f7237063a944e0502ebd1fb7c3730b0f3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:27:23 +0800

    wego: update 2.4 bottle.

--- Formula/w/wego.rb
+++ Formula/w/wego.rb
@@ -7,12 +7,7 @@ class Wego < Formula
   head "https://github.com/schachmat/wego.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "591831e4debaf931b8a40abaa611fc0b694e269a008627a1e672b5c81e31a9c8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "591831e4debaf931b8a40abaa611fc0b694e269a008627a1e672b5c81e31a9c8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "591831e4debaf931b8a40abaa611fc0b694e269a008627a1e672b5c81e31a9c8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "688840f5a042c1c4aae5329e9fefd5c11f294bb2eae10fb90696e520113a92ef"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ae7cca0c98758eac802ac7a5d0123df9b5c9fc3df187057cb6d96441aa83b003"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c7c8c74038135b51fd1058c1c289d9d80e9cf75d0e02d6f6858e046009e4197e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9fe76b893f6f07357494a287b1372906ff587a53af9f6683c908dfa20d955e3c"
   end
 
   depends_on "go" => :build
