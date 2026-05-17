5fe1c0eff0561fe0c9781a229723b49a7427667d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:31:24 +0800

    vfox: update 1.0.11 bottle.

--- Formula/v/vfox.rb
+++ Formula/v/vfox.rb
@@ -12,12 +12,7 @@ class Vfox < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "69890410a3bd1bf0a3713d051080834b75e9f0d7a6c8820a9050890dd65a9276"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "db62c1d48fc63366b538503c0547031b8b672c05a2572efb2d24797c7c173f22"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45541f695d69baa81859eecbeaa6a656e6b4e59382f3bceab8093add0d232bac"
-    sha256 cellar: :any_skip_relocation, sonoma:        "50d89f00234675b47ea92536ec0435791d43f74a72361460557a0bdf0fe5281b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7124f73212f71bc4e8427914cb82c66eb30e6bad5e75116b7b43bc49957fa4ed"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa64805f938b39440fcba1a3e564c863e10b1d2d71846cc59ce85533c24891bf"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c111732e634b2be9c3018c86a82391d551dd4a782f44fb6a4756d007e905743d"
   end
 
   depends_on "go" => :build
