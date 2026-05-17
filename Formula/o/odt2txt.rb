8612645d13b391e91ad876a4cab9f8fe5a56c199
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 15:59:47 +0800

    odt2txt: update 0.5 bottle.

--- Formula/o/odt2txt.rb
+++ Formula/o/odt2txt.rb
@@ -6,13 +6,7 @@ class Odt2txt < Formula
   license "GPL-2.0-only"
 
   bottle do
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ca8de3bdaff0abb474769434e2e6092d96befa2c79d741d387ea29be3c5c0969"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff99e00af2117a1c83d6a07d49286d93270c76e1be4b3fef4b5834daecda6a1f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d97dc3cf0b9cc3f503e98ec8bfc5fc175742f580db47d291ed1e325c49c85f84"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b554ee321e2dae2d44819148d0d1d6a01f5565df9d899f129a5d975a4454e353"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "762a04dc77992f1dd8040b8a922808bc103aa52b8a3a83e9a4270474aa778dc4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "742559144816885e1eaaa016a1ab273445fd6d31ebb9e4b636ede27c4963df4d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dda4afad59faa172b72a802386315e0283c9bccc9777c77477aeb2e5c32e6795"
   end
 
   on_linux do
