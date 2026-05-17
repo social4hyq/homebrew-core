5353bed4bfed1dd676b10d472b62d3283eed423b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:37:14 +0800

    mihomo: update 1.19.25 bottle.

--- Formula/m/mihomo.rb
+++ Formula/m/mihomo.rb
@@ -7,12 +7,7 @@ class Mihomo < Formula
   head "https://github.com/MetaCubeX/mihomo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "13bbb1e1c07716b1b36ba35ae1262b0381e4545bc19cf86282583bd7186efc9d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6bbaef69f84dcdd5fdd034d4022448df41bd6c89a0e162a762641decec1907cf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7dc8b5343e2172bcc9427c0871bca2f7bdce74aa922085cfacc916b02b2d38d7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6a77e7ea3bb30685484a372aec889bfc1c16ff1b20dfdedbe458f8e5f5df8ed1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b5be4c70f249a6d667f3e39883edc7cfd4336313c286465ed8866f8e81506a5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "110635d2f46cb664903c85f14f37e2857abe2d86b3178ad218864f4a9585f7e0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "465773b0b9f929e534b4a6e2bcd51317e3d0fde9d72f7d99df21ef4afcfa6896"
   end
 
   depends_on "go" => :build
