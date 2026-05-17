7d4f97801d4237023fe9ecd5dc0caf060d1be282
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 14:03:21 +0800

    moribito: update 0.2.6 bottle.

--- Formula/m/moribito.rb
+++ Formula/m/moribito.rb
@@ -7,14 +7,7 @@ class Moribito < Formula
   head "https://github.com/ericschmar/moribito.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "37034fa0d7d8f39b905f495d15d385ebad49b085045dc6ea469d29af44e2194b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37034fa0d7d8f39b905f495d15d385ebad49b085045dc6ea469d29af44e2194b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37034fa0d7d8f39b905f495d15d385ebad49b085045dc6ea469d29af44e2194b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "37034fa0d7d8f39b905f495d15d385ebad49b085045dc6ea469d29af44e2194b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b60752bdc952a103dc312c9ea4f404f1e0b260e5914fde7d810f91a996f31db2"
-    sha256 cellar: :any_skip_relocation, ventura:       "b60752bdc952a103dc312c9ea4f404f1e0b260e5914fde7d810f91a996f31db2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "60ff659612cd002a25d1b9f2210e354d5608a6a8be6b39a1ee8eedaff24e037a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9c70137bf656cf11da599cd653e60ad2976f1a5b00ee756ce9f6ec93766a3b27"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c240dde81e5a85c19b8d09ed929db314cbac53cf0f0eac31507a13ce97ac0b85"
   end
 
   depends_on "go" => :build
