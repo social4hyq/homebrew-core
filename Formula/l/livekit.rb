67494562fd1ba03c4c8e771267f78d63920b298f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:41:57 +0800

    livekit: update 1.11.0 bottle.

--- Formula/l/livekit.rb
+++ Formula/l/livekit.rb
@@ -15,12 +15,7 @@ class Livekit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "709910643d206898713186728826850efc17b36892e5b4ae988e036da63d60c2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4071b8fd9a5855f64804cccb9f526e94cdb155fa68cbc6b392085e5a040754e2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "86a6fd26b72661316dae9cab756b5d439faf4d47eb44cf9bfa59f22addc9c813"
-    sha256 cellar: :any_skip_relocation, sonoma:        "238b0cd9bffdeed1771d2e5fb2c4f8745411535f74377ceb3552f1e7fea6c993"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "08e5691dabad209b47994beac3781e29268c5d5050226f88bef9b473209c616a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49784c416f84d7d74ad1c11902d0563be39f2ccca45936217c56dc29c5244115"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "26a54d63d66fd6ee147f13c8646d47feea3c0785944116aafd66db3c338ee503"
   end
 
   depends_on "go" => :build
