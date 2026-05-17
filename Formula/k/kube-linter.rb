ce43a06bf9dc9468e6b40304fc2e6e61c246b990
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 14:57:11 +0800

    kube-linter: update 0.8.3 bottle.

--- Formula/k/kube-linter.rb
+++ Formula/k/kube-linter.rb
@@ -7,12 +7,7 @@ class KubeLinter < Formula
   head "https://github.com/stackrox/kube-linter.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4b16ae96700ced1179dc45c0c136a4cfeba5a5651f6e2ea38ac8886f8410b986"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4b16ae96700ced1179dc45c0c136a4cfeba5a5651f6e2ea38ac8886f8410b986"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b16ae96700ced1179dc45c0c136a4cfeba5a5651f6e2ea38ac8886f8410b986"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4ed1130dc09dfcebb9b263d146b4b6629b533900e4f206206c659e97ea9d79b5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "470534c3ce9782cb5a59df64177e236a7b89e61951230408e80f6ab27000391e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "743b7bd5e1afb0c95f0626f45bcaecdd4c5f1f48908ece1fdff2cfd094e30421"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c058c3ee6e58724a4c682aa7f3adbc3b9ef10d1160983c2a7df0217d69cd7196"
   end
 
   depends_on "go" => :build
