425fee1efce2b7a79c9ca0cfe14eaffa36784260
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 17:30:35 +0800

    docker-machine: update 0.16.2-gitlab.46 bottle.

--- Formula/d/docker-machine.rb
+++ Formula/d/docker-machine.rb
@@ -9,12 +9,7 @@ class DockerMachine < Formula
   head "https://gitlab.com/gitlab-org/ci-cd/docker-machine.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "daf32b25c976f79c3f73ac246a31e1ce07c973344e7531a60df64ae1a9ab0500"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "daf32b25c976f79c3f73ac246a31e1ce07c973344e7531a60df64ae1a9ab0500"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "daf32b25c976f79c3f73ac246a31e1ce07c973344e7531a60df64ae1a9ab0500"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f6f8fd8a6f3f83bd0cc29f6f5503f866a55b097e269ec1f97baf801dcce32274"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a6c5bfe1f4d7f5bc723875f759b983cad83188780c75818c68bd55d35ec58acb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e4afac0b3bfffa6ea13406cc82f666b15a64587306b44edffc8d630366884fc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "19ac5e4ec39d5089c6c14533ecabd768d340e94a3e7c1061e5661ab4fb36ba67"
   end
 
   depends_on "go" => :build
