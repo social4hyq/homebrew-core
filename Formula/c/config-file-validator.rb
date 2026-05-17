ab86e425c137229347caa70451ffdbdae212ec68
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:46:48 +0800

    config-file-validator: update 2.2.0 bottle.

--- Formula/c/config-file-validator.rb
+++ Formula/c/config-file-validator.rb
@@ -7,12 +7,7 @@ class ConfigFileValidator < Formula
   head "https://github.com/Boeing/config-file-validator.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "17386a1482d4a7bed23a281f0a4d883a849ff2fbee6b72512f4bf77300ae3cfe"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "17386a1482d4a7bed23a281f0a4d883a849ff2fbee6b72512f4bf77300ae3cfe"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "17386a1482d4a7bed23a281f0a4d883a849ff2fbee6b72512f4bf77300ae3cfe"
-    sha256 cellar: :any_skip_relocation, sonoma:        "133d5bf3b29150d9d18837036205f3d809d6af62e8e7df3af9823fd61e7ef00d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "13adf752054f0b868ec3fb7a53776db10fc16947219dab4af5d9bc68abbf8a9c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b2c7952f1780ee48b06ad4bcd2182352ded459216efffefbc81321b4666db1a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "00330ad716fcdb48d270789ea041d385da829fbae0441a72915b7c59758d0b3b"
   end
 
   depends_on "go" => :build
