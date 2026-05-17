70f199b579fdddc86a57f294db739bac8a0f81f3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:11:47 +0800

    aicommit: update 0.6.5 bottle.

--- Formula/a/aicommit.rb
+++ Formula/a/aicommit.rb
@@ -7,14 +7,7 @@ class Aicommit < Formula
   head "https://github.com/coder/aicommit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "076bbcf508c5867b4b0b525a0075200c671c2a5f909799ab4292bab2a4e93a30"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7a7c316f47dae0539e95407a1d404c7ccbb37f771f6a9049e3abc6dc6421484c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7a7c316f47dae0539e95407a1d404c7ccbb37f771f6a9049e3abc6dc6421484c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7a7c316f47dae0539e95407a1d404c7ccbb37f771f6a9049e3abc6dc6421484c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "03e62c692d3d64bc97dd6a388a8e3b810c1c8d8830a76495e0c8a80a689655a4"
-    sha256 cellar: :any_skip_relocation, ventura:       "03e62c692d3d64bc97dd6a388a8e3b810c1c8d8830a76495e0c8a80a689655a4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b473355665502296b1832fdc242a0056bac8dfec6eeb7fab010c89b596d3bb5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "848a5368d7881eb41335e58e54eb995252a4339ef65802360df8abcd05928b6b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "65e0cd862aafb3d730ca0cac854779800d46da35a8775b3bac2548a9971be616"
   end
 
   depends_on "go" => :build
