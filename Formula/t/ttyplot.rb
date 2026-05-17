82cfb1757d6b7d7b5247542db98cdfe90d97908a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 16:26:32 +0800

    ttyplot: update 1.7.1 bottle.

--- Formula/t/ttyplot.rb
+++ Formula/t/ttyplot.rb
@@ -11,14 +11,7 @@ class Ttyplot < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "816129fa92f1f7570a96c8ef72b50739cdf8b61aa1e82757b39ef9c7167743b0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "91f2f451e768d21221e8ac31f5d6cfcac856315052a16bd213c5410166de3eb4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a5b767fb22376073b1cba6e278d070248b8c8dd5823559360b36386218eed7f7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "39a3bd658a12d3eb8be9baa5ab23b4bc85c2cfacb96fd05aac64b8dfae705702"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a15fda21cf564d3fc84ec3930879758b07d6d462c08c086fab150957d56593e8"
-    sha256 cellar: :any_skip_relocation, ventura:       "e2aad133932bcf084b6cfae316e8ff6bd1cdc51b449cbb0a918365bfd9ae1788"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9477520982405b4c6dcc165fefbf076da0774c10cf32e0423b4be6f88cb45742"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b1a52187d8e41064b9d3e9d6a6c452fdb03e989591ee0d05a2d4829280579e3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "95a88df327bd104729413a3f843fd1a815f1897d353dc596645fc166caab9c2e"
   end
 
   depends_on "pkgconf" => :build
