f85aac99311e700dbfcb0ab0e52151736fceb287
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 15:09:19 +0800

    enca: update 1.22 bottle.

--- Formula/e/enca.rb
+++ Formula/e/enca.rb
@@ -7,12 +7,7 @@ class Enca < Formula
   head "https://github.com/Project-OSS-Revival/enca.git", branch: "master"
 
   bottle do
-    sha256 arm64_tahoe:   "186d5551a3f3b293b464cb9d8b2f00f9a8d23d8c7488593cfb301133fe7c47d2"
-    sha256 arm64_sequoia: "b6d87405f58557daf9bb694f39de18da6cfb4364980cea4bc6514dfeb83a948e"
-    sha256 arm64_sonoma:  "b12040e349cb6a5db5042b4cca7b8fa10b6302a4d55fe80bfb09fe3dbc271cd2"
-    sha256 sonoma:        "0756a5511840591047a2b8f81c2c77bafe9c65c73d82195a43fe22e5c0213cf0"
-    sha256 arm64_linux:   "cdc368ac529c459ac9453ecd1789ea7ab70911415a1a73b247323e8036a6bafe"
-    sha256 x86_64_linux:  "4ee249a13bafbc6ac78d69fbfd642546b73b68757a89d7e21d54ecd9172eec4a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3f95d648c1b22e9ea088fab1085558e6856f98db41d6dde209686adfb3db9b3f"
   end
 
   def install
