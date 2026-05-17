aec91cd4e0ef26b03b89f320baeaadd233744ef0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 16:17:20 +0800

    carapace: update 1.6.6 bottle.

--- Formula/c/carapace.rb
+++ Formula/c/carapace.rb
@@ -7,12 +7,7 @@ class Carapace < Formula
   head "https://github.com/carapace-sh/carapace-bin.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd294927a3419d5a07f132a5dd3ad594d9c2aaee7ae30e2ed1adacfcc4c7ec78"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd294927a3419d5a07f132a5dd3ad594d9c2aaee7ae30e2ed1adacfcc4c7ec78"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd294927a3419d5a07f132a5dd3ad594d9c2aaee7ae30e2ed1adacfcc4c7ec78"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e76d649e19c3ee6ab3e8ebe58ee829cd74709219353ba0efe2d95b9d8ed08716"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0515f75c020bbdb372659a2d99994c4c0d6a656417c1b0e500e7426652a6078"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74fd01998030963474607d1efad352e957f71d4aea83ab1704781878023f3ea4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1203e6897ea27a10d3d1791c875953234c5c654ae745e93781b42086d4d4ffc8"
   end
 
   depends_on "go" => :build
