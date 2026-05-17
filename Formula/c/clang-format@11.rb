9d3fd857fd5f01a7bd96f0929a99de317628fb8c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:47:43 +0800

    clang-format@11: update 11.1.0 bottle.

--- Formula/c/clang-format@11.rb
+++ Formula/c/clang-format@11.rb
@@ -6,13 +6,7 @@ class ClangFormatAT11 < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b1923e76a7a7db381a5aeec33e8b36b58dbc1894ed430bb0c0d2030b23351466"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80badd34294c905914f5525552db99369edbeb249d6ef7ca418d9df12988acf1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1d0b218f548e745af34b0652e5032bd6a526f36ce5e087fa907ea4d14700ebce"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8cf417694a7860c8b13c14fbcb0c85734e909769a376a16af6af60c8ae85c4e7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9cd3b2638563b5d8b649bd25f641713494eaca6117557f5c02e3eadb214e425c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c8221c67cd82366a807f2efed7dc2e1d927e4865e6f2d06d8bec74089e30efd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0752fdc865b3c1e3d92c0c1af7af1b47b9cf76565e2ea4cfc382b2743a6e0693"
   end
 
   deprecate! date: "2026-02-18", because: :unmaintained, replacement_formula: "clang-format"
