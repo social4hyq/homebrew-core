f84b2b61e3b603c7fd92ec15f5949330bd271bfe
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:37:28 +0800

    frei0r: update 3.1.3 bottle.

--- Formula/f/frei0r.rb
+++ Formula/f/frei0r.rb
@@ -7,12 +7,7 @@ class Frei0r < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cb1baea44ebcb0326f0dba61097f698ecff3e4f285fe762a2abe89e8c00f3686"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "335c72440b4b11ccde51af02d725619aa6575c55e030dbce2dd01064bb14d3a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78497853c70fc0008cd05b167286a8af83e407328a40ded6757a5e4809755a1f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e30e9aa5de02512b280e3759cb3c31a7950f54cfb2958ae8ed4267ec822f95f9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7e81d5aaa75f8af15983363ff563aa83206fff5eccc6a6aec922b817f6bdb859"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "142646aafd63ccbe84e942106a5697a470cd0a38837b9eab50670635eda1494e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "49d9c8b8c78d6d1a81962429f9e40a862c345e19fcf7cb368112a23508693e22"
   end
 
   depends_on "cmake" => :build
