7ee75e422cf46f12344ac120f16a31399d8be934
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 01:49:38 +0800

    algol68g: update 3.11.3 bottle.

--- Formula/a/algol68g.rb
+++ Formula/a/algol68g.rb
@@ -11,12 +11,7 @@ class Algol68g < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "9f01841b4a940ecc87541656d419b285155d2dffc3e8a1ed845a845ec376a5e7"
-    sha256 arm64_sequoia: "aef201fcbbb33ee0d48d9ad8a4f8fc9eb54116f3b3f264d9d7782770f1628d7c"
-    sha256 arm64_sonoma:  "b049518dea22f0430b667aa20832f430787fd5ef5b3f94b07a6458f525f8fbae"
-    sha256 sonoma:        "fd56f8179069541d59f3f65eaaf24964f4f94fa53e2bc69568b219d557669d9f"
-    sha256 arm64_linux:   "edef0183060f45e20ccf33095b2c64533f8e0c166ac9a0015660b8493effa279"
-    sha256 x86_64_linux:  "03172e7465af281f6cbcba83fa9faeca6f9c31fd148266c5a17a9978b39fd29c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "22c023c4af39ecc229485d63ed0192bf2e4e8524ee507f210b52dd192dafba5e"
   end
 
   uses_from_macos "curl"
