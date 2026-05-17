0951b1523af0d79765d47bad4825fae94babd58e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 17:42:24 +0800

    ratchet: update 0.11.4 bottle.

--- Formula/r/ratchet.rb
+++ Formula/r/ratchet.rb
@@ -7,14 +7,7 @@ class Ratchet < Formula
   head "https://github.com/sethvargo/ratchet.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "054e4744b0fac5342afb2e622206d80fdbb1b96e9b21cae1f7a0f8c133ba91f5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "87e9d4f5ece669b81b693c4e13c8d0e66a879fcbc40f99337a0f0e5c3065716d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "87e9d4f5ece669b81b693c4e13c8d0e66a879fcbc40f99337a0f0e5c3065716d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "87e9d4f5ece669b81b693c4e13c8d0e66a879fcbc40f99337a0f0e5c3065716d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0afec9e6ec42dffa7e6c9d5c4119bd21e326f38eeb73dfe212dcbd4deca9f5a4"
-    sha256 cellar: :any_skip_relocation, ventura:       "0afec9e6ec42dffa7e6c9d5c4119bd21e326f38eeb73dfe212dcbd4deca9f5a4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "735721ebf63318e966738056cba7806b62a7b2029eb6c8c8c2b512ae6c090723"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2b32f00ee26be2f4e60855b7f0a71d122df2b94c55509360052d15d7001c167"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6b858f0005e237b026327e6235e2da5e56ca80a67ad365714dc56e234c1bd350"
   end
 
   depends_on "go" => :build
