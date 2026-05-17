89d18941e8a7398b4abd51c8390cbc351624d5fb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 18:11:51 +0800

    checkbashisms: update 2.26.8 bottle.

--- Formula/c/checkbashisms.rb
+++ Formula/c/checkbashisms.rb
@@ -11,7 +11,7 @@ class Checkbashisms < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3f24a13ec015cc38acb252a67ac420e99c04485d05548a26b50ae261d973ec89"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dd80dc12cbf43ca8feebcbf8dc6b4095a487b8a0b5e5f2adf872e472e987c4dd"
   end
 
   def install
