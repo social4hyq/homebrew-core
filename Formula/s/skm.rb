261ab3c85e4ac57cb748fcad44ce3e380aa4f5f6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 01:50:20 +0800

    skm: update 0.8.9 bottle.

--- Formula/s/skm.rb
+++ Formula/s/skm.rb
@@ -7,12 +7,7 @@ class Skm < Formula
   head "https://github.com/TimothyYe/skm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "191babe323ee970ffb96461252a523c4cba700aeeee5cf0b7a10b497e015853a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "191babe323ee970ffb96461252a523c4cba700aeeee5cf0b7a10b497e015853a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "191babe323ee970ffb96461252a523c4cba700aeeee5cf0b7a10b497e015853a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c5f7482e6e1b36ef321aa0c3973a8fb74a900d953e027ec984468e7006a8ff52"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1041d12764f8b89703abaf91855a2cbf17abc48566f22c26e80e26960c6c78fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6451d9ca56b17d58ddbd61b8a3251900a09d4856dbfeb99125c8be468129ce18"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "34eb697fc6b697f051f1236d1f58dc7df95555138e0bd5c4d7b77fa0a0618f43"
   end
 
   depends_on "go" => :build
