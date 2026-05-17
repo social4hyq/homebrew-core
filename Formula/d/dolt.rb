07dc9077311d66779773332e524e2f3d2d00fdde
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 16:35:20 +0800

    dolt: update 2.0.3 bottle.

--- Formula/d/dolt.rb
+++ Formula/d/dolt.rb
@@ -13,12 +13,7 @@ class Dolt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "9c482a13a5872e31e4dbd298add9fbc0eed7ed10af7e1a33f609fb3ce094fa1b"
-    sha256 cellar: :any,                 arm64_sequoia: "18ab15fb9207fc00fda969c3ece10a8c236be43912913fc8dee36b053ad2d204"
-    sha256 cellar: :any,                 arm64_sonoma:  "8cc11dd3ce6b4cb0ae143bad47c18d213dc3ede4e2cb107ea2d1f29288b6a61b"
-    sha256 cellar: :any,                 sonoma:        "9be81eb39f975e3c6e56c22f4421070d7898a0a6804c3a474f6cd841fb3b73dd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3fb0969ac0228f6a1fcf2053c259b588cf6cb8e7971f1dc83984615c97034799"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b68b51c7be1f8d65e3588b78403aa43530c6bfa6e686852a8fce830506174aac"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5fe6cbb6a322d62b71c9d1e7d0c4fd5c29dce97f9736f1a1b0875cfc67a7572a"
   end
 
   depends_on "go" => :build
