bea5ad735df055536ca1551032615874836ea917
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 17:32:29 +0800

    mcp-publisher: update 1.7.9 bottle.

--- Formula/m/mcp-publisher.rb
+++ Formula/m/mcp-publisher.rb
@@ -7,12 +7,7 @@ class McpPublisher < Formula
   head "https://github.com/modelcontextprotocol/registry.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "37792e8554ad120a2c1ee3db5a4854042af66ced363ccd506a3dedc44c7189f2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37792e8554ad120a2c1ee3db5a4854042af66ced363ccd506a3dedc44c7189f2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37792e8554ad120a2c1ee3db5a4854042af66ced363ccd506a3dedc44c7189f2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e4de96faa7eba89f45ba13373f578823c7b11159083a861f91302bb8b90afaed"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "618d321d7403a818d65d006a4efe27da8c038b73fe699de151e3bd1b7344a4f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b262e2e7bff8a0400d3df2e3878e65d93e10c52cf36574ef04a604ceed431c58"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "69e05153c6af81d5ac11d65ad259a886f2e02ae18c6f7123a470497a162979b1"
   end
 
   depends_on "go" => :build
