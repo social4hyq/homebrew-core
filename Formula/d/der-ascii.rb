2b1685cf36da18a0e9163e716ba11e06767f268d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 16:07:23 +0800

    der-ascii: update 0.7.0 bottle.

--- Formula/d/der-ascii.rb
+++ Formula/d/der-ascii.rb
@@ -7,12 +7,7 @@ class DerAscii < Formula
   head "https://github.com/google/der-ascii.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d4a18fe39f98ab330d0be623e93e92fc7fc21663a21e8fedb2e0c4108e42acce"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d4a18fe39f98ab330d0be623e93e92fc7fc21663a21e8fedb2e0c4108e42acce"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d4a18fe39f98ab330d0be623e93e92fc7fc21663a21e8fedb2e0c4108e42acce"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9f90e0345b09d95c169528d54c1910032ff8cda324a16fd80db4e9c5c8320dfd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "841b6ec4e24ecd59cece4aef187231f63b7fe67e61534ed43cd9c3d1aa80055a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7313f36585dbab7370d770765e0e8e974dfa6da731d44e0340c193307b83659"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e027e6792bab63d759493210332d7d2986e680b58eb8beac35855a38ed412798"
   end
 
   depends_on "go" => :build
