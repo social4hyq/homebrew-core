a6cb2ff71070acd7af304aa58871cf7ee9508a13
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:14:36 +0800

    oksh: update 7.8 bottle.

--- Formula/o/oksh.rb
+++ Formula/o/oksh.rb
@@ -7,12 +7,7 @@ class Oksh < Formula
   head "https://github.com/ibara/oksh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ada92d98bf5afc55fa57bd5a67656dda5694f6f45a1fbaa99c22b92a792f1579"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "efe4ab1f8b53d82cb1faf48bc2b0f3fedc8bd3ea3defb33ca51dee8d2afd0553"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "63a57d4c141205fae7176186dd0a4f9f74db9191a79e679e842cdf2f2b6528f2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d13fac5d94bf0f2cab0029848d4324a4e938d8efc3d2d22f89d13ee44388aabb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4cb56f25784348e87b2824c946f27e38403cdf1a956e1dd36add9b8305484807"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c873e983dc4fb309264a6da0b94725eee67beab552cff478cd114b26b6fde3e5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "be463af96b2f8aaaafb307f561722c2b2062c211efbbb73e0c07c0a45865f55a"
   end
 
   def install
