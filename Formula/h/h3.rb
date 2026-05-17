c49ff9bf3e544496de9cac1991cbb4b874978734
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 22:07:02 +0800

    h3: update 4.4.1 bottle.

--- Formula/h/h3.rb
+++ Formula/h/h3.rb
@@ -6,12 +6,7 @@ class H3 < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "68636ef86a67e8737e4c2803218fd9d0148c2dae7c1827b3e9873f4ef2755851"
-    sha256 cellar: :any,                 arm64_sequoia: "0328b7e8d86e51153623a95e9207966eca223ac9832b54e769d1597807beb48c"
-    sha256 cellar: :any,                 arm64_sonoma:  "14d7541ec9d1099be12c1b66ca1a9eeac3d7cc170d6663ec3cc9d1f8c647d24b"
-    sha256 cellar: :any,                 sonoma:        "dc4b1ab1840bed0128839ae704c27909d11fe02a5bf31065ad7ca75793431098"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "549cd280f37fe31ce96f36deb4d212000e17ece12a8f7b7dce395e1b90fce120"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2471501e51c265e88b78973fc2234ebccfd7d4d4210bddddb8744c619060d0a1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5484a938e07ea3b9e2b08a6d8c7ecf58cc6a5ae7bec5aca0c76fe08b5e8b68f8"
   end
 
   depends_on "cmake" => :build
