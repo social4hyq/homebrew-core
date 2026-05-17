c250df6103ad16dc7b23168ca582fc5692f3837a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:57:48 +0800

    vulkan-loader: update 1.4.350.0 bottle.

--- Formula/v/vulkan-loader.rb
+++ Formula/v/vulkan-loader.rb
@@ -13,14 +13,7 @@ class VulkanLoader < Formula
   end
 
   bottle do
-    sha256               arm64_tahoe:   "a3ed8ff9247a4e058afb5d17c9c87856d95b5681409f1b5d230b671691769101"
-    sha256               arm64_sequoia: "20f5cc0a364d8875c3d5f256f88d7795678d6afad1cfa7447ddceb928d5e0c9f"
-    sha256               arm64_sonoma:  "ea5d2e9c708c61482e5b8f6d8acfd46626db1191f945daff914e53af358b2a41"
-    sha256 cellar: :any, tahoe:         "5761fee6fae907918097c8b4b86c22cd17cec7e9ff295223e2e584b5bb314991"
-    sha256 cellar: :any, sequoia:       "5e443d4b19f0b3be2e2eef152249f5ea0f8d8d1d2704b8e226d7ef898527ae6e"
-    sha256 cellar: :any, sonoma:        "2d266cb426bdf54c35c26ef16d1afe9d5aa7df8921476702f1f29a7d33cac611"
-    sha256               arm64_linux:   "cac29a07107d501055ce5584e0786e8d26fdcc59f234454c1f4af82e25d6df54"
-    sha256               x86_64_linux:  "082a2139e5d1f6070f7d20eb920ba0ea9141aa4a5d1523fb5888e9fceb2b4094"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0759f8bd7916e83d07c3e4ddb4090303be64a8866caf5e5912f171a5dcc9db79"
   end
 
   depends_on "cmake" => :build
