d5ffde92e023dd81d7951536f39e98c14804f187
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 22:02:26 +0800

    tektoncd-cli: update 0.45.0 bottle.

--- Formula/t/tektoncd-cli.rb
+++ Formula/t/tektoncd-cli.rb
@@ -11,12 +11,7 @@ class TektoncdCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7ced03fd56defd8104ca6ea49e8cf1614d6b093fdb20051b5dfaf1a711ab0dfe"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83b8a76f775e8a3a0e21566dcb5596c15cf77a2cbeaa18081327f2085891dd2a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0fdd450213d5bf04fb18bab3f5465295de588d943dc374d72eeb043835a6b758"
-    sha256 cellar: :any_skip_relocation, sonoma:        "11007e6d79e38eedf78e036e92afd06c1999915d101ac1ebbc6587e8a71ad303"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a31dbf910228523c8d2238bd98256883e9ac9fb377dc1b59a76c569fe5f22076"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc68cb56e03b5fc5052dd3bfaf89d681bd418677ca564575b8bbfb2dd1ca92a8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "313bdfcfe8b92b6bf47026412bb212e13c8387d01177a776eca530cff97fc15d"
   end
 
   depends_on "go" => :build
