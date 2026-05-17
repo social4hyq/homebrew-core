a694b69ae7883aed589d7b2deb1bf6095e5b961b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-17 22:37:06 +0800

    gitbackup: update 1.1 bottle.

--- Formula/g/gitbackup.rb
+++ Formula/g/gitbackup.rb
@@ -7,12 +7,7 @@ class Gitbackup < Formula
   head "https://github.com/amitsaha/gitbackup.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4031134b43e7809de19c4bb8370d2e26f39d8f4d6ab5c69f4506319ca9ff1a19"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4031134b43e7809de19c4bb8370d2e26f39d8f4d6ab5c69f4506319ca9ff1a19"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4031134b43e7809de19c4bb8370d2e26f39d8f4d6ab5c69f4506319ca9ff1a19"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2e29126d9a0958e8e694229d6d452752c8dd466cb97d6e584bcd500f9ea8e0af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3de7efa5c385da6b54a5d9d410500a7fb812eaeb6cafbb386c893f1b966f136a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eb1ace4fdc8a6aad703a4d8ab2ad5e26e44ab13467f305e543a1cc9d8bc8da48"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7ec61e3b3df02d6264b4b33b27ce31820fe8e2b895ab7df2b0157fbfe2e0b8ba"
   end
 
   depends_on "go" => :build
