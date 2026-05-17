fd7f9d92d0fdea173a4b9d71e4db49141b8c23e8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 02:22:20 +0800

    osmfilter: update 0.9 bottle.

--- Formula/o/osmfilter.rb
+++ Formula/o/osmfilter.rb
@@ -8,13 +8,7 @@ class Osmfilter < Formula
   head "https://gitlab.com/osm-c-tools/osmctools.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "64388c3368efe673234fd5d7990c0f791df04fb1622c27f347332e56d3fc44ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e09d876529de57fa2b1a1ad7409e5247e800d47bc0d78b1e4b8ed8ee9191c8be"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "32b130a827813abb0f08be6192ca91c886935172279932f0f3b7906ac4e846a0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ce176d1a39ea1d4ef3807e937f6ade079256b6925db43c1474f0a3cedf3c917c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "22da060bca2c2eb0dacadf143c8152c9722b1b2f15418483e51c8f48390fa85d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa55d22946aab558581f444b82e9695ce06f3fe7e550da005447ccf1de942078"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "771940f0cd520bcf74d13dabed9329eb9e092106a0f642296b2f9e9183acf811"
   end
 
   depends_on "autoconf" => :build
