c13b05bb96a0c1953474daf85a1d1764419ef2d4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 14:33:11 +0800

    mmdbinspect: update 2.0.0 bottle.

--- Formula/m/mmdbinspect.rb
+++ Formula/m/mmdbinspect.rb
@@ -7,12 +7,7 @@ class Mmdbinspect < Formula
   head "https://github.com/maxmind/mmdbinspect.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "32ea559ea3db8ed663b41e20b5d3db3c3982fd92acfcec7235de26b3be73bb9e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32ea559ea3db8ed663b41e20b5d3db3c3982fd92acfcec7235de26b3be73bb9e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "32ea559ea3db8ed663b41e20b5d3db3c3982fd92acfcec7235de26b3be73bb9e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e5d866271963d5a0a7152f47a41e8b1da2046e2f7e395287d65c3f1cb2cf78f4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "657f79c53a27815dbbc4ef247beae40307111f4d814d9751e1f3e02aec247bfb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "807a8b443eb151b0c5e9fa87b7cff7af1df3f8255d5b3ab8d067ac7e422f50df"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d595bd9797e064f424e3ce8704041be435ee4bc8e54811c247b37417bd566b0a"
   end
 
   depends_on "go" => :build
