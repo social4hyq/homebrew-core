e4c9d08556984ff49e297c824ace7aea38fc0ba1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 15:43:12 +0800

    nsq: update 1.3.0 bottle.

--- Formula/n/nsq.rb
+++ Formula/n/nsq.rb
@@ -7,13 +7,7 @@ class Nsq < Formula
   head "https://github.com/nsqio/nsq.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5a22f058697307ea3179db86098d939c2a0bab28729af92fc2237fa9ec46b843"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "164a08f1fe7e3007fddbb6f4111ed19863d08888501a00a7b0328234801636f7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1248437269d3aa06758ad5e02fb3fd13bf6ab46f7296b39c80bf9cf02cb1cf6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "353cac603cccbd9e62920f680f8328b17cc02baa391a3665b03345bc90edb1e5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c88ffbec2368aae0d432ebb3cb2b7dbe08b654bbc0d86f842a0034b9afa9a74e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a5066cbc44a03c78e4604cd45f34ba5eefadc51e876f61a7af5814ccc095096d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "749208e52a619bd9e4308604183c140a24229081d1c8cf5babfb0d9b3c9b3efa"
   end
 
   depends_on "go" => :build
