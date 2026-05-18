42e86e2b3436f30f6bc2c7afcf25b695f0fd750b
hqzing <hqzing@outlook.com>
2026-05-18 10:37:52 +0800

    portable-ruby: 4.0.3

--- Formula/p/portable-ruby.rb
+++ Formula/p/portable-ruby.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/portable-formula", __dir__)
 class PortableRuby < PortableFormula
   desc "Powerful, clean, object-oriented scripting language"
   homepage "https://www.ruby-lang.org/"
-  url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.1.tar.gz"
-  sha256 "3924be2d05db30f4e35f859bf028be85f4b7dd01714142fd823e4af5de2faf9d"
+  url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.3.tar.gz"
+  sha256 "77964acc370d5c8375b9502e5ba6c13c03ef91ab9eb9f521c84fb42b9c9a6b0f"
   license "Ruby"
 
   # This regex restricts matching to versions other than X.Y.0.
@@ -44,8 +44,8 @@ class PortableRuby < PortableFormula
   end
 
   resource "bootsnap" do
-    url "https://rubygems.org/downloads/bootsnap-1.21.1.gem"
-    sha256 "9373acfe732da35846623c337d3481af8ce77c7b3a927fb50e9aa92b46dbc4c4"
+    url "https://rubygems.org/downloads/bootsnap-1.23.0.gem"
+    sha256 "c1254f458d58558b58be0f8eb8f6eec2821456785b7cdd1e16248e2020d3f214"
 
     livecheck do
       url "https://rubygems.org/api/v1/versions/bootsnap.json"
@@ -53,6 +53,14 @@ class PortableRuby < PortableFormula
         json.first["number"]
       end
     end
+  end
+
+  # Fix performance regression in GC sweeping of classes.
+  # https://github.com/Homebrew/brew/issues/21859
+  # Remove with Ruby 4.0.4.
+  patch do
+    url "https://github.com/ruby/ruby/commit/2b22593ac12d0e8cbcf8299f0fea14c6311715d8.patch?full_index=1"
+    sha256 "fb7efdd6ed383aacf4d2d2cc5aeb8bb180f47dc3930c4280c5e137963780411c"
   end
 
   patch do
@@ -72,7 +80,8 @@ class PortableRuby < PortableFormula
     # - irb
     #   - reline
     #   - rdoc
-    allowed_gems = %w[debug fiddle irb reline rdoc]
+    # - rake
+    allowed_gems = %w[debug fiddle irb rake reline rdoc]
     bundled_gems = File.foreach("gems/bundled_gems").select do |line|
       line.blank? || line.start_with?("#") || allowed_gems.any? { |gem| line.match?(/\A#{Regexp.escape(gem)}\s/) }
     end
@@ -209,6 +218,7 @@ class PortableRuby < PortableFormula
       require "fiddle"
       require "bootsnap"
     EOS
+    system testpath/"bin/rake", "--version"
     system testpath/"bin/irb", "--version"
     system testpath/"bin/gem", "environment"
     system testpath/"bin/bundle", "init"
