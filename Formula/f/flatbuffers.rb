0bb059a920a6a3aaec9c65f3648a52766837e593
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 14:59:16 +0800

    flatbuffers: update 25.12.19 bottle.

--- Formula/f/flatbuffers.rb
+++ Formula/f/flatbuffers.rb
@@ -12,13 +12,7 @@ class Flatbuffers < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "328b035addba1451d382b7df88101a006f38f4f148e41b3cf37927139e4b7926"
-    sha256 cellar: :any,                 arm64_sequoia: "7ff96ca0377f7007e2b7476ccf5d128f8ca1e4f1ae48799c54c4e3169f47da65"
-    sha256 cellar: :any,                 arm64_sonoma:  "9861b60f635fbd2b053d9e44d6693f28066f674602a6e678a6810b389627f0ae"
-    sha256 cellar: :any,                 sonoma:        "4f9b13c5ecb225cea16d5c60ec050933f6b5f76bcb08d9d32f7036eecb2448db"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "94a3da2f6d6349bdbbac53f0a8b17c022ff0cfb163029dfe589c4793198445d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7e4ccf9797920f3ecfbbb9a5a540b1416f377d7a7b04a851f23860427df1434"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "89c2620e47f1c1876a2992bc4a3ce9db631836c019e66bb572b2c31162daf042"
   end
 
   depends_on "cmake" => :build
