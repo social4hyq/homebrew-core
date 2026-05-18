79b887c676028bc1fc8f2ca9ebf2bbbace3466c6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 15:54:25 +0800

    ni: update 30.1.0 bottle.

--- Formula/n/ni.rb
+++ Formula/n/ni.rb
@@ -6,7 +6,7 @@ class Ni < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9bae4e2633784a61fce85ef956aa2a86b2a617587a52a30b2bb74a9a5a0fbcd9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "754a981f0dc2e5a66972f5861e8b5c3d3397ddd2d40d1c13a441dcf68854760f"
   end
 
   depends_on "node"
