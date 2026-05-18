4b29a5f8302c1b7f1f3b4e29960f97d33ab28fc2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 17:18:46 +0800

    peco: update 0.6.0 bottle.

--- Formula/p/peco.rb
+++ Formula/p/peco.rb
@@ -7,12 +7,7 @@ class Peco < Formula
   head "https://github.com/peco/peco.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "323de7d5f3d9f0e575f9b017c805514b396e1a2d25c0ea15e5378279da8dca6f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "323de7d5f3d9f0e575f9b017c805514b396e1a2d25c0ea15e5378279da8dca6f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "323de7d5f3d9f0e575f9b017c805514b396e1a2d25c0ea15e5378279da8dca6f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6ccbfdfee81da5ec438744fdff6a5e5d6ce4d6bb14def3fe806da90881946af3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0f26b8b4db22eb33007c301bedf8408146dd2189b478283cee3475bc54885bbd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48428648fdfbae5e429763f95164fdd2fbb5007aa071e2eda4d386feccf5b183"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dfb8177fa2ec394ff7a7a8a9284c7052da0df41a1be9aea211f387fc367893d8"
   end
 
   depends_on "go" => :build
