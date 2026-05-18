0083033aef9f926b7684c54f791d2e8e973c47a5
hqzing <hqzing@outlook.com>
2026-05-18 12:21:32 +0800

    llvm-gcc-compat: use atomgit.com domain

--- Formula/l/llvm-gcc-compat.rb
+++ Formula/l/llvm-gcc-compat.rb
@@ -1,7 +1,7 @@
 class LlvmGccCompat < Formula
   desc "Symlink OHOS LLVM tools to GCC-style names (gcc, ld, ar, etc.)"
-  homepage "https://gitcode.com/Harmonybrew/homebrew-core"
-  url "https://gitcode.com/Harmonybrew/homebrew-core.git", revision: "a8784ea451ad819d27a548411b8853fca6de3124"
+  homepage "https://atomgit.com/Harmonybrew/homebrew-core"
+  url "https://atomgit.com/Harmonybrew/homebrew-core.git", revision: "a8784ea451ad819d27a548411b8853fca6de3124"
   version "1.0.0"
   license "BSD-2-Clause"
 
