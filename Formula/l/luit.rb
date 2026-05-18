085f17d2330946c1a7b4e1b7128e674369b8f66f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 12:02:32 +0800

    luit: update 20250912 bottle.

--- Formula/l/luit.rb
+++ Formula/l/luit.rb
@@ -11,13 +11,7 @@ class Luit < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34fe7ef5c7484db64581c5f869a42e054a74996177ce74c93a2623dd9695d345"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8e538cd9ca308afad3716482ee10e7f623ffd90ab8f4917700c8dd102c029525"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "07c6106341ee4be787dfac58149cb8bced04babe0832f4baa86eab5ef11c57f0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5a0508f2e99339bb372a134092c562a6f5bd83b2b5187cf303c31d77f6849c82"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "81f95c06decc5fba417ef18968914855efd095abbbb57a71e999fa9ff91d10b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32a25f291b796b14cd3885b2007fb54efc12d256ba1e44705986bbcae7a5708c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0aaebb50a82e7b68e75286b4fe2a5e4624e435cb8e303002ec9f91f5fdd1800f"
   end
 
   on_linux do
