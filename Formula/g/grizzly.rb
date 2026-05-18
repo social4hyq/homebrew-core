e9cbd9999d5666aa140fbb49a96b7d096e0e058b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 11:20:56 +0800

    grizzly: update 0.7.1 bottle.

--- Formula/g/grizzly.rb
+++ Formula/g/grizzly.rb
@@ -7,14 +7,7 @@ class Grizzly < Formula
   head "https://github.com/grafana/grizzly.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d7d0f848c871281f79da1be4fba2bb1c7c0eabb1a15c19b854d661aecd4a948d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c00e5b79f192d86d855287c9abb183eede829f47d6892bfef011d0977fb45cc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c00e5b79f192d86d855287c9abb183eede829f47d6892bfef011d0977fb45cc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "9c00e5b79f192d86d855287c9abb183eede829f47d6892bfef011d0977fb45cc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "679f770b4eff802b8e9b6eb199fb9ae076f14093ea58342fcc1752d43985415f"
-    sha256 cellar: :any_skip_relocation, ventura:       "679f770b4eff802b8e9b6eb199fb9ae076f14093ea58342fcc1752d43985415f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d86a0471bd365daf4e24ae7e8a4890dceb816eb939ae28819369a56d878dacf0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1cd0d526f55b71f84e88c4bda49551c661b56f7dcace86fbbbb637a27fd4274"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d0d5798478abacc24372b2bc9b6cf75c75722e9ee728e8d29743e2440bf1a952"
   end
 
   # https://github.com/grafana/grizzly/pull/613
