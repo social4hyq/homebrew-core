3e6ca931a2fa0939dd7d6a594cf1229ae639adac
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 14:21:56 +0800

    git-credential-oauth: update 0.17.2 bottle.

--- Formula/g/git-credential-oauth.rb
+++ Formula/g/git-credential-oauth.rb
@@ -7,12 +7,7 @@ class GitCredentialOauth < Formula
   head "https://github.com/hickford/git-credential-oauth.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d60c85756632b4de61dd73bb072f703614786ddc20d26a67ca4923a460753b36"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d60c85756632b4de61dd73bb072f703614786ddc20d26a67ca4923a460753b36"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d60c85756632b4de61dd73bb072f703614786ddc20d26a67ca4923a460753b36"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a060d242f2a2be956538ce2600049cedf2199a134e6e16af90f3c28b8f322630"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7920c04e1ffa86ec1d3c287fbed4c1f8ba1623b003124fadab736b8c4aa0acef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42bd76ff4cfedf495012509915065ac573a0a19da4ae3046949eb7c1621589a1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "87c63f64f2f1838756759bdbfdc78c14b8f7de079eb541a2f28fb822daead596"
   end
 
   depends_on "go" => :build
