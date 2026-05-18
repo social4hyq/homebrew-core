d9d0ad25f7f8afb020af1dac9727e72d9637798e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 12:43:19 +0800

    dnscontrol: update 4.38.0 bottle.

--- Formula/d/dnscontrol.rb
+++ Formula/d/dnscontrol.rb
@@ -16,12 +16,7 @@ class Dnscontrol < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b2ffe9a83051679fc88f7beb53ff8a347fd6a00d4d4f23181213c70099ffe415"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c2064375c6bb24e9435c166898d511833b3a39166a6f59122d0a5840e47201a9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fda1a22ac7cb719ca51f59db7f39e30c8c8c88c32772d4ff8de6c71130877395"
-    sha256 cellar: :any_skip_relocation, sonoma:        "35a8b654083c7d310a12af6a50cb53c3a7a73c9ac57266cd809dab5c3aa8863d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "057a90249d04b8930bab42723f011601821f021cd7ad4b93e42fc7e256e8a50b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d780b09808d3d685d102b3d109822d93189e7be28fc6f8b85521cf7e588f6a80"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1eaca3b496d772ba540ef1f4f85cf0ede54a58dd3efee7f042ed8d2acd6d8d21"
   end
 
   depends_on "go" => :build
