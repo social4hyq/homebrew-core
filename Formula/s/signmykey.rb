754a642adc5020323c1b7f9e5112b65830391db3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 11:26:32 +0800

    signmykey: update 0.8.9 bottle.

--- Formula/s/signmykey.rb
+++ Formula/s/signmykey.rb
@@ -7,13 +7,7 @@ class Signmykey < Formula
   head "https://github.com/signmykeyio/signmykey.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "90a3fa84385d0929c68ee02274431316e4084865eb5ad9dbb7cb060be7a7b354"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90a3fa84385d0929c68ee02274431316e4084865eb5ad9dbb7cb060be7a7b354"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90a3fa84385d0929c68ee02274431316e4084865eb5ad9dbb7cb060be7a7b354"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7ea74b2812543ca3d79a381359f2412bed177bd86cc3208dd4afad608781fb82"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0fabc7ade5f371c73f57886c343c2aba945fe667eb0b2ee8c6cd21522e4a1fd4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5234e51f91e4299f1792be2cab5ec37bd1c669830851af5488dba303537edf6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1ffac78964f9020cb524620f25626c01d47c2f25f46965d9ba96b633e0063244"
   end
 
   depends_on "go" => :build
