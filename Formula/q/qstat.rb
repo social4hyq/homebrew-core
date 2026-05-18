0996e0f7bc1f804533823968815664e1cece06dc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-18 16:20:37 +0800

    qstat: update 2.18 bottle.

--- Formula/q/qstat.rb
+++ Formula/q/qstat.rb
@@ -6,12 +6,7 @@ class Qstat < Formula
   license "Artistic-2.0"
 
   bottle do
-    sha256                               arm64_tahoe:   "c7a13c065f07c78ac3ece908defa738146039d54b9d3642e06d06766031e5d8e"
-    sha256                               arm64_sequoia: "6268312f03fbb890419d8c4343dbff526567b7aa2aa537915160fd60f1080706"
-    sha256                               arm64_sonoma:  "37c1e516e543cb130b2076a88a53bbf8eb1f7c6a25afe26fed10caba13043b29"
-    sha256                               sonoma:        "dbe7c4cc53a379cd620bab83df9c7ed807b67b0793518b262139d465841b3eca"
-    sha256                               arm64_linux:   "7e6d928552859166fd08ef486163d0894745c1b5b6c40b789205ab06c4208794"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ae181c9f7e61cb31c66d525ff2bcfd6da52c803e412b795e3009881879f0ca5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e0213b024a8a9e524aeaa1e031563c0d130e7dd6b2a85668611a65ab02b5ee73"
   end
 
   depends_on "autoconf" => :build
