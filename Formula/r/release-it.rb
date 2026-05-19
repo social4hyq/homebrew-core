468e6e714d1e80c102958f7a98c179000533e41c
hqzing <hqzing@outlook.com>
2026-05-19 13:10:49 +0000

    release-it 20.0.1 (new formula)

---
+++ Formula/r/release-it.rb
@@ -0,0 +1,32 @@
+class ReleaseIt < Formula
+  desc "Generic CLI tool to automate versioning and package publishing related tasks"
+  homepage "https://github.com/release-it/release-it"
+  url "https://registry.npmjs.org/release-it/-/release-it-20.0.1.tgz"
+  sha256 "9a1ce384b24fb2be3a9543b9e9053e618b1fa915351f93248c01265c6564c3e2"
+  license "MIT"
+
+  livecheck do
+    url :homepage
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "25dbf6cb7babb0045d3fdc60cc9d0036e37a7c989b52c9191fc26dae54e5a76b"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/release-it -v")
+    (testpath/".release-it.json").write("{\"foo\": \"bar\"}")
+    (testpath/"package.json").write("{\"name\":\"test-pkg\",\"version\":\"1.0.0\"}")
+    assert_match(/Let's release test-pkg.+\(1\.0\.0\.\.\.1\.0\.1\).+Empty changelog.+Done \(in \d+s\.\)/m,
+      shell_output("#{bin}/release-it --npm.skipChecks --no-npm.publish --ci"))
+    assert_match "1.0.1", (testpath/"package.json").read
+  end
+end
