878f5a291681028d3800b0869fb3c37aeefbfbc4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-19 17:41:53 +0800

    gwt: add 2.13.0 bottle.

--- Formula/g/gwt.rb
+++ Formula/g/gwt.rb
@@ -10,6 +10,11 @@ class Gwt < Formula
     strategy :github_latest
   end
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "23f3d4d9c8d41a04636695b55a4c12846decbfc2c185170bc93afc7a8200c0a1"
+  end
+
   depends_on "openjdk"
 
   def install
