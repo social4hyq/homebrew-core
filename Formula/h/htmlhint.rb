974554e8b00cc397458c343c762177ecab74b2df
hqzing <hqzing@outlook.com>
2026-05-19 14:54:10 +0000

    htmlhint 1.9.2 (new formula)

---
+++ Formula/h/htmlhint.rb
@@ -0,0 +1,52 @@
+class Htmlhint < Formula
+  desc "Static code analysis tool you need for your HTML"
+  homepage "https://github.com/htmlhint/HTMLHint"
+  url "https://registry.npmjs.org/htmlhint/-/htmlhint-1.9.2.tgz"
+  sha256 "c32775c6abf4ed12bbb87c7310ff76802270989f4880a25196d196b7c7fe7c17"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "69e0a71a735b3a9c39a171ed08a9f92fcc26a5a04eee483e331a7402dd6e1a44"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/htmlhint --version")
+
+    (testpath/"invalid.html").write <<~EOS
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Test</title>
+      </head>
+      <body>
+        <h1>Hello World</h2>
+        <img src="test.png">
+        <a href="#" target="_blank">Link</a>
+      </body>
+      </html>
+    EOS
+
+    output = shell_output("#{bin}/htmlhint #{testpath}/invalid.html", 1)
+    assert_match "Tag must be paired", output
+    assert_match "Scanned 1 files, found 2 errors in 1 files", output
+
+    (testpath/"valid.html").write <<~EOS
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Test</title>
+      </head>
+      </html>
+    EOS
+
+    output = shell_output("#{bin}/htmlhint #{testpath}/valid.html")
+    assert_match "Scanned 1 files, no errors found", output
+  end
+end
