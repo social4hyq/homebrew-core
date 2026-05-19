92c6afcb326d3832c00603972a6caf6e092d22a8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 00:57:32 +0800

    rggen: update 0.36.1 bottle.

--- Formula/r/rggen.rb
+++ Formula/r/rggen.rb
@@ -28,12 +28,7 @@ class Rggen < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "37f1ceb0085f6a7f03093d7cf54e998f4df5e1b25ec0dae6365c5020bbab57b1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37f1ceb0085f6a7f03093d7cf54e998f4df5e1b25ec0dae6365c5020bbab57b1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37f1ceb0085f6a7f03093d7cf54e998f4df5e1b25ec0dae6365c5020bbab57b1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "caa811466dcfc98feb7fdd7fce8b8baa0bf3f12d24bc9f7b4df7e44971ccedfa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "35fdce56b3c59e689592a0a6b3c8803e211384d3c4b43cbb47a9c4fda4c6f02d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ff477fca88784c844ec9b3c25d36bb19c1044acd06ffb2af1e814e27650ea7fa"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ad249dc7397483c46e06f73e789521cacff7212bd4da3a7523b19edb56b8ec53"
   end
 
   head do
