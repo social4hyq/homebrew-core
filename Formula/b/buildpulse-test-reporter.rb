37091245e303f7c057c3d8b00c96bdd36ae0c7c8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 00:24:03 +0800

    buildpulse-test-reporter: update 0.29.0 bottle.

--- Formula/b/buildpulse-test-reporter.rb
+++ Formula/b/buildpulse-test-reporter.rb
@@ -12,15 +12,7 @@ class BuildpulseTestReporter < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4711592a6b669b5d417bf261032b49b16a0939ccd1af8d0b72bf09eff8875988"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9517c8459dbd5480f9c0d20230d1a331e22de9225109d0b777fc9c438fbb366"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9517c8459dbd5480f9c0d20230d1a331e22de9225109d0b777fc9c438fbb366"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d9517c8459dbd5480f9c0d20230d1a331e22de9225109d0b777fc9c438fbb366"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3f5d4e0bcadb62fd9c54073b4868129f67bb484189800987c64a663a6b594a49"
-    sha256 cellar: :any_skip_relocation, ventura:       "3f5d4e0bcadb62fd9c54073b4868129f67bb484189800987c64a663a6b594a49"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b19d3cb0925d0cb510b6a5fd8e0fb277309db68a43bda6e6687cb474b6e0e734"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a2acf192f12d0435f4d764e2fccd9a532b708ec0bd24cfc2d4b7a0beed3840c2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f3c2861e0d78e67c935c738b25e38655bc79d370789d27da59396d346472f1f1"
   end
 
   depends_on "go" => :build
