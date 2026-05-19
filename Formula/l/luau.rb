7ddf7fd5ce8e820b750cab3784fe8a6f21a31a7f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 02:46:55 +0800

    luau: update 0.721 bottle.

--- Formula/l/luau.rb
+++ Formula/l/luau.rb
@@ -13,12 +13,7 @@ class Luau < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6b2728400e4b043b5a35cc518d0f1e9aa21f06d11578a84e438491ba5adcaedc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2807813f4ecf91389f7b2dce455b797ab1aadc6f46406049277fb6b860e22ebc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0e40e6f67838fc083f3705859eb17f9e4a848891b93edd6bedf5c30d507f305b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "17bfc895972cc63d8941fc39796d9fb985ebf62ad0a31b8194272cecea5acfb2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6bca9f286d71015725747810641f2ff7856350e1ea61d94ec0bbd252b7ad099b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4eed42f3807e272ca7f9414f55ef833d67a0799aa8b15b18274ca561111e5c88"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3d8a62c27c88ce004d11ddb3619f9e23ff476b58405a615e2051550c20a30d0a"
   end
 
   depends_on "cmake" => :build
