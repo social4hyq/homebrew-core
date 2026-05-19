baf25189552bf102a03d5455511c7b5b00a84920
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 01:52:31 +0800

    telegram-downloader: update 0.20.2 bottle.

--- Formula/t/telegram-downloader.rb
+++ Formula/t/telegram-downloader.rb
@@ -9,12 +9,7 @@ class TelegramDownloader < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "11453e2008360f1c3fd259a09f1e662a97ea50ff2b2add1bbf7174bc6b545887"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cffcc3cca28d77e5b68dd88d126a7f6d17c8aa7a582b817d284b0e071f77885d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "223f28d9518f67b1ae1ee333d661ae6a762949e3f06b71226a60f6095c7a6343"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2a06af5fe69dede9d66af3af4238d8f8f29ac327b9f404830a8bebb014b68349"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a3da7066f0ba2f2db4bfe0de854d5cc2d535be1c8bf7c4d59176514c7ca87d67"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e5265b9515152ca3e103db20a166ae1aaea8e3bc40d8500f678aee3c9c31ff9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d194a9d75b1a56737143fb38cdb18436a6dbfce09e4382b356c0556022245cf9"
   end
 
   depends_on "go" => :build
