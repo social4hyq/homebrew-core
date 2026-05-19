ce76418164b04c2df31386db23d972ac4a8bcdcf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-19 18:47:36 +0800

    virtualenv: update 21.3.3 bottle.

--- Formula/v/virtualenv.rb
+++ Formula/v/virtualenv.rb
@@ -9,7 +9,7 @@ class Virtualenv < Formula
   head "https://github.com/pypa/virtualenv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ded0c4f43a55fbf69fd6dfc00f0f160a3841fe251468b5b085cc2c3b95b337d6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d77c240c3f057b3f42a96d84d97815513749856aee650817ec280452d4540fe8"
   end
 
   depends_on "python@3.14"
