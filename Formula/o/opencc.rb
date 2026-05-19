792c768d2209459bba868e12916a118c34dffcc2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 03:14:01 +0800

    opencc: update 1.3.1 bottle.

--- Formula/o/opencc.rb
+++ Formula/o/opencc.rb
@@ -8,12 +8,7 @@ class Opencc < Formula
   head "https://github.com/BYVoid/OpenCC.git", branch: "master"
 
   bottle do
-    sha256 arm64_tahoe:   "591ba4f3b3d2a5c75f346450759a75645cf9d4ead293aee029711f4516710eb3"
-    sha256 arm64_sequoia: "7374ebb5b9fe32296913faf7e730a30e3bda9dc7c62e4e236bb9fecb321c2895"
-    sha256 arm64_sonoma:  "33ec0628c320472651c3dd80f5a32e8cdaf97b2ba7d0eb6cfd4163bc2aab8907"
-    sha256 sonoma:        "2e16308d74b6393e374667c7302647bdb8ca97e93731414142fc337272f39a56"
-    sha256 arm64_linux:   "c0ed290e76d711edb47f76cdaf5815f57b133e6ac7d56cb659dc7306fa241fc3"
-    sha256 x86_64_linux:  "c133c27d6f2216b72f4e5cad57507908b3f8ecf5dd9ea7f8ecc4f71ee6057ed6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f985f42a379b46632133bb20a1a4dc7416eb35e7bf54c01a26a916d57c732028"
   end
 
   depends_on "cmake" => :build
