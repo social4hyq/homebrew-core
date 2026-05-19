103d51b197bdb433e8597a5e65e812e48c00f634
hqzing <hqzing@outlook.com>
2026-05-19 13:49:12 +0000

    getmail6 6.19.12 (new formula)

---
+++ Formula/g/getmail6.rb
@@ -0,0 +1,57 @@
+class Getmail6 < Formula
+  include Language::Python::Shebang
+
+  desc "Extensible mail retrieval system with POP3, IMAP4, SSL support"
+  homepage "https://getmail6.org/"
+  url "https://github.com/getmail6/getmail6/archive/refs/tags/v6.19.12.tar.gz"
+  sha256 "85bb39673d5160202e7ab32a02a22d95819ab01d81c3aca349e4617b98f8fb83"
+  license "GPL-2.0-only"
+
+  # Upstream uses GitHub releases to indicate that a version is released
+  # (there's also sometimes a notable gap between when a version is tagged and
+  # and the release is created), so the `GithubLatest` strategy is necessary.
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e69c7b852da170af02893228765ab35a68eefd8de2cc7cbba36eb3661e85afe9"
+  end
+
+  uses_from_macos "python"
+
+  def install
+    files = %w[getmail getmail_fetch getmail_maildir getmail_mbox]
+    rewrite_shebang detected_python_shebang(use_python_from_path: true), *files
+    libexec.install files
+    bin.install_symlink libexec.children
+    libexec.install "getmailcore"
+    man1.install Pathname.glob("docs/*.1")
+  end
+
+  test do
+    %w[INBOX/cur INBOX/new INBOX/tmp].each { |f| (testpath/"getmail/#{f}").mkpath }
+    (testpath/"getmail/getmailrc").write <<~EOS
+      [retriever]
+      type = SimpleIMAPSSLRetriever
+      server = imap.gmail.com
+      port = 993
+      username = test@brew.sh
+      #password = ... or
+      password_command = ("pass", "test@brew.sh")
+
+      [destination]
+      type = Maildir
+      path = #{testpath}/getmail/INBOX/
+
+      [options]
+      read_all = true
+      delete = true
+    EOS
+    output = shell_output("#{bin}/getmail --getmaildir #{testpath}/getmail 2>&1", 2)
+    assert_match "Program \"pass\" not found", output
+
+    assert_match version.to_s, shell_output("#{bin}/getmail --version")
+  end
+end
