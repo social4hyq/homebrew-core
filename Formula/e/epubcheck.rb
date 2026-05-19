474d70f8c3732770a9a70140f0c3bcdd09bf2ad6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-19 17:35:00 +0800

    epubcheck: add 5.3.0 bottle.

--- Formula/e/epubcheck.rb
+++ Formula/e/epubcheck.rb
@@ -5,6 +5,11 @@ class Epubcheck < Formula
   sha256 "6c07e68584b2e2ce2f89fe06e1246dfead3eb36b46b340e7d93524f29dcff6c5"
   license "BSD-3-Clause"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c1bb37e3b5bb6dcae20d9712d6de60b71f67868dbaafa142fdca647dc160e3c7"
+  end
+
   depends_on "openjdk"
 
   def install
