1e1ae9fd323e1f1363e1243139804ad8b92005ee
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-19 09:59:09 +0800

    vim 9.2.0500

--- Formula/p/vim.rb
+++ Formula/p/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/refs/tags/v9.2.0450.tar.gz"
-  sha256 "6811815aaa2c40d72837f62dce17d1cbc69def741863ae485d52396695453ad6"
+  url "https://github.com/vim/vim/archive/refs/tags/v9.2.0500.tar.gz"
+  sha256 "c4d73c2f16f6a20ecba82f6e1d2586f4aaf66a4dc13f45d686b292be7768cd62"
   license "Vim"
   compatibility_version 1
   head "https://github.com/vim/vim.git", branch: "master"
