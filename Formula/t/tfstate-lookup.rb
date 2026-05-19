f2cc8e1d6c42988e83813e500fdf6215719bc90b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-19 18:49:18 +0800

    tfstate-lookup: update 1.12.1 bottle.

--- Formula/t/tfstate-lookup.rb
+++ Formula/t/tfstate-lookup.rb
@@ -6,7 +6,7 @@ class TfstateLookup < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3ae3d493d7516e44aff3dea2edc1b3638ededc83add0e271bbd46a949fe47536"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "edec617c02eb20d0e2beee592637b7934ffa2350ca529e6952b1c659ee0d46c3"
   end
 
   depends_on "go" => :build
