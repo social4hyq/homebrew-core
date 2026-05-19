5cdcf2c5a3eac887ccdb575100dfc47c996709fe
lanfungcing <lanfq.26@qq.com>
2026-05-19 03:47:36 +0000

    groonga 16.0.2 (new formula)

---
+++ Formula/g/groonga.rb
@@ -0,0 +1,117 @@
+class Groonga < Formula
+  desc "Fulltext search engine and column store"
+  homepage "https://groonga.org/"
+  url "https://github.com/groonga/groonga/releases/download/v16.0.2/groonga-16.0.2.tar.gz"
+  sha256 "4bb7c995ef370b2370ed16fe78ddeebe827da22127d1cecf0696f8f5f28dde69"
+  license "LGPL-2.1-or-later"
+  compatibility_version 1
+  head "https://github.com/groonga/groonga.git", branch: "main"
+
+  livecheck do
+    url :homepage
+    regex(%r{>v?(\d+(?:\.\d+)+)</a> is the latest release}i)
+  end
+
+  bottle do
+    sha256 arm64_tahoe:   "fa4e7c569fff51e13e2fe1d78335683e1bd0baba10539172ef5f9d5119b9cc33"
+    sha256 arm64_sequoia: "83792e45b9bc7256d1616af8627699791a904bafc7fe67f626e03f2e763ee3fe"
+    sha256 arm64_sonoma:  "03428dd8b7e3394116dd94ebf409d5770e885c42e99a835ca427916bbc7654dc"
+    sha256 sonoma:        "891468af7fc00888fc84199146675864a355d271846fa1e28ff0fd938f3da8ca"
+    sha256 arm64_linux:   "f72eafa105c6c5067f264b6246738046064694a063a77a314e2528a891c35ce3"
+    sha256 x86_64_linux:  "c15ff28279da67efb9fe1c278f6618d5273d2551fb7ff59d4f02c27f5ef149de"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "lz4"
+  depends_on "mecab"
+  depends_on "mecab-ipadic" => :no_linkage
+  depends_on "msgpack"
+  depends_on "onigmo"
+  depends_on "simdjson"
+  depends_on "zstd"
+
+  uses_from_macos "libedit"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  link_overwrite "lib/groonga/plugins/normalizers/"
+  link_overwrite "share/doc/groonga-normalizer-mysql/"
+  link_overwrite "lib/pkgconfig/groonga-normalizer-mysql.pc"
+
+  resource "groonga-normalizer-mysql" do
+    url "https://github.com/groonga/groonga-normalizer-mysql/releases/download/v1.3.0/groonga-normalizer-mysql-1.3.0.tar.gz"
+    sha256 "693c24eff9ba95cd498ba28f8d5826843caec347b5aa6976e565e69535b44147"
+
+    livecheck do
+      url :url
+    end
+  end
+
+  def install
+    # Removed bundled libraries but keep files needed by build scripts even when unused
+    rm_r(Dir["vendor/*"] - ["vendor/CMakeLists.txt", "vendor/mecab", "vendor/mruby", "vendor/plugins"])
+
+    # Explicitly disable features to avoid opportunistic linkage from superenv CMAKE_PREFIX_PATH.
+    # Also set FETCHCONTENT_FULLY_DISCONNECTED=ON to avoid fallback to fetching bundled copies.
+    args = %W[
+      -DCMAKE_INSTALL_LOCALSTATEDIR=#{var}
+      -DCMAKE_INSTALL_RPATH=#{rpath};#{rpath(source: lib/"groonga/plugins/functions")}
+      -DCMAKE_INSTALL_SYSCONFDIR=#{etc}
+      -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+      -DGRN_WITH_BASE64=no
+      -DGRN_WITH_BUNDLED_ONIGMO=OFF
+      -DGRN_WITH_CURL=no
+      -DGRN_WITH_FAISS=no
+      -DGRN_WITH_H3=no
+      -DGRN_WITH_KYTEA=no
+      -DGRN_WITH_LIBEDIT=system
+      -DGRN_WITH_LLAMA_CPP=no
+      -DGRN_WITH_LIBSTEMMER=no
+      -DGRN_WITH_LZ4=system
+      -DGRN_WITH_MECAB=yes
+      -DGRN_WITH_MESSAGE_PACK=system
+      -DGRN_WITH_SIMDJSON=system
+      -DGRN_WITH_XSIMD=no
+      -DGRN_WITH_XXHASH=no
+      -DGRN_WITH_ZEROMQ=no
+      -DGRN_WITH_ZLIB=yes
+      -DGRN_WITH_ZSTD=system
+      -DGroongalz4_FIND_QUIETLY=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "_build", *args, *std_cmake_args
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+
+    resource("groonga-normalizer-mysql").stage do
+      args = ["-DCMAKE_INSTALL_RPATH=#{rpath(source: lib/"groonga/plugins/normalizers")}"]
+
+      system "cmake", "-S", ".", "-B", "_build", *args, *std_cmake_args
+      system "cmake", "--build", "_build"
+      system "cmake", "--install", "_build"
+    end
+  end
+
+  test do
+    IO.popen("#{bin}/groonga -n #{testpath}/test.db", "r+") do |io|
+      io.puts("table_create --name TestTable --flags TABLE_HASH_KEY --key_type ShortText")
+      sleep 2
+      io.puts("shutdown")
+      # expected returned result is like this:
+      # [[0,1447502555.38667,0.000824928283691406],true]\n
+      assert_match(/\[\[0,\d+.\d+,\d+.\d+\],true\]/, io.read)
+    end
+
+    IO.popen("#{bin}/groonga -n #{testpath}/test-normalizer-mysql.db", "r+") do |io|
+      io.puts "register normalizers/mysql"
+      sleep 2
+      io.puts("shutdown")
+      # expected returned result is like this:
+      # [[0,1447502555.38667,0.000824928283691406],true]\n
+      assert_match(/\[\[0,\d+.\d+,\d+.\d+\],true\]/, io.read)
+    end
+  end
+end
