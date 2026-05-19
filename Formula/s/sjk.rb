e75addc548a9647cf2b237cbdfb7e03a02a33f98
hqzing <hqzing@outlook.com>
2026-05-19 15:47:49 +0800

    sjk: rebuild

--- Formula/s/sjk.rb
+++ Formula/s/sjk.rb
@@ -5,10 +5,6 @@ class Sjk < Formula
   sha256 "6aab07cdf0ecad394e225a1f47d7342cb23bfd8b7d5c65c945f81835363ec937"
   license "Apache-2.0"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cd4f304cd513ee169233e947608d7ec63e22beb05f664c69cd3a7e2b5c41e9d8"
-  end
-
   depends_on "openjdk"
 
   def install
