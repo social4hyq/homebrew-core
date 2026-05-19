4e01a0010f5fe0900e32704b784633bc97a108a2
hqzing <hqzing@outlook.com>
2026-05-19 15:17:15 +0000

    lesspipe 2.25 (new formula)

---
+++ Formula/l/lesspipe.rb
@@ -0,0 +1,44 @@
+class Lesspipe < Formula
+  desc "Input filter for the pager less"
+  homepage "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html"
+  url "https://github.com/wofr06/lesspipe/archive/refs/tags/v2.25.tar.gz"
+  sha256 "fb9d026bdeb81ccdb054c31e292112805ec48b7463978a4cf2af556c7a63487a"
+  license all_of: [
+    "GPL-2.0-only",
+    "GPL-2.0-or-later", # sxw2txt
+    "MIT", # code2color
+    any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"], # vimcolor
+  ]
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e8265110f7141aa5664f5a72b4da3b360c10665ae09ac9b8b644e0f66632085a"
+  end
+
+  uses_from_macos "zsh" => :build # needed to guarantee installation of zsh completions
+  uses_from_macos "perl"
+
+  on_macos do
+    depends_on "bash"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--shell=bash"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      add the following to your shell profile e.g. ~/.profile or ~/.zshrc:
+        export LESSOPEN="|#{HOMEBREW_PREFIX}/bin/lesspipe.sh %s"
+    EOS
+  end
+
+  test do
+    touch "file1.txt"
+    touch "file2.txt"
+    system "tar", "-cvzf", "homebrew.tar.gz", "file1.txt", "file2.txt"
+
+    assert_path_exists testpath/"homebrew.tar.gz"
+    assert_match "file2.txt", pipe_output(bin/"archive_color", shell_output("tar -tvzf homebrew.tar.gz"))
+  end
+end
