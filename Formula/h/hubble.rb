3935816c0222d97ac734dd9003ff32be5114744f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 02:33:31 +0800

    hubble: update 1.19.3 bottle.

--- Formula/h/hubble.rb
+++ Formula/h/hubble.rb
@@ -15,12 +15,7 @@ class Hubble < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7f9c4b9088ebe17273edd057fdda087ed3f59b2f579a6c7568a888c4fb5afd7b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e2b2fe5f6153ae0c17952578d2b30c12373200fcd6b6d98e15cd2b0d94da2f6d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20af894f38f6b6d0514e6e6c45d92ca73c3779c92503c3b195d04ca863140d79"
-    sha256 cellar: :any_skip_relocation, sonoma:        "25adb578d9b3f3694913d6d8be4e97d3a24de147b9985f4a9b8f1baddd070d9d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc27cd20d25364dc397c373179e517450e12e01d5b71a8e870f30bb2977ad341"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6e2f19f31f0f13342c11830c1a84825c2a70ae05a46a661bb395a1441b94e1d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5130ca9f9388d270e6d9b39b80731ffd8f9229206776d90482b0d6863b3843b0"
   end
 
   depends_on "go" => :build
