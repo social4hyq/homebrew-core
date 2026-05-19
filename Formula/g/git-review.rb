12b0351eb9924a0dcc70dbf69c3156c18abc7e1a
hqzing <hqzing@outlook.com>
2026-05-19 15:34:38 +0000

    git-review 2.5.0 (new formula)

---
+++ Formula/g/git-review.rb
@@ -0,0 +1,59 @@
+class GitReview < Formula
+  include Language::Python::Virtualenv
+
+  desc "Submit git branches to gerrit for review"
+  homepage "https://opendev.org/opendev/git-review"
+  url "https://files.pythonhosted.org/packages/da/92/ddc922d34061791a4d0fd483ee4ffc5e026e93783b70fe5a29a129d0cf63/git_review-2.5.0.tar.gz"
+  sha256 "1bcffaef02848a5a3b066e8268c7d700a77cbd8b2e56b128d30f60cd431cf0a8"
+  license "Apache-2.0"
+  revision 6
+  head "https://opendev.org/opendev/git-review.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "cfcbed94f7312a1d87eaddb4151cb6f576f3185874492e290099fe74943a5214"
+  end
+
+  depends_on "certifi"
+  depends_on "python@3.14"
+
+  conflicts_with "gerrit-tools", because: "both install `git-review` binaries"
+
+  pypi_packages exclude_packages: "certifi"
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
+    sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/05/b1/efac073e0c297ecf2fb33c346989a529d4e19164f1759102dee5953ee17e/idna-3.14.tar.gz"
+    sha256 "466d810d7a2cc1022bea9b037c39728d51ae7dad40d480fc9b7d7ecf98ba8ee3"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/43/b8/7a707d60fea4c49094e40262cc0e2ca6c768cca21587e34d3f705afec47e/requests-2.34.0.tar.gz"
+    sha256 "7d62fe92f50eb82c529b0916bb445afa1531a566fc8f35ffdc64446e771b856a"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "git", "init"
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "BrewTestBot@test.com"
+    system "git", "remote", "add", "gerrit", "https://github.com/Homebrew/brew.sh"
+    (testpath/".git/hooks/commit-msg").write "# empty - make git-review happy"
+    (testpath/"foo").write "test file"
+    system "git", "add", "foo"
+    system "git", "commit", "-m", "test"
+    # submit code to `main` branch (default is `master`)
+    system bin/"git-review", "--dry-run", "main"
+  end
+end
