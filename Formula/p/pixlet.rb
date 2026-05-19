d2b84e8d5bd3607786f470b5387ac1dedcb8c632
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 02:43:11 +0800

    pixlet: update 0.52.0 bottle.

--- Formula/p/pixlet.rb
+++ Formula/p/pixlet.rb
@@ -7,12 +7,7 @@ class Pixlet < Formula
   head "https://github.com/tronbyt/pixlet.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "b13307e83629598bf6a43d4e6a7ceda39c8d83d5b7e2f2d4c744b5e49b46be9e"
-    sha256 cellar: :any,                 arm64_sequoia: "cb3afde9aa2fa185eedb1656bda5fb4445baa1878a85583136bc23691cc4364c"
-    sha256 cellar: :any,                 arm64_sonoma:  "0f95bab0dc2c2372baddd8cc40f15a113e5f8fdc05f20188e2519656bfa2c314"
-    sha256 cellar: :any,                 sonoma:        "d454da2ff15e904f3c6f1eb2345d694bba369e6c6dcf9088a6fa254b7acb59d4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3551298753d469e15c6f2a5f88fb88b687c4423a5e6b2f52853aca22e62d9e23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76ccbc5242978bae43e39cd34e276f63b1258bd92dd06b188bd15d45ce5065f7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "768bf6ae8ce2bb3358ea874dd88a1094925e4794856ce5a28592a90176f8f38b"
   end
 
   depends_on "go" => :build
