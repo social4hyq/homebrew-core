4824e676c71a59a3b6a295106e625c0a1f53465f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 00:04:34 +0800

    bom: update 0.7.1 bottle.

--- Formula/b/bom.rb
+++ Formula/b/bom.rb
@@ -7,13 +7,7 @@ class Bom < Formula
   head "https://github.com/kubernetes-sigs/bom.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7b48f971e9f1eb27f2597c5b769d5c5b04c98b4a58e96d2a3518d4a70217a929"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b48f971e9f1eb27f2597c5b769d5c5b04c98b4a58e96d2a3518d4a70217a929"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b48f971e9f1eb27f2597c5b769d5c5b04c98b4a58e96d2a3518d4a70217a929"
-    sha256 cellar: :any_skip_relocation, sonoma:        "24e9021594ab6c2789cbbd4bcdb40d8c5572091da769a155a89e83b577d3e79d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d745d7af5e11523d0717206e9f42265663b3c6b39e5eaa51e366474897d6e140"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afd114ea53280a22f13d028a6c33e61e34193b24b54ac2ecf8e9c66db5e07b2f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "328ac43155f1eac728ae3e5b64ae02f83f723cfe65aaa80f8858a48cbdb573cf"
   end
 
   depends_on "go" => :build
