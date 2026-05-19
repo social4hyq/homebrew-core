c2ba949370ac7d5b44f377cbdcc37a07e6bcd7ae
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 00:59:35 +0800

    grpcui: update 1.4.3 bottle.

--- Formula/g/grpcui.rb
+++ Formula/g/grpcui.rb
@@ -12,14 +12,7 @@ class Grpcui < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "96371f211cdbdcd5d568f474d105235f0b3ebdb624b46af9d29691a7769f9c28"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9ac35799e9b7d1c2a6c9eb8b1672f10ba975d0e32efe2665de2ecdc55d10ceeb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9ac35799e9b7d1c2a6c9eb8b1672f10ba975d0e32efe2665de2ecdc55d10ceeb"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "9ac35799e9b7d1c2a6c9eb8b1672f10ba975d0e32efe2665de2ecdc55d10ceeb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ba7e1920f634f492ee2b36de11763a0dc3b3f16e6c4691ef66bf5d1a6c651a30"
-    sha256 cellar: :any_skip_relocation, ventura:       "ba7e1920f634f492ee2b36de11763a0dc3b3f16e6c4691ef66bf5d1a6c651a30"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "678cb499d5e1ff8120437eb008be019761b5afc74175df5ac292262a09dec25f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a604659d4ba4f40852cc1d2b7924e0540f3e59f9f5628472826dfc6b0022dde"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7fa643da6bc93231810d1d4ce082aed4d5e1384a5f30fa96fb42a7e3e237dcfd"
   end
 
   depends_on "go" => :build
