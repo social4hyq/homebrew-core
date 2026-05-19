6dbb376f29ce41215ba9f2c0e77e716fe68a3c07
hqzing <hqzing@outlook.com>
2026-05-19 14:02:32 +0000

    kerl 4.4.0 (new formula)

---
+++ Formula/k/kerl.rb
@@ -0,0 +1,24 @@
+class Kerl < Formula
+  desc "Easy building and installing of Erlang/OTP instances"
+  homepage "https://github.com/kerl/kerl"
+  url "https://github.com/kerl/kerl/archive/refs/tags/4.4.0.tar.gz"
+  sha256 "0f32eb08172baffdca9264c5626f6d7fd650369365079fe21f8b8ab997885d8c"
+  license "MIT"
+  head "https://github.com/kerl/kerl.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9e44f6243554ba4b4ff9c6e84a77328a7fd893d842786f402d1a03dde413b3e9"
+  end
+
+  def install
+    bin.install "kerl"
+
+    bash_completion.install "bash_completion/kerl"
+    zsh_completion.install "zsh_completion/_kerl"
+    fish_completion.install "fish_completion/kerl.fish"
+  end
+
+  test do
+    system bin/"kerl", "list", "releases"
+  end
+end
