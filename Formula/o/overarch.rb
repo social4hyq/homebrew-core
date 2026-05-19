afb740d423e64871ab79faa8d078e9621f79bc70
hqzing <hqzing@outlook.com>
2026-05-19 14:04:08 +0000

    overarch 0.41.0 (new formula)

---
+++ Formula/o/overarch.rb
@@ -0,0 +1,73 @@
+class Overarch < Formula
+  desc "Data driven description of software architecture"
+  homepage "https://github.com/soulspace-org/overarch"
+  url "https://github.com/soulspace-org/overarch/releases/download/v0.41.0/overarch.jar"
+  sha256 "f76f89e2cb067832d508836fe21e04e7c3837291d5f52575fd3f5980595990e0"
+  license "EPL-1.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c1d96dbc1d9914297156de3e30c1a93d5854deb1b81339b0cf1870313920e97d"
+  end
+
+  head do
+    url "https://github.com/soulspace-org/overarch.git", branch: "main"
+    depends_on "leiningen" => :build
+  end
+
+  depends_on "openjdk"
+
+  def install
+    if build.head?
+      system "lein", "uberjar"
+      jar = "target/overarch.jar"
+    else
+      jar = "overarch.jar"
+    end
+
+    libexec.install jar
+    bin.write_jar_script libexec/"overarch.jar", "overarch"
+  end
+
+  test do
+    (testpath/"test.edn").write <<~EOS
+      \#{
+        {:el :person
+         :id :test-customer}
+        {:el :system
+         :id :test-system}
+        {:el :rel
+         :id :customer-uses-system
+         :from :test-customer
+         :to :test-system}
+        {:el :context-view
+         :id :test-context-view
+         :ct [
+              {:ref :test-customer}
+              {:ref :test-system}
+              {:ref :customer-uses-system}]}
+        {:el :container-view
+         :id :test-container-view
+         :ct [
+              {:ref :test-customer}
+              {:ref :test-system}
+              {:ref :customer-uses-system}]}}
+    EOS
+    expected = <<~EOS.chomp
+      Model Warnings:
+      {:build-problems (),
+       :unresolved-refs-in-views (),
+       :unresolved-refs-in-relations ()}
+      Model Information:
+      {:nodes-by-type-count {:person 1, :system 1},
+       :nodes-count 2,
+       :views-by-type-count {:container-view 1, :context-view 1},
+       :relations-by-type-count {:rel 1},
+       :views-count 2,
+       :elements-by-namespace-count {nil 3},
+       :relations-count 1,
+       :synthetic-count {:normal 3},
+       :external-count {:internal 3}}
+    EOS
+    assert_equal expected, shell_output("#{bin}/overarch --model-dir=#{testpath} --model-info").chomp
+  end
+end
