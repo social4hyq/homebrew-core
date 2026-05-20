c02358459028853453582d3be530a0eb7eb2b44d
hqzing <hqzing@outlook.com>
2026-05-20 19:14:11 +0000

    gitter-cli 0.8.5 (new formula)

---
+++ Formula/g/gitter-cli.rb
@@ -0,0 +1,23 @@
+class GitterCli < Formula
+  desc "Extremely simple Gitter client for terminals"
+  homepage "https://github.com/RodrigoEspinosa/gitter-cli"
+  url "https://github.com/RodrigoEspinosa/gitter-cli/archive/refs/tags/v0.8.5.tar.gz"
+  sha256 "c4e335620fc1be50569f3b7543c28ba2c6121b1c7e6d041464b29a31b3d84c25"
+  license "MIT"
+
+  bottle do
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, all: "fc3d9fc87869b22fb03bd8a53405527174c3a9cc910332d8c906dfd79b34ded5"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match "access token", pipe_output("#{bin}/gitter-cli authorize")
+  end
+end
