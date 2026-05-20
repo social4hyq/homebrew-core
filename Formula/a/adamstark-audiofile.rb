4da7e8752482465fb2704908d1f7e9402852d409
hqzing <hqzing@outlook.com>
2026-05-20 16:36:06 +0000

    adamstark-audiofile 1.1.4 (new formula)

---
+++ Formula/a/adamstark-audiofile.rb
@@ -0,0 +1,33 @@
+class AdamstarkAudiofile < Formula
+  desc "C++ Audio File Library by Adam Stark"
+  homepage "https://github.com/adamstark/AudioFile"
+  url "https://github.com/adamstark/AudioFile/archive/refs/tags/1.1.4.tar.gz"
+  sha256 "e3749f90a9356b5206ef8928fa0a9c039e7db49e46bb7f32c3963d6c44c5bea8"
+  license "MIT"
+  head "https://github.com/adamstark/AudioFile.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "dce0123d95e01e4609051018ea590c2811908a0e75cb97f7c445c491d21de87e"
+  end
+
+  def install
+    include.install "AudioFile.h"
+  end
+
+  test do
+    (testpath/"audiofile.cc").write <<~CPP
+      #include "AudioFile.h"
+      int main(int argc, char* *argv) {
+        AudioFile<double> audioFile;
+        AudioFile<double>::AudioBuffer abuf;
+        return 0;
+      }
+    CPP
+
+    system ENV.cxx, "-std=c++17",
+           "-I#{include}",
+           "-o", "audiofile",
+           "audiofile.cc"
+    system "./audiofile"
+  end
+end
