356eaea0742db37242e7c762417e219a00fd1d42
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 11:04:57 +0800

    firefly: update 1.4.0 bottle.

--- Formula/f/firefly.rb
+++ Formula/f/firefly.rb
@@ -12,13 +12,7 @@ class Firefly < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "adbf91b1583f58b9cdf99e5b5f4702e7e382b68678d1a65038dac45a79d266bf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e65c9e835856a687ce4758d8de149fbec354041ef361e19bb16a29b44a55c86f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dde58aad5d6ce8e08b375223c5632a992f15dc010ff73db649d00f5d5fc94957"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9e58a9911ab5d61f995508662d10c94c6f75f9d4dabc6297e8e830b76130fbb9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8487d1cff3e216289b07a109fd41186c425d789eed7921e354c41a2d10bbbe60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14926594845c1055febc3c85ac56b950804433b2cd42014731881fe414bcf6c6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a470016308cac24a8227af27ca07fd537f38c196e6a32fcbb2d1cb6b4325df22"
   end
 
   depends_on "go" => :build
