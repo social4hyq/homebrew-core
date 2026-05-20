63d9fab7662b0e15b9faf7d24c770ce7d10e23f5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 20:39:36 +0800

    brook: update 20260101 bottle.

--- Formula/b/brook.rb
+++ Formula/b/brook.rb
@@ -7,12 +7,7 @@ class Brook < Formula
   head "https://github.com/txthinking/brook.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4fc892393fb319b3573da8caf28e1a2083c052653375c1148af3cc40b482e30e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4fc892393fb319b3573da8caf28e1a2083c052653375c1148af3cc40b482e30e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4fc892393fb319b3573da8caf28e1a2083c052653375c1148af3cc40b482e30e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "dfeb21863c273b142f342a69ee50ed785bc6e738623dcc8a8d5ebba8edcce716"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a9ed2618dc37deddbd6030631cf0130859fe4c496b683377643bddd926af182"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d8008603eeef8439794732a85541831c398010f9794f2224fb4f8159629e9c9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0ebe2a5a1ef395c7118f1892e78122d9889a0b4aa52c6015501969722598d345"
   end
 
   depends_on "go" => :build
