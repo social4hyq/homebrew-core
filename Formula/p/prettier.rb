e14b3571c8ca8573d5d2cf544b5a45c41f22bd6a
hqzing <hqzing@outlook.com>
2026-05-20 11:11:37 +0000

    prettier 3.8.3 (new formula)

---
+++ Formula/p/prettier.rb
@@ -0,0 +1,25 @@
+class Prettier < Formula
+  desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
+  homepage "https://prettier.io/"
+  url "https://registry.npmjs.org/prettier/-/prettier-3.8.3.tgz"
+  sha256 "c8a850e71b7366f1bac1885e05b2f929d47168009f3b6914413902ca8b980fde"
+  license "MIT"
+  head "https://github.com/prettier/prettier.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "84ec5b4eb6051f6b0f4105ac4e08bdc2344d99524ee2bafd9d886dc1d999a647"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    (testpath/"test.js").write("const arr = [1,2];")
+    output = shell_output("#{bin}/prettier test.js")
+    assert_equal "const arr = [1, 2];", output.chomp
+  end
+end
