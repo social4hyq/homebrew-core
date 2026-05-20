f564b964f3ab84e2c7c607bf94e4b45942155992
hqzing <hqzing@outlook.com>
2026-05-20 17:43:22 +0000

    git-gerrit 0.3.0 (new formula)

---
+++ Formula/g/git-gerrit.rb
@@ -0,0 +1,25 @@
+class GitGerrit < Formula
+  desc "Gerrit code review helper scripts"
+  homepage "https://github.com/fbzhong/git-gerrit"
+  url "https://github.com/fbzhong/git-gerrit/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "433185315db3367fef82a7332c335c1c5e0b05dabf8d4fbeff9ecf6cc7e422eb"
+  license "BSD-3-Clause"
+  head "https://github.com/fbzhong/git-gerrit.git", branch: "master"
+
+  bottle do
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, all: "2b2647c6487fc6987733e3ec0d88f6c3cb87013244b69d91027355548868cf35"
+  end
+
+  conflicts_with "gerrit-tools", because: "both install `gerrit-cherry-pick` binaries"
+
+  def install
+    prefix.install "bin"
+    bash_completion.install "completion/git-gerrit-completion.bash" => "git-gerrit"
+  end
+
+  test do
+    system "git", "init"
+    system "git", "gerrit", "help"
+  end
+end
