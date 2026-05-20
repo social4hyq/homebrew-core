425e76c4e275caf76cd372bc692e068d9cc2e276
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 11:47:14 +0800

    atf: update 0.23 bottle.

--- Formula/a/atf.rb
+++ Formula/a/atf.rb
@@ -12,14 +12,7 @@ class Atf < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "acbf43372e23b1ec798d9220e6035f76a8d42d61af90ab40c43b32dc90b22745"
-    sha256 arm64_sequoia: "14d17d25d50313d4fbd2794874b972925f23c67dea567ae1ec692efd32c2647f"
-    sha256 arm64_sonoma:  "b8f3e848fb9147744c021fae004abe9cb8ac262c94d6bcd4a487b79ea73e6cee"
-    sha256 arm64_ventura: "b0e02364b9ab0b317420b1ed1045e9c701ad002653b9180832a18e29c5c542ec"
-    sha256 sonoma:        "9cd739eb6f0f1ec5c7a7718c66fea9ea8a3435ffd3506da93d60393f0bda5f0d"
-    sha256 ventura:       "f454fa5d1f845261d605e9d94a926b6cd4e3a120a6782fe0f93776ab01697df6"
-    sha256 arm64_linux:   "5b259959f57b981567bc83a2889401ef8224bc504e46428a62569cb3eebb054a"
-    sha256 x86_64_linux:  "9e2c5311676f2350c618ba8b2092a67df2fa2300c1212d1f25bd70f4650ea934"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "de0b68beae3b78786aed4202ca48e48a8bc01e3fc307bf9c994b7e80ab9ed3b4"
   end
 
   depends_on "autoconf" => :build
