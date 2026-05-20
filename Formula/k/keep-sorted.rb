9c43c1bdf586a4cbc1be31d5c1208f8a547353c9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 10:09:36 +0800

    keep-sorted: update 0.8.0 bottle.

--- Formula/k/keep-sorted.rb
+++ Formula/k/keep-sorted.rb
@@ -8,12 +8,7 @@ class KeepSorted < Formula
   head "https://github.com/google/keep-sorted.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a332a0e53eb63b14eed0fd1c573ee8eab1c1e64aed98334ca98ea011c5feb5aa"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a332a0e53eb63b14eed0fd1c573ee8eab1c1e64aed98334ca98ea011c5feb5aa"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a332a0e53eb63b14eed0fd1c573ee8eab1c1e64aed98334ca98ea011c5feb5aa"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ea664b552fd616f7feb5073b16df66f597cd27cffa45e4aa3d28c4c1e44ff82b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1961c31d26427ccead20527f1fc7b8da6b6aadd3c5838caf251a0d119ffba53c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eab88ac9736e72dcfc0a386d64ca5492200f5962fe136fe1d5f7db12228791f6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8a4f9c59a48b7be251acbf5b5335b15c637df4c743562c955b46c9f14ed77338"
   end
 
   depends_on "go" => :build
