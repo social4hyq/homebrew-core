30818d2f6b11ae5cbceecac9d25d22424a861bec
hqzing <hqzing@outlook.com>
2026-05-20 22:09:06 +0000

    spectral-cli 6.16.0 (new formula)

---
+++ Formula/s/spectral-cli.rb
@@ -0,0 +1,43 @@
+class SpectralCli < Formula
+  desc "JSON/YAML linter and support OpenAPI v3.1/v3.0/v2.0, and AsyncAPI v2.x"
+  homepage "https://stoplight.io/open-source/spectral"
+  url "https://registry.npmjs.org/@stoplight/spectral-cli/-/spectral-cli-6.16.0.tgz"
+  sha256 "05da62e983a5e9f4bbe7f1af06ad7212a2cf4213d096f92f10d8544c86fa2f13"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "fe0de1191ffcd935d7621651f379251f0be2bdfe0c5aa6d061657ccfe734db53"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args(ignore_scripts: false)
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    resource "homebrew-petstore.yaml" do
+      url "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/b12acf0c/examples/v3.0/petstore.yaml"
+      sha256 "7dc119919441597e2b24335d8c8f6d01f1f0b895637f79b35e3863a3c2df9ddf"
+    end
+
+    resource "homebrew-streetlights-mqtt.yml" do
+      url "https://raw.githubusercontent.com/asyncapi/spec/1824379b/examples/streetlights-mqtt.yml"
+      sha256 "7e17c9b465437a5a12decd93be49e37ca7ecfc48ff6f10e830d8290e9865d3af"
+    end
+
+    test_config = testpath/".spectral.yaml"
+    test_config.write "extends: [\"spectral:oas\", \"spectral:asyncapi\"]"
+
+    testpath.install resource("homebrew-petstore.yaml")
+    output = shell_output("#{bin}/spectral lint -r #{test_config} #{testpath}/petstore.yaml")
+    assert_match "8 problems (0 errors, 8 warnings, 0 infos, 0 hints)", output
+
+    testpath.install resource("homebrew-streetlights-mqtt.yml")
+    output = shell_output("#{bin}/spectral lint -r #{test_config} #{testpath}/streetlights-mqtt.yml")
+    assert_match "7 problems (0 errors, 6 warnings, 1 info, 0 hints)", output
+
+    assert_match version.to_s, shell_output("#{bin}/spectral --version")
+  end
+end
