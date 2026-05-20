e6b5e4ec9f997cab1dee1393d5d54556af988588
hqzing <hqzing@outlook.com>
2026-05-20 21:16:47 +0000

    python-argcomplete 3.6.3 (new formula)

---
+++ Formula/p/python-argcomplete.rb
@@ -0,0 +1,39 @@
+class PythonArgcomplete < Formula
+  include Language::Python::Virtualenv
+
+  desc "Tab completion for Python argparse"
+  homepage "https://kislyuk.github.io/argcomplete/"
+  url "https://files.pythonhosted.org/packages/38/61/0b9ae6399dd4a58d8c1b1dc5a27d6f2808023d0b5dd3104bb99f45a33ff6/argcomplete-3.6.3.tar.gz"
+  sha256 "62e8ed4fd6a45864acc8235409461b72c9a28ee785a2011cc5eb78318786c89c"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3cbfebfe5fc664a233aec15fcd7049e5ded3cd8d69b7ba58bce32e084e2a2db7"
+  end
+
+  deprecate! date: "2026-02-13", because: "does not meet homebrew/core's requirements for Python library formulae"
+  disable! date: "2026-08-13", because: "does not meet homebrew/core's requirements for Python library formulae"
+
+  depends_on "python@3.14"
+
+  def install
+    virtualenv_install_with_resources
+
+    # Bash completions are not compatible with Bash 3 so don't use v1 directory.
+    # Ref: https://kislyuk.github.io/argcomplete/#global-completion
+    bash_completion_script = "argcomplete/bash_completion.d/_python-argcomplete"
+    (share/"bash-completion/completions").install bash_completion_script => "python-argcomplete"
+    zsh_completion.install_symlink bash_completion/"python-argcomplete" => "_python-argcomplete"
+
+    # Build an `:all` bottle by replacing comments
+    site_packages = libexec/Language::Python.site_packages("python3")
+    inreplace site_packages/"argcomplete-#{version}.dist-info/METADATA",
+              "/opt/homebrew/bin/bash",
+              "$HOMEBREW_PREFIX/bin/bash"
+  end
+
+  test do
+    output = shell_output("#{bin}/register-python-argcomplete foo")
+    assert_match "_python_argcomplete foo", output
+  end
+end
