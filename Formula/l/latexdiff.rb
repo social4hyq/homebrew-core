aedb44d161aa3a81b79f676f3fa853d20747ca84
hqzing <hqzing@outlook.com>
2026-05-20 10:53:24 +0000

    latexdiff 1.4.0 (new formula)

---
+++ Formula/l/latexdiff.rb
@@ -0,0 +1,50 @@
+class Latexdiff < Formula
+  desc "Compare and mark up LaTeX file differences"
+  homepage "https://www.ctan.org/pkg/latexdiff"
+  url "https://github.com/ftilmann/latexdiff/releases/download/1.4.0/latexdiff-1.4.0.tar.gz"
+  sha256 "2aaea10ae1a9c1b975c9dc933da6ac6f83f665eea76b358daf86b97b606912ea"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "7d747cedc4241a804cc7280b7605d20853ddfdbad574b7ee90fabca194f23152"
+  end
+
+  uses_from_macos "perl"
+
+  def install
+    bin.install %w[latexdiff-fast latexdiff-so latexdiff-vc latexrevise]
+    man1.install %w[latexdiff-vc.1 latexdiff.1 latexrevise.1]
+    doc.install Dir["doc/*"]
+    pkgshare.install %w[contrib example]
+
+    # Install latexdiff-so (with inlined Algorithm::Diff) as the
+    # preferred version, more portable
+    bin.install_symlink "latexdiff-so" => "latexdiff"
+  end
+
+  test do
+    (testpath/"test1.tex").write <<~'TEX'
+      \documentclass{article}
+      \begin{document}
+      Hello, world.
+      \end{document}
+    TEX
+
+    (testpath/"test2.tex").write <<~'TEX'
+      \documentclass{article}
+      \begin{document}
+      Goodnight, moon.
+      \end{document}
+    TEX
+
+    expect = /^\\DIFdelbegin \s+
+             \\DIFdel      \{ Hello,[ ]world \}
+             \\DIFdelend   \s+
+             \\DIFaddbegin \s+
+             \\DIFadd      \{ Goodnight,[ ]moon \}
+             \\DIFaddend   \s+
+             \.$/x
+    assert_match expect, shell_output("#{bin}/latexdiff test[12].tex")
+  end
+end
