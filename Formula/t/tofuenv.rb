20a8d727c9ea6e964d38e7e13ba06534f72cd054
hqzing <hqzing@outlook.com>
2026-05-20 11:31:11 +0000

    tofuenv 1.0.7 (new formula)

---
+++ Formula/t/tofuenv.rb
@@ -0,0 +1,32 @@
+class Tofuenv < Formula
+  desc "OpenTofu version manager inspired by tfenv"
+  homepage "https://tofuutils.github.io/tofuenv/"
+  url "https://github.com/tofuutils/tofuenv/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "047c6a01a0d4c7ded2cf126ae1e891bb3479b2544ec2d2f0d3951de2d08f6c7d"
+  license "MIT"
+  head "https://github.com/tofuutils/tofuenv.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3febcab9a632034fdedf2d4ae0105d4425bcdb12ac73e58b586a1ddf6db36487"
+  end
+
+  uses_from_macos "jq", since: :sequoia
+  uses_from_macos "unzip"
+
+  on_macos do
+    depends_on "grep"
+  end
+
+  conflicts_with "opentofu", "tenv", because: "both install tofu binary"
+
+  def install
+    prefix.install %w[bin lib libexec share]
+  end
+
+  test do
+    tf_version = shell_output("#{bin}/tofuenv list-remote").lines.first.chomp
+    with_env(TOFUENV_TOFU_VERSION: tf_version, TF_AUTO_INSTALL: "false") do
+      assert_equal tf_version, shell_output("#{bin}/tofuenv version-name").strip
+    end
+  end
+end
