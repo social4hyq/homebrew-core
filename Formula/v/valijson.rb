ac70bbf398a44dca1aff3a1f0c6e396addf4c96f
hqzing <hqzing@outlook.com>
2026-05-20 19:16:47 +0000

    valijson 1.1.0 (new formula)

---
+++ Formula/v/valijson.rb
@@ -0,0 +1,33 @@
+class Valijson < Formula
+  desc "Header-only C++ library for JSON Schema validation"
+  homepage "https://github.com/tristanpenman/valijson"
+  url "https://github.com/tristanpenman/valijson/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "bb37d86f5fe78f559f108517f30ce587c960ea5bd23d71413b7493cda6c3a4cc"
+  license "BSD-2-Clause"
+  revision 1
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4298a22e52b33e0d97bfa5b5d0e1ff6e25e8938aa140911ba28023dcc72664d7"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "jsoncpp" => :test
+
+  def install
+    system "cmake", " -S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <valijson/schema.hpp>
+      #include <valijson/adapters/jsoncpp_adapter.hpp>
+      #include <valijson/utils/jsoncpp_utils.hpp>
+
+      int main (void) { std::cout << "Hello world"; }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++17", "-L#{Formula["jsoncpp"].opt_lib}", "-ljsoncpp", "-o", "test"
+    system "./test"
+  end
+end
