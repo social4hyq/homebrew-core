d74a31895a88e62d09d1e1025b5007b619d8f170
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 20:48:19 +0800

    gauth: update 1.5.0 bottle.

--- Formula/g/gauth.rb
+++ Formula/g/gauth.rb
@@ -7,14 +7,7 @@ class Gauth < Formula
   head "https://github.com/pcarrier/gauth.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e07c7cf508ea13a153defdeb3c81b662d5672d7572edecac06920a093cecb1ff"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0d91ac9029a1df98f1c2a3b5318f364c4b826c3f267e8d420fa389b5fc536dd1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0d91ac9029a1df98f1c2a3b5318f364c4b826c3f267e8d420fa389b5fc536dd1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0d91ac9029a1df98f1c2a3b5318f364c4b826c3f267e8d420fa389b5fc536dd1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "51e2838fc31e6a7dd13880b2f95ef69d7732e8db0832feeec7f30b6ec18354b1"
-    sha256 cellar: :any_skip_relocation, ventura:       "51e2838fc31e6a7dd13880b2f95ef69d7732e8db0832feeec7f30b6ec18354b1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f1e7e417bad6562df6256368f6ec1c99816222be6dd5e05af4c600cd38509469"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f0fe17b342e5ff16879bea09f90bdf1ab70f33a9dea3b3ba9fbf3cfbf47028d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c865fbb3350939927e7d022dfd4b414e21948e31437e60d61e244f0345c796b0"
   end
 
   depends_on "go" => :build
