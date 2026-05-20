b7820d95ac6130cc6f6634b245639426b11401e1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 10:01:38 +0800

    topydo: update 0.16 bottle.

--- Formula/t/topydo.rb
+++ Formula/t/topydo.rb
@@ -8,12 +8,7 @@ class Topydo < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0f4329353c1d1fb2e9b6d08296304d03763fe1a3339626fa2aeaaf4d85d09b72"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b2ab91f9a1c6d364941ea3917da7176cd447d496cf37776fec1bd0680aef74b2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "98b6180f111937e21f5b7b625cda75146db1fe661af6a8687912e2fbc73430bb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "996cc264fc9ff19db29b8b7355585a79d9451ca4bb2e73ff0076eac88b35eb9d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3285119c254079b0d9e1ea4080664c4bc08e7f819734b34544dc8cdef70f8dda"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3285119c254079b0d9e1ea4080664c4bc08e7f819734b34544dc8cdef70f8dda"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7d6a04d64e48387a3a47ed16b0ae2fd4cc739ed641739e9ca632532d0961bbbd"
   end
 
   depends_on "python@3.14"
