cf3325f1b92cbcde71916ef38a4a2545c8ed077a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 20:56:19 +0800

    roll: update 2.7.0 bottle.

--- Formula/r/roll.rb
+++ Formula/r/roll.rb
@@ -6,12 +6,7 @@ class Roll < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c7b7d5f5d981510645495a608c12615909ffa3b9b3fc6ba5e9faf5fdacb6c510"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9a9d4588c91eb0325b8601960a763f76232ecb28c866ef5b87050b7f3bbb72b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b7d2ac3789b181f01e5e4cd5bd914df40dcbb43f133be87a7320a445011ba5c2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7cc13149f0571a32c72e58ff8bd306a4bc1c0401ec6e4cf084ab6d198178ffde"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "caa11bc9eac9982f9e6a17ac7e28af140533f97e84faff5022e5757829682e0a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc292cc25912ad6b5282b6f12d418b5a097cabb71fe41f098bc03243ed9eadad"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2ea2531b3d6d40cf4e02a047774182e2d0be6a90a0417c52d4f2ecbc010c9f65"
   end
 
   head do
