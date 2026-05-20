382d40b989855fbba0eb57565371162b16731eee
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 09:57:25 +0800

    xlsxio: update 0.2.36 bottle.

--- Formula/x/xlsxio.rb
+++ Formula/x/xlsxio.rb
@@ -7,12 +7,7 @@ class Xlsxio < Formula
   head "https://github.com/brechtsanders/xlsxio.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5dcc3ae7f8963c3ee9f56bb2117d666d089e40b801edd882f77d5281ca5c7001"
-    sha256 cellar: :any,                 arm64_sequoia: "9181031f8b1370139f5cd5c40698ad29e63d2e907181385c237be46c6a7789e6"
-    sha256 cellar: :any,                 arm64_sonoma:  "309fd1194f2edab285b7e492816f61e8fbb342c5ccade5d2613220f6118d48a0"
-    sha256 cellar: :any,                 sonoma:        "20a7d4de3d612257ef01593a42db2609dedb3a6e883b65f7cf7b49019bbc9341"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bed81fdea13430eae538e69af3e1d984e0a275b71aaacf904abf8f8a1d810dd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a8fe52036112b2ed8497ea996965894e3be4eab10c17dbcf106ff46a0f116a0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d880aa0d5430eb60825b760a84edac1c49537d7b72f2718d72312a560d9bd66e"
   end
 
   depends_on "libzip"
