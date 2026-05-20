cc0090ecd5f193f58b4e1344f70b20b4657dae4d
hqzing <hqzing@outlook.com>
2026-05-20 11:43:53 +0000

    luajit 2.1.1774896198 (new formula)

---
+++ Formula/l/luajit.rb
@@ -0,0 +1,105 @@
+# NOTE: We have a policy of building only from tagged commits, but make a
+#       singular exception for luajit. This exception will not be extended
+#       to other formulae. See:
+#       https://github.com/Homebrew/homebrew-core/pull/99580
+# TODO: Add an audit in `brew` for this. https://github.com/Homebrew/homebrew-core/pull/104765
+class Luajit < Formula
+  desc "Just-In-Time Compiler (JIT) for the Lua programming language"
+  homepage "https://luajit.org/luajit.html"
+  # Update this to the tip of the `v2.1` branch.
+  # Get the latest commit with:
+  #   `git ls-remote --heads https://github.com/LuaJIT/LuaJIT.git v2.1`
+  # This is a rolling release model so take care not to ignore CI failures that may be regressions.
+  url "https://github.com/LuaJIT/LuaJIT/archive/18b087cd2cd4ddc4a79782bf155383a689d5093d.tar.gz"
+  # Use the version scheme `2.1.timestamp` where `timestamp` is the Unix timestamp of the
+  # latest commit at the time of updating.
+  # `brew livecheck luajit` will generate the correct version for you automatically.
+  version "2.1.1774896198"
+  sha256 "88a592afa9907d6b0c6e1e7ac9b39982622e3ca086f0646d4ea89b0e4e81f093"
+  license "MIT"
+  compatibility_version 7
+  head "https://github.com/LuaJIT/LuaJIT.git", branch: "v2.1"
+
+  livecheck do
+    url "https://api.github.com/repos/LuaJIT/LuaJIT/branches/v2.1"
+    strategy :json do |json|
+      date = json.dig("commit", "commit", "author", "date")
+      "2.1.#{DateTime.parse(date).strftime("%s")}"
+    end
+  end
+
+  no_autobump! because: :incompatible_version_format
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "0cf1bf9dc443912fe1d129c3dafd807f0862c56510a14f371ef4cce1f06cf693"
+    sha256 cellar: :any,                 arm64_sequoia: "ddb4848c243b68cebd8546fb96000fcd57f32760f4de5903ae3bc8734e785a1e"
+    sha256 cellar: :any,                 arm64_sonoma:  "2b9dd489a6f593d18a3ae38311815587217c3a503f5c3601eed1433742298295"
+    sha256 cellar: :any,                 sonoma:        "8cb8f4baf2c5a00aad2044b3de9f0273089e8c32c0522c96c109f3bc6b8bc2ca"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a97af5d4ff135e526da7da8eea141f23fc7600f5824c5545f3097f1cdb427424"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5444e91b64efa53f527302dc914aa5dfd3d08518084b4149f872badc2758c3f3"
+  end
+
+  def install
+    # 1 - Override the hardcoded gcc.
+    # 2 - Remove the "-march=i686" so we can set the march in cflags.
+    # Both changes should persist and were discussed upstream.
+    inreplace "src/Makefile" do |f|
+      f.change_make_var! "CC", ENV.cc
+      f.gsub!(/-march=\w+\s?/, "")
+    end
+
+    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+    # is not set then it's forced to 10.4, which breaks compile on Mojave.
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s if OS.mac?
+
+    # Help the FFI module find Homebrew-installed libraries.
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath(target: HOMEBREW_PREFIX/"lib")}"
+
+    # Pass `Q= E=@:` to build verbosely.
+    verbose_args = %w[Q= E=@:]
+
+    # Build with PREFIX=$HOMEBREW_PREFIX so that luajit can find modules outside its own keg.
+    # This allows us to avoid having to set `LUA_PATH` and `LUA_CPATH` for non-vendored modules.
+    system "make", "amalg", "PREFIX=#{HOMEBREW_PREFIX}", *verbose_args
+    system "make", "install", "PREFIX=#{prefix}", *verbose_args
+    doc.install (buildpath/"doc").children
+
+    # LuaJIT doesn't automatically symlink unversioned libraries:
+    # https://github.com/Homebrew/homebrew/issues/45854.
+    lib.install_symlink lib/shared_library("libluajit-5.1") => shared_library("libluajit")
+    lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
+
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+    end
+  end
+
+  test do
+    assert_includes shell_output("#{bin}/luajit -v"), " #{version} "
+
+    system bin/"luajit", "-e", <<~EOS
+      local ffi = require("ffi")
+      ffi.cdef("int printf(const char *fmt, ...);")
+      ffi.C.printf("Hello %s!\\n", "#{ENV["USER"]}")
+    EOS
+
+    # Check that LuaJIT can find its own `jit.*` modules
+    touch "empty.lua"
+    system bin/"luajit", "-b", "-o", "osx", "empty.lua", "empty.o"
+    assert_path_exists testpath/"empty.o"
+
+    # Check that we're not affected by LuaJIT/LuaJIT/issues/865.
+    require "macho"
+    machobj = MachO.open("empty.o")
+    # always generate 64 bit non-FAT Mach-O object files
+    # per https://github.com/LuaJIT/LuaJIT/commit/7110b935672489afd6ba3eef3e5139d2f3bd05b6
+    assert_kind_of MachO::MachOFile, machobj
+    assert_predicate machobj, :object?
+    assert_equal Hardware::CPU.arch, machobj.cputype
+  end
+end
