9b646ed435b34a9118e81e8ebd67a9a5892abff5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 20:21:18 +0800

    kekkai: update 0.2.7 bottle.

--- Formula/k/kekkai.rb
+++ Formula/k/kekkai.rb
@@ -7,12 +7,7 @@ class Kekkai < Formula
   head "https://github.com/catatsuy/kekkai.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "54d2e7c252146a68b0a8cf26388ffab317bc80b9ae36d7ddc7f415e3d71bf39a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54d2e7c252146a68b0a8cf26388ffab317bc80b9ae36d7ddc7f415e3d71bf39a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54d2e7c252146a68b0a8cf26388ffab317bc80b9ae36d7ddc7f415e3d71bf39a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b3d91d7f52f339ff6dc783a21b6883a5f6b430e7f126bdb88c72ae27d40c5c61"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "76c30905742e5c0762be30d8b6e8f5327924f091f162bc6933c9249ff0dbfd4f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ccb1bdee3af1e45115ebd8010258d8426f24f41af9da6afec99a1c4b641f3142"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e29aa06b8b7a4619fd1f4c5f7077f74dd837adc03757f88ec5d2f8f81197606b"
   end
 
   depends_on "go" => :build
