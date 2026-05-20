53e15d0d0e7c44ae58a4fbc9121ac9b889ce83b8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 20:58:20 +0800

    dispenso: update 1.5.1 bottle.

--- Formula/d/dispenso.rb
+++ Formula/d/dispenso.rb
@@ -6,12 +6,7 @@ class Dispenso < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "cfa3dc2188a6867fa7e7c27cbc0761f6683f8c941a86bbc1e484626cc99b8578"
-    sha256 cellar: :any,                 arm64_sequoia: "4281d2b609a86c254cbbcdbb351e603b85b70748da2954c1810781a1aeb67769"
-    sha256 cellar: :any,                 arm64_sonoma:  "efb4f85c4d8aaaf96666934346cf4ee46cf000a60df21783827b85186a30f0a5"
-    sha256 cellar: :any,                 sonoma:        "72c83dc6993fdd5a3fd38a0e8fab343bb774e7254157b3a8b68d047fb12a9fb4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "43f84d73d3c3f6febd9211ef6e9e8bed6b8a492ecdae7067be45e0f414747078"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b75186ae2034cfd299c92bffc56fab21abc14f800ffa07b784266a62d5331b8e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c03b0d512b5a4083ddc778effb9444cc17135ff155190898663c9f67a7049d2e"
   end
 
   depends_on "cmake" => [:build, :test]
