988423318204ff695b29dcc7714947351f22092d
hqzing <hqzing@outlook.com>
2026-05-20 17:23:29 +0000

    eslint 10.4.0 (new formula)

---
+++ Formula/e/eslint.rb
@@ -0,0 +1,29 @@
+class Eslint < Formula
+  desc "AST-based pattern checker for JavaScript"
+  homepage "https://eslint.org"
+  url "https://registry.npmjs.org/eslint/-/eslint-10.4.0.tgz"
+  sha256 "6aafcae1290de51bc953bda12abf7fc343f6aee420820a60dc67450681080a4c"
+  license "MIT"
+  head "https://github.com/eslint/eslint.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "6b655271900a7b36be3cf2dd192a91c3127d0ebb0914683e98ff014ec9a7544a"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    # https://eslint.org/docs/latest/use/configure/configuration-files#configuration-file
+    (testpath/"eslint.config.js").write("{}") # minimal config
+    (testpath/"syntax-error.js").write("{}}")
+
+    # https://eslint.org/docs/user-guide/command-line-interface#exit-codes
+    output = shell_output("#{bin}/eslint syntax-error.js", 1)
+    assert_match "Unexpected token }", output
+  end
+end
