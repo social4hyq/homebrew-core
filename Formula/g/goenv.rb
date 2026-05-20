f0717cac208e5d6894f63b321a15c2befa78e21f
hqzing <hqzing@outlook.com>
2026-05-20 17:26:48 +0000

    goenv 2.2.39 (new formula)

---
+++ Formula/g/goenv.rb
@@ -0,0 +1,38 @@
+class Goenv < Formula
+  desc "Go version management"
+  homepage "https://github.com/go-nv/goenv"
+  url "https://github.com/go-nv/goenv/archive/refs/tags/2.2.39.tar.gz"
+  sha256 "9c5571e731c1bbf4f6cc1d3606da1960ba56cf0ce1206c58a74e52e6df430e35"
+  license "MIT"
+  version_scheme 1
+  head "https://github.com/go-nv/goenv.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+  end
+
+  def install
+    inreplace_files = [
+      "libexec/goenv",
+      "plugins/go-build/install.sh",
+      "test/goenv.bats",
+      "test/test_helper.bash",
+      "plugins/go-build/test/test_helper.bash",
+    ]
+    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
+
+    prefix.install Dir["*"]
+    %w[goenv-install goenv-uninstall go-build].each do |cmd|
+      bin.install_symlink "#{prefix}/plugins/go-build/bin/#{cmd}"
+    end
+  end
+
+  test do
+    assert_match "Usage: goenv <command> [<args>]", shell_output("#{bin}/goenv help")
+  end
+end
