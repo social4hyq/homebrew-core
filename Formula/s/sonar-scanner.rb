2dd4d703839f279ec1182a9262ffb6887d88adb3
hqzing <hqzing@outlook.com>
2026-05-20 19:18:46 +0000

    sonar-scanner 8.1.0.6389 (new formula)

---
+++ Formula/s/sonar-scanner.rb
@@ -0,0 +1,29 @@
+class SonarScanner < Formula
+  desc "Launcher to analyze a project with SonarQube"
+  homepage "https://docs.sonarqube.org/latest/analysis/scan/sonarscanner/"
+  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-8.1.0.6389.zip"
+  sha256 "ab76ab3c360025e9108be5b55be066f304a164f8b2850d2f2f333915db51bc1b"
+  license "LGPL-3.0-or-later"
+  head "https://github.com/SonarSource/sonar-scanner-cli.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b630ad2c6adb19207e4508e19a87082be8dafa8a0e09f8fa5ba4e37e7161e922"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm_r(Dir["bin/*.bat"])
+    libexec.install Dir["*"]
+    bin.install libexec/"bin/sonar-scanner"
+    etc.install libexec/"conf/sonar-scanner.properties"
+    ln_s etc/"sonar-scanner.properties", libexec/"conf/sonar-scanner.properties"
+    bin.env_script_all_files libexec/"bin/",
+                              SONAR_SCANNER_HOME: libexec,
+                              JAVA_HOME:          Language::Java.overridable_java_home_env[:JAVA_HOME]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/sonar-scanner --version")
+  end
+end
