ac9b19f73c12ba5dd7f12c256e921c4bb0c0a6d3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 21:15:09 +0800

    lutok: update 0.6.2_1 bottle.

--- Formula/l/lutok.rb
+++ Formula/l/lutok.rb
@@ -13,12 +13,7 @@ class Lutok < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5375d9b2d49d55cda41f53ba11c44a47b6c9fe76e52a5b3592bf48afda9b9871"
-    sha256 cellar: :any,                 arm64_sequoia: "cf023d7cb40f5d1a0fbb65a3c937a1bca17828e2e2356960af0a119bfeca6e81"
-    sha256 cellar: :any,                 arm64_sonoma:  "02acd0a9b44150ba2297dddee6eb3ca10f244ca63a093e5a655e2cf8eec1fad6"
-    sha256 cellar: :any,                 sonoma:        "0f73bf9da21d6a0a923303f03d2ae52373a1885e39eb050ed291e35d33a1934a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6d750bc1a48d2b4b621462e267437c33c32b0305ad4acb72c51900759068f5a5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e216fa951f1f38a59d2a4962c6d58e61ec90ac9d7f27403940cbd9a79c06c4ad"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "88a6fa3fed07cecf59b315998f64173d61e104f55fca85ddf72e3e78276402a0"
   end
 
   depends_on "autoconf" => :build
