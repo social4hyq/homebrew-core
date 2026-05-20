ef4374d582e035862404f513d1a19707c41b8658
hqzing <hqzing@outlook.com>
2026-05-20 20:41:00 +0000

    style-dictionary 5.4.0 (new formula)

---
+++ Formula/s/style-dictionary.rb
@@ -0,0 +1,36 @@
+class StyleDictionary < Formula
+  desc "Build system for creating cross-platform styles"
+  homepage "https://github.com/style-dictionary/style-dictionary"
+  url "https://registry.npmjs.org/style-dictionary/-/style-dictionary-5.4.0.tgz"
+  sha256 "fa196d9d251347a072e342932751d56e6dda49fa1e440694b857a32659131807"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "95dbd9912f05056ab936f782863e2490d2096ec7aeaebb9dae6b4db497bffcee"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+
+    # Build an `:all` bottle by removing example files
+    examples = libexec/"lib/node_modules/style-dictionary/examples"
+    rm %w[
+      advanced/create-react-native-app/android/app/proguard-rules.pro
+      complete/android/demo/proguard-rules.pro
+    ].map { |file| examples/file }
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/style-dictionary --version")
+
+    output = shell_output("#{bin}/style-dictionary init basic")
+    assert_match "Source style dictionary starter files created!", output
+    assert_path_exists testpath/"config.json"
+
+    output = shell_output("#{bin}/style-dictionary build")
+    assert_match "Token collisions detected", output
+  end
+end
