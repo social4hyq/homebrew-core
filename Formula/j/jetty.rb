cb5cc1673a1d0bb586992baa40d43345cdba6741
hqzing <hqzing@outlook.com>
2026-05-20 18:11:26 +0000

    jetty 12.1.9 (new formula)

---
+++ Formula/j/jetty.rb
@@ -0,0 +1,58 @@
+class Jetty < Formula
+  desc "Java servlet engine and webserver"
+  homepage "https://jetty.org/"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-home/12.1.9/jetty-home-12.1.9.tar.gz"
+  sha256 "a520e41b4776b9e498c8a8f61c33c3916a00b54e56dc299ddb8078ff731eca4e"
+  license any_of: ["Apache-2.0", "EPL-2.0"]
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-home/maven-metadata.xml"
+    regex(%r{<version>(\d+\.\d+\.\d+)(?!\.[a-zA-Z])</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "25ee8285e5d04e63dce9a7915ea01365fc5f17a7ec52e64860fd0b78e6813c10"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    (libexec/"logs").mkpath
+
+    (bin/"jetty").write <<~EOS
+      #!/bin/bash
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      export JETTY_HOME="#{libexec}"
+      exec "${JAVA_HOME}/bin/java" -jar "${JETTY_HOME}/start.jar" "$@"
+    EOS
+  end
+
+  test do
+    http_port = free_port
+    ENV["JETTY_ARGS"] = "jetty.http.port=#{http_port} jetty.ssl.port=#{free_port}"
+    ENV["JETTY_BASE"] = testpath
+    ENV["JETTY_RUN"] = testpath
+
+    log = testpath/"jetty.log"
+
+    # Add the `demos` module to the "JETTY_BASE" (testpath) for testing.
+    system "#{bin}/jetty --add-module=demos > #{log} 2>&1"
+    assert_match "Base directory was modified", log.read
+
+    pid = fork do
+      $stdout.reopen(log, "a")
+      $stderr.reopen(log, "a")
+      exec bin/"jetty", *ENV["JETTY_ARGS"].split
+    end
+
+    begin
+      sleep 20 # grace time for server start
+      assert_match "webapp is deployed. DO NOT USE IN PRODUCTION!", log.read
+      assert_match "Welcome to Jetty #{version.major}", shell_output("curl --silent localhost:#{http_port}")
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
+  end
+end
