eb7971b58440ce280667ed24bdd30944c9a5cb9f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 00:22:15 +0800

    proot: add 5.4.0 bottle.

--- Formula/p/proot.rb
+++ Formula/p/proot.rb
@@ -5,6 +5,10 @@ class Proot < Formula
   sha256 "29248aac2a7ce10c3bd5ee5602742ec33b2532310ff9cf73b79f3c133e5a5f68"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f3afc35c4f4ba53beb871c87d42eb52a29e7fc20a7cf5045f124dbb3ea8ed6f8"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "talloc"
 
