88e71af930f89e0728c40b0290457d6ea284115b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 18:10:25 +0800

    aom: update 3.14.0 bottle.

--- Formula/a/aom.rb
+++ Formula/a/aom.rb
@@ -8,12 +8,7 @@ class Aom < Formula
   head "https://aomedia.googlesource.com/aom.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "72e45baf4c02b6aa2ad059dc6469d7f5a0c2f087118f4089879c6319533363d2"
-    sha256 cellar: :any,                 arm64_sequoia: "44eb9342758b1f7dc17ee80c6bac93f316966781964902813ed39931b9ee6610"
-    sha256 cellar: :any,                 arm64_sonoma:  "c73438ff1056315ffb2eedd1a8e6e31a78242090d5d5f9519c4fa556f1e854e5"
-    sha256 cellar: :any,                 sonoma:        "0ef9d87db1682476846d69e6fdcef03c296efc4896bfe7bd100c2d93c841a252"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8078a6f9d17db9ec9e4bb522deb266bb61411c4b14b8d109921af147df43a88f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3663e4528ce21efa8f1d7dac9e3e1104f054db76134e1dbf01fba57c3e7c6e45"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b97049e5fe90ca2c0931a831b7e4bb0fc8400858a1027090a3ef46b67e325ea7"
   end
 
   depends_on "cmake" => :build
