60a6adb70ab1fa3be5648a18baf629c87985b086
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 09:57:14 +0800

    oxfmt: update 0.50.0 bottle.

--- Formula/o/oxfmt.rb
+++ Formula/o/oxfmt.rb
@@ -6,12 +6,7 @@ class Oxfmt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "1dcdcad8da2cef3c1f13563f4d1fa8aacd87fd294287c92ecdf25f53833d7dce"
-    sha256 cellar: :any,                 arm64_sequoia: "920c6de773c35392f56a8f20864f03668c90c5c5ef250014b08a9d126d9488ae"
-    sha256 cellar: :any,                 arm64_sonoma:  "920c6de773c35392f56a8f20864f03668c90c5c5ef250014b08a9d126d9488ae"
-    sha256 cellar: :any,                 sonoma:        "755b72200061f0ff95fd4470aabee38a01830be49e1bdcae9000a3bc26cfa347"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ffc262aaac4314f7acb5f247ae0d4b95497623733f9ed561052d847ce1917f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8a1b431ac3aea4c99e8e664aba622ab11f27ebc44a8b4142f2d1f6553338344"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8203c2dacfb5ca19c9aed20caf97d574c1200a5e760a03da1df68569633e00c2"
   end
 
   depends_on "node"
