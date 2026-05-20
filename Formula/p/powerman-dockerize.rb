47d63eb7a1fd9dcf04b552f49bfd16d20aab8a57
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 10:13:31 +0800

    powerman-dockerize: update 0.24.3 bottle.

--- Formula/p/powerman-dockerize.rb
+++ Formula/p/powerman-dockerize.rb
@@ -7,12 +7,7 @@ class PowermanDockerize < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ea36ce1326cf135d59fb62ab94d4d9a86bcd8c26271b2a0d36a73a9e48665e43"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea36ce1326cf135d59fb62ab94d4d9a86bcd8c26271b2a0d36a73a9e48665e43"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea36ce1326cf135d59fb62ab94d4d9a86bcd8c26271b2a0d36a73a9e48665e43"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1ab2896f5416a6f8b1d29d2419793757bd25c1aa3e36fd7c95be6b1cd83a0cc3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0e68e24d383c5df34414a26b602129b898873a4cd7f34068205c52d08a96e154"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76ae163fbb8d52e493a73f2f6c8e02dd12e4b02a654b6bc4a7ada9bb61778aca"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "190b82a144284ce2f93797dd38b1cadd1df3e5442bdfc868e80b236f16dd8595"
   end
 
   depends_on "go" => :build
