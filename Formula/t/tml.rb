064fa29a351a570abc1d715be89455dc695078f8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-20 21:23:45 +0800

    tml: update 0.7.1 bottle.

--- Formula/t/tml.rb
+++ Formula/t/tml.rb
@@ -7,12 +7,7 @@ class Tml < Formula
   head "https://github.com/liamg/tml.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "10b34badd3fef44e0bf17c4b4c82a136d08f4ad72fb80037b5897f338a683464"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "10b34badd3fef44e0bf17c4b4c82a136d08f4ad72fb80037b5897f338a683464"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "10b34badd3fef44e0bf17c4b4c82a136d08f4ad72fb80037b5897f338a683464"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4b24f741548871912f5a3528e28b5cc992e4ae75d194e4598c27d506f2700a4f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a9f31aae22bfb77db6c3d0541ebd2917baafcc96d2bbbf3937b5a482cfb0de47"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6e8917c0a83fd27f6c87020f2279523fefd4ebbcdc479802b6caac90a3e532c9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "18772555ed5d9f027c387ebdaf7fe748605447af15f7e84f1c1efc3694712290"
   end
 
   depends_on "go" => :build
