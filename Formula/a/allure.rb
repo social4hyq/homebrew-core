3daa4e4fc1792cd032ae0cc7c0d8f79354d59ed1
lanfungcing <lanfq.26@qq.com>
2026-05-20 08:06:36 +0000

    allure 2.41.0 (new formula)

---
+++ Formula/a/allure.rb
@@ -0,0 +1,56 @@
+class Allure < Formula
+  desc "Flexible lightweight test report tool"
+  homepage "https://github.com/allure-framework/allure2"
+  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.41.0/allure-commandline-2.41.0.zip"
+  sha256 "9e835b210e61507fcdeef4bef283611568acb3fbd56e4f9d02aeccb5f216b817"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=io/qameta/allure/allure-commandline/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ff9c6f44b186403cdbf833f6bf6e450bfe22def41ba7b873d54cf975f6a09205"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    # Remove all windows files
+    rm(Dir["bin/*.bat"])
+
+    libexec.install Dir["*"]
+    bin.install libexec.glob("bin/*")
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  test do
+    (testpath/"allure-results/allure-result.json").write <<~JSON
+      {
+        "uuid": "allure",
+        "name": "testReportGeneration",
+        "fullName": "org.homebrew.AllureFormula.testReportGeneration",
+        "status": "passed",
+        "stage": "finished",
+        "start": 1494857300486,
+        "stop": 1494857300492,
+        "labels": [
+          {
+            "name": "package",
+            "value": "org.homebrew"
+          },
+          {
+            "name": "testClass",
+            "value": "AllureFormula"
+          },
+          {
+            "name": "testMethod",
+            "value": "testReportGeneration"
+          }
+        ]
+      }
+    JSON
+    system bin/"allure", "generate", testpath/"allure-results", "-o", testpath/"allure-report"
+  end
+end
