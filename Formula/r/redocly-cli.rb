2b5f108d0065301254d3136d64d439c9538e920f
hqzing <hqzing@outlook.com>
2026-05-20 22:11:56 +0000

    redocly-cli 2.31.2 (new formula)

---
+++ Formula/r/redocly-cli.rb
@@ -0,0 +1,85 @@
+class RedoclyCli < Formula
+  desc "Your all-in-one OpenAPI utility"
+  homepage "https://redocly.com/docs/cli"
+  url "https://registry.npmjs.org/@redocly/cli/-/cli-2.31.2.tgz"
+  sha256 "3fe665cf169b290181370f20fa04cb853a26990b8385b7197dd2fd111abc473c"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c48b4fa89c06eff09b6d1060c4e8ee5d144d100d4cfeae186b0bdd605126f671"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/redocly --version")
+
+    test_file = testpath/"openapi.yaml"
+    test_file.write <<~YML
+      openapi: '3.0.0'
+      info:
+        version: 1.0.0
+        title: Swagger Petstore
+        description: test
+        license:
+          name: MIT
+          url: https://opensource.org/licenses/MIT
+      servers: #ServerList
+        - url: http://petstore.swagger.io:{Port}/v1
+          variables:
+            Port:
+              enum:
+                - '8443'
+                - '443'
+              default: '8443'
+      security: [] # SecurityRequirementList
+      tags: # TagList
+        - name: pets
+          description: Test description
+        - name: store
+          description: Access to Petstore orders
+      paths:
+        /pets:
+          get:
+            summary: List all pets
+            operationId: list_pets
+            tags:
+              - pets
+            parameters:
+              - name: Accept-Language
+                in: header
+                description: 'The language you prefer for messages. Supported values are en-AU, en-CA, en-GB, en-US'
+                example: en-US
+                required: false
+                schema:
+                  type: string
+                  default: en-AU
+            responses:
+              '200':
+                description: An paged array of pets
+                headers:
+                  x-next:
+                    description: A link to the next page of responses
+                    schema:
+                      type: string
+                content:
+                  application/json:
+                    encoding:
+                      historyMetadata:
+                        contentType: application/json; charset=utf-8
+                links:
+                  address:
+                    operationId: getUserAddress
+                    parameters:
+                      userId: $request.path.id
+    YML
+
+    assert_match "Woohoo! Your API description is valid. 🎉",
+      shell_output("#{bin}/redocly lint --extends=minimal #{test_file} 2>&1")
+  end
+end
