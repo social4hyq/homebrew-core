fd8849080d3aeb174405e20848e99457727c778b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 14:47:47 +0800

    docker-agent: update 1.61.0 bottle.

--- Formula/d/docker-agent.rb
+++ Formula/d/docker-agent.rb
@@ -7,12 +7,7 @@ class DockerAgent < Formula
   head "https://github.com/docker/docker-agent.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "226148b5fc84180de72c3d69117c85ff1c345d5961bbb02f1254b11384c2b118"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "565d645826997b8e488099b0e48eaa257ec9ac2eae8db54562907bc0f4d21ff0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fbe2b8728a87f03e5faa0f6c3ecd5d4f5e01cba1af88848e1290e65c0cae9223"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5826b292835d635c91a2a659724b6adfea8c2115f856bed062da438e9c9ada3e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "761e2b71bf07ba9205eb7990f9f433bc943cbb2391ecb444aca6d1d191a3b7a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa95a63bb66ff86c536a2278dd9b2ac021df0b99e7b056b6cced8691679eeded"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d53de136386fbffda4ff35e2c353ce75a400ad566e2611b614390ef5e286ba31"
   end
 
   depends_on "go" => :build
