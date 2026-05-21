f300fdafd37de76c03f3606f7d48318103e8b03c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 13:14:30 +0800

    zxc: update 0.11.0 bottle.

--- Formula/z/zxc.rb
+++ Formula/z/zxc.rb
@@ -7,12 +7,7 @@ class Zxc < Formula
   head "https://github.com/hellobertrand/zxc.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "8fbd5c1f1f9020ad8cfce4e9cc707e8c66f8b45121a133cfa0f57d4c0836a026"
-    sha256 cellar: :any,                 arm64_sequoia: "5385b148dfe5e631df5a5361a65433061b0306283a37eb444a8f1345c39c23f5"
-    sha256 cellar: :any,                 arm64_sonoma:  "6c415e06a5180027ca519c2d9b72e4fe45aab71647ff98f334bf8b84efad7f26"
-    sha256 cellar: :any,                 sonoma:        "dd75aa0998af77a4dbf6ea9bdc3bc22b028c75b3da97c22dcf2f874e7f7cd296"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f49490371f19c39f4801c7164a6c39545c8e26f27cd3042124b47cf9f81dcd41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ee15464079ff409d2915a4af3f665202753151641c9226b52553c5cc0a4958b8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d9b4c392684bc24f6cb28ef8f14edd190614e62b2940818536e54b444a98b6e5"
   end
 
   depends_on "cmake" => :build
