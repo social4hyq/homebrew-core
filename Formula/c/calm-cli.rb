9c2e791647f6a064fcf2256a2f386b9a165170ed
hqzing <hqzing@outlook.com>
2026-05-21 16:05:42 +0000

    calm-cli 1.40.0 (new formula)

---
+++ Formula/c/calm-cli.rb
@@ -0,0 +1,36 @@
+class CalmCli < Formula
+  desc "CLI allows you to interact with the Common Architecture Language Model (CALM)"
+  homepage "https://github.com/finos/architecture-as-code/tree/main/cli"
+  url "https://registry.npmjs.org/@finos/calm-cli/-/calm-cli-1.40.0.tgz"
+  sha256 "69b0c466803be3c93878721fec1381625d9b0dfc1116976a45e34823b7cb422d"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9d2e75fa55e2a8fd01f92dad272ed729e9ad6731d6ce8666cbc4e8d2a8987d9e"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    resource "testdata" do
+      url "https://raw.githubusercontent.com/finos/architecture-as-code/refs/heads/main/calm/getting-started/conference-signup.pattern.json"
+      sha256 "26bb2979bb3e8a3a8eea2dfe0bd19aaa374770be61ee42c509c773c2fcc6c063"
+    end
+
+    testpath.install resource("testdata")
+    system bin/"calm", "generate",
+                       "--pattern", "./conference-signup.pattern.json",
+                       "--output", "./conference-signup.arch.json"
+    system bin/"calm", "validate",
+                       "--pattern", "./conference-signup.pattern.json",
+                       "--architecture", "./conference-signup.arch.json",
+                       "--output", "./conference-signup.validate.json"
+
+    assert_match version.to_s, shell_output("#{bin}/calm --version")
+  end
+end
