1d196953d0a6a1813b7f37218a80d85e93be29f6
hqzing <hqzing@outlook.com>
2026-05-21 20:54:12 +0000

    vue-language-server 3.3.1 (new formula)

---
+++ Formula/v/vue-language-server.rb
@@ -0,0 +1,40 @@
+class VueLanguageServer < Formula
+  desc "Vue.js language server"
+  homepage "https://github.com/vuejs/language-tools"
+  url "https://registry.npmjs.org/@vue/language-server/-/language-server-3.3.1.tgz"
+  sha256 "05ad259dc7e2dfdea20247fa7c5c29f7957a92be9a63cb18081613bc7451de4d"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9bb604a607d2902266efd634546c8327ad22b2ca040db64a39c1cf55cc774b00"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3(bin/"vue-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
