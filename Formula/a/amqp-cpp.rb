ba10c9cd824ebcc91f706ac8c334407bb9274db3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 22:25:19 +0800

    amqp-cpp: update 4.3.27 bottle.

--- Formula/a/amqp-cpp.rb
+++ Formula/a/amqp-cpp.rb
@@ -12,14 +12,7 @@ class AmqpCpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "f300647f32576320289d224bdf7f4500522765a6c2ed8f10711372547d24114b"
-    sha256 cellar: :any,                 arm64_sequoia: "d42884b0048b3524ff2503b74e3606a415534df02f36b76daa738005f801c660"
-    sha256 cellar: :any,                 arm64_sonoma:  "6088a49ef4492292f4643805af8808a6d3942a1f3b4e1fda5b29aefb08bc96c6"
-    sha256 cellar: :any,                 arm64_ventura: "052fb79e01072ebee63a343fca762f19fb3d3586502e6049c0e8666db559663e"
-    sha256 cellar: :any,                 sonoma:        "58aa9b3ed88fa9452bab84e4265c04ff686abb23cfefd9ce1b40066fa50cdacd"
-    sha256 cellar: :any,                 ventura:       "a0549cf3274f68d0dd55199876406836104d0df3af6dbc4ffa56031a2b8859ce"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8951f9439fa4acf6c6616b1130c7a2cbffe3ef79e9443865592448baca178ea9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61ccf7cf9045f334afa3df40778602bdf1382d681c0ae5811ac34e6863906fcb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f1b8178b2c59db7e5fe55099a0c6628ba363772ba6fb336ca6eff246595b9731"
   end
 
   depends_on "cmake" => :build
