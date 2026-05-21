60cb4aa13da06dc95532a54d9d238c2dfc562511
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 12:51:24 +0800

    woodpecker-cli: update 3.14.1 bottle.

--- Formula/w/woodpecker-cli.rb
+++ Formula/w/woodpecker-cli.rb
@@ -7,12 +7,7 @@ class WoodpeckerCli < Formula
   head "https://github.com/woodpecker-ci/woodpecker.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b7f0761766b946953df9e88c43c2fb168e844bb2616f0b69b6d331231be199e4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7f0761766b946953df9e88c43c2fb168e844bb2616f0b69b6d331231be199e4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b7f0761766b946953df9e88c43c2fb168e844bb2616f0b69b6d331231be199e4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0b653301f4ec83d3886de632fcd8ea8884e0f0354a4de5ff9306c9cc004b350f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "de101794908d6ca5db19b144e3d86d47bfb4831af2dc0efa3d23d6df05b1d33e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99e5490ac11dfe37ed5ef8b4dff424f008823592c88d01c48cf8bd4070a04601"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ad13e8a938ed9f4112082135274be58d70f37203c683240c7b69327366382898"
   end
 
   depends_on "go" => :build
