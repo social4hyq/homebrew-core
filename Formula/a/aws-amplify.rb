c52f76b4a7070d135301352d29dee178ea2d2a01
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 21:09:08 +0800

    aws-amplify: update 14.5.0 bottle.

--- Formula/a/aws-amplify.rb
+++ Formula/a/aws-amplify.rb
@@ -6,12 +6,7 @@ class AwsAmplify < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "6306010324300d260a7e96094214b793f4287203c6e60f95020774faf913dcef"
-    sha256 cellar: :any,                 arm64_sequoia: "e4368c0eb954c02fab060b2fb05fef98a35a21557c51688412472e286d686962"
-    sha256 cellar: :any,                 arm64_sonoma:  "e4368c0eb954c02fab060b2fb05fef98a35a21557c51688412472e286d686962"
-    sha256 cellar: :any,                 sonoma:        "8346c84c26cdf1ce652c1bf6e97fda05110c903453d9dcda35c6cb00d34ae91e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "919570a74cc2824c8ab3aabd90eb5e2c13405edb43ae26d4d785668144f8bb3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb5cd451b33efd5da644333c04d9b9f1c12f8fc146d95459e8cf1110091200d7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "057506965c15a332f7cbd947a1c6ea68d1bb6fc83ce29814ce7662d421319d27"
   end
 
   depends_on "node"
