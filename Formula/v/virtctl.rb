1aeb59a93896497d9845541957575cb86901c6d5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 09:50:13 +0800

    virtctl: update 1.8.2 bottle.

--- Formula/v/virtctl.rb
+++ Formula/v/virtctl.rb
@@ -15,12 +15,7 @@ class Virtctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ca7784d238e5914d2c84a66c2bcda5d27d3b79a0995c8cb15d8bbf4a6e47080c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8e3c16ea80f0d3199363559c13a6b404c51596a768ec48b85109ed1a53fd3524"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae03467122b703521e81bb92d4bc97299cfa62d12e36cf29d7448b39dfbeb9df"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7c1de0f619a63f1585b184895b4e8147f7da65c5a06a10796eacd7c37e3ff0fc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ae62c2ecdbf4ee8a9b136c2ded316448db597af2f5d743138d6bd9a95c7b718"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2b606686b74ca4161c9e2569d93e5836bcdc5fcbfe5f13c27278157596e9ab4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "eda46890b331b1a431f37c80d1d4eb056dfd960aa77b9ce089f9ddafd9dbfdd7"
   end
 
   depends_on "go" => :build
