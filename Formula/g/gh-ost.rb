5418cdf7f9617a2e5c198fddb513eecc70d6f667
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 19:04:08 +0800

    gh-ost: update 1.1.9 bottle.

--- Formula/g/gh-ost.rb
+++ Formula/g/gh-ost.rb
@@ -12,12 +12,7 @@ class GhOst < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a1c71250037404630df76c18f252118facc474b5e2a6769c6558fa5f04a105cf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0f0e3f8dc64ab186702813b749dd5fec589dbc4be6476b8f6ae11f6a436b1ca"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b4400397d24ec74720312b23d489c8b18d0e6c7086bc48c09a6f7b7d094a1197"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7a874f959a202e74254c85181ec8d97b8036f735692b05cd6793785a4330efcc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a5a7358c74cb32e12754c3a2aad575f6cd6268b222a122b99522add63a2ef20"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "53e1ba3b16e29e6d0c3069a50adb4fb3d6bc7bb8e2c21a9146fa84bf48040c66"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "33fa1e19ce7a1ba2e8f74ffb2b73b55e333f987c92561243691af0173f3daada"
   end
 
   depends_on "go" => :build
