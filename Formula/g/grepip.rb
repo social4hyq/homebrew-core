ab96d3776699d66e859824159abf8c92a608ee56
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 18:01:10 +0800

    grepip: update 1.2.2 bottle.

--- Formula/g/grepip.rb
+++ Formula/g/grepip.rb
@@ -12,15 +12,7 @@ class Grepip < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "22b47360d59e127a7ef33ec598cbae91fbc393d2c4a7d787a589bf1e308e9695"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ef333d42f469784211e7520c89ac8ac07014ca9e56a7ef3a87a29749ddae30dd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef333d42f469784211e7520c89ac8ac07014ca9e56a7ef3a87a29749ddae30dd"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ef333d42f469784211e7520c89ac8ac07014ca9e56a7ef3a87a29749ddae30dd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d831e7a316e08e58f230b208ed0b7a5db4b2c3ae425dbb71a52b1099d15bf77b"
-    sha256 cellar: :any_skip_relocation, ventura:       "d831e7a316e08e58f230b208ed0b7a5db4b2c3ae425dbb71a52b1099d15bf77b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e2ad83ecab46b2e50890c75e9e583b060de0fb480da868639645ad48702f84b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89fb159a561f18bd7ddb41e4fe0e280d1aa632165f0b887d4ce2c04af65633bc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8c7f2719c536a5e8231c6a9ca2ba04d382f5a4fbf80e19fc2d20288986ff86de"
   end
 
   depends_on "go" => :build
