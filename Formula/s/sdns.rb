974c3eab71c9fb07ac2101d4755113e0045fe237
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 12:59:59 +0800

    sdns: update 1.6.7 bottle.

--- Formula/s/sdns.rb
+++ Formula/s/sdns.rb
@@ -7,12 +7,7 @@ class Sdns < Formula
   head "https://github.com/semihalev/sdns.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "64a3944a17cd0c7c65922b633e1cc5f279ddc1da31328a9bd9991f517d314d03"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c80d1681b7b33f2230ae33fa195eff539b869d79c8b2c9a25ade508ff7c68fd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4e002af92779e40f4b848ae52c816e27902cdfb929d6c27850686d62f3807e82"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f21f779e845de8c64981b93035c1b18a7e5fed9b2b909463d0aa87d46ffa4489"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e6765b3994617dc0a32f3bc42b4271260972b0fe428181e2049fb3bc44925e82"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d1793a0a04ab6217d7c0710c3969b114d04b2ce5d916254615d2723f550ee2b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a0d7418a94fcc8bacc7311ffe1894b4e90f9597ff4cf3bd513747e5e111be379"
   end
 
   depends_on "go" => :build
