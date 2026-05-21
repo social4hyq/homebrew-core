97feab558f5fe02e4b627a949a895392fc186110
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 09:56:53 +0800

    tig: update 2.6.0 bottle.

--- Formula/t/tig.rb
+++ Formula/t/tig.rb
@@ -7,12 +7,7 @@ class Tig < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5464963cbef880b886995cebc53ba6465f96a360a08ab04a0652b98a691d7de2"
-    sha256 cellar: :any,                 arm64_sequoia: "8e4dfb08982c3aa19fab2a11c5f9d0c9ba4bc84f31be5b6eb1949f550330b947"
-    sha256 cellar: :any,                 arm64_sonoma:  "6aef8066f91d46c4ce81efdad40d741f89ea45fa6b3e9f324f7a4f2aebe430e3"
-    sha256 cellar: :any,                 sonoma:        "0a3dd5d81806bac10f99984b1e84d5d89c857a706207ddee9ca82ea00c0eb15f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4281d358afb993ded992ad26db2c21c1f08c1389b88bb2732e26e204c001439b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dfe9453ea284cf1c2a94073e7da2f52b0d1f645a620b3401d4a39b3ba4bd697a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f5ee54170a90576327a7c8d38ebe830b42398d2280e9854eee34e939b96dc7a1"
   end
 
   head do
