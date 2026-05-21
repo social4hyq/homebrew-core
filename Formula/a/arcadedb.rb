a4dddb0caafd6fd82a576a74d2bb892b2fbafdc3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 09:49:32 +0800

    arcadedb: update 26.4.2 bottle.

--- Formula/a/arcadedb.rb
+++ Formula/a/arcadedb.rb
@@ -6,7 +6,7 @@ class Arcadedb < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "02784c3be3a41bf5e3d26156746b22bd9fbabc860f48659b69b2eb81243d826e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "04745523cb08dd18d8fe8ed8a1fd59756f00da1d6c27b5a37cbe3fb32185c043"
   end
 
   depends_on "openjdk"
