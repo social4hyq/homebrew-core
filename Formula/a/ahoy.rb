82694ce730343a47c93271b8b745c2f60590192c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 10:08:00 +0800

    ahoy: update 2.5.0 bottle.

--- Formula/a/ahoy.rb
+++ Formula/a/ahoy.rb
@@ -7,14 +7,7 @@ class Ahoy < Formula
   head "https://github.com/ahoy-cli/ahoy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a1d4f45c4c570160683c363f45827d0e8632297f68794018f8686ca1e88f02ee"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "39cc43e109a7f06f80f5bbea045f5877292645bebdc4e8dda3dcc60699bef370"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "39cc43e109a7f06f80f5bbea045f5877292645bebdc4e8dda3dcc60699bef370"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "39cc43e109a7f06f80f5bbea045f5877292645bebdc4e8dda3dcc60699bef370"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fde6ef7692a9bf8513258596831c349e349b49beea85ff7ba06c0046305e3156"
-    sha256 cellar: :any_skip_relocation, ventura:       "fde6ef7692a9bf8513258596831c349e349b49beea85ff7ba06c0046305e3156"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6d628f6bd4c2687f8b9736b8b365e311472af4b81dfe798014816b436e08d8fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "689b92185a351f9b622340c9fc7c1234df8a33e6bea026075bb79f52e2de13c5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0fe320b03d9c66f2add5501fdeb6cf0f7a696a97849bc724bde078b330e7fc86"
   end
 
   depends_on "go" => :build
