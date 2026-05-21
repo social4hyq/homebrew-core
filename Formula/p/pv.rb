b2bf159b25310a54b0007ef72b0daa917a7a669c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 17:33:38 +0800

    pv: update 1.10.5 bottle.

--- Formula/p/pv.rb
+++ Formula/p/pv.rb
@@ -11,12 +11,7 @@ class Pv < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "f309f22e145a6363d5af01ec38016aadc7ea7b9ae2c07ddb703c557bcd1ade32"
-    sha256 arm64_sequoia: "43179d4e43abe9d8ec0bca93b9e25f262107f7cc5a7d996b3870ef2624d8cbc9"
-    sha256 arm64_sonoma:  "0479752260d1e7fb1f2dda3f7adec447f71233bea499ca29b4acb5fe70735a77"
-    sha256 sonoma:        "6c3b9e1445e54e804b4c20ff1cdec774894a056d56a5beb31340cd6c4a05127b"
-    sha256 arm64_linux:   "66ae9869504115c75385077cb1f5a4804c3531fc31cde3b2f9ed83bc4c0cb29a"
-    sha256 x86_64_linux:  "5c52013320f8b5291a45a846e6e82ad97e8a6f07eabb440ef75d3f11c0f76eea"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "213917054fdd8ec4cec9ca2019f0bb60c5cab7bcd1c08944d021796f89c9395c"
   end
 
   uses_from_macos "ncurses"
