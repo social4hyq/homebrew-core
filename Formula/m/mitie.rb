9bfc03c12539cf9d129d60ceeb8e4c435928fd62
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 22:06:41 +0800

    mitie: update 0.7_3 bottle.

--- Formula/m/mitie.rb
+++ Formula/m/mitie.rb
@@ -8,13 +8,7 @@ class Mitie < Formula
   head "https://github.com/mit-nlp/MITIE.git", branch: "master"
 
   bottle do
-    rebuild 4
-    sha256 cellar: :any,                 arm64_tahoe:   "025cb3eb83b916c6311a5282c51ba1c44d9890dcb158b4b8327e5ae0deda4a8b"
-    sha256 cellar: :any,                 arm64_sequoia: "9aa9238fdd29d5a705c3e342f883e147424c62329bc9cdb81ef939079c7d67f3"
-    sha256 cellar: :any,                 arm64_sonoma:  "876af22594ab08bfb316893df9fd16f8e761c42502bafde4eaf82fd188a3f420"
-    sha256 cellar: :any,                 sonoma:        "71792924c5aca7fe2d3db9db416c89552b95690ecf236e681cbe2d1e846ba72e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c9999c37c7d297991dd328c5868a1eea42dc26a193e5725d9a766e2c11852394"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "906519532491d62f21e16e31ef3fccb49672ca033bf811cf289f93bcb6468eeb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c2f051020dd92e8a915d5fed2b5e0682cb338b80cbc2fc4b549369365f507511"
   end
 
   depends_on "python@3.14"
