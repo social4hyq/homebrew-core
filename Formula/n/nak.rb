b2d82e984ef87ca7eeaff361c14ddad6734b68b7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 17:36:11 +0800

    nak: update 0.19.10 bottle.

--- Formula/n/nak.rb
+++ Formula/n/nak.rb
@@ -12,12 +12,7 @@ class Nak < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c6cdd31a926d05654d7284a594df8d7b2fd7adba3f847b867d7931ed3f2ab7ed"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c6cdd31a926d05654d7284a594df8d7b2fd7adba3f847b867d7931ed3f2ab7ed"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c6cdd31a926d05654d7284a594df8d7b2fd7adba3f847b867d7931ed3f2ab7ed"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1c10ad5a2e5000a04b39889c4f870d3de66924a9d09dc717bcba02ec51cb73ff"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "305704972aedf3a611de8e8cdebfbdb75b0b926ff368ca2a6245dbfe239baba6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "424e6ea495cf275e73285d1081deb2486ffc6588258676fd4d16d9274605cc11"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8d1e34f199cee877281a863ad670e1f6bfe576c9f368a11ee5a101f0f96013f6"
   end
 
   depends_on "go" => :build
