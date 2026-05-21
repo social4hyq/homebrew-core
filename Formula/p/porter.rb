2fc1d338670e062417af0e0873ee6a707cb9f0d5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 21:15:34 +0800

    porter: update 1.5.0 bottle.

--- Formula/p/porter.rb
+++ Formula/p/porter.rb
@@ -12,12 +12,7 @@ class Porter < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f09ff66e6988ead5e5a08e44463ae7b2b0ad2fbf47c83ca97bc44959a0cbdbf3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "299b7617b835a08462e2bb45527a725795f7224def44d4c5bba8e5abefb905a3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1cd437e10b25015be4d6b117a6ec72bc916c9d1b4864e7e38adad87f8b299fd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3521d617fdc76dd58e4ec0210bd9b273053b856f41d0e306a7e39c5e406553fd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f9dd9a6d8c1eee295740c554e7823d2ef94602be32578f518127aaea4eb50b68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85c23a8e11f460b3e6865f8a0e6f012a986a33268a9950909b4337be2bdc8da7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "105976ea52e09c8724833194d829b90367fc2281548b4361e65971a8312dca73"
   end
 
   depends_on "go" => :build
