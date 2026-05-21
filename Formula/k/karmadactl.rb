e93a8f66b99d8653411c9bbca0898ec42dae9c7c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 16:51:19 +0800

    karmadactl: update 1.17.2 bottle.

--- Formula/k/karmadactl.rb
+++ Formula/k/karmadactl.rb
@@ -7,12 +7,7 @@ class Karmadactl < Formula
   head "https://github.com/karmada-io/karmada.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b48a55346fa9e7624741b429e23ee3750dcd77dbae884d9f7a622dc7fd40e3db"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e1b638b88e290bde13c074680897e1f421002df4b2b61f67ae48df241a8e7704"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aab7c80220d55b4780987b43d53a10617d8f868cbf8d8c834f762fbe9b9f567b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "513eebfa9e0e7ea3410b4e57abab674f5c1c456d424c1c4379f27816fb6b5297"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3a69d649236ef5cd82e354c64a6193169415e79435a9e9ca8af9d447fd87a30f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "671d516b63b219d89c1adb22830ab9f6073c4900603fc00da054cb0e3f93d1b4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cadff77763eed85e4f85edc97ec09482465420f0c2dcd01268d1e9dd1c1d0938"
   end
 
   depends_on "go" => :build
