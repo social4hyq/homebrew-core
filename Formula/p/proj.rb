17907051a96b04dcdbd9f31221f13e8e1ce21dc4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 22:38:08 +0800

    proj: update 9.8.1 bottle.

--- Formula/p/proj.rb
+++ Formula/p/proj.rb
@@ -14,12 +14,7 @@ class Proj < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "c532d0adc3fdc96268bab9406be54069c26476eb5b8cecb8f748d5df4d1978da"
-    sha256 arm64_sequoia: "ec5842c5c5660dccf23487fd8f70e892f91e37107e9feb849b731b3044022cf6"
-    sha256 arm64_sonoma:  "628b330cac12775231d22bd1978606e10e47dcb8c53f570901d122e5077f3d90"
-    sha256 sonoma:        "9b66a3600a992e790a90dee9baa9e6cd18ddb35caf6460ad4f59a8e105aa0706"
-    sha256 arm64_linux:   "3774b7278555c3d2d39a73f05d08bea617fc08427f62d3a6231dd48764053f4c"
-    sha256 x86_64_linux:  "fa40fef6883cfb3b098a98a7630ffe26a131e4b6996291bb8c1b873b4393ea72"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8ef60d394befe99a136a64c52253952dbf27d0a6ae884d0487f2a3cb547996fe"
   end
 
   depends_on "cmake" => :build
