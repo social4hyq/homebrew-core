e179b03615403575bc15c50f7c07b864ba6d2a5f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 09:59:15 +0800

    quickjs: update 2025-09-13-2 bottle.

--- Formula/q/quickjs.rb
+++ Formula/q/quickjs.rb
@@ -12,12 +12,7 @@ class Quickjs < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "0c4a5557adceba07641b2d0df1a7dc4a80a95cb8d95cb8e89a75deabd2cac755"
-    sha256 arm64_sequoia: "6b8a9c3c838646ff828233f6ed5af58974985f076ab68a9b4337da8d0e01411a"
-    sha256 arm64_sonoma:  "6ac5d8f62afabf4912fb5173a417c08f9118f3ed0fd88c069ee91d74d6a08c1b"
-    sha256 sonoma:        "a2ce1cdd3b921b02c123850b83ff78d4d983fc487c04bfdf93352d415bf6f253"
-    sha256 arm64_linux:   "4a3b2817c7f28db585ece2b4e317fe6ce0079a6c716dd2ef9e612a80d7106089"
-    sha256 x86_64_linux:  "547f293bb0c5b1de7fe993352bc26192627f16a36128d64bb9b9dbd9b2afca2e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "45ed084b561d6c127fed93233526acd02a0e490cfd7d9b3e3d1d11cd685237b4"
   end
 
   conflicts_with "quickjs-ng", because: "both install a `qjs` binary"
