f45ccf9625bb16297761928cf0ef1e00a2b473af
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 21:19:32 +0800

    toxcore: update 0.2.22_1 bottle.

--- Formula/t/toxcore.rb
+++ Formula/t/toxcore.rb
@@ -19,12 +19,7 @@ class Toxcore < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "cbbb31e412bfd8ce19e4bed4468c763c4a30553df87af6daaca7890b60863fc8"
-    sha256 cellar: :any,                 arm64_sequoia: "22e925fce4d035689ba871e1f04ea5254f2239b7ba660e23a21decb7d172c575"
-    sha256 cellar: :any,                 arm64_sonoma:  "590fe74f3e30221b3eef2f02e322da73d547531635ee9749a5c69dac41fd8b08"
-    sha256 cellar: :any,                 sonoma:        "02d2a7718ed5dc375c0b69c5a4406ab3bb48465f3f1e447dfa821af05d72c044"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "310a1c7117ef774e06419060a7e836f80927052bbf1022831dccaf264ace5e9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ffb493b9bb0c9aeea6a33d9450f6497d4e27c9d4e06b3b2621a033269c09e78c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e92ea6a61582caecee004fb62f1e66b3cfb267384f0f527cb3f9a0a411cd2778"
   end
 
   depends_on "cmake" => :build
