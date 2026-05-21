468ee38966f8fd346e95c908c8201960052284f0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 17:00:13 +0800

    eksctl: update 0.226.0 bottle.

--- Formula/e/eksctl.rb
+++ Formula/e/eksctl.rb
@@ -8,12 +8,7 @@ class Eksctl < Formula
   head "https://github.com/eksctl-io/eksctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7cfe57fcfba206426d9431e57a19cbce9e94c763fe796a37c789974aca7532cf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "852346e4e7c55571bbbe0745782fc9f73c763aaf6b03b58b527b0e9ba6486106"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bdf76db240bab40aec743d8a7ae460571ab9c0f85d46d09d20990cfe4c4e1ef2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7f9f315d6fc3886deb1689003ba4a1beaaecd93c8371ccb675b4636e7820c855"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "65febb2fe04e80a47159af84faac59329c2376c093e5fae7578d0e66cdd19019"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8a3da75e5a574536af3fa00b9f7277c47f3365d5a916c25ea0e63203cf9d6d2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "07b73cac40438a936b6e5f71fe84a816799d5a0a76f77d73369dab5ff3035cc4"
   end
 
   depends_on "go" => :build
