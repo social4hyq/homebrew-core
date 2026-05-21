f2303b4ca104f316155f10631ae8ac40fdba4137
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 19:18:42 +0800

    skillshare: update 0.19.15 bottle.

--- Formula/s/skillshare.rb
+++ Formula/s/skillshare.rb
@@ -7,7 +7,7 @@ class Skillshare < Formula
   head "https://github.com/runkids/skillshare.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "bf595e3039ce8be06634d80083aaa6837df96a8c66cdce6e9c580cd7c96c813c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e38a7b50d4cace6995e1be68bd9a34127747c9c7b509e12e914b5d160aef2aca"
   end
 
   depends_on "go" => :build
