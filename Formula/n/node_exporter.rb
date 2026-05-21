aacf1a53cc789a39c70e6eca93ae4816a9067c55
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 09:53:24 +0800

    node_exporter: update 1.11.1 bottle.

--- Formula/n/node_exporter.rb
+++ Formula/n/node_exporter.rb
@@ -12,12 +12,7 @@ class NodeExporter < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d948b78c2421e07943ed291d48c763f6c1101018651f977c4fc24cafa018fec3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "16e5df568574e4545c085a4158511f4b2b1d0bead68210a62e16565ca0d602a7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7280ed9bd7d83c9205421459f2f462664f6512b29194ad01a639b4e7b76b73a5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "aa2331dd0c6c725ace1b027b61009803698caf442f6612e686aecd27f25f3c8b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c156048f29121e32978e067f9c1e18a76b9bd1b446d033a52110b32ab32cf8cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83cf562ab7570c1b71093a20723b505b7285c16dd2ed803336efba9053cd1499"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f08c23983bf9aa15a70c5d96e5a35d70da65b37b4c5545dc2af0f41152fd1b94"
   end
 
   depends_on "go" => :build
