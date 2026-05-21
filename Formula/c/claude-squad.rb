5bd4b3ec5c74591fe94e77fb127e1b962ac571fa
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 18:45:33 +0800

    claude-squad: update 1.0.17 bottle.

--- Formula/c/claude-squad.rb
+++ Formula/c/claude-squad.rb
@@ -7,12 +7,7 @@ class ClaudeSquad < Formula
   head "https://github.com/smtg-ai/claude-squad.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "90cbd80b298afd441defb1da944303155f57dea72037c2573d4d95efea900108"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90cbd80b298afd441defb1da944303155f57dea72037c2573d4d95efea900108"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90cbd80b298afd441defb1da944303155f57dea72037c2573d4d95efea900108"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3f3fed37e3eb6ef5f69b8511d9fae39a2239ffb0a82dd97165ebb2de3283c284"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "09e3a84b9e1a183efcdce56f611d4855eea586486bed0c09993faecd55f97d1d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23d1907017fa5c54fb5e660975965960ebb7cb268a7c3cd5304f4ea4099ea365"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3d97b5032954d679c8e12e3d6e9b3e913eb7ea8109c4b481c7f507886b34c40e"
   end
 
   depends_on "go" => :build
