1af94b02a7482066941e5c7628df438b7fb56c67
hqzing <hqzing@outlook.com>
2026-05-21 20:30:18 +0000

    google-java-format 1.35.0 (new formula)

---
+++ Formula/g/google-java-format.rb
@@ -0,0 +1,67 @@
+class GoogleJavaFormat < Formula
+  include Language::Python::Shebang
+
+  desc "Reformats Java source code to comply with Google Java Style"
+  homepage "https://github.com/google/google-java-format"
+  url "https://github.com/google/google-java-format/releases/download/v1.35.0/google-java-format-1.35.0-all-deps.jar"
+  sha256 "bfb7f9ead6cd328389bc2da53860443bc0e805dfd08cc889bfdf43b26cb2a6e8"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2a12a2156a19226e997743574718b35bb4bc06a371e0843989eada4d5970a5d0"
+  end
+
+  depends_on "openjdk"
+
+  uses_from_macos "python"
+
+  resource "google-java-format-diff" do
+    url "https://raw.githubusercontent.com/google/google-java-format/v1.35.0/scripts/google-java-format-diff.py"
+    sha256 "c1f2c6e8af0fc34a04adfcb01b35e522a359df5da1f5db5102ca9e0ca1f670fd"
+
+    livecheck do
+      formula :parent
+    end
+  end
+
+  def install
+    if version != resource("google-java-format-diff").version
+      odie "google-java-format-diff resource needs to be updated"
+    end
+
+    libexec.install "google-java-format-#{version}-all-deps.jar" => "google-java-format.jar"
+    bin.write_jar_script libexec/"google-java-format.jar", "google-java-format"
+    resource("google-java-format-diff").stage do
+      rewrite_shebang detected_python_shebang(use_python_from_path: true), "google-java-format-diff.py"
+      bin.install "google-java-format-diff.py" => "google-java-format-diff"
+    end
+  end
+
+  test do
+    (testpath/"foo.java").write <<~JAVA
+      public class Foo{
+      }
+    JAVA
+
+    (testpath/"bar.java").write <<~JAVA
+      class Bar{
+        int  x;
+      }
+    JAVA
+
+    patch = <<~DIFF
+      --- a/bar.java
+      +++ b/bar.java
+      @@ -1,0 +2 @@ class Bar{
+      +  int x  ;
+    DIFF
+
+    assert_match "public class Foo {}", shell_output("#{bin}/google-java-format foo.java")
+    assert_empty pipe_output("#{bin}/google-java-format-diff -p1 -i", patch)
+    assert_equal <<~JAVA, (testpath/"bar.java").read
+      class Bar{
+        int x;
+      }
+    JAVA
+  end
+end
