ba17481321f13a2893def8c9daf6d8074353b06a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 06:49:25 +0800

    jcal 0.6.0

--- Formula/j/jcal.rb
+++ Formula/j/jcal.rb
@@ -1,8 +1,8 @@
 class Jcal < Formula
   desc "UNIX-cal-like tool to display Jalali calendar"
   homepage "https://savannah.nongnu.org/projects/jcal/"
-  url "https://download.savannah.gnu.org/releases/jcal/jcal-0.4.1.tar.gz"
-  sha256 "e8983ecad029b1007edc98458ad13cd9aa263d4d1cf44a97e0a69ff778900caa"
+  url "https://download.savannah.gnu.org/releases/jcal/jcal-0.6.0.tar.gz"
+  sha256 "1ff30b55c2aaa8483ce13674c1ed08c4a6cca31bcaa598bd23889c17dbb2a419"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -14,13 +14,7 @@ class Jcal < Formula
     sha256 cellar: :any_skip_relocation, arm64_ohos: "a25b1b71aadf79d4ba89438f10acf00bcec45d9931776afbdc7e71b7023946b7"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-
   def install
-    shell_name = OS.mac? ? "/bin/sh" : "/bin/bash"
-    system shell_name, "autogen.sh"
     system "./configure", "--prefix=#{prefix}",
                           "--disable-debug",
                           "--disable-dependency-tracking"
