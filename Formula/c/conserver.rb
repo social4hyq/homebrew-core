e9906c945ab193ae285eb60b19216007c6767134
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 18:28:37 +0800

    conserver: update 8.3.0 bottle.

--- Formula/c/conserver.rb
+++ Formula/c/conserver.rb
@@ -11,12 +11,7 @@ class Conserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "18a385e584cd2d5496a7a638a2eeb78a071b5ca4fc84dd4acadfc3b1d32d773c"
-    sha256 cellar: :any,                 arm64_sequoia: "f316ec7ce2a3c077720937ae72f97c0ba14de6fa07c780153f38026275e55727"
-    sha256 cellar: :any,                 arm64_sonoma:  "d46c8edcd5f6f406c7f22544e4660936e782f20b7c23f93337b7ae71b606e2c1"
-    sha256 cellar: :any,                 sonoma:        "383529209992368495464900474448a0e19bc1afea889426ac8c43479b95aee9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5427990f7696fc5dbcb6f2d47256571c544384384011727da0b22e0771ed131e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e62d98d49a9f17bb8be6628da6758273db6c59f53ff11e9f601d7095ed35013"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2ac4f36a1f187d86e87ea0ce1336da6caa9a1039e63bb185a52bf7b26c7e48f9"
   end
 
   depends_on "openssl@3"
