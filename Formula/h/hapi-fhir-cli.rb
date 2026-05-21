5911547fa976114ce5cea734ed248d56f105fcc0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 06:45:10 +0800

    hapi-fhir-cli 8.10.0

--- Formula/h/hapi-fhir-cli.rb
+++ Formula/h/hapi-fhir-cli.rb
@@ -1,8 +1,8 @@
 class HapiFhirCli < Formula
   desc "Command-line interface for the HAPI FHIR library"
   homepage "https://hapifhir.io/"
-  url "https://github.com/hapifhir/hapi-fhir/releases/download/v8.8.1/hapi-fhir-8.8.1-cli.zip"
-  sha256 "9e58b9dbec0249c667c326390b95f15a12413578ef86b23e348799ffe6c1672b"
+  url "https://github.com/hapifhir/hapi-fhir/releases/download/v8.10.0/hapi-fhir-8.10.0-cli.zip"
+  sha256 "ad954244931181087198f7e92c8c0f6b81b07ec5b54b9cb020c5e7c3628d241f"
   license "Apache-2.0"
 
   # The "latest" release on GitHub is sometimes for an older major/minor, so we
