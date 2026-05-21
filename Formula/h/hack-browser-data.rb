cdeb9ba4360de5b6e66fd8da2da753e58d20797b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 22:04:32 +0800

    hack-browser-data: update 1.0.0 bottle.

--- Formula/h/hack-browser-data.rb
+++ Formula/h/hack-browser-data.rb
@@ -7,12 +7,7 @@ class HackBrowserData < Formula
   head "https://github.com/moonD4rk/HackBrowserData.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a7b249fa7e8dc58e6e3aa31174dff9d67b788d91248866c5e9e60cf0c7e06c4e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a7b249fa7e8dc58e6e3aa31174dff9d67b788d91248866c5e9e60cf0c7e06c4e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a7b249fa7e8dc58e6e3aa31174dff9d67b788d91248866c5e9e60cf0c7e06c4e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "04be65e5e5a9e26b6021468947d1052ce9453a07bc0c1daa2d02f3317e77fe89"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eea96e3264bb785543efea3792ff7f3167c530e5e11a134747ef386d4ae56123"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "43ce0a5471d222a7edb530c9370199fc6db91475b6fb9c47f17f23d870708279"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f446dfbdf2e136a6e51921c7ce711c940f057c8fcb681c6262a6f882e32f7113"
   end
 
   depends_on "go" => :build
