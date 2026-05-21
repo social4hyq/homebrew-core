901decd4ec501793faa195c1c6c467441c7373b9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 18:37:46 +0800

    crane: update 0.21.6 bottle.

--- Formula/c/crane.rb
+++ Formula/c/crane.rb
@@ -7,12 +7,7 @@ class Crane < Formula
   head "https://github.com/google/go-containerregistry.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "32695c20f1c07b36937021d07f23e27772b79890e3b19c6be2625a9fa9a2ca22"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32695c20f1c07b36937021d07f23e27772b79890e3b19c6be2625a9fa9a2ca22"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "32695c20f1c07b36937021d07f23e27772b79890e3b19c6be2625a9fa9a2ca22"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f818306197fc432c7954fec8bfbea3e5f27f1b5c3e77e5d98f647da845585441"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d337b14394fa03c0fbd5a7b8e1767464ebec628214f7411682909c1f942c74e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64b2f0eaad991cd71b5c90e80fc16b1a9dce55218c326aa23fcd526eb6b9524d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "eb1d785a740f1c3b41714ad891dea86caf9db9cde0cd7a681a77b191d7e90a1a"
   end
 
   depends_on "go" => :build
