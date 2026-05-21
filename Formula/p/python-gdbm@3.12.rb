6d6a0db2bb3d9e6f24765e5f16745fa933d3da77
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 13:46:13 +0800

    python-gdbm@3.12: update 3.12.13 bottle.

--- Formula/p/python-gdbm@3.12.rb
+++ Formula/p/python-gdbm@3.12.rb
@@ -10,12 +10,7 @@ class PythonGdbmAT312 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "0e5d91f359c8c637340ecba17b83a5fc4fdecf773ef100e3af4f2a0bd87ef2b8"
-    sha256 cellar: :any, arm64_sequoia: "28301403f1c5249d19cb51e82c1edc35c36c54cfd171e1df53859de48cd9915a"
-    sha256 cellar: :any, arm64_sonoma:  "679ecd75ce11fad8e4bbc46e47f77bd712e0cef54e357f5c13a0568c2937c6c7"
-    sha256 cellar: :any, sonoma:        "ec513ca3af354f7753f4b75f50266ac0ee3f454c27a42f1e1244ce62cf25aab4"
-    sha256               arm64_linux:   "301118405058ffc7144603b4a39099eeb0a3c379a2b564a040f912a5544aed08"
-    sha256               x86_64_linux:  "4582c9437253e10e7195b441f7dd5677b58d75b7523ad361b805ec7c5e339a6c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9cd2c9b0b5ba234e0754fb1c3b029d28281298edaa616f686994f845760ce252"
   end
 
   depends_on "gdbm"
