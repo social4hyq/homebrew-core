581059494f7a251e3ec5da2cc26d9c72951d003c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 12:51:32 +0800

    groovy: update 5.0.6 bottle.

--- Formula/g/groovy.rb
+++ Formula/g/groovy.rb
@@ -11,7 +11,7 @@ class Groovy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7724b5b5bb03cfaba2b700a5af69abf0dddfa2c66bace18353121c614a2ba860"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6904f9d478770cb2092bb5aa282715d8758c0cacb4d00de9368683b9d6c1a247"
   end
 
   depends_on "openjdk"
