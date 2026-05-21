7d81a23aa8056864b970d6193a42f8c6e262cdf9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 09:49:30 +0800

    repren: update 2.0.0 bottle.

--- Formula/r/repren.rb
+++ Formula/r/repren.rb
@@ -8,7 +8,7 @@ class Repren < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c09a2852d09607acb30bb60525e255c652f6dc0cc2312f4e086884d2a3aa7364"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "413ba8b17e5706fb0ce42e4cc0e9ff801259a4a86f04b54be22d64256c819e47"
   end
 
   depends_on "python@3.14"
