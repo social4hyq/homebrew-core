0606b213cb280e87ba4336a6813b752f1b4b3b5a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 13:24:36 +0800

    docker-debug: update 0.7.11 bottle.

--- Formula/d/docker-debug.rb
+++ Formula/d/docker-debug.rb
@@ -7,13 +7,7 @@ class DockerDebug < Formula
   head "https://github.com/zeromake/docker-debug.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f0faba1df5f1cadcedabd4487f30a0db88072ee50cd92d095f7366b410fb6c42"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0faba1df5f1cadcedabd4487f30a0db88072ee50cd92d095f7366b410fb6c42"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0faba1df5f1cadcedabd4487f30a0db88072ee50cd92d095f7366b410fb6c42"
-    sha256 cellar: :any_skip_relocation, sonoma:        "978911584a039d17fbc927934d0d9c9eb3477eba58b6ea1df5a461a6f3d4e92b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ec74e9da2a07744c7608e482b1573a9178746022cc19ddac7b4c0e7a1f7fc82a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cacde51f354d073566e5f6452a545a0df24ae0ace59d8c2a60adb4954519cdf4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c972c25b14e9d8553a8ad0afd22478ca0600e6e6c2727b1f351c70f871f48cf8"
   end
 
   depends_on "go" => :build
