901fb61a0190e17bb9a3f68b4ef74dcc0fa32cd2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 13:33:21 +0800

    haste-client: update 0.3.0_2 bottle.

--- Formula/h/haste-client.rb
+++ Formula/h/haste-client.rb
@@ -8,14 +8,7 @@ class HasteClient < Formula
   head "https://github.com/toptal/haste-client.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "896ee31e42cbb6469b782f0501ff16cad0e536d9e50fcc4f603c15af19c9397a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6968438749dec734a5a5ab8c4dac48fd5eee5b2c8adfe6ec8908d17c72788c1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e6968438749dec734a5a5ab8c4dac48fd5eee5b2c8adfe6ec8908d17c72788c1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e6968438749dec734a5a5ab8c4dac48fd5eee5b2c8adfe6ec8908d17c72788c1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e6968438749dec734a5a5ab8c4dac48fd5eee5b2c8adfe6ec8908d17c72788c1"
-    sha256 cellar: :any_skip_relocation, ventura:       "e6968438749dec734a5a5ab8c4dac48fd5eee5b2c8adfe6ec8908d17c72788c1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d3a5464d1d5022277c0c64052cc9bc98d2664901f78ad655e153fea1fcb08c44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dee9ebcf14e0273c14d8e28ceb77984f9fcd521e11cbc65f74762897d0e73cba"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "89bd5a7d2d381c96dbb3b1d1e91c394128970356c2e3cf2291af7de7851a5d20"
   end
 
   deprecate! date: "2025-04-18", because: :repo_archived
