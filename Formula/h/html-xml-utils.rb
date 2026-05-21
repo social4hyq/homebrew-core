109c6b0ec75a33e7f279b92ccb9d96552e0bf13f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 21:34:30 +0800

    html-xml-utils: update 8.7 bottle.

--- Formula/h/html-xml-utils.rb
+++ Formula/h/html-xml-utils.rb
@@ -11,14 +11,7 @@ class HtmlXmlUtils < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "805c56aa869cb7f89d9fc11cd1edb4913d9bf76e5c4911d0c59bc9ce77b47a74"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4bd647931de55d7910433f203fca693d2deeb2c80bc5190464b56cc2d9cd0bc1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae39f3f7704ef5261b41cbddbb1facefde3b3224d12d91ec722a243c11d5cafd"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "72ded5b4323716b813c46b80f31f4604fe7bc7611c5795c865bfc68f4a9ad9bf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ef5904e06e9a4f2381485ae3ff342d68a6dced43269a9c9098bdfc19503ca08e"
-    sha256 cellar: :any_skip_relocation, ventura:       "af7e2d3010b2a13594f27ad96dc45c8a59ada49610aabf24761cb72a210a52ed"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "63363ff100b473875d5af798b0085b7467363f147bc3d793807f1cb37a6b2192"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f555fdca29f038169dc2499fc0e680e40c30b18b4224b43b3cc7ac5967bef21c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b4034e5878e0ce3aa6eca0eaacb0823220d44e0a91de83a0e49b3866367a8c69"
   end
 
   def install
