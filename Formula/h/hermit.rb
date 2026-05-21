596ee0f58eac420da83f9c28a87efa9b9b8081da
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 18:23:44 +0800

    hermit: update 0.52.1 bottle.

--- Formula/h/hermit.rb
+++ Formula/h/hermit.rb
@@ -7,12 +7,7 @@ class Hermit < Formula
   head "https://github.com/cashapp/hermit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "086e4e40a9f279053a9b842a9c087d833164d57281304f7a513a44cf5db2402c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7dff8ee22bd32b8275fe3a74f6aca00ab8929ac705f8541bc275e55c3d20bdb4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1879c86f918eea0b74d1d50d3045e3f02aad809f957e5a7a35a41623d47f336e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "61a16633fad5996c5794eef5594a38177661e84667f80a479c6820890cf69247"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ea5ee2861852cfec51994380dfe03a6d49bd1da5aa83347b92e558d77eb45832"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7872f281aa49f7bab33cada17b489514677d79ef4329909ad0fb340e4febee57"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4631252d562d505178149b612e1cf58c01b2a351910efd22e80bd4a6e528cba4"
   end
 
   depends_on "go" => :build
