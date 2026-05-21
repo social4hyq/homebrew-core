f0639185e8b939546dd59a09be57f746ad18a1e2
hqzing <hqzing@outlook.com>
2026-05-21 20:35:56 +0000

    seal 4.3.2 (new formula)

---
+++ Formula/s/seal.rb
@@ -0,0 +1,103 @@
+class Seal < Formula
+  desc "Easy-to-use homomorphic encryption library"
+  homepage "https://github.com/microsoft/SEAL"
+  url "https://github.com/microsoft/SEAL/archive/refs/tags/v4.3.2.tar.gz"
+  sha256 "90c36b28bda750327f9ae172a4b15ad0706412f7d8239471636b94f944dfcac6"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "7cc01d9da127c29dfd802c11e3814b7b2f5a68634183771a6dc2c45bcecc3916"
+    sha256 cellar: :any,                 arm64_sequoia: "3ff8fd30e84624004babcca418e0c6c0ea62a474f20e04a958a8ea554d08101e"
+    sha256 cellar: :any,                 arm64_sonoma:  "39fec0847163290974daf97015cadccd2d3b9cd2f445b0958d66c760e85862fc"
+    sha256 cellar: :any,                 sonoma:        "cfd97a692850aee3755660ad63d81c9bff076f86e4d45a39ccff16683dbaddce"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1d544832700f6e72900c75555f332c5e1a9a63d89ff36c7090ba0352852d1dc2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e89a843fa2378aba5156d3374b36bc47bb273031493e9949594a41bd4bc1ae32"
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "cpp-gsl"
+  depends_on "zstd"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  resource "hexl" do
+    url "https://github.com/IntelLabs/hexl/archive/refs/tags/v1.2.6.tar.gz"
+    sha256 "5035cedff6984060c10e2ce7587dab83483787ea2010e1b60d18d19bb3538f3b"
+  end
+
+  def install
+    if Hardware::CPU.intel?
+      resource("hexl").stage do
+        hexl_args = %w[
+          -DHEXL_BENCHMARK=OFF
+          -DHEXL_TESTING=OFF
+          -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+        ]
+        system "cmake", "-S", ".", "-B", "build", *hexl_args, *std_cmake_args
+        system "cmake", "--build", "build"
+        system "cmake", "--install", "build"
+      end
+      ENV.append "LDFLAGS", "-L#{lib}"
+    end
+
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DSEAL_BUILD_DEPS=OFF
+      -DSEAL_USE_ALIGNED_ALLOC=#{OS.mac? ? "ON" : "OFF"}
+      -DSEAL_USE_INTEL_HEXL=#{Hardware::CPU.intel? ? "ON" : "OFF"}
+      -DCMAKE_CXX_FLAGS=-I#{include}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "native/examples"
+  end
+
+  test do
+    cp_r (pkgshare/"examples"), testpath
+
+    # remove the partial CMakeLists
+    File.delete testpath/"examples/CMakeLists.txt"
+
+    # Chip in a new "CMakeLists.txt" for example code tests
+    (testpath/"examples/CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.12)
+      project(SEALExamples VERSION #{version} LANGUAGES CXX)
+      # Executable will be in ../bin
+      set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${SEALExamples_SOURCE_DIR}/../bin)
+
+      add_executable(sealexamples examples.cpp)
+      target_sources(sealexamples
+          PRIVATE
+              1_bfv_basics.cpp
+              2_encoders.cpp
+              3_levels.cpp
+              4_bgv_basics.cpp
+              5_ckks_basics.cpp
+              6_rotation.cpp
+              7_serialization.cpp
+              8_performance.cpp
+      )
+
+      # Import Microsoft SEAL
+      find_package(SEAL #{version} EXACT REQUIRED
+          # Providing a path so this can be built without installing Microsoft SEAL
+          PATHS ${SEALExamples_SOURCE_DIR}/../src/cmake
+      )
+
+      # Link Microsoft SEAL
+      target_link_libraries(sealexamples SEAL::seal_shared)
+    CMAKE
+
+    system "cmake", "-S", "examples", "-B", "build", "-DHEXL_DIR=#{lib}/cmake"
+    system "cmake", "--build", "build", "--target", "sealexamples"
+
+    # test examples 1-5 and exit
+    input = "1\n2\n3\n4\n5\n0\n"
+    assert_match "Parameter validation (success): valid", pipe_output("bin/sealexamples", input)
+  end
+end
