01d9e873798768bf42ba289b9410fa5169ed1d27
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 15:19:55 +0800

    spoa: update 4.1.5 bottle.

--- Formula/s/spoa.rb
+++ Formula/s/spoa.rb
@@ -6,12 +6,7 @@ class Spoa < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "ad55584bc1d671174019fc33fba25846d45695b1495a8a58e4f569df3f51e4a7"
-    sha256 cellar: :any,                 arm64_sequoia: "e78b56f6b7ccd0c550496370806409371af5aa0e247b19fb6a19766dc8588b98"
-    sha256 cellar: :any,                 arm64_sonoma:  "a233dfaffda20031385fff9037eed19790d91745b7a158c251924de0710eac83"
-    sha256 cellar: :any,                 sonoma:        "6e5926cd309fa1eeac6595bc1bedad2d27a1b9073c263829509cd9a598b286b3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "13f431aa4da78f6726cf32c9d188b5bde27f7120d54db94763cb5f1f7aef29fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d45598cf9672ff7d9daa64cdb07d315980580e21fddb16100b4de4c8c2b849c9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3b9be803ad9e9020fe9480585d9cac836ec421e85cee4f1d8dcf2d9585c9780a"
   end
 
   depends_on "cmake" => :build
