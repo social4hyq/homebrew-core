be0f760b4397f94427fac5b36894ef4fbfbf2447
hqzing <hqzing@outlook.com>
2026-05-21 17:02:35 +0000

    arjun 2.2.7 (new formula)

---
+++ Formula/a/arjun.rb
@@ -0,0 +1,88 @@
+class Arjun < Formula
+  include Language::Python::Virtualenv
+
+  desc "HTTP parameter discovery suite"
+  homepage "https://github.com/s0md3v/Arjun"
+  url "https://files.pythonhosted.org/packages/04/22/c5b969720d2802de2248c2aac0414ee5ae234887cfe150564d591c73fb23/arjun-2.2.7.tar.gz"
+  sha256 "b193cdaf97bf7b0e8cd91a41da778639e01fd9738d5f666a8161377f475ce72e"
+  license "AGPL-3.0-only"
+  revision 7
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "282835520f1b80bc86dfa5dbb7ed8a26689c47515b50d2886d962142dfdf7be2"
+  end
+
+  depends_on "certifi"
+  depends_on "python@3.14"
+
+  pypi_packages exclude_packages: "certifi"
+
+  def python3
+    "python3.14"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
+    sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
+  end
+
+  resource "dicttoxml" do
+    url "https://files.pythonhosted.org/packages/ee/c9/3132427f9e64d572688e6a1cbe3d542d1a03f676b81fb600f3d1fd7d2ec5/dicttoxml-1.7.16.tar.gz"
+    sha256 "6f36ce644881db5cd8940bee9b7cb3f3f6b7b327ba8a67d83d3e2caa0538bf9d"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/82/77/7b3966d0b9d1d31a36ddf1746926a11dface89a83409bf1483f0237aa758/idna-3.15.tar.gz"
+    sha256 "ca962446ea538f7092a95e057da437618e886f4d349216d2b1e294abfdb65fdc"
+  end
+
+  resource "ratelimit" do
+    url "https://files.pythonhosted.org/packages/ab/38/ff60c8fc9e002d50d48822cc5095deb8ebbc5f91a6b8fdd9731c87a147c9/ratelimit-2.2.1.tar.gz"
+    sha256 "af8a9b64b821529aca09ebaf6d8d279100d766f19e90b5059ac6a718ca6dee42"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/ac/c3/e2a2b89f2d3e2179abd6d00ebd70bff6273f37fb3e0cc209f48b39d00cbf/requests-2.34.2.tar.gz"
+    sha256 "f288924cae4e29463698d6d60bc6a4da69c89185ad1e0bcc4104f584e960b9ed"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    port = free_port
+    (testpath/"server.py").write <<~PYTHON
+      from http.server import HTTPServer, BaseHTTPRequestHandler
+
+      class Handler(BaseHTTPRequestHandler):
+          def do_GET(self):
+              self.send_response(200)
+              self.send_header("Content-Type", "text/html")
+              self.end_headers()
+              self.wfile.write(b"<html><body>OK</body></html>")
+
+          def log_message(self, *args):
+              pass
+
+      HTTPServer(("127.0.0.1", #{port}), Handler).serve_forever()
+    PYTHON
+
+    server_pid = spawn python3, testpath/"server.py"
+    sleep 2
+
+    begin
+      dbfile = libexec/Language::Python.site_packages(python3)/"arjun/db/small.txt"
+      output = shell_output("#{bin}/arjun -u http://127.0.0.1:#{port}/ -m GET -w #{dbfile}")
+      assert_match "No parameters were discovered", output
+    ensure
+      Process.kill("TERM", server_pid)
+      Process.wait(server_pid)
+    end
+  end
+end
