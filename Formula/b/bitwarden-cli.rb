da8ea53688540a9469077f1b521e76f2d6063fc2
hqzing <hqzing@outlook.com>
2026-05-21 21:38:32 +0000

    bitwarden-cli 2026.4.2 (new formula)

---
+++ Formula/b/bitwarden-cli.rb
@@ -0,0 +1,38 @@
+class BitwardenCli < Formula
+  desc "Secure and free password manager for all of your devices"
+  homepage "https://bitwarden.com/"
+  url "https://github.com/bitwarden/clients/archive/refs/tags/cli-v2026.4.2.tar.gz"
+  sha256 "5797d89a1553460b6363b3726c5a592bbe8c0591df7aa40485c42dde2f1d01b9"
+  license "GPL-3.0-only"
+  head "https://github.com/bitwarden/clients.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^cli[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e373754c769cc1959b2c1ca20e7e28420887a0ec52978d8135b3d15badfdb2f7"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args(prefix: false)
+    cd buildpath/"apps/cli" do
+      # The `oss` build of Bitwarden is a GPL backed build
+      system "npm", "run", "build:oss:prod", "--ignore-scripts"
+      system "npm", "install", *std_npm_args
+    end
+    bin.install_symlink libexec.glob("bin/*")
+
+    generate_completions_from_executable(bin/"bw", "completion", "--shell", shells: [:zsh])
+  end
+
+  test do
+    assert_equal 10, shell_output("#{bin}/bw generate --length 10").length
+
+    output = pipe_output("#{bin}/bw encode", "Testing", 0)
+    assert_equal "VGVzdGluZw==", output
+  end
+end
