b017a14c5c3773d3463ca486d5290350623c8730
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 12:28:38 +0800

    fabio: update 1.7.1 bottle.

--- Formula/f/fabio.rb
+++ Formula/f/fabio.rb
@@ -7,12 +7,7 @@ class Fabio < Formula
   head "https://github.com/fabiolb/fabio.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eb25e969f5d2fabda13b1d12833c11c7a5097920cfde7bded768cbba021f123c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe868a5ae16369edfdd0caec7034bec710fd78a608b63a8a4571a6452cca3ba3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1fb0cb390188d54d104aee606ec39a49f3e7a2c7a19ffe39033fb441ba48fc6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b37285485f1dc7e90730872a5e27b5e05a8922698839da5616dbf65021b02b49"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "18bfe6c7313ce49fc11b24927bf6633e0f64778084c286d9c602a0abf4b01dfe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3860d29e384f93c3f1959acf37171210a88d357c54c1a00e05f983a08d459ad1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c49c5455fc5ef4264878384c3825ec03549478bf5116a226b0545dce233746b7"
   end
 
   depends_on "go" => :build
