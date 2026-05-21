d51731a669f6c47786fa52a397c5a7130c1599b7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 19:08:23 +0800

    conftest: update 0.68.2 bottle.

--- Formula/c/conftest.rb
+++ Formula/c/conftest.rb
@@ -7,12 +7,7 @@ class Conftest < Formula
   head "https://github.com/open-policy-agent/conftest.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1a509a800966752646d9cdfd8928d30bb6a1d1c66473ce3cd9c68e72b32b7918"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a509a800966752646d9cdfd8928d30bb6a1d1c66473ce3cd9c68e72b32b7918"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1a509a800966752646d9cdfd8928d30bb6a1d1c66473ce3cd9c68e72b32b7918"
-    sha256 cellar: :any_skip_relocation, sonoma:        "59300fe25b9f5c8072fe02e7774bfa9bbc9c7747cfaa2d1b422e8b72fd62ed8c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "af118a9076b99ccbde937a74e3592c22f6c2e5798b4d0497b93d5d73fd0b8bb3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "410e041060923ab2ec2e4f7dda61d7c931a52a040774c207041c0eca5dc963c4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9974efa52bf8edec5f5950b79ea5c3bf6c03ce9448f3b1ee1ca63df52f6f693f"
   end
 
   depends_on "go" => :build
