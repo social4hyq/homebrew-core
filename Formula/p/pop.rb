2d5e3fb58c5f920f41e1314971f9cc499ccf7398
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 21:25:14 +0800

    pop: update 0.2.1 bottle.

--- Formula/p/pop.rb
+++ Formula/p/pop.rb
@@ -6,12 +6,7 @@ class Pop < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "15bf4e71c44dc1941a56c5f2ed7f073e7b5cd8c58aa62d053a8d6544f25e68a1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "15bf4e71c44dc1941a56c5f2ed7f073e7b5cd8c58aa62d053a8d6544f25e68a1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "15bf4e71c44dc1941a56c5f2ed7f073e7b5cd8c58aa62d053a8d6544f25e68a1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "341a35f0e46f9e161a3aba9f12072485c498f5e832863345b9be443018d1427d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8d2dbc83e39eed8dc8d69cf0e513d8e304f0730f34cc21873b05a81e63660da7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3314c12b863cc246833842eda33f371255dae1ddfdcfeff4c0368b1226a82da6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "375844768881e9ef16d9dc35d72a27d38d81ad87bccca3adadb390dc05cabc2a"
   end
 
   depends_on "go" => :build
