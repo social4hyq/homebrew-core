020a553e5411fc2a105a9bd73a68ecbd1c3cc092
hqzing <hqzing@outlook.com>
2026-05-21 17:52:13 +0000

    airtable-mcp-server 1.13.0 (new formula)

---
+++ Formula/a/airtable-mcp-server.rb
@@ -0,0 +1,30 @@
+class AirtableMcpServer < Formula
+  desc "MCP Server for Airtable"
+  homepage "https://github.com/domdomegg/airtable-mcp-server"
+  url "https://registry.npmjs.org/airtable-mcp-server/-/airtable-mcp-server-1.13.0.tgz"
+  sha256 "7dc5eef043b2f0214ead63e6a06e8a9d4645a35d01da60e5dab675047060cf95"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a40b6ad1159c478c26c093c48263ab9ac088d34fb875d39b99173953646498df"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    ENV["AIRTABLE_API_KEY"] = "pat123.abc123"
+
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    output = pipe_output("#{bin}/airtable-mcp-server 2>&1", json, 0)
+    assert_match "The name or ID of a view in the table", output
+  end
+end
