08fdab83e35ee6d5242278613ff433eb870728b1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 16:35:49 +0800

    regula: update 3.2.1 bottle.

--- Formula/r/regula.rb
+++ Formula/r/regula.rb
@@ -8,13 +8,7 @@ class Regula < Formula
   head "https://github.com/fugue/regula.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0495a23cf7487740bfb918ced57eedfaaf47768bb8499e369434a060b612d4a2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0495a23cf7487740bfb918ced57eedfaaf47768bb8499e369434a060b612d4a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0495a23cf7487740bfb918ced57eedfaaf47768bb8499e369434a060b612d4a2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "03a0738fdd999c084d73ca360ff1caab512eb61cf3df26cc5ad03821045cfe65"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ad755b9efc4d3e1513548ec6272d496d3212f8f8e727290bd8d795bee6a1a961"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b284378ae1275d96ebae8ddf706301071bb6d00fceb70983c0795871dc973ee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bd54d9f9f2544ec6cdfaade36d2e034316d5f49f91c94e27569fd80ba74ecfda"
   end
 
   deprecate! date: "2025-04-27", because: :repo_archived, replacement_formula: "policy-engine"
