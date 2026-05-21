1f4a111c5a6730aebed4243b5ea8e4ca9d1ba016
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 13:49:05 +0800

    libtpms: update 0.10.2 bottle.

--- Formula/lib/libtpms.rb
+++ Formula/lib/libtpms.rb
@@ -6,12 +6,7 @@ class Libtpms < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "66fd464e0e778e9d1c3bfff440e5f280891e5eee4d58e2c536a0d855bda23116"
-    sha256 cellar: :any,                 arm64_sequoia: "0439dcadfc4c140158d1a5d7f63c7ffeb3fdc69b4e90943c422e3d5de7664a7e"
-    sha256 cellar: :any,                 arm64_sonoma:  "112fd1f45e4a807f37a3e1cb29039e767c6d202d06209387ae484502d79775de"
-    sha256 cellar: :any,                 sonoma:        "f066899b2329dd5224682c03485d8f376538f888d443f4efa411cbb2ad5d01fd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9647eaa61b0021650f0d1f7bf6f9d6dce81d4df1cf33cedbc2de1315e99035d3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8726633ed87a22297c0943d83329308c0e9187496cfdab7c322789f536e8a4b8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ecada6ccda5069dac7fe876cd7c006abfcf44d635d3aa8645d92324a28b44745"
   end
 
   depends_on "autoconf" => :build
