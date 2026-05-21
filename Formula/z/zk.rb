6059bb12bf7b718834bd9111295598960c3cd6a8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 17:35:38 +0800

    zk: update 0.15.4 bottle.

--- Formula/z/zk.rb
+++ Formula/z/zk.rb
@@ -12,12 +12,7 @@ class Zk < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "d1199b12a32b2a8b2205721d7d9e0b1909ee5e7da79e12c6b7d7cee355771ed6"
-    sha256 cellar: :any,                 arm64_sequoia: "da9a090f84ad99042001f312c812238d0dfcb8e307b45623dd1cb2c45f502513"
-    sha256 cellar: :any,                 arm64_sonoma:  "2454098ebe5d7e54fc1c2b88a07056c97f691469382ff23ad20f247f896b41f0"
-    sha256 cellar: :any,                 sonoma:        "f2c54b6ad8308bd145bdc0d302f3f0994c544ec092861546c24b29d5e985f4df"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5dbf7460386fb6507ab299db1087633d101806834577755f8d01d699c2600da7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5614a228da713bdd80367821902b49a5da8563de49d825ab16cad269352d7dec"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "30928d9b9399d79cb7b8890a5f801b4350a4b97c820940ca3a983b1037753109"
   end
 
   depends_on "go" => :build
