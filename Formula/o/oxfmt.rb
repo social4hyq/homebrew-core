079f578312c5a7d0b05b48859acfdf77d119f0ea
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 19:26:04 +0800

    oxfmt: update 0.51.0 bottle.

--- Formula/o/oxfmt.rb
+++ Formula/o/oxfmt.rb
@@ -6,7 +6,7 @@ class Oxfmt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8203c2dacfb5ca19c9aed20caf97d574c1200a5e760a03da1df68569633e00c2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a6b0ce002117da770471101859cbd216b993aff60c775193d3037cc25276f7ed"
   end
 
   depends_on "node"
