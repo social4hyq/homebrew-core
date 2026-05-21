e050adaff2c2c8a8645423a5c05c3d1a71fcf83a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-21 18:58:31 +0800

    euler-py: update 1.4.0_3 bottle.

--- Formula/e/euler-py.rb
+++ Formula/e/euler-py.rb
@@ -10,8 +10,7 @@ class EulerPy < Formula
   head "https://github.com/iKevinY/EulerPy.git", branch: "master"
 
   bottle do
-    rebuild 6
-    sha256 cellar: :any_skip_relocation, all: "6db4a36f8704a74ee7a62cbd6fb742b57b1586b592b0d5d53f6d24e34bb85be5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1f84c6da88c643ace8a3b2a594c16e0c61b37f64034796a4c2188298f3e005f3"
   end
 
   depends_on "python@3.14"
