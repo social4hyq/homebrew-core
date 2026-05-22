f968163bf17f4364847a796af95b1ea3ce9f9695
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 20:40:03 +0800

    mbedtls@2: update 2.28.10 bottle.

--- Formula/m/mbedtls@2.rb
+++ Formula/m/mbedtls@2.rb
@@ -6,14 +6,7 @@ class MbedtlsAT2 < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5464397f0b136439069dd2c763b20192d423cd5c1c2f0771b031656a660e75a8"
-    sha256 cellar: :any,                 arm64_sequoia: "1ac6bd9c970e98200758af383b9e8295387bd7b4c247dcdcb16cb05521b39607"
-    sha256 cellar: :any,                 arm64_sonoma:  "194e03a15d26c9c866962875b2d1e5ccedab9e10f7ad9d60e01a155c88fdc2b6"
-    sha256 cellar: :any,                 arm64_ventura: "353399bc51df1d729cdcf045ad7da4f268acbc4f562eb9ae9fb18e12658ee9a1"
-    sha256 cellar: :any,                 sonoma:        "2269cb5ed16600a247a38499b79c7418c9af39087c0d0e0775b045e87cd11f7c"
-    sha256 cellar: :any,                 ventura:       "b73d53ebca5b7ca6781171b446c2120d01ca0979c3d510f51026de739650a470"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "839293c23e53ef5576077ab17ca04d66e5ea23dc70b6da9ce5f31672bc4d5b29"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ae36df2af07705fc3817ecc1374a68eb2a21dac30b77e1e1b46970f4ab8e8a8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9743a186024538a11d67c8af4af4f3a132f65523d133899ac522a734669ac415"
   end
 
   keg_only :versioned_formula
