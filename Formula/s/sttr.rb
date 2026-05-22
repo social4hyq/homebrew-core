804ac2b06a49aa1bc405add3d9a4294c088d4a68
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:20:38 +0800

    sttr: update 0.2.30 bottle.

--- Formula/s/sttr.rb
+++ Formula/s/sttr.rb
@@ -7,13 +7,7 @@ class Sttr < Formula
   head "https://github.com/abhimanyu003/sttr.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "41b39d24d4c8cc8e75d384bbd42bd9bd22a7da7ac5954b3ee6d1652cbbd9c9d3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41b39d24d4c8cc8e75d384bbd42bd9bd22a7da7ac5954b3ee6d1652cbbd9c9d3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41b39d24d4c8cc8e75d384bbd42bd9bd22a7da7ac5954b3ee6d1652cbbd9c9d3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "20b9c91d03c9c512dd4f00d557e514f06cc17c44f3341153c2b61e3f4c933b91"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0f4a64e268d9c4e296016ad939191a88a07e5fa852ba4e22e887d90f015047c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "253fef747f7bd342a2253655912062395c5bac02ed98435f38c25ad86b1c5b3c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "97e51dc9b584e291d8677761f669cb633b4f8c5600aa12954e5ba6663dffb596"
   end
 
   depends_on "go" => :build
