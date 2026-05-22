6b38d0012464f8b5d987afd5a93f5fde1b1bd4ec
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 11:02:01 +0800

    cffi: update 2.0.0_1 bottle.

--- Formula/c/cffi.rb
+++ Formula/c/cffi.rb
@@ -8,14 +8,7 @@ class Cffi < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "04b0f56120f37b235dc1a8063149e279bad1cc9132727b25158393db8c930c2c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0c87ffc33c6042cf8c12508099ccbed120d6b76ecef0e0745d728960d48e13b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d588e61a2f80a3c6609fe497c391fc01dc24e2af1fcae3a520a353af32bc827"
-    sha256 cellar: :any_skip_relocation, tahoe:         "1252a16612b1585b96a870ac5c1d0af6beb31c1cfa76449e6599f87b5ec81698"
-    sha256 cellar: :any_skip_relocation, sequoia:       "050dfb668072267e2bb9e2eb03a02b31f6d15ecbce1c823ab0af521da3f05223"
-    sha256 cellar: :any_skip_relocation, sonoma:        "76c34aa7a07dff752819c015de41f2ba14d42441cc07cd7e4a4be7fd8baa8083"
-    sha256                               arm64_linux:   "cb50ba60bbd5cb6b2da0637c3f3090d07d3a68665076c5f553c65bfb061b1734"
-    sha256                               x86_64_linux:  "bad3335bd82f947b8283832392a0c37035e500e5732dd63b0f9878dcaf05d639"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cf0df5ee21a406a43e12002568e9f051cba6312a593cfd6f764013aa27639021"
   end
 
   depends_on "python@3.13" => [:build, :test]
