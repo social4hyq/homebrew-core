64fa922ded1d68c523fa2aadd6506d3dc335a8e9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:09:07 +0800

    hss: update 1.10 bottle.

--- Formula/h/hss.rb
+++ Formula/h/hss.rb
@@ -6,12 +6,7 @@ class Hss < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "2bb2eb21cf15af087ab7ed8e764cc90fbdd452f1ea0e81c71d79a20cf099cc83"
-    sha256 cellar: :any,                 arm64_sequoia: "ab1d14ee93ade63ab4f7ee790cfc4ef658871bc7be566039b0f99bda7830fb06"
-    sha256 cellar: :any,                 arm64_sonoma:  "3606b82b29cd0c9de3db42b5089f1c8744a009fa229534100142a3091fd50f3e"
-    sha256 cellar: :any,                 sonoma:        "04fe2ff9d6beae3e4f5b1774710410a31b301ff0617424c21798b784109f425b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "98dd1270d4dae19983c37c4f415236e7a8ba168b5c6c2885552bc4c7639ded5c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85d4b12a7f8b94e6394e8252d2b81230d96f1dd554bdf0d7669552ed93c52184"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8bee7bd1f3641fd1ee075938fd8532ce96ba9994d9f62b877a5bfd3b78f8144f"
   end
 
   depends_on "readline"
