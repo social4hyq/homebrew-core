696db309dd24fa4eb7dbf686d5ceb5f038adcb7d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:22:47 +0800

    fairy-stockfish: update 14.0.1 bottle.

--- Formula/f/fairy-stockfish.rb
+++ Formula/f/fairy-stockfish.rb
@@ -16,12 +16,7 @@ class FairyStockfish < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "44c93f2b4f6e3b7ff1daa96bce98e9bc64b34c343921b6e145a23bf153131b2c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8b70af3b59355821eaf2ee870e0f962159bf599b8964c1ba965cc848c0cd9708"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3523a6ba3cf152a83e798aeb19298fc8b59df95194441f8dba655d56264867c1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1943dab5d7ebe253c43cde8c33d5683a08619be682dedc41f6b4c107f0718c71"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f4ffe9bc9209dbe890aac368fe2c74f7d6793ea9ece4ff6f837148dfc3405f8b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8316c36ce185f713c34e19e6da7c5d5033b4d7ffd4d850d6fad6e65b5d187683"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "19b667a2a6fe430934351b3ee2a4bc4fce512b957420065114a448e49d4a7824"
   end
 
   def install
