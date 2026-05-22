62ba77fc90968214437dc0bcdc937335b7b38970
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:02:27 +0800

    tenv: update 4.12.2 bottle.

--- Formula/t/tenv.rb
+++ Formula/t/tenv.rb
@@ -12,12 +12,7 @@ class Tenv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b713ee5113fbf6621dbc494b8aea2c5adca8fc9625733e07027af0659a4b88c2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b713ee5113fbf6621dbc494b8aea2c5adca8fc9625733e07027af0659a4b88c2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b713ee5113fbf6621dbc494b8aea2c5adca8fc9625733e07027af0659a4b88c2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "997b7392776c086b0e1abb293a59afd5d0f62eda6dda4ad22d6ac7b92dd14c0b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "77b53be4eaf7c2a4471e6675f0830d7f1cc11da7b69f2281f03efe40b77963fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ad748396d0784ece8574ae703c51516474bacaf75cb2509da550b2d78c233da"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "457434b750ffcc25aebbbba33c6330b36f7f085167f258fc20539f1809a79b50"
   end
 
   depends_on "go" => :build
