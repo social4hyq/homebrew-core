ea3dc4474fc17c010df0bb1717ef4df0a6d22ef4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:52:22 +0800

    base16384: update 2.3.2 bottle.

--- Formula/b/base16384.rb
+++ Formula/b/base16384.rb
@@ -7,13 +7,7 @@ class Base16384 < Formula
   head "https://github.com/fumiama/base16384.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "1f5f8def255ca57e635603ef572bb652a7920bae85be2e2c84a13c446d6d4615"
-    sha256 cellar: :any,                 arm64_sequoia: "deeb2188d7479f513249484059fd1de2adbeeecc433b5017bd352ca29328d9c4"
-    sha256 cellar: :any,                 arm64_sonoma:  "7908aa72a066df28976ac2c33318b3bc14dcdcdb14e03f075aaef4e49f927b0d"
-    sha256 cellar: :any,                 sonoma:        "b9c40a9eeae7fb639505085bf436817583a4bef68ab415966a232facbb1634b8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "292b2d8e8b87a90ee2dbb426a69ed69f8c58945cda95c879478eeab33d38c818"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3010ed072ea82794a4f40a0ec7a11e2bc51a6f0a8d25aa18483be84976b9a2b3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a92c622d9a78c7f22c57a5c37cfe6d0a123e0c7bab8a672f3e23c8d51fd1358b"
   end
 
   depends_on "cmake" => :build
