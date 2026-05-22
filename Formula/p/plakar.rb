9592fea25d4160e14527533957db2d9e8a54a751
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 14:33:10 +0800

    plakar: update 1.0.6 bottle.

--- Formula/p/plakar.rb
+++ Formula/p/plakar.rb
@@ -11,13 +11,7 @@ class Plakar < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "054379a5d933068e2b4cb8f48fc11769a05ec55f5cc798520266046948bbee35"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f34a5a006de72588b106a09723fb32c3c321681652072687799cfae22ccce3cb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd24298b97a6640cfe6c9b903e0a0764f79495cb7bc0b59e149a57b2690e44e5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d4ded53b51adcfce38ee43603aaa60d7cc9222e26f18ca0ece5a69dbc5927346"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5a75c2526701fa560eb4af9eb9f49ee348a10591a2de1827b19c82efe6986532"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58e4bd7f403dc08a91f22db461e9e3647aeb5fde6c269d096bad97a5eca34f71"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "87c0bd36cc1cbb9ee2b33f6b815595b43a2e3081eed4f2fa8b2ed91bb5017a9e"
   end
 
   depends_on "go" => :build
