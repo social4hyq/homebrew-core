d16bbfd55593c5d06b9e1823c286bb5c7313df3f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:06:59 +0800

    dnspyre: update 3.11.1 bottle.

--- Formula/d/dnspyre.rb
+++ Formula/d/dnspyre.rb
@@ -7,12 +7,7 @@ class Dnspyre < Formula
   head "https://github.com/Tantalor93/dnspyre.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bb15fd8e32c9e177b97b3e0be8fa718aab92fc0680ef6d8e97ead1ca58cdd7db"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "172a6b5c3596fac0911fa73d326a60b49978408472bada0f4a0a080e77f4f088"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5eed0ed8dbcb550819c13a0445ccfecb31e84445fdb3467a0e4428c23e8dd6a6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "292bc522e5f6cd0e58ad7ca08586b6293fc639e53940d9dc66b916dea967e853"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "33747e1aa7f4d87f9726a51a8f222f1ed5803f4ab457d801dda7a59402f1ec9f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1fafb75512b9461e17be172e640139a9cd79cfe53a01aad6cf3e03f3e6319ada"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0a1de084a7708f381d5eaf66458f52aedc9678b4c210542b8065f9c4f34d5030"
   end
 
   depends_on "go" => :build
