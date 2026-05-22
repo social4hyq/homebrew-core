9efec95b444ad0126577f3b3a46db831dd0dd029
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 23:12:40 +0800

    htmldoc: update 1.9.23 bottle.

--- Formula/h/htmldoc.rb
+++ Formula/h/htmldoc.rb
@@ -7,13 +7,7 @@ class Htmldoc < Formula
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 arm64_tahoe:   "a65465e42cb84f558378c966a77ae8b7ca0409bc8686f89c62b4fc45d55bd18a"
-    sha256 arm64_sequoia: "f1e6b353bd87fbe41d903d7580d4710d709597c2409b519ea0aaef820a369034"
-    sha256 arm64_sonoma:  "738c308de152b3f2f9df6bbe41b8ba2b6e120759549f49281820693794384e11"
-    sha256 sonoma:        "25ade32fb291157a367cb4900027b04baeb229a21aac12516ede7caa86b1a583"
-    sha256 arm64_linux:   "85a884b0c94907488539fcd47a78530fe1102045e052dd14647bdcbcb4de4556"
-    sha256 x86_64_linux:  "9bc6c13c652f278395d6a156d10b1f5cc9afccae9a123fb465b840d0eb9446be"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c7e2610f270ed2bc841a171ea39e541de97153d76a713bac23fd42367cca1747"
   end
 
   depends_on "pkgconf" => :build
