93011dda69f9661342940dfdf7dc89ab9685d8c7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:11:27 +0800

    jreleaser: update 1.24.0 bottle.

--- Formula/j/jreleaser.rb
+++ Formula/j/jreleaser.rb
@@ -6,7 +6,7 @@ class Jreleaser < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d6f6218dd3e0dd00b8ecb9a67ac313a9c186c822eac9352bfe5f92a812b1e859"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "438f0a3d93fce61457ca55629fe96e452805c153a16566f476c9b34d75031003"
   end
 
   depends_on "openjdk"
