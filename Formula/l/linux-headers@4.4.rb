28176c38b450d656607bb6ea654c0c0a608d6187
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 12:16:26 +0800

    linux-headers@4.4: update 4.4.302 bottle.

--- Formula/l/linux-headers@4.4.rb
+++ Formula/l/linux-headers@4.4.rb
@@ -10,9 +10,7 @@ class LinuxHeadersAT44 < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "b809f68c023e71c860af7b6fa6f57f6e3e55130fc69b20fc42f8c4c9685b7b3e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2b2ca7202c544d6a14778915175fc811e370cd7a5249c6d0216c83d78cc421ed"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "17e919ef8620c1e35edfb1dd3ba690e4878827dd0a45fe5ee149c7fc93c692ec"
   end
 
   keg_only :versioned_formula
