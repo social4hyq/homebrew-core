492abd2517ccd332dbd09c398ef7e2a8cbceab8f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 14:55:13 +0800

    oggz: update 1.1.3 bottle.

--- Formula/o/oggz.rb
+++ Formula/o/oggz.rb
@@ -12,14 +12,7 @@ class Oggz < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "603716b336363c7460ee420337b0de8c0577261236bb6f464b95442f634c103c"
-    sha256 cellar: :any,                 arm64_sequoia: "662621ea7c3e2bba651d8a4d58de1b7e11fe77457919bec4a1a891344a9fa52b"
-    sha256 cellar: :any,                 arm64_sonoma:  "b73a313c07c02a73751ff8837ff86eee990bb438c7391f3ef401ad7252a1b2fc"
-    sha256 cellar: :any,                 arm64_ventura: "574236c1d041249054c6767741940b013cebc9771ca97108fbab98973d4b5898"
-    sha256 cellar: :any,                 sonoma:        "5d24d31ff9a43ff9889dea391bb86e16016efec62385d0b17e8648d0242e3bd1"
-    sha256 cellar: :any,                 ventura:       "734cb42ad73f66af33c147440e1af89b3a3e1f508dbb902e12f9ab164e252aba"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "38b2592995a25f2e1024b80fce55beed16e179fa53ee4306671e61f9b6624ec4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad3472b35ebd01c5335c7557dd3274fd27d365e09722e0beed0749e0e47c28cb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c0f29349901884ef0e36ae5e33c509f65223f53338ccbfb682accb961da82c34"
   end
 
   depends_on "pkgconf" => :build
