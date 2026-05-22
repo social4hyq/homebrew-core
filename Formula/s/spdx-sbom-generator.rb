e6f9405d5a816a3be504ae8bd0f1e2632fee3843
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 11:35:23 +0800

    spdx-sbom-generator: update 0.0.15 bottle.

--- Formula/s/spdx-sbom-generator.rb
+++ Formula/s/spdx-sbom-generator.rb
@@ -6,19 +6,7 @@ class SpdxSbomGenerator < Formula
   license any_of: ["Apache-2.0", "CC-BY-4.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "3d5f579302dcda06b94f698a0cab5ecf485d399b4e9da91a33433a992c2684c0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "ca95017f73479fc8907cb24a392bab8d15c69789c565d0f2129502e00d520742"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "489910223a1ba3e54e06461065428738c88018a25f2d82f7fe4e87bb8e8eb552"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1905f9d0dd236c453fe5579857110071680d4241ad4eedb1150bcd5163208fdb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "75f26e3e6fef82389087371f38b3a90081f74a6ad7f0d8dfc2d25f5908600ba9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f56b5a0cc9581b2f7347a23264cd70dcb76e1fa170ababf583fa1d4fad6b36e6"
-    sha256 cellar: :any_skip_relocation, sonoma:         "7fe1dd10d9e54db236333d077866b2ccf958d28b70d6961df38d51e900e517bf"
-    sha256 cellar: :any_skip_relocation, ventura:        "4090620fb4c0f354d773f76323a89052c356d46fd45f311204cc6c3e15644036"
-    sha256 cellar: :any_skip_relocation, monterey:       "af8c523abaa929f3616245751392b6fe9ba998e0f88e798e831d66def859fd88"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3f116d9eb974cd064162a5c55c0143b8b2bf2cb2534b76a27eaedfcef6031da6"
-    sha256 cellar: :any_skip_relocation, catalina:       "f733c9630d8ad36f7ded3b2f9a10267251052625fb63e21fb3acc00f7863f919"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "d00ff724f5f72911dd7c49600f95a556d18c4654588d6ae4a0a79278431ce29d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd798a009fd918ecc7ebc002b181fc87aa382722f07d6ffdc5b2ab0481ea3951"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "857def5d94ddd4bd8a24cbd41fd9f8470dfbf1c17f93036e2e69f609dda4e614"
   end
 
   deprecate! date: "2025-02-13", because: :repo_archived
