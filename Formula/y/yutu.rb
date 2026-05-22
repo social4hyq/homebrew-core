5534f7fef168506723cf5a895019b3ab3633c11e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:40:07 +0800

    yutu: update 0.10.8 bottle.

--- Formula/y/yutu.rb
+++ Formula/y/yutu.rb
@@ -12,12 +12,7 @@ class Yutu < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c545dc4c3b262f641dad584a7e89c1c7f850d0cab360d1409b1f0e04a5a7ea2b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c545dc4c3b262f641dad584a7e89c1c7f850d0cab360d1409b1f0e04a5a7ea2b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c545dc4c3b262f641dad584a7e89c1c7f850d0cab360d1409b1f0e04a5a7ea2b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a97054e668c2bc5669f89ee0ba1c6fbc1d2667af3ee5a592027b744800686f88"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b4a642c3dbabe9fcc2871685aaab9c5a2e12b2bb1025d2a14dc200f6ddcf8d33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b5ca5eec04e41f45122fcf6376aed04aa5f7373729d7c3aa78e9c1013100e55"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "424aca9896be0435faf73cb4c2f8fb3d951d3ec908882470049da3c12407319c"
   end
 
   depends_on "go" => :build
