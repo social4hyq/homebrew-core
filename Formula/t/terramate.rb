eeccd7a80606f3d2915983c9ee555424e12acace
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 12:46:31 +0800

    terramate: update 0.17.0 bottle.

--- Formula/t/terramate.rb
+++ Formula/t/terramate.rb
@@ -7,12 +7,7 @@ class Terramate < Formula
   head "https://github.com/terramate-io/terramate.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "263917efe5ac55fb415f96f3bcf5f25d18ffca8a54554937bac5adfce2720366"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "263917efe5ac55fb415f96f3bcf5f25d18ffca8a54554937bac5adfce2720366"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "263917efe5ac55fb415f96f3bcf5f25d18ffca8a54554937bac5adfce2720366"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e36c6dd8ee0aa8af981bf23990c9c694e88a81c51580e6746903526d5b2ea9b2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8fcb1501717fdf8f21222d9c7eebe98de6d48fd34087b92ae280761775f67df4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8936b942af15710f4fa711b4a377d306636515cb6b36a8f329e6af2563a02c7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "643ecf473a53a2d8687479d40ad83af9c7d7654efadfbb25d6d869434da42533"
   end
 
   depends_on "go" => :build
