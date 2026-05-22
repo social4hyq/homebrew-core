45d16aa1c8c8ab191aa6b296ac868407f2949a46
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:34:55 +0800

    foxglove-cli: update 1.0.31 bottle.

--- Formula/f/foxglove-cli.rb
+++ Formula/f/foxglove-cli.rb
@@ -7,12 +7,7 @@ class FoxgloveCli < Formula
   head "https://github.com/foxglove/foxglove-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cbecdaa31a416270a4352110b4e432570dbce113053889791a8b2aa115d8ee44"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e08df2a52faad8db844e5172f41d1ed7c73550e52abbec88083fa1bf4b7d1ef"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "16ed66cdbff59115706e390dcc0aabca6e25d7aa8e77df27fb6ac94952bbf558"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0e856beaefef6f0e557ee0fa19ac3e396fd8e34682d4adb9e324ec18c78801ed"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "115a2034a4c16aa7285f22f3b66383a8a82fc4824a42278d300ecfe229089bff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ccd8d2a1f61d8d8116e475a55f8fe350114b18dfa73989ccea90d7775f97d905"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cc6b98b5173302ed38458079571ea2eeb13d830c8be8e9bbbd3ba6d3c87f4d71"
   end
 
   depends_on "go" => :build
