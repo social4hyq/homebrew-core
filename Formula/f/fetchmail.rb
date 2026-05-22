eaa1f3381325573aae5f47548b6e1bebf50d4321
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:12:26 +0800

    fetchmail: update 6.6.4 bottle.

--- Formula/f/fetchmail.rb
+++ Formula/f/fetchmail.rb
@@ -17,12 +17,7 @@ class Fetchmail < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "3eef14d0a0b0fffc3b2f6ddbec6da2fefe1a90e19fc281ff3411e7de36ea6248"
-    sha256 cellar: :any, arm64_sequoia: "d58409b9692beec1b1bdc8ca37210ac4991f3cdef26e947676e95ca487ec70b4"
-    sha256 cellar: :any, arm64_sonoma:  "6d7886b25f77b93eb8d7f15a560dc7132399c3ca7595549563667029bcfd095e"
-    sha256 cellar: :any, sonoma:        "fa8975f9360b7b42aea87f6845a5ca59f48e6512f5e7ffb82b922bc259b13196"
-    sha256               arm64_linux:   "9b6ac5cc80e64d6a57f92f7e0bc18b69fcf6965129a4230ea89c09582ece6cb4"
-    sha256               x86_64_linux:  "084fcfb03b211a040733b291dae9b6c11e8c0e3dbdf5a7afcf969ad0710ac89d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c18d07802773ac221a5a430e4233215e1b24627f019ba23421f65efc23a1cd22"
   end
 
   depends_on "pkgconf" => :build
