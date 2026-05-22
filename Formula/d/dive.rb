cc0534d33882060cce72c567724a88375bf416df
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 23:17:50 +0800

    dive: update 0.13.1 bottle.

--- Formula/d/dive.rb
+++ Formula/d/dive.rb
@@ -7,13 +7,7 @@ class Dive < Formula
   head "https://github.com/wagoodman/dive.git", branch: "main"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a2a98b730f1f2fdd600b00603db02e66f43334a703e3e42f7ea019e333d9c426"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a2a98b730f1f2fdd600b00603db02e66f43334a703e3e42f7ea019e333d9c426"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a2a98b730f1f2fdd600b00603db02e66f43334a703e3e42f7ea019e333d9c426"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2a6716f77316ff980b2a7b4bf794192abdf9e135968eed6dc486d4528478c272"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "964ca148b9e1b73a5565a266db9a61678e145666d9326c6bd86aaa521ec3a8f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea8b52ef11f336b96dcf14916b461f5da574a13da8e7fa6b29d12a058646af2e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9d384d104560a65957ca056e6a41af74d9372f03e0a1c206f17c820bdb37cfd1"
   end
 
   depends_on "go" => :build
