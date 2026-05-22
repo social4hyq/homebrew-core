f3968185e8b25fa9c8416305f437e3546300f1a5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 23:01:48 +0800

    objconv: update 2026-05-14 bottle.

--- Formula/o/objconv.rb
+++ Formula/o/objconv.rb
@@ -20,12 +20,7 @@ class Objconv < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f06a2e64300c2685c083fd95df7bef59538666948122ddc86ec034da588d0f08"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c9c93c8dc00a939a982e3f91ead2bb123398c2d51b16012e884e68940f64279"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d273ae1e33ceead2ffd568a7fad04359b97b55c01961d537f58888b594b0a37"
-    sha256 cellar: :any_skip_relocation, sonoma:        "62afeccb878f94d41864cc49e74823fc1c9be2d95cdc04547f921cc0c1d41ecf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0575326904e82bba08aef0be413897313d89fb7046d0dddb3a89b6a56d9fab2c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "54a33822922a82171119df29ba1f0a86ffb40e5be12ed88863482008a6f57eec"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9c6e4ff91cfaa345637709a958785c98116562044ca99345aa6d763161c9b98d"
   end
 
   uses_from_macos "unzip" => :build
