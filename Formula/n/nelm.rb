17b9d5dee5e45015189f46bf0301986306f45dcf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:58:01 +0800

    nelm: update 1.23.1 bottle.

--- Formula/n/nelm.rb
+++ Formula/n/nelm.rb
@@ -14,12 +14,7 @@ class Nelm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "36cefbcc7e5ab4f20f87f9f514a26faf51d69418ce1e2f8141c6c8b9515bb9fc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e1335ad709081f28cc09a7c3db5ff52e540ab0313fa40d5147bf0f022a8b451"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "47a594c57b1b1d149cb72cbbe713193808b4a31e07277a6b15e4e0af1cc851db"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ee2a92647c60949c409b8531f1c8a3f44342a5693e4365a8510bd6228ec24d5c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "19aeaa6b4f63f993fdfc5ea89a0f8bacb83c6d9661b9e6333cfaec4e0e8627a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "952aad4b1c5c2944bc427d9436a10dab3c6aa20782213fdfd8f1591989a863d0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "044d6b84234695ab33ca64112dcf1edfedff46ad68324866afba3e1dafe25a96"
   end
 
   depends_on "go" => :build
