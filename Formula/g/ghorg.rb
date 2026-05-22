5a70ed5738abed6216bead8d678559c5c8d62a8e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 23:13:40 +0800

    ghorg: update 1.11.11 bottle.

--- Formula/g/ghorg.rb
+++ Formula/g/ghorg.rb
@@ -7,12 +7,7 @@ class Ghorg < Formula
   head "https://github.com/gabrie30/ghorg.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b8ede1256526f9b7bbdf4b914de96ea944c75174608333df52270b0cda0618c5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b8ede1256526f9b7bbdf4b914de96ea944c75174608333df52270b0cda0618c5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b8ede1256526f9b7bbdf4b914de96ea944c75174608333df52270b0cda0618c5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "92ad2c66d7530401e8dc28b469963c9d64a86a4b118bc0e8a47aec1a7588bc77"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "767fa919a31a01628ada3adc4cb84937f43173b823939ac1c9aab899ddac6ee6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85e44f9db27e124ebd6545f44b9dc285c5029896981b0669a7e7bdfe14263818"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d569cc33fbf03ed446d52e3d1b24a6ef1e36543b21a6b3f14c1765d9bfc95330"
   end
 
   depends_on "go" => :build
