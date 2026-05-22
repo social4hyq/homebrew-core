fdb0684eec32fd916a2a4b3aa6929f0d85618bdc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 12:18:24 +0800

    tiff2png: update 0.92_3 bottle.

--- Formula/t/tiff2png.rb
+++ Formula/t/tiff2png.rb
@@ -7,13 +7,7 @@ class Tiff2png < Formula
   revision 3
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "cc94154a902618ac2619cec04577a4dc5532ed13adff9506fffd4180521d5b78"
-    sha256 cellar: :any,                 arm64_sequoia: "52071a61b3b8a6a78b8d48025aabc0ad60cf8c4945ab879bb44e884b36fc56db"
-    sha256 cellar: :any,                 arm64_sonoma:  "0dacffb8b0bad6b6e7ef4493a2e97ac17198fdf09f78918d8b8ae3a07f47bd5e"
-    sha256 cellar: :any,                 sonoma:        "062634cd7eafd6689cce9dad0335fa49edee9523df3ad90bf0b1f27c9637ec18"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "421b0c9de1f281a0bc9656c60e53dfafa075bc9e398c5d20e6063d8f7eaf823a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26cc20cd78f76377d345f2c3517b66f069319067eeeb83d72ce0b6f74a122282"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a132709b65202e40e91c8844d752c9a25ece144a0fd2a59edb310d4f2adab78d"
   end
 
   depends_on "jpeg-turbo"
