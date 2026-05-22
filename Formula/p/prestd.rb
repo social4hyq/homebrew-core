daf1ad9b5049bff7e1c6e5de50d73477b06eeada
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:32:52 +0800

    prestd: update 1.5.5 bottle.

--- Formula/p/prestd.rb
+++ Formula/p/prestd.rb
@@ -12,13 +12,7 @@ class Prestd < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "acc8c8862ea6867aa11b5a5e9431c978c6252bc9c267b455cb004be3c4589efa"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "49bb751ee32ae1ba13d0e1b87b801b4b3f80d74adfdabc44467068c0abcd1a12"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1d02886e9ac10386dc793878548d3429bd99405212f10a27d25d63519702192"
-    sha256 cellar: :any_skip_relocation, sonoma:        "adaa7ed259ca16397bdc67c323eb8969aa02b6541275b892f98efae4ae49bb36"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "067511ae084f7eeb570344d47d52ed78ff71b5a679c92bd8dfb63d411ccb7307"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ac75a592d6bd7599df97d1ee3cd17d52c5d6c3ab8e37834a03a45abac8f81b2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5d6eb7a85261fa217723abcf3a83127f2b11afebc0807925d8046332a1a451c2"
   end
 
   depends_on "go" => :build
