8f844fbbf117c52cce3002a2f4c400410d0a3128
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 22:42:26 +0800

    idsgrep: update 0.6 bottle.

--- Formula/i/idsgrep.rb
+++ Formula/i/idsgrep.rb
@@ -11,13 +11,7 @@ class Idsgrep < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_tahoe:   "599f0677a847ef317bb1cf0a80501b84531bf0a3083f6cf31052bb5f70bab0fc"
-    sha256 arm64_sequoia: "1c94fbfcf90ac7576c2b006a8ee3c80f07a4013eebb232eec219b47203b8a765"
-    sha256 arm64_sonoma:  "84923961452a0d7c070b7c78375c936fedff5455612aa226e747df022cfa1daf"
-    sha256 sonoma:        "d3d1fc3dee5ac6f139f457881bfc85f865ed0ed5dd94fa856396085c9bebcac7"
-    sha256 arm64_linux:   "656ffb858db27d6e80c9df19854d995ee1d45c0aaca683b2048125ad8021dc80"
-    sha256 x86_64_linux:  "4ba4398d84c8f0d78e838d5b39182bd0eab662606d8a77e895fc663e5558307d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "896cf12377790d7394f7f16172eba49c7c371f5532120e8fee82baff5e118ba8"
   end
 
   depends_on "cmake" => :build
