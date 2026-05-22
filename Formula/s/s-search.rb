e350234a8327b3ee2799e285dda32ca7ed755f94
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 09:29:49 +0800

    s-search: update 0.7.5 bottle.

--- Formula/s/s-search.rb
+++ Formula/s/s-search.rb
@@ -7,12 +7,7 @@ class SSearch < Formula
   head "https://github.com/zquestz/s.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f5da527e1a6e4bb8b7f9b79799312c08a1851c1090c34198b9419ee00e402a59"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5da527e1a6e4bb8b7f9b79799312c08a1851c1090c34198b9419ee00e402a59"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f5da527e1a6e4bb8b7f9b79799312c08a1851c1090c34198b9419ee00e402a59"
-    sha256 cellar: :any_skip_relocation, sonoma:        "50de2910152b0055f4e60acb0a131d4ba1d6f9cfd8c6fc01fe3ec12ff8a6aaf3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b00fcf7d658b762f43a5566355864fb9ca076e5925c2e260c8ff030d867d2169"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "77b55c3ddda370d75aa6e1cdee019b80de44884548c0476eb1d99ccc53722389"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d101f2098a89eb05be8396b9b519f8c6b6ffb173deaf1edf5fbfc83dd963e780"
   end
 
   depends_on "go" => :build
