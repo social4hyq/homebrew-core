90cd448593abd10afcf9ce321a93abc830d21fbe
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 15:04:16 +0800

    xmp: update 4.3.0 bottle.

--- Formula/x/xmp.rb
+++ Formula/x/xmp.rb
@@ -6,12 +6,7 @@ class Xmp < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_tahoe:   "4b0ed19e822b10ffb401686f212cd40e8317b2b2a8062131aea81af29e1ca840"
-    sha256 arm64_sequoia: "827d5de28c8c9c3c2f8b91f7e107fa1ec24777e75e70b59ba0f6e22dd7fa6263"
-    sha256 arm64_sonoma:  "f76343ee866498fd243770813aa6540ce0076462ef7ea031093890563fd0b6c6"
-    sha256 sonoma:        "ba07f514ac50b72a5a0af249aa53610b6615778c7dad7edeedc06dd8acea4d6f"
-    sha256 arm64_linux:   "7b8bb21bd902a1b7fb8288e7fbda094b365cd1146bb407a12b68b45e29ca8507"
-    sha256 x86_64_linux:  "2c7e626e658c88f98a46aff117b9fec0b248493840375532d58fcf8660ce333b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f2c5416e3ba6a1b6ef44e3cb55aab555e60511ec5bc7a433012d4d4dad488ad5"
   end
 
   head do
