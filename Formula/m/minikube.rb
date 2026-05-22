5aae9dff47db200c04d2df68b78c5cc4e1791b1f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 20:27:30 +0800

    minikube: update 1.38.1 bottle.

--- Formula/m/minikube.rb
+++ Formula/m/minikube.rb
@@ -8,12 +8,7 @@ class Minikube < Formula
   head "https://github.com/kubernetes/minikube.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5c31e8d9b87939095f0417654c408364fee92953905447c1128ee6c5d70c72a9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80690c46408b61a2073e533325cf16cdaf4777c66eec5c2e3a87b983615d1e1b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "430e398f58a5cd08a7f65143fb05a847a8d53304cea571818ed25b0d5ce716df"
-    sha256 cellar: :any_skip_relocation, sonoma:        "97505b2bc08deed55885c7ca457f3f3b1150663818bd607a0927968eee778729"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ad4573edd8932cd1f1cdd9f82b360f97124ff63fdf161ac9a145b0444285def"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f82f02f15af2c9b7931dd0c240bb5ac63d40276325ed53c1bcfe5c4aff72efe"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "69e0855d505f4434af4d6f4f05182759505949f6c027bb17b67317b7f422e4f5"
   end
 
   depends_on "go" => :build
