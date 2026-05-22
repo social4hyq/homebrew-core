36920d0508c0792df6c1772c84a611320716c8c5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:06:05 +0800

    xc: update 0.9.0 bottle.

--- Formula/x/xc.rb
+++ Formula/x/xc.rb
@@ -7,12 +7,7 @@ class Xc < Formula
   head "https://github.com/joerdav/xc.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6f689507921d13ac9c5c3b31858a07034fdde3c9b2745cff3965bba02b8de3b7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f689507921d13ac9c5c3b31858a07034fdde3c9b2745cff3965bba02b8de3b7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6f689507921d13ac9c5c3b31858a07034fdde3c9b2745cff3965bba02b8de3b7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e56d4eef3826dcd65197d0b5f38eb49632d374163b59af75695050645018d1f4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "61c8010926e5f1fa66da4eb63c21dafe8e5f92febd5de72d3d542f309a25c3c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "965b9b02792fdfae0f1072431a50dea31e8df09bde42521c3b2eadb9432b5c09"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ec8e7697b03859fffadc170a01abf8bebdf54e07aebc71fbb0f660f5ae17f201"
   end
 
   depends_on "go" => :build
