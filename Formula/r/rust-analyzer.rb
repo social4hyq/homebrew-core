e52b122ea3a2ff70ae077acdaf5d483d0fb7c05f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 20:26:10 +0800

    rust-analyzer: update 2026-05-18 bottle.

--- Formula/r/rust-analyzer.rb
+++ Formula/r/rust-analyzer.rb
@@ -7,12 +7,7 @@ class RustAnalyzer < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "66a29580b501ff5b3cc8beb11e53fac4a4f383a403167788e56e5255bac7d612"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "88f485ee5c0c7523eb32fe1ac5dadd8c7827f3c1f1b2f2403ba7b00bf9bc59ad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "429977db0d1d1894bac7a7378a4ae7fdef4fbf2809d3c1dd04d3f077dfd9c68b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f9f9db8d76b75ed831384e52cdd3624c9269988e68dcac7374b94225221fba34"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5c24bfa5ad352afe2c607cb05d0eb142f6982baba1137fc7abe5ecda6edaaa17"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2220b56ceaa22efd793883d43c842d2ed847d292c83bed468764ffe4a53a06f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0e12b754abbfb2434ef31e1db4f78cd12fd18d045c7e020188f288fc32dbaa31"
   end
 
   depends_on "rust" => :build
