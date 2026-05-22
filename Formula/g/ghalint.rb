228e60a89c99cbce5c82c98da8e13b1c366c7306
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 22:17:22 +0800

    ghalint: update 1.5.6 bottle.

--- Formula/g/ghalint.rb
+++ Formula/g/ghalint.rb
@@ -7,12 +7,7 @@ class Ghalint < Formula
   head "https://github.com/suzuki-shunsuke/ghalint.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dc6245ebb383c63ad0be81107c0a6d6082bd049c8733a488ce7f8750fca1a087"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dc6245ebb383c63ad0be81107c0a6d6082bd049c8733a488ce7f8750fca1a087"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dc6245ebb383c63ad0be81107c0a6d6082bd049c8733a488ce7f8750fca1a087"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fa7695f943362c7bc2c992a4193116a24b4b067eb95748118e4ddb6e4239ba6c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e3ae564117778a881047c0bc57755a117a6e6666e9d63d0a2c1964528f755605"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92b02ec283b1788992a002e459295dc3c6303f3c3b8f60d1206537c2aefe5bee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ead976b9d7a1c460bccf3c1d3279fbd4ffe6d9b66b3ff87c76c29ae6e00beb1a"
   end
 
   depends_on "go" => :build
