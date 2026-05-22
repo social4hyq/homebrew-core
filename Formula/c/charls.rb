820fc3c95347107567137020a3e0e242b2375162
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 20:30:17 +0800

    charls: update 2.4.3 bottle.

--- Formula/c/charls.rb
+++ Formula/c/charls.rb
@@ -7,12 +7,7 @@ class Charls < Formula
   head "https://github.com/team-charls/charls.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "d026f97becba3a0bbfaf933fbf767af9da3e1972e1cc9c6b683e4eac2258cdfa"
-    sha256 cellar: :any,                 arm64_sequoia: "2c94de1ca73686929663fe7acc812c2cba31bdfcb3495a9cbe4103fc7ac0b95c"
-    sha256 cellar: :any,                 arm64_sonoma:  "4c1509a741bb03131dfa27768cd9c5ff9a8a164c53d187e884c0f7adae71bfd7"
-    sha256 cellar: :any,                 sonoma:        "5a33c400850f38587bf460f1ad51c36c495123ed0c0439c44bc82b887f9a5466"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "99f69ca4e19039a2dd83c70ecd79fdcf1e1205ccbc76c1e523a5a64a1bac47a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6e9a6b109a28f9821c38754a80b8a7e47d0ded3e16f2b207fbe65ed28a58fd2f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dbd43c08d1ba3eafb80d0a5f3e17a50e3440c6fa78005c0c7f0a08fa23a6fd58"
   end
 
   depends_on "cmake" => :build
