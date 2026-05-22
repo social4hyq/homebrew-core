18ed56ee42d6931a1a4428a94d3441f8c2c20704
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 21:27:16 +0800

    ipbt: update 20260410 bottle.

--- Formula/i/ipbt.rb
+++ Formula/i/ipbt.rb
@@ -12,12 +12,7 @@ class Ipbt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2b4c3e90232369f35d47f594aa32973befe1aa273d6e86ab05ba9120db428e26"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e61b90b67295680942b77076e63e19c3898f83b4f20866069219e154636189bc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bbb93ec319c807cc8368a11896772edb3c3bd027e1bbfbbb210ed34ed7df1e4f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "200a6fe42ba970e051837b39dcab5001ae37eacfb2fba47c6270ff29ebbffcb2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "455ff4752bca6510113674292dc611f98acb179bb11c04389f4c03a1d4f4ae16"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "162b9ca81927d323dca7c6c95181eef5b03f5dd01deba452ccde94fd16c38a54"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b43fee1082746c6d7143f2d9f81e5bf72280436bc23b7e821750a859f0b9c551"
   end
 
   depends_on "cmake" => :build
