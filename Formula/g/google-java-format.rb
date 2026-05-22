9a601bd34555951072c32c5bf618ca0e7da9a296
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-22 11:44:47 +0800

    google-java-format: update 1.35.0 bottle.

--- Formula/g/google-java-format.rb
+++ Formula/g/google-java-format.rb
@@ -8,7 +8,7 @@ class GoogleJavaFormat < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2a12a2156a19226e997743574718b35bb4bc06a371e0843989eada4d5970a5d0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "901e92a519787554d191fdae96a12fa6df14b340cc5d4ed29bcfb24f1b8c1837"
   end
 
   depends_on "openjdk"
