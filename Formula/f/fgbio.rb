c04697b9100355ee2217ad903280312d5da47708
hqzing <hqzing@outlook.com>
2026-05-23 20:00:07 +0000

    fgbio 4.1.0 (new formula)

---
+++ Formula/f/fgbio.rb
@@ -0,0 +1,28 @@
+class Fgbio < Formula
+  desc "Tools for working with genomic and high throughput sequencing data"
+  homepage "https://fulcrumgenomics.github.io/fgbio/"
+  url "https://github.com/fulcrumgenomics/fgbio/releases/download/4.1.0/fgbio-4.1.0.jar"
+  sha256 "6f957bfc4d5fcd56e5e4a4cf63c9d54a6f8603958bc17e8dcb67dec941cfae6f"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3201ca8195c7509fcbfdd7047d7693c770e3d7173309dae3793d53a1509311ce"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "fgbio-#{version}.jar"
+    bin.write_jar_script libexec/"fgbio-#{version}.jar", "fgbio"
+  end
+
+  test do
+    (testpath/"test.fasta").write <<~EOS
+      >U00096.2:1-70
+      AGCTTTTCATTCTGACTGCAACGGGCAATATGTCT
+      ctgtgtggattaaaaaaagagtgtctgatagcagc
+    EOS
+    cmd = "#{bin}/fgbio HardMaskFasta -i test.fasta -o /dev/stdout"
+    assert_match "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN", shell_output(cmd)
+  end
+end
