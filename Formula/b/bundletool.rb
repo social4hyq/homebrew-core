238faba8fba0b39943078bf46143cd9633262d9e
hqzing <hqzing@outlook.com>
2026-05-23 18:43:14 +0000

    bundletool 1.18.3 (new formula)

---
+++ Formula/b/bundletool.rb
@@ -0,0 +1,37 @@
+class Bundletool < Formula
+  desc "Command-line tool to manipulate Android App Bundles"
+  homepage "https://github.com/google/bundletool"
+  url "https://github.com/google/bundletool/releases/download/1.18.3/bundletool-all-1.18.3.jar"
+  sha256 "a099cfa1543f55593bc2ed16a70a7c67fe54b1747bb7301f37fdfd6d91028e29"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "12d47cf60e022e6fd1130a87d77ceaae4c36220051f116c9d0b5392392ea56d6"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "bundletool-all-#{version}.jar" => "bundletool-all.jar"
+    bin.write_jar_script libexec/"bundletool-all.jar", "bundletool"
+  end
+
+  test do
+    resource "homebrew-test-bundle" do
+      url "https://github.com/thuongleit/crashlytics-sample/raw/master/app/release/app.aab"
+      sha256 "f7ea5a75ce10e394a547d0c46115b62a2f03380a18b1fc222e98928d1448775f"
+    end
+
+    resource("homebrew-test-bundle").stage do
+      expected = <<~EOS
+        App Bundle information
+        ------------
+        Feature modules:
+        \tFeature module: base
+        \t\tFile: res/anim/abc_fade_in.xml
+      EOS
+
+      assert_match expected, shell_output("#{bin}/bundletool validate --bundle app.aab")
+    end
+  end
+end
