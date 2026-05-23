ffc501be634bd3b0ea4533ecb6ef07ce78b09a82
hqzing <hqzing@outlook.com>
2026-05-23 20:10:17 +0000

    fuseki 6.1.0 (new formula)

---
+++ Formula/f/fuseki.rb
@@ -0,0 +1,41 @@
+class Fuseki < Formula
+  desc "SPARQL server"
+  homepage "https://jena.apache.org/documentation/fuseki2/"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-6.1.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-6.1.0.tar.gz"
+  sha256 "a4a845bb602515f66e3397e89b52180ca055f4b240ddb442a1a793a98ce4d280"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4db6b0901cecd3a7a7a5fd2ad92cde832ebcb09c95c78ea38e0b572689295137"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    fuseki_env = Language::Java.java_home_env.merge(
+      FUSEKI_BASE: var/"fuseki",
+      FUSEKI_HOME: libexec,
+      FUSEKI_LOGS: var/"log/fuseki",
+      FUSEKI_RUN:  var/"run/fuseki",
+    )
+
+    bin.install %w[fuseki-server fuseki-backup fuseki-plain]
+    bin.env_script_all_files(libexec, fuseki_env)
+    chmod 0755, libexec.children
+    libexec.install "fuseki-server.jar"
+
+    # Create a location for dataset and log files,
+    # in case we're being used in LaunchAgent mode
+    (var/"fuseki").mkpath
+    (var/"log/fuseki").mkpath
+  end
+
+  service do
+    run opt_bin/"fuseki-server"
+  end
+
+  test do
+    system bin/"fuseki-server", "--version"
+  end
+end
