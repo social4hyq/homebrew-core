1d4eb59cad36835a88760562308aae6253339750
hqzing <hqzing@outlook.com>
2026-05-23 18:28:31 +0000

    ssh-copy-id 10.3p1 (new formula)

---
+++ Formula/s/ssh-copy-id.rb
@@ -0,0 +1,30 @@
+class SshCopyId < Formula
+  desc "Add a public key to a remote machine's authorized_keys file"
+  homepage "https://www.openssh.com/"
+  url "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-10.3p1.tar.gz"
+  mirror "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-10.3p1.tar.gz"
+  version "10.3p1"
+  sha256 "56682a36bb92dcf4b4f016fd8ec8e74059b79a8de25c15d670d731e7d18e45f4"
+  license "SSH-OpenSSH"
+  head "https://github.com/openssh/openssh-portable.git", branch: "master"
+
+  livecheck do
+    formula "openssh"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c766ecb834fc8f95b7e3d05ffb9c301023963c9b0ad3efb94d8407d83e2d1b51"
+  end
+
+  keg_only :provided_by_macos
+
+  def install
+    bin.install "contrib/ssh-copy-id"
+    man1.install "contrib/ssh-copy-id.1"
+  end
+
+  test do
+    output = shell_output("#{bin}/ssh-copy-id -h 2>&1", 1)
+    assert_match "identity_file", output
+  end
+end
