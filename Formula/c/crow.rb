eddd8b34fac70f009b3707bb73bf47d03e0262f3
hqzing <hqzing@outlook.com>
2026-05-23 16:43:35 +0000

    crow 1.3.2 (new formula)

---
+++ Formula/c/crow.rb
@@ -0,0 +1,40 @@
+class Crow < Formula
+  desc "Fast and Easy to use microframework for the web"
+  homepage "https://crowcpp.org"
+  url "https://github.com/CrowCpp/Crow/archive/refs/tags/v1.3.2.tar.gz"
+  sha256 "82926bba66a48fa8dd0165cbc1f1b96b6dc9c3e56d08d318d901196e13eccf1a"
+  license "BSD-3-Clause"
+  head "https://github.com/CrowCpp/Crow.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ea8ea782dc4dfa04e0be6da9cf4755adf6fc93ae84674fde27fa8c049b59ddcb"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "asio"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+           "-DCROW_BUILD_EXAMPLES=OFF", "-DCROW_BUILD_TESTS=OFF", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <crow.h>
+      int main() {
+        crow::SimpleApp app;
+        CROW_ROUTE(app, "/")([](const crow::request&, crow::response&) {});
+      }
+    CPP
+
+    system ENV.cxx, "test.cpp", "-std=c++17", "-o", "test"
+    system "./test"
+  end
+end
