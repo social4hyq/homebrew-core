7a96775400133f9ee426de52165762d14b77609b
hqzing <hqzing@outlook.com>
2026-05-23 17:51:55 +0000

    reuse 6.2.0 (new formula)

---
+++ Formula/r/reuse.rb
@@ -0,0 +1,100 @@
+class Reuse < Formula
+  include Language::Python::Virtualenv
+
+  desc "Tool for copyright and license recommendations"
+  homepage "https://reuse.software"
+  url "https://files.pythonhosted.org/packages/05/35/298d9410b3635107ce586725cdfbca4c219c08d77a3511551f5e479a78db/reuse-6.2.0.tar.gz"
+  sha256 "4feae057a2334c9a513e6933cdb9be819d8b822f3b5b435a36138bd218897d23"
+  license all_of: [
+    "GPL-3.0-or-later",
+    "CC-BY-SA-4.0",
+    "CC0-1.0",
+    "Apache-2.0",
+  ]
+  head "https://github.com/fsfe/reuse-tool.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b0c54095d67e082855143d97b642e9912f59182e89d082f6a69cd649afca0d82"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9648e3d0ae7497eb247f4a461602f1f7f6a876bc18f724eea8d314cc0fc75e1b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "327e52d1a950d585a5cd0a3def3ddc7f8f5ef87857bc681ac9777dddcc8c260e"
+    sha256 cellar: :any_skip_relocation, tahoe:         "38fdf43c2bb803c2a4ebadada4e3af205d2119f3e17e0c68ff0fbd998586ddc4"
+    sha256 cellar: :any_skip_relocation, sequoia:       "27707a68325827425ec1cdd5f1d48bec63f7a84d2d5f66252b962b72c87b1824"
+    sha256 cellar: :any_skip_relocation, sonoma:        "389a80a6fbc3cfc0c2b610a125af1752cf3e9daf6b4a398c6f46a57b5d03c95c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1eb816b024f2264daffcf5e2cd000c5087ebc15254fa4016538d7a563c9d8d94"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e41b6ec378751332431d99ae9947c34451c4b82768d7105710f0acce38c0602"
+  end
+
+  depends_on "python@3.14"
+
+  resource "attrs" do
+    url "https://files.pythonhosted.org/packages/6b/5c/685e6633917e101e5dcb62b9dd76946cbb57c26e133bae9e0cd36033c0a9/attrs-25.4.0.tar.gz"
+    sha256 "16d5969b87f0859ef33a48b35d55ac1be6e42ae49d5e853b597db70c35c57e11"
+  end
+
+  resource "boolean-py" do
+    url "https://files.pythonhosted.org/packages/c4/cf/85379f13b76f3a69bca86b60237978af17d6aa0bc5998978c3b8cf05abb2/boolean_py-5.0.tar.gz"
+    sha256 "60cbc4bad079753721d32649545505362c754e121570ada4658b852a3a318d95"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
+    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/46/61/de6cd827efad202d7057d93e0fed9294b96952e188f7384832791c7b2254/click-8.3.0.tar.gz"
+    sha256 "e7b8232224eba16f4ebe410c25ced9f7875cb5f3263ffc93cc3e8da705e229c4"
+  end
+
+  resource "jinja2" do
+    url "https://files.pythonhosted.org/packages/df/bf/f7da0350254c0ed7c72f3e33cef02e048281fec7ecec5f032d4aac52226b/jinja2-3.1.6.tar.gz"
+    sha256 "0137fb05990d35f1275a587e9aee6d56da821fc83491a0fb838183be43f66d6d"
+  end
+
+  resource "license-expression" do
+    url "https://files.pythonhosted.org/packages/40/71/d89bb0e71b1415453980fd32315f2a037aad9f7f70f695c7cec7035feb13/license_expression-30.4.4.tar.gz"
+    sha256 "73448f0aacd8d0808895bdc4b2c8e01a8d67646e4188f887375398c761f340fd"
+  end
+
+  resource "markupsafe" do
+    url "https://files.pythonhosted.org/packages/7e/99/7690b6d4034fffd95959cbe0c02de8deb3098cc577c67bb6a24fe5d7caa7/markupsafe-3.0.3.tar.gz"
+    sha256 "722695808f4b6457b320fdc131280796bdceb04ab50fe1795cd540799ebe1698"
+  end
+
+  resource "python-debian" do
+    url "https://files.pythonhosted.org/packages/bf/4b/3c4cf635311b6203f17c2d693dc15e898969983dd3f729bee3c428aa60d4/python-debian-1.0.1.tar.gz"
+    sha256 "3ada9b83a3d671b58081782c0969cffa0102f6ce433fbbc7cf21275b8b5cc771"
+  end
+
+  resource "python-magic" do
+    url "https://files.pythonhosted.org/packages/da/db/0b3e28ac047452d079d375ec6798bf76a036a08182dbb39ed38116a49130/python-magic-0.4.27.tar.gz"
+    sha256 "c1ba14b08e4a5f5c31a302b7721239695b2f0f058d125bd5ce1ee36b9d9d3c3b"
+  end
+
+  resource "tomlkit" do
+    url "https://files.pythonhosted.org/packages/cc/18/0bbf3884e9eaa38819ebe46a7bd25dcd56b67434402b66a58c4b8e552575/tomlkit-0.13.3.tar.gz"
+    sha256 "430cf247ee57df2b94ee3fbe588e71d362a941ebb545dec29b53961d61add2a1"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"reuse", shell_parameter_format: :click)
+  end
+
+  test do
+    (testpath/"testfile.rb").write ""
+    system bin/"reuse", "annotate", "--copyright=Homebrew Maintainers",
+                  "--exclude-year",
+                  "--license=BSD-2-Clause",
+                  testpath/"testfile.rb"
+    header = <<~RUBY
+      # SPDX-FileCopyrightText: Homebrew Maintainers
+      #
+      # SPDX-License-Identifier: BSD-2-Clause
+    RUBY
+    assert_equal header, (testpath/"testfile.rb").read
+    system bin/"reuse", "download", "BSD-2-Clause"
+    assert_path_exists testpath/"LICENSES/BSD-2-Clause.txt"
+  end
+end
