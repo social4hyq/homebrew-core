70f5ab42d7d2062f865af51939fbb98a322fffce
hqzing <hqzing@outlook.com>
2026-05-23 16:38:26 +0000

    yuicompressor 2.4.8 (new formula)

---
+++ Formula/y/yuicompressor.rb
@@ -0,0 +1,36 @@
+class Yuicompressor < Formula
+  desc "Yahoo! JavaScript and CSS compressor"
+  homepage "https://yui.github.io/yuicompressor/"
+  url "https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.zip"
+  sha256 "3243fd79cb68cc61a5278a8ff67a0ad6a2d825c36464594b66900ad8426a6a6e"
+  license "BSD-3-Clause"
+  revision 1
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, all: "85fa9d1169b77f59259088ebe086f6611f7d14b87216c4e3ed0090f3c04f34cd"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "yuicompressor-#{version}.jar"
+    bin.write_jar_script libexec/"yuicompressor-#{version}.jar", "yuicompressor"
+  end
+
+  test do
+    path = testpath/"test.js"
+    path.write <<~JAVASCRIPT
+      var i = 1;      // foo
+      console.log(i); // bar
+    JAVASCRIPT
+
+    output = shell_output("#{bin}/yuicompressor --nomunge --preserve-semi #{path}").strip
+    assert_equal "var i=1;console.log(i);", output
+  end
+end
