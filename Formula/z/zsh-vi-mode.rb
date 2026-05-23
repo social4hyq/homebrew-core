9b38fc3bd4b9fef6dfcc212d9fb6e6f98ce24bda
hqzing <hqzing@outlook.com>
2026-05-23 18:29:50 +0000

    zsh-vi-mode 0.12.0 (new formula)

---
+++ Formula/z/zsh-vi-mode.rb
@@ -0,0 +1,31 @@
+class ZshViMode < Formula
+  desc "Better and friendly vi(vim) mode plugin for ZSH"
+  homepage "https://github.com/jeffreytse/zsh-vi-mode"
+  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "28cdbc1803cf5545e5e5e4b29db075dc4d45b8c498bb6118bfff5c7df1b68622"
+  license "MIT"
+  head "https://github.com/jeffreytse/zsh-vi-mode.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "640f9ffc4af6c23405417a2076f924ddfa3b07be4eac462470f6521c010841dd"
+  end
+
+  uses_from_macos "zsh"
+
+  def install
+    pkgshare.install "zsh-vi-mode.zsh"
+    pkgshare.install "zsh-vi-mode.plugin.zsh"
+  end
+
+  def caveats
+    <<~EOS
+      To activate the zsh vi mode, add the following line to your .zshrc:
+        source #{opt_pkgshare}/zsh-vi-mode.plugin.zsh
+    EOS
+  end
+
+  test do
+    assert_match "zsh-vi-mode",
+      shell_output("zsh -c '. #{pkgshare}/zsh-vi-mode.plugin.zsh && zvm_version'")
+  end
+end
