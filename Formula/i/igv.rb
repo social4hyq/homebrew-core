d0742e45693a80b7d049a3b2e04b375a3ae45596
hqzing <hqzing@outlook.com>
2026-05-23 17:10:35 +0000

    igv 2.19.7 (new formula)

---
+++ Formula/i/igv.rb
@@ -0,0 +1,35 @@
+class Igv < Formula
+  desc "Interactive Genomics Viewer"
+  homepage "https://igv.org/doc/desktop/"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.19/IGV_2.19.7.zip"
+  sha256 "692ae6c3037a6633c33afdbbe960a715f537173f26a16f56a58cb9ccbe163e9f"
+  license "MIT"
+
+  livecheck do
+    url "https://igv.org/doc/desktop/DownloadPage/"
+    regex(/href=.*?IGV[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "23c89e905e0e048e3bfbf3a7fe26ab96df8591a241be76b1ffd159fd9f0e193a"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    inreplace ["igv.sh", "igvtools"], /^prefix=.*/, "prefix=#{libexec}"
+    bin.install "igv.sh" => "igv"
+    bin.install "igvtools"
+    libexec.install "igv.args", "lib"
+    bin.env_script_all_files libexec, Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/igvtools")
+    assert_match "org/broad/igv/ui/IGV.class", shell_output("#{Formula["openjdk"].bin}/jar tf #{libexec}/lib/igv.jar")
+
+    ENV.append "_JAVA_OPTIONS", "-Duser.home=#{testpath}"
+    (testpath/"script").write "exit"
+    assert_match "Using system JDK.", shell_output("#{bin}/igv -b script")
+  end
+end
