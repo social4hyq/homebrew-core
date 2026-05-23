dfe393011e3193f5b9a4687ea4f18eeef36cca54
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-23 22:49:50 +0800

    helmfile: update 1.5.2 bottle.

--- Formula/h/helmfile.rb
+++ Formula/h/helmfile.rb
@@ -8,7 +8,7 @@ class Helmfile < Formula
   head "https://github.com/helmfile/helmfile.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "57adda82f23b3117bc03449c556a17ea0641172178d55e1dd12a62c9a508ef73"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "64827cfab2264610446ce777058562fc89cc77b21848b692aad3e9f336a34fc0"
   end
 
   depends_on "go" => :build
