5e445dda5472d2c6e9abac8658828587c5732359
hqzing <hqzing@outlook.com>
2026-05-23 16:24:15 +0000

    rapidfuzz-cpp 3.3.3 (new formula)

---
+++ Formula/r/rapidfuzz-cpp.rb
@@ -0,0 +1,37 @@
+class RapidfuzzCpp < Formula
+  desc "Rapid fuzzy string matching in C++ using the Levenshtein Distance"
+  homepage "https://rapidfuzz.github.io/rapidfuzz-cpp/"
+  url "https://github.com/rapidfuzz/rapidfuzz-cpp/archive/refs/tags/v3.3.3.tar.gz"
+  sha256 "fa0fbd40110df8134cf05bddbaa4e237dbc4fd915ab9a3029ff481a8d3e8b757"
+  license "MIT"
+  head "https://github.com/rapidfuzz/rapidfuzz-cpp.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "54a85141d45ff381701842443506a4cb6e385f0c07a03fdfb9ba926add8c2e60"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <rapidfuzz/fuzz.hpp>
+      #include <string>
+      #include <iostream>
+
+      int main()
+      {
+          std::string a = "aaaa";
+          std::string b = "abab";
+          std::cout << rapidfuzz::fuzz::ratio(a, b) << std::endl;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++17", "-I#{include}", "-o", "test"
+    assert_equal "50", shell_output("./test").strip
+  end
+end
