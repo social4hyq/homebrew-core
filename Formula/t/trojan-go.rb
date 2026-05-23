0d1559b8d6f3bb0f0b67bd79507f9cb25c6faf07
hqzing <hqzing@outlook.com>
2026-05-23 15:55:12 +0000

    trojan-go 0.10.6 (new formula)

---
+++ Formula/t/trojan-go.rb
@@ -0,0 +1,164 @@
+class TrojanGo < Formula
+  desc "Trojan proxy in Go"
+  homepage "https://p4gefau1t.github.io/trojan-go/"
+  url "https://github.com/p4gefau1t/trojan-go.git",
+      tag:      "v0.10.6",
+      revision: "2dc60f52e79ff8b910e78e444f1e80678e936450"
+  license "GPL-3.0-only"
+  head "https://github.com/p4gefau1t/trojan-go.git", branch: "master"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e5ff7287bf3913b64c7f3a691df6229bd4a2dbb5f44d46e73ce1b36b968bfdc2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e5ff7287bf3913b64c7f3a691df6229bd4a2dbb5f44d46e73ce1b36b968bfdc2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e5ff7287bf3913b64c7f3a691df6229bd4a2dbb5f44d46e73ce1b36b968bfdc2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aef59484731100b7a80263353a251ad5c33281f0f3ce0fe4c3c427f42aaa3867"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3546d218a396d0ce42976b0593c71f4cd48f0edd81b0e2ba1dde4d3cd15e4004"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d15ceba20c4d223f29bcb8daa59d4e045a089cfdd49c10a9a98dd770565d6fc"
+  end
+
+  depends_on "go" => :build
+
+  resource "geoip" do
+    url "https://github.com/v2fly/geoip/releases/download/202509050142/geoip.dat"
+    sha256 "a01e09150b456cb2f3819d29d6e6c34572420aaee3ff9ef23977c4e9596c20ec"
+  end
+
+  resource "geoip-only-cn-private" do
+    url "https://github.com/v2fly/geoip/releases/download/202509050142/geoip-only-cn-private.dat"
+    sha256 "845483083b4a7a82087d4293e8a190239ae79698012c1a973baac1156f91c4c2"
+  end
+
+  resource "geosite" do
+    url "https://github.com/v2fly/domain-list-community/releases/download/20250906011216/dlc.dat"
+    sha256 "186158b6c2f67ac59e184ed997ebebcef31938be9874eb8a7d5e3854187f4e8d"
+  end
+
+  def install
+    execpath = libexec/name
+    ldflags = %W[
+      -s -w
+      -X github.com/p4gefau1t/trojan-go/constant.Version=v#{version}
+      -X github.com/p4gefau1t/trojan-go/constant.Commit=#{Utils.git_head}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:, tags: "full"), "-o", execpath
+    (bin/"trojan-go").write_env_script execpath,
+      TROJAN_GO_LOCATION_ASSET: "${TROJAN_GO_LOCATION_ASSET:-#{pkgshare}}"
+
+    pkgetc.install "example/client.json" => "config.json"
+
+    resource("geoip").stage do
+      pkgshare.install "geoip.dat"
+    end
+
+    resource("geoip-only-cn-private").stage do
+      pkgshare.install "geoip-only-cn-private.dat"
+    end
+
+    resource("geosite").stage do
+      pkgshare.install "dlc.dat" => "geosite.dat"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      An example config is installed to #{etc}/trojan-go/config.json
+    EOS
+  end
+
+  service do
+    run [opt_bin/"trojan-go", "-config", etc/"trojan-go/config.json"]
+    run_type :immediate
+    keep_alive true
+  end
+
+  test do
+    (testpath/"test.crt").write <<~EOS
+      -----BEGIN CERTIFICATE-----
+      MIIBuzCCASQCCQDC8CtpZ04+pTANBgkqhkiG9w0BAQsFADAhMQswCQYDVQQGEwJV
+      UzESMBAGA1UEAwwJbG9jYWxob3N0MCAXDTIxMDUxMDE0MjEwNFoYDzIxMjEwNDE2
+      MTQyMTA0WjAhMQswCQYDVQQGEwJVUzESMBAGA1UEAwwJbG9jYWxob3N0MIGfMA0G
+      CSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8VJ+Gv2BRZajCUJ8LxGCGopO6w27xvwLu
+      U0ztdJibWCUUYxGk7IDnhnarbpD18CnZ0bqqUvu/gn1Lod5rHUuDdh2KdMefiugR
+      bu1jtKxi25kKfd+12nqph7dI9iuenroHUi5SBxCCKEQSo528/2QIeltTtBASNiKB
+      CBjdIu0wjwIDAQABMA0GCSqGSIb3DQEBCwUAA4GBAGm7Lrhd7ZP91d7ezBLQZ3L/
+      xciCZUmm6DcMfGgel13aI8keYID5LPUoIJ8X3uoOu2SV7r4G53mJKtyyqUKfbMBG
+      DSq4rm8g2L9r5LdVYQFcvJjJxHGLMOvZUvm7NiQH1/zd73nHYhu+0yravaUkywEl
+      fhs+mOABareCK+xi+YT0
+      -----END CERTIFICATE-----
+    EOS
+    (testpath/"test.key").write <<~EOS
+      -----BEGIN PRIVATE KEY-----
+      MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALxUn4a/YFFlqMJQ
+      nwvEYIaik7rDbvG/Au5TTO10mJtYJRRjEaTsgOeGdqtukPXwKdnRuqpS+7+CfUuh
+      3msdS4N2HYp0x5+K6BFu7WO0rGLbmQp937XaeqmHt0j2K56eugdSLlIHEIIoRBKj
+      nbz/ZAh6W1O0EBI2IoEIGN0i7TCPAgMBAAECgYBRusO0PW82Q9DV6xjqiWF+bCWC
+      QnfuL3+9H6dd0WC84abNzySEFyLl1wO+5+++22e+IHdKnVKlTKLFZMzaXU88UJjG
+      WwQdKhLPw4MvVsPNwFtDlP+EyKfzKHlQ5PAhPjw5Hz1isE2b98JNqMbj0QMZqpES
+      hm391fmfk8QPBPsLyQJBAPpWUOfJcQUC1bh0qF/XatLmg6A4DEHyhbZq/kehcvZK
+      zes71uzcW1NuzDE3ahbv3IFy5UOWWWiPXD1Dp/iGBYUCQQDAlzs+rd9Uaqq4ZfdA
+      iH2wkUub+2kcRi59MlH9B22Wb+VmWTqcwwhVFlKB8to/0bIsK+cae8D1VBYLhuZu
+      yKADAkEAzxrYBlrOiPHGdLr2jYv/UYnpvYSBB5In8znjMsmr/Xz3jTRNZFoNqCHT
+      BqisuVspl2LBr7/UKj/odLrjXSUrrQJAUIuvQnKTcYm+5qn2c23iK0NI/O5zsliD
+      vuaZtZoysfUQWvK8ea1zwao5TZHUx1YbDzA5UjEprTDCm4WKwBB2IwJBANbtLRvS
+      CsWbp+cEK+zSllqBhvlJQUf2DNQRGHsItbq1dbqNA3xF1WWh6IQSevN4M1exdBLa
+      OOqlfB3Fyb6Mld0=
+      -----END PRIVATE KEY-----
+    EOS
+
+    http_server_port = free_port
+    fork do
+      server = TCPServer.new(http_server_port)
+      loop do
+        socket = server.accept
+        socket.write "HTTP/1.1 200 OK\r\n" \
+                     "Content-Type: text/plain; charset=utf-8\r\n" \
+                     "Content-Length: 0\r\n" \
+                     "\r\n"
+        socket.close
+      end
+    end
+
+    trojan_go_server_port = free_port
+    (testpath/"server.yaml").write <<~YAML
+      run-type:     server
+      local-addr:   127.0.0.1
+      local-port:   #{trojan_go_server_port}
+      remote-addr:  127.0.0.1
+      remote-port:  #{http_server_port}
+      password:
+        - test
+      ssl:
+        cert:       #{testpath}/test.crt
+        key:        #{testpath}/test.key
+    YAML
+    server = fork { exec bin/"trojan-go", "-config", testpath/"server.yaml" }
+
+    trojan_go_client_port = free_port
+    (testpath/"client.yaml").write <<~YAML
+      run-type:     client
+      local-addr:   127.0.0.1
+      local-port:   #{trojan_go_client_port}
+      remote-addr:  127.0.0.1
+      remote-port:  #{trojan_go_server_port}
+      password:
+        - test
+      ssl:
+        verify:     false
+        sni:        localhost
+    YAML
+    client = fork { exec bin/"trojan-go", "-config", testpath/"client.yaml" }
+
+    sleep 3
+    begin
+      output = shell_output("curl --socks5 127.0.0.1:#{trojan_go_client_port} example.com")
+      assert_match "<title>Example Domain</title>", output
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
