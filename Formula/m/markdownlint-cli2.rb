150dece90450f8c9a55a47419a185ea131f445e9
hqzing <hqzing@outlook.com>
2026-05-23 18:27:33 +0000

    markdownlint-cli2 0.22.1 (new formula)

---
+++ Formula/m/markdownlint-cli2.rb
@@ -0,0 +1,34 @@
+class MarkdownlintCli2 < Formula
+  desc "Fast, flexible, config-based cli for linting Markdown/CommonMark files"
+  homepage "https://github.com/DavidAnson/markdownlint-cli2"
+  url "https://registry.npmjs.org/markdownlint-cli2/-/markdownlint-cli2-0.22.1.tgz"
+  sha256 "57f59b92ce95a5d0beb9d484bf5ecede02fde29f913ad1a6cc74b15901650037"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "bbe8169e727e039a180e0dcb14d6c15e91acf780a44169ef0ef2934eabb6e435"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    (testpath/"test-bad.md").write <<~MARKDOWN
+      # Header 1
+      body
+    MARKDOWN
+    (testpath/"test-good.md").write <<~MARKDOWN
+      # Header 1
+
+      body
+    MARKDOWN
+    assert_match "Summary: 1 error(s)",
+      shell_output("#{bin}/markdownlint-cli2 :#{testpath}/test-bad.md 2>&1", 1)
+    assert_match "Summary: 0 error(s)",
+      shell_output("#{bin}/markdownlint-cli2 :#{testpath}/test-good.md")
+  end
+end
