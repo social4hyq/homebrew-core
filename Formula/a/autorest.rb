cb76259f8609794e720fcf49c22be6b090ebc2ec
hqzing <hqzing@outlook.com>
2026-05-23 16:11:34 +0000

    autorest 3.8.0 (new formula)

---
+++ Formula/a/autorest.rb
@@ -0,0 +1,32 @@
+class Autorest < Formula
+  desc "Swagger (OpenAPI) Specification code generator"
+  homepage "https://github.com/Azure/autorest"
+  url "https://registry.npmjs.org/autorest/-/autorest-3.8.0.tgz"
+  sha256 "0d1739956d385d7f0951d4622078c73d9dada27340ca39cfaaa8ba7f8ab5bae1"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b9e9e79e98255f252a645acc787169370311c4822e30d35639cc491e8d67910f"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    resource "homebrew-petstore" do
+      url "https://raw.githubusercontent.com/Azure/autorest/5c170a02c009d032e10aa9f5ab7841e637b3d53b/Samples/1b-code-generation-multilang/petstore.yaml"
+      sha256 "e981f21115bc9deba47c74e5c533d92a94cf5dbe880c4304357650083283ce13"
+    end
+
+    resource("homebrew-petstore").stage do
+      system (bin/"autorest"), "--input-file=petstore.yaml",
+                               "--typescript",
+                               "--output-folder=petstore"
+      assert_includes File.read("petstore/package.json"), "Microsoft Corporation"
+    end
+  end
+end
