6b3819f74798de403d71fad8f9d966fdb27226d8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 04:01:31 +0800

    hexapoda: update 0.2.3 bottle.

--- Formula/h/hexapoda.rb
+++ Formula/h/hexapoda.rb
@@ -7,12 +7,7 @@ class Hexapoda < Formula
   head "https://github.com/simonomi/hexapoda.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0117efc2f1230cf5633e23fee608e18c99d47ae33de1d98401aca60dba21ae49"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8ac724c6d3e6e158c6208a8a6006afd1c45c5252ccd94f72bd2fab38b047c0b5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c45a0d4b07453a89082ff3a57dd8b05ae2d942fe727682c0b19c107d476bd1d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e626bdf04765e42513dcf5dfe181920a0714833cbcb8cae806f6cccddbf8fbf0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0c1df6bf55b780a9bffcedc45e94c052372e73c42ccba1926ab2252e8e444e80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51a649b455fdf164bd84b8ff8b248be5ff19761a876d300105132221ec815a1a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e2b5f898ba5f005d3c970c4ee5e8f0ceef4cb3e52c1041b64a4406937a2ec615"
   end
 
   depends_on "rust" => :build
