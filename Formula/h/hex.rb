c707dd8730697a6efa2fd11c68fe6b382e67bf51
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 03:24:12 +0800

    hex: update 0.7.0 bottle.

--- Formula/h/hex.rb
+++ Formula/h/hex.rb
@@ -7,12 +7,7 @@ class Hex < Formula
   head "https://github.com/sitkevij/hex.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "58ed01d7409f38346036169f1bdc4712f5f7cdeac3ea5b6418571df9431a9d3c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f7b01ab0296d58105b91302a3432ca76689efe6288b67dfc57faa5d29f8ef153"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9e055782ea2fdcabc09058105aeb6bdc573c37ef7ebf0e521c46f79e5d612a96"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0b4a84c0d4be4b28925f79c9e18aaeda0729d4eeac75cf1e3f8df84129babec6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ef0500e202d684ff46ee876924101c0ed7bded9c8b98392afb67d59090df3e1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55750f621384c95c077d74a3c739b7db018aec4d864d2febcc126c14aa6a4afc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "71d535a88cf73dbfc06e7e0e37fa621a5fdd1236b5085b4d35597460b39f8cb9"
   end
 
   depends_on "rust" => :build
