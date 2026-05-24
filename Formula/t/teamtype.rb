4046ec217b90dcfa975c19ddbc541e9c64172de5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 04:01:05 +0800

    teamtype: update 0.9.1 bottle.

--- Formula/t/teamtype.rb
+++ Formula/t/teamtype.rb
@@ -7,13 +7,7 @@ class Teamtype < Formula
   head "https://github.com/teamtype/teamtype.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f305fb49649d180299c2cd52a2e24c92088389058c9069cd07e272d237656019"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3d701160add6b5f51cbd6b8dd79cee38b26ae3bd1dfeba6dd6d5f526e9d309e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b1caebb81e31e88e9d3de8fd439293bf5078c903b021cb79d35806eb87662575"
-    sha256 cellar: :any_skip_relocation, sonoma:        "894b2d5c75a01fce4f124476df1cc0438b160a12688749bb355148162c4e20ad"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "48431d07b888a42dcac294713e26a88f72e68dc1427f9795db5ae31c9728d348"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f13a452a13a112335fece8cb2a97e23414c7164ae7d22eec8a180525ec4553b7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "69d25784f437b006c1420a60fc6b2441484ecfd0e760141e83fe2d3650004489"
   end
 
   depends_on "rust" => :build
