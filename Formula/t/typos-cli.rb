c2466d7a5f54488310842b1dcaa63a4811864d81
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:09:35 +0800

    typos-cli: update 1.46.3 bottle.

--- Formula/t/typos-cli.rb
+++ Formula/t/typos-cli.rb
@@ -11,12 +11,7 @@ class TyposCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "080b6247145b6f4b7db753f31803cf4beccacd86f574507d17572da065a3ad3e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3cddacb606ad7b3f5e2c2193f10a9c58ca15984a910258d133b36bae7d65be14"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d3dc25e36dc5688bbbe608aaa9e1afbf9bdc5d9ed619e2e9c95f27d8bb42d57"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7787bfa005d87d939034f06bbb2deaf1bd5a0901f6b1bd2ed7ba4ee38cc18421"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6630e4cbf1c64cabab938f045c2316dabd6fdecdf37ef6879e0b65f7ab23c4ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6ec476d16e8d8f1e00f3a5d07e16aa4704910451f4b397aae31620594767d84"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7d1fa8fbb72235d41270572e3e6e992dc2245ee1377ce5c52c88f3ba1ca15e38"
   end
 
   depends_on "rust" => :build
