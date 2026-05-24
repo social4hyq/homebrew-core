d9e581d63d03e8d4f1795de0a6925b9ffa9627fb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:56:20 +0800

    arkade: update 0.11.99 bottle.

--- Formula/a/arkade.rb
+++ Formula/a/arkade.rb
@@ -12,12 +12,7 @@ class Arkade < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e919976d772ac329895c9a87364b71661f193569078924348c8787e7b2ff6744"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e919976d772ac329895c9a87364b71661f193569078924348c8787e7b2ff6744"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e919976d772ac329895c9a87364b71661f193569078924348c8787e7b2ff6744"
-    sha256 cellar: :any_skip_relocation, sonoma:        "586034f33b6e70b4835396e46ab0e85d9269684771c3c333958cbd5564f3c68a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb8aabff9de7815cedb1921f0289baa0a99c3a07ef0b7a84132b062e058b58de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c8ef8ce0823c6071a7419b41a0478d67f405245defd781173cd7e526a8a8f57"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3d14e86271e21f8786bd257827a32a5b5d2a6dd8cddc6a95167b3c4b3f4a6b8d"
   end
 
   depends_on "go" => :build
