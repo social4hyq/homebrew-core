4873afa8fccd3e07645d4a4ab5249a0498639611
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:46:53 +0800

    leaf-proxy: update 0.14.2 bottle.

--- Formula/l/leaf-proxy.rb
+++ Formula/l/leaf-proxy.rb
@@ -7,12 +7,7 @@ class LeafProxy < Formula
   head "https://github.com/eycorsican/leaf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ce4ddbc5258b07aa7d6fc9b17843eb49d3cb45df5d0a5d341d0d30a5557161e0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea5824292f3c9cd445bdbb5d510aa17f781116cdd00b50bd2aafb0df10846ad9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d45757c8417275f5a65eb781991564d9a737d39cab6c17675b01f6245113629c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "143381004f03317bd67bc74f5e8457218e6d6ea42d99ee7053994eb26f48b02b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1d489c122431f770ff38e13b235af6403a14a826808da7c113ee1f87013dc5b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "441cf4911893e34463aef2e632ad6255d48128c750f0b6f7c66ed3d94b86383e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "19eb6a3c126eac08c703c7c3ed5c1df8f7ea2a4da64826ff0d300c5f9deaa33e"
   end
 
   depends_on "rust" => :build
