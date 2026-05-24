87f4346b321f8818849bb58eea0e549376e47733
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:54:07 +0800

    helix: update 25.07.1 bottle.

--- Formula/h/helix.rb
+++ Formula/h/helix.rb
@@ -7,14 +7,7 @@ class Helix < Formula
   head "https://github.com/helix-editor/helix.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "7f0f62bb6efba768f5d1f2e39cd060be9e17d35704c66597955da5bb3a898c25"
-    sha256 cellar: :any,                 arm64_sequoia: "55a17081c4827c430ac891d6d60ed6c88f8ac698727f70467d879aef85f40e7b"
-    sha256 cellar: :any,                 arm64_sonoma:  "b0af62e33605ad1fd05cdb6ef8c978a71cc714299ae12164d1d09aa71f6fa3c9"
-    sha256 cellar: :any,                 arm64_ventura: "9a37be124a4fd1c03b74c965a8f0267d112af349d24b7df90f0dc8f7db9c9b36"
-    sha256 cellar: :any,                 sonoma:        "0549f3a3483f52e6f4cf91d1eb5d0bbc7337f8306cce31c33f833ce41acbe1b4"
-    sha256 cellar: :any,                 ventura:       "860969afdadeda30a99d0bffce48ace7b9e8c25bd8d8b03bc734043268c96955"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb65655cfc088fb01da162a60ab75b419bbacb4f9e262c3be09c4193c382b56d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c25fb6e1058913aaeb3b39e024bd7c917ceb6bb701ade5ce9cfe2e967113363"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f1de95e2c6b0ca932adcde33ffcdacb4830e1fba58fc272bd086bb456ce84e11"
   end
 
   depends_on "rust" => :build
