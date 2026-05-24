93cb21558b35a19556c945850a4ee07171b955f2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 01:16:25 +0800

    lstr: update 0.2.1 bottle.

--- Formula/l/lstr.rb
+++ Formula/l/lstr.rb
@@ -7,13 +7,7 @@ class Lstr < Formula
   head "https://github.com/bgreenwell/lstr.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a145541e3eb562c02740fd457f3cd5a603b77718ffd570571ff2ea5fbb047e4a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96fdc1f49bacf56b1caef9a24869aa968240b58f1ee3c67101d77de66987e55c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c459cdc51f312f8dc5848af81d469484b43d7f9eb9501836f0ea3723c03f400"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fa3ae1d10d7ca531c029c10588c96e8cd0a02d308cd0b3c77112f45843481397"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "60eff494111d095ffee7343cba34dfb34a26e6aac7bb3fa7bf1108d2b4b0cd13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02ce861934304a640142f38b36bc5c1e523f4235766579e6cbac10bc17793b55"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "48f669846dc07830efc1c5564921b8498e9dcd13497bb06443dc72e2e4020f76"
   end
 
   depends_on "rust" => :build
