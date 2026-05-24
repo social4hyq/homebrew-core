31c6985ddfe10f6909336ec823334d572def1c83
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 03:30:19 +0800

    skate: update 1.0.1 bottle.

--- Formula/s/skate.rb
+++ Formula/s/skate.rb
@@ -7,13 +7,7 @@ class Skate < Formula
   head "https://github.com/charmbracelet/skate.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "966917f5054c2a722ce35156256437222602a118fa3a27a609c54397b7155730"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "966917f5054c2a722ce35156256437222602a118fa3a27a609c54397b7155730"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "966917f5054c2a722ce35156256437222602a118fa3a27a609c54397b7155730"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e28973a036de11d215bb911adf8708dd1e5d75071578bbc288c907c061cf23bc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5d5dad73511d894bf5ef083edeb30eef46b13398fabf73b1f6d92e062644c9c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69757b83e851a3f06e7bd001afd3f27b813076f66c7b99c0ed84b6715daa1a3f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5301c7927e91621aa1c4dea06c74e028b454b03d2ae201371acd36571992d17f"
   end
 
   depends_on "go" => :build
