f3eb7fa31277a5a67fba496e9f894240e3b412e7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 22:02:21 +0800

    planus: update 1.3.0 bottle.

--- Formula/p/planus.rb
+++ Formula/p/planus.rb
@@ -7,12 +7,7 @@ class Planus < Formula
   head "https://github.com/planus-org/planus.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5305b7ce3f3309166104fc02522883d8bfd22ae452cde7dbae926882e5ea7fef"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71741fd531fb3eb105cb8a9873d10970eeb1d2cd1b270c1aa017e1b2ee9c15b0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3473a2125c4c41b932c2b7ba40988f02a565e027f5f6d8bd1fe0e8e242bb2d37"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d88d632135ede2537474539ddd5bc51087b3582c3cf61ffe92821a757d2ef98d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5e4437750ab00b286495e9947d79930e1797cf1f2121b06aadf6ec568d4ec261"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "334767fa2e76bfba0518a882c5fb33e5e4a6cad3b3016365bd8de363a617eaee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5a48320b9a328de3710cf411108474b63d398fc423a0c456fc71011993560e1d"
   end
 
   depends_on "rust" => :build
