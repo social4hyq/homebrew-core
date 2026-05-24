500add058fd645ec7b9a4d1d0b50678c0a261266
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 21:29:21 +0800

    neatvi: update 19 bottle.

--- Formula/n/neatvi.rb
+++ Formula/n/neatvi.rb
@@ -8,12 +8,7 @@ class Neatvi < Formula
   head "https://repo.or.cz/neatvi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9406bf951d0b0a979a5612007984dfa91f22d5454a5e870584ec08a471774f2e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3db63f358cf12903b9cafc397ae157e06a9061b482dcfaf8d62abb92bb6237ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ecf2f32002b0368c91203b6e591a78e6d2a2833564f3614ce2eb33a39301d962"
-    sha256 cellar: :any_skip_relocation, sonoma:        "68226401e3beb02fc3a153b0da657ff845c343bf9f22eb1f19ae9e6d2a7665c1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c1eba594e3c37a0d0dfe78dba342cfe5bf43c05664367539fb37093c24ecfb5c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb3ab5cc3f3209cd2789e90195f6e9ebddd963bf021a1b5eae80b648e556e2d3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1354d38a543e60078618436774a86d3b662829d48953c716e8150375c0289aa2"
   end
 
   def install
