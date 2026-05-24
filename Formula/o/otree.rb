b24884e96600bacdd365d77848344d495dd13692
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 22:26:03 +0800

    otree: update 0.6.5 bottle.

--- Formula/o/otree.rb
+++ Formula/o/otree.rb
@@ -7,12 +7,7 @@ class Otree < Formula
   head "https://github.com/fioncat/otree.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fc284b6852155633602e05b83af7abe3beca93ca3cd0c2fb0e6b381bd5d71800"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4bfe228d721e046bbc432bbb07ce51b24636e19e8c8a4c3121bd31c1bc491599"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8e6aa21316b114197bd4e88b7fa8a15b041c3e2b42d7e4a3cccaff1fd655a7dc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1c17f845069734c7c8b2425df273a621b4edb3f9a15d3a740ca92c386b355659"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fea68fc6e840268433b89bd037722198e60bd2e61d26b84cfcc1205596cfeccd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f35e12e01dea0134aa2d1abf921e96ebad8a1cdb5a6de267362d1e3f85116651"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f68239576b0fe6c05efd839265fb80b1c5d50afa6f3fb563637b2b0ef0a10764"
   end
 
   depends_on "rust" => :build
