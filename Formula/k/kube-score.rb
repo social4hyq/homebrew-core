a60f744e3661597f46b521128d29ec25891a4c05
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:40:09 +0800

    kube-score: update 1.20.0 bottle.

--- Formula/k/kube-score.rb
+++ Formula/k/kube-score.rb
@@ -8,14 +8,7 @@ class KubeScore < Formula
   head "https://github.com/zegl/kube-score.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f56227624dd0d4980bddcaa35592e0848a44dced318dcb595838eec62bf8b835"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8c59a7d469dad58f1fa600c7e28fd9355ea66991c5c96921c72408eb631c8823"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8c59a7d469dad58f1fa600c7e28fd9355ea66991c5c96921c72408eb631c8823"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8c59a7d469dad58f1fa600c7e28fd9355ea66991c5c96921c72408eb631c8823"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5c6eef05adc999c965abb8fbfe2e87845b4ae9dcd48109648350823fb755c861"
-    sha256 cellar: :any_skip_relocation, ventura:       "5c6eef05adc999c965abb8fbfe2e87845b4ae9dcd48109648350823fb755c861"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3d8264d94b19a2eb76c3e07f8dfb29ea87127d0941c5d3bb53bea0202b93ebc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69f6522e699234368ab760c7c0f55b66cb5fa9c020c923d9656248c56de0e169"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bbbd2807ffadd16f8f38bf2c5ff8e3e1b2ad04ddf453102d25dba8c044d42618"
   end
 
   depends_on "go" => :build
