d136c3a6c7446a901e5c776d3f8cbd19216a4ad0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:37:01 +0800

    oslo: update 0.13.0 bottle.

--- Formula/o/oslo.rb
+++ Formula/o/oslo.rb
@@ -7,13 +7,7 @@ class Oslo < Formula
   head "https://github.com/openslo/oslo.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1836a8af35219dad7ccc7356b6fc21582ea7caf647794abcf8e49de38c1aa0d8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1836a8af35219dad7ccc7356b6fc21582ea7caf647794abcf8e49de38c1aa0d8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1836a8af35219dad7ccc7356b6fc21582ea7caf647794abcf8e49de38c1aa0d8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "54e95d65cc8760adaf32a962d23acfb89bceeb6e5c02ca013d3b3892c4bfcd33"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "24bfddf720e0e388ee1c742d8274a4b9c418f7715a6b770f8214a61fe177f8d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc923992b76b691507a0423c709070cbace601f1fef41ba8217a7d442591e900"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "de9fb9127cb849e9dd4314a86569664ff27be7c91d22dec6513309df7a33feff"
   end
 
   depends_on "go" => :build
