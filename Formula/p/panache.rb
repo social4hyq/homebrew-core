d39efa0756be817bcf0b7a1903a4b9542d7ef4be
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:59:23 +0800

    panache: update 2.48.0 bottle.

--- Formula/p/panache.rb
+++ Formula/p/panache.rb
@@ -7,12 +7,7 @@ class Panache < Formula
   head "https://github.com/jolars/panache.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e91d5cace3c0c587143766f13af089c0ed6af1508b2218928f96df8d1ac72171"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "049dcfabdb5468ba3cba77b87d8e2b15e3fe1e065237045185ed5eafec08253a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1c6b7b39ea64c006f0ae3088c090f135681f2a21889b206c0da741b553e8b475"
-    sha256 cellar: :any_skip_relocation, sonoma:        "19ef435059e74457a9d357eee1a6cbe14dce1ed9dd014a353d6fd4a91e2cb757"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4fdf37bc96177865f2dbf6585b00fd9af0f2ee5550233dab63edd222014a428f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a5e4efc55824ca6c3764bb932ab81a2f2e18c211b4bb83c4dcc67f30aea5c83"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c3c962c96fe7c7bae7f9f429913d8de0a16f74803a9902d6ea3f89a64fcc6cdf"
   end
 
   depends_on "rust" => :build
