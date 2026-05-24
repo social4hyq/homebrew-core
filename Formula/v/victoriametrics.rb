a777e6babf61edcf312707648fd4c8fce7fcf9b5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 02:35:17 +0800

    victoriametrics: update 1.143.0 bottle.

--- Formula/v/victoriametrics.rb
+++ Formula/v/victoriametrics.rb
@@ -14,12 +14,7 @@ class Victoriametrics < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4f158f7faba39d20ac1523a658eda5108eb5c337695805c9b52d9ac86e67be3b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f4cd43146cb40548c34d3440341ff51cb51bcf7e5b59e7cc1171e6ce1c707f08"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1ea39f36ea665e70b4c42d0d554286813f45311dc2bf48bcb869f60b0e1ada7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d758c6c6dd803fbc2f63d85a2416b1225bc095317214b57967be150b4bc4af22"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b6a089d1cec30b61d44e4634d043a7aaabd6701500ae844a6c8e89ac23043832"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a3fada9f9a4cf0889160a971cd3889d84aa2fa92f26887ed41ee93d2af79043"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5b63d1c6eb013a4ecba51daa7ba17ce29fee1fae75392c309d677829c9e655a1"
   end
 
   depends_on "go" => :build
