db2f85ceb29d4859951f69207c605771ec270333
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 01:21:54 +0800

    vespa-cli: update 8.692.16 bottle.

--- Formula/v/vespa-cli.rb
+++ Formula/v/vespa-cli.rb
@@ -12,12 +12,7 @@ class VespaCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4371173e7838e97d386011d9e250e5326c62b952f7540ef1a7572c00b7d21158"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce8981070169d3e20ca6d1c22d61cedcdebd8f82abbc5ca11695a35beba91f07"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ad5826ebb1d168657198bbc755f7e36cf9c215d0e64f8d35cc4f410f38584955"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ab31cd4787c39327b35e34d80bd5ae1a70b4b491c7681512e892d38420f94316"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "12e438656ed80fd207cb98f60265c0fd195ce7ec0f153a0cc4de7e00a78539d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9f184d4c247f485fef11f944c353052c6ed7b861260464850b3343a9e7cc7ea"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "02bf2dd368ecc78a58571a488c3c40174f374d1e20cf6b62a6d5852d57318f3a"
   end
 
   depends_on "go" => :build
