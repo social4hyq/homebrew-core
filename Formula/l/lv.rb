b79a3a4796a30af94bb8a12a9642444a03c2072b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 03:38:15 +0800

    lv: update 4.51-10.1 bottle.

--- Formula/l/lv.rb
+++ Formula/l/lv.rb
@@ -9,12 +9,7 @@ class Lv < Formula
   head "https://salsa.debian.org/debian/lv.git", branch: "master"
 
   bottle do
-    sha256                               arm64_tahoe:   "ecb4b2ddef16ba003c1bed5ba898b4d7938b86d06b8d47a1a2f10bd1b9e1b34e"
-    sha256                               arm64_sequoia: "efaec6f504463810396e09314511c35269bf24a9e032190aff1be242605d3245"
-    sha256                               arm64_sonoma:  "3ca1bf0482f72301256a514a9b707981422d9c2307e2561eef693bab87c02697"
-    sha256                               sonoma:        "e0400df58db2d0cffa3be9f627cdd7439be8388f4cf15d7d557747fe2b5c6e1a"
-    sha256                               arm64_linux:   "f540993311d9f5564f0b2d6b9781927fe862e475ff901748d37eb989f55fb04a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51062f445c5a3c2bbdb8573e5a5a58affe8a0629fe10773ec2f1b204dda4873c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "be1f6d64f2dee5eddbb34da18014bd5a3877cb3641d2e67cca0fc439dfc45365"
   end
 
   uses_from_macos "ncurses"
