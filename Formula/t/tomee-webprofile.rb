dafb723662404a6138b35563eaa3912350c8563b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 03:31:25 +0800

    tomee-webprofile: update 10.1.5 bottle.

--- Formula/t/tomee-webprofile.rb
+++ Formula/t/tomee-webprofile.rb
@@ -7,7 +7,7 @@ class TomeeWebprofile < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e5e4e3ecc8096540ce4946768e8e4bf93176b3159535b1acb4353d7703c95fe4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "eec45f5eb67a8a9a26656b29756170728f93aa727d8312214827506df3713770"
   end
 
   depends_on "openjdk"
