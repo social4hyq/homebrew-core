b5b05b6adc169fb543fcaf74e8e111b394466c63
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 03:20:35 +0800

    domain-check: update 1.0.2 bottle.

--- Formula/d/domain-check.rb
+++ Formula/d/domain-check.rb
@@ -7,12 +7,7 @@ class DomainCheck < Formula
   head "https://github.com/saidutt46/domain-check.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6dc3a3825a8ed97d4801594785706fbc6de93938bc6fdc51646f90ff48ad8b9b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2f081fb38c78ec4e3e8e5a8c7c9c60824a57472aa3232e4c4301da3cf5eece24"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "58e12669d93aa4548ec57d5bba34e8debedaf5d684113e637f2fada060fd62a6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7155ec86dcf2d43f982b20959c906a2aae5d6abdc1eadcf4c557a13338e8df9d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "247221e67740fbfaa07daeab28b729211c75fec790dbece9a52336f4d9da50d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "626d05f289cc1e412d8dcd8de8328b928b1b55c0115c33917a125dd262fbc13c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0c926b300c6c328a3630af1c971e8a6717ef7ab73c53f9a13e08c421639094e0"
   end
 
   depends_on "rust" => :build
