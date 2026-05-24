26cfda68d3dce95cfd9108ab5d0f803324ceb5d4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 03:32:16 +0800

    tvnamer: update 3.0.4_14 bottle.

--- Formula/t/tvnamer.rb
+++ Formula/t/tvnamer.rb
@@ -10,7 +10,7 @@ class Tvnamer < Formula
   head "https://github.com/dbr/tvnamer.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d53deedadb3008ce7ee061df51374c60365b8f9c2deca4dcfc8499458b9b95dd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e1a39c18df7024232f81f193191b43ce8c3969069be107a5f9662056278adadd"
   end
 
   depends_on "certifi"
