f844a07ab83551458210732a82ffd1c6d019a18b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 02:19:14 +0800

    worktrunk: update 0.53.0 bottle.

--- Formula/w/worktrunk.rb
+++ Formula/w/worktrunk.rb
@@ -7,12 +7,7 @@ class Worktrunk < Formula
   head "https://github.com/max-sixty/worktrunk.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0b9ccde3a0e93b52340530ff4256c0f08b7e8a2d7871b2669c35c070140cc152"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd31bc4dc36aad2458985bc48075c63a401e5792313967cb0a8f92e4bc9e62d3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c53968c1ab9089e98d842de3c8f0521187d7c1c2143a7a9c8394c7aa65adfe34"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a90894b0fe20443a3f78909d4733d6b826e79d20e8a459fbcca3e772890aaf84"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b663f145eed521ae99f000ce5a693a8291f28cb8f409bd9f40e28637e0fbc9a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b781506734dffcfb8cf32595b3d6cf13f3a88da4a5e3031405eb98543594b11"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f422f1ad4d3122ecd071414d83626e2910a3ecd63f38763e7d71a8207086c395"
   end
 
   depends_on "rust" => :build
