b2a7fc412da7e2763bd9028686f852cefe5272e5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 22:40:59 +0800

    sentry-native: update 0.14.2 bottle.

--- Formula/s/sentry-native.rb
+++ Formula/s/sentry-native.rb
@@ -6,12 +6,7 @@ class SentryNative < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "09cdf9fce6646bd7274beabc9bf572892b93bb1a36196be81503ca996931bc93"
-    sha256 cellar: :any,                 arm64_sequoia: "57cd0f3f14c89b00dfcb49a0a18a2d186f2b78088de199825688592138e7c672"
-    sha256 cellar: :any,                 arm64_sonoma:  "c03916750a815d9f3d329f3834d410ab4528a8482479db912f57332e769d9dff"
-    sha256 cellar: :any,                 sonoma:        "1a38093ea7fe5b7f8ce377eab550c25f6c6c499439f4548df00caabb74c63560"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bfe4ed0f65814ba3a3da9d70d1b1038af89e2f9eddcd19268c8e6edb62125cb8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ce08827ed367b20e9ed250e555d648d84e396c83310635df06d97f97fdf10c40"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a2ed8fb839c7910c6735796bf10ef0ff7265b560f68d1748c8e6ef8b264a4cc2"
   end
 
   depends_on "cmake" => :build
