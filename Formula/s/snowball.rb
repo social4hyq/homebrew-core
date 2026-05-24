7bcc938499e44b3652b4b3f2987deaee58cb686c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 02:15:00 +0800

    snowball: update 3.1.0 bottle.

--- Formula/s/snowball.rb
+++ Formula/s/snowball.rb
@@ -6,12 +6,7 @@ class Snowball < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a8832e3a8b61dfd5eab2e1c6b5ea4e4dd0924bb4863e7ce9a2238361f9c83547"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "817a7892a41e2be04705b07f92e6bd252ab150e67df1300ab7ba9ec9afbe2c43"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0832bda4060bb98c1117f6c837cf124d82fa12c4e994c7c01e5eba80a0bd36b6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b21d0a40c0fec2d7f9b00e7617960da1ffc55e5ba7db78da03d4f43e59356b24"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c931ba1a4017fed2ccf0448f78340a04440d5d0bcdf0a9e8082f379e995aa60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e107b0c82bc1ee45569e2cb0d8de09abf62e363dd45756d1b2b8652a4382a321"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b13875e8ae7d65bc8d2daed90bcd77ae01d97763823aca511792e26cae841acc"
   end
 
   def install
