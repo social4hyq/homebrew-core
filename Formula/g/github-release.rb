67b377b1f75dabb8400fd0e0bd723491eff031cb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 23:11:51 +0800

    github-release: update 0.11.0 bottle.

--- Formula/g/github-release.rb
+++ Formula/g/github-release.rb
@@ -7,14 +7,7 @@ class GithubRelease < Formula
   head "https://github.com/github-release/github-release.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a699dd62fac62ae63696f53b9bb8ab1daf2dac9fa89d3826c97bf99dead45c60"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "783735e7a1d5f4bc00ebfb61e97c31b815af39718562f6efd8b3aa228600e2b0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "783735e7a1d5f4bc00ebfb61e97c31b815af39718562f6efd8b3aa228600e2b0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "783735e7a1d5f4bc00ebfb61e97c31b815af39718562f6efd8b3aa228600e2b0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a20268af91f08bc5e7eb972f9713dd0d5a9cb68be43a0e16b1e822bfa4c7e71e"
-    sha256 cellar: :any_skip_relocation, ventura:       "a20268af91f08bc5e7eb972f9713dd0d5a9cb68be43a0e16b1e822bfa4c7e71e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4288f2f1c32a376ff28685879e6040efb1463f6670289ed3194f72e89ceb4db1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "510735d16c9f1d621e3012a676d55ec0b72a6fc1a9d0a6476c30a84ba627daf0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c5f31e35e2b5ace94434ffb632d5013d5f57fbaed9d14480db7cd858f0e75e39"
   end
 
   depends_on "go" => :build
