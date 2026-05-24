73366efa5bcb89d360bc8c1f3ed4fd45faa19eb8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 01:16:23 +0800

    miniserve: update 0.35.0 bottle.

--- Formula/m/miniserve.rb
+++ Formula/m/miniserve.rb
@@ -6,12 +6,7 @@ class Miniserve < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "29cd82cffcde691ad0e8faae29aacaea87345becfa60781208d9d10f511e163e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "58b8e8d1cdbe0c34560eee97534ee2c2d618c87721935008054f33856adfaa55"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7fff25517959d6cb7afccb61b36f37c1c2b691a8b5868ebfa48e6d3da83929d9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b984fec4e5919fa601856ed2904ea79f84a43ac272aec7fbd8332bac17ff6049"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f70bb0f5bfd39e8026b4ca2b3ec3b26b3739ae452f12310521551aaf8c12f751"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49b74f86fd6a0c30c321459845038c6c3cf6e29cec2c23fb3892e49d3e3b1a22"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "830cebaf472ce3e036ed5d49361d663411664220f2e06700dce7360b4a0cd889"
   end
 
   depends_on "rust" => :build
