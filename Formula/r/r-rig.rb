fb94603a23322c1216d36ad0b73926d79410266e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 22:19:08 +0800

    r-rig: update 0.8.0 bottle.

--- Formula/r/r-rig.rb
+++ Formula/r/r-rig.rb
@@ -6,12 +6,7 @@ class RRig < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fe5defa6b5af48a59774d14dae1556d402c603f89572c07168089c935e898010"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "33fa01a2f31adccd71f719eea4615382a595d785813a35ee2c114208d81ae838"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f8fa195e9987e430abe4f57498a00f2558c1ea0a4f70aff6744dc2ee2b03b48d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fcec14e3f72dbf0c8f8da5de56219bbaf77dd1d5db7b145579830c57561db6b5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a3b7fc23b8e34ca7aa1a6a80c4e908c00f76f247328255f8caccd88194562e1e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7cf6f120a7d70fcde340d7b9736a00f678423d91d3b7242f628b0ea9991d3120"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a2f70081f7aac1e0a598a627e74cadad8f0539bf9379579445dab0c8b0ed1a6f"
   end
 
   depends_on "rust" => :build
