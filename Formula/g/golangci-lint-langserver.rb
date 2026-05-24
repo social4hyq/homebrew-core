7155727dd530fb51e9f0a0ca10f171c2ee212921
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 21:08:16 +0800

    golangci-lint-langserver: update 0.0.12 bottle.

--- Formula/g/golangci-lint-langserver.rb
+++ Formula/g/golangci-lint-langserver.rb
@@ -7,12 +7,7 @@ class GolangciLintLangserver < Formula
   head "https://github.com/nametake/golangci-lint-langserver.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "09b9cd130cddc6d9dd601a0ed223ae6868ea4c9095247ea1f860b728a41d813d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "09b9cd130cddc6d9dd601a0ed223ae6868ea4c9095247ea1f860b728a41d813d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "09b9cd130cddc6d9dd601a0ed223ae6868ea4c9095247ea1f860b728a41d813d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "aa20c1efcdd23c9e6c3a0d1ad8439374309d8c5826ddb59f59ed952205254afa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "10bf1b26ff7728cebb5f35c6950e8ba4fa697222e416ec05c0bfffbae2ecd535"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc965eebeebd065ca1a21f765a294705fadf2f5ea77d22f883e03d81ec78be97"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0dbf6550636dd379fd0d62c57eca27f36820cd148f85648058b40f1f1be7406f"
   end
 
   depends_on "go" => :build
