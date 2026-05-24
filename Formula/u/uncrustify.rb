4fb54994fe2cc5df2c3b053fec0b9a290d225ddd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 02:17:50 +0800

    uncrustify: update 0.83.0 bottle.

--- Formula/u/uncrustify.rb
+++ Formula/u/uncrustify.rb
@@ -7,12 +7,7 @@ class Uncrustify < Formula
   head "https://github.com/uncrustify/uncrustify.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "134d4f23548f37e9bcd1473e631356aa661c8932f5d0a7e921651b9c2a845476"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8e7415d06eeb5fb68674acf1fc13d75bb48c91786067bc1173bf8bf9b3d62111"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d7aa4f2db4a166e7ab54f519ca24d77fd03d6a0b1d7f9a6766e71d9898b8faf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ca8cf7e06ca7c3c4dce17357161c6bfde9916b44737de009ea85c30a5611606d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cc65d0aceb78338ff319e0a29d4e6f74e462dcd2e0e5b873eaba957c1ba7a8a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "36ee588a84777f80672a003e012b940ae10437a6f546076128c62ff75debc969"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "20aab6214bbbff7c47a5f3a207c841b973d00abbe8d9e30f8f3da940b73219b4"
   end
 
   depends_on "cmake" => :build
