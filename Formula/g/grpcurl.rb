ecc0b8e7e715ff225b315a1c0bad7e5717698fa2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 23:59:53 +0800

    grpcurl: update 1.9.3 bottle.

--- Formula/g/grpcurl.rb
+++ Formula/g/grpcurl.rb
@@ -7,14 +7,7 @@ class Grpcurl < Formula
   head "https://github.com/fullstorydev/grpcurl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4f0fd06ad531b223c2059d6163ad171e27b62942634eeceadd63f1092cb568bf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0b6be18c234287de07dc328bcf18ab337dc9b89a477e20110546d7615bd31815"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b6be18c234287de07dc328bcf18ab337dc9b89a477e20110546d7615bd31815"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0b6be18c234287de07dc328bcf18ab337dc9b89a477e20110546d7615bd31815"
-    sha256 cellar: :any_skip_relocation, sonoma:        "73e6d2427885ab08ae862f8e43be8b3d988917a1fdc722026cdd587be1629d1e"
-    sha256 cellar: :any_skip_relocation, ventura:       "73e6d2427885ab08ae862f8e43be8b3d988917a1fdc722026cdd587be1629d1e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d37e79c73502d787668a26837157ac3cbf673ad47cc63c2c7571cd70a93ad8c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14e2492169d85b40c8251c3ca7891c58d35f2f12cd781b720c29582573e12c9a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "580a98bc606f49670df2eba5c8241fd2ff962d2e842c5d216f210f0b9e296dfd"
   end
 
   depends_on "go" => :build
