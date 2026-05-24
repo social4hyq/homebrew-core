41300f4336f4417cb09540f45aca2e92cc2fd8c9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 01:25:59 +0800

    juju: update 4.0.10 bottle.

--- Formula/j/juju.rb
+++ Formula/j/juju.rb
@@ -8,12 +8,7 @@ class Juju < Formula
   head "https://github.com/juju/juju.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "abc969bd104ca60e43323cad926de041e144a3dd1418aa0dbc2c1e58c415269a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5952e333289a04511be819d84138256168b5e1218dec81b109b7c8a27c751d2a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a09efc7813b98ee78ab80b71781d8349bda598a7442dcf63b752487d57067045"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ba6bdc84f260ebf890b21a28380a427bf4163c978c2a704e53c4c672460b6bb3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d1aeee435b4043cc706b88e72bf92537bf6a3bd30e3a3cd273c84acb0794c5ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61518815981435c850ce62e87db413bef0b69a3b6e431d6dfc50fb115d113b40"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "794a27305d220cd43b85234b07681a6c14ee608b18cf131f9f6c12d454de1c26"
   end
 
   depends_on "go" => :build
