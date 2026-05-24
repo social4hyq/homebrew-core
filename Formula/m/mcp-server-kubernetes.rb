eb426d18f9bae408284d84628917a068d1b5f935
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 06:49:10 +0800

    mcp-server-kubernetes 3.8.0

--- Formula/m/mcp-server-kubernetes.rb
+++ Formula/m/mcp-server-kubernetes.rb
@@ -1,8 +1,8 @@
 class McpServerKubernetes < Formula
   desc "MCP Server for kubernetes management commands"
   homepage "https://github.com/Flux159/mcp-server-kubernetes"
-  url "https://registry.npmjs.org/mcp-server-kubernetes/-/mcp-server-kubernetes-3.6.2.tgz"
-  sha256 "bb420fbdaf5c7d10505c5c5636b3890123b01ed5c71b97e1206a361c14fd6410"
+  url "https://registry.npmjs.org/mcp-server-kubernetes/-/mcp-server-kubernetes-3.8.0.tgz"
+  sha256 "9341cbe41747e55edbfed4bb178541c80fa96fc77f9090c211cee77c7b21d135"
   license "MIT"
 
   bottle do
