98fc111673adbb5aaeeab4538459e5cc5f29e76f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 22:30:46 +0800

    ignite: update 29.10.0 bottle.

--- Formula/i/ignite.rb
+++ Formula/i/ignite.rb
@@ -7,12 +7,7 @@ class Ignite < Formula
   head "https://github.com/ignite/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7d7cfa24fa703759099b5b3c6633450deed3948d4b7efb31fd537775a4f3a4b4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9101f5e6bac265f4885eff791c30d37bb45140a170bcf96f7f1bfdbf926b560a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "749c2e63478fc580597e5d8996e8cc88db0e19bce6466cf0d195aab2be89faff"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a00f105cd465fd963829e3b91aca1e2e9d2d943a9f279e8be2f4831481c43764"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7515bfdf7f8e77019ff9b85c6176742086fe6bc46677b53fa289729cdb6a43e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "632260e5c804e7848ab43b114ca4afc67795083ea6102a0127884f53c4d32a1d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6ab2bb618bb9403ff30e400f7026446d72f24c6bc6916adb9cb757de344848e8"
   end
 
   depends_on "go"
