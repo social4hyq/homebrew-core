737b617ea6625ec0885c73e4bd5336083bd2bd66
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 02:15:11 +0800

    aiken: update 1.1.21_1 bottle.

--- Formula/a/aiken.rb
+++ Formula/a/aiken.rb
@@ -8,12 +8,7 @@ class Aiken < Formula
   head "https://github.com/aiken-lang/aiken.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1880676384c217aebfa1835c94df19fa25a9f30049c1259a037d68d0e532c82e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd5de1d90c2b519dc31172a78c2561b0ff231089d2c6059ff5cb4cb1ac149fc8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b094e38d7c67bde13386ad4b32f2242dd15cf1f611d5867fc39881ba80b36631"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8bfc0c056f9d7d513c0df13b2ef087130a5d931cfad9ac39fc82489ed996bb45"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca34ae5907f8efe7affd7e3170ea1a4cf8d926cb010c216d217e217175396946"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ee3811ce4cac5efefda4a264ffe65fbf7631bac3ef59aa4cb25aef84525daee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e7671b4fb69fc80b728ea29cb63491d506a3cf0c05b7b691e244a621d3783b4c"
   end
 
   depends_on "pkgconf" => :build
