1bf49a8d31a694150f898f23eed34252eccec783
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-24 22:10:21 +0800

    htmlcleaner: update 2.29_1 bottle.

--- Formula/h/htmlcleaner.rb
+++ Formula/h/htmlcleaner.rb
@@ -7,15 +7,7 @@ class Htmlcleaner < Formula
   revision 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "481e7ce48a7dc9bddaa359cd8b30412f993266133f606e0970031feb20a3e924"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ee4cfa168e59ef59a77de847f53f23c4f9a2a7be9a1f1844608c72b5ff1b7af"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "35348580167a179573bf545a60831a398a8d70a0c567d313acdb1707d9068d15"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b8823d1ba1ef2e847fe95da75054ec681d3aafcafef2c1bf084b7f917c02884c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5e7f1b1c468d1a5ead2ed450fed70e621ee5b429486da31ac05474d9871473ed"
-    sha256 cellar: :any_skip_relocation, ventura:       "8dc19a84c07fee65cef8e0889cae31a1e9f1a437f78911808340bbaf5a7a9a2b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0cdd1dd6b3a16f874d8980cc78f599dddc97713c322b5e8f962a7cf88810efd9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49d3b92068ee5e037f6efe9232227d6b49069c0f6e0a09a1abd34467290755b1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e248f5d36953bd0cf23d3140dec3fed38f35d5d9123abf6c6cc23e47522cadc5"
   end
 
   depends_on "maven" => :build
