1710cfd68a4bf5fe8c115b18efdc83960fbab09f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 00:59:29 +0800

    netmask: update 2.5.0 bottle.

--- Formula/n/netmask.rb
+++ Formula/n/netmask.rb
@@ -6,13 +6,7 @@ class Netmask < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "cb0530aeaef1e274a3e3457b498d583424b9558fbb4647b20ff0d5020a46cb55"
-    sha256 cellar: :any,                 arm64_sequoia: "378d351713f5eea2019eaea0b601dc206a9dd6a0a3235dd77d450e908876f2b7"
-    sha256 cellar: :any,                 arm64_sonoma:  "d4229b7337b45ee6950de060d01bf4dcaf0d4588152e3e8a765e5b0cae9119fb"
-    sha256 cellar: :any,                 sonoma:        "62421d9e2ff7f4a5f547531b580132eb1732d8d9aa5df3fdcb644e8b170b6a8b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1fc97a92cfc5d50e0a561fd8744b6a3050ea1f42acc152eca977514ae3e6750"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc66aefa5400e4501b5b66157e684899d3790074ae8fc4b163edd957d931c7ba"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "44087c22c068fd6be9437c5e06f79fe050f79913f3d5ba71b07dcb05ba5f4e6f"
   end
 
   depends_on "autoconf" => :build
