435bf9cfde71cc05d7efc058ae5812d9396e45e2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 01:29:50 +0800

    diamond: update 2.2.0 bottle.

--- Formula/d/diamond.rb
+++ Formula/d/diamond.rb
@@ -6,12 +6,7 @@ class Diamond < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e3e69f7f5b7b92d335de4b98bcd8398925227675f8f120992ca44cd795358de7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "622377d3ba4cfb50e49d85e523af4f8bff24532a32b0c869eba15e97d9277ac0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "29d26af781608771feb8896ff1528db0b68e1e98998a999364a9306748f5dd97"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ecb12efad1a38e2a05176f66fe84b862427c1c999a28bfe406b109d872d935f5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "809f12de9c7f4d6ea499435e497fee8b77a6fbc161f5a95bb74a738a9eef52a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "81339d78d33babfa714699e616e87cea29c8e723b1290c1230c0c53c6d995069"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0dbd3de6c627869ee25fdc3b9a0943b31bbe05684c87cd406647248ee8dfbb62"
   end
 
   depends_on "cmake" => :build
