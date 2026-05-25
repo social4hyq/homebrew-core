d6c9a661b1fd19af2992eca0e7dda2624c43e857
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 22:43:35 +0800

    lf: update 41 bottle.

--- Formula/l/lf.rb
+++ Formula/l/lf.rb
@@ -7,12 +7,7 @@ class Lf < Formula
   head "https://github.com/gokcehan/lf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0614ed4032dfb16c5d420b1a45de4df073a5fe96ecd707065f66896eae3c86f4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0614ed4032dfb16c5d420b1a45de4df073a5fe96ecd707065f66896eae3c86f4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0614ed4032dfb16c5d420b1a45de4df073a5fe96ecd707065f66896eae3c86f4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "52277c0b4e6e2b2940a495a845d8763be4cdd13752639183b6693a19aa37d8fc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bb678371b96adea7927bd147af8242d7002350bc2465ba186445df0aa4947b09"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fca1291939ad6be0337c0154638cbe4c5a4fad868390ed472f5020999ffb4671"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fca6299983fe598ba533c3c152a588c050ad30748666bae70c81deace916f5a3"
   end
 
   depends_on "go" => :build
