71f5b50ae59a5b7b4f8c2b1489e777b85d97864d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 09:46:53 +0800

    mcfly: update 0.9.4 bottle.

--- Formula/m/mcfly.rb
+++ Formula/m/mcfly.rb
@@ -7,12 +7,7 @@ class Mcfly < Formula
   head "https://github.com/cantino/mcfly.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "adb59f4c01dfe97f374aab237d28fb3206dc85daf3a85a0b425eb1aa6f1c8d29"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37c37ed43e36e995c8ec71cb72dd4f3f4e2271fa46e9c666d9549322078fafc1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8e2584592bb53ae2eea44285f31e7bd39569838ebbcdc6962d1bfe6818122a89"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1f25c5093b996476e61ae0182bca0b73a8ff66dba729f2a95c5ac942d109018d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4cc5e2c97279ba22d4f2114e05668037a40ee42591d9beec496ee4b928bac539"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8485745197b6e8cdbdbff7c2ad76b15519c62501614fd0389a5d29c0a5d99b0c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "59f7cc5aea2b2a94e0b61a092ea51d46b459bc46481acb9347008a4cfee0974b"
   end
 
   depends_on "rust" => :build
