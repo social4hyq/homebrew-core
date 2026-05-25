797e6fb923421250e8fe4bd67357b0ce55eb75aa
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 23:23:32 +0800

    httpx: update 1.9.0 bottle.

--- Formula/h/httpx.rb
+++ Formula/h/httpx.rb
@@ -7,12 +7,7 @@ class Httpx < Formula
   head "https://github.com/projectdiscovery/httpx.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7987afba78eed331caa1e98202be1aa8627951e7ed6d39a28b606d4f1731a24e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7d8b19a1623a47f37c0075b387b7981530fe5fecef632ff3cabbd75ae32e6cff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ee7b434669383bda6d2ecf5440171da466149ff95c1eb945c1ffb1c88494629"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a3716c1e1f58c7d7885500d45ccb937fe39e7aef7a70d7a6f2df9b9e8d9f56af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ab30fed23c3987757f83508078aa871ef97cb610a567187200735e882a4174a1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed81c71788dcffb3f6169171d9a47da3d3a8be873edf99a6db7256fd514d28a1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0b415b970eb1ee28003e035167529e0e5ac38fb3c98989b0291345b86f7abd71"
   end
 
   depends_on "go" => :build
