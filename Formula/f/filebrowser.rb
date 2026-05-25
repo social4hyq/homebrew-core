94dc069d55054d6ab36fef0dee763238ed5a99dc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 17:19:11 +0800

    filebrowser: update 2.63.5 bottle.

--- Formula/f/filebrowser.rb
+++ Formula/f/filebrowser.rb
@@ -7,7 +7,7 @@ class Filebrowser < Formula
   head "https://github.com/filebrowser/filebrowser.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4cb10884013f293ab0aa1d1c0f3b1146f11fb9297d24f1779c110a4c379edce9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f8fb6404205f18d478ea7e8357312691120ad5b0f1c99655712aa5937b2952a3"
   end
 
   depends_on "go" => :build
