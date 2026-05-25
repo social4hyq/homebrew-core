de1b78e2fe04f92d4229160ba68a6282fa015a3c
hqzing <hqzing@outlook.com>
2026-05-25 18:35:04 +0000

    dockcheck 0.7.8 (new formula)

---
+++ Formula/d/dockcheck.rb
@@ -0,0 +1,27 @@
+class Dockcheck < Formula
+  desc "CLI tool to automate docker image updates"
+  homepage "https://github.com/mag37/dockcheck"
+  url "https://github.com/mag37/dockcheck/archive/refs/tags/v0.7.8.tar.gz"
+  sha256 "b30c9151bb1672108562ab91cf947bce437d622098c13c4d00a519022e13fc71"
+  license "GPL-3.0-only"
+  head "https://github.com/mag37/dockcheck.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a508ab845eaa04fb571ed900731707c56974b9a7a3be1c15e0827e63c0ca9515"
+  end
+
+  depends_on "regclient"
+
+  uses_from_macos "jq", since: :sequoia
+
+  def install
+    bin.install "dockcheck.sh" => "dockcheck"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dockcheck -v")
+
+    output = shell_output("#{bin}/dockcheck 2>&1", 1)
+    assert_match "user does not have permissions to the docker socket", output
+  end
+end
