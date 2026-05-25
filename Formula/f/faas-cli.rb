2c2226d04300aad0d769df2c1186e69416537a16
hqzing <hqzing@outlook.com>
2026-05-25 18:58:19 +0000

    faas-cli 0.18.8 (new formula)

---
+++ Formula/f/faas-cli.rb
@@ -0,0 +1,90 @@
+class FaasCli < Formula
+  desc "CLI for templating and/or deploying FaaS functions"
+  homepage "https://www.openfaas.com/"
+  url "https://github.com/openfaas/faas-cli/archive/refs/tags/0.18.8.tar.gz"
+  sha256 "4d0eaf6c01ebbd69dcee389c3446bb6ea0cf042cb6f92fd471b5ec7b2785db96"
+  license "MIT"
+  head "https://github.com/openfaas/faas-cli.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "adaa4ef0ef1be410575ade58f0797c7a1e2c185d3b8d5ed96425070e3582c62c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "adaa4ef0ef1be410575ade58f0797c7a1e2c185d3b8d5ed96425070e3582c62c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "adaa4ef0ef1be410575ade58f0797c7a1e2c185d3b8d5ed96425070e3582c62c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "52dafaf0046bd274a4b6b574dc176741ac520dab34aefec7317901eb108448d2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "061c10610fa92e25019ca8ae562376518cfe4d949f66dfa384df3c1e5cc20879"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b66e4b5d91605455a548ef4856d84cf75b515ec5020057db5c28f9b5bb3c90a"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["XC_OS"] = OS.kernel_name.downcase
+    ENV["XC_ARCH"] = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+    project = "github.com/openfaas/faas-cli"
+    ldflags = %W[
+      -s -w
+      -X #{project}/version.GitCommit=
+      -X #{project}/version.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "-a", "-installsuffix", "cgo"
+    bin.install_symlink "faas-cli" => "faas"
+
+    generate_completions_from_executable(bin/"faas-cli", "completion", "--shell", shells: [:bash, :zsh])
+    # make zsh completions also work for `faas` symlink
+    inreplace zsh_completion/"_faas-cli", "#compdef faas-cli", "#compdef faas-cli\ncompdef faas=faas-cli"
+  end
+
+  test do
+    require "socket"
+
+    server = TCPServer.new("localhost", 0)
+    port = server.addr[1]
+    pid = fork do
+      loop do
+        socket = server.accept
+        response = "OK"
+        socket.print "HTTP/1.1 200 OK\r\n" \
+                     "Content-Length: #{response.bytesize}\r\n" \
+                     "Connection: close\r\n"
+        socket.print "\r\n"
+        socket.print response
+        socket.close
+      end
+    end
+
+    (testpath/"test.yml").write <<~YAML
+      provider:
+        name: openfaas
+        gateway: https://localhost:#{port}
+        network: "func_functions"
+
+      functions:
+        dummy_function:
+          lang: python
+          handler: ./dummy_function
+          image: dummy_image
+    YAML
+
+    begin
+      output = shell_output("#{bin}/faas-cli deploy --tls-no-verify -yaml test.yml 2>&1", 1)
+      assert_match "stat ./template/python/template.yml", output
+
+      assert_match "dockerfile", shell_output("#{bin}/faas-cli template pull 2>&1")
+      assert_match "node20", shell_output("#{bin}/faas-cli new --list")
+
+      output = shell_output("#{bin}/faas-cli deploy --tls-no-verify -yaml test.yml", 1)
+      assert_match "Deploying: dummy_function.", output
+
+      faas_cli_version = shell_output("#{bin}/faas-cli version")
+      assert_match version.to_s, faas_cli_version
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
