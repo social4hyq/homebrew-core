4104e001d6bbeef706069a8f6697934646409104
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 10:20:46 +0800

    wgcf: update 2.2.31 bottle.

--- Formula/w/wgcf.rb
+++ Formula/w/wgcf.rb
@@ -7,7 +7,7 @@ class Wgcf < Formula
   head "https://github.com/ViRb3/wgcf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "bc4555752610377f8413beda7cc56b6443ea0ba5cd24010ed7829393a64cd7a4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0c3f2a417b8f64059692b76ee7a1d366f64be4f75416be3da81b8b0b5b29b5df"
   end
 
   depends_on "go" => :build
