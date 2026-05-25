151e7af5bf2cacfb67d86c8850395a271795ddf2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 23:09:18 +0800

    gffread: update 0.12.9 bottle.

--- Formula/g/gffread.rb
+++ Formula/g/gffread.rb
@@ -6,12 +6,7 @@ class Gffread < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fc146d732fa9d7ff8575941815ef0d06f3a57330da3a7ec9f18daa3c92f56e5f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa002f0123c04ce5f9e4b24589d6487bad99a2381b739f01edee872c4e58a2cc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "387db5e149ae11a55955833a00abd3d7b9310633a3887c94ca4ca073cd92cc7e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3a34d37615094a8ce4869a376bc42c594824b73e32f0dc37e7fa21eaaf8b5bd6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "30c6ff37eef9a58c0d9998112c59c8d0b9df539e1d8a6e054c195cae4db0bb34"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e89307727dd3730db1edd52ac69069225ebeb70b0cfbb4b0e117cbb67e40ab99"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "758dd5465561a1b343b120b5eafbd80c2e6a4dbd3b6213d2334c9613af6b1a70"
   end
 
   def install
