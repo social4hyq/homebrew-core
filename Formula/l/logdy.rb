edaf8ef4e5876b3e07cc09ca3db0ffd8698ed341
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 23:09:24 +0800

    logdy: update 0.17.1 bottle.

--- Formula/l/logdy.rb
+++ Formula/l/logdy.rb
@@ -7,13 +7,7 @@ class Logdy < Formula
   head "https://github.com/logdyhq/logdy-core.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "53f402762567926f939ba76f3f885f54828264dc2c160c6d5bcf46f8afe49227"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "53f402762567926f939ba76f3f885f54828264dc2c160c6d5bcf46f8afe49227"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "53f402762567926f939ba76f3f885f54828264dc2c160c6d5bcf46f8afe49227"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f77f84e02e77364a615b9aa0c20abe11e759944c4398b4b886fb2dc0766f8a72"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4251ec939cfdb8640b16d3c2a1f0a1c8a8afe46a84f5630936470b0ff4e9caf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "164bd9fef16faca0f6a3d14b38a72c63127c1b7a8c4739428e2aba731265b3bc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0703130e42ef700009ca0f87f093a10f77e907b8e30b3bc43f7b402180f9d6f3"
   end
 
   depends_on "go" => :build
