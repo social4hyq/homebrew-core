5f0fb9f7a99a48f2dc0a02cb8996de9530f1c6c1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 17:20:48 +0800

    docker-machine: update 0.16.2-gitlab.47 bottle.

--- Formula/d/docker-machine.rb
+++ Formula/d/docker-machine.rb
@@ -9,7 +9,7 @@ class DockerMachine < Formula
   head "https://gitlab.com/gitlab-org/ci-cd/docker-machine.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "19ac5e4ec39d5089c6c14533ecabd768d340e94a3e7c1061e5661ab4fb36ba67"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6e12ade6affe202d43ade99275c1bb8d804cb2b83583fb06df1cc8ff9090562d"
   end
 
   depends_on "go" => :build
