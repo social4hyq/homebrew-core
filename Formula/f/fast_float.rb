59f1d79f60f9eb9cef9efd0e5388f6cd9b6baf60
hqzing <hqzing@outlook.com>
2026-05-25 05:13:04 +0000

    fast_float 8.2.5 (new formula)

---
+++ Formula/f/fast_float.rb
@@ -0,0 +1,44 @@
+class FastFloat < Formula
+  desc "Fast and exact implementation of the C++ from_chars functions for number types"
+  homepage "https://github.com/fastfloat/fast_float"
+  url "https://github.com/fastfloat/fast_float/archive/refs/tags/v8.2.5.tar.gz"
+  sha256 "17c7fb14499fcf42c3f5d143df0fbe22172e92749ec5f75ef13224005421a654"
+  license "Apache-2.0"
+  head "https://github.com/fastfloat/fast_float.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "593c7737e090f56d72ad78326769ae7beca7fd11616a426a0fb3126498b98b4c"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test-fast-float.cc").write <<~CPP
+      #include "fast_float/fast_float.h"
+      #include <iostream>
+
+      int main() {
+          const std::string input =  "3.1416 xyz ";
+          double result;
+          auto answer = fast_float::from_chars(input.data(), input.data()+input.size(), result);
+          if(answer.ec != std::errc()) { std::cerr << "parsing failure\\n"; return EXIT_FAILURE; }
+          std::cout << "parsed the number " << result << std::endl;
+          return EXIT_SUCCESS;
+      }
+    CPP
+
+    ENV.append_to_cflags "-I#{include}"
+    ENV.append "CXXFLAGS", "-std=c++11"
+
+    system "make", "test-fast-float"
+    output = shell_output("./test-fast-float")
+
+    assert_match "parsed the number", output
+    refute_match "parsing failure", output
+  end
+end
