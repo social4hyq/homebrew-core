b99ece8b7eef420dfa318cd98f402cd4e0c80580
hqzing <hqzing@outlook.com>
2026-05-25 18:56:23 +0000

    docker-completion 29.5.2 (new formula)

---
+++ Formula/d/docker-completion.rb
@@ -0,0 +1,35 @@
+class DockerCompletion < Formula
+  desc "Bash, Zsh and Fish completion for Docker"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/cli/archive/refs/tags/v29.5.2.tar.gz"
+  sha256 "e5cf80d9e0b6630bb2e97decf664ad5f0c732b296b7c47030044c3a7287bbddb"
+  license "Apache-2.0"
+  compatibility_version 1
+  head "https://github.com/docker/cli.git", branch: "master"
+
+  livecheck do
+    formula "docker"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3b610d95bf47634f39625917d7886bf41c1ddd28e5ca0f1e760d292278a005ef"
+  end
+
+  conflicts_with cask: "docker-desktop"
+
+  # These used to also be provided by the `docker` formula.
+  link_overwrite "etc/bash_completion.d/docker"
+  link_overwrite "share/fish/vendor_completions.d/docker.fish"
+  link_overwrite "share/zsh/site-functions/_docker"
+
+  def install
+    bash_completion.install "contrib/completion/bash/docker"
+    fish_completion.install "contrib/completion/fish/docker.fish"
+    zsh_completion.install "contrib/completion/zsh/_docker"
+  end
+
+  test do
+    assert_match "-F _docker",
+      shell_output("bash -c 'source #{bash_completion}/docker && complete -p docker'")
+  end
+end
