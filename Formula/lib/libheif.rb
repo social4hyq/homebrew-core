b2088bb463a6c2bfeb0b4da65ac5c21077f8e1ff
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 18:08:16 +0800

    libheif: update 1.22.0 bottle.

--- Formula/lib/libheif.rb
+++ Formula/lib/libheif.rb
@@ -7,12 +7,7 @@ class Libheif < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "1f3e43af36614e0ac6fd9415e29b6eb142d0e32a1defafd0214a90527b7eb1dd"
-    sha256 cellar: :any,                 arm64_sequoia: "b33e8f022966a1a1f8c26b78ff83c725868313a4245180f75fc1618bfeba07ab"
-    sha256 cellar: :any,                 arm64_sonoma:  "dbdc427e50935cc75110a4f4d993f4313f9517655d9a464f6a59b7702cbfd18d"
-    sha256 cellar: :any,                 sonoma:        "e84d7d1f6c6c8b92ee45308ffc07f8f89a454a40b0a3db10f35f6103f8061507"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "38eca108f6b72bb0112a5740c9d683b258726f6c9bb8b7daee6df68da58f25a0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e88f68728cf651f75a143fdbf4e38abd3b211bcaa907d6efad67a2a51117a740"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a5a724ff29f26fa251fbefb2936bd2ba89929987fec0e752be0cda7292e5a920"
   end
 
   depends_on "cmake" => :build
