4b92760f70fb9d0eb71376d8df2b1d8d181b62f6
hqzing <hqzing@outlook.com>
2026-05-25 13:24:37 +0000

    jolie 1.13.4 (new formula)

---
+++ Formula/j/jolie.rb
@@ -0,0 +1,63 @@
+class Jolie < Formula
+  desc "Service-oriented programming language"
+  homepage "https://www.jolie-lang.org/"
+  url "https://github.com/jolie/jolie/releases/download/v1.13.4/jolie-1.13.4.jar"
+  sha256 "fd374b75b29c55f6c073b1cd29e6087ae48f99cd1541d0417010cd86629bd9c9"
+  license "LGPL-2.1-only"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "22d036e00176a261eab692373493d226359f4bd044dfc7ace796c423f3e41a1e"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    system Formula["openjdk"].opt_bin/"java",
+    "-jar", "jolie-#{version}.jar",
+    "--jolie-home", libexec,
+    "--jolie-launchers", libexec/"bin"
+    bin.install libexec.glob("bin/*")
+    bin.env_script_all_files libexec/"bin",
+      JOLIE_HOME: "${JOLIE_HOME:-#{libexec}}",
+      JAVA_HOME:  "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+  end
+
+  test do
+    file = testpath/"test.ol"
+    file.write <<~EOS
+      from console import Console, ConsoleIface
+
+      interface PowTwoInterface { OneWay: powTwo( int ) }
+
+      service main(){
+
+        outputPort Console { interfaces: ConsoleIface }
+        embed Console in Console
+
+        inputPort In {
+          location: "local://testPort"
+          interfaces: PowTwoInterface
+        }
+
+        outputPort Self {
+          location: "local://testPort"
+          interfaces: PowTwoInterface
+        }
+
+        init {
+          powTwo@Self( 4 )
+        }
+
+        main {
+          powTwo( x )
+          println@Console( x * x )()
+        }
+
+      }
+    EOS
+
+    out = shell_output("#{bin}/jolie #{file}").strip
+
+    assert_equal "16", out
+  end
+end
