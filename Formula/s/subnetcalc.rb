ff1072674a2edde0d9c047cb7f936588bc5b55ce
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 15:15:21 +0800

    subnetcalc: update 2.6.6 bottle.

--- Formula/s/subnetcalc.rb
+++ Formula/s/subnetcalc.rb
@@ -7,12 +7,7 @@ class Subnetcalc < Formula
   head "https://github.com/dreibh/subnetcalc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "4d898fb3ede780ca246054d1675db73ea9f1e959c589cc11b4fb950504db6325"
-    sha256 cellar: :any,                 arm64_sequoia: "e010fe936044545ca967b333b8aa8b070b4cd0bb1f62397e653b5b33bed1b101"
-    sha256 cellar: :any,                 arm64_sonoma:  "76790a61513fbea2c785817d20f3e291dedc4498715d66ec2ab31503cc8a5507"
-    sha256 cellar: :any,                 sonoma:        "7ea1c64b4a61e8bef1d52603169419784eeea83c168c1582c1b74c42c6db80d9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1f0c5a7bf7a46c48de87ac1fdc1ae974c8a20468e142f3f97436fa210852666"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab319c8808024515852af04a7384ff64d16b1668fd34004eb5a4bdb4aaf52248"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0d634e2b07a8c812d764bd1b3c1347305800086f841c95c321905584cc2c1ce7"
   end
 
   depends_on "cmake" => :build
