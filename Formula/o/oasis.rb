e2e3859b602b5cf1824f4b6b07c28e5bbb693606
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 15:03:06 +0800

    oasis: update 0.19.0 bottle.

--- Formula/o/oasis.rb
+++ Formula/o/oasis.rb
@@ -7,12 +7,7 @@ class Oasis < Formula
   head "https://github.com/oasisprotocol/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4c881285a2dade84b7e9dcd78c13f49fe6091b1cee472ae39ba24a43b7a621f2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fdbe856144e718b64eb71a7c050224aa9f948e84e79a453181387928fbcf9c09"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "874cbc5da9f5a1ddc06eaa652863eab4f445e59cf1acae624bc7f7cbcf2bdd31"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9b1ade713897f6738f366998bc79ff598818cc3ce83ed58809b61958b1d22007"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e6cf0c0177223a57495df7457c3004a2ff8c02c5a9281b1f061e7e328f49d199"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e6243fcfc7854579cca9eb0a9506eb93842df0cd64ca418dc4bd3f7220b528e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5eddd5464b82bd99e2b5061bebd1c6c84c86f0fe5269275bfecfa9a3e4e67064"
   end
 
   depends_on "go" => :build
