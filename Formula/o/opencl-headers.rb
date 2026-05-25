ce20dadb8e3b5340c3284d77a2b306ee64a137ec
hqzing <hqzing@outlook.com>
2026-05-25 04:44:07 +0000

    opencl-headers 2025.07.22 (new formula)

---
+++ Formula/o/opencl-headers.rb
@@ -0,0 +1,41 @@
+class OpenclHeaders < Formula
+  desc "C language header files for the OpenCL API"
+  homepage "https://www.khronos.org/registry/OpenCL/"
+  url "https://github.com/KhronosGroup/OpenCL-Headers/archive/refs/tags/v2025.07.22.tar.gz"
+  sha256 "98f0a3ea26b4aec051e533cb1750db2998ab8e82eda97269ed6efe66ec94a240"
+  license "Apache-2.0"
+  head "https://github.com/KhronosGroup/OpenCL-Headers.git", branch: "main"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "5ad1a11035085dfb898950f5cf253247c71bc4baa748befda760fc66d093a79f"
+  end
+
+  keg_only :shadowed_by_macos, "macOS provides OpenCL.framework"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    # Build an `:all` bottle by adding symlinks same as macOS
+    include.install_symlink "CL" => "OpenCL" if OS.linux?
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <stdio.h>
+      #include <CL/opencl.h>
+
+      int main(void) {
+        printf("opencl.h standalone test PASSED.");
+        return 0;
+      }
+    C
+
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}"
+    assert_equal "opencl.h standalone test PASSED.", shell_output("./test")
+  end
+end
