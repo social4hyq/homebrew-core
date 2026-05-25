bef5e2d627c67b49e66d32678620b59f588c1431
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 22:27:10 +0800

    dbmate: update 2.33.0 bottle.

--- Formula/d/dbmate.rb
+++ Formula/d/dbmate.rb
@@ -7,12 +7,7 @@ class Dbmate < Formula
   head "https://github.com/amacneil/dbmate.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d3bff0524ec4aa7175c2733c112a0916c07a9b3d8b3739a3850207a9a2bc03f5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c362807df33805f8d475e299bc6a22c9a1dac8346e445ffe1eee638f4f3cd8c1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b564a1bd09368a6c2323d2803e7eb77e4479f6df282e5255f4afafa9fd52727"
-    sha256 cellar: :any_skip_relocation, sonoma:        "82ee30716276545ce62eb907c023e603867ecdf84e2063cdea201753cd869aad"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9789c110b212bd6d1166331503055f4582c2dde56623dc00a5006dd2153af5a5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13c5f4c9e787578f8ef9fbe8cfc1c970537e7c00b8c94af2bda4dfd1202890a2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8114efbee1b3d39c927612f78eb1c74cf8ea2a03f637847ab67f27ff017d2664"
   end
 
   depends_on "go" => :build
