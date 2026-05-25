667f09736ce27380e2962d619d3bc3b940629ae0
hqzing <hqzing@outlook.com>
2026-05-25 16:16:27 +0000

    bnd 7.2.3 (new formula)

---
+++ Formula/b/bnd.rb
@@ -0,0 +1,59 @@
+class Bnd < Formula
+  desc "Swiss Army Knife for OSGi bundles"
+  homepage "https://bnd.bndtools.org/"
+  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/7.2.3/biz.aQute.bnd-7.2.3.jar"
+  sha256 "0122ea026c24285fdff48c5f523bc83750956bb920c12615c497f3d1c175f08b"
+  license any_of: ["Apache-2.0", "EPL-2.0"]
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "731e5b852f0bc3d2f0173e17515cea7f904e3ea22a3bfaca6ec333e43ce76d33"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "biz.aQute.bnd-#{version}.jar"
+    bin.write_jar_script libexec/"biz.aQute.bnd-#{version}.jar", "bnd"
+  end
+
+  test do
+    # Test bnd by resolving a launch.bndrun file against a trivial index.
+    test_sha = "baad835c6fa65afc1695cc92a9e1afe2967e546cae94d59fa9e49b557052b2b1"
+    test_bsn = "org.apache.felix.gogo.runtime"
+    test_version = "1.0.0"
+    test_version_next = "1.0.1"
+    test_file_name = "#{test_bsn}-#{test_version}.jar"
+    (testpath/"index.xml").write <<~XML
+      <?xml version="1.0" encoding="utf-8"?>
+      <repository increment="0" name="Untitled" xmlns="http://www.osgi.org/xmlns/repository/v1.0.0">
+        <resource>
+          <capability namespace="osgi.identity">
+            <attribute name="osgi.identity" value="#{test_bsn}"/>
+            <attribute name="type" value="osgi.bundle"/>
+            <attribute name="version" type="Version" value="#{test_version}"/>
+          </capability>
+          <capability namespace="osgi.content">
+            <attribute name="osgi.content" value="#{test_sha}"/>
+            <attribute name="url" value="#{test_file_name}"/>
+          </capability>
+        </resource>
+      </repository>
+    XML
+
+    (testpath/"launch.bndrun").write <<~EOS
+      -standalone: ${.}/index.xml
+      -runrequires: osgi.identity;filter:='(osgi.identity=#{test_bsn})'
+    EOS
+
+    mkdir "cnf"
+    touch "cnf/build.bnd"
+
+    output = shell_output("#{bin}/bnd resolve resolve -b launch.bndrun")
+    assert_match(/BUNDLES\s+#{test_bsn};version='\[#{test_version},#{test_version_next}\)'/, output)
+  end
+end
