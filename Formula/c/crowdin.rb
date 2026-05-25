909e2e9914b1c0a89cc1455b75e605b46584c38c
hqzing <hqzing@outlook.com>
2026-05-25 19:27:09 +0000

    crowdin 4.14.2 (new formula)

---
+++ Formula/c/crowdin.rb
@@ -0,0 +1,46 @@
+class Crowdin < Formula
+  desc "Command-line tool that allows to manage your resources with crowdin.com"
+  homepage "https://support.crowdin.com/cli-tool/"
+  url "https://github.com/crowdin/crowdin-cli/releases/download/4.14.2/crowdin-cli.zip"
+  sha256 "5df38198c91b8b75eb8d105f0eb27ceb2d7e06e24872f849d6d50b403b060aa1"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0666a7dc58a65e51c228ec92812fcb93d912e460f2222166412e6ff8be7726c1"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "crowdin-cli.jar"
+    bin.write_jar_script libexec/"crowdin-cli.jar", "crowdin"
+  end
+
+  test do
+    (testpath/"crowdin.yml").write <<~YAML
+      "project_id": "12"
+      "api_token": "54e01--your-personal-token--2724a"
+      "base_path": "."
+      "base_url": "https://api.crowdin.com" # https://{organization-name}.crowdin.com
+
+      "preserve_hierarchy": true
+
+      "files": [
+        {
+          "source" : "/t1/**/*",
+          "translation" : "/%two_letters_code%/%original_file_name%"
+        }
+      ]
+    YAML
+
+    system bin/"crowdin", "init"
+
+    assert "Failed to collect project info",
+      shell_output("#{bin}/crowdin upload sources --config #{testpath}/crowdin.yml 2>&1", 102)
+  end
+end
