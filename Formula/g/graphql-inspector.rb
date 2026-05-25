b591ff58fba9010b2168221fc3d244611e030e11
hqzing <hqzing@outlook.com>
2026-05-25 21:09:23 +0000

    graphql-inspector 6.0.8 (new formula)

---
+++ Formula/g/graphql-inspector.rb
@@ -0,0 +1,40 @@
+class GraphqlInspector < Formula
+  desc "Validate schema, get schema change notifications, validate operations, and more"
+  homepage "https://the-guild.dev/graphql/inspector"
+  url "https://registry.npmjs.org/@graphql-inspector/cli/-/cli-6.0.8.tgz"
+  sha256 "ff3ac120934059750cf078f9967c8ae81fabb5e8cec75326282d0dddb7da5dee"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3fe25c2d4bacf0aad14ec9e13d6563398879d5dd00a5b32d53c5a5376d55129a"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    (testpath/"oldSchema.graphql").write <<~GRAPHQL
+      type Query {
+        hello: String
+      }
+    GRAPHQL
+
+    (testpath/"newSchema.graphql").write <<~GRAPHQL
+      type Query {
+        hello: String
+        world: String
+      }
+    GRAPHQL
+
+    diff_output = shell_output("#{bin}/graphql-inspector diff oldSchema.graphql newSchema.graphql")
+    assert_match "Field world was added to object type Query", diff_output
+    assert_match "No breaking changes detected", diff_output
+
+    system bin/"graphql-inspector", "introspect", "oldSchema.graphql"
+    assert_path_exists "graphql.schema.json"
+  end
+end
