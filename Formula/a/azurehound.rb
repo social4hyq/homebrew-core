97ffe7ef858e81f59138b116528ab12978033473
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 09:48:36 +0800

    azurehound: update 2.12.1 bottle.

--- Formula/a/azurehound.rb
+++ Formula/a/azurehound.rb
@@ -12,12 +12,7 @@ class Azurehound < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "44ed7046767e7227e327ef24e98468b4364466ea0f97755351f0a7c4d1abe53c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "44ed7046767e7227e327ef24e98468b4364466ea0f97755351f0a7c4d1abe53c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44ed7046767e7227e327ef24e98468b4364466ea0f97755351f0a7c4d1abe53c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1f1d6ef932b450a40b451a6d1c7d97a5665b9f4123e232fc2b5aefd87d7c7928"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ceca60d109fdabdc903b168319c46424d1ceeb949985aa279f21cbcd36a97b49"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84f73d1cb774e82508dcc34a0f86bedc5bd5259eefb1aa7b89a4b93d7c470376"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "196710a3569ee98c2854f9981b75f8597df99d59cb8d3d3e89baa540ab34b6d4"
   end
 
   depends_on "go" => :build
