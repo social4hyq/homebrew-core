452b9d5fc7864e6c9ec4a94776ac88cbf3e7b09a
hqzing <hqzing@outlook.com>
2026-05-25 13:27:58 +0000

    bundler-completion 1.0.3 (new formula)

---
+++ Formula/b/bundler-completion.rb
@@ -0,0 +1,26 @@
+class BundlerCompletion < Formula
+  desc "Bash completion for Bundler"
+  homepage "https://github.com/mernen/completion-ruby"
+  url "https://github.com/mernen/completion-ruby/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "cbcd002bba2a43730cff54f5386565917913d9dec16dcd89345fbe298fe4316b"
+  license "MIT"
+  version_scheme 1
+  head "https://github.com/mernen/completion-ruby.git", branch: "main"
+
+  livecheck do
+    formula "ruby-completion"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "eba47de2a5fee4ae57cc2e1eec146d6b8602819de68ab8865a092cfbfe8aa2e8"
+  end
+
+  def install
+    bash_completion.install "completion-bundle" => "bundler"
+  end
+
+  test do
+    assert_match "-F __bundle",
+      shell_output("bash -c 'source #{bash_completion}/bundler && complete -p bundle'")
+  end
+end
