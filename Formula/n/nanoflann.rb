a851421979705d9855cdec3d1fd8198a7b0e9e4b
hqzing <hqzing@outlook.com>
2026-05-25 21:05:19 +0000

    nanoflann 1.9.0 (new formula)

---
+++ Formula/n/nanoflann.rb
@@ -0,0 +1,46 @@
+class Nanoflann < Formula
+  desc "Header-only library for Nearest Neighbor search with KD-trees"
+  homepage "https://github.com/jlblancoc/nanoflann"
+  url "https://github.com/jlblancoc/nanoflann/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "14dc863ec47d52ec3272b4fd409fd198a52e6cab58ece70b1da9c3dc2e478942"
+  license "BSD-3-Clause"
+  head "https://github.com/jlblancoc/nanoflann.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "056957181cd417f850903953ae174fd716f26e1e349ff965cfb89956a146ddc7"
+  end
+
+  depends_on "cmake" => :build
+
+  on_macos do
+    depends_on "gcc" => [:build, :test] if DevelopmentTools.clang_build_version <= 1200
+  end
+
+  fails_with :clang do
+    build 1200
+    cause "https://bugs.llvm.org/show_bug.cgi?id=23029"
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DNANOFLANN_BUILD_EXAMPLES=OFF"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <nanoflann.hpp>
+      int main() {
+        nanoflann::KNNResultSet<size_t> resultSet(1);
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-o", "test", "-std=c++11"
+    system "./test"
+  end
+end
