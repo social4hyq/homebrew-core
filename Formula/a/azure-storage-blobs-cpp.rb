a134b9e2c229150d6927772809176e160e70cc1b
hqzing <hqzing@outlook.com>
2026-05-25 18:10:18 +0000

    azure-storage-blobs-cpp 12.17.0 (new formula)

---
+++ Formula/a/azure-storage-blobs-cpp.rb
@@ -0,0 +1,64 @@
+class AzureStorageBlobsCpp < Formula
+  desc "Microsoft Azure Storage Blobs SDK for C++"
+  homepage "https://github.com/Azure/azure-sdk-for-cpp/tree/main/sdk/storage/azure-storage-blobs"
+  url "https://github.com/Azure/azure-sdk-for-cpp/archive/refs/tags/azure-storage-blobs_12.17.0.tar.gz"
+  sha256 "b512b4b5aeecd45fa26e696efef0d6e83e22b0e3b6cfb536f14cc8faeb82816a"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^azure-storage-blobs[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "80bd20199e276a785f2ef142feae41ae6e042f216157830d1f498a4d4e7c632d"
+    sha256 cellar: :any,                 arm64_sequoia: "076f6e19dac8934a60a5b5481d27ed07bb35e7882812989b05ba5f6fda326b68"
+    sha256 cellar: :any,                 arm64_sonoma:  "430234d1764d5be339264614746a9d34cf110fb9231432224174cda0c0382477"
+    sha256 cellar: :any,                 sonoma:        "1dcabbf9043dacd88d548ef14ec1e0cfe728233e49f3b47eecb8d2cfac8eec3d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d83dc191c9bbb7883634b5309015bc6c9cc564508612708808577b5c03a788c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "232744e667f39a70f538921cfc67a51a6c890531c1485a5871002b9692795371"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "azure-core-cpp"
+  depends_on "azure-storage-common-cpp"
+
+  def install
+    ENV["AZURE_SDK_DISABLE_AUTO_VCPKG"] = "1"
+    system "cmake", "-S", "sdk/storage/azure-storage-blobs", "-B", "build", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # From https://github.com/Azure/azure-sdk-for-cpp/blob/main/sdk/storage/azure-storage-blobs/test/ut/simplified_header_test.cpp
+    (testpath/"test.cpp").write <<~CPP
+      #include <azure/storage/blobs.hpp>
+
+      int main() {
+        Azure::Storage::Blobs::BlobServiceClient serviceClient("https://account.blob.core.windows.net");
+        Azure::Storage::Blobs::BlobContainerClient containerClient(
+            "https://account.blob.core.windows.net/container");
+        Azure::Storage::Blobs::BlobClient blobClinet(
+            "https://account.blob.core.windows.net/container/blob");
+        Azure::Storage::Blobs::BlockBlobClient blockBlobClinet(
+            "https://account.blob.core.windows.net/container/blob");
+        Azure::Storage::Blobs::PageBlobClient pageBlobClinet(
+            "https://account.blob.core.windows.net/container/blob");
+        Azure::Storage::Blobs::AppendBlobClient appendBlobClinet(
+            "https://account.blob.core.windows.net/container/blob");
+        Azure::Storage::Blobs::BlobLeaseClient leaseClient(
+            containerClient, Azure::Storage::Blobs::BlobLeaseClient::CreateUniqueLeaseId());
+
+        Azure::Storage::Sas::BlobSasBuilder sasBuilder;
+
+        Azure::Storage::StorageSharedKeyCredential keyCredential("account", "key");
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "-std=c++14", "test.cpp", "-o", "test",
+                    "-L#{lib}", "-lazure-storage-blobs",
+                    "-L#{Formula["azure-core-cpp"].opt_lib}", "-lazure-core"
+    system "./test"
+  end
+end
