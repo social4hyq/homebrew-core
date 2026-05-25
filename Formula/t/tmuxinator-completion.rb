8676a05b6cc20c7b5e2b0c9094480443a6545d3c
hqzing <hqzing@outlook.com>
2026-05-25 21:54:23 +0000

    tmuxinator-completion 3.4.0 (new formula)

---
+++ Formula/t/tmuxinator-completion.rb
@@ -0,0 +1,27 @@
+class TmuxinatorCompletion < Formula
+  desc "Shell completion for Tmuxinator"
+  homepage "https://github.com/tmuxinator/tmuxinator"
+  url "https://github.com/tmuxinator/tmuxinator/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "ea043d727660d4a10e0e851c0f44128849ac420b9fba477dad2a436b6f568173"
+  license "MIT"
+  head "https://github.com/tmuxinator/tmuxinator.git", branch: "master"
+
+  livecheck do
+    formula "tmuxinator"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "21e78f290cb447cc1a906963c7b27a51175b7e04d168c5ef128226f5e8c2119b"
+  end
+
+  def install
+    bash_completion.install "completion/tmuxinator.bash" => "tmuxinator"
+    zsh_completion.install "completion/tmuxinator.zsh" => "_tmuxinator"
+    fish_completion.install Dir["completion/*.fish"]
+  end
+
+  test do
+    assert_match "-F _tmuxinator",
+      shell_output("bash -c 'source #{bash_completion}/tmuxinator && complete -p tmuxinator'")
+  end
+end
