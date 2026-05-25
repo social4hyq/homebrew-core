e6f23030c5ed2e83a8dfead99877edf5d72c3817
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 15:18:34 +0800

    tevent: update 0.17.1 bottle.

--- Formula/t/tevent.rb
+++ Formula/t/tevent.rb
@@ -11,14 +11,7 @@ class Tevent < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e7447eb820e3be9a34b7dece613895a67f10de300bb30d0e59322467591f3edf"
-    sha256 cellar: :any,                 arm64_sequoia: "cf0a28518720b3c7c49c61ab6c9022cae01592fd4080679182cb7b8540f85155"
-    sha256 cellar: :any,                 arm64_sonoma:  "d6842bb23c4ce73840132be51a67414f3351c6a6f1aeb8a4f0c4a2c39b2a4853"
-    sha256 cellar: :any,                 arm64_ventura: "35724246e278253aa0f95ca74069a1349748379fc0559d00a1ffba72a5178399"
-    sha256 cellar: :any,                 sonoma:        "e6a681b4cf04d84f7b6a8ef3f446f9d798238fa38034594194cd777e5f4ff623"
-    sha256 cellar: :any,                 ventura:       "a33ba3c6e7c392e14b4f3793449a38b74294b1556b74564eaba98fbcbde7a931"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8ff33cd50247ce9a038e70ad4b99e91e4fe5b71d2127297f0f313188b51733ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2030d92ac3d56d9387cbf24abb485f6699c557f39fce15d6f46b1ea7e03ad6c2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b7262ced97fe303b00736be3cb4663120ddd10b90591ce6c2c0060ddcb717576"
   end
 
   depends_on "cmocka" => :build
