329b812fe6256507584ad5a7f2f23d48e7e0c2ff
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 10:45:00 +0800

    ripgrep: update 15.1.0 bottle.

--- Formula/r/ripgrep.rb
+++ Formula/r/ripgrep.rb
@@ -13,12 +13,7 @@ class Ripgrep < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "f4dc761b07edb8e6438b618d22f7e57252903e2f2b973e2c7aa0da518fc374b9"
-    sha256 cellar: :any,                 arm64_sequoia: "0153b06af62b4b8c6ed3f2756dcc4859f74a6128a286f976740468229265cfbe"
-    sha256 cellar: :any,                 arm64_sonoma:  "d9c83b35f30d48925b8c573afa83ec32b10aaca8f247bc938650a838d188c5df"
-    sha256 cellar: :any,                 sonoma:        "ab382b4ae86aba1b7e6acab3bc50eb64be7bb08cf33a37a32987edb8bc6affe4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bbdef955d5752e53473be06b698c45ce31682cd47d75e7c706365450bd08ff44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "349bc55db5ad4b4e8935b889d44c745ae23605c1d57d6eb639dbd5c86d573a88"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5c3834f8667477e6f2d04f461c0279fe14268606e6e87ec0ae8126a44b787fa9"
   end
 
   depends_on "asciidoctor" => :build
