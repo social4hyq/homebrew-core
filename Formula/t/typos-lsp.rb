75b737fe6b10344005d30508a92ab9c140938362
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 22:22:22 +0800

    typos-lsp: update 0.1.52 bottle.

--- Formula/t/typos-lsp.rb
+++ Formula/t/typos-lsp.rb
@@ -11,12 +11,7 @@ class TyposLsp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d729289cebbeeafef87962377457b0a6eefe205ff9254fbab18d9d23ef1d3ad"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ce8fcadf5f2d9a0601de907e34309d0471ef30048cc709e0767a916c42d3940"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "add71e7fe15238c7cedbedb1389b079a7b7abd90504462097bf611063bdfd6eb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "09452b4683b695524636abf3d5a02956228a93328e8bd562309fbe8efdbcd4c2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "49c507d3db31663bd1f23d0052e6aeb2e9b02d121c16b7ebf3e4baef660b10a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "986575aa2a2caaa35db320a8cab39b5ff928ba89eda49d363571991b1787baa4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "266126b136e29aa7a7dadaa05142182b84e0113569fb49b87be4ec216b3b6796"
   end
 
   depends_on "rust" => :build
