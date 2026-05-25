646dd637455fe8e8cbb65b30645cdd3c867d78a4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 15:12:20 +0800

    libsidplayfp: update 3.0.1 bottle.

--- Formula/lib/libsidplayfp.rb
+++ Formula/lib/libsidplayfp.rb
@@ -11,12 +11,7 @@ class Libsidplayfp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "69c4987324a6eaa1065f86f163ffec361c69ce68a96c34feed4312ea4b4cd128"
-    sha256 cellar: :any,                 arm64_sequoia: "25229d51be5e28a3c2158025c7fd5da6baa7ba35a8f559f9e4fb1302ededed9d"
-    sha256 cellar: :any,                 arm64_sonoma:  "dd06a02193b11356d6986e7c7f8358ca4e8cc21343329e8ed4606027d7c44f25"
-    sha256 cellar: :any,                 sonoma:        "de7a17072056d0ee80c192b6d9c54f69ac68d0dd705a422a785560015d0dba69"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ac464c1c9e7b3f970a76e954311ab604faca3fba1cb8580edf2f98fc8ac696e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e14acb29bdb264ff2b1d8b89cf469196b004d95d940051dbe8ae2361a63350f3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "25c05d464f2d880ac4b0db63e79f1f2a86430fe2c97e3bd910a22e1ca0be6b09"
   end
 
   head do
