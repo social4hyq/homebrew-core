e74e0b7d19f5494451eb3be185b8f547d59f4143
hqzing <hqzing@outlook.com>
2026-05-25 17:07:09 +0000

    tomcat@9 9.0.118 (new formula)

---
+++ Formula/t/tomcat@9.rb
@@ -0,0 +1,63 @@
+class TomcatAT9 < Formula
+  desc "Implementation of Java Servlet and JavaServer Pages"
+  homepage "https://tomcat.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.118/bin/apache-tomcat-9.0.118.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.118/bin/apache-tomcat-9.0.118.tar.gz"
+  sha256 "2fd31ef9da9929b878997f731cf25536feac574161c02d9399727471c7c406b2"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "09dfc3b9996937697093bf8357a42ab7689b93d080ab0cccdd6fe5fe4bcdce26"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "openjdk"
+
+  def install
+    # Remove Windows scripts
+    rm_r(Dir["bin/*.bat"])
+
+    # Install files
+    prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
+
+    pkgetc.install Dir["conf/*"]
+    (buildpath/"conf").rmdir
+    libexec.install_symlink pkgetc => "conf"
+
+    libexec.install Dir["*"]
+    (bin/"catalina").write_env_script "#{libexec}/bin/catalina.sh", Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      Configuration files: #{pkgetc}
+    EOS
+  end
+
+  service do
+    run [opt_bin/"catalina", "run"]
+    keep_alive true
+  end
+
+  test do
+    ENV["CATALINA_BASE"] = testpath
+    cp_r Dir["#{libexec}/*"], testpath
+    rm Dir["#{libexec}/logs/*"]
+
+    pid = fork do
+      exec bin/"catalina", "start"
+    end
+    sleep 3
+    begin
+      system bin/"catalina", "stop"
+    ensure
+      Process.wait pid
+    end
+    assert_path_exists testpath/"logs/catalina.out"
+  end
+end
