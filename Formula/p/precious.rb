eb205c7ad49aea735bf8db0eb6f7a0f573900976
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-25 14:56:05 +0800

    precious: update 0.10.2 bottle.

--- Formula/p/precious.rb
+++ Formula/p/precious.rb
@@ -7,12 +7,7 @@ class Precious < Formula
   head "https://github.com/houseabsolute/precious.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e353b6b071a2685bb336b928f349f4972fe1ae6e191fd29de0222e3d2974e87d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a70c12c9bd50dcf82fddbcf749512798e2507db11f2bb8d461b362746c89d7fb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f5bde820805239a60f4d653592d1c8194fb6124afab1132687446874a361ac3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3a2aa357c9f34be8cbad09f3f63c4eb558fe15a051e9eb113484cbfcf869ac40"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "61f748963bb1c21c9a8051fedc3838fa6928356ced612b6a9c3c285d2c4ecb1e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3bc673c7e646c2bc0950e6d3e02ae8603e2b0c8f92d529a048a66d0a4761222"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5f9d9942a04043886f25a1905cc2b76308a7ef3c6c45dae1c44ee8a10931e5a7"
   end
 
   depends_on "rust" => :build
