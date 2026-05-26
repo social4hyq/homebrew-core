b2737a682a76c9babab4db239357fd7228e54514
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 18:03:45 +0800

    yoke: update 0.20.15 bottle.

--- Formula/y/yoke.rb
+++ Formula/y/yoke.rb
@@ -9,12 +9,7 @@ class Yoke < Formula
   head "https://github.com/yokecd/yoke.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5390695d3ab78777f94d9f84962be78426671fef3dc4bdef1ae11959802af718"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "02deedeb0f41b837ffbfa9d434b12dd1801a4b61c80c78f0629f3af39a1563ce"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8f02f8811f5494460539d2a0cab45db40e263bfc75cc3b4de8d835bbb9d219d3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b89272a741e2a3fc378d119497f9c2095d40a5afaf2425a218a200a5c2cb3714"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c1b9826db2ebdff0e743782c3ddc1cf8a348aeec771c16f988f861df3f899c18"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26f72f6b850e7a5e176ae2a25f04843d9e5556d986ca87aa67c9ad1fe2da1d23"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d4aeffa93b91420df2756d0fa83cd3a88a11c3a675cda637265a6c7e1b363fb6"
   end
 
   depends_on "go" => :build
