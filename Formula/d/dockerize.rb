db2b36b0f436b2d8222776c670e8a1dc05e5953e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:24:57 +0800

    dockerize: update 0.11.0 bottle.

--- Formula/d/dockerize.rb
+++ Formula/d/dockerize.rb
@@ -7,12 +7,7 @@ class Dockerize < Formula
   head "https://github.com/jwilder/dockerize.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a5ec84fb8fbb2ab6d3175e63a541974373bc0f2e2464ae9076137db779200bb5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a5ec84fb8fbb2ab6d3175e63a541974373bc0f2e2464ae9076137db779200bb5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a5ec84fb8fbb2ab6d3175e63a541974373bc0f2e2464ae9076137db779200bb5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "822b1c647fdd56d7f0b240aed0baf832a6126a497b0cc02318ae2ba1b1d5e15f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5310cfbff0584493f275849cbabdc079a0c1ed5d066a3ca42dd077723b85cd52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "910b1f7c1c83b9373a0718f221296c001f3652b359c885ab61b23202a6cae623"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d1798e3be324c0a1bfe4b0a7c63bdae9ab888f449048b6c065d515a6b6a45b93"
   end
 
   depends_on "go" => :build
