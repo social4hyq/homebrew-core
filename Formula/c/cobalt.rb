1f7f6fa9d7c850983cde75ba4092ad57a5507f78
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 15:34:56 +0800

    cobalt: update 0.20.4 bottle.

--- Formula/c/cobalt.rb
+++ Formula/c/cobalt.rb
@@ -6,12 +6,7 @@ class Cobalt < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7f3e66329a31b4bd2cfc2c4104c33fa18c8e3d29b8e31ec8bacddc433e9eeee3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "366acaee9d6f099ef35c78f4c343ed150462a46907c1b29e66f83d295c3b6ded"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bce9ba78d6061d536289c6fdb642642bea584f03cc011e475d4cf1f27b6049e8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f8421b9bc3aade05d10ea0744ca9cf2b4696ff85283a7e1b5eaeecccba431a74"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "addedad1239ca5cf0bfc4d65199d26c33f980041407d0f1fb64c0fc63b06d91c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6e75ee3e155b750a00833882cef7a966a3d02feabeb8ab965fa8091af6e7b808"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "10750a0faa7b9c724a63960925a37acc6e93e5bbb96554bd1634fdb9b446f532"
   end
 
   depends_on "rust" => :build
