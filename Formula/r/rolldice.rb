95d9e7be77e721229c432157292b7bd75dfb3fba
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:01:50 +0800

    rolldice: update 1.16 bottle.

--- Formula/r/rolldice.rb
+++ Formula/r/rolldice.rb
@@ -6,13 +6,7 @@ class Rolldice < Formula
   license "GPL-2.0-only"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "09a28787c7477eadf288aa7a5ae71453ad26dd4625bb10f16fc018268a30d3ab"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "031af343a052cfcf699f759e5437a87deb519e69a4a5e6eefb321b9e2e1076bf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1d08c666a00ef58322bc11b9082b063330b818186a6f6fb06ef65e92b599ed96"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9d31227c05f614fa803c1de1beba50ad7eafa06ac6f32afd8cf84af2798e778c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ebac82e928ab9231e039a3081c30662038cb584b9f3fb8bbca4c6eaa380440c5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92a41852d00239f7c3874e32f8cf7da6f8053a9ae0ddd906dd6e1f2c5da4176f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "089e36fd226b98cad7f10d6d6f4e5cc22203e76a0cfed3f77865a31dca85b8ee"
   end
 
   uses_from_macos "libedit" # readline's license is incompatible with GPL-2.0-only
