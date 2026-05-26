1afced8f0ca1af33c47d17b8cd89d614c33bfe8d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:28:42 +0800

    cli11: update 2.6.2 bottle.

--- Formula/c/cli11.rb
+++ Formula/c/cli11.rb
@@ -7,7 +7,7 @@ class Cli11 < Formula
   head "https://github.com/CLIUtils/CLI11.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f04181692a4d3389a7b03d14388607b5eaac5742d9761cc8c447a5df5416ab75"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fbe997cc8bd959f00490370adf22d044db2363be041393f6e3174fb1f6635066"
   end
 
   depends_on "cmake" => :build
