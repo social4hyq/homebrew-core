e25feb86d15e1b8519fe3b1462f5503130b7d943
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:41:53 +0800

    drill: update 0.9.0 bottle.

--- Formula/d/drill.rb
+++ Formula/d/drill.rb
@@ -7,12 +7,7 @@ class Drill < Formula
   head "https://github.com/fcsonline/drill.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "13ae6a7c0008708c01518af46e904e89ad976758c366d4e01b532b705629bf57"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fcb1d6d7ba6a60c0b61a263bf89f8bf92e08f8c9086720640b9db232b741b430"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea68c80898ceea9c47daf7984c9fec885994d83c2c79c926ae5c60edf06faaf3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9a0743d5cb645e6323ed57e8775844a617e967f9b9736862a4065101617c0a96"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e5dfdcd4ad9eb746c6742df724fa52365cf9284c14abd4dc4de5a76dcfb00ca6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6aa7af21ce2ba9c1963ba394dd94bc2f34156e93cb3a150faa794f0a0148e79c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ab70cb65cce6fa9333edf12b4a60e9d7aef46d1180537988ea808b5881238dc1"
   end
 
   depends_on "pkgconf" => :build
