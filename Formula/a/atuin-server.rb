168eda05832d861600e92cd2c34eb5ee6f75e1e5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 20:42:20 +0800

    atuin-server: update 18.16.1 bottle.

--- Formula/a/atuin-server.rb
+++ Formula/a/atuin-server.rb
@@ -7,12 +7,7 @@ class AtuinServer < Formula
   head "https://github.com/atuinsh/atuin.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fcb44d10d69271755ec4faef6db6042125805fb19e01d8a2600dcdc0add24808"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0e3f52b33753dab6f44671e1546ad8047de856173cfc85e48bb4a19f8f39ec0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "900ce2f8d2d41e9d1abe41e9d128b1963f17bd1079c7aac60ee280e61c70ec02"
-    sha256 cellar: :any_skip_relocation, sonoma:        "904496cb6e5c3dae118687258499944bdff7f6bc69d2d86db961687737f8544e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "88b1b28d402d70abc6b90cf8eaca62729245211e19c2ed57c1dfcd519be021c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc546dc50022138af0b53389e3de0bbe482aecfe2cbbeb7e019d707af2a08d7f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b1caa2393f89c886ae3421fda50c0663fb2cef6e7ba59fa0eceb04be19f6d32d"
   end
 
   depends_on "protobuf" => :build
