798fd0d3792101af326c388aa2a022b9a74ea147
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:53:23 +0800

    xwininfo: update 1.1.6 bottle.

--- Formula/x/xwininfo.rb
+++ Formula/x/xwininfo.rb
@@ -6,14 +6,7 @@ class Xwininfo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "96d34d4f1087c1eafa6c74937b5b0df87ee49e80762ed24023e89e0c78878931"
-    sha256 cellar: :any,                 arm64_sequoia: "de81136f2da87e8553dbabd1ef911279447842a5d9a03cdf9b05830ed464fca0"
-    sha256 cellar: :any,                 arm64_sonoma:  "c990cacb2c672ffeef97ce1e9e6bf07a62de68b31e0c844a374c6ea4330caddc"
-    sha256 cellar: :any,                 arm64_ventura: "16c77dd94be1ff317276e082156a1e9d28456e832c30aa41c0320ea320d51e85"
-    sha256 cellar: :any,                 sonoma:        "1d472ba18ecda4f5179eb264dd065876d1db5671c7b400302394e2014e70105a"
-    sha256 cellar: :any,                 ventura:       "5573df075228169227184182f804a0991056d0e517c5cba11916554b1bf44f1b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "145243b1c9fcf322bbd5815d6dd2a968d3332d3281ef899fbf385aadb92dc1e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0eec07b0f162130654bb10cfa0911bd34c3e7f32a92b35454a1ffbf70a41c0d1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "491496df7bc62969919d1e69a2ed7deec782a7134debf1c2e5c35bd2356795b3"
   end
 
   depends_on "pkgconf" => :build
