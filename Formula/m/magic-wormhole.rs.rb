a159a7c0d38c6b3341edabaa4b0891d2c4d7065b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 15:18:27 +0800

    magic-wormhole.rs: update 0.8.1 bottle.

--- Formula/m/magic-wormhole.rs.rb
+++ Formula/m/magic-wormhole.rs.rb
@@ -7,12 +7,7 @@ class MagicWormholeRs < Formula
   head "https://github.com/magic-wormhole/magic-wormhole.rs.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fb6fc9ebd5dd3e8938121a9f5fe30a7f4da6761a6149aaa397815cfc55c4e730"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13959efef8fb4cf5e870c1985756dd83b3ec84cda92716d23ca10495af073a58"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dae9b37b7d39c8b1cb28ce41a30e7dae0efcc83a7c83519bc868afe016087c73"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7816b8449b9c690c720d749c983c63daf3bd746ab332cd4673db098005b991b5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d5ac2b3ba20334153bab8f4360dc54576119c1ddb71306d2aa0c0e8bc7db79dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45aaddeca394e30d8e3e55fb4e9a707776dc37ecf146d79bcf15820741c99244"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0687e4dea0b95e82abf83e8398d46adbd31302743280e64031377988f56ebd5d"
   end
 
   depends_on "rust" => :build
