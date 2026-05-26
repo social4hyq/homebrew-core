d173e8b16cd9e9ebe30b4517cb9c583cc7e371e0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 18:57:48 +0800

    ducker: update 0.6.5 bottle.

--- Formula/d/ducker.rb
+++ Formula/d/ducker.rb
@@ -6,12 +6,7 @@ class Ducker < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b0c5d716a4e2af43b24e7ceb7d1e09b5856a2103d133027a442ffb0fd2104321"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a54018ff813415704c08ef18f54122ee747c452539271357dc240e93b118d13"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c63897960385cdd9a991969bbe100e86aacc73cc92b89940d8eb711508aa4054"
-    sha256 cellar: :any_skip_relocation, sonoma:        "993e25a290b9569dec9a08e1e7d748867df48f0b3e2493f08cf93ef214147697"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f56bb6221802f0b328df276f230a7e176fb749f0f16548441c295f322eec6327"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "772aac295ea8c9230302e24433f70097996adaca212ecfe764ce4874345aee2b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4110aef267364c44ef1788785dffc4a466e2a741dab4eef51ee8be8b266db5f8"
   end
 
   depends_on "rust" => :build
