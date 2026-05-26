911e5d284a4d7e2f44b7c939058d4e992e08ebe9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:42:03 +0800

    faad2: update 2.11.2 bottle.

--- Formula/f/faad2.rb
+++ Formula/f/faad2.rb
@@ -6,14 +6,7 @@ class Faad2 < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "b85c98bb399962a24b327bc0f361d4375169ee0490e2fa540e5958f65b7fe967"
-    sha256 cellar: :any,                 arm64_sequoia: "8778a14f57fd5d09238b9f35247fa3150b5c9fcb80e38bb5d6f730cb5c1cfd82"
-    sha256 cellar: :any,                 arm64_sonoma:  "2474465c5fda244f0841c983afb014b802332b04190ae10cb3a0c4ef00c18515"
-    sha256 cellar: :any,                 arm64_ventura: "de416959deef6759acef50251453fc80ed2dc7cc59756e04014369b61d58ef53"
-    sha256 cellar: :any,                 sonoma:        "c35f541928355a39a84f727c99f1ec98792c7a4ea60cf1e4e4f004a503ef19ce"
-    sha256 cellar: :any,                 ventura:       "561f17f4430cb7808ab93cf94a65852f24f13186bb8dd5d3f5f6f920e5b63dd8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "60e3d314b07dfa58ed8534ff0678217307d811ef4198f7e5abcbb46339757f2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b83a27d69240eda9c177272cf3858a078d10240cd0d45783fda5836bcde86c6f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "09abbbae322d34a11d939906a97285e0efe590f7338d879b96445016112cc295"
   end
 
   depends_on "cmake" => :build
