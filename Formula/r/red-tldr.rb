827950b0a801ca9bd1861f7768f9435171dca3e7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 16:31:10 +0800

    red-tldr: update 0.5.0 bottle.

--- Formula/r/red-tldr.rb
+++ Formula/r/red-tldr.rb
@@ -7,13 +7,7 @@ class RedTldr < Formula
   head "https://github.com/Rvn0xsy/red-tldr.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b899f36c60a21b05cc3e16a051e0db36d90a60882864f149b812204a7e427652"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b899f36c60a21b05cc3e16a051e0db36d90a60882864f149b812204a7e427652"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b899f36c60a21b05cc3e16a051e0db36d90a60882864f149b812204a7e427652"
-    sha256 cellar: :any_skip_relocation, sonoma:        "18d4aca22446fbd71994dc3eeca8abd0b492760fd3148d2fa170e1cfbbf4ae82"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "92cf80e291015b87bdfeb91ac777b62b5d09e8a4ab2e120e23275d149473b52b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6458162f5cf6ce365bb6a4f3688e7867fff6c8af0ab5e31386d9bfbfa4232e4e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ca0679e652d79ee52286276792803cf0dba9fe5201fb7a49804de05e55a2fff5"
   end
 
   depends_on "go" => :build
