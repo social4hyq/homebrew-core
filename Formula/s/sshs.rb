59193442f62dd88c764efe3ec70e21728f9df508
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 21:36:44 +0800

    sshs: update 4.7.2 bottle.

--- Formula/s/sshs.rb
+++ Formula/s/sshs.rb
@@ -6,14 +6,7 @@ class Sshs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2443c66fec7f7bfb754004d9a1770583c4ecd3788e267733a5f1d179aef82fc6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c8691620baa73fad3c9d41b66bb555b49018b5a4026f78b8ebfc24b726b3a8e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "438e736bbae8e0aeeac0f5b1e9833fe59601e8f94aad84093a2bf6ad7b4f3e8d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "dee907cd51f13ea86e5ff1905ab59f896f6896a06fe8ce5da239719638bc0061"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f1fa7aba391712fb5797c09cb9f77e41662e722402754aad4acde490dc0d2429"
-    sha256 cellar: :any_skip_relocation, ventura:       "7e8fa97a5a345fecb049ee6b602009dfdd7a85102e80ab31691422d82fe83d59"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "78da6be289b347c12f3c4d25788707e582fcb499be6a9e548d54add0c0254a75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4fdb3557d5009b2c521a74fc93c456b1bc3b2772da8002fb072cfbd445d14be"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "92597f66350954fc3f31e73ab50dbcef104713b2dee5a4024d6a09cc63115dcf"
   end
 
   depends_on "rust" => :build
