aaee4446908be4f6e51c7e0ca3de209f4c6c66d1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 20:44:45 +0800

    assh: update 2.17.2 bottle.

--- Formula/a/assh.rb
+++ Formula/a/assh.rb
@@ -7,12 +7,7 @@ class Assh < Formula
   head "https://github.com/moul/assh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "90d63d7a873cf718e770fbef532468ec2edebc8a6bd5704cd7c4f8c9d7bf554f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "683e1c8048760076146ce095596469ae2514d0cd2d60f3c2c60418289c9b8612"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8adb4a260bbf2e5b517166888b55222d7867fd7bc110fa350ac15e1852a76dd5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "beda22fe935a54c00605d0ad185f0f148ec5aad08c5612378fc9837fb3643096"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "533d73958dd2c5a622ebcf0eff45c1023f48932861fd55c04b95f562da91904a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d4976e76276e8a26c1023f8d799ad4607bd3dc7c05d46b4a2eb96a04b14d95e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9b4e3b18148ab8fadc11044586e80f70f037f6de69007af689f05c457844693f"
   end
 
   depends_on "go" => :build
