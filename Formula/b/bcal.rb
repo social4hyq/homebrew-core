0bfc563c090b199cc21747518a1b7beef6a5b433
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 16:20:51 +0800

    bcal: update 2.5 bottle.

--- Formula/b/bcal.rb
+++ Formula/b/bcal.rb
@@ -11,12 +11,7 @@ class Bcal < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d3282e866c6ab81d95728ba8e2f863a97bdf389417b194741c0b63f81eb65d03"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fcd6cc5fb598a558848647702c22444d8a40944b3eaf6ee92f02c2496fbc6b40"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b71b24116d10a0a414a1dc11687363d06b685549abea992d5be85e102bb05dcd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "27dd59b087eadb1eaea3287f49f37c12489d18a41a0f7ec2160cda03035b1805"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b68b2889f1b14c7c4978f1836d7854a572ac94888f9f3f9932ecd67919fa31b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "375590abb9324e0a68d3b0efb786624d10006fa3c8f766f4d9a2ed1745d92095"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "83fcb5b3caee649ec3bd5dfcb23e8edb25fab6e62b51cd76f1ae6d7a70714271"
   end
 
   on_linux do
