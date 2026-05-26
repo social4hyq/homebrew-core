f3ab7a7be954963b370e56449eae028468bf126a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 20:47:34 +0800

    jupp: update 3.1jupp41 bottle.

--- Formula/j/jupp.rb
+++ Formula/j/jupp.rb
@@ -13,18 +13,7 @@ class Jupp < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:    "851ad092f5d0183d4c1d09bba60a6f8a00cc4056ecc37bd44b70a8c29e27d7ae"
-    sha256 arm64_sequoia:  "69af220c327528966dd893daab6d625a5b5241b9273f7b53decb37c5f1e33efe"
-    sha256 arm64_sonoma:   "501d87f4b79bf8ae4a8a67b811fbca484e6bc68f6b1e04d9abfb0b1948430880"
-    sha256 arm64_ventura:  "821daf3c2f840c5a9942de15a4b0c226928e35808079db3f0d48e686a474f08d"
-    sha256 arm64_monterey: "b5732141fc6bfe41e312ee4492c2680a3dcfba4a3951c8f6ab590b2e6c887a01"
-    sha256 arm64_big_sur:  "f50e0562ad9f204659b90c6fe30d96708bcb59100e049770ca1da0c8668ebf0a"
-    sha256 sonoma:         "67aeb4c92738d070ab3401d6282d122a95a257a0c6d5572a6da184764a2018ab"
-    sha256 ventura:        "df22aefb2425b9730ca87ff48af85ef2a6660e8b2e6e6d8c68be5a20690553e7"
-    sha256 monterey:       "d4a1370a276e3e6dbe6194ec7d99a488cf79e9a03e7bdef06a347c9210c4f365"
-    sha256 big_sur:        "c2666e8966b8fc4322e70d4b5ccfe363b30ebb0166fc619cc354214bad9718f1"
-    sha256 arm64_linux:    "51d77066bd02369aac33dda74c4ed5c2dc66873e11ae1f64f329fb4872a7c5b5"
-    sha256 x86_64_linux:   "903a19653cafcf340fce69eff4be1d4a5574dde54d87c5934aa269cb25050311"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9825e95c1d9131c0af5957faa2e864cded539758e52881e14773f837acb97556"
   end
 
   depends_on "autoconf" => :build
