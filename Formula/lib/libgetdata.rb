c615552dc0e3a444e87bc56f0cd418ed016ccd29
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:02:58 +0800

    libgetdata: update 0.11.0 bottle.

--- Formula/lib/libgetdata.rb
+++ Formula/lib/libgetdata.rb
@@ -7,13 +7,7 @@ class Libgetdata < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "bea9f976c185d2e9e5377cf772635f6e72a40380bda01cb5cbdb65e22a2a30bc"
-    sha256 cellar: :any,                 arm64_sequoia: "c6dfdb5b0c484f44ba193dd52bed2f63d141d30930340fab2bbfc7a95256e4e9"
-    sha256 cellar: :any,                 arm64_sonoma:  "ee816cb9530d8ea7c20cb12cb23fa9a1a39f9568532c8b7489452d9db84c803b"
-    sha256 cellar: :any,                 sonoma:        "df59283bc4c705adf5fe040cd33a4a82f6e724a38f46907817a9f6f6f1064ef7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c7e8f70f87715450ab87a39327ea8dbef1dcf0e83c3e962dc59a4ee8e3a7ea2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "71be7913ffccb6f8ba8a1cd425b2855046706c0ea14cd0607c1a3c0f295e01de"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "40bba92f1d0e458ec17d1a80cb7187465989077447532584bef808c6d4d2caff"
   end
 
   depends_on "libtool"
