a19d0f23f8b2394a5772d5d8f6a5c97e812e0db2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:21:56 +0800

    aliyun-cli: update 3.3.16 bottle.

--- Formula/a/aliyun-cli.rb
+++ Formula/a/aliyun-cli.rb
@@ -13,12 +13,7 @@ class AliyunCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e0b734ca7306bda9556763c7046423ed0e4518f1fc0532553599d45d1b968873"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0b734ca7306bda9556763c7046423ed0e4518f1fc0532553599d45d1b968873"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e0b734ca7306bda9556763c7046423ed0e4518f1fc0532553599d45d1b968873"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a494d5efcccdd4b73bd68084d5e6b4acc87c5d4d3e57cc63d9b6c878209c6bd8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1779714094b1ff30ed206cf77b82ad4cd0db37e2f9e7b6d8971bfdd855533310"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "263d232023339b97eda2f5370465bdfe9e19f9ec100bf508743fa1cf1bebe586"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "11896158106e02420f57a1e1bc0f5d5bc6a3f6887137985fcfebfabed7513d32"
   end
 
   depends_on "go" => :build
