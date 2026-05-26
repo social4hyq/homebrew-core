ec4b4982936e789f204f86d5e008afcf6d262090
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 20:55:36 +0800

    kubeone: update 1.13.5 bottle.

--- Formula/k/kubeone.rb
+++ Formula/k/kubeone.rb
@@ -7,12 +7,7 @@ class Kubeone < Formula
   head "https://github.com/kubermatic/kubeone.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "06933354b47fed3f13e8cc4bb635313e066e645fd898427c569067599f4f26ab"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71fd211ff4b1c1bcef5309315aa0f016649a24f947cdf9808bfa896c81795a90"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c3866db5bc5bf41f0c12425172212d3fcd31f98582ca3572d1fc914fbcc75a4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "313fba9a4a22ab50e5f760e0fd50fa9bc66de39a117e1b99affff20124c9967b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "15a54f2cc7248f98db56dc4b140e39c1f752d2ec476314859ee48b58682bb813"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9f3ea16b807f87dd2663f660e7d7c81f8f079b32aa87f6aabcfe4934c80f5549"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cee085611873ab4f7a594c23bf56c7102ef49c8aa2f66701d3850839fb0f9f49"
   end
 
   depends_on "go" => :build
