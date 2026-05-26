fa32c56a8f254b55321091efa1753dcc3304f6d2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 21:49:02 +0800

    cai: update 0.13.0 bottle.

--- Formula/c/cai.rb
+++ Formula/c/cai.rb
@@ -7,13 +7,7 @@ class Cai < Formula
   head "https://github.com/ad-si/cai.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "09673dfec36162105f0b4862e8fe7e2ec3ce00a51f056443d7ead5972fb822de"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cfd28dd258e464e1b31ac3a2921f355a48ce267f30b01b5676e06e411e62884e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34e17a4e521d560f2501580cf447cf5452290b7154ddfea0339a2bfb750bab08"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ee72d21142f09d3316c3a98f5ef993c98bc55f76ca09fd31adb0fafc8d9dbc79"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7fcf810b92e598c45a5f71cb2e236bbcf5cb31026982cbf21f849d062d6297f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d254783eec727228983659f48e79dc5263248bbf8f7b4eb8b26c043f45e2065"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a372a1936aa10163d4e3c65ba4ee05bd8070d1f243e6940de94724ae47f7b4c4"
   end
 
   depends_on "pkgconf" => :build
