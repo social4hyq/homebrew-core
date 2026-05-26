c2d08fde111ea54ab39dcac2a74acc4d9ede508a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:44:30 +0800

    mdserve: update 1.1.0 bottle.

--- Formula/m/mdserve.rb
+++ Formula/m/mdserve.rb
@@ -7,12 +7,7 @@ class Mdserve < Formula
   head "https://github.com/jfernandez/mdserve.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "14ef5c7e6cd6c0fb3f941824f84ac34c3c8124d5f5bf9c78e89b16f96d4ca324"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "231269e5e95a19282c8372311bf248469f265f8bbccb4a55ea79ee5a5f4890f1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e9196915dcbbc08997a3983ae3c0dbe227c1e7a4dfb24acc8d92de3225b4a0c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3e48efc973366def35b6be6a76e1436f8fac4de92ebb00745119e982edc2d6e5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ac98a80cd18a7f550c030b932703c3983953c6c174804909376e6c0d21640f47"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "482b075537a6af00bf3856510cf13127987c1b7ee29722bc5f3c65207e106ebd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fef7c98c8e8a0030391566231686eb6bc6888c45c1b135127d366612c783d9d0"
   end
 
   depends_on "rust" => :build
