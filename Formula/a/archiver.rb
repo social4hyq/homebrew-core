23b3574c7bfa96e196341f7fbaf79a7d6280ce37
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 18:21:33 +0800

    archiver: update 3.5.1 bottle.

--- Formula/a/archiver.rb
+++ Formula/a/archiver.rb
@@ -7,19 +7,7 @@ class Archiver < Formula
   head "https://github.com/mholt/archiver.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "7da16b2c8807513b73777c4e799fa9f3a6fef4a0a00e5a5287977882bc4a1c68"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "5b6c238c7e684837a57ff1038a05b92a89b1db0311443ff3f66da556eeeb873a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "247cc8a86532c7e40d47ee84872895a7790a87c0c732cb1d4dfd19a25e8b724f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "50eb72205f3ffce1c6b64a7182454213c07cf6396469046875c5687f5f7018a9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9194e883ec240998c4c2ec26a4cc8d79d1ad29964b592ac0cc45c9b6c5da7dd8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7a4c576219a90d52a24dec089f2ef3cd900f5d9779d57fc6f6d83c8e2ae7241c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5239b35339f165119d9e93b34775bd986982c296286dfdb3fbbb456f1776d66a"
-    sha256 cellar: :any_skip_relocation, ventura:        "27b0c4dd81f24dd779dccb56944733ac3381adf3dcd9c9087f70dc53908359e8"
-    sha256 cellar: :any_skip_relocation, monterey:       "f0c4b8adac0f867744ccde72ed8d83f66bcf098f45e32edf7c1dfd347772ee9f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "74fd6ad65f0b92af3a034874d6317065b7805d98cb945006d05dff0117d179d6"
-    sha256 cellar: :any_skip_relocation, catalina:       "b2a0192ed66099721b7662fe5d772f8a99ecb5c8922270cbc825cdcbb7032378"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "3e4334939b05d0ec67d9f84d3761b876be7a3f802239ab3848d89a9d9ece61b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "461a212b25cc90af84996b43067bbd096cf343bbe04a39b4aa40d10cb235e238"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "28180a74e985ef919a09646fe6178c10e74ad4f5db0154dc096a8433342f94df"
   end
 
   deprecate! date: "2025-04-27", because: :repo_archived
