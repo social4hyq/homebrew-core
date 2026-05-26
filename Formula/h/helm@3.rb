3b3bf3a4eb7eb49715892e0e17fc0537eae9fd5a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 23:38:47 +0800

    helm@3: update 3.21.0 bottle.

--- Formula/h/helm@3.rb
+++ Formula/h/helm@3.rb
@@ -13,12 +13,7 @@ class HelmAT3 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d4f59f57e74ba376302565617bfa4dbef9a84e1a201514ff19c7af2b7664e1e9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54ded298c41a9a34fd66fbc183b91b408507e4fcb93fc613a3db237ac555cba2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7fa2c5e6961bfb1e9c771eeb729dcb60ba5ec948f07ddfa13c608a53490fba34"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2c9c46ffe53c780aa88f48a00028350d936bebd86f969967652022bb16095388"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e712583c054229cb7911600b37ba1c05bbd2648ce0c4f76b6313e82cc991897b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e292c653e8d552f73972f97cade9764d4a0c02c8b970478b62ed9f5ba0509d27"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9b61b7efaf8515768165e6dc9d6790ca0701bbea98c359b9a74f36eeb9e34c05"
   end
 
   keg_only :versioned_formula
