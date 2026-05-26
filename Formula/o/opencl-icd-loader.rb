97583d56d4512609116a39b5f5fb0716abce10be
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 18:11:19 +0800

    opencl-icd-loader: update 2025.07.22 bottle.

--- Formula/o/opencl-icd-loader.rb
+++ Formula/o/opencl-icd-loader.rb
@@ -7,14 +7,7 @@ class OpenclIcdLoader < Formula
   head "https://github.com/KhronosGroup/OpenCL-ICD-Loader.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e0d457a1acf0efbcc69f119d0cebad5a3cb5c073690e94349c3bfdcfde5f6c77"
-    sha256 cellar: :any,                 arm64_sequoia: "bf65ff7f27a7877c993902277e68ff4b88e5e49a15fbd9ceb310352dc16a5793"
-    sha256 cellar: :any,                 arm64_sonoma:  "5d863bc7eb594b7a84db8a789772d4e28fa55564c07ec34b8b2ed9e8f0936149"
-    sha256 cellar: :any,                 arm64_ventura: "3d8ba3a3c64ce7cb9f0c9a3185f8a432cd8e8bd08fb57ab8b5446f7f85db9e9a"
-    sha256 cellar: :any,                 sonoma:        "3b29127cbd3ad9bfd0c59cea68ba772ce5c1c4e323b5d566d5249a1528b869b9"
-    sha256 cellar: :any,                 ventura:       "fd9899b082534a219d434469052716a6a9f016b9a9fdc15a355b8e5ee2ee075a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "74bad9df1b4a44f9f0675fabf03d7af9225993f5c3144f4104db82cace1d42cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eaff23dec7418eeb114b986687c61f1e00aa3262d1a9d0843d06dbfe900fa4d0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4cfc57ccaf190fe22824fa113a859ae665bb203d96332cf4a64b83d0bba3dba0"
   end
 
   keg_only :shadowed_by_macos, "macOS provides OpenCL.framework"
