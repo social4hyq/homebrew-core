33e54346c6819b5dd7a3ed6c6c3f4c3c316faf9e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 16:00:06 +0800

    dprint: update 0.54.0 bottle.

--- Formula/d/dprint.rb
+++ Formula/d/dprint.rb
@@ -7,12 +7,7 @@ class Dprint < Formula
   head "https://github.com/dprint/dprint.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7ce446bf4d666d34116cdd34c38a14ac25cafc0bcc15d062bea39a42883417af"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ccf4f5eef723fb6d7cf578dd25048a63674608d8581c0c68a5a0afcb7d6c68b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ebbf3923aa6b61a02d60af8dc4ae4355e3be062f86cf4a7bfcf8ca662bca3c0f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d35a8feb0a83ded26184e6a24aec240e5e1827e1f4c21f30211e970e45929134"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b8baa8d6d33843804dc1bf874626b90d8f5449368744123d425d1305f0fa0521"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bb1f11229517adc4d059f391f731edf0ad0967d548978b8d160ba8cdaa7b413b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ee0946c641701b53b449e8fb1205399e98ef37e5b429bd5fe227aa61b10b2dea"
   end
 
   depends_on "pkgconf" => :build
