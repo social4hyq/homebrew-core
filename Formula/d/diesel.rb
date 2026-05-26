2f45fe368c83fdade1b25a8c816b76db2e4b80b4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:29:03 +0800

    diesel: update 2.3.9 bottle.

--- Formula/d/diesel.rb
+++ Formula/d/diesel.rb
@@ -7,12 +7,7 @@ class Diesel < Formula
   head "https://github.com/diesel-rs/diesel.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "39f78a782269228dcef12d4920060c987fd2a1940d1996e794a4e97b67732d66"
-    sha256 cellar: :any,                 arm64_sequoia: "50a68b79d7f1e7791da69bee493e64a6a56e3a878b24a1a9ca354f0d610ac017"
-    sha256 cellar: :any,                 arm64_sonoma:  "d36554af9f0a8350895ca1977b1ee5304b87b90204eabc73c4c7aaf454996839"
-    sha256 cellar: :any,                 sonoma:        "445f1093d2e477d94e60d9b90feade982cda49d51c1dc710d833a54805c8c827"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "436379da3c999e8fc5bc567b845e1fcef2ef298269a42403e64c56b63f837292"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ad1bdd6f8e46ccec6b9883fe52be4048f7c817880e4d5b4cf5f003d92e67638"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "600f625e710122085869816695ee2841b763394065347563d79050a42cd2bad0"
   end
 
   depends_on "rust" => [:build, :test]
