1ae23fd15dc8b50c9e7ea3afe0efa9b4505e2333
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:03:48 +0800

    azure-storage-blobs-cpp: update 12.17.0 bottle.

--- Formula/a/azure-storage-blobs-cpp.rb
+++ Formula/a/azure-storage-blobs-cpp.rb
@@ -11,12 +11,7 @@ class AzureStorageBlobsCpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "80bd20199e276a785f2ef142feae41ae6e042f216157830d1f498a4d4e7c632d"
-    sha256 cellar: :any,                 arm64_sequoia: "076f6e19dac8934a60a5b5481d27ed07bb35e7882812989b05ba5f6fda326b68"
-    sha256 cellar: :any,                 arm64_sonoma:  "430234d1764d5be339264614746a9d34cf110fb9231432224174cda0c0382477"
-    sha256 cellar: :any,                 sonoma:        "1dcabbf9043dacd88d548ef14ec1e0cfe728233e49f3b47eecb8d2cfac8eec3d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d83dc191c9bbb7883634b5309015bc6c9cc564508612708808577b5c03a788c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "232744e667f39a70f538921cfc67a51a6c890531c1485a5871002b9692795371"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6fe994723adb643ebe2ff8035456f83727f8c953c8feab5c01dd2620e577686e"
   end
 
   depends_on "cmake" => :build
