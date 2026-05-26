60fa5b0d3c759dd7f9fc58ba28b43d2494fd2825
hqzing <hqzing@outlook.com>
2026-05-26 17:19:03 +0000

    davmail 6.7.0 (new formula)

---
+++ Formula/d/davmail.rb
@@ -0,0 +1,67 @@
+class Davmail < Formula
+  desc "POP/IMAP/SMTP/Caldav/Carddav/LDAP exchange gateway"
+  homepage "https://davmail.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/davmail/davmail/6.7.0/davmail-6.7.0-4068.zip"
+  version "6.7.0"
+  sha256 "46203cbf37103092af904da8945d988bad39c7b325579df31310dee46ef75926"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://sourceforge.net/projects/davmail/rss?path=/davmail"
+    regex(%r{url=.*?/davmail[._-]v?(\d+(?:\.\d+)+)(?:-\d+)?\.(?:t|zip)}i)
+  end
+
+  no_autobump! because: :incompatible_version_format
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9edabee235f963cc4122c9076e18e3023e562cf976057f0f62866147f2f98e34"
+  end
+
+  depends_on "openjdk"
+
+  uses_from_macos "netcat" => :test
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_jar_script libexec/"davmail.jar", "davmail", "-Djava.awt.headless=true"
+  end
+
+  service do
+    run opt_bin/"davmail"
+    run_type :interval
+    interval 300
+    keep_alive false
+    environment_variables PATH: std_service_path_env
+    log_path File::NULL
+    error_log_path File::NULL
+  end
+
+  test do
+    caldav_port = free_port
+    imap_port = free_port
+    ldap_port = free_port
+    pop_port = free_port
+    smtp_port = free_port
+
+    (testpath/"davmail.properties").write <<~EOS
+      davmail.server=true
+      davmail.mode=auto
+      davmail.url=https://example.com
+
+      davmail.caldavPort=#{caldav_port}
+      davmail.imapPort=#{imap_port}
+      davmail.ldapPort=#{ldap_port}
+      davmail.popPort=#{pop_port}
+      davmail.smtpPort=#{smtp_port}
+    EOS
+
+    spawn bin/"davmail", testpath/"davmail.properties"
+    sleep 10
+
+    system "nc", "-z", "localhost", caldav_port
+    system "nc", "-z", "localhost", imap_port
+    system "nc", "-z", "localhost", ldap_port
+    system "nc", "-z", "localhost", pop_port
+    system "nc", "-z", "localhost", smtp_port
+  end
+end
