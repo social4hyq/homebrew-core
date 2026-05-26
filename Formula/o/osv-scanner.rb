7aa1d77f4c45d80e0b80da74d8ca9b0f7b2aef52
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:28:42 +0800

    osv-scanner: update 2.3.8 bottle.

--- Formula/o/osv-scanner.rb
+++ Formula/o/osv-scanner.rb
@@ -7,12 +7,7 @@ class OsvScanner < Formula
   head "https://github.com/google/osv-scanner.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6c908a40747ea7cbd10f548d5f8edc32e4db31f6ae06a6b3016fb047de1b16c2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3eae4ee581c5abb4527366e9a01299acb1337624a8caea87590f3da5d412d42a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b752628540f57cc390eb4ac0944a790a112c96739b01cad432d70e4b51299f46"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a2d287434f2736e340ee3fb5d833e476d8619621d0c11bdec73a93f3f99d7f08"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0fcceaab3ebca098831b180e076c243e8a3eda8eab9e094b2daf4615683ecd11"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d1a8f8e99b621f2ef851017de9d6909e61cfdeed0262df7b21af7f11003a8e04"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "82e6a349e0020748e7564307b652d0ef5b4bed9a3a350a2f57f6c1d8997830f1"
   end
 
   depends_on "go" => [:build, :test]
