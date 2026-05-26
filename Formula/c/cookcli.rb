7f211e951c564accf400a62d490e9d400a00f06e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 15:08:45 +0800

    cookcli: update 0.30.0 bottle.

--- Formula/c/cookcli.rb
+++ Formula/c/cookcli.rb
@@ -7,12 +7,7 @@ class Cookcli < Formula
   head "https://github.com/cooklang/cookcli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0269f50e5b7ca22272dc340a05bc2e601d76447deff7497904ab6644ffc6be3b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8135cd1eb9bb656139279e2ee54eee6bfe07c6a800ea9e90aa64eb3013a0f5f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6b491ea1202807f52a385115c1266f7f5c59b080c16d361d07b64788e9e6e967"
-    sha256 cellar: :any_skip_relocation, sonoma:        "71215553f9fd4adbc704f0130c9983505d2dd2ca8fe10bc733827c23b2e89a6c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2273908070259c1ab3b4dbe7511ea090bf5ee76d5f994921c063a7400a789adf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b989d88f6908530b7e135b46f0aeaf2d6149aca05df54e6fbfbd444b1bc1904"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2e4012c22ac4f3ef3dc7483947d2ffd60abd4ca4267d3eae5e3dc29dff2bc299"
   end
 
   depends_on "node" => :build
