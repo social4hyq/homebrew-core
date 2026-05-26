d3c4530007f718c06a254f849f4aead2e44cb771
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:09:41 +0800

    highs: update 1.14.0 bottle.

--- Formula/h/highs.rb
+++ Formula/h/highs.rb
@@ -15,12 +15,7 @@ class Highs < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5cef4f4d3548edb590d6bc8fdb2137131cc420c5def0b6a7a0569b951f399eb8"
-    sha256 cellar: :any,                 arm64_sequoia: "d26d1f6efe554d8fcefe6f88dbdb2e976dc9bf3e5f6fd3886ec9bc925a5cacbb"
-    sha256 cellar: :any,                 arm64_sonoma:  "4c6b8d197d4b6e20669a09bf7d7fdb0aa3c4ab288147b006a0f9840addf7b5fb"
-    sha256 cellar: :any,                 sonoma:        "98a9ace95fb8bef7337173bab209953c013b1a79dc8ef28e9b3cc156fb067e70"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "83217af7a3f47e9bfbb82a36fe7c87abad407c2b5a991d2945be13c93035baa5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "018bd3b27f5d43f0c80bc37bd9a0363d3a6e066970071bb7150ad519e3687b88"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ab6dcce1e0c9b9db13776ed208c4f00521ca3df8a453f542630ce28efc2e354b"
   end
 
   depends_on "cmake" => :build
