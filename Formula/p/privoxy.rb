9c9e7e0d0ac580d768a306bf77bd3ea3ca40aa26
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:35:44 +0800

    privoxy: update 4.1.0 bottle.

--- Formula/p/privoxy.rb
+++ Formula/p/privoxy.rb
@@ -11,13 +11,7 @@ class Privoxy < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "c0ae969ac878ba8034f22bc56240e5994bd9a042116fc7e7d23fa788fe1e484e"
-    sha256 cellar: :any,                 arm64_sequoia: "82c2364535cd0c530fd02bf4082021445c4fe23b68fa2cb509ff801a37f6ef43"
-    sha256 cellar: :any,                 arm64_sonoma:  "c480bbb71e9cdac494396bd35a155410a2970989c7e1557f9b4727e262b4d052"
-    sha256 cellar: :any,                 sonoma:        "65fe313f31698cef56c5537b4edc8b475ea94b23cbd46e8e598e02d2644a4dd8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "38b56a0785bf29774bfe49fc96a7746b5b1434075aeef27f089fdd9ed47b2a95"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "71ec0d7dd8b9eaa5d3729dfda746c25ae91eba7dfc3d28ef35861778b3dbf003"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "abfda0f60e9a63216f0c2c13b5a8e8ada2a9629c16499fc45b2d6dd792492931"
   end
 
   depends_on "autoconf" => :build
