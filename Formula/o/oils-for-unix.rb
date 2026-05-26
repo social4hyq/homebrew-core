709b2f6d2f3ed5d27036783c16a467ebb0f41481
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 15:40:43 +0800

    oils-for-unix: update 0.37.0 bottle.

--- Formula/o/oils-for-unix.rb
+++ Formula/o/oils-for-unix.rb
@@ -11,12 +11,7 @@ class OilsForUnix < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "78ad773133116d4f0552f7aee472eadaeb746242c18709af21f1174b56dbfe40"
-    sha256 cellar: :any,                 arm64_sequoia: "9376c438f4cc76b588affa35c61f2be91729ff34ff213afdaf27e091ccebfd7f"
-    sha256 cellar: :any,                 arm64_sonoma:  "1211e0bcdd6b6ccf0e16cfb310b40295f496e131374c390184aafef8c88bef48"
-    sha256 cellar: :any,                 sonoma:        "21baa2abb4dead1655801f44a1ec764ba37c3dbc99ff0e3489affb94b711065d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "96888e840150140c0101352d2f6aa8ba526b48cec35564555255edc65e36c57b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b779dc36db8448f09582af439acb8b41e2d712dbb7ebbbc340526cb7c3f9131"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "908edcdde4854bc2dee47a9b320e26596b4a5e0554546f72b5b473fd857eac55"
   end
 
   depends_on "readline"
