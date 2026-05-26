1486349d937341847d43bc3ee3ff360197882933
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 23:20:29 +0800

    havn: update 0.3.7 bottle.

--- Formula/h/havn.rb
+++ Formula/h/havn.rb
@@ -6,12 +6,7 @@ class Havn < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "80effddbf9cd0ab82bc48db616c239b4ac183f3434ced0a661c144c31028745f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "695911f9beda78d855ac1b704e3913cdd3ab9e9adb531e9b85b3f649f6483d9d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8f7b71c3a026e27cd55550e273a308f269e132920e7a087dc77281a037959ca0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "197791ac62027f052b0ba02a15be889290d52be47d6e75e7373873bed8c095fa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "092c984b553e08ffa39211865d0ccde1ce86ed41bf5d2eda558fe661bd311abb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d71e486efa3ffee2132834356f5f9a4f1beaeab91e165bc9341bed86e7993858"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "19db3c3737a1958f4768c5872f518a2739967e298ecef12109bcc0e1c315749b"
   end
 
   depends_on "rust" => :build
