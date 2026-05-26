21dc3ffffae8888c531c8495b71d4280b9d9a258
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 15:22:06 +0800

    phpantom-lsp: update 0.8.0 bottle.

--- Formula/p/phpantom-lsp.rb
+++ Formula/p/phpantom-lsp.rb
@@ -6,12 +6,7 @@ class PhpantomLsp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d6a3dfcbf59e11ef11f0c156be9e48f0f0ca8ac96417ddee7a35bc2aa72fbfb8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5452e27af9eb12abef381120648b2e10788b00e4043bb1cf3d81b42021254192"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8993d849576697186291b433497e8428589a30925fde1b6ac2469a80c1b2f61a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "dbd36f3d201028b6cbfc3d74441cf249630f05c3900635e1aff0027ffd982a87"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b993c4b8399070f0e65ff717294392aa537f028f671345c2454e1328c5321f1e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1cad2ac759d6b9b969c7b6f173c1ff80c79da62105f6f33ebbbfe15a9a82c4d2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "53d0da55411c781e87887220ed2998926b85e6f6a22d99a27c66406b4c46414b"
   end
 
   depends_on "rust" => :build
