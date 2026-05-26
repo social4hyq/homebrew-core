35489716c4a354e34815ae820a96f648c8450657
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:10:24 +0800

    vultr: update 3.10.0 bottle.

--- Formula/v/vultr.rb
+++ Formula/v/vultr.rb
@@ -7,12 +7,7 @@ class Vultr < Formula
   head "https://github.com/vultr/vultr-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bb667b796bf993370f6b80bfebbae08d066690c01146058916f95d97f584e40c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f2915a7cc09f41ca33ca13795a8822494b9323137583486cf8f3af70c8398cbd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c302a8cda670f6b3dba3c5b0640165895da997dc83a3d9457cef9c5f5df83585"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bd3bf88a15aa01f5d5d710deed2950bdcfe46ef88c2c17298727399a8daad4bb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e4dc6ed485efcd40a9c20ea058c29bcc97a36e5afdf2b9219032216a2288e363"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2056e0b7edc603c871ce11b932c8386f3e01ef86536e88c03371db27b2be69b0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "73422b530f7354091379fe11e1196cfcc4cd678fc857b9c3670707453f6efc33"
   end
 
   depends_on "go" => :build
