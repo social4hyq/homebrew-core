6a55df7fffb5555d89277aebda715239386e1b9e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:05:24 +0800

    mklittlefs: update 4.1.0 bottle.

--- Formula/m/mklittlefs.rb
+++ Formula/m/mklittlefs.rb
@@ -6,14 +6,7 @@ class Mklittlefs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ebd5f7c2ba836785f2a85f02288e6b3cefdcac40f2e0c4086a291ec1f9b06e67"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cbb829ab8e9c3c40a518155aabc2d05808689225bb7177e31ee3ad2b80a2bc7e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2afd18134810381f7cc7071cc2d57742e3f0903ac0043bfb1d87d54fdc673567"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "48862fcd19df8ee2370233baa3ea48a92c27ad25dc5a3f8fc8c0aa404b4cf81d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8c49d44f1d0ccd0ae7597dadf8a3c40e932cc5d775135d450fdb8df8a6d173c3"
-    sha256 cellar: :any_skip_relocation, ventura:       "69a5cc92966e36ce10c658fc2b5e1e1d7f8e99a2cb727e5b2bc7eaa9f59ef710"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "718c5352d9f335d60515c74268ebbc2cbbb879a82e4063893418fedbf9601a42"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f08d3944b06a3190264af01ab13388745ebd1783419c4637cf248d1590f886a8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "151a9d3d93e5acc9439bf8ec304f723a351a17de0eac3e815b1674407f95492e"
   end
 
   def install
