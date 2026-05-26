7fda8bd01e126ce5f28b57ca51219345e57723cc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 21:02:09 +0800

    gama: update 1.2.1 bottle.

--- Formula/g/gama.rb
+++ Formula/g/gama.rb
@@ -7,14 +7,7 @@ class Gama < Formula
   head "https://github.com/termkit/gama.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "733b2137013ca3a99db81a60d928e604309223613e98bdccd86e6c9368a7724f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9078cd53e0763f843729a0ffda3321354a6b664ffb2bed87e372e700cd9faef1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9078cd53e0763f843729a0ffda3321354a6b664ffb2bed87e372e700cd9faef1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "9078cd53e0763f843729a0ffda3321354a6b664ffb2bed87e372e700cd9faef1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0c8a11688f5d7cc46cfb8bdcfab4ec50a22bd17447ca4d274942270e2c671ed5"
-    sha256 cellar: :any_skip_relocation, ventura:       "0c8a11688f5d7cc46cfb8bdcfab4ec50a22bd17447ca4d274942270e2c671ed5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "14ca6d3872fc83bcc8846dee1548dbfbb630df0d474f651a82f97e6798dcd537"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "104456f00e51024a56c6e94f32e471b1288925f7cbe4a568fe73f360379566dd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1d8cebb403889dfd5dea928552fe4524d7eb143dd3fa0174bf0a6eeedb836917"
   end
 
   depends_on "go" => :build
