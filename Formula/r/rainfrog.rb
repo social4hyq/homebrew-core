2fa3c3cf0584f8b1497abf363998c9ee0caaa79a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:22:29 +0800

    rainfrog: update 0.3.18 bottle.

--- Formula/r/rainfrog.rb
+++ Formula/r/rainfrog.rb
@@ -7,12 +7,7 @@ class Rainfrog < Formula
   head "https://github.com/achristmascarl/rainfrog.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b8e5b7bc7e1ed2687faf4e47d8736a56f9ee109ef66ef7ef1ff785d02f44e17c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8deb9745d8cefc698cc5328432cf291702eab75c9384bf31db3448d867bc60bf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2d14ae46cea9e8529a86b581e08bc9bd522716ef42d246f03c9631614771edf1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8f624d48f4c911d48f4f13619070669aaf7a7436f853430cfbe74ee06c9c002a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e72811170fa1e263f14500ece775f807cb23448c0b2be3b32a9d8d1332640c44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b93372b8eddd5f8bddd2f59fed8d61867e6fd678fa2ca44dd9c52d9e1e6967a9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9f83cc9c2c6d46c49ef2550bcf8cab8a40c757c12cfbf3757c636d66822fa2c7"
   end
 
   depends_on "rust" => :build
