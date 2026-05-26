6142ff3417db3ca42d848a4a60eb81399d5747de
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 21:04:43 +0800

    deck: update 1.23.1 bottle.

--- Formula/d/deck.rb
+++ Formula/d/deck.rb
@@ -7,12 +7,7 @@ class Deck < Formula
   head "https://github.com/k1LoW/deck.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ba6ce0cd4ae42207058a30c5a8273350f1f431938ab139a3313b0da36b9b9ec4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba6ce0cd4ae42207058a30c5a8273350f1f431938ab139a3313b0da36b9b9ec4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ba6ce0cd4ae42207058a30c5a8273350f1f431938ab139a3313b0da36b9b9ec4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "835666444a10444c0b31210aafefd17ba7c5cd3467a3f1bd13bae4baa5c5a1fb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6e56ccfe7540f9fe2ce7496c630a36ece3255c56eded5e9270ef5c9e272002ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89c9ad69f26cd141939672e0b9ea8e89c5914e7ca90cacadf48b989712193b09"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "945c2dd45ca25f7a8554e89cc9ba05bc17f9ccd9c7b3ae61a163f8465f795046"
   end
 
   depends_on "go" => :build
