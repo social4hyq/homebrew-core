05918a4b428002d9ea390ffe3f2fce49577afa90
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 18:06:50 +0800

    kool: update 3.6.0 bottle.

--- Formula/k/kool.rb
+++ Formula/k/kool.rb
@@ -7,12 +7,7 @@ class Kool < Formula
   head "https://github.com/kool-dev/kool.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d91f2e9d8b46f95ee9782233b7e182434a5ac77cc84d21c8fc2d97dcdcf4f703"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d91f2e9d8b46f95ee9782233b7e182434a5ac77cc84d21c8fc2d97dcdcf4f703"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d91f2e9d8b46f95ee9782233b7e182434a5ac77cc84d21c8fc2d97dcdcf4f703"
-    sha256 cellar: :any_skip_relocation, sonoma:        "31a23b93af595df38fa5e8b4da05f08cb28cd65fc45eb5258bd3586bfa867779"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "449f0b84102e6b11f9a8a6b51f0e89e75ab0d96e9b2f92c2f48ef927f1290a59"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31693d6c71d67b9200013d12128632e0fd07550487bb427f4f6889b06416a6d1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a2374d1f2cd9b3f3513f95ba21258c54a0382ecfc95d81b01fd51503fbece3e4"
   end
 
   depends_on "go" => :build
