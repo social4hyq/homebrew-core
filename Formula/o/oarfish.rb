3daa1d6e0030e3cef88196339522af0c1bdfea16
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:35:59 +0800

    oarfish: update 0.9.4 bottle.

--- Formula/o/oarfish.rb
+++ Formula/o/oarfish.rb
@@ -6,12 +6,7 @@ class Oarfish < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12b890e8f49a81c2187ada86430d011e2c82dce7845fed0b1939f1a47c2a3b06"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f6a2879736b570eb10a1573db41b94b5dc89971d26b129e70500fac42588e950"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14fe4b957e0505a07d7226cf75fd3926d2f0da368be0ba2ebce28c2c6bcff1db"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5853e72f7739fce7c0db125d2ad1c0753e59bf3a06ee50e8587b954cc0d39245"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "87b8dca3f609adb67d5a50bce43ca50e3fd5431c1b0b23a5333e36aab77c94b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c39a1ef9df8e29ecd014dca0df060ba3c196dcf82b6f525aa051284e8f661338"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3ffd23eb069dd218d9ee67d138fbff220c611e81ce27d32ca5786c0dd2f54791"
   end
 
   depends_on "rust" => :build
