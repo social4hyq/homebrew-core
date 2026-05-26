9aa66636ee6b4c665cb164a6cb210cbac41a3a1f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 20:46:58 +0800

    bagel: update 0.7.0 bottle.

--- Formula/b/bagel.rb
+++ Formula/b/bagel.rb
@@ -7,12 +7,7 @@ class Bagel < Formula
   head "https://github.com/boostsecurityio/bagel.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "22b44df0b60070e7b15fbbf00a5bc6aea08eed50cbf3c0cb03cfe8f799b8b53a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "22b44df0b60070e7b15fbbf00a5bc6aea08eed50cbf3c0cb03cfe8f799b8b53a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "22b44df0b60070e7b15fbbf00a5bc6aea08eed50cbf3c0cb03cfe8f799b8b53a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "767be387ffd0275451a0948a26082a9e59fd50a77549e7b4f01fc278125f26c2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "20621b1c3778d0488f9b9b3d255da9bf5bcf2b696ea7e46b9dead572d451b9f7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8ddeefee9b398829ef5b4dca00b152e660ea20a819ffe917eb47f22a53ae099"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "400364c61edfac74d6c536a9c5d7aa12ecb9a28f014e41ffad3bef895b6d5782"
   end
 
   depends_on "go" => :build
