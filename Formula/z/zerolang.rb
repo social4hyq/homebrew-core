f3d0c96560fe1269c7652ae6db719e4477457f7f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 23:16:20 +0800

    zerolang: update 0.1.4 bottle.

--- Formula/z/zerolang.rb
+++ Formula/z/zerolang.rb
@@ -7,12 +7,7 @@ class Zerolang < Formula
   head "https://github.com/vercel-labs/zero.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ead31a2770f9527b2a96481967f9b0fad98d9f3bcfa8e2032e50da544a8138c1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ad6ed42db5f41b625fec531c2edc15f4558a66c9e488bff5cedcdd5545940ffb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "947af0b6d5c3241a362ad4a2ee5c6c8c55cb80b260564ea3087d61b356ecf47e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ed63f585ddf9869d5e7e38ec2d1aef4c4ae3b975f95719a214bca0c795c84ea8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "349d119faecc11a9efbd1297298ff82abe45a06d5f3f80a2f0dc4fefbc26eb81"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f5addb4c2b863e1a78354b25437d26551a7e2ff9fcb5ab52ea3e80142ec4ebaf"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c05912bba0d8d72e55e5324f0b2a4f517f82c88b574ea3fb8f780038c1b74056"
   end
 
   def install
