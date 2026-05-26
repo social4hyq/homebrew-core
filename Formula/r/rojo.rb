6c9ea0304abdfb95c6fba73a4fe1ed4a835bd44c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 19:43:05 +0800

    rojo: update 7.6.1 bottle.

--- Formula/r/rojo.rb
+++ Formula/r/rojo.rb
@@ -9,12 +9,7 @@ class Rojo < Formula
   head "https://github.com/rojo-rbx/rojo.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7342fb03360adf9da87f85e2f7e0a4d9165ed15fba501a5d570e417ea8131929"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf6599feb5a14753afc072b730d5f13f288b7e85820b172c8086aca04ec8e06a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b06bacf8121e829628f38ef50ebe51088cb8c84cadb6b2662dc57faeb8d77042"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1dd5086711d202388aefd2c5c0bfa918474440258ca4d5f7bca95ec74cb2c99d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b89b0bbd30ab66f4bf75373ca1fd2334a19805b89a027282014a46bb7e90cbe0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cff0fe4bb57f025548214ff6227c8973ac5308a2414fde120542a0bb5332e7b7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9f12d10abf388557d5fac8aeeb90ee50f9880b0cabeb32e1856d401e73c4a368"
   end
 
   depends_on "pkgconf" => :build
