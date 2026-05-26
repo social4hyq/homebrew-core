8686b29410423c0e4f5c12432eafd231707a7fc9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:27:00 +0800

    msieve: update 1.53 bottle.

--- Formula/m/msieve.rb
+++ Formula/m/msieve.rb
@@ -13,13 +13,7 @@ class Msieve < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "7e5d447bbc245a2b91d8993937c87c8e559678ebd68f972905d6c11e0d2b29bf"
-    sha256 cellar: :any,                 arm64_sequoia: "73413cbe23c21e464017e6abe5b7b10bf2e3e384e2e23f51dceef0be7bddc051"
-    sha256 cellar: :any,                 arm64_sonoma:  "b35f5ce318727e40a0c2d540707475a883853effa8ea4c5214dec80f776b15ed"
-    sha256 cellar: :any,                 sonoma:        "ca06f592fd7bc5ab600f422c05facec833296457fc5b9f511d80eae85b9537f3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5f469949e04536f662b6f3d7f8a4afdded9af65d7f4feefcb7fd9670dd92f3e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00366a1231d99d1ecb28d24e714467b1a5f4437504934d882fcb64fc35338ee5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6f5dd8645a42a2947ddcbc3c5e0ad67ce8c6c847cf538f757694b1d272f85994"
   end
 
   depends_on "gmp"
