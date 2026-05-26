16ab9ccdf0686e66421758c261dc4e558c05c5af
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 22:01:44 +0800

    jsdoc3: update 4.0.5 bottle.

--- Formula/j/jsdoc3.rb
+++ Formula/j/jsdoc3.rb
@@ -6,7 +6,7 @@ class Jsdoc3 < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8d86f3f9bc538cb7fa6038821e62cf8aa0bfff3a057799f3aab176894a6c8b21"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1953b3aac0f0fb0c7d0afa22b68cbee99bbed14aaa74056cc666a83c514a3a0c"
   end
 
   depends_on "node"
