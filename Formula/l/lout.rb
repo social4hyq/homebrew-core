71b9fd786c0517a2bf3369e162e3f2ebc58595cd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 23:28:21 +0800

    lout: update 3.43.4 bottle.

--- Formula/l/lout.rb
+++ Formula/l/lout.rb
@@ -6,12 +6,7 @@ class Lout < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_tahoe:   "b8d0ee5097c542dd495701a5b383a6cabe1e08cc49ce2144c9e20c4ed372590d"
-    sha256 arm64_sequoia: "4bc74cb1822bfcb333c7741cfe52876c2ef3bd46e1fff4ce2d43fef63df5df60"
-    sha256 arm64_sonoma:  "48458dd5194f830f27ad18b48900c733ffdcfb90343fe110c9deb6a6d439c2a5"
-    sha256 sonoma:        "96518ac2aa54a68e3c4286a4b3e94f77ec4c1c4502466d4e2f6e8e10a0b0306b"
-    sha256 arm64_linux:   "bd5da0dd25b268c804630d677692757518472af004fd5e412b648ec03e4674a1"
-    sha256 x86_64_linux:  "3615a978686e8e6f48f041c6b7e8190d669b89978b809ee3b3ac82a233609dbb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d153f924cb75328f880ab383724bac6a792cae80114f41eb36cf7d97dc3011e2"
   end
 
   def install
