fa348bdd7df8a45f8473f769f71349ca634ff99e
hqzing <hqzing@outlook.com>
2026-05-26 21:40:49 +0000

    kubevious 1.0.64 (new formula)

---
+++ Formula/k/kubevious.rb
@@ -0,0 +1,103 @@
+class Kubevious < Formula
+  desc "Detects and prevents Kubernetes misconfigurations and violations"
+  homepage "https://github.com/kubevious/cli"
+  url "https://registry.npmjs.org/kubevious/-/kubevious-1.0.64.tgz"
+  sha256 "f67bb559bd0aee165186307e678cf03141ade8f8b79127bd10be0f47d85d0348"
+  license "Apache-2.0"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "0a90f43ca3f6dc2ce0eff8244708ad484bf9a6860f949a04fb124a0536f41b72"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s,
+      shell_output("#{bin}/kubevious --version")
+
+    (testpath/"deployment.yml").write <<~YAML
+      apiVersion: apps/v1
+      kind: Deployment
+      metadata:
+        name: nginx
+      spec:
+        selector:
+          matchLabels:
+            app: nginx
+        replicas: 1
+        template:
+          metadata:
+            labels:
+              app: nginx
+          spec:
+            containers:
+            - name: nginx
+              image: nginx:1.14.2
+              ports:
+              - containerPort: 80
+    YAML
+
+    assert_match "Lint Succeeded",
+      shell_output("#{bin}/kubevious lint #{testpath}/deployment.yml")
+
+    (testpath/"bad-deployment.yml").write <<~YAML
+      apiVersion: apps/v1
+      kind: BadDeployment
+      metadata:
+        name: nginx
+      spec:
+        selector:
+          matchLabels:
+            app: nginx
+        replicas: 1
+        template:
+          metadata:
+            labels:
+              app: nginx
+          spec:
+            containers:
+            - name: nginx
+              image: nginx:1.14.2
+              ports:
+              - containerPort: 80
+    YAML
+
+    assert_match "Lint Failed",
+      shell_output("#{bin}/kubevious lint #{testpath}/bad-deployment.yml", 100)
+
+    assert_match "Guard Succeeded",
+      shell_output("#{bin}/kubevious guard #{testpath}/deployment.yml")
+
+    assert_match "Guard Failed",
+      shell_output("#{bin}/kubevious guard #{testpath}/bad-deployment.yml", 100)
+
+    (testpath/"service.yml").write <<~YAML
+      apiVersion: v1
+      kind: Service
+      metadata:
+        labels:
+          app: nginx
+        name: nginx
+      spec:
+        type: ClusterIP
+        ports:
+        - name: http
+          port: 80
+          targetPort: 8080
+        selector:
+          app: nginx
+    YAML
+
+    assert_match "Guard Failed",
+      shell_output("#{bin}/kubevious guard #{testpath}/service.yml", 100)
+
+    assert_match "Guard Succeeded",
+      shell_output("#{bin}/kubevious guard #{testpath}/service.yml #{testpath}/deployment.yml")
+  end
+end
