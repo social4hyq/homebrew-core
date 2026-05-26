55d749c5d5537445896ac9b809530b3633169dd9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 21:40:06 +0800

    nmstatectl: update 2.2.60 bottle.

--- Formula/n/nmstatectl.rb
+++ Formula/n/nmstatectl.rb
@@ -12,12 +12,7 @@ class Nmstatectl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a8f879302d3a276b42f433bd6754d14280b779020757918e13387bba386b5949"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dd4994135f9e26a7a4f168e3a5318bf86dc34978f77723d2ef183cbd5df2805b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6eb93f7b7809622eb65fd3d1c0091cd4097d10408c552068c49bb93a49d38406"
-    sha256 cellar: :any_skip_relocation, sonoma:        "556c1b0b49cf92bc858f011876947d63065329646d094b65b548a746a6a3ada4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3c39c5fadc2e7e4513fa2a5e7873a4de8fb24d3a99976ec957cc4fe3c048b8a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "73b48651f675cdbda8d9fa8f921035561df1d21e09e7a71d9b2d656578664af5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a3772a005043ffda108cd26e2d30c0463efd8aac90e512de5265c48f420c0255"
   end
 
   depends_on "rust" => :build
