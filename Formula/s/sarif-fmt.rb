a558e7da32165ce2039e80843473ff2f7298cc4c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 14:33:21 +0800

    sarif-fmt: update 0.8.0 bottle.

--- Formula/s/sarif-fmt.rb
+++ Formula/s/sarif-fmt.rb
@@ -12,12 +12,7 @@ class SarifFmt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "082dcb6c6de6d371b362f9e59bdcc672f211be143b81bb5a59e4bae020eba4cc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "edc1f6bff6d9bf0921ce1480207c7a5a66cf35fd694565338d463a33c9e138f3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "768c9041d5073662247a5ef5301484ec0dcf1063a95a5f96aa5e9d586f0199e0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9aeab703f9e6dddcda32538295d1b0fc9c0c7813b0e789a9ef72ad1eb0fc0460"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9a0f052f8cd78fc71c33d70546dec05a22df96596d51688b5bf4d587474b571a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa79b3709106c0654578c1399165788b15c10e9fa97ea626355367ae7c343a3d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "affdf789631b355279c9456a4b19ea168b99bc61da09081888a082eba9a6c938"
   end
 
   depends_on "rust" => :build
