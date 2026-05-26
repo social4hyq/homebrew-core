970820ff5b1efd541e1c574d798a79fe25c91086
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 18:44:54 +0800

    ast-grep: update 0.42.3 bottle.

--- Formula/a/ast-grep.rb
+++ Formula/a/ast-grep.rb
@@ -7,12 +7,7 @@ class AstGrep < Formula
   head "https://github.com/ast-grep/ast-grep.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5d0fe480ee1fb474b7eeb2bb62fb04a839de4384d613f5116a4428ee81c160b8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "049a2cefb3568ef7c1727bd8d2de03b87619d4fd9f836420bea03dc25a07282d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d0196fae8b95e4f1efdd1bac4f53cce311ef4c242c750138114db405de97208"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0cbbca27ad6966a734644f9e7b5328436fb8be7a4eed38a11018eefc6c6fc093"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "285610c3aea721472cccaab9b68adc340b15bb72aef0177481994fd6b71ba6a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e36514fcd295a97c5a3ff223a39f873aa847710ca00c82cf052a7b82e6cdda42"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0a63c22fa136f70cb8defa0c2d08383c3b9627bfdc3ddda4dfc2698df7b08587"
   end
 
   depends_on "rust" => :build
