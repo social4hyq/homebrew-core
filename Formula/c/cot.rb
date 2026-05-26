d9f18a43b0e79a7eafceaa56e40768057e512752
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-26 23:14:08 +0800

    cot: update 0.6.0 bottle.

--- Formula/c/cot.rb
+++ Formula/c/cot.rb
@@ -6,12 +6,7 @@ class Cot < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "454d155e10b009831d5014f79f25db39a0cb49f42c290ae531283a08d6dab395"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "39d5e27f1c722bb31492aada8e6e8dda1f5103863750bfcd8e869517aedaa656"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "194f4bb4e1373580f4b0c8d5bd8a02b0cdcc7be750d2e2831ce53a997da9fc9f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ee3848a97acaa1726100b12d22f0f4cfef4965f36093b124d00f7c1eaf50f4a6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "05d0532eba3440e1f11ee348c9083c20818ce887c4b3e9b3b1dc674161933eaa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8951a89b95eeb21287693d27f179d40aec1bef8b4497742b01af87d1095c4b6b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3d1d24b0b12a661466e2124765a3a3ab04c4430816dbb7581328a54d5ea392a4"
   end
 
   depends_on "rust" => :build
