b33820aef679bb54c8fe15bb3f23b529d0407d19
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 03:22:21 +0800

    hoverfly: update 1.12.7 bottle.

--- Formula/h/hoverfly.rb
+++ Formula/h/hoverfly.rb
@@ -7,12 +7,7 @@ class Hoverfly < Formula
   head "https://github.com/SpectoLabs/hoverfly.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd5bc3e3c57f74d975f7548f4fc24b41e50be7da780d1f082c0dab4c7797b708"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd5bc3e3c57f74d975f7548f4fc24b41e50be7da780d1f082c0dab4c7797b708"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd5bc3e3c57f74d975f7548f4fc24b41e50be7da780d1f082c0dab4c7797b708"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5f339637ba22d182fc4401889ba276f3d66919a9b01f2445cf35e48a598645e0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c9223cca1f187847b8da3785d33f82a57df78867a0e60b783f91911fa5bd2ccc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "98432f2f7f1d1431c469124639018d303d0b9f91984968fb096842a2e213c458"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6577eded3e76217b54fec726bac95aefbbaa218fa9afced9d3a38c9e02b8a2ea"
   end
 
   depends_on "go" => :build
