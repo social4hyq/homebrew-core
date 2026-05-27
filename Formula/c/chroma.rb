adf245805f27fc84076640d88cb26baac1d3b683
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 04:01:21 +0800

    chroma: update 2.25.0 bottle.

--- Formula/c/chroma.rb
+++ Formula/c/chroma.rb
@@ -7,12 +7,7 @@ class Chroma < Formula
   head "https://github.com/alecthomas/chroma.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "40aaa2bac5464cc251c07592c361ff73ef22f768f0f0279df8e7b2000e8e418e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "40aaa2bac5464cc251c07592c361ff73ef22f768f0f0279df8e7b2000e8e418e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "40aaa2bac5464cc251c07592c361ff73ef22f768f0f0279df8e7b2000e8e418e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4b9a27cfc42f7ca5483eec6555deb5706a531aa8b227abecaa8943bec99fcc48"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3e9734eaa72c640e3140dc89d7d60f1610d61714e2cfd0431d484c96a1bab1e7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6bf42baa80131271f2579eb7fb724fdb4d0cc4493ef735c2d914e27a73699537"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8ad589cf64074928abf46f1212e944869117172e71f033c359664b4e42cc15dd"
   end
 
   depends_on "go" => :build
