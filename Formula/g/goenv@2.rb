9cfb70a16f60e9ef14aa2f2393189a82f9434284
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 03:33:00 +0800

    goenv@2: update 2.2.40 bottle.

--- Formula/g/goenv@2.rb
+++ Formula/g/goenv@2.rb
@@ -11,7 +11,7 @@ class GoenvAT2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5369ded9060184d95823f3ed663c1bca51d4a1ce5128c9430c96eec7b703256a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bf4b9c3339294ae8069e1a08b0db902c7b51bc57cbc110eecc9f505521e2af2a"
   end
 
   keg_only :versioned_formula
