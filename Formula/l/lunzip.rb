a2688d06a900fcacb0afeef38186505f1c057b82
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 04:02:23 +0800

    lunzip: update 1.16 bottle.

--- Formula/l/lunzip.rb
+++ Formula/l/lunzip.rb
@@ -11,12 +11,7 @@ class Lunzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1dc588e22b50b7d39507443df148d8a2ca055e31ed8e7d2a383b701f8006537b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68207de85c6fd73a7d36e1cf7994227769563cbe21eba7bc15209e0f89ff1750"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e52b1fe664ab2c763dd3e1a0ab348904e3d30ec35ee5aa89aa80b9109386380e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "aa8baefaf9f55a75e2e246bb53bacefa5a1a4676e3a7bae517679f66c8cf352c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ab42bf3c8abd80b7b029d15ac4e366aeaea557890b6f600311ced3ee6590177c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6121f21bb49d13bb7542984ba53b79ed66534bfcf31180b3da77fc4350e35ab1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "79a037ae7b4f3a50f5af28b1a35095112b626d144c680f837cb5bd1ba7271b09"
   end
 
   depends_on "lzip" => :test
