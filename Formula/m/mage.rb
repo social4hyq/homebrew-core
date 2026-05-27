b12f590b98e99c5d1d01e1dd44b3c5b7b03cae46
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 07:07:55 +0800

    mage: update 1.17.2 bottle.

--- Formula/m/mage.rb
+++ Formula/m/mage.rb
@@ -8,12 +8,7 @@ class Mage < Formula
   head "https://github.com/magefile/mage.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c4bd5e578716ac74dd900c27728e12cabea8c7b52d505d5aa6e542530357d128"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c4bd5e578716ac74dd900c27728e12cabea8c7b52d505d5aa6e542530357d128"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c4bd5e578716ac74dd900c27728e12cabea8c7b52d505d5aa6e542530357d128"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9e8357d038724c1ede8ef3b453ac4f88afad14cb47fc5e1392b9f1821b274493"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3f6e766bccf62f9bf72a9b75aa0fd79d3538c39b2377e8a8164e35c4db49a41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "43d22223bcf08c76baa8481e3f7e77d77e66980de21067b1e2a9a6e4f9114ccb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0e07ed6041cf82baf64e8ff74ae4330e39f8a9d0a8e2270d7199c76241b545bc"
   end
 
   depends_on "go"
