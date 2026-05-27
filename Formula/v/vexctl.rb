a28edc37eead201e69d43420ce6ab03563da9d0b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 11:08:37 +0800

    vexctl: update 0.4.1 bottle.

--- Formula/v/vexctl.rb
+++ Formula/v/vexctl.rb
@@ -6,13 +6,7 @@ class Vexctl < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f2c5b287cf8b296ef3e6d85b38212179eebea16f070bfce1baea6ac0d119e7cf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f2c5b287cf8b296ef3e6d85b38212179eebea16f070bfce1baea6ac0d119e7cf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f2c5b287cf8b296ef3e6d85b38212179eebea16f070bfce1baea6ac0d119e7cf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "dcae34a87491e8efef1aa942274facd1fc985f36e76cf6b9671e4b0d66738335"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1adeeacdaf6ad3a33abafc10856dc356bfab928a5a18de8cfa69a015ad18f7ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9053ff8fcb65e7f520118e1d2a02dd377a0a9951b5dc3f4a667e640e81eb919"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7ea876718c8f9165afb973180b35bc5c228727708b13b68fe1c69d15fe3a49c9"
   end
 
   depends_on "go" => :build
