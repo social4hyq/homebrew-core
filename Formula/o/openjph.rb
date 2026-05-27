c2083f3b627008f218ac67cba748f4f3578b6a0b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 01:28:00 +0800

    openjph: update 0.27.3 bottle.

--- Formula/o/openjph.rb
+++ Formula/o/openjph.rb
@@ -8,12 +8,7 @@ class Openjph < Formula
   head "https://github.com/aous72/OpenJPH.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "1017ceff9efe338afe41d3457605873b38a4f64051a67e79dc129d6178b4b5b9"
-    sha256 cellar: :any,                 arm64_sequoia: "ed56deb929a1d39ececb4f471e89c7043ea9a7f0f1e0aef8622f5217c559eae5"
-    sha256 cellar: :any,                 arm64_sonoma:  "88271d81447d931c1b6150fe372b8787e7524fd16e965b64658db8468ef638ec"
-    sha256 cellar: :any,                 sonoma:        "70728b71d49d15add086d75bfdcf0802dc39817bbfbb4e09501dd94476fe4c49"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bdf747bdc76a1151087b1c7efe1db18e1fd7e938e078333d23a52280fc6d992e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b9fa791196bd379fa3a8d0d1ee9c852096073895d57d2f10d05968ddd2f47cf"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c6e70f99b5ffe483869e96c3baf35b712bf1e41bbce52526459644352dee5b78"
   end
 
   depends_on "cmake" => :build
