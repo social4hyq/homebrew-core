42a237a632f0d4dff7424d4a5d457defd8d84970
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 07:53:35 +0800

    zola: update 0.22.1 bottle.

--- Formula/z/zola.rb
+++ Formula/z/zola.rb
@@ -6,12 +6,7 @@ class Zola < Formula
   license "EUPL-1.2"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "d0b4f6dcd6f93092e5d6b7c5523d4708542371a56983805abd7c63331ed1eac5"
-    sha256 cellar: :any,                 arm64_sequoia: "5ef5e79cc049d96c6ed8e203a104e66471aad1e61303d595ab3b2c5907033d2e"
-    sha256 cellar: :any,                 arm64_sonoma:  "ad449dec819db4ee40826148692be8392eaa8694cbba236646ccd9f292885070"
-    sha256 cellar: :any,                 sonoma:        "acdcc9fec950ac9c5a109cbedaf4dd57bd6a5aaaf1ace613f84a35db23740c2c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "85106de0bbb3f42628d011213df85a1ca705d785fe2dfa50bc0d7ad72d23ae23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd1481a6f76baf68adfac5b59d3df2db3027ec32cb0bd526dcd7583e41fe007e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9413aa73aa7274a9e33a6132f0330b990d736c7c337b57d6add1a7a96c198506"
   end
 
   depends_on "pkgconf" => :build
