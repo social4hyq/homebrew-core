8e0922d75b4968059d0be23c7a4a0c24166b09d4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 00:44:12 +0800

    ancient: update 2.3.0 bottle.

--- Formula/a/ancient.rb
+++ Formula/a/ancient.rb
@@ -6,12 +6,7 @@ class Ancient < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "6817d9ce2e9d71f8e176280373905983d18dc6984a8cb9fd8587a77189ed77a3"
-    sha256 cellar: :any,                 arm64_sequoia: "08b6b80e0b301194514d37f7bcbfbb9f3f6741a5f76d26d11238591d4095481a"
-    sha256 cellar: :any,                 arm64_sonoma:  "bff6bffe97748fcafa7153b84ab9f4e777140bcd61b283368e1700d059b7a248"
-    sha256 cellar: :any,                 sonoma:        "e7d17674bacf02c4c01a07812e801e662b76fd68de9ad82287bf1fd65c4e2e18"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b3c75d64ba336cfa2c79cff5db5f0f918f95448565e93685df2679d25b429aac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc2e89a54843d08f940bf5a541e85ad5ae0ece2fa2f5b146a2e22a792ced7a2e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "51dab0db0cc59130256f837a5cc0ffaf57ed8f11a80d5080d7fc3b11b42dc3c8"
   end
 
   depends_on "autoconf" => :build
