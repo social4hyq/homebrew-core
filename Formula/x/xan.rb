c09c6fd67b7f90ab3eabc1df9306634faccd4395
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 11:31:52 +0800

    xan: update 0.57.1 bottle.

--- Formula/x/xan.rb
+++ Formula/x/xan.rb
@@ -7,12 +7,7 @@ class Xan < Formula
   head "https://github.com/medialab/xan.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e11b13c4a68aa541191ed1b14a2787f2ae97590b9c33c45c80b5ebc66cfe678c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "77f00dc466a89f91b754ee1293d65e9d083f9964b536b3f7e0b407d2da409034"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "979e4efaed1cde3b0594f66556a84ac14832b5a5f3804aaf180a4582f3a7aa9f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f62b650e551663ddaf12e376261cd1e72c002dc72d2a5651c2dbf8246855c595"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4b6b097dbcf8a445687a135b3496df80574c0f0e2d7a5400500c68628aa54d14"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e470db632cb3c386dddbfd8627922efb8932cd1897ccde4201339e90419ea10"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "85e3a4be2f28d07efac271a2f453ba155e3a6d058d8af113d60cc0a10f574f8e"
   end
 
   depends_on "rust" => :build
