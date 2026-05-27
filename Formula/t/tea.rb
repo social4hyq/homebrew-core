8d39b8555537b1d35c64c8bd65979270d126cfed
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 12:31:58 +0800

    tea: update 0.14.1 bottle.

--- Formula/t/tea.rb
+++ Formula/t/tea.rb
@@ -7,12 +7,7 @@ class Tea < Formula
   head "https://gitea.com/gitea/tea.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8002beb5d9ee2061005984b90a21f7414292fe21d58b3e38ed4d6c3d35ef4304"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8002beb5d9ee2061005984b90a21f7414292fe21d58b3e38ed4d6c3d35ef4304"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8002beb5d9ee2061005984b90a21f7414292fe21d58b3e38ed4d6c3d35ef4304"
-    sha256 cellar: :any_skip_relocation, sonoma:        "343f8e211413ae14e71279aa52b91f1ab00c7001bb00e459ce2818e26ed136f8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "53d1a65bddb69f2f4e2d9a6a7f76f5df4db3533fadaef90416fbfd5e2ccafebf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f1b2c7a307df6a8549827cfee645eaa43e23b07582baa412592119b4248f00d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "62b390e76a32507418431619541446ca9e80b9b1c0279743a22f3033ecd1a154"
   end
 
   depends_on "go" => :build
