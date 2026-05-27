439b7c49a918141557bad90a09a8b7d9043c4842
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 01:42:57 +0800

    kraftkit: update 0.12.11 bottle.

--- Formula/k/kraftkit.rb
+++ Formula/k/kraftkit.rb
@@ -12,12 +12,7 @@ class Kraftkit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fa76637f084c45dce2a0c02389e8c9beb333587664f5e779d8cc16cee7c2f1c3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65f4f4f24b07ee417c8b96ab0b8e3712cbe78537e595ad898ea850c2d60868d8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c53916d5576bca5553a24fcc56a6b2d5a2249374bede73fd1485911b2ddd60f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "46bf368d7a9ff9381ba4b329377594334bda8ea90ef22313c4dc4f58ad33183e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e472f1bbbf79857f32c7893c4b815b6ee953d5b498075dcd5198c6f211f68bfe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25ae0c93927fdd73e40d0921b5b25b418d215b04fdb08a696c04a16de8d0fa99"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7b78da5e4f40deab9c6c8778b4851a338bde00d91aeb4bd5a7ed857961846249"
   end
 
   depends_on "go" => :build
