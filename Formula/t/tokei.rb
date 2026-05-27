29a5251fc6e42a88737b0969e813f36b08bb9d84
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 10:53:54 +0800

    tokei: update 14.0.0 bottle.

--- Formula/t/tokei.rb
+++ Formula/t/tokei.rb
@@ -12,12 +12,7 @@ class Tokei < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a4c00ab0590860aca31baefc4fc26bf12e31df2d767ff4f082fdbfdef062720c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "50338ef998ef5c9c3aaf8832185361def1793e7a0b7992022da0bbf9e40db791"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8c9bca298c011219d16cfec0bc5616441c50e561364b646303cbc3f54fd3708b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6e972212515cb41388ef0c41055a095d306f358f20c886c394443203a1a0ded4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "043a8dd76dbc8b13e862d725b30e1989f05ffcefdf1c2d6b770daacb99bdd9db"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "827d4ca8ee4f3691311a0c9d2bbd410391e789868de263fdaf5929f4045238ad"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "667e471f1c5f8cd7de617de7fd4697350c642b441eb7cfb4ccbc6d2d749488e9"
   end
 
   depends_on "rust" => :build
