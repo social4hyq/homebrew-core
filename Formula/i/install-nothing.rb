3478305a3d9032686b6f2177a7398e469c1fde6b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 16:11:56 +0800

    install-nothing: update 0.5.0 bottle.

--- Formula/i/install-nothing.rb
+++ Formula/i/install-nothing.rb
@@ -7,12 +7,7 @@ class InstallNothing < Formula
   head "https://github.com/buyukakyuz/install-nothing.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "87f77b50975349223e18f63a0d1de03cbaef86db5711dbef0d1c166708ae9226"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d032123d0201bb3e68ff634399e98770bef3ae3625fe28d5a2a7bdee13d221e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b086e4a01e79d85da4989e2cc748b637b9457fe324aec2252fe4a191a8e03c81"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e17602aa9e85136ddf3758f10012d34ba13e39baab9241d0e826f8e224a97acf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d24c153d233b51da3858f8cdd14faa7fd71bb1bc9cf8258f0f985905f51f9eac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93db2ef7a614641d659738c9b2b7c15978bc00695e8aa46b08351e6c8dfbc458"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "25347f49ec48ca780b7049c21e17f1e03976d3954f5b5890ae58c15d0e036e9f"
   end
 
   depends_on "rust" => :build
