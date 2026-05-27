d9310677dc5ef8bc5191ddbd98d8965a06de7695
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 11:09:33 +0800

    libxmp-lite: update 4.7.0 bottle.

--- Formula/lib/libxmp-lite.rb
+++ Formula/lib/libxmp-lite.rb
@@ -6,12 +6,7 @@ class LibxmpLite < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "201ea45ab0fd62dbebb197abfb1ff8489d75b62b98556eb55393e7ce46909938"
-    sha256 cellar: :any,                 arm64_sequoia: "3b0101130351878977713c358a85efc5aeba42ff6020c0937fcfab1ef370d325"
-    sha256 cellar: :any,                 arm64_sonoma:  "6eb64812621b57dec829e8c6a539c8d4b00d560588ae73d321f070ae772427ab"
-    sha256 cellar: :any,                 sonoma:        "cce627af00de3a26e520f4e359465d34ba615e0c184c30637627bc25ab7a6a1f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0dfa89164cbb60ee10d098a68f7677fd6d7b0f4e9c62827297d6e3250628d794"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e20f182fa8f91177086c114ceed9ee5fb0271ac8a0e1a5ea734e5f13a600893"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0cc11b9ffe6fba7c6eee09325bf3706afef3a5f5d2a75ecf050b29994d42612b"
   end
 
   def install
