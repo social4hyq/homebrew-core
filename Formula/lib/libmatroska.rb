b3b2d73a05ab37240e35b9efa75fd0ac53980753
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 07:37:46 +0800

    libmatroska: update 1.7.1 bottle.

--- Formula/lib/libmatroska.rb
+++ Formula/lib/libmatroska.rb
@@ -12,19 +12,7 @@ class Libmatroska < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "bf1a6c41880272d8a17b3c0b66bcafa1f2e250ca16f1cef4230e58cdd8ff3b01"
-    sha256 cellar: :any,                 arm64_sequoia:  "59abce74d2eef80b0ec6751dd6d9357c2c130d9d8d11dce0ef9d0f47fbe7007a"
-    sha256 cellar: :any,                 arm64_sonoma:   "297b69d493d6b09441e452745dee037ed4c211642a30ab4acfb3f229423e9995"
-    sha256 cellar: :any,                 arm64_ventura:  "029766b0222c5d9a72a3cc63410c18a1d6b485243bdb4430f42e515ab24e18dd"
-    sha256 cellar: :any,                 arm64_monterey: "7648ded88703290bc998629288b942f2ac26585c9945d1443d14fe454654e306"
-    sha256 cellar: :any,                 arm64_big_sur:  "bb9b3e6993c88b36acddbce97f6085f6785cd57812bece2b37fb56360054010a"
-    sha256 cellar: :any,                 sonoma:         "4abfa8d4c378d3d1a635d47c7520590fa33366bc8d2f8d5feb00fbe960d1c5b1"
-    sha256 cellar: :any,                 ventura:        "7e0cfe0a5bc3503bb309dce7ba7f78a75259daff686a387b9413c7db9580ed77"
-    sha256 cellar: :any,                 monterey:       "e2282bbaca89473b275731eee79be5a8ac1f5402c9603be4a0545c65b2d929d5"
-    sha256 cellar: :any,                 big_sur:        "1b3b6df53eb2070d742dec37fcf4f2ebf81728bfe1c64e82ac4a78bb58c80288"
-    sha256 cellar: :any,                 catalina:       "3a4ca07a150e0719bc8bacedced44c6cec1116e0050095e8c669d37a4d47eb6c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "1f6f5188ac26a332bf87653ef4627f5ed1220ac05aeed89f616bc5f9e4898fbb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61b54ecbcbef470960678043620106e649fb75f05ba3b26ef6d24aad0476a9fe"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2909e11245edb034da71a782d560a23fbd11c01b364a13dcd63258681386b0f0"
   end
 
   depends_on "cmake" => :build
