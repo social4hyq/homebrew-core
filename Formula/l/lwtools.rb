fbfbbbe35187e9949e10b1ebf9f53baf0f661c9b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 12:29:02 +0800

    lwtools: update 4.24 bottle.

--- Formula/l/lwtools.rb
+++ Formula/l/lwtools.rb
@@ -11,13 +11,7 @@ class Lwtools < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3f904db8779abd74708ebadd543361ae5f9ffee9a764cfe8ce3ff0e5db32d1f2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf4cd6f23617d013f4089741a4ff693a2d0c22b92b0d978a3a7c2b715382a0ce"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "433acd7c0e10e9a77f57dc2a96db7c0b90e0714c1537206d98dadf36bbcf7869"
-    sha256 cellar: :any_skip_relocation, sonoma:        "13ff219d341591d4612e649fff1f87e4978d91ca440f096d7189a9382abacfc2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d8140c9ed5b53b94b1abe2a57951fc16e57d01a962a554c9b3e69fbf4c4cc882"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "209fcd523569b4579aca0fffe15766ec01190f2e7e0426af08107ccf7971e166"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "758af3f5781943934d82835d487dc5571d1fe21a42309128764b182073bd9222"
   end
 
   def install
