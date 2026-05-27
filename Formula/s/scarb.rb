c699a5e21a330b7385b884edebf053c12f0b2929
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 05:05:15 +0800

    scarb: update 2.18.0 bottle.

--- Formula/s/scarb.rb
+++ Formula/s/scarb.rb
@@ -12,12 +12,7 @@ class Scarb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c73861def66d01bdd578b7ed8b634c2362e480f6ffdb0d2da23b00008d668805"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f7adf654ff015dec16fd76f62a3aca1274c2d36053e3f6e189e82eb69ac0d245"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c275505db4ef9c8fb1ddcc2d99b4de2ea3e0fe249b3b85f0631f3c01c08e698"
-    sha256 cellar: :any_skip_relocation, sonoma:        "35b30f39df715de4e675befd48b506c7ae4f1610e148f2eb242ff31067cc5bad"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "05489e481bdfe73a4b31fa73cf0bed87421b343d5a8c5b7dfe499a7d30688793"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bcc59824731f38034ed0a02e44b6a5758cd57d86a8344e3b0ae45e59df3f9b94"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "320c057dffcc792cf7250e9ad4d2f1a2d63253d8cb974f464609e235cf0c0fab"
   end
 
   depends_on "rust" => :build
