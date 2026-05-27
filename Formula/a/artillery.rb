ec2f2ea10ad7814c6b18c7c775d9bf6518dd6b84
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 07:33:57 +0800

    artillery: update 2.0.32 bottle.

--- Formula/a/artillery.rb
+++ Formula/a/artillery.rb
@@ -11,12 +11,7 @@ class Artillery < Formula
   end
 
   bottle do
-    sha256                               arm64_tahoe:   "dca938d95f9e90f08a7b46fdfc651eadffed6a3038c7609d7fdd5df0362c210c"
-    sha256                               arm64_sequoia: "f384a9b4438e5f56f895a19164a21b06de4a4e0c646ad009c9d67e1eb989ac01"
-    sha256                               arm64_sonoma:  "08243ae957d929a2c23891d547c325ced2757e28619c5b4325827d802904b61e"
-    sha256                               sonoma:        "b6774f2604d9357122529cde913bff2b17fd301aca1e935dbad3a53d310c8bcb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "06fbbc99eb0a7741adf2977fc0e926526767ccf36a8a0c83625f45162c7aa484"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e58158349ebb45b9b4d44fa09ca6331e355f841a7004532abfe30a1310555b6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a694fad3a3bebee3319c818f8bbe0839bab7425bdde2d8e83cb031f15ab800c4"
   end
 
   depends_on "node"
