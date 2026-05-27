2edb0e980e0341ad2c82cfaff558f0f4b6626d68
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 03:06:39 +0800

    cntb: update 1.6 bottle.

--- Formula/c/cntb.rb
+++ Formula/c/cntb.rb
@@ -7,12 +7,7 @@ class Cntb < Formula
   head "https://github.com/contabo/cntb.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aab7b4107c3bbf83ed83496409641d6e24042ede466bed096e77626bad3b9287"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aab7b4107c3bbf83ed83496409641d6e24042ede466bed096e77626bad3b9287"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aab7b4107c3bbf83ed83496409641d6e24042ede466bed096e77626bad3b9287"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6af27760b6a17b17718f8f9d3fc63ab635c5406a3aa6c64637b2dd1867e9d9ad"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "33757577830b8e59c9c23297b255c542140245fa2a388339a692c90b238c1b9e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "480eb7831df079625173f7bf3ac52a40808f6e93514be3771a4ae16a822ed411"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d01061a9b3623fa36d82ae4558316d7d583f80faec40d14ce3290e162b715e7c"
   end
 
   depends_on "go" => :build
