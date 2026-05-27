4f97a3c8fb6d145de8f7a48e849a35b90499ee57
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 12:06:40 +0800

    termshot: update 0.6.1 bottle.

--- Formula/t/termshot.rb
+++ Formula/t/termshot.rb
@@ -6,12 +6,7 @@ class Termshot < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e9e84923bf4300c54e04efee620db1bde1ea367dcc0f2dfafba0c46b92dbaa4a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e9e84923bf4300c54e04efee620db1bde1ea367dcc0f2dfafba0c46b92dbaa4a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9e84923bf4300c54e04efee620db1bde1ea367dcc0f2dfafba0c46b92dbaa4a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "788fe895144017fafd44bb3df8973655f6ce930f28f814e677a06d9c2fd76c99"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4e7a5aa03dfbc7a006bf75306ef8439d46d2f127437fee9498c0ab364234efe5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3d0eb5a80ad7724cc0f400cf54403ecd7a0fd7cb5a3ca11442465b3eb83352a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5ddfb6580acdb9e709ccccee420f23c8c0c13b8c1ae7bc3ff668545d55d81e14"
   end
 
   depends_on "go" => :build
