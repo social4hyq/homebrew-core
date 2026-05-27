d3ac209fedb4ba27c438598863e6112ab0e597f3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 07:30:27 +0800

    somo: update 1.3.3 bottle.

--- Formula/s/somo.rb
+++ Formula/s/somo.rb
@@ -6,12 +6,7 @@ class Somo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef01038be1ee8c5452f0fa68445d00ec96711d5d352d11da24132c162c0fa4a7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa162c6300f8ea7e9d7da424a2a4f42bbe495726c22c0a8de52df104425296c3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d428ae775005c5011bf7268e594c25feca21fe4e90b511878d4407e461c9596"
-    sha256 cellar: :any_skip_relocation, sonoma:        "417817d218be58597a62871b5f48edf2dfad9467d2348ba1c3416a2e130cf123"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cca798d71b1102e80716a71cdd186ad17f09ffb3d226ffc57085260ffaa0967e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eb10e51aed123acad3aabff2e6383d114bcb706ea5d17fb3d874df6394e6ab83"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5c3d2932acbe864a51a5bf666609be3c7ddfafb14d2ff0466812e7b1e1bda9dd"
   end
 
   depends_on "rust" => :build
