856355603be0a329081f06daa93853333c852cff
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 04:07:06 +0800

    aws-iam-authenticator: update 0.7.16 bottle.

--- Formula/a/aws-iam-authenticator.rb
+++ Formula/a/aws-iam-authenticator.rb
@@ -16,12 +16,7 @@ class AwsIamAuthenticator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7f31fe27f01f0af0a702258b92d2a966464c3019563b7ff856321e8e9680a426"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a2bb8ae5c533fd2da64d838ead2671335b031db19dc2c1c4b9d1ed4678b91b16"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b10afa1e6d20301b491ba3acab5cbcf8a6f93022de75e2dd1e3c2aa03932202"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0ff7560b21af8c1686f11d7c24e372b2fb9e792873f049fa5d4330ac57d8c8e3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "38957f3cc5d5c896b53ba26c2b07080c00d4968cddb0090699b489ba66833426"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f80c273837b2b69430b2ead27ee95c1f1877e5a3acd35f29e28ef15ba453a173"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "341fec00cd31941b69d5f626c7ac39fa3c0437a0e1c12be3c55b78f83abb1bba"
   end
 
   depends_on "go" => :build
