6af385f7b95af59cf1d1fd07ac1914d2a6628575
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 06:27:27 +0800

    xmlstarlet: update 1.6.1_1 bottle.

--- Formula/x/xmlstarlet.rb
+++ Formula/x/xmlstarlet.rb
@@ -7,12 +7,7 @@ class Xmlstarlet < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a8da4979558af0c5679c2cadfac4c5a0a9f851eccd6a3ef74e0475b1dae7edfb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0c422e59ff501221aafa5679bcb5c83703f54c83768c0a144fc80d997b1961d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "355f3500eef2193e7a18e4fa465dda10c686690cb3118d60c644b2f09e9076f7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "80f27cfee431b938bb3e4fadd7fc1a8dbe740ce1b00a053e6bee64441f7e5e23"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "04a42fc41c21c9c4fb1483128c6a6be0ffe23ac31f181904aeb68cd12fe4dc8f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5a94dccc66555dee58f477b358e98d91f5b9afff2c4385471ab19fb57c42e32"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "10e9da4069cbfe3bf1694055e31ee2379ee52ab2d532664e23b8d6c5f82c47bf"
   end
 
   uses_from_macos "libxml2"
