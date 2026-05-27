b4f0e1f884ea64ad8e9e4dd466d0bc7c6ab1381b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 00:20:09 +0800

    wolfmqtt: update 2.0.0 bottle.

--- Formula/w/wolfmqtt.rb
+++ Formula/w/wolfmqtt.rb
@@ -7,12 +7,7 @@ class Wolfmqtt < Formula
   head "https://github.com/wolfSSL/wolfMQTT.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "fff3eba4a8f27e7499aea5e907056a0b426a7fb37e4b6a42e6d973706afc6707"
-    sha256 cellar: :any,                 arm64_sequoia: "ea2baa494710479d90c4f5b53ba98f827f54ab77be899aac2b894a0d7361d601"
-    sha256 cellar: :any,                 arm64_sonoma:  "0547bb45ff40f0ad99677a9bb38227f07fa09216b1a169f856cf6fc6ac6d1d5b"
-    sha256 cellar: :any,                 sonoma:        "759b90e07d98503c6419d7bee4aeda2b4bf86b3fe62f30b592c67f7c6e76ad6a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c8e3ac7fc1f23a804746f1fd156b682603af2112efabb520548c937c909b09b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a6a06c4d763ac4a9191e805f4f14418fa151c85138f636991b7dca032a1d5a4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6bfaa989384a27bbaa9a9cd1bc30519c0f7cf43795132dc814180f5bb4405afb"
   end
 
   depends_on "autoconf" => :build
