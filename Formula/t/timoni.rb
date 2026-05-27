2fa09202cd76fb19b221fa25965d748195f02bf8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 01:22:11 +0800

    timoni: update 0.26.0 bottle.

--- Formula/t/timoni.rb
+++ Formula/t/timoni.rb
@@ -7,12 +7,7 @@ class Timoni < Formula
   head "https://github.com/stefanprodan/timoni.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "985eb65543c6d66660b51aa07720366d96b41552b1784b87f5f2fd40d06f76da"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acb0e994d2cdfbd6dd5e585b2023e32226c46d3c35d0356322a055db6ebd6f96"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "310f8d997f8b4d0c51ce67020c7711c0b2d4f1200ad8fbfe66f5953494dba727"
-    sha256 cellar: :any_skip_relocation, sonoma:        "881a71472b9b239c67395e56b63a4280d959ac6da38000b0175077be4027546c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "937ed7f4c671083e30e6e68a3ee3ed5915aa79a1e1830a7029375396d5c57f23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e987529e9b46b86f2094188368fd02daac82fb00728bdabb94e665c44b15e898"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "299d9395f43419467706fdb103e1690685fdd49430949de34769bdcee73bdd70"
   end
 
   depends_on "go" => :build
