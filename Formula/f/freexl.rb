1864e0f7b86a7cf2647c9ce7e52cdaf3ee17a8cf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 15:47:15 +0800

    freexl: update 2.0.0 bottle.

--- Formula/f/freexl.rb
+++ Formula/f/freexl.rb
@@ -11,18 +11,7 @@ class Freexl < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "c45940e1dd8bad980e3a6e16f2ee83636ea6b92ec106b352d27aa009abac96a0"
-    sha256 cellar: :any,                 arm64_sequoia:  "f1ad6599c594b856dcb08b7aafaddf394b0708924130fbeb53c38d0aba2215d7"
-    sha256 cellar: :any,                 arm64_sonoma:   "9472c48a14d17743af81465d7d8a9bb8f8063d07c50e953b6bc305cdbcfc8b49"
-    sha256 cellar: :any,                 arm64_ventura:  "1fbeb7ff7273a9e9a26eccbbc4d9943167645de04654f5ada57b399262a66eb9"
-    sha256 cellar: :any,                 arm64_monterey: "d5b5f5091fde8145fb4854df71e5a0d4c85064983f0ff50e8649b66e72459436"
-    sha256 cellar: :any,                 arm64_big_sur:  "65496307a6328795f5a4eaeac73e715d3b10852538476a37b141c69500db205b"
-    sha256 cellar: :any,                 sonoma:         "df6da8220ec7db83dd316ed8845036bb7ef3b89a89f76db6e258c457a390398b"
-    sha256 cellar: :any,                 ventura:        "deffaa3f557b73b8bfe491a641ed7bb0727bd8b8f6d81cbb8795f530e7db2624"
-    sha256 cellar: :any,                 monterey:       "3578de5c3c6d52a04ee32fad357d1c4f25ee62a8d2a05dbf21fbe5e5e3595620"
-    sha256 cellar: :any,                 big_sur:        "915b680af0a7f34c12f86630fe22ac48b479fc14e24df6a4fb2c9274b0a971d3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "26dd6ba610ed245687f64deae257c8aa2e21a9878a6430f442a0cbea4a894425"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a23aa6cd549e49b7d3d3f5bf160d97d1125c26587074580b2926060003269e9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "123301fb27ce337334181539a1204d030606588ed03195f9a731e045f8e9aadd"
   end
 
   depends_on "doxygen" => :build
