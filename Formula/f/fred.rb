bbd9a6a973938159fa38ba29cf90350c22415623
hqzing <hqzing@outlook.com>
2026-05-27 14:15:49 +0000

    fred 1.2.1 (new formula)

---
+++ Formula/f/fred.rb
@@ -0,0 +1,58 @@
+class Fred < Formula
+  include Language::Python::Virtualenv
+
+  desc "Fully featured FRED Command-line Interface & Python API wrapper"
+  homepage "https://fred.stlouisfed.org/docs/api/fred/"
+  url "https://files.pythonhosted.org/packages/81/97/25f2721387b30613436d80e0d82d6b2dea76f053139950e5b02d7786dccd/fred_py_api-1.2.1.tar.gz"
+  sha256 "04691b7571ffb424f0ce3a823f1917605efa59e9434c6a58f5e5f71ac134047d"
+  license "MIT"
+  revision 2
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "13d10773ef060d0d9b93826004a99f8f0ea398fdcf0e102417ee759fa7d92d15"
+  end
+
+  depends_on "certifi"
+  depends_on "python@3.14"
+
+  pypi_packages exclude_packages: "certifi"
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
+    sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/23/e4/796662cd90cf80e3a363c99db2b88e0e394b988a575f60a17e16440cd011/click-8.4.0.tar.gz"
+    sha256 "638f1338fe1235c8f4e008e4a8a254fb5c5fbdcbb40ece3c9142ebb78e792973"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/82/77/7b3966d0b9d1d31a36ddf1746926a11dface89a83409bf1483f0237aa758/idna-3.15.tar.gz"
+    sha256 "ca962446ea538f7092a95e057da437618e886f4d349216d2b1e294abfdb65fdc"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/ac/c3/e2a2b89f2d3e2179abd6d00ebd70bff6273f37fb3e0cc209f48b39d00cbf/requests-2.34.2.tar.gz"
+    sha256 "f288924cae4e29463698d6d60bc6a4da69c89185ad1e0bcc4104f584e960b9ed"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"fred", shell_parameter_format: :click)
+  end
+
+  test do
+    # assert output & ensure exit code is 2
+    # NOTE: this makes an API request to FRED with a purposely invalid API key
+    invalid_api_key = "sqwer1234asdfasdfqwer1234asdfsdf"
+    output = shell_output("#{bin}/fred --api-key #{invalid_api_key} categories get-category -i 15 2>&1", 2)
+    assert_match "Bad Request.  The value for variable api_key is not registered.", output
+  end
+end
