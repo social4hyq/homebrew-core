6a005d84fb3f4008ba98cfac7a74871cbe0bf056
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 06:49:06 +0800

    slsa-verifier: update 2.7.1 bottle.

--- Formula/s/slsa-verifier.rb
+++ Formula/s/slsa-verifier.rb
@@ -15,13 +15,7 @@ class SlsaVerifier < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "74a05f9a862de1e4b5ff0c5ec8a276684dddb282196ecb13010941960049274f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "74a05f9a862de1e4b5ff0c5ec8a276684dddb282196ecb13010941960049274f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "74a05f9a862de1e4b5ff0c5ec8a276684dddb282196ecb13010941960049274f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6af212bf84e8c9a121278554c084c8316cd360ecf3a29cbe0d9918b256e3f1dd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f211fb47549850522eb0187f98471eda17743dfc79c7572a6018aa282f141267"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e588593813873d0d0c768d1e85c38db2e6e1871cc567950fd7a2e053b4706841"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "11c75dfa3c637edaeac2bd5de224c5e3eb362c030c5114286d0f9a8b3f826002"
   end
 
   depends_on "go" => :build
