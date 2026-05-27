9608d4721c0efe32a8ea0a43939f078a700955ae
hqzing <hqzing@outlook.com>
2026-05-27 11:32:22 +0000

    groovysdk 5.0.6 (new formula)

---
+++ Formula/g/groovysdk.rb
@@ -0,0 +1,35 @@
+class Groovysdk < Formula
+  desc "SDK for Groovy: a Java-based scripting language"
+  homepage "https://www.groovy-lang.org/"
+  url "https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-sdk-5.0.6.zip"
+  sha256 "401f2b34d835838201d250cfaeecfe0cc48a668359154d1fbbfabb3ca75b7c1e"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://groovy.apache.org/download.html"
+    regex(/href=.*?apache-groovy-sdk[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "659850968a85f3935bdae36dbd466e3cec4955c2bd3c8da0a041f08c8e71157c"
+  end
+
+  depends_on "openjdk"
+
+  conflicts_with "groovy", because: "both install the same binaries"
+
+  def install
+    # We don't need Windows' files.
+    rm(Dir["bin/*.bat"])
+
+    bin.install Dir["bin/*"]
+    libexec.install "conf", "lib", "src", "doc"
+    bin.env_script_all_files libexec/"bin",
+                             GROOVY_HOME: libexec,
+                             JAVA_HOME:   "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+  end
+
+  test do
+    system bin/"grape", "install", "org.activiti", "activiti-engine", "5.16.4"
+  end
+end
