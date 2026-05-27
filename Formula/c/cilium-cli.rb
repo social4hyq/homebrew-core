f45f7cef9d80e9086dc73d93391918f78044a466
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 03:02:57 +0800

    cilium-cli: update 0.19.4 bottle.

--- Formula/c/cilium-cli.rb
+++ Formula/c/cilium-cli.rb
@@ -15,12 +15,7 @@ class CiliumCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b8ec3a2e375b0680248926ceae7ce3a62ece31d91f9c4a1567d489e111380e7f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "365c14c6cb05b9eb584c0b8609cd7cf97a163472269b01ee4fda5b7082922dce"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ab3bc1eff4e41d17760222f1b8c38afac6188987c5bf12eeeddec0fbab87fe66"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ee0442ca001960088dd7e588eb5a4defff09f62de7011cfe2238ab98398a538e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fe57781c328b462925ce385e38b1dc7fc908b006bfba274c91f341ab099cfd06"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "239f92e5da47427006cb77afa522a8f2cdc89c875946137016c57728dc66f175"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cbad7fa97a4bfda86e063186bf60682f688461b1a786d78692cbd5bb94cea301"
   end
 
   depends_on "go" => :build
