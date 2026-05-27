d5b81028c9e97279d14b8b5d82dcf93f6542cfcd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 05:03:29 +0800

    rustledger: update 0.15.0 bottle.

--- Formula/r/rustledger.rb
+++ Formula/r/rustledger.rb
@@ -7,12 +7,7 @@ class Rustledger < Formula
   head "https://github.com/rustledger/rustledger.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3721fe47964dd533229fe15cabb7fd202dd779e2289be8ff845b504ae05f979a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "02e1615620285b37657efcaacaa67e5daeec04861635907b2bf99361cb8070ac"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "551cdbd7b4df447f5034dbacfb3b16fde8820ad5ecfe02d4e66aa81248b9da86"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f83a882d77859c8943ad3aa982d74d349f0f3a96cb871653379970a23d7e27b3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "421ace1864d34ad7ec3d812587dedc296e4b076c25eac7b22eb6cf078516f803"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28a0557322e3aced63781f4a69ac1747e80b0c40d28822cc0335708b4dcc8ec9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "435b03177a8a43887f63acb4d728b205334ff51fe1ab1eeaf1859ded795cffea"
   end
 
   depends_on "rust" => :build
