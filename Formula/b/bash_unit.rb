23dd4058e3944181ad1cfd5143cabbb35fcd005f
hqzing <hqzing@outlook.com>
2026-05-27 15:17:04 +0000

    bash_unit 2.3.3 (new formula)

---
+++ Formula/b/bash_unit.rb
@@ -0,0 +1,26 @@
+class BashUnit < Formula
+  desc "Bash unit testing enterprise edition framework for professionals"
+  homepage "https://github.com/bash-unit/bash_unit"
+  url "https://github.com/bash-unit/bash_unit/archive/refs/tags/v2.3.3.tar.gz"
+  sha256 "a2f76ddca88e2bef7c628c8cac6bf68b93a388fce143f6a4dc770fe1b3584307"
+  license "GPL-3.0-only"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8c7b39455fab02fd97f73960e4c41b3d65f7695d5442d3950385c4d5fe874f09"
+  end
+
+  def install
+    bin.install "bash_unit"
+    man1.install "docs/man/man1/bash_unit.1"
+  end
+
+  test do
+    (testpath/"test.sh").write <<~SHELL
+      test_addition() {
+        RES="$((2+2))"
+        assert_equals "${RES}" "4"
+      }
+    SHELL
+    assert "addition", shell_output("#{bin}/bash_unit test.sh")
+  end
+end
