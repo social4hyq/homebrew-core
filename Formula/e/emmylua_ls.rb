77e0195db14418b8b28c409d40374834a2d0029b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 16:24:07 +0800

    emmylua_ls: update 0.23.1 bottle.

--- Formula/e/emmylua_ls.rb
+++ Formula/e/emmylua_ls.rb
@@ -8,12 +8,7 @@ class EmmyluaLs < Formula
   head "https://github.com/EmmyLuaLs/emmylua-analyzer-rust.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc5864c695135aa178800e8984d12c2175ff9688284fd2b079442bf951a57a67"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65e78667a942eddb5760e98b1ecff9d8b2aa75155ba8bded66cc84e90dc846bc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7fa169bd40a4b43afbd2e17626c891c6f6f6265b603e3578d9a393a3ee5d0c3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cfbc3a60bd1c5d28eef0708f9763ee84cbc97453263621a25e42dd9af734290d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "37f2d090376b4f96e969432c625d909d09e4355d4a80061546c4fb2b4464fbf8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1e8a221ca355a6393ec9504262566a00dc4de5a664a502c903e5366393f442a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9988a526761f53129366ae3baeaee3f056a631d50e2333866a429fdd00b91c20"
   end
 
   depends_on "pkgconf" => :build
