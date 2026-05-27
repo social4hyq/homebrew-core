b173d4a7a8a44cb0f9088c850d5bbcf6a5be5903
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 04:44:53 +0800

    virtualpg: update 2.0.1 bottle.

--- Formula/v/virtualpg.rb
+++ Formula/v/virtualpg.rb
@@ -11,15 +11,7 @@ class Virtualpg < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "96423b815a0781a3635f54f02bac58f5b6f97ad735184005b812b7bbe2bda8c6"
-    sha256 cellar: :any,                 arm64_sequoia: "8e204f28da296c7deb1ce80831b2bfe8d7456aa540642c554de86f031c7aabad"
-    sha256 cellar: :any,                 arm64_sonoma:  "d66f74bcc667dbd4b71f0a9cc8374b3bd17d1ec31e9a112bb61729af23e9479f"
-    sha256 cellar: :any,                 arm64_ventura: "666a4cdf6831d4a2d8ca68c9f1674a23215d947dae38c6a5a966f051d172ba4d"
-    sha256 cellar: :any,                 sonoma:        "19e10266fbc74189b0acd7ed6e8d9026008ccf7f7731ef66cbe093caf2e79ec8"
-    sha256 cellar: :any,                 ventura:       "8f2292bbe12befda53781d168e3a0dc8fdbf3b387e53abf920a25971605b42cc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e8800783153dea8ff0de92a169b8a9810aa69e0682ca0fd567c983d8925e42c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a3eea533c6345f3ba72760bb196a0c673414e87b02763a4cb48f260acdce535"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "395238cbe5f4ae054ea898f3ea61444aef37146a5125d95214143dd59d38d61a"
   end
 
   depends_on "libpq"
