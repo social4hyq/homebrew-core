cc3851ffde6bbf0865379d4ef9af54606e2d3db1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 12:27:41 +0800

    fortitude: update 0.9.0 bottle.

--- Formula/f/fortitude.rb
+++ Formula/f/fortitude.rb
@@ -7,12 +7,7 @@ class Fortitude < Formula
   head "https://github.com/PlasmaFAIR/fortitude.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c3aa77b513f1a25b317d4b42eb5f0414e806bf0b39563d0f98eb24522e755f4f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e9f173ebbbc57e9cf0c5691c064da5b758c47efe2f54220d7b362bdfcf3fe62"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ccae67fb22d0966ad50cb0d8cabaa5183eaa53061cd29e519f69e2705619c11"
-    sha256 cellar: :any_skip_relocation, sonoma:        "400f4d96043009f5b4258a79178f0bbb95d9cb62d9478f497d712d15cea2ff40"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "546941a3467143b2291927d1e94ae7b6288781cc3f1c91b6d8ddc32c0f895913"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a816f1146d8b65153c7372c856a23f8440068e5ad8aa36e4e79a43ef5acc2a43"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8807ac2fce8f0b23e9f465b20d23fc82f67ba84101c79557f3598fdc7e0af861"
   end
 
   depends_on "rust" => :build
