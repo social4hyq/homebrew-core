93aa70bfee9092129f045194148777819d45e4df
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 00:44:07 +0800

    sccache: update 0.15.0 bottle.

--- Formula/s/sccache.rb
+++ Formula/s/sccache.rb
@@ -12,12 +12,7 @@ class Sccache < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e4d00962cd674546f2d35d3ab2898f7f5ce73789ba42af8dc20a1c661f01c2a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "417f7f28ab9eeb71b96c89f3bc6ceef6c33e1b307f721ee4f5661b1d13e2b790"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b5810a01e8915c95a4051a5d3dd38571c1b2b943d61318d99b17a315a4fba86"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1c4a260bff569c03049be751700a28b67882b6d1b0b69db9808cd87195160046"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8d0b06edd202fc9c264a6ec9e6252976f2e23355db340cb721aff9087352a3d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6dcfa6b6c43646b15a2e89c5ddcc1e7c71032b473a00c1a240bc22ba698b33b0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9a1d361c7026bd53ee84493cea0aaa48c2d19232dcb3ed5226929ed9b5c97b12"
   end
 
   depends_on "pkgconf" => :build
