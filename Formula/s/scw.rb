4a2510f226d5c22e6259884aa416106dfa7a7870
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 15:31:53 +0800

    scw: update 2.56.1 bottle.

--- Formula/s/scw.rb
+++ Formula/s/scw.rb
@@ -11,12 +11,7 @@ class Scw < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e5b12a997f32bc022eb01f310b5cdfc397587e92b13616e7316bc0e16dfd7739"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fbababfac7acb453faeaf1b4c924ccd1f4de83788b645cfe80fcd604a5c6f185"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7cd00f4f6efcb786535b698c58e937df7d1ce150909ae81b3dcc52fd02adb11c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "185ecbd56454b7ba204db8d962bc3def691386bc6e4bef36dfe64d566c78f676"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3daf0e66e90b4781fce30290a219e3076b0bba160bea09b560d33f3ad6bc2f7e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "875b3775fe4a7ed571a8f9874b907f2fef47984ae115a69ed7249521868c982d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b4665f36418bffeaa487d6d30a170414e014783f17033d7aad4ddf62368881d8"
   end
 
   depends_on "go" => :build
