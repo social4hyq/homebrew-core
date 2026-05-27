7bf5d4ef3ffb7747418ec9918e1a5934b7e96667
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 15:32:13 +0800

    bender: update 0.31.0 bottle.

--- Formula/b/bender.rb
+++ Formula/b/bender.rb
@@ -7,12 +7,7 @@ class Bender < Formula
   head "https://github.com/pulp-platform/bender.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "65085211d2ab9229701cdd216eb917708b6963f98d3301acecb9d51674d257a6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7f59c1b0625267c5526dd6cb5ae13eb0fa1e05bf5bdcdc8a48d4470ffe4b4f61"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9d3d2c7818fea85741ddab32f1b15b2dcca2cb013a0d5200c8c5e77ce707516"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b1d60f88ea984716d828cdb133c94209a9e805526a25b92a3842ba91e4da23b0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0aeee5275f1ff86d5b488ec25ff0bc1af62573ffc87813ab88429c5d75dd77bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9488f4a1f1321757d1ef2e1b4ff00194b00898d40049a5580191f2665aa5c54c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9a7ebad507da8fef1cd7ea01d87c61eaceda98d608d139dc0a5e55fa1495ee73"
   end
 
   depends_on "rust" => :build
