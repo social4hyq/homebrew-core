11689925255823a6fd009ad9ad6937b411668089
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 01:00:49 +0800

    serf: update 0.10.2 bottle.

--- Formula/s/serf.rb
+++ Formula/s/serf.rb
@@ -7,15 +7,7 @@ class Serf < Formula
   head "https://github.com/hashicorp/serf.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2901eb9d2de6b46caefd9bf736b479a4fdbde4f83856c4c57bc114411a332134"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13e3b04200c7af32b917a447452939a6dbf82ebc40e7937465e4150c2fa2418d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "13e3b04200c7af32b917a447452939a6dbf82ebc40e7937465e4150c2fa2418d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "13e3b04200c7af32b917a447452939a6dbf82ebc40e7937465e4150c2fa2418d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d6d6ae5c6519299a5066731b9a37a7503453400849633423839d85cdf80e7d98"
-    sha256 cellar: :any_skip_relocation, ventura:       "d6d6ae5c6519299a5066731b9a37a7503453400849633423839d85cdf80e7d98"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a687881da8ed7915ff023c57afcbece681a54dd3c9a267df6d3c849db209de2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd73aa1d5303d52c424bb871db029524e8f64619c0aa0dfd1bccde4f9d023211"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "68b4f697ac856715809bc209963128429f8b3b05fd06190d4b9c844c86e0fcd5"
   end
 
   depends_on "go" => :build
