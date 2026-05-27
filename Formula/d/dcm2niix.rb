f23c52fceca0df6514f4729a40bfa5a9ba07cb08
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 16:45:27 +0800

    dcm2niix: update 1.0.20260416 bottle.

--- Formula/d/dcm2niix.rb
+++ Formula/d/dcm2niix.rb
@@ -13,12 +13,7 @@ class Dcm2niix < Formula
   end
 
   bottle do
-    sha256                               arm64_tahoe:   "6f1e01c9d3edee9470d14033c1a3b9f6c7bddc7474fc0f916949ce8e4c9dece2"
-    sha256                               arm64_sequoia: "781e5f1d49c8e2811b60cdd71316a10813d1f9fee0637ea4614271e30667c712"
-    sha256                               arm64_sonoma:  "8e78e0a91b0fa02bec640991fe6f07a3766c80bcb816a79b25dd6033f7fb18d3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6d9bab3bc043ae32cb80aa7a64d9f668736b14eec546f529b2f3f8b73e7a52ae"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8903003d4c70a0973cace04352d99b7912d6f712421f7a06bc1d45f0f994d879"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af1694c2b47ee8fd650956909c9a46b252d146381d695d46f0fcf35938043443"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "006b45d0e4f06b7d8c4649364ce45ba25bbabec3654f45d3d70f0d6fe24382dd"
   end
 
   depends_on "cmake" => :build
