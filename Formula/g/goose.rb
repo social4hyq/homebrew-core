6adde6eee63d2e0fd35730fc696d99f951bb97fb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 14:43:21 +0800

    goose: update 3.27.1 bottle.

--- Formula/g/goose.rb
+++ Formula/g/goose.rb
@@ -7,12 +7,7 @@ class Goose < Formula
   head "https://github.com/pressly/goose.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "57427c333956a1be6355eb9c3fe5d60be85ae9e9d495a616191a283deffbcf32"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "98400f3afe38672c137fb26a42f46cd585a996dff76636efc5b1a40726d4e818"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5518cd87c225d3d83c03e741e43d31d60aa9907e6907e0670842451b879210e0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e682888273337ae7c308c380f9e3e42ef285fceb0a2a6c2b7bdfcdcf2787d920"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3faafbb2df138d0eebd11724bcbb42ebd270c71d9ad03405b614713a1c177590"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b65e646c1ce234632b67690760c6d419a1de33eeb3276fc701bdfa6b7862cd3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e1525f78e3c304a68a278105858affa5e178f2a7bc27719ba6cc901eaf048c15"
   end
 
   depends_on "go" => :build
