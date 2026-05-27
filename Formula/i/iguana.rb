6f68178f03191e61a71726ed1d66957d97113617
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 03:43:05 +0800

    iguana: update 1.1.0 bottle.

--- Formula/i/iguana.rb
+++ Formula/i/iguana.rb
@@ -7,7 +7,7 @@ class Iguana < Formula
   head "https://github.com/qicosmos/iguana.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "33af9d2bfa1971283aa8149a4fc8469a8da267345d23e8cba97536d8a06c4c41"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1d1a2684a0f74dcf182a926b61fca5b6e53b49d2a7aa3f8db06fb193a6bd206f"
   end
 
   depends_on "frozen"
