700d5cc852abffd5c30d1ab1363523986fbf52a3
hqzing <hqzing@outlook.com>
2026-05-27 09:41:19 +0000

    fatal 2026.05.25.00 (new formula)

---
+++ Formula/f/fatal.rb
@@ -0,0 +1,29 @@
+class Fatal < Formula
+  desc "Facebook Template Library"
+  homepage "https://www.facebook.com/groups/libfatal/"
+  url "https://github.com/facebook/fatal/archive/refs/tags/v2026.05.25.00.tar.gz"
+  sha256 "66bf6c6230902cd020a4b8f3815e0024eb5c520aa6a6e94048c42cb02edbdfe8"
+  license "BSD-3-Clause"
+  head "https://github.com/facebook/fatal.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "13767a4bc64d995a36f4af9d9d09ade9b5e6dfe634012446c271823fbfde9fc3"
+  end
+
+  def install
+    rm "fatal/.clang-tidy"
+    include.install "fatal"
+    pkgshare.install "demo", "lesson", *buildpath.glob("*.sh")
+    inreplace "README.md" do |s|
+      s.gsub!("(lesson/)", "(share/fatal/lesson/)")
+      s.gsub!("(demo/)", "(share/fatal/demo/)")
+    end
+  end
+
+  test do
+    system ENV.cxx, "-std=c++14", "-I#{include}",
+                    include/"fatal/benchmark/test/benchmark_test.cpp",
+                    "-o", "benchmark_test"
+    system "./benchmark_test"
+  end
+end
