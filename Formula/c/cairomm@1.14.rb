145b6ec8f2dc7b9938da452f32b3d44b2b697d50
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 11:37:44 +0800

    cairomm@1.14: update 1.14.5 bottle.

--- Formula/c/cairomm@1.14.rb
+++ Formula/c/cairomm@1.14.rb
@@ -11,16 +11,7 @@ class CairommAT114 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:    "3c84812ac04258b3fe766341479b99b1e28a9572b995e7bd33e3184fdb29055a"
-    sha256 cellar: :any, arm64_sequoia:  "87bd872e7129feb2db9ca965fe5e065ebef30f6a196f0c80d8012b60cef66bf2"
-    sha256 cellar: :any, arm64_sonoma:   "cf8e7aa143ab206a8986997621217fc40e219d23a88e09d5a3d1ee1ce58d78c2"
-    sha256 cellar: :any, arm64_ventura:  "76dbfad402d8573c6acfdbbf785ce70eb13e626318aa0c8f241864b43a8c8ee2"
-    sha256 cellar: :any, arm64_monterey: "bd41c2293acda52bcef8fa7332b90860007ecb37b864677f59d624f1fff457df"
-    sha256 cellar: :any, sonoma:         "48cd095943b2001726bd9b8f678424725e73c3ac4d088e61cff65b973641ae0c"
-    sha256 cellar: :any, ventura:        "a642e54afb60394f1dc138b3611b0c729f09d9aa0217a351823b1ae75816bcf2"
-    sha256 cellar: :any, monterey:       "4ef3f6806bae703ef82e302f33c28d96136d3bbbdef56c36f9032cf6aaa10c46"
-    sha256               arm64_linux:    "d23cd3b782f7fbaa3bdf7445334de4f170ab6349feefc443e3362944515a0190"
-    sha256               x86_64_linux:   "4fce5fb84ea0095ce2f9e0996c926fc8bb54f55a458a9eb1be50a40241f8a6e5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dbb36e44880f143102bb6f0fd79815588d4015705d0363819c9a23f172afbeec"
   end
 
   depends_on "meson" => :build
