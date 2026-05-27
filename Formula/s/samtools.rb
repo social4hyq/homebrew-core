ddca4847857f55ca8a0820d8467db8d4f63d53f9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 07:17:48 +0800

    samtools: update 1.23.1 bottle.

--- Formula/s/samtools.rb
+++ Formula/s/samtools.rb
@@ -6,12 +6,7 @@ class Samtools < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "f90e6a1ab4dfb450306fc386b77aff90777e89d4fe918135209fd95d9c0d81e5"
-    sha256 cellar: :any,                 arm64_sequoia: "1917a110f8464fc967a692f375c939250d730f1776cdb614b2d9333300f1a2a3"
-    sha256 cellar: :any,                 arm64_sonoma:  "49f2737b5def65598ca2fa8811463a64a32c7aea8484969f5a24e0f9ce198248"
-    sha256 cellar: :any,                 sonoma:        "64e86c434b6878b264e05310ef29cd6dee574156b5b51fb638caf372a48a37ea"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e32b90bbb900986a7b082eba4bdf35495a65c1330c927d439c3bbcc9186ce3bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2d8d142e02362f0009664adac7f9e5d896311a6d1bb658b8e98782181663e94"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f60c10e36640cc77b3a86b31b4cd29881bff7d2140ab518354bc6833b2ab28c0"
   end
 
   depends_on "htslib"
