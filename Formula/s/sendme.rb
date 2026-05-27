cf0650f4f0447bdd68c6ccdf49fce321d137d659
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 00:39:42 +0800

    sendme: update 0.34.0 bottle.

--- Formula/s/sendme.rb
+++ Formula/s/sendme.rb
@@ -7,12 +7,7 @@ class Sendme < Formula
   head "https://github.com/n0-computer/sendme.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b08e1195a14b1b1e779db64f0a105f718cd8e03f33f982a5a9646d003e35e4ca"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a022e4c99baae0d3ed3027b1009d8dfbb19fa3253a4dcb33ffc8e8316ec578f8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41371d12d68282c5938d489f34110becf71c794dd46d8e6f3dc6c9a7ee7e2df4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4f1624af90bb13f2fbe21e1e7243832dfe2ff7cb2b7555d26493cd23e34778bb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fdd3fa6bda56ecb3f564ec0205b44f0a1d3fcb47defcd362300f569b9e5a4058"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "329dcf751da295667160e8bcabeb549cc543589d7024a256c0e84b224e06bafd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1efa4c9ba9a7483a7514e021c2ec30800cbcd12252973d106203ae0e82844859"
   end
 
   depends_on "rust" => :build
