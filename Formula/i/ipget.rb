26593c41a893145b14c9e8e0fa0e7049ae47e0ba
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 15:25:18 +0800

    ipget: update 0.13.1 bottle.

--- Formula/i/ipget.rb
+++ Formula/i/ipget.rb
@@ -7,12 +7,7 @@ class Ipget < Formula
   head "https://github.com/ipfs/ipget.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "61d41d721f5d93799d5cb4422f3b44d09f602160c4cbcc4b8c974fb6a1c9eb68"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6d63e98d336bf8dc7f11680ad7ea1af775c13280d50d1b5fa911f5b20acc0ee9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c1556693bcd82e85ddf273f098ad2d7609ad6bb8cc439605a52151dde917dbcd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d2660824a7d3b97aa24936aa84ff595ca09f7e0c885cd698761674d45a8b0c31"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "00d6bfd3b6dea6292bafd8c7bc438d52681941dd52c5ee317ab7080b7921961c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd6dafc463b658a9d6db25cc3ed8ad20fbbac60736dfacc4b9da08425eeed1eb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f06c2148dda90dc34d42ac7f65f0c51230ef6f1a6a00d7e7814c3397ea7533a2"
   end
 
   depends_on "go" => :build
