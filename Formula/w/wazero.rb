8d852051a11c856d7cc476a8698f9eea4d4522c6
hqzing <hqzing@outlook.com>
2026-05-27 10:20:32 +0000

    wazero 1.11.0 (new formula)

---
+++ Formula/w/wazero.rb
@@ -0,0 +1,83 @@
+class Wazero < Formula
+  desc "Zero dependency WebAssembly runtime"
+  homepage "https://wazero.io"
+  url "https://github.com/tetratelabs/wazero/archive/refs/tags/v1.11.0.tar.gz"
+  sha256 "a785f0eabe510e454a01e0d187675a913f96814d0c7e38c4717e03f6d5420ed4"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e9eb11965062b5259c0dac49f69ff488d9e38bd96aebf78d7169984d288e931b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e9eb11965062b5259c0dac49f69ff488d9e38bd96aebf78d7169984d288e931b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9eb11965062b5259c0dac49f69ff488d9e38bd96aebf78d7169984d288e931b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1791849ae4bc71b21c16f5411234a8b6147d730cdfaeb4feada8ccfcd952fdac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "45288c26d39cf14c8fab4921edf826540965ae2280cac66580678ba3061aedec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4191f33d9b5007bb554ce3240aa0b242eab381ccfc6ea1fd51157601448801d"
+  end
+
+  depends_on "go" => :build
+  depends_on "wabt" => :test
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/tetratelabs/wazero/internal/version.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/wazero"
+  end
+
+  test do
+    assert_equal version.to_s, shell_output("#{bin}/wazero version").chomp
+
+    (testpath/"mount.wat").write <<~EOS
+      ;; print the preopen directory path (guest side of the mount).
+      (module
+        (import "wasi_snapshot_preview1" "fd_prestat_get"
+          (func $wasi.fd_prestat_get (param i32 i32) (result i32)))
+
+        (import "wasi_snapshot_preview1" "fd_prestat_dir_name"
+          (func $wasi.fd_prestat_dir_name (param i32 i32 i32) (result i32)))
+
+        (import "wasi_snapshot_preview1" "fd_write"
+          (func $wasi.fd_write (param i32 i32 i32 i32) (result i32)))
+
+        (memory (export "memory") 1 1)
+
+        (func $main (export "_start")
+          ;; First, we need to know the size of the prestat dir name.
+          (call $wasi.fd_prestat_get
+            (i32.const 3) ;; preopen FD
+            (i32.const 0)) ;; where to write prestat
+          (drop) ;; ignore the errno returned
+
+          ;; Next, write the dir name to offset 8 (past the prestat).
+          (call $wasi.fd_prestat_dir_name
+            (i32.const 3) ;; preopen FD
+            (i32.const 8) ;; where to write dir_name
+            (i32.load (i32.const 4))) ;; length is the last part of the prestat
+          (drop) ;; ignore the errno returned
+
+          ;; Now, convert the prestat to an iovec [offset, len] writing offset=8.
+          (i32.store (i32.const 0) (i32.const 8))
+
+          ;; Finally, copy the dirname to stdout via its iovec [offset, len].
+          (call $wasi.fd_write
+            (i32.const 1) ;; stdout
+            (i32.const 0) ;; where's the iovec
+            (i32.const 1) ;; only one iovec
+            (i32.const 0)) ;; overwrite the iovec with the ignored result.
+          (drop) ;; ignore the errno returned
+        )
+      )
+    EOS
+
+    system "wat2wasm", testpath/"mount.wat", "-o", testpath/"mount.wasm"
+
+    assert_equal "/homebrew",
+      shell_output("#{bin}/wazero run -mount=/tmp:/homebrew #{testpath/"mount.wasm"}")
+  end
+end
