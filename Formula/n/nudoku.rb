23b9291d7c341abda6cc2dbf5311e2b5a28f2b9f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 11:14:53 +0800

    nudoku: update 8.0.0 bottle.

--- Formula/n/nudoku.rb
+++ Formula/n/nudoku.rb
@@ -7,12 +7,7 @@ class Nudoku < Formula
   head "https://github.com/jubalh/nudoku.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "23f852beb2766426f9b5126d34fa384045387c4c8ffcadc6ca2ed5fb6a7780c5"
-    sha256 cellar: :any, arm64_sequoia: "23fbd7ad2727a862d6fa8b8a6ed458bf7ec973d9b319f3d56f8a31c038632544"
-    sha256 cellar: :any, arm64_sonoma:  "9de871a41e93c471d24c2ee3a4be9e5502c03147c282ab1f8ec96c02f42feab4"
-    sha256 cellar: :any, sonoma:        "2e0303165131c4e483fea029ca68c3dc16fd991201b4c314a257584faeed2569"
-    sha256               arm64_linux:   "aa24c19f9bc4d7d9802023cf1fabf721f4fdb4dc88b9ddecab412a4927a7ec55"
-    sha256               x86_64_linux:  "abce8638e046b755c3d568808a022e7b0c74689e55c6b98f54e1d14bdc58424f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4cbd1346f61cee6cd9435c822ab5c75d2e99ce1de45c515b74288fa1f8b1be6d"
   end
 
   depends_on "autoconf" => :build
