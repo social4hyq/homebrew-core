00314828cc35a5e253606a8a30fb8c722430da0f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 06:07:13 +0800

    vscli: update 1.3.1 bottle.

--- Formula/v/vscli.rb
+++ Formula/v/vscli.rb
@@ -7,12 +7,7 @@ class Vscli < Formula
   head "https://github.com/michidk/vscli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2273f8d6dc3e13c957790a6998f02a7d833b99db3b068923da6ded29d0b89817"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "22b01724115be58e091a1944af0e8f8a3b4a663a29e5a3a7ca994cd90842ae4b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac9796e0c0ac9e59e1939e44138470380f150eb2def5b10d60f4625bbb74db86"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9d47361b0293b964768ac61816149744584184c6e441bc4229a187f6cb8e43df"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6e6e8f7c90afd4354a42e678dd887adb959a9e0eb6e756aa3a00da1b90b5218c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b42485a36a1a3586d9873cb8ef5fb3e0dfe6e00c1d04148d712d3064a56be218"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "35b1b5d4191d3c7761dda84d282b6e695ff5553e47ec76201032580b8efc5de8"
   end
 
   depends_on "rust" => :build
