87269d4e54a96a85a2a4355d7d49d119d0165dcc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-27 16:21:10 +0800

    bwa: update 0.7.19 bottle.

--- Formula/b/bwa.rb
+++ Formula/b/bwa.rb
@@ -7,13 +7,7 @@ class Bwa < Formula
   head "https://github.com/lh3/bwa.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cd17b7367cbda46d49bba96116e29ec1ffae87d4595bb599180608d3d2bd8f36"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5bb4e7a2c62091c64124fe181f164b3bfe693862a2d84b48774dd0478c11c5a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2cbbb8189bf2e3a42a409777cdaaa9d00128e75b3db842c553e6686b57bdbe4e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a5db6c986e2526f8857cfa30d97c7c7c685a26e936f05b2e1cca6e63b62dd262"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "11f9c38e101df4dfb8b60fce5b1f81f2a70c6728fdda14aa4c7fadea5f1b89de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "195e334b5f9c69bbbddd656bec4b6b6be44e264a20fa1379e379bc57725bd621"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b229a78b9acf34c240905a1929de06a13b65279cf13441281ab7008f47b0fa68"
   end
 
   on_linux do
