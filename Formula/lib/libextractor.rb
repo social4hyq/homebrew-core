5eba0d6d170d3b3e21f1466a7b38386e75638dc3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 00:48:18 +0800

    libextractor: update 1.13 bottle.

--- Formula/lib/libextractor.rb
+++ Formula/lib/libextractor.rb
@@ -7,13 +7,7 @@ class Libextractor < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 arm64_tahoe:   "c2fd159149aca1a534cbfefa4d19fc9247eeb7c1884ec184d02b51abc1a10104"
-    sha256 arm64_sequoia: "ff68520aee98762315e38bac3cd80d4671aea25cdfea91b74a94ca097ccff5d8"
-    sha256 arm64_sonoma:  "26b036e111b2bb5ce208cb586fb4c5790d24ffdf4a87587bb6bb46ca0c484e96"
-    sha256 sonoma:        "d45f64b417e7c7029835b32d45ad8222b64b24ade727cfc0b795869438f35c9a"
-    sha256 arm64_linux:   "46b5c12a0f96cb68769bb7bc30fb1d4b64cab9051e9cc3e902f342f2f6cfc92d"
-    sha256 x86_64_linux:  "62f5457e278cd0c44c770067398736631f9e516738e2a2c672e65d6eb27de541"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "47dbbb28ef10a80f228af39c5b23c18bf6dfa1d3bfbb6ab813cbb971c3963434"
   end
 
   depends_on "pkgconf" => :build
