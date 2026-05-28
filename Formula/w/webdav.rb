f70f6a7b185acf2c388c3d784ee56901ec2c60b2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 08:09:59 +0800

    webdav: update 5.11.9 bottle.

--- Formula/w/webdav.rb
+++ Formula/w/webdav.rb
@@ -7,12 +7,7 @@ class Webdav < Formula
   head "https://github.com/hacdias/webdav.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f73c3ef1c1f728b484ae9516bfbb21faf92b18ae211080c572a13d06d83d7bfb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f73c3ef1c1f728b484ae9516bfbb21faf92b18ae211080c572a13d06d83d7bfb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f73c3ef1c1f728b484ae9516bfbb21faf92b18ae211080c572a13d06d83d7bfb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6edf6d971c3df47ccc189cddc9a9f8c143752f3236dab5c4a6e441b6ed40dad5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "22f4681e8a97903787d2bd6da7af38d9c0220bb0df2837a9e924610ad7d4b02d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "383a183d34b87fff8306ad4d1b666a6ee933145d9125ef66b67652904c484a91"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7a81642c0011765b2e2a936c294eb606150ca8065197742d468c9644687ff0a1"
   end
 
   depends_on "go" => :build
