01c3f5f36238bdf5f9fb6a3a3e0f1750d9d17508
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 13:50:38 +0800

    ta-lib: update 0.6.4 bottle.

--- Formula/t/ta-lib.rb
+++ Formula/t/ta-lib.rb
@@ -6,14 +6,7 @@ class TaLib < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "7817d0d8bfa2799e2b90618352a6d276dfa36ff1af3f343c9f4d7fe0be8d6240"
-    sha256 cellar: :any,                 arm64_sequoia: "0ef739e4436909635fc3bacd8df2362d37a9467f11631111463dd387776b6b13"
-    sha256 cellar: :any,                 arm64_sonoma:  "8d7deb75b383ebc50513ebd077c4268acdb09d602b19ed4c8878f915f8f8b1e4"
-    sha256 cellar: :any,                 arm64_ventura: "12709c4df99f1dce0315b653a1bdb962f3cc6c429bda41a13d17f4dd43079644"
-    sha256 cellar: :any,                 sonoma:        "c34c5da2df4abefcb86d3f130a7e8fe0b85fad2c83e2343d9e65e4a615c6b235"
-    sha256 cellar: :any,                 ventura:       "151e74186d18550800b5e7e714c50e99e25b1e4f536fc83521e45e4ce389fc0e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e6bc65c4e2fb0d567fb739d473d9ee2b696741bb5d05f0f10c8594e096c875c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "868d1163296be1e3d5f10bcdf4047f0e218eb91617da562eac14fd04cd05433d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "16dc9a521a4a779c36ce7803a9f895721ee1390f9b7548c9caf0bc14fd70c354"
   end
 
   on_macos do
