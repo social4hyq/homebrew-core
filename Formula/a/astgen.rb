7a9213346687f0bf183650838f483b954f9184c4
hqzing <hqzing@outlook.com>
2026-05-28 15:51:52 +0000

    astgen 3.42.0 (new formula)

---
+++ Formula/a/astgen.rb
@@ -0,0 +1,37 @@
+class Astgen < Formula
+  desc "Generate AST in json format for JS/TS"
+  homepage "https://github.com/joernio/astgen"
+  url "https://github.com/joernio/astgen/archive/refs/tags/v3.42.0.tar.gz"
+  sha256 "fdaa0b63ec5ce7b718011cc4df302d415a8d7b80ba68a62599221a8e256610fd"
+  license "Apache-2.0"
+  head "https://github.com/joernio/astgen.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "5415f948a456b8a335b2f52c2f79bbaca22c0cae02cf888cbb8598ce509dca16"
+  end
+
+  depends_on "node"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  def install
+    # Install `devDependency` packages to compile the TypeScript files
+    system "npm", "install", *std_npm_args(prefix: false), "-D"
+    system "npm", "run", "build"
+
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    (testpath/"main.js").write <<~JS
+      console.log("Hello, world!");
+    JS
+
+    assert_match "Converted AST", shell_output("#{bin}/astgen -t js -i . -o #{testpath}/out")
+    assert_match "\"fullName\":\"#{testpath}/main.js\"", (testpath/"out/main.js.json").read
+    assert_match '"0:7":"Console"', (testpath/"out/main.js.typemap").read
+  end
+end
