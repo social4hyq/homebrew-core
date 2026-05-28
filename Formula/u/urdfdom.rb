f7f2518ff9ba0bbe5398c0c8ffee47243077e91d
hqzing <hqzing@outlook.com>
2026-05-28 20:08:50 +0000

    urdfdom 6.0.0 (new formula)

---
+++ Formula/u/urdfdom.rb
@@ -0,0 +1,97 @@
+class Urdfdom < Formula
+  desc "Unified Robot Description Format (URDF) parser"
+  homepage "https://wiki.ros.org/urdf/"
+  url "https://github.com/ros/urdfdom/archive/refs/tags/6.0.0.tar.gz"
+  sha256 "3305bb725095c78ca3408a510e11f530cd9bb45229779084695b21b2e693bde9"
+  license "BSD-3-Clause"
+  compatibility_version 2
+
+  # Upstream uses Git tags (e.g. `1.0.0`) to indicate a new version. They
+  # created a few releases on GitHub in the past but now they simply use tags.
+  # See: https://github.com/Homebrew/homebrew-core/pull/158963#issuecomment-1879185279
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "effa824c55c30479584093c942e6dced50df62e50f5c231ea255bd13a33e3f07"
+    sha256 cellar: :any,                 arm64_sequoia: "04b0418753b6c2c8ee5f07804984c95e1578feb2c34d9ef069e5ddd6e92044e3"
+    sha256 cellar: :any,                 arm64_sonoma:  "e5a5f49411905286f36a609b362234ee2facababd82a0e74bb85b6e7e4978252"
+    sha256 cellar: :any,                 sonoma:        "b7d0acc7b93e4b398c0ab501a33cc05ba41fb14180aea6ff013ada12ef9202bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11a9f9031a71408e26083dd7b3dfaeb44484d18329f14ca4267e297359d9aac0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "519e798be04e44dd8e8c118ce8b4cdc8a2a60882c2d801bf1253720a290031f8"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :test
+  depends_on "console_bridge"
+  depends_on "tinyxml2"
+  depends_on "urdfdom_headers"
+
+  def install
+    ENV.cxx11
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <string>
+      #include <urdf_parser/urdf_parser.h>
+      int main() {
+        std::string xml_string =
+          "<robot name='testRobot'>"
+          "  <link name='link_0'>  "
+          "  </link>               "
+          "</robot>                ";
+        urdf::parseURDF(xml_string);
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", *shell_output("pkg-config --cflags urdfdom").chomp.split,
+                    "-L#{lib}", "-lurdfdom_world",
+                    "-std=c++11", "-o", "test"
+    system "./test"
+
+    (testpath/"test.xml").write <<~XML
+      <robot name="test">
+        <joint name="j1" type="fixed">
+          <parent link="l1"/>
+          <child link="l2"/>
+        </joint>
+        <joint name="j2" type="fixed">
+          <parent link="l1"/>
+          <child link="l2"/>
+        </joint>
+        <link name="l1">
+          <visual>
+            <geometry>
+              <sphere radius="1.349"/>
+            </geometry>
+            <material name="">
+              <color rgba="1.0 0.65 0.0 0.01" />
+            </material>
+          </visual>
+          <inertial>
+            <mass value="8.4396"/>
+            <inertia ixx="0.087" ixy="0.14" ixz="0.912" iyy="0.763" iyz="0.0012" izz="0.908"/>
+          </inertial>
+        </link>
+        <link name="l2">
+          <visual>
+            <geometry>
+              <cylinder radius="3.349" length="7.5490"/>
+            </geometry>
+            <material name="red ish">
+              <color rgba="1 0.0001 0.0 1" />
+            </material>
+          </visual>
+        </link>
+      </robot>
+    XML
+
+    system bin/"check_urdf", testpath/"test.xml"
+  end
+end
