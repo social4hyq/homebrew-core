ee1c8077925e44d6e4c323e2b21495dfe0b28f7f
hqzing <hqzing@outlook.com>
2026-05-28 19:49:58 +0000

    pacparser 1.5.1 (new formula)

---
+++ Formula/p/pacparser.rb
@@ -0,0 +1,111 @@
+class Pacparser < Formula
+  desc "Library to parse proxy auto-config (PAC) files"
+  homepage "https://github.com/manugarg/pacparser"
+  url "https://github.com/manugarg/pacparser/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "88dda1833b5c467ea61d0217fa58fbc7980f7c5a856ca2af325e6a110b1c081d"
+  license "LGPL-3.0-or-later"
+  head "https://github.com/manugarg/pacparser.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "ea5cbb98b0b0e6d373ec70d530b833994e4c584a07a840036ec315c2f98a2760"
+    sha256 cellar: :any,                 arm64_sequoia: "8ea1c897777749583547e805e3e3bf24bddff2885f3de19e3e1776a847fd4867"
+    sha256 cellar: :any,                 arm64_sonoma:  "08753fe7e3b8ca81975eb04eaa3910d6a3525c6b47430116a9c7d32a04187e51"
+    sha256 cellar: :any,                 sonoma:        "1919e315e40ae050ec1a688c2d71bd5fba3be3501204846915703bdd8902281c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0d2efcb8a835e146afcca260147f23fbac7215e3e72328a1180ccb600883c688"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49f82f1154bb8bec4e99421cd0e1bd4dbcc19df47205ffafe46fec56edcb8d84"
+  end
+
+  def install
+    # Disable parallel build due to upstream concurrency issue.
+    # https://github.com/manugarg/pacparser/issues/27
+    ENV.deparallelize
+    ENV["VERSION"] = version
+    Dir.chdir "src"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    # example pacfile taken from upstream sources
+    (testpath/"test.pac").write <<~'EOS'
+      function FindProxyForURL(url, host) {
+
+        if ((isPlainHostName(host) ||
+            dnsDomainIs(host, ".example.edu")) &&
+            !localHostOrDomainIs(host, "www.example.edu"))
+          return "plainhost/.example.edu";
+
+        // Return externaldomain if host matches .*\.externaldomain\.example
+        if (/.*\.externaldomain\.example/.test(host))
+          return "externaldomain";
+
+        // Test if DNS resolving is working as intended
+        if (dnsDomainIs(host, ".google.com") &&
+            isResolvable(host))
+          return "isResolvable";
+
+        // Test if DNS resolving is working as intended
+        if (dnsDomainIs(host, ".notresolvabledomain.invalid") &&
+            !isResolvable(host))
+          return "isNotResolvable";
+
+        if (/^https:\/\/.*$/.test(url))
+          return "secureUrl";
+
+        if (isInNet(myIpAddress(), '10.10.0.0', '255.255.0.0'))
+          return '10.10.0.0';
+
+        if ((typeof(myIpAddressEx) == "function") &&
+            isInNetEx(myIpAddressEx(), '3ffe:8311:ffff/48'))
+          return '3ffe:8311:ffff';
+
+        else
+          return "END-OF-SCRIPT";
+      }
+    EOS
+    # Functional tests from upstream sources
+    test_sets = [
+      {
+        "cmd" => "-c 3ffe:8311:ffff:1:0:0:0:0 -u http://www.example.com",
+        "res" => "3ffe:8311:ffff",
+      },
+      {
+        "cmd" => "-c 0.0.0.0 -u http://www.example.com",
+        "res" => "END-OF-SCRIPT",
+      },
+      {
+        "cmd" => "-u http://host1",
+        "res" => "plainhost/.example.edu",
+      },
+      {
+        "cmd" => "-u http://www1.example.edu",
+        "res" => "plainhost/.example.edu",
+      },
+      {
+        "cmd" => "-u http://manugarg.externaldomain.example",
+        "res" => "externaldomain",
+      },
+      {
+        "cmd" => "-u https://www.google.com",  ## internet
+        "res" => "isResolvable",               ## required
+      },
+      {
+        "cmd" => "-u https://www.notresolvabledomain.invalid",
+        "res" => "isNotResolvable",
+      },
+      {
+        "cmd" => "-u https://www.example.com",
+        "res" => "secureUrl",
+      },
+      {
+        "cmd" => "-c 10.10.100.112 -u http://www.example.com",
+        "res" => "10.10.0.0",
+      },
+    ]
+    # Loop and execute tests
+    test_sets.each do |t|
+      assert_equal t["res"],
+        shell_output("#{bin}/pactester -p #{testpath}/test.pac " +
+          t["cmd"]).strip
+    end
+  end
+end
