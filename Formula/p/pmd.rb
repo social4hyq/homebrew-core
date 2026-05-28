60dc8e88c0ed6624cda8b39f1690ac8c1764de20
hqzing <hqzing@outlook.com>
2026-05-28 20:25:09 +0000

    pmd 7.24.0 (new formula)

---
+++ Formula/p/pmd.rb
@@ -0,0 +1,41 @@
+class Pmd < Formula
+  desc "Source code analyzer for Java, JavaScript, and more"
+  homepage "https://pmd.github.io"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.24.0/pmd-dist-7.24.0-bin.zip"
+  sha256 "110934b36d39c19094d1b77386931978093f238f2c2f1851748822b69c7367ac"
+  license "BSD-4-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "04620155cae27795ad1d6f5930c165e99fa2d1a472150a9a0024920d7e05a7a4"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm Dir["bin/*.bat"]
+    libexec.install Dir["*"]
+    (bin/"pmd").write_env_script libexec/"bin/pmd", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"java/testClass.java").write <<~JAVA
+      public class BrewTestClass {
+        // dummy constant
+        public String SOME_CONST = "foo";
+
+        public boolean doTest () {
+          return true;
+        }
+      }
+    JAVA
+
+    output = shell_output("#{bin}/pmd check -d #{testpath}/java " \
+                          "-R category/java/bestpractices.xml -f json")
+    assert_empty JSON.parse(output)["processingErrors"]
+  end
+end
