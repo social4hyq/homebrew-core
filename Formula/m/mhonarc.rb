5321b4a260741e53c954db4d0d0297651ef3e815
hqzing <hqzing@outlook.com>
2026-05-28 01:42:44 +0000

    mhonarc 2.6.24 (new formula)

---
+++ Formula/m/mhonarc.rb
@@ -0,0 +1,32 @@
+class Mhonarc < Formula
+  desc "Mail-to-HTML converter"
+  homepage "https://www.mhonarc.org/"
+  url "https://cpan.metacpan.org/authors/id/L/LD/LDIDRY/MHonArc-2.6.24.tar.gz"
+  sha256 "457dc7374ee59cb75a0729e51cef2f2c52b48180f739d8fd956ea19882815f33"
+  license "GPL-2.0-or-later"
+  revision 1
+  head "https://github.com/sympa-community/mhonarc.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "d648abb0708b23fccab9dc49db5ced470814ee385962ab662f7209b657fe05e3"
+  end
+
+  depends_on "perl"
+
+  def install
+    # Using Perl's `installprefix` rather than `prefix` allows install.me to use
+    # Homebrew Perl directory structure even if the prefixes are different paths.
+    inreplace "install.me", "$Config{'prefix'}", "$Config{'installprefix'}"
+
+    system "perl", "install.me",
+           "-batch",
+           "-perl", Formula["perl"].opt_bin/"perl",
+           "-prefix", prefix
+
+    bin.install "mhonarc"
+  end
+
+  test do
+    system bin/"mhonarc", "-v"
+  end
+end
