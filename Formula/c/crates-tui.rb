7f4c025d227365f98bc25ee67e93965f4af7372f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 13:43:47 +0800

    crates-tui: update 0.1.25 bottle.

--- Formula/c/crates-tui.rb
+++ Formula/c/crates-tui.rb
@@ -7,14 +7,7 @@ class CratesTui < Formula
   head "https://github.com/ratatui/crates-tui.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7a4d3f4d546495d9eada59e3329b86c0172422e4796be32ca05c01eb3303e6f6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f56fcc14fa7ac18c23012b124c2c1332b496ca0e9ade5a07a6d531a5f7bb4e62"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "30f81c73519341098deba38f9709c99347db6b33612bba1550711e68a45a0501"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3bb365cdc581270ec3b678e116906be8804b8456f9177a9da96c82596c5c4d8a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8a9d22f1366b378bcc6815bcfaaf8757f7411e2ebcbadd8d25fdcaaee58e2162"
-    sha256 cellar: :any_skip_relocation, ventura:       "b2a38ed0df2652636b17333925225751941160871dd6a2f33a41e3d178a92271"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c7d2f55e2a85701ef682ebd95b1e545419d31ce1342eb665b044441309e0f1d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d61432d14ad58729837802eb342892a52a6fda9c600efdea3e25f6f3f4f570d7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "497ae7cc619e3eaa07ae3d355834ee26f9acaf5fbbc88655f0f03c3b3bc23f2c"
   end
 
   depends_on "pkgconf" => :build
