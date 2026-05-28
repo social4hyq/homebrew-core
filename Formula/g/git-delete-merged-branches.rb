edc279df8ac9bd9deb9e8bed17a6ee847b9d754f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 13:07:21 +0800

    git-delete-merged-branches: update 7.5.1 bottle.

--- Formula/g/git-delete-merged-branches.rb
+++ Formula/g/git-delete-merged-branches.rb
@@ -8,7 +8,7 @@ class GitDeleteMergedBranches < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "75fff64274ee6fa957644a73d8b644d976f4864c52388a8d8e68587ddd3257d9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "353687e11a2e8e386d05e2aebda7ccb6635f742e4b83837cb0fb644ce7198eb9"
   end
 
   depends_on "python@3.14"
