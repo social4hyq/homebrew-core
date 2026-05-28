5912f5d949d85bd4b6ae102c06c94ffc1db0dc4f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 12:01:09 +0800

    bacon-ls: update 0.29.0 bottle.

--- Formula/b/bacon-ls.rb
+++ Formula/b/bacon-ls.rb
@@ -7,12 +7,7 @@ class BaconLs < Formula
   head "https://github.com/crisidev/bacon-ls.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d78a9c0a390f4893f4412423bb10828fecec14cd9682d9875ad0b4256353b5e5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "18bdeae5cfb6a925b730cbd25ca2b4b1a641b08f45ba4ee76880de209a15eb19"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8f611eafd253e3cd00703d4f8259635c606df8678bd8462dbf7f9e68f3a5b72d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "770416f2f8abb9e5243de37ec8b027f31852eddf505f26af800cdccaf1ebd086"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5e5eba7e45ffa58a3edda89b8fc287b1cdb88d8a25b357690c7b240ecef188fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7f9f3149741fccf804f00ee8a5be0d50a8861cc4e2bd7186840b48eae779510"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5267398a54f268587db0b0c6cacc7ed827f5d543f15eb19ec49cc4a38d8744a2"
   end
 
   depends_on "rust" => :build
