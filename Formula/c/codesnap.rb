d24615ac155e55e9e1b6719dbc2bd693bf6b51f7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 13:28:45 +0800

    codesnap: update 0.13.1 bottle.

--- Formula/c/codesnap.rb
+++ Formula/c/codesnap.rb
@@ -7,12 +7,7 @@ class Codesnap < Formula
   head "https://github.com/codesnap-rs/codesnap.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b09e81e2e477b4766f7131c2c7cce96c0c4f324ed6b536b8c7f620be8925ed8b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e9984bfb2152fb6cb6d32f5dadcbf192786fe6e6da6a5d1daabebeaab13df014"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d45c584ac455a83be1b596f9a8e063bcbb5300d7ff75d43641737b423b41d376"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5a408a988e5588a6105fe3fda0fc271aaebc86c84f97fbacf8a743f1787b5925"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9bd2da94e62d3648e54e1cb6428f6842e500e46c7a04bcb83c7c1147aaee92d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82461844e1d7743af3f1ed81053a3478c68161d031b6de6cf6715d0340b50579"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "69dbf88b98a2887b810b40c28dfb132dd1cee1295f8a67501509176b77fde565"
   end
 
   depends_on "pkgconf" => :build
