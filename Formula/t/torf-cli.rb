c04b439851fd86c4b59656902c8ab1390ce98b72
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 08:38:55 +0800

    torf-cli: update 5.2.1 bottle.

--- Formula/t/torf-cli.rb
+++ Formula/t/torf-cli.rb
@@ -8,7 +8,7 @@ class TorfCli < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "b7758a90df48547bab1b1e348803e2d94ff4e056db3670f11ef23774f47739f0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a26628dd72afecc9a0f1e57bc6c2687ed4a7eed5028cd90a42832c8abf9bd6f7"
   end
 
   depends_on "python@3.14"
