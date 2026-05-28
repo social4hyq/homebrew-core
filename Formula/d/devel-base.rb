e932b4ec8417a71bb2fc4353e0cc825f53232177
hqzing <hqzing@outlook.com>
2026-05-28 20:24:33 +0800

    devel-base 1.0.1

--- Formula/d/devel-base.rb
+++ Formula/d/devel-base.rb
@@ -2,7 +2,7 @@ class DevelBase < Formula
   desc "Essential build tools collection"
   homepage "https://atomgit.com/Harmonybrew/homebrew-core"
   url "https://atomgit.com/Harmonybrew/homebrew-core.git", revision: "a8784ea451ad819d27a548411b8853fca6de3124"
-  version "1.0.0"
+  version "1.0.1"
   license "BSD-2-Clause"
 
   bottle do
@@ -13,14 +13,14 @@ class DevelBase < Formula
   depends_on "coreutils"
   depends_on "diffutils"
   depends_on "gawk"
+  depends_on "gnu-sed"
   depends_on "gnu-tar"
   depends_on "gpatch"
   depends_on "grep"
   depends_on "gzip"
-
   depends_on "llvm-gcc-compat"
-
   depends_on "make"
+  depends_on "ohos-sdk"
   depends_on "texinfo"
 
   def install
