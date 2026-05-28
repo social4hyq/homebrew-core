ea4aaa1e785a7c8424c23a341b70220cc8b83657
hqzing <hqzing@outlook.com>
2026-05-28 17:20:36 +0000

    mockserver 6.1.0 (new formula)

---
+++ Formula/m/mockserver.rb
@@ -0,0 +1,46 @@
+class Mockserver < Formula
+  desc "Mock HTTP server and proxy"
+  homepage "https://www.mock-server.com/"
+  url "https://search.maven.org/remotecontent?filepath=org/mock-server/mockserver-netty/6.1.0/mockserver-netty-6.1.0-brew-tar.tar"
+  sha256 "dbf1789a74b684841416e20f734adfe772ecac007c362d5776eae29a1b988fa5"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/mock-server/mockserver-netty/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2f861415b192031b7af5f5eceb6339cd7c06084da4b4563a26f5e668efd9bc88"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    inreplace "bin/run_mockserver.sh", "/usr/local", HOMEBREW_PREFIX
+    libexec.install Dir["*"]
+    (bin/"mockserver").write_env_script libexec/"bin/run_mockserver.sh", JAVA_HOME: Formula["openjdk"].opt_prefix
+
+    lib.install_symlink "#{libexec}/lib" => "mockserver"
+
+    mockserver_log = var/"log/mockserver"
+    mockserver_log.mkpath
+
+    libexec.install_symlink mockserver_log => "log"
+  end
+
+  test do
+    port = free_port
+
+    mockserver = spawn bin/"mockserver", "-serverPort", port.to_s
+
+    loop do
+      Utils.popen_read("curl", "-s", "http://localhost:#{port}/status", "-X", "PUT")
+      break if $CHILD_STATUS.exitstatus.zero?
+    end
+
+    system "curl", "-s", "http://localhost:#{port}/stop", "-X", "PUT"
+
+    Process.wait(mockserver)
+  end
+end
