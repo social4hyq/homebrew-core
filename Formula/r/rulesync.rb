b470e7526f767993a6442a2bc31d97b0995f53fa
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 10:47:45 +0800

    rulesync: update 8.21.0 bottle.

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -6,7 +6,7 @@ class Rulesync < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "25af4ea5c577a3f4600ab11633e635a49e5b26ad804a619f3cc05384fe9fa9b8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e4acaecd2ebccf968d4f841297c5a6142fdc460f37e78c0384d28c27b0fd8cc6"
   end
 
   depends_on "node"
