55d21952c3fc86f8e4f2f27da40556ae611c995d
hqzing <hqzing@outlook.com>
2026-05-28 01:33:55 +0000

    urdfdom_headers 3.0.0 (new formula)

---
+++ Formula/u/urdfdom_headers.rb
@@ -0,0 +1,43 @@
+class UrdfdomHeaders < Formula
+  desc "Headers for Unified Robot Description Format (URDF) parsers"
+  homepage "https://wiki.ros.org/urdfdom_headers/"
+  url "https://github.com/ros/urdfdom_headers/archive/refs/tags/3.0.0.tar.gz"
+  sha256 "314b322696cbc64d87c31e859f1d4e9983873c1db2706d13d7c2aa8c1a31119e"
+  license "BSD-3-Clause"
+
+  # Upstream uses Git tags (e.g. `1.0.0`) to indicate a new version. They
+  # created a few releases on GitHub in the past but now they simply use tags.
+  # See: https://github.com/Homebrew/homebrew-core/pull/158963#issuecomment-1879185279
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9e9c205df141a432984ca75973890f3c1a394d6b47c665605ed9db85afaee30a"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :test
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <urdf_model/pose.h>
+      int main() {
+        double quat[4];
+        urdf::Rotation rot;
+        rot.getQuaternion(quat[0], quat[1], quat[2], quat[3]);
+        return 0;
+      }
+    CPP
+
+    system ENV.cxx, shell_output("pkg-config --cflags urdfdom_headers").chomp, "test.cpp", "-std=c++11", "-o", "test"
+    system "./test"
+  end
+end
