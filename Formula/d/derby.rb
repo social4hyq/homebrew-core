ef6038f3ba8d84ae66b7eede9483e0f91662f3d0
hqzing <hqzing@outlook.com>
2026-05-28 18:18:17 +0000

    derby 10.17.1.0 (new formula)

---
+++ Formula/d/derby.rb
@@ -0,0 +1,48 @@
+class Derby < Formula
+  desc "Apache Derby is an embedded relational database running on JVM"
+  homepage "https://db.apache.org/derby/"
+  url "https://www.apache.org/dyn/closer.lua?path=db/derby/db-derby-10.17.1.0/db-derby-10.17.1.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/db/derby/db-derby-10.17.1.0/db-derby-10.17.1.0-bin.tar.gz"
+  sha256 "cbcfe4a0f07aab943cf89978f38d9047a9783233a770c54074bf555a65bedd42"
+  license "Apache-2.0"
+
+  bottle do
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, all: "a4ec775f18dc3de32bd09a237f3ed1eba99aa4b137cf4eedb529232a03e85201"
+  end
+
+  # Project is retired and will have no further releases
+  # See https://db.apache.org/derby/#Derby+Retired
+  deprecate! date: "2026-01-03", because: :unsupported
+  disable! date: "2027-01-03", because: :unsupported
+
+  depends_on "openjdk"
+
+  def install
+    env = Language::Java.java_home_env.merge(DERBY_INSTALL: libexec, DERBY_HOME: libexec)
+    rm_r(Dir["bin/*.bat"])
+    libexec.install %w[lib test index.html LICENSE NOTICE RELEASE-NOTES.html
+                       KEYS docs javadoc demo]
+    bin.install Dir["bin/*"]
+    bin.env_script_all_files libexec/"bin", env
+    (var/"derby").mkpath
+  end
+
+  service do
+    run [opt_bin/"NetworkServerControl", "-h", "127.0.0.1", "start"]
+    keep_alive true
+    working_dir var/"derby"
+  end
+
+  test do
+    assert_match "libexec/lib/derby.jar] #{version}", shell_output(bin/"sysinfo")
+
+    pid = spawn bin/"startNetworkServer"
+    begin
+      sleep 12
+      system bin/"stopNetworkServer"
+    ensure
+      Process.wait(pid)
+    end
+  end
+end
