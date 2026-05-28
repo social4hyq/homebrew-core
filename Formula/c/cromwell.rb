b033452dfa00e84befa4ad198ea94338b011e077
hqzing <hqzing@outlook.com>
2026-05-28 17:19:31 +0000

    cromwell 92 (new formula)

---
+++ Formula/c/cromwell.rb
@@ -0,0 +1,74 @@
+class Cromwell < Formula
+  desc "Workflow Execution Engine using Workflow Description Language"
+  homepage "https://github.com/broadinstitute/cromwell"
+  url "https://github.com/broadinstitute/cromwell/releases/download/92/cromwell-92.jar"
+  sha256 "e0e3a050d4124e81369a79059e5774142b2f06bd89df4a0b035f559db85cedf5"
+  license "BSD-3-Clause"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "65f1fe5fc374b0cce66e8ab04ad5b768549e005112207ae41d79b633457bd97b"
+  end
+
+  head do
+    url "https://github.com/broadinstitute/cromwell.git", branch: "develop"
+    depends_on "sbt" => :build
+  end
+
+  depends_on "openjdk"
+
+  resource "womtool" do
+    url "https://github.com/broadinstitute/cromwell/releases/download/92/womtool-92.jar"
+    sha256 "99cd3675c48696470f4d4e8b397fc613d7b342eb2ef2fa96f86db114bd9ed5f8"
+
+    livecheck do
+      formula :parent
+    end
+  end
+
+  def install
+    odie "womtool resource needs to be updated" if build.stable? && version != resource("womtool").version
+
+    if build.head?
+      system "sbt", "assembly"
+      libexec.install Dir["server/target/scala-*/cromwell-*.jar"][0] => "cromwell.jar"
+      libexec.install Dir["womtool/target/scala-*/womtool-*.jar"][0] => "womtool.jar"
+    else
+      libexec.install "cromwell-#{version}.jar" => "cromwell.jar"
+      resource("womtool").stage do
+        libexec.install "womtool-#{version}.jar" => "womtool.jar"
+      end
+    end
+
+    bin.write_jar_script libexec/"cromwell.jar", "cromwell", "$JAVA_OPTS"
+    bin.write_jar_script libexec/"womtool.jar", "womtool"
+  end
+
+  test do
+    (testpath/"hello.wdl").write <<~EOS
+      task hello {
+        String name
+
+        command {
+          echo 'hello ${name}!'
+        }
+        output {
+          File response = stdout()
+        }
+      }
+
+      workflow test {
+        call hello
+      }
+    EOS
+
+    (testpath/"hello.json").write <<~JSON
+      {
+        "test.hello.name": "world"
+      }
+    JSON
+
+    result = shell_output("#{bin}/cromwell run --inputs hello.json hello.wdl")
+
+    assert_match "test.hello.response", result
+  end
+end
