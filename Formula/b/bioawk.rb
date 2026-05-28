ca004c8d327606219a930565b30278fa6ff4e44a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 13:04:08 +0800

    bioawk: update 1.0 bottle.

--- Formula/b/bioawk.rb
+++ Formula/b/bioawk.rb
@@ -6,13 +6,7 @@ class Bioawk < Formula
   license "HPND"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dc8c77af3f46588cab03f40541d48dfa09fe06115be3be5c42495213ad7fa003"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f4f29845cf2a16730b9ade42596e972a8ac74b0eee296bed5bede33a08903ae0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "843653fc5797fdf0a2ba5203dab1f73261a05f2f43e6fff2ba123726cb85aa7d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6eb824cbf1be1f383777e3cfc835929ccf9e872aff310edad8cd279a0e503550"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cf7bf412ddbdf9a37d1e63c4227290886366f91d3a97b1a574c2577a93a99380"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e0a03da1e2774fcef306bcf11edac499e446d2fc9bd413078ce56f38ed1ad52"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b3a45c95e121fc85bec5f7932e066909bcf5268667913bc901587130406d9a8a"
   end
 
   uses_from_macos "bison" => :build
