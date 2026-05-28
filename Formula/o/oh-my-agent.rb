09b509312c8f26c2d7426319b2ae51f42a0eb271
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 07:41:28 +0800

    oh-my-agent 8.12.0

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -1,8 +1,8 @@
 class OhMyAgent < Formula
   desc "Portable multi-agent harness for .agents-based skills and workflows"
   homepage "https://github.com/first-fluke/oh-my-agent"
-  url "https://registry.npmjs.org/oh-my-agent/-/oh-my-agent-8.11.0.tgz"
-  sha256 "f47b38aff586fb01da4aa77caeb66c471f0673d46e48b165c53a7b724d8e3346"
+  url "https://registry.npmjs.org/oh-my-agent/-/oh-my-agent-8.12.0.tgz"
+  sha256 "287ad0b47385643df9a99cc7e7db092a89bc8fe1ce202a3326024096ffec66a9"
   license "MIT"
 
   bottle do
