ebe414e00996d565c3fca1c9dbb804ee7c497c53
hqzing <hqzing@outlook.com>
2026-05-28 02:17:02 +0000

    tartufo 6.0.0 (new formula)

---
+++ Formula/t/tartufo.rb
@@ -0,0 +1,66 @@
+class Tartufo < Formula
+  include Language::Python::Virtualenv
+
+  desc "Searches through git repositories for high entropy strings and secrets"
+  homepage "https://tartufo.readthedocs.io/en/stable/"
+  # TODO: Switch to PyPI when upstream fixes workflow for 2FA
+  url "https://github.com/godaddy/tartufo/archive/refs/tags/v6.0.0.tar.gz"
+  sha256 "ba84bb6192a3647a0dd2f8b4c08c7aff46e8d5bc742e13ee1714477ae8ad7787"
+  license "GPL-2.0-only"
+  revision 2
+  head "https://github.com/godaddy/tartufo.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3135dd3e81b173cece071490037581416f3d532be0ddfd62aac46d862673073d"
+  end
+
+  depends_on "pygit2" => :no_linkage
+  depends_on "python@3.14"
+
+  uses_from_macos "libffi"
+
+  pypi_packages exclude_packages: "pygit2"
+
+  resource "cached-property" do
+    url "https://files.pythonhosted.org/packages/61/2c/d21c1c23c2895c091fa7a91a54b6872098fea913526932d21902088a7c41/cached-property-1.5.2.tar.gz"
+    sha256 "9fa5755838eecbb2d234c3aa390bd80fbd3ac6b6869109bfc1b499f7bd89a130"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/bb/63/f9e1ea081ce35720d8b92acde70daaedace594dc93b693c869e0d5910718/click-8.3.3.tar.gz"
+    sha256 "398329ad4837b2ff7cbe1dd166a4c0f8900c3ca3a218de04466f38f6497f18a2"
+  end
+
+  resource "gitdb" do
+    url "https://files.pythonhosted.org/packages/72/94/63b0fc47eb32792c7ba1fe1b694daec9a63620db1e313033d18140c2320a/gitdb-4.0.12.tar.gz"
+    sha256 "5ef71f855d191a3326fcfbc0d5da835f26b13fbcba60c32c21091c349ffdb571"
+  end
+
+  resource "gitpython" do
+    url "https://files.pythonhosted.org/packages/33/f6/354ae6491228b5eb40e10d89c4d13c651fe1cf7556e35ebdded50cff57ce/gitpython-3.1.50.tar.gz"
+    sha256 "80da2d12504d52e1f998772dc5baf6e553f8d2fcfe1fcc226c9d9a2ee3372dcc"
+  end
+
+  resource "smmap" do
+    url "https://files.pythonhosted.org/packages/1f/ea/49c993d6dfdd7338c9b1000a0f36817ed7ec84577ae2e52f890d1a4ff909/smmap-5.0.3.tar.gz"
+    sha256 "4d9debb8b99007ae47165abc08670bd74cb74b5227dda7f643eccc4e9eb5642c"
+  end
+
+  resource "tomlkit" do
+    url "https://files.pythonhosted.org/packages/cc/18/0bbf3884e9eaa38819ebe46a7bd25dcd56b67434402b66a58c4b8e552575/tomlkit-0.13.3.tar.gz"
+    sha256 "430cf247ee57df2b94ee3fbe588e71d362a941ebb545dec29b53961d61add2a1"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"tartufo", shell_parameter_format: :click)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tartufo --version")
+
+    output = shell_output("#{bin}/tartufo scan-remote-repo https://github.com/godaddy/tartufo.git")
+    assert_match "All clear. No secrets detected.", output
+  end
+end
