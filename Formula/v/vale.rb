2d3f56b92fcd27e801b8a02a0dcc96eb5836755c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 12:18:33 +0800

    vale: update 3.14.2 bottle.

--- Formula/v/vale.rb
+++ Formula/v/vale.rb
@@ -6,12 +6,7 @@ class Vale < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b7a7a6e477944d5105346e8c8c45bf4ca87b65efd7dae5567d10936b0f59e15b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d02c37de960c050f190f470e0ee0f7f4bbae94ab1e61f27afdfb29c2bcd10731"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21ca221fc848aec0c12951cf55e75c70f3ce067bd003175a299ec0f105ccd472"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bea63409ae93b9555840f0f075e0cdf761361d747540d2f789eb73ab56564df5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6f249ab213dc746ba005a124c9c4b664f650018a7de3e6d337423dba892c188e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a241e0dac5c487867d87b5a806348295bab57e0f99e97f26f1cd88b03dd2a92d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "81765b51f54f3c310ad54202ed2d7013bf645c925d369b92af1084feb1927a10"
   end
 
   depends_on "go" => :build
