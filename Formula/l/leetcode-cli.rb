9e6d41e25227dc0486dc69aadfb1f1901a3a4200
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-28 08:42:05 +0800

    leetcode-cli: update 0.5.0 bottle.

--- Formula/l/leetcode-cli.rb
+++ Formula/l/leetcode-cli.rb
@@ -7,12 +7,7 @@ class LeetcodeCli < Formula
   head "https://github.com/clearloop/leetcode-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "2714994b45df5976c2f02d57e02a860397738ef3c1b33bae6edb32955636b241"
-    sha256 cellar: :any,                 arm64_sequoia: "5c17215533eadbc515621812fdbce284b89ce60e365dba38672d79a197d40302"
-    sha256 cellar: :any,                 arm64_sonoma:  "391a1860fad787cfcbef1fa1c0dc04335913621944da57999e89957955c67cf6"
-    sha256 cellar: :any,                 sonoma:        "655b0ad5fb1c9a7b7cdc9b09c42193fa11a10dda1008913ccb748bd6d24edf08"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a6da7b9dd64506d8a577505d4b2716ce342bd0fdfd2b843d92996f35c7299b54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af6826c657dda9c1c44bcfe1f3c8b029168867e994a149cc4353e1fb849aec58"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9d8103af720cf5e158d9b4a252630061bc9a0b93311f34a5bed7dfe26eee3c17"
   end
 
   depends_on "pkgconf" => :build
