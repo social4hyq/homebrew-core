4456a6fd3debf20ba398d91b304adc449107fb78
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 17:02:06 +0800

    dvanalyzer: update 1.4.2 bottle.

--- Formula/d/dvanalyzer.rb
+++ Formula/d/dvanalyzer.rb
@@ -11,13 +11,7 @@ class Dvanalyzer < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e8bb414e0719a71fcc95ea8c1351f983fa7df1d0a3c350182c9be47b8064fefc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92eda7b054009d95d846a8a4cebf111bd26eb9b4340d5548489ed4645b6fd667"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ea8f647c562fd62ed7acc15cb685dc8445b90168ddbb6013199e8feb8337738"
-    sha256 cellar: :any_skip_relocation, sonoma:        "309bb248ed85a89a2a03562dabb9b3e97c2c24b3f0a81dd8f51cf2511c1e380a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f25ff17a0c20c42a7b826e116d5c5687354980a75c403026c2b732f27180b597"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a381c4467638323d1c9ab03fca279140ee97c76c99a3894c59b10757bfe1c711"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "682598856913a028acba27734b0920da8f292cda3cc96dad13cf96afa75fceec"
   end
 
   on_linux do
