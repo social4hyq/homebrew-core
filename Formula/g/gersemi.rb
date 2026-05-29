e6e17bc602707a4f5caab96b42685fe2c0942b4f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 06:54:02 +0800

    gersemi 0.27.7

--- Formula/g/gersemi.rb
+++ Formula/g/gersemi.rb
@@ -3,8 +3,8 @@ class Gersemi < Formula
 
   desc "Formatter to make your CMake code the real treasure"
   homepage "https://github.com/BlankSpruce/gersemi"
-  url "https://files.pythonhosted.org/packages/12/57/5423766eddb6f73a56e57e3dc09f863c8298493e2f7a7654d1f595fb3d17/gersemi-0.27.6.tar.gz"
-  sha256 "5cd2d0aa57638384e9967994953d29fd43009541f2e6010139a48c98260eb055"
+  url "https://files.pythonhosted.org/packages/01/45/93f06bcc8fd631e50875ae70c542de7a46e6a06b9e19f39fb903b61e8ac8/gersemi-0.27.7.tar.gz"
+  sha256 "f598d62bd2bee0b6cbfc4e71f82c86f768ab31d135a312585fabbd7e57b64e09"
   license "MPL-2.0"
 
   bottle do
@@ -21,8 +21,8 @@ class Gersemi < Formula
   end
 
   resource "platformdirs" do
-    url "https://files.pythonhosted.org/packages/9f/4a/0883b8e3802965322523f0b200ecf33d31f10991d0401162f4b23c698b42/platformdirs-4.9.6.tar.gz"
-    sha256 "3bfa75b0ad0db84096ae777218481852c0ebc6c727b3168c1b9e0118e458cf0a"
+    url "https://files.pythonhosted.org/packages/d7/47/e4501f49c178ae1d9f4a75073fda4204f52647993f075a9db4d14930e0c5/platformdirs-4.10.0.tar.gz"
+    sha256 "31e761a6a0ca04faf7353ea759bdba55652be214725111e5aac52dfa29d4bef7"
   end
 
   resource "pyyaml" do
