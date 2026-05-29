8b4b0cc635030280743bf8186ecb938e5b103c0c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 16:45:24 +0800

    ggh: update 0.1.5 bottle.

--- Formula/g/ggh.rb
+++ Formula/g/ggh.rb
@@ -7,12 +7,7 @@ class Ggh < Formula
   head "https://github.com/byawitz/ggh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f19e7aab53fda165524a48499bc2426204ecbca9012a1f860c5ca6f624f29338"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f19e7aab53fda165524a48499bc2426204ecbca9012a1f860c5ca6f624f29338"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f19e7aab53fda165524a48499bc2426204ecbca9012a1f860c5ca6f624f29338"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f461df86eb13b905275e1329d1938686b363b4ad2521b643109ebd2338fcccfc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3b0cd2a8832adfb19727d4a81750fdfcd1e1268fb3ca4804b4e616d996f0add5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d17c99fe36147ed032691e97d9fdc12073ff980a93a5a932fbfe07e315babb65"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f0c6f788b3a096a7b69283c19cc32e79825e3f6aae1d55ecabbfae8a4f2dbf5b"
   end
 
   depends_on "go" => :build
