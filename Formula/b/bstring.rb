fed6c284863a3127a21d000455dd851b9478f854
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 00:05:04 +0800

    bstring: update 1.1.0 bottle.

--- Formula/b/bstring.rb
+++ Formula/b/bstring.rb
@@ -7,12 +7,7 @@ class Bstring < Formula
   head "https://github.com/msteinert/bstring.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "66f545fd557342e3560304aa78e39175200a6f273955bab4fafe33388e1708aa"
-    sha256 cellar: :any,                 arm64_sequoia: "44c469620eaedcf00a73624f1dfcd9b2e9938cb996154d968fa10839633e71ff"
-    sha256 cellar: :any,                 arm64_sonoma:  "0c44ce068f5f426899aaf2015c23a35550c28188e18e1d13266547491cce4e8b"
-    sha256 cellar: :any,                 sonoma:        "2f842371020537e4110b9fe388ed73ad7488435ecb04d75aa390c75f0da664d4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6c13cb49f5a8255a1c8adf436297f6cbd5e6e49890f45d772ad079f9a1c97e23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9db207735a893a428a7e470852c19e223eb24a208b53b14a13c0e4a4061c598a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "08381d08ce19a4db3d91a1fd8869507a9fb43730ec5f2ca5cfb6e114beb86452"
   end
 
   depends_on "meson" => :build
