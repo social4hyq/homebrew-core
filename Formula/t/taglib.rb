94782f70a2ee44c88b8446259416c7ea1cffdd77
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 13:16:09 +0800

    taglib: update 2.3 bottle.

--- Formula/t/taglib.rb
+++ Formula/t/taglib.rb
@@ -8,12 +8,7 @@ class Taglib < Formula
   head "https://github.com/taglib/taglib.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a928c47709f2870b87d973f74358edf18816d8263304f5e4e9911898162107a3"
-    sha256 cellar: :any,                 arm64_sequoia: "65e5ecc716029c9d1eae11fb643a18df772980a01a09cad434af97bcba955bdb"
-    sha256 cellar: :any,                 arm64_sonoma:  "6b2e26e84d1f58b7a04b40e33bbcdab30f3459779f9229c248f196ca8e94f3d7"
-    sha256 cellar: :any,                 sonoma:        "eb9aefea72d75bee73fc35661433d9f2ebd20f408f70c91011b81c3ccb140f96"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b6c5c4793004c5c00d0e1267fd3ac8258a9a136cbfe7b471942543733da76d1e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "051fc7c416146bc92af62e067e517c3ee37b8834f7a7a8ebb66d1f6dd98f11fd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "49c7c80d91961b0116eaba7465246d868ab2574beba81510d03c4620f2e75feb"
   end
 
   depends_on "cmake" => :build
