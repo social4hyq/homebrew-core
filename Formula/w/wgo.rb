64db8d4d4109b7371331728b3584e3eb95edc313
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 12:48:12 +0800

    wgo: update 0.6.4 bottle.

--- Formula/w/wgo.rb
+++ Formula/w/wgo.rb
@@ -6,12 +6,7 @@ class Wgo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b2b92e968a82f99328678374d06ce7596cfee234d84fd2f0f8f7abe3b7fb6086"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b2b92e968a82f99328678374d06ce7596cfee234d84fd2f0f8f7abe3b7fb6086"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b2b92e968a82f99328678374d06ce7596cfee234d84fd2f0f8f7abe3b7fb6086"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b650c2c0ec76ad70ac89a5975fc1837a9577f9c02aa0b619c96d0b08c6b51241"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e1811a14743469a2c1ffa8bd0026d0002a1f9a4fa204b17f3f7d049a98047a15"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a37ea3c23353f2300673c562fd2a69307bc0a71510c696ac973509138b12889"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dfea833dabb2ae5a1f05acaafd5417b51f44134502f7ae361cb5f1ffc86c7612"
   end
 
   depends_on "go" => :build
