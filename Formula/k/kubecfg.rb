3b424a805c950c0e72def0caf75d10bd6ca19a64
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 13:00:07 +0800

    kubecfg: update 0.36.0 bottle.

--- Formula/k/kubecfg.rb
+++ Formula/k/kubecfg.rb
@@ -7,14 +7,7 @@ class Kubecfg < Formula
   head "https://github.com/kubecfg/kubecfg.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dafc40beef50da4e6258e2650d4e74d9038b8b47ac36d607937c2ae468d1e682"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5d7373d5b92e6d7fa40d26f346278f9946d7ddf773c8312ca49b1e13d06a85df"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "895b4c9f01632ae07409dee040f42fb73ee784a6726cdfbcf22bced5beeb3718"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0910ce4d7d1fe0cedfa2c3947b21e1b53a88a863ccbb97e42dafb05abccc12f2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "afcc53d1d2bdae49341594f9eaf3741194d1ba7b918a4a65aa21ef4ba39e5110"
-    sha256 cellar: :any_skip_relocation, ventura:       "f693e55e6ca8a34c770b67807e92969817a2a4ee015c84d07057285779d57249"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "03813301e8958ac9dfb657e548d38d919609aef21593df3bcc9917adccfa27bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b82e88d3134489324a448c33bed9315a57878b9aab5b2508ce678a6e2be31a5a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "df091f357bb81438c689c803ac0fbd4219d0c4f6d74beb88e6f8bd69bfd7fe0c"
   end
 
   depends_on "go" => :build
