7661857008ab8c69f1940ee822464ba24be65db8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 13:09:31 +0800

    charm: update 2.5.2 bottle.

--- Formula/c/charm.rb
+++ Formula/c/charm.rb
@@ -7,15 +7,7 @@ class Charm < Formula
   head "https://github.com/juju/charmstore-client.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "23c05ff713871756c9636e01d7de20645150e6180d9426f8fe33a3f4ff0e6885"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e2ddeaf1bab0b233267484c6bdac331a9113af93d5819e6f48ef7642b4696dc1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e2ddeaf1bab0b233267484c6bdac331a9113af93d5819e6f48ef7642b4696dc1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e2ddeaf1bab0b233267484c6bdac331a9113af93d5819e6f48ef7642b4696dc1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4a50613b56ac6dbd9bd7c8def6f03899188f3a4a0992098e75e6d49493b4cd19"
-    sha256 cellar: :any_skip_relocation, ventura:       "4a50613b56ac6dbd9bd7c8def6f03899188f3a4a0992098e75e6d49493b4cd19"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "60e259f6541281a54f94e6b9dea178bacc9d348330e7b781441995f35e97eb73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "393502f468997806b663a548026487a0cc7bdceec9bfce3bfa6cccba45551d59"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c6a82bb722aafe20f56c5bc197a4bf305faff1ac477a9b2af98b94000c7c32e3"
   end
 
   depends_on "breezy" => :build
