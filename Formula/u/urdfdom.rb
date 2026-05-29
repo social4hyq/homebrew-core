47e0722cfb79b7c40d6f5c839423e6a08703683c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 11:29:27 +0800

    urdfdom: update 6.0.0 bottle.

--- Formula/u/urdfdom.rb
+++ Formula/u/urdfdom.rb
@@ -15,12 +15,7 @@ class Urdfdom < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "effa824c55c30479584093c942e6dced50df62e50f5c231ea255bd13a33e3f07"
-    sha256 cellar: :any,                 arm64_sequoia: "04b0418753b6c2c8ee5f07804984c95e1578feb2c34d9ef069e5ddd6e92044e3"
-    sha256 cellar: :any,                 arm64_sonoma:  "e5a5f49411905286f36a609b362234ee2facababd82a0e74bb85b6e7e4978252"
-    sha256 cellar: :any,                 sonoma:        "b7d0acc7b93e4b398c0ab501a33cc05ba41fb14180aea6ff013ada12ef9202bd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "11a9f9031a71408e26083dd7b3dfaeb44484d18329f14ca4267e297359d9aac0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "519e798be04e44dd8e8c118ce8b4cdc8a2a60882c2d801bf1253720a290031f8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a6b9d773fe166a3037bd940f5daef48cf16eee87573fd79f8452223ea6c2eb5f"
   end
 
   depends_on "cmake" => :build
