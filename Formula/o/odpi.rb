7dfbf048c684ef4a6e8828225cfda3de0342609d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 12:12:20 +0800

    odpi: update 6.0.0 bottle.

--- Formula/o/odpi.rb
+++ Formula/o/odpi.rb
@@ -6,12 +6,7 @@ class Odpi < Formula
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "bb1fe7b315e2159a2ec658ebcb255466d8e7934a9334e9d3f5137f3c334eabb5"
-    sha256 cellar: :any,                 arm64_sequoia: "0b944ae9da22534f21126f7eca817ee4baac3aa17454f1722b3e25febd5969ae"
-    sha256 cellar: :any,                 arm64_sonoma:  "d4831e62bddc6abcc76ce7680f9e3a2e3bbbc133ea4f76c1bbb879a14b8dd1d9"
-    sha256 cellar: :any,                 sonoma:        "1b1fe28f3a2bc19f1941dbf6098c7681d0deaa9615257f71dd8f452c40f96b2e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4fb4c98302c9677a564272bf9e210ebd78236944076c502c8b3e6aad38b101be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acb3d83d48ffe23b73c40800c6dda24994dabb7410447e79810e753afcbf156d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bf5d10437a22fd6c1c20434b6b4697c182d21627d27375dfba1c373187efa54a"
   end
 
   def install
