02c91be475b1dc80915f300e0723670b4f0dc54c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 15:25:38 +0800

    docker-agent: update 1.69.0 bottle.

--- Formula/d/docker-agent.rb
+++ Formula/d/docker-agent.rb
@@ -7,7 +7,7 @@ class DockerAgent < Formula
   head "https://github.com/docker/docker-agent.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "997d11e651af273dbdb05dcf7f2fe34dd102851d9f6c32ce7949412be1997774"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dd5440d7fe3e6618dd78a6ae52ec913bc051e2cfdaa7b7132491fa91fa17a7ca"
   end
 
   depends_on "go" => :build
