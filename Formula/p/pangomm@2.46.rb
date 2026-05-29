01ea37419360d6de7d4cca925a61b8459e2a98fa
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 18:24:42 +0800

    pangomm@2.46: update 2.46.4 bottle.

--- Formula/p/pangomm@2.46.rb
+++ Formula/p/pangomm@2.46.rb
@@ -11,15 +11,7 @@ class PangommAT246 < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_tahoe:   "59acdc333ff48dcaa839724326a109208097bf5395a4a6f932f094e2479f50f4"
-    sha256 cellar: :any, arm64_sequoia: "c5c43ab69df3ee85326bb0abf15fd4057a46ce3900015be64abb8c8573d3202e"
-    sha256 cellar: :any, arm64_sonoma:  "6468295dc13b7795464c8acbeb4d790c15b33e6431ff71401c05929cf2f9f3ff"
-    sha256 cellar: :any, arm64_ventura: "b323db7914c80c804a6804b16cb58c8cb9a11d38ce52826b1cc469fa8dfd2510"
-    sha256 cellar: :any, sonoma:        "ee486c1037b89986dec5c26f870d092f4510ba0e121dbb96525fab6a61d358f7"
-    sha256 cellar: :any, ventura:       "1fc51b4f1d0ffde9fffb314a586bcf812fe84496c2a5c56f9fa362cfa91e9cf2"
-    sha256               arm64_linux:   "83a7dc15c14d9031b26bb31705692823f7579ee94d962eb0e6e48952f7c50ba9"
-    sha256               x86_64_linux:  "6b6a26c7bf0e1ba43a9a87dcf9c5b669da81070f282c6b26a70204207713bde3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "52a0334d037a645a14a9f25104bf17ac26df9a4bd76282765bacb3ce152d4b37"
   end
 
   depends_on "meson" => :build
