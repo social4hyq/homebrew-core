0c8ded339ed08b603003d140afc9b1de7ba9dbec
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 13:05:13 +0800

    addlicense: update 1.2.0 bottle.

--- Formula/a/addlicense.rb
+++ Formula/a/addlicense.rb
@@ -7,14 +7,7 @@ class Addlicense < Formula
   head "https://github.com/google/addlicense.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d805d6f3d40e1004189f3ac86a50250f2a2c7eef920127565c59f646f703ed30"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d805d6f3d40e1004189f3ac86a50250f2a2c7eef920127565c59f646f703ed30"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d805d6f3d40e1004189f3ac86a50250f2a2c7eef920127565c59f646f703ed30"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d805d6f3d40e1004189f3ac86a50250f2a2c7eef920127565c59f646f703ed30"
-    sha256 cellar: :any_skip_relocation, sonoma:        "feee4e9cf952c2d3bc600e044b275c059450a897543c033c7bae0961020175fb"
-    sha256 cellar: :any_skip_relocation, ventura:       "feee4e9cf952c2d3bc600e044b275c059450a897543c033c7bae0961020175fb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "146bce5af0963bcfad9a958fcb92b082c1ae4c95971f09a43f7dffecbc83ca0f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8c37cd4c9c5b2dcb5fc8091b30900e71216d0d7fd4fe6d562d10b2d1a38fd46"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cf61c2115a50cdb8cbe0c547b352826de7bd70033bfb5d299079f8571bfd0146"
   end
 
   depends_on "go" => :build
