6f832749e3c7594086d1c3ed2798d332a516fd68
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 12:04:10 +0800

    ic-wasm: update 0.9.11 bottle.

--- Formula/i/ic-wasm.rb
+++ Formula/i/ic-wasm.rb
@@ -6,12 +6,7 @@ class IcWasm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1f0f9431b78c9425870ccd321f68d572bf229c7878bec10141748683840eef1a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "975a009d2854cdd961491c4043b3a46290e7a336a301bef940fbcfad8c6dd14f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37b6f1e4c8291331a9496d493278edd1721a5cdc2919f765e5ff40ae3025b329"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4ec91a5d740423514e6a4e0bde2e68ff70cccecdf07701da43edbe078ba7b022"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "39d0a2cad637f4eff061b84ccdd7e3fc70827e29be8abd83f7c3f2ae202f89a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c54ab40750cdaec9ef7f02cc2053b8cfad9775f0c924668686803171f8ef567"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d36b17cd359549b803f63d802eeeb55bc51d3c797379415ef73473a01b0862bd"
   end
 
   depends_on "rust" => :build
