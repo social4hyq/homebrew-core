d98c3e8f2c309ba8a0f666cd9d046b60781289cc
hqzing <hqzing@outlook.com>
2026-05-29 17:20:01 +0000

    functionalplus 0.2.28 (new formula)

---
+++ Formula/f/functionalplus.rb
@@ -0,0 +1,34 @@
+class Functionalplus < Formula
+  desc "Functional Programming Library for C++"
+  homepage "https://github.com/Dobiasd/FunctionalPlus"
+  url "https://github.com/Dobiasd/FunctionalPlus/archive/refs/tags/v0.2.28.tar.gz"
+  sha256 "8864a3e9bebde6ebed71b49ac2a036cedf9ae0f02ce758bc28c21e6a2ae15803"
+  license "BSL-1.0"
+  head "https://github.com/Dobiasd/FunctionalPlus.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "99a0a032e6e1a98799d695b1ed87a3f3152807c0d33bc345fd19dfab92a8348d"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <fplus/fplus.hpp>
+      #include <iostream>
+      int main() {
+        std::list<std::string> things = {"same old", "same old"};
+        if (fplus::all_the_same(things))
+          std::cout << "All things being equal." << std::endl;
+      }
+    CPP
+    system ENV.cxx, "-std=c++14", "test.cpp", "-I#{include}", "-o", "test"
+    assert_match "All things being equal.", shell_output("./test")
+  end
+end
