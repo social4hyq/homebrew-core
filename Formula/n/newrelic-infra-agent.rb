cd6f8f4878fd1a7f7d8067f5c9c62a568edc6985
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 16:30:56 +0800

    newrelic-infra-agent: update 1.76.0 bottle.

--- Formula/n/newrelic-infra-agent.rb
+++ Formula/n/newrelic-infra-agent.rb
@@ -15,12 +15,7 @@ class NewrelicInfraAgent < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "298a7b0033602caf972049352878086f4f2e8d4256add526116574482d843883"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f9d55eff1bc90597d795b74537fe187b1f7326f1ee6b2aee9e5bc151f0d06586"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "561d29814ced35e855f370e36e7c9c3cc2f0bc5d43ffc41634823b0782479c39"
-    sha256 cellar: :any_skip_relocation, sonoma:        "60e097400ac0697402b7a366dc811bd02aa7d86fbe2fc09aa338c4f1c30822e4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a14312803259f0bd9c4e53f8d7a3382a556b85da80f7175443ea4c51f16aa144"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "600caa71514b50035b38e6d228a07af0077be567cde98ba8e9e7df04cb407736"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d8aeba5c1db170c16ad7fdfca13ddda1d8ddae5a70f9b9f0858358feaa74a6b9"
   end
 
   depends_on "go" => :build
