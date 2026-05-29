0dafb0d20a3f5dd3d636bb89267a6652ff3613ec
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 18:51:54 +0800

    spot: update 2.15.1 bottle.

--- Formula/s/spot.rb
+++ Formula/s/spot.rb
@@ -11,12 +11,7 @@ class Spot < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e010d364f9d17297ee1e337e93b93668170a84afcfdb160f9e87aabbeef528fb"
-    sha256 cellar: :any,                 arm64_sequoia: "1427e29e5eb6a4a0cfb3cec84d6ebcd15c5569bc905b4641950317cf0e08bd66"
-    sha256 cellar: :any,                 arm64_sonoma:  "0b1ca9fa345ef9f3e5aa2e8ccf0d71d4ff248c4ab35e8f7beaa8ecf7b0d166e1"
-    sha256 cellar: :any,                 sonoma:        "ab85af1ccfea03b126c66a0f0ae1f42c03b095ebf32f2d10898867ba0e4d6cb0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aae9243c82ee4ca4cbdd565e8c458e1b265d1892282b511792c007f76ed17996"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4754a6b50687310303b786546b8d5937ba2274f96ebe4638b8fd992e6c90796d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "be9d2388c97836f126272ba4c56275e5abe2e30c2d258fea6a49de16be615ee5"
   end
 
   depends_on "python@3.14" => :build
