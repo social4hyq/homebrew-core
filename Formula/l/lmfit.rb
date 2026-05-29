dc8ca2cc2770368f0d06640d63758a7bea88c4ed
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 11:53:56 +0800

    lmfit: update 10.0 bottle.

--- Formula/l/lmfit.rb
+++ Formula/l/lmfit.rb
@@ -6,12 +6,7 @@ class Lmfit < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e30e6afe8c7246d88c2ecc8cfd62cc3c326e035a8ea67fd2546960d23d476f55"
-    sha256 cellar: :any,                 arm64_sequoia: "6d995c18ee8647862cc3ef8f49a89b3260b74f8b0162b27ea63eb965adcee02e"
-    sha256 cellar: :any,                 arm64_sonoma:  "b540071bc480f6f445d956611bed6d3818f41de82a2e02e0337cc0322780adcc"
-    sha256 cellar: :any,                 sonoma:        "9ba0e39ce1f606cf8ef93827b70ace200f5847094d02c2001b930c4baacc6582"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "39da5cc940190e897a5b4cd90787d8cf75c68ddba48a02936b3f2e214f049883"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97ced0fb2d37329984bd28388f20b26c0633bbd79c7f53dfa38201678f730233"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "af53b32779bc6b482d9dac7b9d46cf55f8d22a4486c532af299e86fba17b8402"
   end
 
   depends_on "cmake" => :build
