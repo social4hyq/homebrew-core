1a9d717ba858132aaf64deea06bed351bfb96003
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 11:47:08 +0800

    aws-c-io: update 0.26.3 bottle.

--- Formula/a/aws-c-io.rb
+++ Formula/a/aws-c-io.rb
@@ -7,12 +7,7 @@ class AwsCIo < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "f96cf4e4319785524c71a88e1cb2f1d065bf5f4b0111c77887115777fbd8dc38"
-    sha256 cellar: :any,                 arm64_sequoia: "3fa6c3a84abe7104e2161f822f6a653ba5b825c4d6f69ac829e61d2dd26257fd"
-    sha256 cellar: :any,                 arm64_sonoma:  "4daec1224842c5e4cf4406c8ed9a04d331533e1107e0d192bde1f370035b7109"
-    sha256 cellar: :any,                 sonoma:        "a2076fe1abd39e628ea7e796d1fea5595585e52ffa7e0884a5ebee3b419e599f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4a16bcbe7b75b407602af6e23e84bcb9cbb96d6780d0f7c9642fcfbb8957bbc3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c2b99161cba1c51c6562c0959ede8b4a9d2b468da23dc672aa1428c0ae5ce5a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7c87b5dda247a1ece15aa81030ee882fb6ba8a4f9ef4296c6fc30587889f6395"
   end
 
   depends_on "cmake" => :build
