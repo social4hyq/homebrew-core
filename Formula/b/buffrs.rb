459be56cfa5aecf39c62ca9fa7d1db9537c10885
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 16:40:26 +0800

    buffrs: update 0.13.2 bottle.

--- Formula/b/buffrs.rb
+++ Formula/b/buffrs.rb
@@ -7,12 +7,7 @@ class Buffrs < Formula
   head "https://github.com/helsing-ai/buffrs.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3dec1a6a2139b11adf776c1b97ac26645b77ab005dffb39167b56a29c874ebf5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b0366ceef4f160e185bcfd36de79b6458a9c98a72c0c7bdcf94cc9dd2d100574"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9dd27b3e1650e250b2a4408deb4a5f1d523f145c64d10e626583f0c4967100ea"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ce90bdc2ce5c03107741108b0c6361cc53d5272c034284481551e9be06d9ce21"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5900a11e0c8d59e893af496fd20500a75c004b157268d4d1ddea55cb66abc96d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ef8b802643975acde7088877eac150257b5c5c5ef89f788a5ab9e13593ef511"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c77cca3e7d5c192b2c8f6cf18c9c6816c1d85e5506efb820197387231cf5487d"
   end
 
   depends_on "rust" => :build
