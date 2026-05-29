2da1378a17dc667ab0cad5c072697a34504fb5ab
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 00:17:00 +0800

    toml2json: update 1.4.0 bottle.

--- Formula/t/toml2json.rb
+++ Formula/t/toml2json.rb
@@ -7,12 +7,7 @@ class Toml2json < Formula
   head "https://github.com/woodruffw/toml2json.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e7b2b0427521ab532a532e7c79660a039ab61e4d25d40b110accec10b997e4d3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3d3a1fdf26a222e53b14398cabf8f8d726359ab6bd73969ac6a814f3d66564f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9ba3deb56cc6c7fc853bb0f3917017886f687b73df694911bd31d07d1d281517"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b9ccb30451c3fee454908111290d7b20c7213ec146e1c7152e5e975a40f78b96"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6b4499857996bd712c846e7850c8ad38ffc013b633ab9e160c191bcd0f1383e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "786b289aec4e5444f47921cdece21b0688c8915c99fd985b70b72e2cbe123323"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ab1762bbb23ed4b4728e8dedbd4b550d95e4aca7df89cd6133f14ecfc6c91ad2"
   end
 
   depends_on "rust" => :build
