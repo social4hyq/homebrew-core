b4b256bc82bf2429ad31df2dfb940b76dba83b46
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 15:25:00 +0800

    dra: update 0.10.2 bottle.

--- Formula/d/dra.rb
+++ Formula/d/dra.rb
@@ -7,7 +7,7 @@ class Dra < Formula
   head "https://github.com/devmatteini/dra.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "00ab0872256516efb79a9abf0da44dae413cde5d8a5900383a52cc591cd229e9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6218b5c71b8c4e019bdb811f32c34be52aabb26e08cfcee505e49fdddcbc89bf"
   end
 
   depends_on "rust" => :build
