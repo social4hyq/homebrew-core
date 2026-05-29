1b08078e9cfcac02e7ee20a71a4c2c773f89ec2a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 17:45:32 +0800

    libcdio-paranoia: update 10.2-2.0.2 bottle.

--- Formula/lib/libcdio-paranoia.rb
+++ Formula/lib/libcdio-paranoia.rb
@@ -10,14 +10,7 @@ class LibcdioParanoia < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5107f894005e9d2dd51fbd16b522d244335231b572232e18a95ac838e2495d29"
-    sha256 cellar: :any,                 arm64_sequoia: "fb138d8a79b5fa0eecf89a7ca0bcd3b294120aeb05200767558b61d3e4e51533"
-    sha256 cellar: :any,                 arm64_sonoma:  "d91bce7e9b1ecda7021bea7ede886f35e7414f691f06e7f5ca76ffce1d8693f3"
-    sha256 cellar: :any,                 arm64_ventura: "0d2130ca34ade60885f0838c9032563a1cac1267b9c88c99c6e87c1922f8b513"
-    sha256 cellar: :any,                 sonoma:        "8b89fb7bf1812fb9bd7910ab0a323996aade2c07bc46a8a163843fff6c531776"
-    sha256 cellar: :any,                 ventura:       "7b9d73af248b22fab2504e019d3c62daf93e8c6beb91ed9f463905f1e066b514"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4a1ec8c229b09241872472ddddea228bd4a8aff3ea7406a8148a12ca78c7b333"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d1e9aab49489534af51d75d1b222be76479ab2d15cfebc930ca570be8dd0d95"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e65a8f2817c75ec28da0dfb775b0e61c75d22e19314a25ac9a2411374b80f0f8"
   end
 
   depends_on "pkgconf" => :build
