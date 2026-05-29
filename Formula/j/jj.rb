6128296473fac61eb9f5fe6b1b69e421d2888089
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 17:27:54 +0800

    jj: update 0.41.0 bottle.

--- Formula/j/jj.rb
+++ Formula/j/jj.rb
@@ -8,12 +8,7 @@ class Jj < Formula
   head "https://github.com/jj-vcs/jj.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "318a1ff827a888d5c7b656f6faee25703ea634b5dd248f7656d3c52a183158c5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee3aac7f321985420e9656f3ee43984629e3423de7fc2edba1730a7dc1ed91d0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ab8e5dff600befe603c5595ffe099b3c91937562bcb3562b320e886650a50c31"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fe25f6f724570371473429eff988bccf292275282796ea07091210b9fdced7fb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2149df44145d6825ffcab4b7bede25e696ea78dd3c02119afb7be25c41b850cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d4731fbf0df26b0a244a6175d175a01f9cfccc90f7cf9ec79bab6430f247a73"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c85b6c38a9a30f2256c3ae1c87c4f09be14273ce076f8f222ed97eaa611e72ed"
   end
 
   depends_on "rust" => :build
