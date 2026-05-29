d926d7cbc64a5f0d8a20cbf84594a393fec032ae
hqzing <hqzing@outlook.com>
2026-05-29 20:14:23 +0000

    uhdm 1.86 (new formula)

---
+++ Formula/u/uhdm.rb
@@ -0,0 +1,90 @@
+class Uhdm < Formula
+  include Language::Python::Virtualenv
+
+  desc "Universal Hardware Data Model, modeling of the SystemVerilog Object Model"
+  homepage "https://github.com/chipsalliance/UHDM"
+  url "https://github.com/chipsalliance/UHDM/archive/refs/tags/v1.86.tar.gz"
+  sha256 "179203b166be5d1be12b901c69c6a569ebebf4fe47bc674b1268bd9319216fce"
+  license "Apache-2.0"
+  revision 2
+  head "https://github.com/chipsalliance/UHDM.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "891f989496491f9c6761524ba78fe17dfcc34143ffce1989aa91f04abb8594f8"
+    sha256 cellar: :any,                 arm64_sequoia: "377be1b55b38ddd0e367f51f32d0751e2e292a138a7a4c1a0ecce43687209657"
+    sha256 cellar: :any,                 arm64_sonoma:  "7a68cab4fc1699b0e803ff3e2a19130ec903a48300ee4c08c8e3a7736996f5a2"
+    sha256 cellar: :any,                 sonoma:        "8581b3d7ffad5bdb3981c9e2c65b739fb4b097e357e060298ea958dfb9f91db4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a72012164f97109986cfd2f443e12ad9b60c7bdc9eaa6812ec1d8b328c7dad5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd28d2d1d6c7099aa4dea9d924ee96a6a8d843c64a69f9af000e65dad60333c3"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python@3.14" => :build
+  depends_on "pkgconf" => :test
+  depends_on "capnp"
+
+  pypi_packages package_name:   "",
+                extra_packages: "orderedmultidict"
+
+  resource "orderedmultidict" do
+    url "https://files.pythonhosted.org/packages/5c/62/61ad51f6c19d495970230a7747147ce7ed3c3a63c2af4ebfdb1f6d738703/orderedmultidict-1.0.2.tar.gz"
+    sha256 "16a7ae8432e02cc987d2d6d5af2df5938258f87c870675c73ee77a0920e6f4a6"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
+    sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
+  end
+
+  def python3
+    which("python3.14")
+  end
+
+  def install
+    venv = virtualenv_create(buildpath/"venv", python3)
+    venv.pip_install resources
+
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DUHDM_BUILD_TESTS=OFF
+      -DUHDM_USE_HOST_GTEST=ON
+      -DUHDM_USE_HOST_CAPNP=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DPython3_EXECUTABLE=#{buildpath}/venv/bin/python
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # Create a minimal .uhdm file and ensure executables work
+    (testpath/"test.cpp").write <<~CPP
+      #include <cassert>
+      #include <stdlib.h>
+      #include "uhdm/constant.h"
+      #include "uhdm/uhdm.h"
+      #include "uhdm/uhdm_types.h"  // for uhdmconstant
+      #include "uhdm/vhpi_user.h"   // vpi_user functions.
+      #include "uhdm/vpi_uhdm.h"    // struct uhdm_handle
+      int main() {
+        UHDM::Serializer serializer;
+        UHDM::constant *value = serializer.MakeConstant();
+        value->VpiFile("hello.v");
+        value->VpiLineNo(42);
+        value->VpiSize(12345);
+        value->VpiDecompile("decompile");
+        uhdm_handle uhdm_handle(UHDM::uhdmconstant, value);
+        vpiHandle vpi_handle = (vpiHandle)&uhdm_handle;
+        assert(vpi_get_str(vpiFile, vpi_handle) == std::string("hello.v"));
+        assert(vpi_get(vpiLineNo, vpi_handle) == 42);
+        assert(vpi_get(vpiSize, vpi_handle) == 12345);
+        assert(vpi_get_str(vpiDecompile, vpi_handle) == std::string("decompile"));
+      }
+    CPP
+
+    flags = shell_output("pkg-config --cflags --libs UHDM").chomp.split
+    system ENV.cxx, "test.cpp", "-o", "test", "-fPIC", "-std=c++17", *flags
+    system testpath/"test"
+  end
+end
