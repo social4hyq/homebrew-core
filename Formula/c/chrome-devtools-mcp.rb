09e8cc53498d66b5c772da9ad185f87692277dde
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-29 15:33:43 +0800

    chrome-devtools-mcp: update 1.1.1 bottle.

--- Formula/c/chrome-devtools-mcp.rb
+++ Formula/c/chrome-devtools-mcp.rb
@@ -6,7 +6,7 @@ class ChromeDevtoolsMcp < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ce7c9d61b92b5b14fa16d19214ee6f2fa84b0e84b8ef27c5128eca09a2fd7505"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "051d1ff06ba871cb0ad9637f17f0644d1faada035d7e3cb9c8f7539c00c42df9"
   end
 
   depends_on "node"
