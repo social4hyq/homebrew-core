1b6790378aef9f779686451008c12ad08dffda8b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 12:43:44 +0800

    parallel-disk-usage: update 0.24.0 bottle.

--- Formula/p/parallel-disk-usage.rb
+++ Formula/p/parallel-disk-usage.rb
@@ -7,12 +7,7 @@ class ParallelDiskUsage < Formula
   head "https://github.com/KSXGitHub/parallel-disk-usage.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e1c5aee987829fdf2db5af5523b867e3615f0d2b2af05f601d6094e0e086bdb2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "592ef84757c9f751ec35c9f01cdacecac8c38f7cd73e338e6bfed6b67571a166"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "04bcaab70eb577ef2777b7577da0e37a4f398ce9999ebc799470d1baef200c90"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ba4bbbec6968e1a1084499788b2447e22e37d9b270411ce958ee64047005c332"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8706e88c9782c3b8dfd71ffdedc70202ea7c207f68cd7ef5a773381c5f5080a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3352f5bcc54da830d134d15751c908cfb49b8d5d70f04546d603bc3e9bd25edd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fbc246290f8e69b0c037a8c03dbfa80354465920ab029c52e83e9f35a9bfa7a6"
   end
 
   depends_on "rust" => :build
