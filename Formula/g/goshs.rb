a509daf1760668f280c767e6e484985f1e398fc1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 10:36:05 +0800

    goshs: update 2.1.0 bottle.

--- Formula/g/goshs.rb
+++ Formula/g/goshs.rb
@@ -7,7 +7,7 @@ class Goshs < Formula
   head "https://github.com/patrickhener/goshs.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "826fce4ee4666cf214c7f703c4241695368b2db3fe57577a5c6fe1ad3793327a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2aecc9c50d7585a059ed8ff7329cff94a852b9dba81b63cb49761ed0c2823042"
   end
 
   depends_on "go" => :build
