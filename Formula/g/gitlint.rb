dee131ca45f59ac97146d74bba857fa6023bdfe9
hqzing <hqzing@outlook.com>
2026-05-30 12:59:53 +0000

    gitlint 0.19.1 (new formula)

---
+++ Formula/g/gitlint.rb
@@ -0,0 +1,63 @@
+class Gitlint < Formula
+  include Language::Python::Virtualenv
+
+  desc "Linting for your git commit messages"
+  homepage "https://jorisroovers.com/gitlint/"
+  url "https://files.pythonhosted.org/packages/73/51/b59270264aabcab5b933f3eb9bfb022464ca9205b04feef1bdc1635fd9b4/gitlint_core-0.19.1.tar.gz"
+  sha256 "7bf977b03ff581624a9e03f65ebb8502cc12dfaa3e92d23e8b2b54bbdaa29992"
+  license "MIT"
+
+  bottle do
+    rebuild 7
+    sha256 cellar: :any_skip_relocation, all: "9bdd4315c315138d33b927f220ba855762f9d46e87e264b7665fa3cb3407fa72"
+  end
+
+  depends_on "python@3.14"
+
+  resource "arrow" do
+    url "https://files.pythonhosted.org/packages/2e/00/0f6e8fcdb23ea632c866620cc872729ff43ed91d284c866b515c6342b173/arrow-1.3.0.tar.gz"
+    sha256 "d4540617648cb5f895730f1ad8c82a65f2dad0166f57b75f3ca54759c4d67a85"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/46/61/de6cd827efad202d7057d93e0fed9294b96952e188f7384832791c7b2254/click-8.3.0.tar.gz"
+    sha256 "e7b8232224eba16f4ebe410c25ced9f7875cb5f3263ffc93cc3e8da705e229c4"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz"
+    sha256 "37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3"
+  end
+
+  resource "sh" do
+    url "https://files.pythonhosted.org/packages/59/52/f43920223c93e31874677c681b8603d36a40d3d8502d3a37f80d3995d43e/sh-2.2.2.tar.gz"
+    sha256 "653227a7c41a284ec5302173fbc044ee817c7bad5e6e4d8d55741b9aeb9eb65b"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
+    sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
+  end
+
+  resource "types-python-dateutil" do
+    url "https://files.pythonhosted.org/packages/fc/83/24ed25dd0c6277a1a170c180ad9eef5879ecc9a4745b58d7905a4588c80d/types_python_dateutil-2.9.0.20251008.tar.gz"
+    sha256 "c3826289c170c93ebd8360c3485311187df740166dbab9dd3b792e69f2bc1f9c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"gitlint", shell_parameter_format: :click)
+  end
+
+  test do
+    # Install gitlint as a git commit-msg hook
+    system "git", "init"
+    system bin/"gitlint", "install-hook"
+    assert_path_exists testpath/".git/hooks/commit-msg"
+
+    # Verifies that the second line of the hook is the title
+    output = (testpath/".git/hooks/commit-msg").readlines.take(2).last
+    assert_equal "### gitlint commit-msg hook start ###\n", output
+  end
+end
