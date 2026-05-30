fe5a6e713c133cb839a1513ce30fc915e80848c1
hqzing <hqzing@outlook.com>
2026-05-30 11:38:20 +0000

    sip 6.15.3 (new formula)

---
+++ Formula/s/sip.rb
@@ -0,0 +1,80 @@
+class Sip < Formula
+  include Language::Python::Virtualenv
+
+  desc "Tool to create Python bindings for C and C++ libraries"
+  homepage "https://python-sip.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/02/b1/79bff1c49a9e19ffe0211cb8905cc514c3f6b8f3f7ae55a40403d346c076/sip-6.15.3.tar.gz"
+  sha256 "bb2516983f9f716d321e5157c00d0de0c12422eba73b8f43a44610a0f6622438"
+  license "BSD-2-Clause"
+  head "https://github.com/Python-SIP/sip.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "45b3408f75b00c082f77836c19426004f329f511bb916e39d0e568a3d3c0d1fa"
+  end
+
+  depends_on "python@3.14"
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/65/ee/299d360cdc32edc7d2cf530f3accf79c4fca01e96ffc950d8a52213bd8e4/packaging-26.0.tar.gz"
+    sha256 "00243ae351a257117b6a241061796684b084ed1c516a08c48a3f7e147a9d80b4"
+  end
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/4f/db/cfac1baf10650ab4d1c111714410d2fbb77ac5a616db26775db562c8fab2/setuptools-82.0.1.tar.gz"
+    sha256 "7d872682c5d01cfde07da7bccc7b65469d3dca203318515ada1de5eda35efbf9"
+  end
+
+  def python3
+    "python3.14"
+  end
+
+  def install
+    venv = virtualenv_install_with_resources
+
+    # Modify the path sip-install writes in scripts as we install into a
+    # virtualenv but expect dependents to run with path to Python formula
+    inreplace venv.site_packages/"sipbuild/builder.py", /\bsys\.executable\b/, "\"#{which(python3)}\""
+  end
+
+  test do
+    (testpath/"pyproject.toml").write <<~TOML
+      # Specify sip v6 as the build system for the package.
+      [build-system]
+      requires = ["sip >=6, <7"]
+      build-backend = "sipbuild.api"
+
+      # Specify the PEP 566 metadata for the project.
+      [tool.sip.metadata]
+      name = "fib"
+    TOML
+
+    (testpath/"fib.sip").write <<~EOS
+      // Define the SIP wrapper to the (theoretical) fib library.
+
+      %Module(name=fib, language="C")
+
+      int fib_n(int n);
+      %MethodCode
+          if (a0 <= 0)
+          {
+              sipRes = 0;
+          }
+          else
+          {
+              int a = 0, b = 1, c, i;
+
+              for (i = 2; i <= a0; i++)
+              {
+                  c = a + b;
+                  a = b;
+                  b = c;
+              }
+
+              sipRes = b;
+          }
+      %End
+    EOS
+
+    system bin/"sip-install", "--target-dir", "."
+  end
+end
