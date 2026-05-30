b5c85194f712a2c23cc1dfc8998de0407d6d9795
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 15:07:59 +0800

    gitlogue: update 0.9.0 bottle.

--- Formula/g/gitlogue.rb
+++ Formula/g/gitlogue.rb
@@ -7,12 +7,7 @@ class Gitlogue < Formula
   head "https://github.com/unhappychoice/gitlogue.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "21290a3aac7b34699fb296a07466568ac963499922cc83695035d5a67570e4a4"
-    sha256 cellar: :any,                 arm64_sequoia: "db85c092aa2bcc464548ff70999a7fbe50b034358103db71e12ffb96a8086271"
-    sha256 cellar: :any,                 arm64_sonoma:  "aedb2f4a7ccf47b999d26d0b4ba39ec02890617f66f2ffe364c5234928b178e5"
-    sha256 cellar: :any,                 sonoma:        "b0111b982470e77c7f3394571274b91876e2183a8252a65bc7da4f072efaebca"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "64583bcfa9046f025f7557863eccd6dcd038be9ff1c887a471aecd1ff195bf36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bde9e99988a373219a9b4401d32bef8492af25cb8a2bc8eae1f7eb39a9f577ff"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "eff4964a1a0b19f6f61661167136924453b3ef5f29bba99cf3d0756b61bba743"
   end
 
   depends_on "pkgconf" => :build
