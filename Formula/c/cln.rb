dbb396fc87a98e55f4c4c8c32d62caa0d8747664
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 15:16:33 +0800

    cln: update 1.3.7 bottle.

--- Formula/c/cln.rb
+++ Formula/c/cln.rb
@@ -11,16 +11,7 @@ class Cln < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:    "198d7aa1178fc5df110461b2718466f2cefdedf75a0e4c88287dc130da5ddefc"
-    sha256 cellar: :any,                 arm64_sequoia:  "2279ade96381f9ac79cf40c1e29b2ea4fc998a25895b54a7fc0f77cc72d1782d"
-    sha256 cellar: :any,                 arm64_sonoma:   "128ed65c291ea5059320c7618340ca04b5a59126a36b6e22de91622175d62339"
-    sha256 cellar: :any,                 arm64_ventura:  "c47f98e423d29abd0de4bb322a4587817db8652d09909b07ec2eff44ff08b0fa"
-    sha256 cellar: :any,                 arm64_monterey: "a7a3af1288376ee0313842c9212c7e33743fcb6d8d17df95348d5360ea657d9c"
-    sha256 cellar: :any,                 sonoma:         "5c8b028628b234da5d4f97a734728ff9f73f381a3bae4ed565c8e21040190fc3"
-    sha256 cellar: :any,                 ventura:        "815ff9c38056bcaa56fb7a445ebb32efa14323f26d42da224014e9ff9a57a236"
-    sha256 cellar: :any,                 monterey:       "16fc46bc77d40dc5ad8060ac9375fe869c136fd1ea9f1da3df466e2a4bdb3960"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "c34b349259faf1c3c86953d6ce41a6b6136ef5a20b775e7907f9476b9d8eaa57"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1303657d924cdb1730d63a5b49b652cb84d07ff7694c528e997e37d796b68ec"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b11416aca186dcd8fe95aa57a45a35dce51aebce5c8b60ca6eb7f402de7a7e79"
   end
 
   head do
