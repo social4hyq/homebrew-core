cd34dcaae4e5f747c9c911bb39a8f705e244f5ba
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 14:52:01 +0800

    glibmm: update 2.88.0 bottle.

--- Formula/g/glibmm.rb
+++ Formula/g/glibmm.rb
@@ -7,12 +7,7 @@ class Glibmm < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "004d7f1ab6701eb47259e35466edede4a59c1a9f224018000e71ee0d82bbd411"
-    sha256 cellar: :any, arm64_sequoia: "b9a29a482b2dcf1fe705476bcdcbc363c48fa2cbaa05b5132b4d4509d6ce0310"
-    sha256 cellar: :any, arm64_sonoma:  "466bf9f96c97bac7dad5747b089aa5a527357434651c59ee3a4574c6862ae75a"
-    sha256 cellar: :any, sonoma:        "c074041d507860b7da2e5c39321d5112ebd9065b3a6260c812d9c4ea2513a5ee"
-    sha256               arm64_linux:   "18b0c3b26d6d8f266fd4466f8577fe90ce40c1197d28f87e9dee421f0b4de70d"
-    sha256               x86_64_linux:  "1e806c45a2aaa2c21a79dc61369ff79f4b2ff72fdcdb6f610f1e6d3b294ac0ff"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4b6c660c0ac7afaa225e69d82a744e69a2758d659ea3b1681e172acca6c5431c"
   end
 
   depends_on "meson" => :build
