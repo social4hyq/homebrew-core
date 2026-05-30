5a0a29347d45514c92ca95f8780fe46444c2800e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 12:33:43 +0800

    invoice: update 0.1.0 bottle.

--- Formula/i/invoice.rb
+++ Formula/i/invoice.rb
@@ -7,13 +7,7 @@ class Invoice < Formula
   head "https://github.com/maaslalani/invoice.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3666463773abc10aef019abd54dbac8b4b699db7783fc0b54e7be6c59b5844df"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3666463773abc10aef019abd54dbac8b4b699db7783fc0b54e7be6c59b5844df"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3666463773abc10aef019abd54dbac8b4b699db7783fc0b54e7be6c59b5844df"
-    sha256 cellar: :any_skip_relocation, sonoma:        "334d0d037207500ac6199cef1639e08c759cdf544f2232543c05f7d32aab631b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "724180390bc239a924d1798745bea25a5ea50fcddc1b8f42adf4941e71be76a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d75038930d911627bed4b65260095a76623c0062c21f77d501451029da75de9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6e1267670d3aa40631fc91ab5857fba839ef8ee565e9a355a89c85646e5b630b"
   end
 
   depends_on "go" => :build
