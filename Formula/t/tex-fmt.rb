166a96adfbf817bb27c407e7955c78879d519066
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-30 15:00:25 +0800

    tex-fmt: update 0.5.7 bottle.

--- Formula/t/tex-fmt.rb
+++ Formula/t/tex-fmt.rb
@@ -7,12 +7,7 @@ class TexFmt < Formula
   head "https://github.com/WGUNDERWOOD/tex-fmt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5be164037ac7b438000cef392c642d0aaa44dcec7d6c6f5697ba5fa292a1b098"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e2e644f93e0b05d1c7d7de759828490398907480bb54670f1dabe8f4feaff18b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "23fa5f27f2598686e652f992565fa80694e36fcac2e9585d2c3fb6a3de88aa07"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bb4acc89497665495792222dabb1b748ba7af1f9d22539a22e3a4c212a89351d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c88d676684f0ee36cea1beef980bcbf89d950bbe8f1a3a28a975af65b7f06cd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d687c3d3889c07d59801d34bbbbb733fc29751fa34d607c9fe48ee096ad15430"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d6cb595d35526f17bb1b5703b0fcae5a1f50c3ff937e60f2e72bcc7591f2829e"
   end
 
   depends_on "rust" => :build
