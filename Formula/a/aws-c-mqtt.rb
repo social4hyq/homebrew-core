b4d92045c612b129f6c48294a8b01e5a1c5bad3f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-31 22:24:58 +0800

    aws-c-mqtt: update 0.16.0 bottle.

--- Formula/a/aws-c-mqtt.rb
+++ Formula/a/aws-c-mqtt.rb
@@ -12,12 +12,7 @@ class AwsCMqtt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a74167566a7bf2b20fe580263056776e6da26b09ecf15026bff6a9ba52e4bd35"
-    sha256 cellar: :any,                 arm64_sequoia: "135c03cd46a4bdcbf0a4933c83a122427af00eea1cfb35f8135f395c126467cb"
-    sha256 cellar: :any,                 arm64_sonoma:  "646ddca9d136d4d1f3ecc5b5da7a86ca6cea3fa72a64cd9990d15b7e8f3bfddf"
-    sha256 cellar: :any,                 sonoma:        "a62dabdc4176ad788ccf8b5a2d88025c58278c1646d2e1e436b750cc6e340768"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0caf142228a9426743089b42c7f2330a3057929d6ce48621a3c81c680ca65727"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45311e3f0f92312f19913aa4e2ef0d151f53cb14872605ef8eab6e01c882495f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "13b194e9763f23314bb768cc2a82786bed70561ebcefc963f6f3d7aed4db76a0"
   end
 
   depends_on "cmake" => :build
