e604952e18947c9c158faa57b9d2e101caf52379
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-31 22:14:37 +0800

    tmux-sessionizer: update 0.6.1 bottle.

--- Formula/t/tmux-sessionizer.rb
+++ Formula/t/tmux-sessionizer.rb
@@ -6,12 +6,7 @@ class TmuxSessionizer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "52fc5a69d6267f256d6224f386e2b1e21673695970783397c8b7651c4d4a3ff8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "df1ee713aae26d6d8aba8d9d28acfb95fd3ba7b347b2a779174118cdbe4726dc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf58c2687f60a767467deea5bd9ef5b21a5cd3ba8bbcc9ae9a8579b415aaf96a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fba2821174c650778172f3e212b186fd78807ee016787df2ab3001db8812c529"
-    sha256 cellar: :any,                 arm64_linux:   "efe76142e388c210962bf6e36dfaff4d7d1cf737b7126804a242d67bd8bd3537"
-    sha256 cellar: :any,                 x86_64_linux:  "6800da10228fe8b4f72a3e6a58c3826602b73c18611a814fe8d92d678e7eb666"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3e0ead556bc7e2009a45aea720a86fa0e0246bde2af470127dc56d73e350c5c3"
   end
 
   depends_on "rust" => :build
