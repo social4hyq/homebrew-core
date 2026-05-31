ce8934ba56c9622cba5b6aed4cffd561f027b319
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-31 22:20:16 +0800

    killport: update 2.0.0 bottle.

--- Formula/k/killport.rb
+++ Formula/k/killport.rb
@@ -6,12 +6,7 @@ class Killport < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fe2f63551697527506e0fe8f97b7c21b28e0eec96dd6b923611a3c292105f3ac"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4084ec7d357dbfb28eba4577097d03c402af83cebaf37d45b4728f3d2f15a7fc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "518d331177158c7f98b6f3614bebfdae1ec7222d90351b30c97e4d949995b09f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2b014c0628cebc3706b16476b2d268d75be4502aba623bd80802ea9efc0db0c5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8ebaa7fef654c5cb054a995279e9dd2f2178985e75efdd2f482e442902fd6ca4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76db18b18f55cb5500a8681356308de355fa008e196013aba8837f2eeaf987a7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "204467a6464242a8543a85c4d2aaea56107c01793816b45dad416807eb2eccb3"
   end
 
   depends_on "rust" => :build
