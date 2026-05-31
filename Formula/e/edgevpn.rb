256503098296a1efb10ba8659e1b9f13cfe87840
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-01 06:25:01 +0800

    edgevpn 0.34.0

--- Formula/e/edgevpn.rb
+++ Formula/e/edgevpn.rb
@@ -1,8 +1,8 @@
 class Edgevpn < Formula
   desc "Immutable, decentralized, statically built p2p VPN"
   homepage "https://mudler.github.io/edgevpn"
-  url "https://github.com/mudler/edgevpn/archive/refs/tags/v0.33.1.tar.gz"
-  sha256 "6aa890688ba7a7671e46d0c65a5e3028915d0968bea6d1d64562909f7f10206e"
+  url "https://github.com/mudler/edgevpn/archive/refs/tags/v0.34.0.tar.gz"
+  sha256 "1115a50182cdcfa3979a8b14f1f00526a244d340271e23eb6501e919178b99b7"
   license "Apache-2.0"
   head "https://github.com/mudler/edgevpn.git", branch: "master"
 
