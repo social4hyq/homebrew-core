0629e6c1c125c2662d12c70e818ebcdc81a45630
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-31 22:31:10 +0800

    go-parquet-tools: update 1.49.3 bottle.

--- Formula/g/go-parquet-tools.rb
+++ Formula/g/go-parquet-tools.rb
@@ -7,12 +7,7 @@ class GoParquetTools < Formula
   head "https://github.com/hangxie/parquet-tools.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c5a0dc9c6ab56d86599daeb3193565882785bbacbab46746973e3c94c24122b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c5a0dc9c6ab56d86599daeb3193565882785bbacbab46746973e3c94c24122b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1c5a0dc9c6ab56d86599daeb3193565882785bbacbab46746973e3c94c24122b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1bf1fcd243d42e0dd9cf2fa7a82296eb79580ba767c39ac1623ef48c8142d282"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "20063fa0e02ba4aecf1420079f379a855cc7dbadf40f463e809e0ddd41dd1afe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df0aef34e61e5f4ab285d6d41193a18b3c4b5abc3ddb9f8c72908e384cab7d4d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "214f16ee21c20dcc8d8a339965524abc33404df6c16bbd9bc05e82cc79879c68"
   end
 
   depends_on "go" => :build
