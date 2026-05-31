9408bf99c81e1a6936c13acd1c46f68ac0860232
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-05-31 22:24:10 +0800

    rust-parallel: update 1.23.0 bottle.

--- Formula/r/rust-parallel.rb
+++ Formula/r/rust-parallel.rb
@@ -6,12 +6,7 @@ class RustParallel < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "465dbbc9008598094a1369048b09b67cf86a3261f3cd2cb17e5cd451687b023f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bb83a9b4281d7988b17a34ad74d2d854ed36442e06c4f211b1336a7b5060c365"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ab4f7a740ede946626f962a501ecf9ff40e4b0bb020e03e765520a79cc5834c8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "20ed463f327f83aca5eb4932eaf75b257921ac860d3ee5d9bcc1d606245da308"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "be8a523ad79ccf15adc622ff13b46352e9ad5462ad80911e1b275d606e2a2ad8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0366106a5adefbb4d0f04b9b928cf01f0e93239391d174e788396ef3e564aeb6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dba8d6b0ff54d9d13d21d4d75c268900524a5b2b893261e2df6896e565a06c50"
   end
 
   depends_on "rust" => :build
