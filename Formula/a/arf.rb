46161f31f3f401e5cd4a8c8b4a20132650056f9a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-01 10:13:15 +0800

    arf: update 0.4.0 bottle.

--- Formula/a/arf.rb
+++ Formula/a/arf.rb
@@ -6,12 +6,7 @@ class Arf < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12a48a83285c033b5b81be70c3f16ef9c5a01487d27dab80ab36a86a52091f01"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c4d6d1695a3bd23dc9f92bd0de2f77b274b66c44f18c9a39ff39f9a2fac6fde"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92b84362ab84a95b6389294ff4f97b6f7789a80e2d05ca352c5cb5d4ca0517d2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cc090f0227610495a89cee288b3c2689a1ccf1f8348bcd6e20a1278f3f59a2b3"
-    sha256 cellar: :any,                 arm64_linux:   "c901facce96ef1978d2dbcd73cba67ee3e7df3c54a6b21362efa3f78375bcd33"
-    sha256 cellar: :any,                 x86_64_linux:  "b13c1f2f0b6ff2a60c50e1fa27cd5da88d134fceb2cacc273333500188363248"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "54aa1156409626c49e0c16ede165e93e5a7c23cbe29b1d87923665308cb08f5d"
   end
 
   depends_on "rust" => :build
