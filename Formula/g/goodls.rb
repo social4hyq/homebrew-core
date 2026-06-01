2e01ebfd67ec1779d3b35b1aed3119304ababd9a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 06:44:43 +0800

    goodls 3.4.0

--- Formula/g/goodls.rb
+++ Formula/g/goodls.rb
@@ -1,8 +1,8 @@
 class Goodls < Formula
   desc "CLI tool to download shared files and folders from Google Drive"
   homepage "https://github.com/tanaikech/goodls"
-  url "https://github.com/tanaikech/goodls/archive/refs/tags/v3.3.0.tar.gz"
-  sha256 "0d377adb177fbb7220af8bf0379e2e707a9fd3b55a91449ed2a1f235e2a2b172"
+  url "https://github.com/tanaikech/goodls/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "5acda68a159e8bc7d8dfe164a2bb44a8868240db9394b6c5eff93233260a4b8b"
   license "MIT"
   head "https://github.com/tanaikech/goodls.git", branch: "master"
 
@@ -13,17 +13,13 @@ class Goodls < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/goodls"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/goodls --version")
 
-    expected = if OS.mac?
-      "URL is wrong"
-    else
-      "no URL data"
-    end
-    assert_match expected, shell_output("#{bin}/goodls -u https://drive.google.com/file/d/1dummyURL 2>&1", 1)
+    output = shell_output("#{bin}/goodls -u https://drive.google.com/file/d/1dummyURL 2>&1", 1)
+    assert_match "URL is wrong", output
   end
 end
