635c2fadff2ab5e3658f66f4cae157969fc71a81
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-01 19:44:11 +0800

    nnn: update 5.2 bottle.

--- Formula/n/nnn.rb
+++ Formula/n/nnn.rb
@@ -7,12 +7,7 @@ class Nnn < Formula
   head "https://github.com/jarun/nnn.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "9661f5ce4e96704568084671343c92755a1de8b542137f0c56e6deb0a6e41387"
-    sha256 cellar: :any,                 arm64_sequoia: "68ba1617d44b25cd6bf5e2ae7bb27ce1152a26668b6dd6d4e080d251f7d204db"
-    sha256 cellar: :any,                 arm64_sonoma:  "61fbe8b70fbc363a96f266367f6a2daca32f8d2e2ed29df83b633c344369caa1"
-    sha256 cellar: :any,                 sonoma:        "24e92f0826f362840d1424012ebf78bd855d46fb827fb253c80e8313c7db137f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "10959b02a38dd226bfdf72ee603fab91d6906694584f6d16e25d4ca4450d8a41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fc7fd26418ce4ac9a9406ef3fd745cdbdb333828c6e0e0fbbfad0dde5b59d10"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0d3f105462e015ff49a265c201067659e572cb7e0bfae1160cd95fd70d5807f5"
   end
 
   depends_on "gnu-sed"
