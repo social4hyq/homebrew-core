3ca5db78c0b2284fadb3d0a94cb543016645479c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-01 14:29:30 +0800

    libpaho-mqtt: update 1.3.16 bottle.

--- Formula/lib/libpaho-mqtt.rb
+++ Formula/lib/libpaho-mqtt.rb
@@ -6,13 +6,7 @@ class LibpahoMqtt < Formula
   license "EPL-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "eeeb71c665576f02cc774b84e777eddda3969b2e63ccae7f66f7c5dc1638f999"
-    sha256 cellar: :any,                 arm64_sequoia: "c0a8cb599d043492514a8412746d4b281215aa5aeda8db1fd12954a40237b138"
-    sha256 cellar: :any,                 arm64_sonoma:  "4320aa4be856759a55293101979436f7a79382d8528b3e8b935068ab5edf0886"
-    sha256 cellar: :any,                 sonoma:        "8a3054d689f09b17faa7392f37ecd140fe2e0358b18b472db84f74f7c7556c50"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4f091e548e02bf60eee30c26f4451aeda7fa77a3f54452349cc98f5568404aed"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "71be99abeb74ab2fada683b2b0fcae74c3ec65c062d3b273ac033697f08342e5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "02a37ed20b1e87514d7b9e056c201c6ff3fca7c368191cc815471feab3c780ba"
   end
 
   depends_on "cmake" => :build
