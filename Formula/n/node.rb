bd05e5a9e66ecfcb23f523ec255df62502d2bc83
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 07:23:30 +0800

    node 26.3.0

--- Formula/n/node.rb
+++ Formula/n/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Open-source, cross-platform JavaScript runtime environment"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v26.2.0/node-v26.2.0.tar.xz"
-  sha256 "ea82be7db418f54b3ef153a02d44d4f6748466f4765ae80bc484f34af40df610"
+  url "https://nodejs.org/dist/v26.3.0/node-v26.3.0.tar.xz"
+  sha256 "319ad5d7d20cc622e55eb75b9f1a2546b77a08bd462b67030d0c89316c2c2349"
   license "MIT"
   head "https://github.com/nodejs/node.git", branch: "main"
 
