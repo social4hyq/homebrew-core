a953e402be937bdde2bdf73dd26798fae9007dda
lanfungcing <lanfq.26@qq.com>
2026-06-01 02:42:06 +0000

    spdlog 1.17.0 (new formula)

---
+++ Formula/s/spdlog.rb
@@ -0,0 +1,70 @@
+class Spdlog < Formula
+  desc "Super fast C++ logging library"
+  homepage "https://github.com/gabime/spdlog"
+  url "https://github.com/gabime/spdlog/archive/refs/tags/v1.17.0.tar.gz"
+  sha256 "d8862955c6d74e5846b3f580b1605d2428b11d97a410d86e2fb13e857cd3a744"
+  license "MIT"
+  compatibility_version 1
+  head "https://github.com/gabime/spdlog.git", branch: "v1.x"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "69443892af6734072aadc934bec0118cab7084e0dc71458e9a2e5942bdf24910"
+    sha256 cellar: :any,                 arm64_sequoia: "b2367992ea0242f207b0ba28339d586dbb190ac83faec6950c7706d7bbc6db8a"
+    sha256 cellar: :any,                 arm64_sonoma:  "07c90f09c9f912bc2d5b5597d8f9fb9b96dd9d5d5fa905d245d32274b03045dc"
+    sha256 cellar: :any,                 sonoma:        "26eb42de91edf67fd36f48ad416eda0da84c101b99dac85057087727fb974c0c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ea613072e73e447b4faba3f7ed8e03cda77dc0427ada1299ea429e7a4fea2604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ff71007269f44e4b90e0e3cabc7d4969ebddac67b2f0469d39a9b403d85c05b8"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "fmt"
+
+  def install
+    ENV.cxx11
+
+    inreplace "include/spdlog/tweakme.h", "// #define SPDLOG_FMT_EXTERNAL", <<~C
+      #ifndef SPDLOG_FMT_EXTERNAL
+      #define SPDLOG_FMT_EXTERNAL
+      #endif
+    C
+
+    args = std_cmake_args + %W[
+      -Dpkg_config_libdir=#{lib}
+      -DSPDLOG_BUILD_BENCH=OFF
+      -DSPDLOG_BUILD_EXAMPLE=OFF
+      -DSPDLOG_BUILD_TESTS=OFF
+      -DSPDLOG_FMT_EXTERNAL=ON
+    ]
+    system "cmake", "-S", ".", "-B", "build", "-DSPDLOG_BUILD_SHARED=ON", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    system "cmake", "-S", ".", "-B", "build", "-DSPDLOG_BUILD_SHARED=OFF", *args
+    system "cmake", "--build", "build"
+    lib.install "build/libspdlog.a"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include "spdlog/sinks/basic_file_sink.h"
+      #include <iostream>
+      #include <memory>
+      int main()
+      {
+        try {
+          auto console = spdlog::basic_logger_mt("basic_logger", "#{testpath/"basic-log.txt"}");
+          console->info("Test");
+        }
+        catch (const spdlog::spdlog_ex &ex)
+        {
+          std::cout << "Log init failed: " << ex.what() << std::endl;
+          return 1;
+        }
+      }
+    CPP
+
+    system ENV.cxx, "-std=c++11", "test.cpp", "-I#{include}", "-L#{Formula["fmt"].opt_lib}", "-lfmt", "-o", "test"
+    system "./test"
+    assert_path_exists testpath/"basic-log.txt"
+    assert_match "Test", (testpath/"basic-log.txt").read
+  end
+end
