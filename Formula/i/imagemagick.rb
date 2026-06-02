6ea0e16bcba6851eaf4e542160d73a207cd54961
lanfungcing <lanfq.26@qq.com>
2026-06-02 12:47:27 +0000

    imagemagick 7.1.2-24 (new formula)

---
+++ Formula/i/imagemagick.rb
@@ -0,0 +1,98 @@
+class Imagemagick < Formula
+  desc "Tools and libraries to manipulate images in select formats"
+  homepage "https://imagemagick.org/index.php"
+  url "https://github.com/ImageMagick/ImageMagick/releases/download/7.1.2-24/ImageMagick-7.1.2-24.7z"
+  sha256 "647e8e0f20d7448e513d84ae7246f43cef66df6920384fdfcac5d8eeb4b38d9a"
+  license "ImageMagick"
+  compatibility_version 1
+  head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+-\d+)$/i)
+    strategy :github_releases
+  end
+
+  bottle do
+    sha256 arm64_tahoe:   "c3fa75601d74151f53ab4e3730c6e983fb6d4ee56e4b8122359c700ee4cf3fff"
+    sha256 arm64_sequoia: "6b38b6261f50891d4848ddf40d405fcef8b958cd5a5196b9048e6601b0f9beb1"
+    sha256 arm64_sonoma:  "a41f6a16480ad27754dc1fe9cf197e600b3699da35cca120199665877538b28f"
+    sha256 sonoma:        "e9712aa8fe24fa82a561f001a798a0db0784f8d77bd4f77118ba71ce55718522"
+    sha256 arm64_linux:   "80a160ca17cdbeaa4b38112b8f7e642ff818294ef45d314dac842c773971ef5c"
+    sha256 x86_64_linux:  "b429c1923f47638ed6e0d101428c8152550abb34fd5831eacb26a5eeb0a600ad"
+  end
+
+  depends_on "pkgconf" => :build
+
+  # Only add dependencies required for dependents in homebrew-core,
+  # recursive dependencies or INCREDIBLY widely used and light formats in the
+  # current year (2026).
+  # Add other dependencies to imagemagick-full formula.
+  depends_on "freetype"
+  depends_on "jpeg-turbo"
+  depends_on "libheif"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "webp"
+  depends_on "xz"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+
+  on_linux do
+    depends_on "libomp"
+    depends_on "zlib-ng-compat"
+  end
+
+  skip_clean :la
+
+  def install
+    # Avoid references to shim
+    inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
+
+    args = [
+      "--enable-osx-universal-binary=no",
+      "--disable-silent-rules",
+      "--disable-opencl",
+      "--enable-shared",
+      "--enable-static",
+      "--with-gvc=no",
+      "--with-modules",
+      "--with-webp=yes",
+      "--with-heic=yes",
+      "--with-raw=no",
+      "--without-gslib",
+      "--with-lqr",
+      "--without-djvu",
+      "--without-fftw",
+      "--without-pango",
+      "--without-wmf",
+      "--without-jxl",
+      "--without-openexr",
+    ]
+    args << "--without-x" if OS.mac?
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      imagemagick-full includes additional tools and libraries that are not included in the regular imagemagick formula.
+    EOS
+  end
+
+  test do
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/magick -version")
+    %w[Modules heic jpeg png tiff].each do |feature|
+      assert_match feature, features
+    end
+  end
+end
