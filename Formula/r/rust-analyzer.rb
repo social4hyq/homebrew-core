a20fda279b3b95d01edfa3bd21979ee9f79c9f8f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 10:09:17 +0800

    rust-analyzer: update 2026-06-01 bottle.

--- Formula/r/rust-analyzer.rb
+++ Formula/r/rust-analyzer.rb
@@ -7,7 +7,7 @@ class RustAnalyzer < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "55f38f1fbecb2667ffb7bb3f4a37cbf3fa4ea8bb42bc90ffec34b8d77eb7d123"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8ba9809cb7603a12f1113d81a4416226eee28d55c774d887123df3bb67b682d0"
   end
 
   depends_on "rust" => :build
