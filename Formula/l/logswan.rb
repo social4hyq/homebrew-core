f056174990fa63094d7debca186b8272b6ae3e46
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-03 03:27:17 +0800

    logswan: update 2.1.16 bottle.

--- Formula/l/logswan.rb
+++ Formula/l/logswan.rb
@@ -6,12 +6,7 @@ class Logswan < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "c9332d6b9093ea70660ed6fda5834c76c60a5bd68d069ba5d766902d77d2b95d"
-    sha256 cellar: :any,                 arm64_sequoia: "68e48b65ca63fd9ba94d96ebc8e72e9b324d0de6286d001eb6294b8e99989ba5"
-    sha256 cellar: :any,                 arm64_sonoma:  "89baa68dc3c35d2e0220e1ebd97d0a83c4023af411a80e257d58c0c96441bf69"
-    sha256 cellar: :any,                 sonoma:        "924f3b9130a56a68193e8d209cfb31dacf2a7347dedf355c5c43454cb386466a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ff8dd3ec7671cea407b56821c73c11a0c4b53331476446f74f350c356d876e54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "694736b14898f12898edaebc9ba5f07d050f83fc59f16c802c89b66a67c816ee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5027838b4417ba122feb52a0391428dc4dccec5f422fbbc7a0f338174d8a02ba"
   end
 
   depends_on "cmake" => :build
