a4f55f941e8ea1e9fc9084d80b0ed285a1958bfb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 16:43:12 +0800

    httpflow: update 0.0.9 bottle.

--- Formula/h/httpflow.rb
+++ Formula/h/httpflow.rb
@@ -7,13 +7,7 @@ class Httpflow < Formula
   head "https://github.com/six-ddc/httpflow.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "6e9299b293094af5b4947a5d1ebdd4ff98c6f56b5d1e148b0b3df61a5cda63c5"
-    sha256 cellar: :any,                 arm64_sequoia: "7579060ce00a8d1c70203cc1670139f45d92f7d22ea820b61c5fd7aa3c2828a6"
-    sha256 cellar: :any,                 arm64_sonoma:  "970e799b29e6a5f403cd4ec79a92de569d05779ace7c25afe1819c7161339eab"
-    sha256 cellar: :any,                 sonoma:        "45e3e0a5357cd5f6d907677653afcdbcb828faae3731705f0f6acdb16eb69d81"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "011031ee7faf0f0e86f2628d1e60307189def32af966b42f977f2c2762c626dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "600ed0d8a1171c7d99c6c53579b72b6f3ef809705e5e9c9f1cfc8ffae39a1ffc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a533963270a4ddceb432f416bb64659c2c03ed1ea9f07ec4e285bcd58ba410e2"
   end
 
   # Last release on 2020-05-07 and needs EOL `pcre` (https://github.com/six-ddc/httpflow/issues/12)
