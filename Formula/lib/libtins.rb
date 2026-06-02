8be79edfe60845c7bccb3e8308d3f75ad47bd85a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 17:00:00 +0800

    libtins: update 4.5 bottle.

--- Formula/lib/libtins.rb
+++ Formula/lib/libtins.rb
@@ -7,13 +7,7 @@ class Libtins < Formula
   head "https://github.com/mfontanini/libtins.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "38159a186f5cfc1814d2213988961c0722fa0516c3e3e358cce144ca75deeb9b"
-    sha256 cellar: :any,                 arm64_sequoia: "90d59f1f6bfbae9ed0df679f1d5b01e5fbf0250a06952bea0569c730ef91308f"
-    sha256 cellar: :any,                 arm64_sonoma:  "3453d410232d79ff731aa368d989f39330673f81dd4c8bd0c639d0a3d3edd256"
-    sha256 cellar: :any,                 sonoma:        "38e9b99edfe1ecc2fc1fd4b631d23003d68c9328c27af7562e0882caced2a0bd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ce171f76d72665a789898ef1699e48c13fa8006b3ccd3c2a54d8fd6f9c843b0b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3dcbd72e7f1c30f197318654896be2ad81b9d16f910ec1cde31a264c8a850a7d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "21561c0d900cacef2cbc0588cdf3129d292376025d1e01870a8bddbc32ac264c"
   end
 
   depends_on "cmake" => :build
