f3e68ed6724e2f4539bd953c518f684138de0521
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 11:43:37 +0800

    etcd: update 3.6.12 bottle.

--- Formula/e/etcd.rb
+++ Formula/e/etcd.rb
@@ -8,7 +8,7 @@ class Etcd < Formula
   head "https://github.com/etcd-io/etcd.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3a06c5d4abfd825c8baeea75be413270887519ba897acabb7236abd6e00f7024"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "18ebd465508a582e5300a39828b16fdd2bc1e76257bda52dcde1be4117870ac1"
   end
 
   depends_on "go" => :build
