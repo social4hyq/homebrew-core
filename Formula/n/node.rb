dd25240eae7ec5a6aa7fc1260ea287c62a3bd5f2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 11:15:35 +0800

    node: update 26.3.0 bottle.

--- Formula/n/node.rb
+++ Formula/n/node.rb
@@ -12,7 +12,7 @@ class Node < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9a84bf5ba207a9b6038dcf7d34250b2c1b9c2cfb5239454a480da5b0e36a9b2d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c5c5f4e458f8c08b1f0150caf8dfb83588f54d12cc7578a833b7efad39028167"
   end
 
   resource "alpine-rootfs" do
