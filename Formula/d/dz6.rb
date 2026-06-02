150fbbce353a8465b40312d0cecabd1c3e927dbc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 17:15:37 +0800

    dz6: update 0.7.0 bottle.

--- Formula/d/dz6.rb
+++ Formula/d/dz6.rb
@@ -6,12 +6,7 @@ class Dz6 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "827842d7edbe838bcef4216c404a7fa407aca18714f3f169b266d75555cf812f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e74f7a02638b73224bb5c3774039c688715f1d29e9be94ea4d690764cba49827"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "db05c1e280a48aaab7a32d509a5401f97f110c314c8cee321c4d4894f470a959"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a79f0617d6d80d560ed2602d193b6ec792c7aeb72652e6633da56edd9e28f4ea"
-    sha256 cellar: :any,                 arm64_linux:   "b40dfe29315797bdca04d439cd9feab90d1b829acb4463084380b337e4adcaf6"
-    sha256 cellar: :any,                 x86_64_linux:  "e50ab6925db3259a807fb8b5d6750001de6f0911f01bda4d61b01aa159e8eb24"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8a1f4505e9b43560a70ee7f51e5aa0a93fddfdc88a74dfcc430bf0101c90ead4"
   end
 
   depends_on "rust" => :build
