5ed9ecae2d61675bb7725c3c91285053a0f0d37b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 16:55:58 +0800

    quien: update 0.10.0 bottle.

--- Formula/q/quien.rb
+++ Formula/q/quien.rb
@@ -6,12 +6,7 @@ class Quien < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
-    sha256 cellar: :any_skip_relocation, sonoma:        "384c69e6a6f3bd6de62f40f6bb9d87bb4522ef1058a4983fedce5e29f6137726"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "108c32294d38248ce3ce4cde2f6327fa2d41c1cdc66b526a438aedd41460d1da"
-    sha256 cellar: :any,                 x86_64_linux:  "813de163d1a8b41f55aa4f2aea52422f57a78ba5a38398640f3c5fa51813459f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f89b2a468ac18e659a6b664b35e3f26791b6b9e514bd0b67a06b6d2c1c37a66c"
   end
 
   depends_on "go" => :build
