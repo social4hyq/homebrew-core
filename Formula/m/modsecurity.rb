44c55ea8f5909662fba35843c7376b631d99144d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-03 03:36:46 +0800

    modsecurity: update 3.0.15 bottle.

--- Formula/m/modsecurity.rb
+++ Formula/m/modsecurity.rb
@@ -6,13 +6,7 @@ class Modsecurity < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "e51b958e7f3ec2ebe0324cb0f133aa6af929c429d22589bb042ac5a9f524eeb7"
-    sha256 cellar: :any,                 arm64_sequoia: "47d9a27168331fe29ea86eb6c398a94ee9c0d7c2a295ab62cdf2364e5de29d73"
-    sha256 cellar: :any,                 arm64_sonoma:  "fc6b93dc556fc26c55831e5c0365fdc6e45261dcf8ecb3a6271ac6a49f6c48f9"
-    sha256 cellar: :any,                 sonoma:        "8bfd7a7e85fe4102ae34638c5bdd7a3c5135aa57e5610f7411c069c47788f050"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e8efcb21e3ece35ac7c3df4bc83c0b38f10ce86881d45f80098a0946ef9467aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c901de2b4e8c412d29aa69ac9974a0e52786fa689172619c72269904e3b3685"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "65a9f3b054442b26fc3b41c2f70a156ae87fc4db107a47bb18e62532e34a3e61"
   end
 
   depends_on "autoconf" => :build
