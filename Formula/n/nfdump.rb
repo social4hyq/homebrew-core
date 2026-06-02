ce4f4f36ed24c6a3e52e78b7c4ded7ed9550b256
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-03 03:17:33 +0800

    nfdump: update 1.7.8 bottle.

--- Formula/n/nfdump.rb
+++ Formula/n/nfdump.rb
@@ -7,12 +7,7 @@ class Nfdump < Formula
   head "https://github.com/phaag/nfdump.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e5d6b64b0cbecaa2cc585e8f387dc9194d02fe6991cf310c35c3d6dd54df3bcd"
-    sha256 cellar: :any,                 arm64_sequoia: "5ca8bd4545874a70900fd98e5696b129b46de6b0d54ea98c65c5777e5ec69d8a"
-    sha256 cellar: :any,                 arm64_sonoma:  "53da326cac8cc89db21970ce35d800a0ff6ab6f2d8888b70d16573e7a9c4cab8"
-    sha256 cellar: :any,                 sonoma:        "b08e1cfd6b72f2a75784bf3dd69688c6ab4614520631d62627267ae1ececf325"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "83ad503a74619c003a0ca0e045d94c2393aaf07c1aa6222dc6834249df04acb9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5fbf3db1dbea03ecd41ccecc432f93b12bac05585f40590a99f27e2fbf20150f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "27d979812b7c66bb84d51c9eba6b4352ef20289742affd1f9f10862b645f4b2c"
   end
 
   depends_on "autoconf" => :build
