35018e49703d22e7412be32e08b00d9be9b42ecb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 12:02:24 +0800

    chainloop-cli: update 1.99.0 bottle.

--- Formula/c/chainloop-cli.rb
+++ Formula/c/chainloop-cli.rb
@@ -12,7 +12,7 @@ class ChainloopCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "117e0f399dcb71eb51c4fac917bac2e1e0bad4e7bdabac6333c87e1fbce634c0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "72e2c4a6edad08db8654be7601c1d79ff804db88fae414db06e60548ed12129a"
   end
 
   depends_on "go" => :build
