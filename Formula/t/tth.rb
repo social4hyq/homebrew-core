f0692b2b66d58099cb50401da2f0c9dc077ffbd2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-03 03:04:07 +0800

    tth: update 4.16 bottle.

--- Formula/t/tth.rb
+++ Formula/t/tth.rb
@@ -11,17 +11,7 @@ class Tth < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "78e1ec7d5be8797a77a217fcc2554a1449311cd638fba7ef1f3c9a706c50f027"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "9de28731b96776948549eece755898c358e0df99e6078f8b6eb07baaa489efd9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "85bba0ec3b07964610a39b5686c62e7ccb25a058b8883184061aaffe3fa55306"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a630245be3f0b2b1f83a21d6af1525ee257d6211bc1c6845e31ab398eae6767"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2e2fb5e9595c2687fecaf6a24da03d4a97c53ed99621750aa9d76f66e6bd7271"
-    sha256 cellar: :any_skip_relocation, sonoma:         "567e126bb0a4f62da3051a005a9490d240d9c3d335c5998914e10c9ea991b29d"
-    sha256 cellar: :any_skip_relocation, ventura:        "8715b445b87dd7f4363ac369d328eb62a3777029052f20340f4d82f46258f2ed"
-    sha256 cellar: :any_skip_relocation, monterey:       "2f3a06677f7f7f9267f37ad78c748b4809e6f10e61cb53804ed712cd94631d9f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "0e4122efc9e5e4ae81a404ef164a420fe399d9b13d761f7805876a0f1078aeb5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d23d9a3cb16bbbd398fef5f4ed527bfcdbe5708ff653056ea1b3bdf7217783bd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dbd510899e558f579012f30fb58c59faa0ba2871ca07a9fb4201324fd80e6c09"
   end
 
   uses_from_macos "flex" => :build
