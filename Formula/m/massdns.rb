9c6e9aedc5cde0668a6059021c48b6df1e1b43eb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-03 03:07:54 +0800

    massdns: update 1.1.0 bottle.

--- Formula/m/massdns.rb
+++ Formula/m/massdns.rb
@@ -6,16 +6,7 @@ class Massdns < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "4ccc67bf2a881c34a2cd1ab5fab97cb109e9cdf1ac60eb6861ae8cea2f02b31d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a9a5d00f32970e270e6d1ab85b7ec437efdd1b74e1e4b40f6d938f74f74ee7f0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e7c9496b840fee8c62fdde8b1799a12e957ccf055a938d34c115c4a1791c9aac"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e9ffbddb8c8d7f7d2d6ce7c65a144b01c038332feee271f1168b8d2885876db"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "74cbadfee753d69341fde43f09661512be522ccfaa8c146f1c1feb08ecb181ba"
-    sha256 cellar: :any_skip_relocation, sonoma:         "0faf77da9ccb9971a4007ef15e811faaadb03cef023719efce6538a7a4a1e21d"
-    sha256 cellar: :any_skip_relocation, ventura:        "d849531a2de18b6f920761dd14353f4bd843814e6dce35a02e526998ae26b17d"
-    sha256 cellar: :any_skip_relocation, monterey:       "e91304ed064fb3001ab55bb5a61bc7830c29681583c00d001e943911e9789b75"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "1b8fc5d3cb53ae0d3f3b7e94757c3ec2cf458fec82695bda03aee3f7461f006c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "191060c2099a28427517a56d63ba4ca14c410870a44ec6a2621fbdb2394853f4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f074be8bb71995b3c4849b0d0e70bc82b7c94e698643190f32775e2cdf09bbcf"
   end
 
   depends_on "cmake" => :build
