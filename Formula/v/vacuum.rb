8bfe6f509d7560adca686f976f452f8883cc0cb7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 08:05:04 +0800

    vacuum 0.28.3

--- Formula/v/vacuum.rb
+++ Formula/v/vacuum.rb
@@ -1,8 +1,8 @@
 class Vacuum < Formula
   desc "World's fastest OpenAPI & Swagger linter"
   homepage "https://quobix.com/vacuum/"
-  url "https://github.com/daveshanley/vacuum/archive/refs/tags/v0.28.0.tar.gz"
-  sha256 "d7e8b4b46049a06bb3cb8489337d1681341cc2c2e00696adad74e2182e3ded41"
+  url "https://github.com/daveshanley/vacuum/archive/refs/tags/v0.28.3.tar.gz"
+  sha256 "c58d490b422171094f7261a903aa6c0783b199e03e4854e335915e207158e8a7"
   license "MIT"
   head "https://github.com/daveshanley/vacuum.git", branch: "main"
 
