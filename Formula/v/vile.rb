f136fa2ba76495969e9c5afde51fe3b36a2b2c0a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 16:42:39 +0800

    vile: update 9.8zb bottle.

--- Formula/v/vile.rb
+++ Formula/v/vile.rb
@@ -11,12 +11,7 @@ class Vile < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "ae7efab15213d0ae0107d3a433ff1c7f4b76b1edb01cc03939d186cd4ce98131"
-    sha256 arm64_sequoia: "3c5aad26fe9462da4a3a634c6ac8799ee3d8350b897e497bdf3e92051ad38033"
-    sha256 arm64_sonoma:  "ca644c2182d7595c99e6666d5f12d5ef2756f65010d4c13db6e896266ff0c847"
-    sha256 sonoma:        "c632b20a8b7bbc9e29711ee11e378a1c6b45767b7a671c9c0ddcb7196b7e2935"
-    sha256 arm64_linux:   "b3c3045a3194a913336c2f3e5c4e1d9de9c8f75f2ee4afb945981d36fce94f31"
-    sha256 x86_64_linux:  "d24ec87b3504534f464ab320b1ba0ba57fc907eac2eea4072116c1b751c1c032"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "851fbfb1dbb27dc7f076c1dae4457d935cb443c52c877f35cf16c47ed56e7da7"
   end
 
   uses_from_macos "flex" => :build
