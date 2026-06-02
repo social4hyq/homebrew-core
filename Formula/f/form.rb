316d4d53d3fd2eb245c3b3ce4cc59d29a52ed9e0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-02 17:05:19 +0800

    form: update 5.0.0_1 bottle.

--- Formula/f/form.rb
+++ Formula/f/form.rb
@@ -12,12 +12,7 @@ class Form < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "f8b52733af783cf3c6e22373eae30f3aa9d036d8309b71048ef8932a84c65c31"
-    sha256 cellar: :any,                 arm64_sequoia: "946e4effaa86c76d0ce562d4a58340a1bea5fe3b0bb767e4f3dc992a63ab4ff8"
-    sha256 cellar: :any,                 arm64_sonoma:  "fa434b6485ceb5ce3d2e831225b0709070b7616f45d40d3fd6395da397867b39"
-    sha256 cellar: :any,                 sonoma:        "ec045556f3270e4eb3ade5975450976c25f81d7227a5166e8b07f4a6817fd4a6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "be93cd028152d809ba05ca09a0c9f38e107c00f0e0d2d27e759b117e0098ece9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "015e675b0c42ff0b774e45f0ce7d335a436885c133a7842d78968553e27b0bfd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a6c9be5b4d92c88b7bdbb0eb369f714b684d65c19465ce10a9f9c18867021b77"
   end
 
   depends_on "flint"
