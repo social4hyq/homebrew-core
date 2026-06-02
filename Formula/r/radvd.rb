b50416066001389674b59745fd8bd2927e1ae19d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-03 03:38:49 +0800

    radvd: update 2.21 bottle.

--- Formula/r/radvd.rb
+++ Formula/r/radvd.rb
@@ -6,12 +6,7 @@ class Radvd < Formula
   license "radvd"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d64fa035f9ba5b0ac270c6371632a00245fabaa7b1672da886973cfaed249ac3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e297345eb63b45c4a464c83887ed49cb664fa638a75bad620a2d64354c9fa66"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ee4ac2ae84b7b542415c377ae60f33d36f6a36f9d8f5c9d917edefdda9b0f7bd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e6dfb5b3126fc391a1388cddbd7e0077daa8944ef578f65ff5d1af87535696e8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4e1e42f18af7226393724e6a10156a9d9bfdcf39950bc20588bec1a1b9102e3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93ae07bba1442d28744ce243d88f0f6218e5374cf39478cfb58b4ed3ce965388"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4d609a44da947a265ea71cf7206491ab007e3fdce852167c7797047dd5f66370"
   end
 
   head do
