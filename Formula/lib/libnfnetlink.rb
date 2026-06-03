db71eb8c49e5572c82ab862b5c0296d3d9136829
lanfungcing <lanfq.26@qq.com>
2026-06-03 09:41:03 +0000

    libnfnetlink 1.0.2 (new formula)

---
+++ Formula/lib/libnfnetlink.rb
@@ -0,0 +1,41 @@
+class Libnfnetlink < Formula
+  desc "Low-level library for netfilter related communication"
+  homepage "https://www.netfilter.org/projects/libnfnetlink"
+  url "https://www.netfilter.org/projects/libnfnetlink/files/libnfnetlink-1.0.2.tar.bz2"
+  sha256 "b064c7c3d426efb4786e60a8e6859b82ee2f2c5e49ffeea640cfe4fe33cbc376"
+  license "LGPL-2.1-or-later"
+  compatibility_version 1
+
+  livecheck do
+    url "https://www.netfilter.org/projects/libnfnetlink/downloads.html"
+    regex(/href=.*?libnfnetlink[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "29a3d3fc305c9252a22b3bce85447d2f895924c959185c00889cedc2d23fc78b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "927f6b62c2f87ff79d92d07e52bb312d3fb24731e694cc804179730b08f15eae"
+  end
+
+  depends_on :linux
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <libnfnetlink/libnfnetlink.h>
+
+      int main() {
+        int i = NFNL_BUFFSIZE;
+      }
+    C
+
+    system ENV.cc, "-D__MUSL__", "test.c", "-I#{include}", "-L#{lib}", "-lnfnetlink", "-o", "test"
+  end
+end
