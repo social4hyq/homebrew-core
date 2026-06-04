e5da2541397bfea96fc5ea1b642ee83bb5d5f3ac
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-04 08:13:15 +0800

    rustic 0.11.3

--- Formula/r/rustic.rb
+++ Formula/r/rustic.rb
@@ -1,8 +1,8 @@
 class Rustic < Formula
   desc "Fast, encrypted, and deduplicated backups powered by Rust"
   homepage "https://rustic.cli.rs"
-  url "https://github.com/rustic-rs/rustic/archive/refs/tags/v0.11.2.tar.gz"
-  sha256 "b65c1b432a9903f554516445588cbab796865a7058380fd7856835b081e0ec0e"
+  url "https://github.com/rustic-rs/rustic/archive/refs/tags/v0.11.3.tar.gz"
+  sha256 "abbafea18ea56f486a68f186bb139b0e8e8d002bb5cd897ed148cf7817a8ed73"
   license "Apache-2.0"
   head "https://github.com/rustic-rs/rustic.git", branch: "main"
 
@@ -10,9 +10,11 @@ class Rustic < Formula
     sha256 cellar: :any_skip_relocation, arm64_ohos: "c5f52b80dd2637fca50f640e2fd12f6bb01f8bfa2c44d8c3c25a77b814884b5a"
   end
 
+  depends_on "cmake" => :build
   depends_on "rust" => :build
 
   def install
+    ENV["AWS_LC_SYS_NO_JITTER_ENTROPY"] = "1"
     system "cargo", "install", *std_cargo_args
     generate_completions_from_executable(bin/"rustic", "completions")
   end
