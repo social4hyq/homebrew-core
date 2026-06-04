5b32d4046f6f652ac8a4a4af03937a9428f2b7e7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 06:19:02 +0800

    calm-cli 1.43.1

--- Formula/c/calm-cli.rb
+++ Formula/c/calm-cli.rb
@@ -1,8 +1,8 @@
 class CalmCli < Formula
   desc "CLI allows you to interact with the Common Architecture Language Model (CALM)"
   homepage "https://github.com/finos/architecture-as-code/tree/main/cli"
-  url "https://registry.npmjs.org/@finos/calm-cli/-/calm-cli-1.42.1.tgz"
-  sha256 "c8ddc4661d5e8274cf69668aff04abcf7339a594f309d207e3ea51df6a7933dc"
+  url "https://registry.npmjs.org/@finos/calm-cli/-/calm-cli-1.43.1.tgz"
+  sha256 "bc5e8354fc241faa380993d61b52ceb32a9ef7e41b6071e6446fa490373df4a1"
   license "Apache-2.0"
 
   bottle do
