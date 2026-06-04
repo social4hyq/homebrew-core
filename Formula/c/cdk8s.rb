ec50ba77c0fa90f5083ece3d0f468b62eee8ec49
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 06:20:51 +0800

    cdk8s 2.207.14

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -1,8 +1,8 @@
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.207.11.tgz"
-  sha256 "01c1b389f523e1ef066cbe86ea1b43367bbe4c559ec6d22215998e8ea569883a"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.207.14.tgz"
+  sha256 "5064795e2b1eea144f33edf953dd10b7742447a7f591af1bcb268a6353d568be"
   license "Apache-2.0"
 
   bottle do
