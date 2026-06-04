697e06dd364f044448f2391ab414a0d123dc8fe0
hqzing <hqzing@outlook.com>
2026-06-04 12:43:54 +0000

    gdk-pixbuf 2.44.6 (new formula)

---
+++ Formula/g/gdk-pixbuf.rb
@@ -0,0 +1,102 @@
+class GdkPixbuf < Formula
+  desc "Toolkit for image loading and pixel buffer manipulation"
+  homepage "https://gtk.org"
+  url "https://download.gnome.org/sources/gdk-pixbuf/2.44/gdk-pixbuf-2.44.6.tar.xz"
+  sha256 "140c2d0b899fcf853ee92b26373c9dc228dbcde0820a4246693f4328a27466fa"
+  license "LGPL-2.1-or-later"
+  compatibility_version 1
+
+  bottle do
+    sha256 arm64_tahoe:   "023b480ec88e40ba0b7879663177a7baaa009d93853cf74b313ce1029f8e7c5b"
+    sha256 arm64_sequoia: "eed711bc1bc0a308e57ddcdcb0c5ab3e40cd03d47ecf82f344b508e1f4317475"
+    sha256 arm64_sonoma:  "f05616e33ac2730638a5e31d6f2835b05b17b08d94feabf773e8c3a92322016a"
+    sha256 sonoma:        "af601b57fead60ddd3d08af5025d41876d37d6f1cf9348bd79e03611b6d56ff7"
+    sha256 arm64_linux:   "2cf25e29b84262b52aa087c5ea8d8f113160e1ed107f50410203cd4d3244e7aa"
+    sha256 x86_64_linux:  "de021570fbdc6b613d5d620620f8ca735371d7fd5f442fbb127eb2e0331a56df"
+  end
+
+  depends_on "docutils" => :build # for rst2man
+  depends_on "gettext" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => [:build, :test]
+  depends_on "glib"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "shared-mime-info"
+  end
+
+  # gdk-pixbuf has an internal version number separate from the overall
+  # version number that specifies the location of its module and cache
+  # files, this will need to be updated if that internal version number
+  # is ever changed (as evidenced by the location no longer existing)
+  def gdk_so_ver
+    "2.0"
+  end
+
+  def gdk_module_ver
+    "2.10.0"
+  end
+
+  def install
+    inreplace "gdk-pixbuf/meson.build",
+              "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format(gdk_pixbuf_libdir)",
+              "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format('#{HOMEBREW_PREFIX}/lib')"
+
+    ENV["DESTDIR"] = "/"
+    system "meson", "setup", "build", "-Drelocatable=false",
+                                      "-Dnative_windows_loaders=false",
+                                      "-Dtests=false",
+                                      "-Dinstalled_tests=false",
+                                      "-Dman=true",
+                                      "-Dgtk_doc=false",
+                                      "-Dpng=enabled",
+                                      "-Dtiff=enabled",
+                                      "-Djpeg=enabled",
+                                      "-Dothers=enabled",
+                                      "-Dintrospection=enabled",
+                                      "-Dglycin=disabled",
+                                      *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+
+    # Other packages should use the top-level modules directory
+    # rather than dumping their files into the gdk-pixbuf keg.
+    inreplace lib/"pkgconfig/gdk-pixbuf-#{gdk_so_ver}.pc" do |s|
+      s.change_make_var! "prefix", HOMEBREW_PREFIX
+    end
+  end
+
+  # The directory that loaders.cache gets linked into, also has the "loaders"
+  # directory that is scanned by gdk-pixbuf-query-loaders in the first place
+  def module_dir
+    "#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-#{gdk_so_ver}/#{gdk_module_ver}"
+  end
+
+  def post_install
+    ENV["GDK_PIXBUF_MODULEDIR"] = "#{module_dir}/loaders"
+    system bin/"gdk-pixbuf-query-loaders", "--update-cache"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <gdk-pixbuf/gdk-pixbuf.h>
+
+      int main(int argc, char *argv[]) {
+        GType type = gdk_pixbuf_get_type();
+        return 0;
+      }
+    C
+    flags = shell_output("pkgconf --cflags --libs gdk-pixbuf-#{gdk_so_ver}").chomp.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
