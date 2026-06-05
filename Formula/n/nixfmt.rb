28e1944232a192785db47d5090101175d57868a2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 21:10:05 +0800

    nixfmt: update 1.3.1 bottle.

--- Formula/n/nixfmt.rb
+++ Formula/n/nixfmt.rb
@@ -7,12 +7,7 @@ class Nixfmt < Formula
   head "https://github.com/NixOS/nixfmt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "4b7d9964f7d50d74fffc0689bf787203359a82186f0973df7e238dab8190c171"
-    sha256 cellar: :any, arm64_sequoia: "97ee688b11b4300b7dd59fbf4ade5a10adb669eed7d4af8752b353eab1bcfa7f"
-    sha256 cellar: :any, arm64_sonoma:  "a878a4382893a31d53f0c5c6cfcaa65ccbd7357a2162c1eefa44e05d683147b8"
-    sha256 cellar: :any, sonoma:        "54904aef1d9a8cd827d3ff4d428423e9e12a780f1bd167b97bf69e068d1a9d86"
-    sha256 cellar: :any, arm64_linux:   "547ee4926cfd41cd95f718a872e63ade591a0a97f0d841bf7f11df86f0cb3a59"
-    sha256 cellar: :any, x86_64_linux:  "2f00cf388e09036ef20dbc248a40a4e3b882849dcc0c9adbae71e46065d3a9b7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "054e6bdff4e2588b1ed25c330708e32c4a6b1777e97ea0f2985444ce0f2576cf"
   end
 
   depends_on "cabal-install" => :build
