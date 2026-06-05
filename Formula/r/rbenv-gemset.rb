ed83c57537b6e7eb90b95db7fe781b8dc7fce2fc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 08:56:36 +0800

    rbenv-gemset 0.5.102

--- Formula/r/rbenv-gemset.rb
+++ Formula/r/rbenv-gemset.rb
@@ -1,8 +1,8 @@
 class RbenvGemset < Formula
-  desc "KISS yet powerful gem / gemset management for rbenv"
+  desc "KISS yet powerful gem/set management for curious engineers and Ruby hackers"
   homepage "https://github.com/jf/rbenv-gemset"
-  url "https://github.com/jf/rbenv-gemset/archive/refs/tags/v0.5.100.tar.gz"
-  sha256 "371fc84e35e40c7c25339cb67599a0a768bc7d3d4daafb05e02ab960bde64ce4"
+  url "https://github.com/jf/rbenv-gemset/archive/refs/tags/v0.5.102.tar.gz"
+  sha256 "193f560fe169b338a63d0bc38cc56ec05bf8639f47073ba487352b1058668e5a"
   license :public_domain
   head "https://github.com/jf/rbenv-gemset.git", branch: "master"
 
@@ -17,6 +17,7 @@ class RbenvGemset < Formula
   end
 
   test do
-    assert_match "gemset.bash", shell_output("rbenv hooks exec")
+    output = shell_output("rbenv hooks exec")
+    assert_match "exec.bash", output
   end
 end
