8e29d9dd156ce2a4a71a82d1cc5a24ee78b40990
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 09:44:04 +0800

    vercel-cli 54.9.0

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -1,8 +1,8 @@
 class VercelCli < Formula
   desc "Command-line interface for Vercel"
   homepage "https://vercel.com/home"
-  url "https://registry.npmjs.org/vercel/-/vercel-54.5.1.tgz"
-  sha256 "dbfb525ba81826fa896f2bb51c3dadf7d5477c2068a83db280a0cc759024505e"
+  url "https://registry.npmjs.org/vercel/-/vercel-54.9.0.tgz"
+  sha256 "7b0e3612683a61efab783c5ec8592d8263a9eef6cdf276fb6373c2ee941aba73"
   license "Apache-2.0"
 
   bottle do
