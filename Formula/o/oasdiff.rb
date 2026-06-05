76ab790d421749553d0c84f57a714338a5884231
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 08:20:42 +0800

    oasdiff 1.18.3

--- Formula/o/oasdiff.rb
+++ Formula/o/oasdiff.rb
@@ -1,8 +1,8 @@
 class Oasdiff < Formula
   desc "OpenAPI Diff and Breaking Changes"
   homepage "https://www.oasdiff.com/"
-  url "https://github.com/oasdiff/oasdiff/archive/refs/tags/v1.18.1.tar.gz"
-  sha256 "55e69b83d75be4f9a9779e0ed04a68247d9cb80cf51206b9f96c8ffde090561b"
+  url "https://github.com/oasdiff/oasdiff/archive/refs/tags/v1.18.3.tar.gz"
+  sha256 "0e68d8a66b03e4b8c60cf7231871e93bad30ef50bf699cdc7d08d89f27891aa1"
   license "Apache-2.0"
   head "https://github.com/oasdiff/oasdiff.git", branch: "main"
 
