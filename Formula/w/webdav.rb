d2796815aaf0ad87db85eea86eee7e643156d39c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 17:51:12 +0800

    webdav: update 5.11.10 bottle.

--- Formula/w/webdav.rb
+++ Formula/w/webdav.rb
@@ -7,7 +7,7 @@ class Webdav < Formula
   head "https://github.com/hacdias/webdav.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7a81642c0011765b2e2a936c294eb606150ca8065197742d468c9644687ff0a1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d5af912dbcb0963b4f2ca3b70f8a50be9667ada604814ee38b601fb57ca5ec1f"
   end
 
   depends_on "go" => :build
