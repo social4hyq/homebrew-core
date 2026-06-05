9ee995b9c36751a4626524b83890c5aec7b9738f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 13:01:35 +0800

    mcp-toolbox: update 1.4.0 bottle.

--- Formula/m/mcp-toolbox.rb
+++ Formula/m/mcp-toolbox.rb
@@ -6,7 +6,7 @@ class McpToolbox < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "754c6d62208b3ff04ac4b96ea9561c2a98cc9bf9fc21e060a3a8e270a4d54cbd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3ebc5ce8cd809f13bc72366f3207392060a814c4da66bf85367215c024f377fa"
   end
 
   depends_on "go" => :build
