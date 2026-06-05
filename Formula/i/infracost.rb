b578b92884d35c35778f74806184ae4405c485da
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 07:00:25 +0800

    infracost 2.2.6

--- Formula/i/infracost.rb
+++ Formula/i/infracost.rb
@@ -1,8 +1,8 @@
 class Infracost < Formula
   desc "Cost estimates for Terraform, Terragrunt, and CloudFormation"
   homepage "https://www.infracost.io/docs/"
-  url "https://github.com/infracost/cli/archive/refs/tags/v2.2.3.tar.gz"
-  sha256 "ef884436bf9d789ede9e3023a352fa5c385b31091f7ff689539a323de87d436b"
+  url "https://github.com/infracost/cli/archive/refs/tags/v2.2.6.tar.gz"
+  sha256 "851fcd8225772abf9f52b3a73bc16e780ba41adcefa0867de9ca2922f9532189"
   license "Apache-2.0"
   head "https://github.com/infracost/cli.git", branch: "main"
 
