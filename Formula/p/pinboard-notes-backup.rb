7da20c9788dcbc879878d76ee6e774817fbdb5d1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 21:43:17 +0800

    pinboard-notes-backup: update 1.0.7.1 bottle.

--- Formula/p/pinboard-notes-backup.rb
+++ Formula/p/pinboard-notes-backup.rb
@@ -7,13 +7,7 @@ class PinboardNotesBackup < Formula
   head "https://github.com/bdesham/pinboard-notes-backup.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "61ab93300efacf6806b22a12a68d41abb160245fbd468320985c72541e74c08a"
-    sha256 cellar: :any,                 arm64_sequoia: "bd015512bf68cff2cce03386ecd6a33a60eda0db5aa5dce024c069f245a57f7b"
-    sha256 cellar: :any,                 arm64_sonoma:  "7f2b54f27fc60f45bd2a54fe75c9c098ee16a2112ab7f323ee0cb618f0151636"
-    sha256 cellar: :any,                 sonoma:        "87d95a1410a2d5f7332b9a86b4b822ef00bec53b26b571be9573308304b44238"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "52b12aa0e866edb547e43aa8b3203a811cdab29f55b3860047a07f4f53bf9ba9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6f27b387f833bca955fe44a98f07222cf8f31539067e5f6b538ad131df13c5a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6547549d13e06cd4cf667638d85e4af32af49053e96eb1cd1d74974b163d5fc9"
   end
 
   depends_on "cabal-install" => :build
