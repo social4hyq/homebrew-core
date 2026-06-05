6812a506ebcb87c2b8f6aad46ef51fa5aae86fa3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 21:11:13 +0800

    dhall-toml: update 1.0.4 bottle.

--- Formula/d/dhall-toml.rb
+++ Formula/d/dhall-toml.rb
@@ -7,13 +7,7 @@ class DhallToml < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git", branch: "main"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_tahoe:   "3d83e52092e335ddba928d06ae826157f26f8179ab7db6b757b4c298bf1faf60"
-    sha256 cellar: :any,                 arm64_sequoia: "996c152df3dfc9a03b591e6e73125344fb7828a98c9b735d1c8ee3b8f3e1f0d5"
-    sha256 cellar: :any,                 arm64_sonoma:  "6338c0b88b6f2eaa8a42246a7ab9c5fa6e3c265578f0fcce47a652f89895ecda"
-    sha256 cellar: :any,                 sonoma:        "c67986762a7976ae8707fb802f8350871b8d3a182b81b04c4fc92222c7cc9f9a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "40e8c1b7e7f0470c7faf90f1d53ba7372a8b581547c1259d1d8363f852318aec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bb108a5e80145086584ae1367ee0eebe475ddc8a89419415e2e1d7536c7624d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6f22b223107ce3e7720d478f89eb60af066c2245f7ff86f32c1116007e66142b"
   end
 
   depends_on "cabal-install" => :build
