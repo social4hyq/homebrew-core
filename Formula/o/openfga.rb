b07201d923891b6157eb7a4064543fe45c7f54d7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 07:40:32 +0800

    openfga 1.17.1

--- Formula/o/openfga.rb
+++ Formula/o/openfga.rb
@@ -1,8 +1,8 @@
 class Openfga < Formula
   desc "High performance and flexible authorization/permission engine"
   homepage "https://openfga.dev/"
-  url "https://github.com/openfga/openfga/archive/refs/tags/v1.16.1.tar.gz"
-  sha256 "d9e28607e7bd2c0d5efcfcc0f80c00ed0895c4dbe482cf4b807e7aad5e61b5a4"
+  url "https://github.com/openfga/openfga/archive/refs/tags/v1.17.1.tar.gz"
+  sha256 "c62b8e2b7a0674e6642f87464ac82f1d608f3299178dce440078c6ddcf002f17"
   license "Apache-2.0"
   head "https://github.com/openfga/openfga.git", branch: "main"
 
