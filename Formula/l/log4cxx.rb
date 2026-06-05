147435d8f70d4b8afc0760005a46143b8da62459
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 20:51:35 +0800

    log4cxx: update 1.7.0 bottle.

--- Formula/l/log4cxx.rb
+++ Formula/l/log4cxx.rb
@@ -7,12 +7,7 @@ class Log4cxx < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "ca7af69b56bfcf1180b4635715292f3bd9e14ee5599d33a0fd0a4a6beded079d"
-    sha256 cellar: :any,                 arm64_sequoia: "a4837b88bb67a8dd807016bb5a651f4d55eac575640fbc55d005d48c2691f8d4"
-    sha256 cellar: :any,                 arm64_sonoma:  "f0efff26e2e0c7b10fcdaea035287ad641a3117f71e70844111dde0160c1d08f"
-    sha256 cellar: :any,                 sonoma:        "5cfb39d4ac5f7f3e22d76493b522c68daae732a54fd7a5f3ed32618282c835b5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f1357e5d46a023bee08eb9c9ca34fcc3e45804accdd58e3ad05f95be402e331a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e51f972640d6cec94b172ffa187e83caa9b10b0ef1ef16922e6e252bc859dcf6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1d88f421f3b948bbed39e4c97e8eb5b8ee104ed3e82ab214c87bc9f681f04f31"
   end
 
   depends_on "cmake" => :build
