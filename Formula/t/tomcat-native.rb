0a84034d23b31ee9d03f3a054a9a599f2fc46203
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 21:55:37 +0800

    tomcat-native: update 2.0.14 bottle.

--- Formula/t/tomcat-native.rb
+++ Formula/t/tomcat-native.rb
@@ -7,12 +7,7 @@ class TomcatNative < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "01b772187233970deb6369572b96780df249dd649ee0df8d0ea377021040a1ff"
-    sha256 cellar: :any,                 arm64_sequoia: "a19f2aef3544afb5d41638df10732d2ddef713f56c9316d5a47106245a88ead6"
-    sha256 cellar: :any,                 arm64_sonoma:  "465c5a3b5ce554658afff8150d7a4094ed1beb812f4b3a3c3455bb5ee389e97a"
-    sha256 cellar: :any,                 sonoma:        "d2074e2e3e88c13c3f4654f7a486a35008d2f2f39ad7b382e7e191553c67abbd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4cd5f44f63aeb7529c886a3962daccae4b9648d5f78066f613690e07de96524e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a859cb4a87a10540f1f6ad44cbb043595d36c5049667be1097a2be24af8a54b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "21c74cd4c22bfbab277dce0a292b221713995a480ea714823e869df4c793a7c4"
   end
 
   depends_on "tomcat" => :test
