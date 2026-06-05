9d605876abf59f6eae4801a6d33a1098071aacbb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 20:52:19 +0800

    dmtx-utils: update 0.7.6_8 bottle.

--- Formula/d/dmtx-utils.rb
+++ Formula/d/dmtx-utils.rb
@@ -7,14 +7,7 @@ class DmtxUtils < Formula
   revision 8
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "0d9a9f83919daef68ed7849bb0edeb5b0c99240b8854c4ea0b16aa95b061888a"
-    sha256 cellar: :any,                 arm64_sequoia: "266e690457d4ad1f3844c37e52e3228121803ab16f5df2a8441ac8f0380a2b12"
-    sha256 cellar: :any,                 arm64_sonoma:  "1ed0bf91ff69184917d25840c46c8b1a390e116b468dba2bfebcfca4688326f4"
-    sha256 cellar: :any,                 arm64_ventura: "baccf696ad55b1eeda946e6e9e6f8085e6cee318bec01446bd260f774ec3ebd7"
-    sha256 cellar: :any,                 sonoma:        "e4166dd09301ba1d1bf09ca625d395b145b5271b6e8c6938c6299b1b6773eac7"
-    sha256 cellar: :any,                 ventura:       "57db09489948aa30d196602969ff17059e3668afd012b2bd21a56c1de4bf4e16"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "003e60bbcf5bf79f791eb3e4e695d45648ae4cdf6d2b600972d900484bc5440c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86b15ef564fc867cfe335573ed209b3c96cb509859ac0507794433316d8246b4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "290ff80fb9f3c24fdd46cad81de5511fea3de52ffb1ddb5b8a9defcd9a561186"
   end
 
   depends_on "autoconf" => :build
