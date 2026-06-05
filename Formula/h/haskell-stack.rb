bcd4073628edf83615ea388d699bd089e4aa56a0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 21:54:32 +0800

    haskell-stack: update 3.9.3 bottle.

--- Formula/h/haskell-stack.rb
+++ Formula/h/haskell-stack.rb
@@ -12,12 +12,7 @@ class HaskellStack < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "966bac3da41b3141075c09addb159676fbb19da6d720a277e892504d465336ea"
-    sha256 cellar: :any,                 arm64_sequoia: "3e73b1dbc050a2df69198f99cb120138947f69e8a4c97afb8d5a6494e525fa5c"
-    sha256 cellar: :any,                 arm64_sonoma:  "ed684089667e38615fc512b86cada8061f76d5e5068b056bd9956dba54d148a3"
-    sha256 cellar: :any,                 sonoma:        "8e4b8ace611090ae24e80d33046f7603d523bc66b2407e4ee44843e03a5ea6c5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "af0742873c1d14df060eb3e3efce2ffb20356838c6ca48c1676febd4bdcb63e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b6589ec069f26a6b7ff4cc21a94284d8a0c1c3df37ba74cbdec5f76c2b4b1db"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9e97b6ffaf3b26d60e2c4952244e7d133af56cbe949441926fe71c27aca01722"
   end
 
   depends_on "cabal-install" => :build
