a68ecf2ced67686ff543fec6045cd45b21af1f47
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-05 20:55:11 +0800

    lol-html: update 3.0.0 bottle.

--- Formula/l/lol-html.rb
+++ Formula/l/lol-html.rb
@@ -7,12 +7,7 @@ class LolHtml < Formula
   head "https://github.com/cloudflare/lol-html.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "996e618aec3e11c7545a6b17599769272e5713b50fa9b9b13630a6c11430122e"
-    sha256 cellar: :any, arm64_sequoia: "0d077d68c9b36ef2d73477d554b5bde706978ad8b2e7426d184f61cdefe470e6"
-    sha256 cellar: :any, arm64_sonoma:  "de61944da3bd3ac5f462ace058a1c6d688d862db275fb4b55d1330d10208177a"
-    sha256 cellar: :any, sonoma:        "d4401724831f7ca8897e20f39707ce932d542e8980509eeea7aa83397d6102fc"
-    sha256 cellar: :any, arm64_linux:   "2e5da20ecb2c9c47860af9f6c78000fd784ea9f0f9795484ef1d122eab536f31"
-    sha256 cellar: :any, x86_64_linux:  "56f4047c7c7f34d1b184997bcb290dcd2d597434efca9023469fb0c3cff85d99"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b4fb79653ee7785ca0b3604f288a8042f1e738bb84bb85e3b62615c22ba1915d"
   end
 
   depends_on "cargo-c" => :build
