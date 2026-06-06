dfee1b6f6fd112bb53379b83d1ce5df97b357aba
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 21:32:56 +0800

    howdoi: update 2.0.20_22 bottle.

--- Formula/h/howdoi.rb
+++ Formula/h/howdoi.rb
@@ -9,12 +9,7 @@ class Howdoi < Formula
   revision 22
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "53748f98d79028d410863d43948eeffc1f6b84aec04cfe335552ff5ed7cfad0a"
-    sha256 cellar: :any, arm64_sequoia: "67cdf2d70a3c06c878ac6c26af752b7309ffaac6cda6ff580e071c7924deaf3a"
-    sha256 cellar: :any, arm64_sonoma:  "03e569713b175d90e6dbca62b16beb0e69e4f75a990548f02f67f4ff1bc5e702"
-    sha256 cellar: :any, sonoma:        "0aa3f65dee48047c146446149dac7fd232049f18ea9ad158bdd47e5631508b7b"
-    sha256 cellar: :any, arm64_linux:   "291ddfeb45b4cbcee0caa179dbc37358eb703d2e7aa4e0c46ea9d4b6ed8b7c7e"
-    sha256 cellar: :any, x86_64_linux:  "51292bc952de5669e98bd0e10b5a943c71dd64b5f13c0a4f5bdd96e8f62ce63a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d3137da132c89c5733804dc22823e1884c6c49f71f84ad1042c0173365953bc8"
   end
 
   depends_on "certifi" => :no_linkage
