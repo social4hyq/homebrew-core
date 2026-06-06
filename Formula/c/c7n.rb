fd6c1b206b54a552c4291f95afabc5910b59377a
hqzing <hqzing@outlook.com>
2026-06-06 20:19:14 +0000

    c7n 0.9.51.0 (new formula)

---
+++ Formula/c/c7n.rb
@@ -0,0 +1,151 @@
+class C7n < Formula
+  include Language::Python::Virtualenv
+
+  desc "Rules engine for cloud security, cost optimization, and governance"
+  homepage "https://github.com/cloud-custodian/cloud-custodian"
+  url "https://github.com/cloud-custodian/cloud-custodian/archive/refs/tags/0.9.51.0.tar.gz"
+  sha256 "b13e6463b59a84950cdfe28a65b26e3fdee7dae16835adb46313ccad25adbf42"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "d5e5b0e66f6a3be249104d8771e8c2ba51b27b3dfb5e76abd35836ba2bd269a3"
+    sha256 cellar: :any,                 arm64_sequoia: "e25050313f643e5e22d5df047f7302a1875c6b1c7a8354a25404c954a02da897"
+    sha256 cellar: :any,                 arm64_sonoma:  "bf10f0d9f14200fdf535aafbcc8c0b5ac451d3681865340230ccd3498f8b5cc8"
+    sha256 cellar: :any,                 sonoma:        "176a87335ec9323d55c0fb53a0c20114473f1b021bb2346f6c9a2db284ad2a2a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "882f398737b89507da0c9c9f9fe1c1a4d77f7ea4144dfb9ebd400fb50acac07f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "269c5a9538ec529d06f9995a499b99bf1fd12b74b61bc582306696a02cb0b425"
+  end
+
+  depends_on "cryptography" => :no_linkage
+  depends_on "libyaml"
+  depends_on "python@3.14"
+  depends_on "rpds-py" => :no_linkage
+
+  pypi_packages exclude_packages: ["cryptography", "rpds-py"]
+
+  resource "argcomplete" do
+    url "https://files.pythonhosted.org/packages/38/61/0b9ae6399dd4a58d8c1b1dc5a27d6f2808023d0b5dd3104bb99f45a33ff6/argcomplete-3.6.3.tar.gz"
+    sha256 "62e8ed4fd6a45864acc8235409461b72c9a28ee785a2011cc5eb78318786c89c"
+  end
+
+  resource "attrs" do
+    url "https://files.pythonhosted.org/packages/9a/8e/82a0fe20a541c03148528be8cac2408564a6c9a0cc7e9171802bc1d26985/attrs-26.1.0.tar.gz"
+    sha256 "d03ceb89cb322a8fd706d4fb91940737b6642aa36998fe130a9bc96c985eff32"
+  end
+
+  resource "boto3" do
+    url "https://files.pythonhosted.org/packages/48/4f/f13d80d377b54dd2973e243e4eb7ce748706cd53876361cc72506006fd8b/boto3-1.43.16.tar.gz"
+    sha256 "6c337bbe608aacc7d335c79e671f0c893870293b74d652f7a7af22ccd0dfef16"
+  end
+
+  resource "botocore" do
+    url "https://files.pythonhosted.org/packages/a1/74/140451a1fe027cb5e387cc7b1ec56224616ca742c330f1492f71c5cba3fb/botocore-1.43.16.tar.gz"
+    sha256 "813dae233d8b365c19aaf7865b32070e34d7e793654881bf86ecbbef3f4ad5c6"
+  end
+
+  resource "jmespath" do
+    url "https://files.pythonhosted.org/packages/d3/59/322338183ecda247fb5d1763a6cbe46eff7222eaeebafd9fa65d4bf5cb11/jmespath-1.1.0.tar.gz"
+    sha256 "472c87d80f36026ae83c6ddd0f1d05d4e510134ed462851fd5f754c8c3cbb88d"
+  end
+
+  resource "jsonschema" do
+    url "https://files.pythonhosted.org/packages/b3/fc/e067678238fa451312d4c62bf6e6cf5ec56375422aee02f9cb5f909b3047/jsonschema-4.26.0.tar.gz"
+    sha256 "0c26707e2efad8aa1bfc5b7ce170f3fccc2e4918ff85989ba9ffa9facb2be326"
+  end
+
+  resource "jsonschema-specifications" do
+    url "https://files.pythonhosted.org/packages/19/74/a633ee74eb36c44aa6d1095e7cc5569bebf04342ee146178e2d36600708b/jsonschema_specifications-2025.9.1.tar.gz"
+    sha256 "b540987f239e745613c7a9176f3edb72b832a4ac465cf02712288397832b5e8d"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz"
+    sha256 "37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
+  end
+
+  resource "referencing" do
+    url "https://files.pythonhosted.org/packages/22/f5/df4e9027acead3ecc63e50fe1e36aca1523e1719559c499951bb4b53188f/referencing-0.37.0.tar.gz"
+    sha256 "44aefc3142c5b842538163acb373e24cce6632bd54bdb01b21ad5863489f50d8"
+  end
+
+  resource "s3transfer" do
+    url "https://files.pythonhosted.org/packages/11/b3/bcdc2f58fa92592db511beda154c2c08d28f21f6c4637f06a42a24b10c21/s3transfer-0.17.1.tar.gz"
+    sha256 "042dd5e3b1b512355e35a23f0223e426b7042e80b97830ea2680ddce327fc45e"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
+    sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
+  end
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/46/58/8c37dea7bbf769b20d58e7ace7e5edfe65b849442b00ffcdd56be88697c6/tabulate-0.10.0.tar.gz"
+    sha256 "e2cfde8f79420f6deeffdeda9aaec3b6bc5abce947655d17ac662b126e48a60d"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    generate_completions_from_executable(libexec/"bin/register-python-argcomplete", "custodian",
+                                         base_name: "custodian", shell_parameter_format: :arg)
+  end
+
+  test do
+    # trim last decimal point version to match semver returned from version command
+    assert_match version.major_minor_patch.to_s, shell_output("#{bin}/custodian version")
+
+    (testpath/"good-policy.yml").write <<~YAML
+      policies:
+      - name: ec2-auto-tag-user
+        resource: ec2
+        mode:
+          type: cloudtrail
+          role: arn:aws:iam::{account_id}:role/custodian-auto-tagger
+          # note {account_id} is optional. If you put that there instead of
+          # your actual account number, when the policy is provisioned it
+          # will automatically inherit the account_id properly
+          events:
+            - RunInstances
+        filters:
+          - tag:CreatorName: absent
+        actions:
+          - type: auto-tag-user
+            tag: CreatorName
+            principal_id_tag: CreatorId
+    YAML
+
+    output = shell_output("#{bin}/custodian validate --verbose #{testpath}/good-policy.yml 2>&1")
+    assert_match "valid", output
+
+    # has invalid "action" key instead of "actions"
+    (testpath/"bad-policy.yml").write <<~YAML
+      policies:
+      - name: ec2-auto-tag-user
+        resource: ec2
+        filters:
+          - tag:CreatorName: absent
+        action:
+          - type: auto-tag-user
+            tag: CreatorName
+            principal_id_tag: CreatorId
+    YAML
+
+    output = shell_output("#{bin}/custodian validate --verbose #{testpath}/bad-policy.yml 2>&1", 1)
+    assert_match "invalid", output
+  end
+end
