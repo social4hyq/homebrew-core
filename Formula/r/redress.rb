6402c83e55a63c6c648c5987b4942ea0ad554aa9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 08:00:42 +0800

    redress 1.2.70

--- Formula/r/redress.rb
+++ Formula/r/redress.rb
@@ -1,8 +1,8 @@
 class Redress < Formula
   desc "Tool for analyzing stripped Go binaries compiled with the Go compiler"
   homepage "https://github.com/goretk/redress"
-  url "https://github.com/goretk/redress/archive/refs/tags/v1.2.68.tar.gz"
-  sha256 "653fd4d2cebea40a9324464338899ff62b6cb88adfa2ba70a04d61ccc6aef514"
+  url "https://github.com/goretk/redress/archive/refs/tags/v1.2.70.tar.gz"
+  sha256 "a07a4ae05352e9696f9191fb05a6b9e67107d5d812d489969b99ddf9cda44aa8"
   license "AGPL-3.0-only"
   head "https://github.com/goretk/redress.git", branch: "develop"
 
