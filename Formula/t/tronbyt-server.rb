6bd446cd869c8325a16c14dc47f74a3c7066d9fd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 19:09:22 +0800

    tronbyt-server: update 2.3.1 bottle.

--- Formula/t/tronbyt-server.rb
+++ Formula/t/tronbyt-server.rb
@@ -7,7 +7,7 @@ class TronbytServer < Formula
   head "https://github.com/tronbyt/server.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "54e39c7338fdf732232967c4fbc16c23855f6fdfa26ad5b122dfd65b0ce0d8dc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c900ae3b8e0f7848c9a98d32353a4bbe5160eefdec3e8e4e3e82338ee55fe09a"
   end
 
   depends_on "go" => :build
