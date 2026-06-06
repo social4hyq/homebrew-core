5a2f3966ade2ab5df5d7fbf6d0a357ae9f12f12a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 08:21:23 +0800

    syft 1.45.1

--- Formula/s/syft.rb
+++ Formula/s/syft.rb
@@ -1,8 +1,8 @@
 class Syft < Formula
   desc "CLI for generating a Software Bill of Materials from container images"
   homepage "https://github.com/anchore/syft"
-  url "https://github.com/anchore/syft/archive/refs/tags/v1.44.0.tar.gz"
-  sha256 "cbb7a58e179901a3bc678e740a71cab9cd5558dd3bddea74f6c4edf4592e8a98"
+  url "https://github.com/anchore/syft/archive/refs/tags/v1.45.1.tar.gz"
+  sha256 "eeae4dceb6d3efded3a7209da5d051bfcba9387dbe15722f22bde5f90eb2200d"
   license "Apache-2.0"
   head "https://github.com/anchore/syft.git", branch: "main"
 
