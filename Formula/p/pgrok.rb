aa5687bfe198f1d98b8916a826f6c64fb6d80abe
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 19:24:22 +0800

    pgrok: update 1.7.0 bottle.

--- Formula/p/pgrok.rb
+++ Formula/p/pgrok.rb
@@ -11,7 +11,7 @@ class Pgrok < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "312b6b180bfd068b502251577b8e16dcd7875e3bee2f7eb49fda80b1c82dc994"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8bacfd561d7777ca04424038a4024cb74ad4547368a8f22e12710897d1c76dfc"
   end
 
   depends_on "go" => :build
