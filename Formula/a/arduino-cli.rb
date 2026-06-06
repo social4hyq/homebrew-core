64b86946e64ee0d08d21f732f4cd4f308e17e549
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 16:20:19 +0800

    arduino-cli: update 1.5.1 bottle.

--- Formula/a/arduino-cli.rb
+++ Formula/a/arduino-cli.rb
@@ -12,7 +12,7 @@ class ArduinoCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a6d278b061877cffd1a3497f12a603ae23736f3cc18a03b5b5462989e66bfada"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "46aebe446b8e0fd8a35511bfe4327a99bd8fd123cf6dc7baca73d71aa6101870"
   end
 
   depends_on "go" => :build
