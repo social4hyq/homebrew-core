076c420ced456c0e22ae70df07029009aa83b82e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 08:20:22 +0800

    superseedr 1.0.9

--- Formula/s/superseedr.rb
+++ Formula/s/superseedr.rb
@@ -1,8 +1,8 @@
 class Superseedr < Formula
   desc "BitTorrent Client in your Terminal"
   homepage "https://github.com/Jagalite/superseedr"
-  url "https://github.com/Jagalite/superseedr/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "93f6bffaa5d77a7a07152beb34e4a452da59eccc24d658b0e0402b73ae68dda1"
+  url "https://github.com/Jagalite/superseedr/archive/refs/tags/v1.0.9.tar.gz"
+  sha256 "63b79fbfba6ebff9227ab25e9e4916f1c6c58e0f07779f971856153a06b79dd4"
   license "GPL-3.0-or-later"
   head "https://github.com/Jagalite/superseedr.git", branch: "main"
 
