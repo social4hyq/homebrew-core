15e6fa8513272c8653d1876f2df4c13f5dab06c5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-06 22:03:01 +0800

    gitea-runner: update 1.0.8 bottle.

--- Formula/g/gitea-runner.rb
+++ Formula/g/gitea-runner.rb
@@ -6,7 +6,7 @@ class GiteaRunner < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a61a2d0f1eb821f8f77d2bb9b9697fedc717a23169544e2bc9ea00deeaefc523"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "56a79a169898eaef95e5c17672aec130b29de6853ab08b5f19ec98ccede3cf32"
   end
 
   depends_on "go" => :build
