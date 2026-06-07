cce40186d493841acc893aa5f9ceffe89f0a39f7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-07 21:47:10 +0800

    shellcheck: update 0.11.0 bottle.

--- Formula/s/shellcheck.rb
+++ Formula/s/shellcheck.rb
@@ -8,14 +8,7 @@ class Shellcheck < Formula
   head "https://github.com/koalaman/shellcheck.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a8c12e69512ed369ded8a58cfb81c8d42eea53d8ac51119fe3f83e050e6a6f4d"
-    sha256 cellar: :any,                 arm64_sequoia: "8cba28650eef6739b81380743c58c323a8846c6ff1cf6fd730e434b945244e5c"
-    sha256 cellar: :any,                 arm64_sonoma:  "606603b04a41787ecbafc31c4e769e190719e9a24fdd3fc181e7f10ac92cbd17"
-    sha256 cellar: :any,                 arm64_ventura: "55dc4f40dd13d13d1075910cf0791344f3d9018d04d6fe5c1686710829354d10"
-    sha256 cellar: :any,                 sonoma:        "3608bc5b421ad7cd939f3a8e3268a3c8f6369948ed555afb9d269134a0bfabe6"
-    sha256 cellar: :any,                 ventura:       "31533a851f7dbc8a808a0787dccd80ce0a22e30c099b49e5d588ec555b3d0d93"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9b6289eca1e028a336e6090a7ab42efb47993c51194941c43fbe7d156f9f4270"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e84559495ee8f9719be30a9d8139a3016d2f448d31fc79ac4d135ab86a228404"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0582888b613263f7b24c02c378646b569ad12ae409280e96fa6497e109da3267"
   end
 
   depends_on "cabal-install" => :build
