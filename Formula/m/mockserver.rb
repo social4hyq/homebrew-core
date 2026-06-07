faed4ab331d98a434aac06129df3de826c9f6408
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-07 17:20:49 +0800

    mockserver: update 7.0.0 bottle.

--- Formula/m/mockserver.rb
+++ Formula/m/mockserver.rb
@@ -11,7 +11,7 @@ class Mockserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e7f81b36bcf9bbe250e6f4d909687a140c651a5710f4012d20bbc9fe189566b1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "391da41a6877a72cde64c32a1e60ea37e7cce9beda1bf075fd132fa729ee6eb2"
   end
 
   depends_on "openjdk"
