b565a5981912406268f724e25746b27884c5ecc3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-07 15:19:39 +0800

    geoipupdate: update 7.1.1 bottle.

--- Formula/g/geoipupdate.rb
+++ Formula/g/geoipupdate.rb
@@ -7,13 +7,7 @@ class Geoipupdate < Formula
   head "https://github.com/maxmind/geoipupdate.git", branch: "main"
 
   bottle do
-    rebuild 2
-    sha256                               arm64_tahoe:   "6ffcdd5b4d20042abc7c43cfad948193d2dcc608bbcf3a0e8df942b05c0995ad"
-    sha256                               arm64_sequoia: "6edf968afba83f6d986ca2dc7ee31b3b2538f2307788fa0f51febbafc66ce702"
-    sha256                               arm64_sonoma:  "12a26bb493069864f06579428ef9a2ba1278883bc6209fa9e97cca49fb814c62"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1c1a3de1f94838ecf0836a3966342dcafa2101f3c44908823e88493ae3262574"
-    sha256                               arm64_linux:   "800bb13a38ccb17e75e9dc3c724f7e859a2bf5ba4c32e9fb55947e42c183a675"
-    sha256                               x86_64_linux:  "96189a674d2791bf8d4eb5063077bf00ba59c7c78d9044f17a12d4cf58afb114"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "61ee5460c8c670e0478352df9236fc9e0fe29a6e5088d4d393800c72f7f2bc11"
   end
 
   depends_on "go" => :build
