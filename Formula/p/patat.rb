88a2b2f98447267f4e25a4c0b59b407525cff5b6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-07 16:38:12 +0800

    patat: update 0.15.2.0 bottle.

--- Formula/p/patat.rb
+++ Formula/p/patat.rb
@@ -16,13 +16,7 @@ class Patat < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_tahoe:   "5078a6ef677ef802f2f78ca950c5e0d756ec8058a00ac43c990af4cc42b641e5"
-    sha256 cellar: :any,                 arm64_sequoia: "a959da2b101e50e8066aa852735b17ef36976a7d72bcd6af8c8e069e02352b79"
-    sha256 cellar: :any,                 arm64_sonoma:  "bb22958819bdc12b2d484ebfd732513d7f370f096d460f5010bdd029967a7d3c"
-    sha256 cellar: :any,                 sonoma:        "027fcd9e810e61b16f996ba16d4406b56c77f0928db0001eea1c2a1904e40fcd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bddf7b47c06f8906f627ef84ff4105e87e75777f4e6d6cdc3f834804e5702813"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb2810208b1abbb9b541d3eb0d576bce58c57e4af2c0b1ddad3dd465138dc962"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "613c46db1c5b4a54d23fbd063f08225a37bf48566a809a437c4cda10638eb705"
   end
 
   depends_on "cabal-install" => :build
