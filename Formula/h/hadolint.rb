c1d43f9193f0a0a8c8ef5815ae46b2ee8ff3b15f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-07 16:50:23 +0800

    hadolint: update 2.14.0 bottle.

--- Formula/h/hadolint.rb
+++ Formula/h/hadolint.rb
@@ -6,13 +6,7 @@ class Hadolint < Formula
   license "GPL-3.0-only"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "5fb72dd7116d76a9c78806853ed98d4da2f15f8ca46c9f5511ef66e3c9c36225"
-    sha256 cellar: :any,                 arm64_sequoia: "7af3406142276fb1dfe68ed7211fe9bca1e348114de6390c53f08dc5d11b5c3c"
-    sha256 cellar: :any,                 arm64_sonoma:  "29512bf0e04e72b3085f88b9240bb30c91665b622c1d8cc2d7c961d985280a47"
-    sha256 cellar: :any,                 sonoma:        "d3fb60d34da4c25bde58751038a8b9d794fe6c9863e5a945250bfa7877e5446b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f6bbbbade4308b73a44f059afe1d9926e0d787ba9a48d1508789a74d7913d0ae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5b8c9a91f19e10eced2063d35eb89e54c239e0270caed1a3ff72afd62292674"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6021fc92e8765be403102bf1c664d5ef0bb5f8346272364b0b5e080dfd7f4b26"
   end
 
   depends_on "cabal-install" => :build
