2936da829ac7c454565b703fa5e2b24194afc669
hqzing <hqzing@outlook.com>
2026-06-08 05:43:52 +0000

    credstash 1.17.1 (new formula)

---
+++ Formula/c/credstash.rb
@@ -0,0 +1,71 @@
+class Credstash < Formula
+  include Language::Python::Virtualenv
+
+  desc "Little utility for managing credentials in the cloud"
+  homepage "https://github.com/fugue/credstash"
+  url "https://files.pythonhosted.org/packages/b4/89/f929fda5fec87046873be2420a4c0cb40a82ab5e30c6d9cb22ddec41450b/credstash-1.17.1.tar.gz"
+  sha256 "6c04e8734ef556ab459018da142dd0b244093ef176b3be5583e582e9a797a120"
+  license "Apache-2.0"
+  revision 15
+  head "https://github.com/fugue/credstash.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5b734f4e5b83059c899d5aa46b394addda63d0478a1372197a8b8f992c7e9fd6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b734f4e5b83059c899d5aa46b394addda63d0478a1372197a8b8f992c7e9fd6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b734f4e5b83059c899d5aa46b394addda63d0478a1372197a8b8f992c7e9fd6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5b734f4e5b83059c899d5aa46b394addda63d0478a1372197a8b8f992c7e9fd6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ae708a36bd879d0993fb5fc88f3b4de384022618ef58686fed363e26056411f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b734f4e5b83059c899d5aa46b394addda63d0478a1372197a8b8f992c7e9fd6"
+  end
+
+  depends_on "cryptography" => :no_linkage
+  depends_on "python@3.14"
+
+  pypi_packages exclude_packages: "cryptography"
+
+  resource "boto3" do
+    url "https://files.pythonhosted.org/packages/0a/37/78c630d1308964aa9abf44951d9c4df776546ff37251ec2434944e205c4e/boto3-1.43.6.tar.gz"
+    sha256 "e6315effaf12b890b99956e6f8e2c3000a3f64e4ee91943cec3895ce9a836afb"
+  end
+
+  resource "botocore" do
+    url "https://files.pythonhosted.org/packages/79/a7/23d0f5028011455096a1eeac0ddf3cbe147b3e855e127342f8202552194d/botocore-1.43.6.tar.gz"
+    sha256 "b1e395b347356860398da42e61c808cf1e34b6fa7180cf2b9d87d986e1a06ba0"
+  end
+
+  resource "jmespath" do
+    url "https://files.pythonhosted.org/packages/d3/59/322338183ecda247fb5d1763a6cbe46eff7222eaeebafd9fa65d4bf5cb11/jmespath-1.1.0.tar.gz"
+    sha256 "472c87d80f36026ae83c6ddd0f1d05d4e510134ed462851fd5f754c8c3cbb88d"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz"
+    sha256 "37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3"
+  end
+
+  resource "s3transfer" do
+    url "https://files.pythonhosted.org/packages/9b/ec/7c692cde9125b77e84b307354d4fb705f98b8ccad59a036d5957ca75bfc3/s3transfer-0.17.0.tar.gz"
+    sha256 "9edeb6d1c3c2f89d6050348548834ad8289610d886e5bf7b7207728bd43ce33a"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
+    sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    ENV["AWS_ACCESS_KEY_ID"] = "test"
+    ENV["AWS_SECRET_ACCESS_KEY"] = "test"
+    output = shell_output("#{bin}/credstash put test test 2>&1", 1)
+    assert_match "Could not generate key using KMS key", output
+  end
+end
