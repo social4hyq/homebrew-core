b600d69660eaef95a1f103ebe1165a31bef687e8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-09 06:54:49 +0800

    keploy 3.5.63

--- Formula/k/keploy.rb
+++ Formula/k/keploy.rb
@@ -1,8 +1,8 @@
 class Keploy < Formula
   desc "Testing Toolkit creates test-cases and data mocks from API calls, DB queries"
   homepage "https://keploy.io"
-  url "https://github.com/keploy/keploy/archive/refs/tags/v3.5.61.tar.gz"
-  sha256 "a1ed9a2a70216bc8301e6231eff2e2bf5424d40223f765cee07fbcb9453399d9"
+  url "https://github.com/keploy/keploy/archive/refs/tags/v3.5.63.tar.gz"
+  sha256 "ed01a377f6297d7edf459bc683a4903e193c43383b0b801e856daa4675a4b7d3"
   license "Apache-2.0"
   head "https://github.com/keploy/keploy.git", branch: "main"
 
