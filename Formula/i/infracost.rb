2330a123ca1bdaa971edad4eded3aeca3cde8106
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-09 14:08:30 +0800

    infracost: update 2.2.7 bottle.

--- Formula/i/infracost.rb
+++ Formula/i/infracost.rb
@@ -12,7 +12,7 @@ class Infracost < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "1ed7451211cd1b9aae7e1c61bec61c4d57a47896d4a474bfc9b982164ff6987c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a1df33fe51aac7ea8ae8efc89221878a1aea3fd7fc359720a26cd31683cdd211"
   end
 
   depends_on "go" => :build
