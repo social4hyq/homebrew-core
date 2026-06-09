190497775a6a5d39a0648369b288f93873b46c3c
lanfungcing <lanfq.26@qq.com>
2026-06-09 12:58:43 +0000

    tesseract 5.5.2 (new formula)

---
+++ Formula/t/tesseract.rb
@@ -0,0 +1,101 @@
+class Tesseract < Formula
+  desc "OCR (Optical Character Recognition) engine"
+  homepage "https://tesseract-ocr.github.io/"
+  url "https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.5.2.tar.gz"
+  sha256 "6235ea0dae45ea137f59c09320406f5888383741924d98855bd2ce0d16b54f21"
+  license "Apache-2.0"
+  compatibility_version 1
+  head "https://github.com/tesseract-ocr/tesseract.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256               arm64_tahoe:   "5c573461bfef6b219120a51bff457cc7eca377c43b971cc5639c109517b1daf4"
+    sha256               arm64_sequoia: "2581ad24c87515f9dfb218ebbec5681bd36af0f4d6ec561dd91bf91cd412a61d"
+    sha256               arm64_sonoma:  "d30bb6d0c4f2675db059a902ea9a8d3a7a6469b5b256ee11f7128b0836df6a8e"
+    sha256 cellar: :any, sonoma:        "002f53df778424f6973071d5422061f2a07bd6da9294e05fbe5847a6763e4820"
+    sha256               arm64_linux:   "56c554781e18f586dc232a1a08ff4b866948cd32e8c99a58c1bfc41dc434b165"
+    sha256               x86_64_linux:  "89e359d2cb5283131f6d74c2bc4e26cbeb65ad36a3dad40eca500464eb5c5933"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkgconf" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "glib"
+  depends_on "harfbuzz"
+  depends_on "icu4c@78"
+  depends_on "leptonica"
+  depends_on "libarchive"
+  depends_on "pango"
+
+  on_macos do
+    depends_on "freetype"
+    depends_on "gettext"
+  end
+
+  resource "eng" do
+    url "https://github.com/tesseract-ocr/tessdata_fast/raw/4.1.0/eng.traineddata"
+    sha256 "7d4322bd2a7749724879683fc3912cb542f19906c83bcc1a52132556427170b2"
+  end
+
+  resource "osd" do
+    url "https://github.com/tesseract-ocr/tessdata_fast/raw/4.1.0/osd.traineddata"
+    sha256 "9cf5d576fcc47564f11265841e5ca839001e7e6f38ff7f7aacf46d15a96b00ff"
+  end
+
+  resource "snum" do
+    url "https://github.com/USCDataScience/counterfeit-electronics-tesseract/raw/319a6eeacff181dad5c02f3e7a3aff804eaadeca/Training%20Tesseract/snum.traineddata"
+    sha256 "36f772980ff17c66a767f584a0d80bf2302a1afa585c01a226c1863afcea1392"
+  end
+
+  patch do
+    file "Patches/tesseract/long-double.patch"
+  end
+
+  def install
+    # explicitly state leptonica header location, as the makefile defaults to /usr/local/include,
+    # which doesn't work for non-default homebrew location
+    ENV["LIBLEPT_HEADERSDIR"] = HOMEBREW_PREFIX/"include"
+
+    ENV.cxx11
+
+    system "./autogen.sh"
+    system "./configure", "--datarootdir=#{HOMEBREW_PREFIX}/share",
+                          "--disable-silent-rules",
+                          *std_configure_args
+
+    system "make", "training"
+
+    # make install in the local share folder to avoid permission errors
+    system "make", "install", "training-install", "datarootdir=#{share}"
+
+    resource("snum").stage { mv "snum.traineddata", share/"tessdata" }
+    resource("eng").stage { mv "eng.traineddata", share/"tessdata" }
+    resource("osd").stage { mv "osd.traineddata", share/"tessdata" }
+  end
+
+  def caveats
+    <<~EOS
+      This formula contains only the "eng", "osd", and "snum" language data files.
+      If you need any other supported languages, run `brew install tesseract-lang`.
+    EOS
+  end
+
+  test do
+    resource "homebrew-test_resource" do
+      url "https://raw.githubusercontent.com/tesseract-ocr/test/6dd816cdaf3e76153271daf773e562e24c928bf5/testing/eurotext.tif"
+      sha256 "7b9bd14aba7d5e30df686fbb6f71782a97f48f81b32dc201a1b75afe6de747d6"
+    end
+
+    resource("homebrew-test_resource").stage do
+      system bin/"tesseract", "./eurotext.tif", "./output", "-l", "eng"
+      assert_match "The (quick) [brown] {fox} jumps!\n", File.read("output.txt")
+    end
+  end
+end
