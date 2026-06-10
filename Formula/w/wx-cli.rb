0ef09076a8229c111ab27b69ed05c52ff0b6cbbe
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-10 16:59:06 +0800

    wx-cli: update 0.3.0 bottle.

--- Formula/w/wx-cli.rb
+++ Formula/w/wx-cli.rb
@@ -6,12 +6,7 @@ class WxCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1b817528ff5a05e6973dc952859b07d501766e9918cc42d2966d12dfcd0e4049"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa6081b612cd39b29a29990b0fe5cdd7c21ba681000f16301a78cd2ed981eda8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "29b50cedb0e7d19ed0e55bd208705f5577abff98931d02aa1feb4c47327ad9b3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "89eb31df09f7a7375c0326c5bf42e3a2b88f34bf02abe320fa478ce733333814"
-    sha256 cellar: :any,                 arm64_linux:   "c8ba8710e4be87e4afffb3908dc6573061d9f14cd93d40dcd3af3e745ce3ede4"
-    sha256 cellar: :any,                 x86_64_linux:  "b0741780081ff08ed00f9a44f4a43748d534a77324d447080bc3113bb1612bca"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "21d857856b78fdd325517ba05e0fa2a7b0c1f46a1d4232b4170e684a6b814c38"
   end
 
   depends_on "rust" => :build
