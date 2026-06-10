ec6341aedd21d3a53f01d3942201774c13ff9f23
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-11 07:25:30 +0800

    oksh 7.9

--- Formula/o/oksh.rb
+++ Formula/o/oksh.rb
@@ -1,14 +1,16 @@
 class Oksh < Formula
   desc "Portable OpenBSD ksh, based on the public domain Korn shell (pdksh)"
   homepage "https://github.com/ibara/oksh"
-  url "https://github.com/ibara/oksh/releases/download/oksh-7.8/oksh-7.8.tar.gz"
-  sha256 "3b30d5a1183b829590cc020d8ab87f22d288e98dc3fdf12feb7159536beaa950"
+  url "https://github.com/ibara/oksh/releases/download/oksh-7.9/oksh-7.9.tar.gz"
+  sha256 "51b2d92515950c959dbf24f6fc33336db8c0526c2a50fee4ca598a18a6114a49"
   license all_of: [:public_domain, "BSD-3-Clause", "ISC"]
   head "https://github.com/ibara/oksh.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "be463af96b2f8aaaafb307f561722c2b2062c211efbbb73e0c07c0a45865f55a"
   end
+
+  uses_from_macos "ncurses"
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
