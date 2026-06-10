87ab31fa0bd3d42f01fd2904a32c0a503b5dd828
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-11 06:14:17 +0800

    bgrep 0.3

--- Formula/b/bgrep.rb
+++ Formula/b/bgrep.rb
@@ -1,8 +1,8 @@
 class Bgrep < Formula
   desc "Like grep but for binary strings"
   homepage "https://github.com/tmbinc/bgrep"
-  url "https://github.com/tmbinc/bgrep/archive/refs/tags/bgrep-0.2.tar.gz"
-  sha256 "24c02393fb436d7a2eb02c6042ec140f9502667500b13a59795388c1af91f9ba"
+  url "https://github.com/tmbinc/bgrep/archive/refs/tags/bgrep-0.3.tar.gz"
+  sha256 "a54ad3101150d750c180718b487406bef630f062a5ef30fde59be10a7caaaf1e"
   license "BSD-2-Clause"
   head "https://github.com/tmbinc/bgrep.git", branch: "master"
 
