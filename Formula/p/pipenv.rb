b8eef28274e96b6da6d77935ba847d92ddf3a3e5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-10 15:06:39 +0800

    pipenv: update 2026.6.2 bottle.

--- Formula/p/pipenv.rb
+++ Formula/p/pipenv.rb
@@ -9,7 +9,7 @@ class Pipenv < Formula
   head "https://github.com/pypa/pipenv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e87a8bdf3a337663a81b07e561fd2313224705503aec3dffbc1dbd33b93802fd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c40f0a713ba3526eb93ab964eaf52392cc5d5523b6a1b9488946cb7387975c7f"
   end
 
   depends_on "certifi" => :no_linkage
