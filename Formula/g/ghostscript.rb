4cd991058826ab34843fbd817939fc9083b9d95e
lanfungcing <lanfq.26@qq.com>
2026-06-10 02:33:22 +0000

    ghostscript 10.07.1 (new formula)

---
+++ Formula/g/ghostscript.rb
@@ -0,0 +1,108 @@
+class Ghostscript < Formula
+  desc "Interpreter for PostScript and PDF"
+  homepage "https://www.ghostscript.com/"
+  url "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs10071/ghostpdl-10.07.1.tar.xz"
+  sha256 "56f6a82907c3a73bba95de1319e029adf16477e34df2dea180d390e71e7c4053"
+  license "AGPL-3.0-or-later"
+  compatibility_version 1
+
+  # The GitHub tags omit delimiters (e.g. `gs9533` for version 9.53.3). The
+  # `head` repository tags are formatted fine (e.g. `ghostpdl-9.53.3`) but a
+  # version may be tagged before the release is available on GitHub, so we
+  # check the version from the first-party website instead.
+  livecheck do
+    url "https://www.ghostscript.com/json/settings.json"
+    strategy :json do |json|
+      json["GS_VER"]
+    end
+  end
+
+  no_autobump! because: :incompatible_version_format
+
+  bottle do
+    sha256 arm64_tahoe:   "6a3f21338c5e00b6d99930d96bdd12c9884147d033ae03f0ba3f3ae9d3f5ed06"
+    sha256 arm64_sequoia: "9011189c67f9727d4ada8f29201d320d0b0fb478dbc56449a1cbf4d44fa1aa2c"
+    sha256 arm64_sonoma:  "bbfa666526be13321a6ad2dfb557661b318c085ac12df041f10501486179406e"
+    sha256 sonoma:        "44e942ac04a6dd9f856f989483c86b372e8b86aac831acba7fc8b754292bc3e3"
+    sha256 arm64_linux:   "7c27cf6a1db4ccbaf969f57d15af41a18b5547424b1c9e3683ea320a0d69c241"
+    sha256 x86_64_linux:  "82acdc247c1252a412cb239f5de814e6bc4b8f6cfff4050b64716b0244d55bdb"
+  end
+
+  head do
+    url "https://github.com/ArtifexSoftware/ghostpdl.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "jbig2dec"
+  depends_on "jpeg-turbo"
+  depends_on "leptonica"
+  depends_on "libarchive"
+  depends_on "libidn"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "little-cms2"
+  depends_on "openjpeg"
+  depends_on "tesseract"
+
+  uses_from_macos "expat"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  conflicts_with "gambit-scheme", because: "both install `gsc` binary"
+  conflicts_with "gerbil-scheme", because: "both install `gsc` binary"
+
+  # https://sourceforge.net/projects/gs-fonts/
+  resource "fonts" do
+    url "https://downloads.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz"
+    sha256 "0eb6f356119f2e49b2563210852e17f57f9dcc5755f350a69a46a0d641a0c401"
+  end
+
+  def install
+    # Delete local vendored sources so build uses system dependencies
+    libs = %w[expat freetype jbig2dec jpeg lcms2mt leptonica libpng openjpeg tesseract tiff zlib]
+    libs.each { |l| rm_r(buildpath/l) }
+
+    configure = build.head? ? "./autogen.sh" : "./configure"
+
+    args = %w[--disable-compile-inits
+              --disable-cups
+              --disable-gtk
+              --with-system-libtiff
+              --without-versioned-path
+              --without-x]
+
+    # Set the correct library install names so that `brew` doesn't need to fix them up later.
+    ENV["DARWIN_LDFLAGS_SO_PREFIX"] = "#{opt_lib}/"
+    ENV.append_to_cflags "-fPIC" if OS.linux?
+    ENV["XCFLAGS"] = ENV.cflags
+    ENV["XCXXFLAGS"] = ENV.cxxflags
+
+    system configure, *args, *std_configure_args
+
+    # Install binaries and libraries
+    system "make", "install"
+    ENV.deparallelize { system "make", "install-so" }
+
+    (pkgshare/"fonts").install resource("fonts")
+  end
+
+  def caveats
+    <<~CAVEATS
+      Ghostscript is now built `--without-versioned-path`. Temporary backwards
+      compatibility symlinks exist but will be removed with 10.07.0 release.
+    CAVEATS
+  end
+
+  test do
+    ps = test_fixtures("test.ps")
+    assert_match "Hello World!", shell_output("#{bin}/ps2ascii #{ps}")
+  end
+end
