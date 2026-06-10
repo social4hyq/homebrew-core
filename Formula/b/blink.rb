be3919ea9e4b162746af9df4e0c38d911c0b8ca4
hqzing <hqzing@outlook.com>
2026-06-10 01:51:33 +0000

    blink 1.1.0 (new formula)

---
+++ Formula/b/blink.rb
@@ -0,0 +1,52 @@
+class Blink < Formula
+  desc "Tiniest x86-64-linux emulator"
+  homepage "https://github.com/jart/blink"
+  license "ISC"
+  head "https://github.com/jart/blink.git", branch: "master"
+
+  stable do
+    url "https://github.com/jart/blink/archive/refs/tags/1.1.0.tar.gz"
+    sha256 "2649793e1ebf12027f5e240a773f452434cefd9494744a858cd8bff8792dba68"
+
+    # Backport fix for `pointer not aligned at _kWhence+0x4`
+    patch do
+      url "https://github.com/jart/blink/commit/f93880dd38877914b71c2276bb476329a9e24ed0.patch?full_index=1"
+      sha256 "c7f6b41eaf9c0edee25191b6aa9e8dd21d29734c1474919f5f67685893bf20d7"
+    end
+  end
+
+  bottle do
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fe96f01e4cb3db83a0d8fe235ce54e5a00ff81d82da4872ac75772d93b49fe02"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "728a23165b6adb6121ef66bf6b0ef9b9e68d863eb66b33bf40814d19bda11134"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ce2f125a8c3ce801bd477453488423c476a1768310bc6da2134a1e79f5edef6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fbbe63a33126b154702707e358e10da2d4adaf9d9fe53db3cad1ed78f9223dac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "53e2fc3a0987c15b8d8062fd564c33e2a2ed9718ae642480ccbad551b8076922"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd87330b12e6d29f51065b58eeef882b7509537e145218f5d49b3102caf6e53e"
+  end
+
+  depends_on "pkgconf" => :build
+
+  on_macos do
+    depends_on "make" => :build # Needs Make 4.0+
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--enable-vfs"
+    # Call `make` as `gmake` to use Homebrew `make`.
+    system "gmake" # must be separate steps.
+    system "gmake", "install"
+  end
+
+  test do
+    stable.stage testpath
+    ENV["BLINK_PREFIX"] = testpath
+    goodhello = "third_party/cosmo/goodhello.elf"
+    chmod "+x", goodhello
+    system bin/"blink", "-m", goodhello
+  end
+end
