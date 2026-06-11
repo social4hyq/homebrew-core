692c17d9f336135bcbe4511c5c7862c02a092542
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-11 10:30:04 +0800

    powerman-dockerize: update 0.25.0 bottle.

--- Formula/p/powerman-dockerize.rb
+++ Formula/p/powerman-dockerize.rb
@@ -7,7 +7,7 @@ class PowermanDockerize < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "190b82a144284ce2f93797dd38b1cadd1df3e5442bdfc868e80b236f16dd8595"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9afa0e14bcc540305473d681a42a0501753d906163cd96282a1cfd40f875342f"
   end
 
   depends_on "go" => :build
