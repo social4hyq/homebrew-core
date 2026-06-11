62ff1d2566aeff879fc34ac0f9b7bf912310e7de
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-12 07:00:17 +0800

    ldns 1.9.2

--- Formula/l/ldns.rb
+++ Formula/l/ldns.rb
@@ -1,8 +1,8 @@
 class Ldns < Formula
   desc "DNS library written in C"
   homepage "https://nlnetlabs.nl/projects/ldns/"
-  url "https://nlnetlabs.nl/downloads/ldns/ldns-1.9.1.tar.gz"
-  sha256 "e2aa09b6d88c6aa01efa658d7a8aa9d9922d557a9cf166818991f81e1cab2b61"
+  url "https://nlnetlabs.nl/downloads/ldns/ldns-1.9.2.tar.gz"
+  sha256 "b524fa21994b6e834200ceb8c27f1b84bda5982fe35706f058196c079db94d5d"
   license "BSD-3-Clause"
   compatibility_version 1
 
