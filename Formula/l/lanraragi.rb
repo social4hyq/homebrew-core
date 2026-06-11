0de2e5e7efde57973f2abb364c186605a4a7471d
hqzing <hqzing@outlook.com>
2026-06-11 08:19:59 +0000

    lanraragi 0.9.71 (new formula)

---
+++ Formula/l/lanraragi.rb
@@ -0,0 +1,98 @@
+class Lanraragi < Formula
+  desc "Web application for archival and reading of manga/doujinshi"
+  homepage "https://github.com/Difegue/LANraragi"
+  url "https://github.com/Difegue/LANraragi/archive/refs/tags/v.0.9.71.tar.gz"
+  sha256 "4dab46dddd2c227bd0428eef4318cad673fca1e2a1420eee1fa2110043827408"
+  license "MIT"
+  head "https://github.com/Difegue/LANraragi.git", branch: "dev"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "82f115b1051d6ae57411796a4a239c639106ee35ccdf1d73c4c4bb5b0979dcb1"
+    sha256 cellar: :any,                 arm64_sequoia: "e09a9184d798b6dfe60d293111c0f353546581f772b9ad3e45de40a851d50f53"
+    sha256 cellar: :any,                 arm64_sonoma:  "5656dc03498a8963e26890841f82298174fc928c8b5a6d0089b402fc97a0cf87"
+    sha256 cellar: :any,                 sonoma:        "339388cc5e250d86fe2022def364a219eebca2f0595f95bb6ac4685d3e897d9e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c177edbccbda0156e095788003c56ac911fc515d3ad941a079ac98d8ff4cb58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0157925219e3eb6d4b3ba454387d9b66d1eb41b9560e24da521d701b7d106f11"
+  end
+
+  depends_on "cpanminus" => :build
+  depends_on "pkgconf" => :build
+
+  depends_on "ghostscript"
+  depends_on "imagemagick"
+  depends_on "libarchive"
+  depends_on "node"
+  depends_on "openssl@3"
+  depends_on "perl" # perl >= 5.36.0
+  depends_on "redis" # TODO: migrate to `valkey`
+  depends_on "zstd"
+
+  uses_from_macos "libffi"
+
+  resource "Image::Magick" do
+    url "https://cpan.metacpan.org/authors/id/J/JC/JCRISTY/Image-Magick-7.1.2-3.tar.gz"
+    sha256 "dc6ee21aed560d36f36be608909344bd2e25d63ab5d959553401e02f5df28a6b"
+
+    livecheck do
+      url :url
+      regex(/href=.*?Image-Magick[._-]v?(\d+(?:\.\d+)*(?:-\d+)?)\.t/i)
+    end
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    ENV["OPENSSL_PREFIX"] = Formula["openssl@3"].opt_prefix
+    ENV["ARCHIVE_LIBARCHIVE_LIB_DLL"] = Formula["libarchive"].opt_lib/shared_library("libarchive")
+    ENV["ALIEN_INSTALL_TYPE"] = "system"
+
+    imagemagick = Formula["imagemagick"]
+    resource("Image::Magick").stage do
+      inreplace "Makefile.PL",
+                "/usr/local/include/ImageMagick-#{imagemagick.version.major}",
+                "#{imagemagick.opt_include}/ImageMagick-#{imagemagick.version.major}"
+
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
+    system "cpanm", "Config::AutoConf", "--notest", "-l", libexec
+    system "npm", "install", *std_npm_args(prefix: false)
+    system "perl", "./tools/install.pl", "install-full"
+
+    # Modify Archive::Libarchive to help find brew `libarchive`. Although environment
+    # variables like `ARCHIVE_LIBARCHIVE_LIB_DLL` and `FFI_CHECKLIB_PATH` exist,
+    # it is difficult to guarantee every way of running (like `npm start`) uses them.
+    inreplace libexec/"lib/perl5/Archive/Libarchive/Lib.pm",
+              "$ENV{ARCHIVE_LIBARCHIVE_LIB_DLL}",
+              "'#{ENV["ARCHIVE_LIBARCHIVE_LIB_DLL"]}'"
+
+    (libexec/"lib").install Dir["lib/*"]
+    (libexec/"tools").install "tools/openapi.yaml"
+    libexec.install "script", "package.json", "public", "locales", "templates", "tests", "lrr.conf"
+    libexec.install "tools/build/homebrew/redis.conf"
+    bin.install "tools/build/homebrew/lanraragi"
+  end
+
+  test do
+    # Make sure lanraragi writes files to a path allowed by the sandbox
+    ENV["LRR_LOG_DIRECTORY"] = ENV["LRR_TEMP_DIRECTORY"] = testpath
+    %w[server.pid shinobu.pid minion.pid].each { |file| touch file }
+
+    # Set PERL5LIB as we're not calling the launcher script
+    ENV["PERL5LIB"] = libexec/"lib/perl5"
+
+    # This can't have its _user-facing_ functionality tested in the `brew test`
+    # environment because it needs Redis. It fails spectacularly tho with some
+    # table flip emoji. So let's use those to confirm _some_ functionality.
+    output = <<~EOS
+      ｷﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━!!!!!
+      (╯・_>・）╯︵ ┻━┻
+      It appears your Redis database is currently not running.
+      The program will cease functioning now.
+    EOS
+    # Execute through npm to avoid starting a redis-server
+    return_value = OS.mac? ? 61 : 111
+    assert_match output, shell_output("npm start --prefix #{libexec}", return_value)
+  end
+end
