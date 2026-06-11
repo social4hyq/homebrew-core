916baa172146648c96e0b18e00e2b61d80627ccf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-11 11:50:41 +0800

    awscli: update 2.35.2 bottle.

--- Formula/a/awscli.rb
+++ Formula/a/awscli.rb
@@ -10,7 +10,7 @@ class Awscli < Formula
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "55e007ed578935b7a3f9ceb8a66119f8146471a3a134dd810d45949a4a9cabdd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7b3ad25c16578182d244dcad0a89d9cba0a289cceaddde929c0f1530ed236d2c"
   end
 
   depends_on "cmake" => :build
