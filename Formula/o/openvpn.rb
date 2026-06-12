948c6b9689c25fa49ef112cf8938ad267b1e1ac9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-12 14:59:15 +0800

    openvpn: update 2.7.4 bottle.

--- Formula/o/openvpn.rb
+++ Formula/o/openvpn.rb
@@ -12,12 +12,7 @@ class Openvpn < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "65b71c2861f4b45d3e44e15fb84541d3649ea9e5197187a0b702032b06e602b4"
-    sha256 arm64_sequoia: "20ca5074988d9ea5976207d42a70745060a91a0e494ba2199dcf21e839c2150d"
-    sha256 arm64_sonoma:  "f90a2ab2e73d3c18ee338ba5d3f003a054669740bca8f00e3274014ab13ccfc9"
-    sha256 sonoma:        "e14dd35951c347f614e22994e40a36da2ba0440ba59dc29f4ad7c18cb7c577d9"
-    sha256 arm64_linux:   "eb36465f2ce87e1df1610e6f6385e7377dd4c8a29d709e9f5f9954fdc88e7093"
-    sha256 x86_64_linux:  "55bd88f9758d78c578fb5f36f2670cb7e1287e91030a52fdcd867d9111183fd2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9a16b98c08b2d54cc926f99fa3c38d0e438cf8298a09e076f1816379f7560bd8"
   end
 
   depends_on "pkgconf" => :build
