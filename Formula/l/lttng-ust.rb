af7c1fface93deb8f774c23aae32c06722e17b21
hqzing <hqzing@outlook.com>
2026-06-12 15:52:17 +0800

    lttng-ust 2.15.0 (new formula)

---
+++ Formula/l/lttng-ust.rb
@@ -0,0 +1,42 @@
+class LttngUst < Formula
+  desc "Linux Trace Toolkit Next Generation Userspace Tracer"
+  homepage "https://lttng.org/"
+  url "https://lttng.org/files/lttng-ust/lttng-ust-2.15.0.tar.bz2"
+  sha256 "6bc9723c7a50dd00c7da83d40b3bbb4b0746819f4d6e377c9bd897c898993e28"
+  license all_of: ["LGPL-2.1-only", "MIT", "GPL-2.0-only", "BSD-3-Clause", "BSD-2-Clause", "GPL-3.0-or-later"]
+
+  livecheck do
+    url "https://lttng.org/download/"
+    regex(/href=.*?lttng-ust[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "ae17463266ec81f0c13af2523c6eec514d908e907eca4e415b5092b4258de9d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5348e0b6be2ca8554dd9bcb1bce4b4e82408f954fd9505e19f45b4a384bd6b93"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on :linux
+  depends_on "numactl"
+  depends_on "userspace-rcu"
+
+  # OpenHarmony: the dynamic linker may not support dlclose at destructor time.
+  # Skip abort() and let the OS reclaim resources at process exit.
+  patch do
+    file "Patches/lttng-ust/0001-ohos-skip-dlclose-abort-on-OHOS.patch"
+  end
+
+  def install
+    # lld on OHOS defaults to --no-allow-shlib-undefined which is stricter than GNU ld.
+    # Shared libraries may reference pthread symbols resolved at runtime.
+    ENV.append "LDFLAGS", "-Wl,--allow-shlib-undefined"
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    cp_r (share/"doc/lttng-ust/examples/demo").children, testpath
+    system "make"
+    system "./demo"
+  end
+end
