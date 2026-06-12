e1cf31c568d2b1f99de8e1672d79e3b4ff2000a0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-12 17:56:11 +0800

    c2patool: update 0.26.67 bottle.

--- Formula/c/c2patool.rb
+++ Formula/c/c2patool.rb
@@ -12,7 +12,7 @@ class C2patool < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7cbd571278a59e53d88b825331a966d40c40adeba620d3bdbfacde13efe573d7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8808b389d7be02c03770901461024e6915e67f8bf45d60cbcc6a65d6bb7044a9"
   end
 
   depends_on "pkgconf" => :build
