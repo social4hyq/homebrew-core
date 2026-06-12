0677f5c365b7d0c3be642d9b580f847032dc528b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-12 16:05:15 +0800

    pyscn: update 1.24.0 bottle.

--- Formula/p/pyscn.rb
+++ Formula/p/pyscn.rb
@@ -7,7 +7,7 @@ class Pyscn < Formula
   head "https://github.com/ludo-technologies/pyscn.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b997a03cb095f39b4625320bdb7edbda935df69e625535015574dedfcf1c1dab"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c168671b40160c96244e31be1fe16532d0d54403b868250422c5c9eedfdd7cca"
   end
 
   depends_on "go" => :build
