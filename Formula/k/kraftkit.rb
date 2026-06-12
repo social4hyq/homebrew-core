56721046db6dc35a97dbd31cd772d546809342dc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-12 14:45:46 +0800

    kraftkit: update 0.12.13 bottle.

--- Formula/k/kraftkit.rb
+++ Formula/k/kraftkit.rb
@@ -12,7 +12,7 @@ class Kraftkit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7b78da5e4f40deab9c6c8778b4851a338bde00d91aeb4bd5a7ed857961846249"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "57de190124712fec14c7642c384b0d6dd957bdf86229ae955b01448678d56ace"
   end
 
   depends_on "go" => :build
