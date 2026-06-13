e7b478f4125de835fdcb2082efb20eb8bcab4690
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-13 19:36:51 +0800

    pscale: update 0.290.0 bottle.

--- Formula/p/pscale.rb
+++ Formula/p/pscale.rb
@@ -7,7 +7,7 @@ class Pscale < Formula
   head "https://github.com/planetscale/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f3e38f37cd2d675ba90da49dff36d8d6eb66b50f617e2725c7460eb5ceb580f1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "116350e6b6c5143bd79400d5f428c8d134e980d782de69147cc44cf37d579579"
   end
 
   depends_on "go" => :build
