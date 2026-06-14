1f6166ae4be89cbf752ba59b1c37da844080eedd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-14 13:17:42 +0800

    quill: update 12.0.0 bottle.

--- Formula/q/quill.rb
+++ Formula/q/quill.rb
@@ -7,7 +7,7 @@ class Quill < Formula
   head "https://github.com/odygrd/quill.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3ff432fbd8f5c32313acdc76b0ad979b23d233d688381c5b93a69af769b69be8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "38b7feafc7987c4bc08ad0aa7ba8e8b59143f6d2adddec171976cd04364a8930"
   end
 
   depends_on "cmake" => :build
