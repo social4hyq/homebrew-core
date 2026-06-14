1fe7046024ac0155fc8d12368ee6468e9745c926
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-14 13:30:46 +0800

    jaguar: update 1.67.0 bottle.

--- Formula/j/jaguar.rb
+++ Formula/j/jaguar.rb
@@ -12,7 +12,7 @@ class Jaguar < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c0145c834b557224780e49d04dafb36eb5c9643d983cdbdc19f721d368a77b1f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "79d51e01239adb1db7f634f323159ea327b0f4b7ab2d0b95a93f7b251fbdca29"
   end
 
   depends_on "go" => :build
