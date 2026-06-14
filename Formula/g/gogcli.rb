f8c44053fcff1e895ff08486c2f3162544ebfd97
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-15 06:38:05 +0800

    gogcli 0.27.0

--- Formula/g/gogcli.rb
+++ Formula/g/gogcli.rb
@@ -1,8 +1,8 @@
 class Gogcli < Formula
   desc "Google Suite CLI"
   homepage "https://gogcli.sh"
-  url "https://github.com/steipete/gogcli/archive/refs/tags/v0.25.0.tar.gz"
-  sha256 "584ee03fed7cfeb24d706cc0e0f0b156e6dc27aa36f69892e47be8736196c21b"
+  url "https://github.com/steipete/gogcli/archive/refs/tags/v0.27.0.tar.gz"
+  sha256 "4779d1a093005a7881b6e157c3d270bbb60debe715e7dfb6a53442776aa295c9"
   license "MIT"
   head "https://github.com/steipete/gogcli.git", branch: "main"
 
