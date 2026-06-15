e8b86d0d8f06178bafc56f153da6fd1e13d94413
hu60cn <hu60.cn@qq.com>
2026-06-15 10:41:42 +0800

    ffmpeg 8.1.1 (new formula)

---
+++ Formula/f/ffmpeg.rb
@@ -0,0 +1,120 @@
+class Ffmpeg < Formula
+  desc "Play, record, convert, and stream select audio and video codecs"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-8.1.1.tar.xz"
+  sha256 "b6863adde98898f42602017462871b5f6333e65aec803fdd7a6308639c52edf3"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
+  # Passing `--enable-version3` changes the license to GPL v3+.
+  license "GPL-3.0-or-later"
+  compatibility_version 2
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  livecheck do
+    url "https://ffmpeg.org/download.html"
+    regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    rebuild 1
+    sha256 arm64_tahoe:   "1d2ef725aa9eb91b3d470a3d6e19257563c927e18a49ba94f619714a2081f3bc"
+    sha256 arm64_sequoia: "b7e349b89442406114070817698304fc124b523e0e76ba33cbb40f3021ef53f0"
+    sha256 arm64_sonoma:  "be2cf1454e80af1a13544df9081d5c5e51d76b81deaa18a4535182f34ecc3198"
+    sha256 sonoma:        "9f89a6a409346ea953a15a0ee27011ccdf660e09fb600ae2a064f4d637b0517e"
+    sha256 arm64_linux:   "2b4868d28550b7a87666d3ce7c431b6fe1de1021b6284addec53e456033a527a"
+    sha256 x86_64_linux:  "85a0a35aa20c97721c647f6a973f13bd56194b9d767b007376abb41e4d2709e1"
+  end
+
+  depends_on "pkgconf" => :build
+
+  # Only add dependencies required for dependents in homebrew-core
+  # or INCREDIBLY widely used and light codecs in the current year (2026).
+  # Add other dependencies to ffmpeg-full formula.
+  # We should expect to remove e.g. x264 eventually (>=2027) when usage of it is
+  # negligible and has all moved to e.g. x265 instead.
+  depends_on "dav1d"
+  depends_on "lame"
+  depends_on "libvmaf" # dependent: ab-av1
+  depends_on "libvpx"
+  depends_on "openssl@3"
+  depends_on "opus"
+  depends_on "svt-av1"
+  depends_on "x264"
+  depends_on "x265"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+
+  # sdl2 has not yet been ported to OHOS, so only enable on macOS
+  on_macos do
+    depends_on "sdl2"
+  end
+
+  on_linux do
+    # fix fatal error 'vulkan_beta.h' file not found on OHOS
+    depends_on "vulkan-headers" => :build
+
+    depends_on "alsa-lib"
+    depends_on "libxcb"
+    depends_on "xz"
+    depends_on "zlib-ng-compat"
+  end
+
+  on_intel do
+    depends_on "nasm" => :build
+  end
+
+  deny_network_access!
+
+  def install
+    # The new linker leads to duplicate symbol issue https://github.com/homebrew-ffmpeg/homebrew-ffmpeg/issues/140
+    ENV.append "LDFLAGS", "-Wl,-ld_classic" if DevelopmentTools.ld64_version.between?("1015.7", "1022.1")
+
+    # Fine adding any new options that don't add dependencies to the formula.
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gpl
+      --enable-libsvtav1
+      --enable-libopus
+      --enable-libx264
+      --enable-libmp3lame
+      --enable-libdav1d
+      --enable-libvmaf
+      --enable-libvpx
+      --enable-libx265
+      --enable-openssl
+    ]
+
+    # Needs corefoundation, coremedia, corevideo
+    args += %w[--enable-videotoolbox --enable-audiotoolbox] if OS.mac?
+    args << "--enable-neon" if Hardware::CPU.arm?
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install (buildpath/"tools").children.select { |f| f.file? && f.executable? }
+    pkgshare.install buildpath/"tools/python"
+  end
+
+  def caveats
+    <<~EOS
+      ffmpeg-full includes additional tools and libraries that are not included in the regular ffmpeg formula.
+    EOS
+  end
+
+  test do
+    # Create a 5 second test MP4
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=5", mp4out
+    assert_path_exists mp4out, "Failed to create video.mp4!"
+  end
+end
