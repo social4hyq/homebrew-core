dc3858a9b152720f3176aa89e1ad63a0cff4108b
hu60cn <hu60.cn@qq.com>
2026-06-15 17:23:46 +0800

    gnutls 3.8.13 (new formula)

---
+++ Formula/g/gnutls.rb
@@ -0,0 +1,96 @@
+class Gnutls < Formula
+  desc "GNU Transport Layer Security (TLS) Library"
+  homepage "https://gnutls.org/"
+  url "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.13.tar.xz"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.8/gnutls-3.8.13.tar.xz"
+  sha256 "ffed8ec1bf09c2426d4f14aae377de4753b53e537d685e604e99a8b16ca9c97e"
+  license all_of: ["LGPL-2.1-or-later", "GPL-3.0-only"]
+  revision 2
+  compatibility_version 2
+
+  # The download page links to the directory listing pages for the "Next" and
+  # "Current stable" versions. We use the "Next" version in the formula, so we
+  # match versions from the tarball links on that directory listing page.
+  livecheck do
+    url "https://www.gnutls.org/download.html"
+    regex(/href=.*?gnutls[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    strategy :page_match do |page, regex|
+      # Find the higher version from the directory listing page URLs
+      highest_version = page.scan(%r{href=.*?/gnutls/v?(\d+(?:\.\d+)+)/?["' >]}i)
+                            .map { |match| match[0] }
+                            .max_by { |v| Version.new(v) }
+      next unless highest_version
+
+      # Fetch the related directory listing page
+      files_page = Homebrew::Livecheck::Strategy.page_content(
+        "https://www.gnupg.org/ftp/gcrypt/gnutls/v#{highest_version}",
+      )
+      next if (files_page_content = files_page[:content]).blank?
+
+      files_page_content.scan(regex).map { |match| match[0] }
+    end
+  end
+
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "734c0efdecbab73827b30af2d46ecb7e4236eb9dbfa55b81b4293d369d434b2a"
+    sha256 cellar: :any, arm64_sequoia: "65c4d021683d2caf362f28a44962ed91d2fc2baf3dfff3e5d1013c2a48528e73"
+    sha256 cellar: :any, arm64_sonoma:  "f0e71d1231213068342c729c70cf8dd5421ec1c87d5b2d21736db4fe4803a301"
+    sha256 cellar: :any, sonoma:        "eb1d17b2a4abf89c34b79dd55b8808c24321ea6fed41da6550f3abbab70ad297"
+    sha256               arm64_linux:   "22abe8b61096729057db1937b2e7d219bac0835e450da4eee0fc9fa9f21f3e23"
+    sha256               x86_64_linux:  "be86239b5f1738315dfb08c0df9a36eee05dc5f2a1db0511efec2bf063c0dac2"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "texinfo" => :build
+  depends_on "ca-certificates" => :no_linkage
+  depends_on "gettext"
+  depends_on "gmp"
+  depends_on "libidn2"
+  depends_on "libtasn1"
+  depends_on "libunistring"
+  depends_on "nettle"
+  depends_on "p11-kit"
+
+  # Backport support for building with older clang
+  patch do
+    url "https://gitlab.com/gnutls/gnutls/-/commit/29c2027c963cf559817d0da37c1fc2efd0c1bd6a.diff"
+    sha256 "c0bfc0164ff131c9d2e1c4d0367430f8b767b64c9c21e0f408bd634d21fb0302"
+  end
+
+  def install
+    # DANE support is disabled so GnuTLS does not have an indirect dependency on OpenSSL.
+    # If the feature is wanted, then can consider shipping as split `gnutls-dane` formula.
+    args = %W[
+      --disable-libdane
+      --disable-silent-rules
+      --disable-static
+      --sysconfdir=#{etc}
+      --with-default-trust-store-file=#{pkgetc}/cert.pem
+      --disable-heartbeat-support
+      --with-p11-kit
+    ]
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+
+    inreplace lib/"pkgconfig/gnutls.pc", prefix, opt_prefix
+
+    # certtool shadows the macOS certtool utility
+    mv bin/"certtool", bin/"gnutls-certtool"
+    mv man1/"certtool.1", man1/"gnutls-certtool.1"
+  end
+
+  def post_install
+    rm(pkgetc/"cert.pem") if (pkgetc/"cert.pem").exist?
+    pkgetc.install_symlink Formula["ca-certificates"].pkgetc/"cert.pem"
+  end
+
+  def caveats
+    "Guile bindings are now in the `guile-gnutls` formula."
+  end
+
+  test do
+    system bin/"gnutls-cli", "--version"
+    assert_match "expired certificate", shell_output("#{bin}/gnutls-cli expired.badssl.com", 1)
+  end
+end
