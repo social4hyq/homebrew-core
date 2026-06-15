492523920462888a6c91b755f74215db9db66290
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-15 12:42:13 +0800

    viddy: update 1.3.1 bottle.

--- Formula/v/viddy.rb
+++ Formula/v/viddy.rb
@@ -7,7 +7,7 @@ class Viddy < Formula
   head "https://github.com/sachaos/viddy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9d964cf8960b1bd373ecc3529aaa06095e81e318682e2ca799bddf9ba1c98f2f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4b1e488aa31944cf31ef29d7823a7a7faa832e4787f511872e8b69288217cc5b"
   end
 
   depends_on "rust" => :build
