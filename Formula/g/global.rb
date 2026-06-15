f3661dc444df89fce6949d6cc920af587d393766
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-16 06:36:24 +0800

    global 6.6.15

--- Formula/g/global.rb
+++ Formula/g/global.rb
@@ -4,9 +4,9 @@ class Global < Formula
 
   desc "Source code tag system"
   homepage "https://www.gnu.org/software/global/"
-  url "https://ftpmirror.gnu.org/gnu/global/global-6.6.14.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/global/global-6.6.14.tar.gz"
-  sha256 "f6e7fd0b68aed292e85bb686616baf6551d5c9424adcddca11d808ba318cb320"
+  url "https://ftpmirror.gnu.org/gnu/global/global-6.6.15.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/global/global-6.6.15.tar.gz"
+  sha256 "cf0937cb3ed521b2ab1acfa7aff45103040b860bb642c4c2f094ac3a3fe86024"
   license "GPL-3.0-or-later"
 
   bottle do
