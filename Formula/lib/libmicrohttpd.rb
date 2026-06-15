79cb55c059ed42fcc52f8edca514894336217c88
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-15 18:28:35 +0800

    libmicrohttpd: update 1.0.5 bottle.

--- Formula/lib/libmicrohttpd.rb
+++ Formula/lib/libmicrohttpd.rb
@@ -8,12 +8,7 @@ class Libmicrohttpd < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "cd84cc41c84e505be8a957788a3f2993c082d5c7c29e68ee951228bcd79374d9"
-    sha256 cellar: :any,                 arm64_sequoia: "291e7f43b1f252816c7daa42c69079ca3022ba778d1381768c42301b269e0286"
-    sha256 cellar: :any,                 arm64_sonoma:  "c1959ffe509c181f211be15b8cef3c5ffec9ab8985354cb56b6321c0760d9e12"
-    sha256 cellar: :any,                 sonoma:        "c889f12e20ce446ef65784e28f3135eedc3c358d69963dab068331a1345a7eef"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c8ef804898c01fa619655d9b62e3754eaf74421322989dbf24dc6aac702ac5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9901822c18a24a632deacd02755e0e20936eb5a5a9134dadda3b844d8e203ed9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8eae5476ac8bc738ab161bf8917ec13517d2c3a938c26772477f8349d519a77f"
   end
 
   depends_on "pkgconf" => :build
