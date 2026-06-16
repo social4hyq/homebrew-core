0e25c446dd9d0c0c792f8f2b4caaf8e715ceae6e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-17 06:37:50 +0800

    faas-cli 0.18.10

--- Formula/f/faas-cli.rb
+++ Formula/f/faas-cli.rb
@@ -1,8 +1,8 @@
 class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "https://www.openfaas.com/"
-  url "https://github.com/openfaas/faas-cli/archive/refs/tags/0.18.8.tar.gz"
-  sha256 "4d0eaf6c01ebbd69dcee389c3446bb6ea0cf042cb6f92fd471b5ec7b2785db96"
+  url "https://github.com/openfaas/faas-cli/archive/refs/tags/0.18.10.tar.gz"
+  sha256 "912125b727325ca12a98c0b8c3d2a50849fc030d1c379430b7641c2b5d3d8bff"
   license "MIT"
   head "https://github.com/openfaas/faas-cli.git", branch: "master"
 
@@ -66,13 +66,13 @@ class FaasCli < Formula
     YAML
 
     begin
-      output = shell_output("#{bin}/faas-cli deploy --tls-no-verify -yaml test.yml 2>&1", 1)
+      output = shell_output("#{bin}/faas-cli deploy --tls-no-verify --yaml test.yml 2>&1", 1)
       assert_match "stat ./template/python/template.yml", output
 
       assert_match "dockerfile", shell_output("#{bin}/faas-cli template pull 2>&1")
       assert_match "node20", shell_output("#{bin}/faas-cli new --list")
 
-      output = shell_output("#{bin}/faas-cli deploy --tls-no-verify -yaml test.yml", 1)
+      output = shell_output("#{bin}/faas-cli deploy --tls-no-verify --yaml test.yml", 1)
       assert_match "Deploying: dummy_function.", output
 
       faas_cli_version = shell_output("#{bin}/faas-cli version")
