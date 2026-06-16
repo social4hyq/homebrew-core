5b3f1ee1e267fd9a203cc0fb0ec86e8816dfe984
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-17 07:29:38 +0800

    neonctl 2.26.5

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -1,8 +1,8 @@
 class Neonctl < Formula
   desc "Neon CLI tool"
   homepage "https://neon.tech/docs/reference/neon-cli"
-  url "https://registry.npmjs.org/neonctl/-/neonctl-2.26.2.tgz"
-  sha256 "ed5a3daabf1a53770a253c1d305f05079aec09ea7a31ebc680d75529d116d496"
+  url "https://registry.npmjs.org/neonctl/-/neonctl-2.26.5.tgz"
+  sha256 "add3e59d9fc292dfa97bf2018953dafcca7ecdfb67958d274a9839cc3b108640"
   license "Apache-2.0"
 
   bottle do
