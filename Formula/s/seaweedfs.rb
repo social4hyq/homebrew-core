f81586208958114cdc414e7b257a350cfd1f927f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-16 14:56:47 +0800

    seaweedfs: update 4.34 bottle.

--- Formula/s/seaweedfs.rb
+++ Formula/s/seaweedfs.rb
@@ -13,7 +13,7 @@ class Seaweedfs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b448565657c153144067fc6f3590f20e37eca95ff3a1f96c4a0dee74d2fda5b9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a62f7346c3b3f1041720fb8725c8d2f83b8c8cc4a9e3a73967a6527cfe21d202"
   end
 
   depends_on "go" => :build
