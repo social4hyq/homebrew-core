f1ab90cfc13e3df7801a454b3d6d07fa4e0a9211
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-17 21:23:59 +0800

    azion: update 4.22.2 bottle.

--- Formula/a/azion.rb
+++ Formula/a/azion.rb
@@ -7,7 +7,7 @@ class Azion < Formula
   head "https://github.com/aziontech/azion.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c31b2abf6f0c3214e24cf4517f369fe93b2554271bde60799aa80848a613629e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4462e7ce466310259fca591376e082b1f98ea17f1d1f76d31b096b3e63bcbfcc"
   end
 
   depends_on "go" => :build
