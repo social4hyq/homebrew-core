2fdca3b811a3c0ef76111eddba8cc919cff143bb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-17 18:21:04 +0800

    kosli-cli: update 2.27.0 bottle.

--- Formula/k/kosli-cli.rb
+++ Formula/k/kosli-cli.rb
@@ -12,7 +12,7 @@ class KosliCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "14de1190273f5c740d78c25a9c7e9ca1f0f9d9dc574740798af7f06c3e9a9b8c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "280351ab78caf713f9afd641d9a9d3bb9404432ae7bdc14ed1abfed99b86fa09"
   end
 
   depends_on "go" => :build
