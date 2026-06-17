8bae9a3b5c8d195f34d1672898451bbc37697a6c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-17 18:13:01 +0800

    mmctl: update 11.8.0 bottle.

--- Formula/m/mmctl.rb
+++ Formula/m/mmctl.rb
@@ -12,7 +12,7 @@ class Mmctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5124bedd0e73aa7ea3e9afc4d069add57ea476f81c893c7655682d8485fdaf5d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "14aab8f21b888bfb5ef623305195a5ae8a249bf94cf73bfbd69d87dc89b49abf"
   end
 
   depends_on "go" => :build
