1d57b3c9112d73e68c807dd0287df5e908a7396d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 22:31:18 +0800

    codeberg-cli: update 0.5.5 bottle.

--- Formula/c/codeberg-cli.rb
+++ Formula/c/codeberg-cli.rb
@@ -7,12 +7,7 @@ class CodebergCli < Formula
   head "https://codeberg.org/Aviac/codeberg-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "8bd5f830c3221b1ac74d72d87d2fc73e56600dc2136089223f4e1b254d12f49f"
-    sha256 cellar: :any,                 arm64_sequoia: "83a74d50b902d8ae120a460c8536f66a8a16f4f38a494148d0fc0339d8ce3be8"
-    sha256 cellar: :any,                 arm64_sonoma:  "7c253e2793055098984dbda0ce9298934d269df26a6f12833d0390895e65d1bf"
-    sha256 cellar: :any,                 sonoma:        "6d5995f7e35e1db9126b3777c035b0848915e4cb1b738d2e57230583dee34b58"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c92e3099bddbb7a90d72b69e6d829d203202361e123072fc11838b77b48ecd72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12d605e557b17e8a81d430e8d5a8bae6aec6b6206b015506a4ed8adfc497a5d2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cdb4449976450f6bed51c764affc8a19b4e6a965ba771cc3733f8d4f14429c88"
   end
 
   deprecate! date: "2026-05-13", because: :repo_archived, replacement_formula: "forgejo-cli"
