17310df7218316da8b230d6e7de775f2ddf0582f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 23:26:29 +0800

    libkeccak: update 1.4.3 bottle.

--- Formula/lib/libkeccak.rb
+++ Formula/lib/libkeccak.rb
@@ -6,12 +6,7 @@ class Libkeccak < Formula
   license "ISC"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "76d0b52421d09d7390ee5c1ecfab8a2cf5e4e6fc172e36ed2d75c0457d4db33a"
-    sha256 cellar: :any,                 arm64_sequoia: "98f18e1c890f10ac9847977b9431f06a463c2c079646b478736b3c4e7e6bdb46"
-    sha256 cellar: :any,                 arm64_sonoma:  "6436b47438451ee3d21c44a668efc8e3a1b4c6572fbca312386eacd16e90c26d"
-    sha256 cellar: :any,                 sonoma:        "f9cc552ba41aeab13af7891a9c41ae1beaf268ce8024db38820b8665240e7954"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "32b32420cd4b89364da22635402652e1ba6d1b74fcc68dab229915e76e1f9597"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7efd8af258f42baf7e3b243eb9463ebeeb97272330737cc4b40324b7a967a114"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a2eb56d9c744d378797f267e8d81929c9fbf006882eb874a6983b57510720484"
   end
 
   def install
