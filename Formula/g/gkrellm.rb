98dee1911e21f4ea6fc0234af49293584c455cfd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 21:01:41 +0800

    gkrellm: update 2.5.1 bottle.

--- Formula/g/gkrellm.rb
+++ Formula/g/gkrellm.rb
@@ -11,12 +11,7 @@ class Gkrellm < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "936a8c81763c7bec55bff88e4191b3c550629ea638728b94924ad7548a52f663"
-    sha256 arm64_sequoia: "cf13c52a8564497ea6b470048141efec9afad5fe0173982a1a8e8d7f6e6211eb"
-    sha256 arm64_sonoma:  "c2113a61b119137b16dcc32073cfbca1851e8def91cf7a7588628e01cb29d5b6"
-    sha256 sonoma:        "5580325f07672b8271d4dfd69a7018e9d6b242a695fc4f62903479cd83ab120b"
-    sha256 arm64_linux:   "29063e4bc0c06e3e6c1328fad75f9f1a5061a1d66d41bfcbe16a7faa91136817"
-    sha256 x86_64_linux:  "f3d9b23caf99e6749de17b505878868a235bb4e7cca6143470c32335a6a1fe7a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3537929eb472b5995b05466df702f0ae0b7cba294669dc813ee7cd9e7e59310f"
   end
 
   depends_on "gettext" => :build
