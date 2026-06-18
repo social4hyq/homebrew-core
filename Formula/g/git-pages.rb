38d890e368a0c8cbce78ca3edce8c3535a87493f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 21:48:32 +0800

    git-pages: update 0.9.0 bottle.

--- Formula/g/git-pages.rb
+++ Formula/g/git-pages.rb
@@ -6,12 +6,7 @@ class GitPages < Formula
   license "0BSD"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e6c7b96db3990b4c5a3ceb8e2878db4448dc4e895da3538632704e435f4816de"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eb031f5c5da010bae4ad0492b98eb9ae034e7956ec8e369f2d8b58b273ac9d68"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5cfab1b2783221e96ba474ace4e507998586cf9a381447529b60525ef82c3cc1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "82635bf2587e9c09ad86fc6668332cd923e68b3d51475ea8f5afeb0b76cde0af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b00eed1115584a55459bfa1b05b0ff455d7bb4c8718b6da6060b9b2529c8b875"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0ded86e03be092098c91dd4519bd57a9bea5d7dfc843889e5dd148c5e75a261"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8434d700c36b734798016c8b5a07783eeae9dd48cbe94d5b112bf2a0f35464a8"
   end
 
   depends_on "go" => :build
