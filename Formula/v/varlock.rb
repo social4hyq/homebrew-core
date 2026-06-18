fb4170f4511e90b6b88dd5bc99715f5bbe23aca6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 09:36:47 +0800

    varlock: update 1.7.0 bottle.

--- Formula/v/varlock.rb
+++ Formula/v/varlock.rb
@@ -6,7 +6,7 @@ class Varlock < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "17222e03a2cb27bfd6393a0fedf70c18e0355fa5c1067ee2a29fd0be665b7756"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fa17971eb2e59bc16eb000a061700383c4fa1a330f2ff7bb2da156e0777ba238"
   end
 
   depends_on "node"
