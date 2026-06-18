09048ca2f8c5926c0bae4d1f78c236828a8087c2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 23:38:47 +0800

    noti: update 3.8.0 bottle.

--- Formula/n/noti.rb
+++ Formula/n/noti.rb
@@ -7,13 +7,7 @@ class Noti < Formula
   head "https://codeberg.org/roble/noti.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9df5cb0595eeccab3df88d19535d8c4959ddb31edebd05ebdaab5f256698a528"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9ce62a753d38d76f7a0da4cd1ab463085f72ccd18a5322f285ed0fda14f31b3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6c6d37577bbc6a96195d28e194a129490f2d87b9afc15639bc80418f60ff598e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9f755675d0005c92a27b5505e203225dfe64b9bf6715b49bb7840850454c5546"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d00980419a447dfbf768ee6cbe98155fe6359b77a860cbfd05977b9d07fb515e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57b3aa7759166c18af61e9a49704e7673f7b9f439eb562447d4bf12c3d6baaec"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0018796d07bf4fb84cc69b00601a10870f9e7f608b901c75b4674b985fb6f9b0"
   end
 
   depends_on "go" => :build
