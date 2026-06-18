0ca4449dfef677afad29eda13edc8fde6de5420f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-18 23:13:41 +0800

    git-pages-cli: update 1.8.2 bottle.

--- Formula/g/git-pages-cli.rb
+++ Formula/g/git-pages-cli.rb
@@ -7,12 +7,7 @@ class GitPagesCli < Formula
   head "https://codeberg.org/git-pages/git-pages-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c78c363abc446d6efe77abbfc0236d13db749230acfc945e31a66c3a4bf592a6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c78c363abc446d6efe77abbfc0236d13db749230acfc945e31a66c3a4bf592a6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c78c363abc446d6efe77abbfc0236d13db749230acfc945e31a66c3a4bf592a6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "769011c9668991a7ebe3d557c6b14450405f03850db7b5fd06e3fc9d7baadc2b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fdc37f0000ccf5a61f23f5540275967adf717fe41fdcc7cc99b120c0e30c4323"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b76ef0dc6982b3c12c8700315ac2f14e1e06f55bb548b5beecd1337ae775fd46"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2e59b7c59a705b21bf32fba4040fb5c74a7aa746fafa745381ef575b28ac7bc0"
   end
 
   depends_on "go" => :build
