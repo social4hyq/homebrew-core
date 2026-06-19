6527a35bfdaae6e50b1f84e0d0aaa05747214607
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-20 00:12:16 +0800

    lighttpd: update 1.4.84 bottle.

--- Formula/l/lighttpd.rb
+++ Formula/l/lighttpd.rb
@@ -11,7 +11,7 @@ class Lighttpd < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "db3d26db0525da028d99c1885fffa0fe89895447c2121909b6419203766d296c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1d81a7f425beb8808ee52402a35a3bfbdb573f16003488ef43706e223a27421e"
   end
 
   depends_on "autoconf" => :build
