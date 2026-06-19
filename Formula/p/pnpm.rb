bf9d2bfb31157f9d01f4abdd1423f0832e8e8dbb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-19 22:58:59 +0800

    pnpm: update 11.8.0 bottle.

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -14,7 +14,7 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c984e1830716c28c19d454b3e554bfa414977a6b45c588f694e9e4928c1fbd03"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "93829ce71c9aa7536b7a8c54f340976a2ed2a1e0decacb11c2bab4bffde4c80a"
   end
 
   depends_on "node" => [:build, :test]
