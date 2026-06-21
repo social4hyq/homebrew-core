ff1724e36172de78f22fa02fac36d368518071ee
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-22 06:33:24 +0800

    fuc 3.1.2

--- Formula/f/fuc.rb
+++ Formula/f/fuc.rb
@@ -1,8 +1,8 @@
 class Fuc < Formula
   desc "Modern, performance focused unix commands"
   homepage "https://github.com/supercilex/fuc"
-  url "https://github.com/supercilex/fuc/archive/refs/tags/3.1.1.tar.gz"
-  sha256 "a26265c85d1f13fec555606a8aaa5b978d84e637de2b3cf321dd85c843339f93"
+  url "https://github.com/supercilex/fuc/archive/refs/tags/3.1.2.tar.gz"
+  sha256 "75b4a568c6a4f04ad1289fc68fc8219b95547ac285330eab46082db19ddf25e0"
   license "Apache-2.0"
   head "https://github.com/supercilex/fuc.git", branch: "master"
 
