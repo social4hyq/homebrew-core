375f4c719f53ddfd058a2d40637784d82f29a7b5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-22 15:40:30 +0800

    gptscript: update 0.9.9 bottle.

--- Formula/g/gptscript.rb
+++ Formula/g/gptscript.rb
@@ -7,12 +7,7 @@ class Gptscript < Formula
   head "https://github.com/gptscript-ai/gptscript.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bff15834f6a38946cdf73bee8b55efb58f3ed81f7a5374d3e80f22192b0ce49f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bff15834f6a38946cdf73bee8b55efb58f3ed81f7a5374d3e80f22192b0ce49f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bff15834f6a38946cdf73bee8b55efb58f3ed81f7a5374d3e80f22192b0ce49f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fadef1894aa8e23ed95daa279b410304eeaeeb2b95281a67632ef8d0c08e426b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f5674b31456e0064de7508605782a1045b4fa345cbc214660611ecaf5130d0e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb2ba5fb7537abadb968090d3133b2ad1e026e66f096dff60eb20278d4fe7263"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ac9448a56ac92315776c6372e37d2b8c0296905d13b515abf16cae062345c744"
   end
 
   depends_on "go" => :build
