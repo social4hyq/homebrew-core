93e3dfbf4e3b0be8b882cf72bf1979519babc94f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-23 08:05:34 +0800

    vcluster 0.35.1

--- Formula/v/vcluster.rb
+++ Formula/v/vcluster.rb
@@ -2,8 +2,8 @@ class Vcluster < Formula
   desc "Creates fully functional virtual k8s cluster inside host k8s cluster's namespace"
   homepage "https://www.vcluster.com"
   url "https://github.com/loft-sh/vcluster.git",
-      tag:      "v0.35.0",
-      revision: "5b46b288665a520c2072a77adbe2f94c2e7e4b4e"
+      tag:      "v0.35.1",
+      revision: "eacd8ce99b51793d0adb3a6e1a8f9416e715e62c"
   license "Apache-2.0"
   head "https://github.com/loft-sh/vcluster.git", branch: "main"
 
