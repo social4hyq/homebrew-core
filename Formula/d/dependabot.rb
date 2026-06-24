e6094b9f0c7033c3e35755c85f5f1a8778f2a847
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-24 12:38:28 +0800

    dependabot: update 1.90.0 bottle.

--- Formula/d/dependabot.rb
+++ Formula/d/dependabot.rb
@@ -7,7 +7,7 @@ class Dependabot < Formula
   head "https://github.com/dependabot/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a52168eaca4ff5202a26735b74ba2254301f8c207ced378b04ba6d7c64f380e5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "00522aff68a91400792d06bfc91596cf51355085fc3d9de793f50d5e881e8773"
   end
 
   depends_on "go" => :build
