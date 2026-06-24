4dff412edea4b1219fe3bd04d0024d9c4d5665db
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 07:46:06 +0800

    qwen-code 0.19.1

--- Formula/q/qwen-code.rb
+++ Formula/q/qwen-code.rb
@@ -1,8 +1,8 @@
 class QwenCode < Formula
   desc "AI-powered command-line workflow tool for developers"
   homepage "https://github.com/QwenLM/qwen-code"
-  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.18.5.tgz"
-  sha256 "661c6328f9616bd81f8b310f9c30dbd9198ddf19155427d4af90413021f56436"
+  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.19.1.tgz"
+  sha256 "7246af967e75719d08721c636102f5121f9a1b35444ebfb8264c1dd341dd0180"
   license "Apache-2.0"
 
   bottle do
