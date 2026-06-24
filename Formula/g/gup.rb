c91d8fb1e6a9dece98e6fdecabef28119a1106b2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 06:48:59 +0800

    gup 1.7.0

--- Formula/g/gup.rb
+++ Formula/g/gup.rb
@@ -1,8 +1,8 @@
 class Gup < Formula
   desc "Update binaries installed by go install"
   homepage "https://github.com/nao1215/gup"
-  url "https://github.com/nao1215/gup/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "7796044ac6e7da3b72768d592077c887112c80e7541a87816a5d562f1a2fb597"
+  url "https://github.com/nao1215/gup/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "0eea4ee4070256dc98c2a8c523a61eae6a4089373e3536ceab60f353974d77f9"
   license "Apache-2.0"
   head "https://github.com/nao1215/gup.git", branch: "main"
 
