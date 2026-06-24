8d41d5afae7d1f9d0211fd854ad68d4e2e54924c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-24 11:13:32 +0800

    tinymist: update 0.15.2 bottle.

--- Formula/t/tinymist.rb
+++ Formula/t/tinymist.rb
@@ -15,7 +15,7 @@ class Tinymist < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "242cc6d27458fede343ab3f6c3101ddf3c1aef4febc58e1d9b44550c628b34ad"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "35dce913f035a866c916c74fbbc77f566914089eb25eee44685623a769e6ae48"
   end
 
   depends_on "rust" => :build
