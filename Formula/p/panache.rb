a1546d127858cad58fee995713b8f597c78e9d5e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 07:39:30 +0800

    panache 2.59.0

--- Formula/p/panache.rb
+++ Formula/p/panache.rb
@@ -1,8 +1,8 @@
 class Panache < Formula
   desc "Language server, formatter, and linter for Markdown, Quarto, and R Markdown"
   homepage "https://panache.bz"
-  url "https://github.com/jolars/panache/archive/refs/tags/v2.57.0.tar.gz"
-  sha256 "5b96301d426937bcc1e986eaa0b841f7f9f2e725820f1be254f394a0769265ab"
+  url "https://github.com/jolars/panache/archive/refs/tags/v2.59.0.tar.gz"
+  sha256 "06e3e73ce9515c4c2f781c4c6cfbeafbd3a72120e1e74b3258daf2184fa4c858"
   license "MIT"
   head "https://github.com/jolars/panache.git", branch: "main"
 
