00995af0f63e7f6e4ed9e3e089ef97d8e1d83b09
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-24 16:03:36 +0800

    kimi-code 0.19.1

--- Formula/k/kimi-code.rb
+++ Formula/k/kimi-code.rb
@@ -1,8 +1,8 @@
 class KimiCode < Formula
   desc "AI coding agent for your terminal"
   homepage "https://github.com/MoonshotAI/kimi-code"
-  url "https://registry.npmjs.org/@moonshot-ai/kimi-code/-/kimi-code-0.18.0.tgz"
-  sha256 "4d9b03676d06b2ead23c1e1666c7bca2640c39992b6dae086eb407a187e01cd9"
+  url "https://registry.npmjs.org/@moonshot-ai/kimi-code/-/kimi-code-0.19.1.tgz"
+  sha256 "8e284d274083546344e7e841365563a338472db112fb5ca73550458793607443"
   license "MIT"
 
   bottle do
