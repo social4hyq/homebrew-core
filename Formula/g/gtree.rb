1109362ff7b0471bf4c11624282642924a3ac599
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-24 12:16:59 +0800

    gtree: update 1.14.3 bottle.

--- Formula/g/gtree.rb
+++ Formula/g/gtree.rb
@@ -7,7 +7,7 @@ class Gtree < Formula
   head "https://github.com/ddddddO/gtree.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f0900098f32076afec2850b8a6dd672ecc584c9032b13973dce0ca5a709b2621"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "63268b4988cbccd2eb0e35f2d31423af8cd399bced29f3b892845855532be8e7"
   end
 
   depends_on "go" => :build
