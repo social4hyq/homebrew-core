9b7cd9fe5fd5d873df41ad6e6bdbc6c9ec10095d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 07:14:18 +0800

    mago 1.40.1

--- Formula/m/mago.rb
+++ Formula/m/mago.rb
@@ -1,8 +1,8 @@
 class Mago < Formula
   desc "Toolchain for PHP to help developers write better code"
   homepage "https://github.com/carthage-software/mago"
-  url "https://github.com/carthage-software/mago/releases/download/1.30.0/source-code.tar.gz"
-  sha256 "aa2d1ff13165706a73c5202231a3c792a97cfb613bd41ee9d0e6097af6a20e06"
+  url "https://github.com/carthage-software/mago/releases/download/1.40.1/source-code.tar.gz"
+  sha256 "ac17729a57b74820d7e0265399b9a9b11fcd99afd41b528197b882e51d2d57dd"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
