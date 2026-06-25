4fe9fa8678c0c3266a71e7aa05622303824d2a4b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 16:36:06 +0800

    papeer: update 0.9.1 bottle.

--- Formula/p/papeer.rb
+++ Formula/p/papeer.rb
@@ -6,7 +6,8 @@ class Papeer < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "89d6aef4573fd22b73e843454ef12c829dd0c4f55dbcb26a1094cb67d0753a2e"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "75ccc14a2c914a68f10911b14dcb4fac1a992c13df49bcc4443b7c878b75e75b"
   end
 
   depends_on "go" => :build
