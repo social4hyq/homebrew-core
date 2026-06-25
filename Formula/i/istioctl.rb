50b0b6b50084d2e5a9a999807be3ec6b23739f7c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 13:12:45 +0800

    istioctl: update 1.30.2 bottle.

--- Formula/i/istioctl.rb
+++ Formula/i/istioctl.rb
@@ -12,7 +12,8 @@ class Istioctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7d8aebf742007bb9bc28d229325598389ad7f68275b6e0c5abd24407e90c4c48"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "67bb5a763b36314e56d0a25b1a895ba59964a8b46247970d33fdf45ae8894925"
   end
 
   depends_on "go" => :build
