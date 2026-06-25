bb0a0d547298831a60de35a44f165c2519b1ff5d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 13:14:32 +0800

    gup: update 1.7.0 bottle.

--- Formula/g/gup.rb
+++ Formula/g/gup.rb
@@ -7,7 +7,8 @@ class Gup < Formula
   head "https://github.com/nao1215/gup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "64d379779bdfc91e35d5b0db81f67b96699044fbe374b5b37fd2e8d398cc2a0c"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a8fead1ea46cad3d83698eef2cc6acdfa2cbd8c9a33b46485332967c4891a9cd"
   end
 
   depends_on "go"
