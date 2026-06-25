3a6ec5db3acbae4c6311ed6afaac21083293f3f1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 15:58:24 +0800

    witr: update 0.3.3 bottle.

--- Formula/w/witr.rb
+++ Formula/w/witr.rb
@@ -6,7 +6,8 @@ class Witr < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9894023920a52f3ae62e0c4423e5b47e647196670c07ae69e1f2c430cd5c8bd6"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9d5cdf62b666af60d681d0f47b07b09d497c65069779596c7c0c9c47bc193b58"
   end
 
   depends_on "go" => :build
