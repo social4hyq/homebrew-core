01e62ccbc01f3074365535991115e5d12047c389
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 18:00:31 +0800

    llmfit: update 0.9.33 bottle.

--- Formula/l/llmfit.rb
+++ Formula/l/llmfit.rb
@@ -7,7 +7,8 @@ class Llmfit < Formula
   head "https://github.com/AlexsJones/llmfit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6e598228b6e2d5bd6759bc766876abdd70794d320cef101470e7be635df35ce2"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "85ebb5b3f0d37bf2b7eeeb1eb0aae6d9b940bcda3117b50361b29c9ac2e2367c"
   end
 
   depends_on "rust" => :build
