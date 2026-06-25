93f2effa0c02439cb61c705e7289868873094b08
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 18:19:59 +0800

    keploy: update 3.5.78 bottle.

--- Formula/k/keploy.rb
+++ Formula/k/keploy.rb
@@ -7,7 +7,8 @@ class Keploy < Formula
   head "https://github.com/keploy/keploy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3e55cf877e77b055ea4992c50bfbf71dae6d519c1f37e5d12db23af8d83e751c"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dd8ad96f61e0bdb48070d1f6daca6557d83058e10aade10ecaf2e1c21fdb045c"
   end
 
   depends_on "go" => :build
