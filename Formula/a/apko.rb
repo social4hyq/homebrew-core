f82c812cce34fe860d5963a6e6a9c055f61139d1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 19:53:40 +0800

    apko: update 1.2.19 bottle.

--- Formula/a/apko.rb
+++ Formula/a/apko.rb
@@ -15,7 +15,8 @@ class Apko < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d77e46d1f0c55b1e2843b69173127c0934f2d6286b9e91080a61b1b7964be59e"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "271f03c35d59a094e48efb09909f36613722e7d85b723d85fafa33d798e4da35"
   end
 
   depends_on "go" => :build
