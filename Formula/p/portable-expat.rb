df08eba0b51e95dee864f2e4b4718c47d80bba05
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-26 07:45:31 +0800

    portable-expat 2.8.2

--- Formula/p/portable-expat.rb
+++ Formula/p/portable-expat.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/portable-formula", __dir__)
 class PortableExpat < PortableFormula
   desc "XML 1.0 parser"
   homepage "https://libexpat.github.io/"
-  url "https://github.com/libexpat/libexpat/releases/download/R_2_8_1/expat-2.8.1.tar.lz"
-  sha256 "bf63752637cab644ed8a93829e1c8788caf67a1626af7e57d83b7568233a9f89"
+  url "https://github.com/libexpat/libexpat/releases/download/R_2_8_2/expat-2.8.2.tar.lz"
+  sha256 "27407e3f9f9dc0042b116d6e4ea1d9eaacfaae2c4189c58189511e0348a742d8"
   license "MIT"
 
   livecheck do
