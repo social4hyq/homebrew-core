9da83cf167028821b27d7bcdc7741d82518d2467
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 12:28:36 +0800

    scw: update 2.58.3 bottle.

--- Formula/s/scw.rb
+++ Formula/s/scw.rb
@@ -11,7 +11,8 @@ class Scw < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "60788c4d9575a71339b9599c5011a86d32a44d730a5be92f340cdcd74afcc1c5"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4507fd5bc9a62cc7df2be6a4b9db52fa5d814072b596cbc7bced9e6ef08faa3f"
   end
 
   depends_on "go" => :build
