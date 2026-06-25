4056532ec188239102b8c1e844535f0e91491ac3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-25 13:00:14 +0800

    mimirtool: update 3.1.2 bottle.

--- Formula/m/mimirtool.rb
+++ Formula/m/mimirtool.rb
@@ -17,7 +17,8 @@ class Mimirtool < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6b38a0b63d4818ca29c2793335af139ce84789c6d54d1fe1e0f26595fdd9725d"
+    root_url "http://192.168.0.27:20080/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0cf3733fc85b179cd11243e9dffc38c31bef0851f178f674a8de9981c45d2bba"
   end
 
   depends_on "go" => :build
