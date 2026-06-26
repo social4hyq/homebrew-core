f12c1c30dbf7b7f608b56305c674101b1404e9a6
hqzing <hqzing@outlook.com>
2026-06-26 16:40:42 +0800

    infisical: rebuild

--- Formula/i/infisical.rb
+++ Formula/i/infisical.rb
@@ -7,7 +7,6 @@ class Infisical < Formula
   head "https://github.com/Infisical/cli.git", branch: "main"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "3030b84df3bb7177e243bcd0392f1cc71cd6953eb2c3941319589f92e0883832"
   end
 
