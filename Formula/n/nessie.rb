d3fae6533daa22bf1c5ab344ac920f0160193a9f
hqzing <hqzing@outlook.com>
2026-06-26 16:48:14 +0800

    nessie: rebuild

--- Formula/n/nessie.rb
+++ Formula/n/nessie.rb
@@ -6,7 +6,6 @@ class Nessie < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "d5e0219512a1b9dfca7501591beae62f058c02215789c47883a8af4c21344990"
   end
 
