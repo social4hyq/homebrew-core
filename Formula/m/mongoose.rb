b63439c4f41dab19a4166577334d14bebc194e05
hqzing <hqzing@outlook.com>
2026-06-26 16:46:21 +0800

    mongoose: rebuild

--- Formula/m/mongoose.rb
+++ Formula/m/mongoose.rb
@@ -6,7 +6,6 @@ class Mongoose < Formula
   license "GPL-2.0-only"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "ff1f4286459fc546c4b7e489d0e4ea65ded48f6f7031d40899a1ea059189b5a6"
   end
 
