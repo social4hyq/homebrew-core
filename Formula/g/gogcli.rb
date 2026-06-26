c81a07ff1331500e2c5207e3f403b880e6fb64dd
hqzing <hqzing@outlook.com>
2026-06-26 16:39:04 +0800

    gogcli: rebuild

--- Formula/g/gogcli.rb
+++ Formula/g/gogcli.rb
@@ -7,7 +7,6 @@ class Gogcli < Formula
   head "https://github.com/steipete/gogcli.git", branch: "main"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "d6be3f45cb7a39a22fe89fd6797dfe6ba046985ee8944edcea8636a3926325c1"
   end
 
