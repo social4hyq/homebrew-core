2b0677ced3e6e988f21c66ae08c5bd0aefe53d14
hqzing <hqzing@outlook.com>
2026-06-26 16:29:41 +0800

    buildkit: rebuild

--- Formula/b/buildkit.rb
+++ Formula/b/buildkit.rb
@@ -15,7 +15,6 @@ class Buildkit < Formula
   end
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "61664fb6c1b989325f88cfeb36f7e60eb549e3f584e73d649e1f06cb7c26f7f6"
   end
 
