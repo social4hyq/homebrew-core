63e4d1843ddbdb5736267f9ce440f2d081f12247
hqzing <hqzing@outlook.com>
2026-06-26 16:27:40 +0800

    asc: rebuild

--- Formula/a/asc.rb
+++ Formula/a/asc.rb
@@ -7,7 +7,6 @@ class Asc < Formula
   head "https://github.com/rorkai/App-Store-Connect-CLI.git", branch: "main"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "a47f6448fd5f92570f57913d971ac28288dfcabd41349473392e37da4abf160b"
   end
 
