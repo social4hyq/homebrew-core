0848fc6e34031abf25e00d8c9652cb6397d8a6f0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-27 07:02:49 +0800

    jfrog-cli 2.111.0

--- Formula/j/jfrog-cli.rb
+++ Formula/j/jfrog-cli.rb
@@ -1,8 +1,8 @@
 class JfrogCli < Formula
   desc "Command-line interface for JFrog products"
   homepage "https://docs.jfrog.com/integrations/docs/jfrog-cli"
-  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.109.0.tar.gz"
-  sha256 "0d246b65bffab877e0e01c1ac9ddefaa5151e278a0c196406b5843551d2e49d0"
+  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.111.0.tar.gz"
+  sha256 "86068668723ca771e5a93c8ca2d069c4d7b4d3b2559340765674a1a0afa2f07e"
   license "Apache-2.0"
   head "https://github.com/jfrog/jfrog-cli.git", branch: "master"
 
