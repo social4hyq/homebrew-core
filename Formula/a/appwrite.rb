41e5d2490b11ff05b7b63cddedfa336e2d387c8c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-27 06:05:44 +0800

    appwrite 22.3.0

--- Formula/a/appwrite.rb
+++ Formula/a/appwrite.rb
@@ -1,8 +1,8 @@
 class Appwrite < Formula
   desc "Command-line tool for Appwrite"
   homepage "https://appwrite.io"
-  url "https://registry.npmjs.org/appwrite-cli/-/appwrite-cli-22.1.3.tgz"
-  sha256 "837813febb94becf716e699ee1a481daf75dc356e7336e41a98d5de1f1288326"
+  url "https://registry.npmjs.org/appwrite-cli/-/appwrite-cli-22.3.0.tgz"
+  sha256 "295aea43d875d563e571b004036864e4c5ab4fe77728cf403a364053a0198616"
   license "BSD-3-Clause"
 
   bottle do
