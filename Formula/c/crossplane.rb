221d0e09a8d1cfbe409a14196a1b79811932bbb7
hqzing <hqzing@outlook.com>
2026-06-26 16:32:27 +0800

    crossplane: rebuild

--- Formula/c/crossplane.rb
+++ Formula/c/crossplane.rb
@@ -12,7 +12,6 @@ class Crossplane < Formula
   end
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "9bd328d84797ec3f5c1c09cda9310875d6c9d249eda33ca28fd4fefec9183d9f"
   end
 
