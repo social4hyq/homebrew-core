0bee40cae9d9dee058a8b939e6df6bc2784a10ab
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-26 20:46:33 +0800

    commitlint: update 21.1.0 bottle.

--- Formula/c/commitlint.rb
+++ Formula/c/commitlint.rb
@@ -6,7 +6,8 @@ class Commitlint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "46ba89613f6126b89d2f57249d6e4f2f580bcab7337c7af5690049b53c43adf6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0707c09cc939c43022e6e5dc3b52002a83658a2df7bb245d09767d81b9a90fdb"
   end
 
   depends_on "node"
