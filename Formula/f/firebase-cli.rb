f6e64de42a70b8988e8e311e3144d1152a6d3bee
hqzing <hqzing@outlook.com>
2026-06-26 16:36:41 +0800

    firebase-cli: rebuild

--- Formula/f/firebase-cli.rb
+++ Formula/f/firebase-cli.rb
@@ -6,7 +6,6 @@ class FirebaseCli < Formula
   license "MIT"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "3b394031a93202b623a811269a02c8dae08aa67384a0864eddc9117b6f1cf635"
   end
 
