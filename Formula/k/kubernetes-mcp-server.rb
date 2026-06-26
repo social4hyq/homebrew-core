88db05c16f081917901755e096373d5f65dac79d
hqzing <hqzing@outlook.com>
2026-06-26 16:42:34 +0800

    kubernetes-mcp-server: rebuild

--- Formula/k/kubernetes-mcp-server.rb
+++ Formula/k/kubernetes-mcp-server.rb
@@ -7,7 +7,6 @@ class KubernetesMcpServer < Formula
   head "https://github.com/containers/kubernetes-mcp-server.git", branch: "main"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "5d3164a881c541018b817a79060266d59fcbf6762532e17ccfbda0b596e64087"
   end
 
