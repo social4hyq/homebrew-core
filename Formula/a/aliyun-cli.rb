3e2eab6e2add45d98acf65158b37b2b7852387e3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-27 06:04:33 +0800

    aliyun-cli 3.4.2

--- Formula/a/aliyun-cli.rb
+++ Formula/a/aliyun-cli.rb
@@ -2,8 +2,8 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-      tag:      "v3.4.1",
-      revision: "2f0f0315d031aecca681b1799c326ed2949c60f2"
+      tag:      "v3.4.2",
+      revision: "08bd781749829970aabf7b6a99809169e21d5d9f"
   license "Apache-2.0"
   head "https://github.com/aliyun/aliyun-cli.git", branch: "master"
 
