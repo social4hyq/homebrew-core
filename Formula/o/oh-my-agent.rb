0e6624f4f7af870f7693aab846a845d35d871da3
hqzing <hqzing@outlook.com>
2026-06-26 16:49:38 +0800

    oh-my-agent: rebuild

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -6,7 +6,6 @@ class OhMyAgent < Formula
   license "MIT"
 
   bottle do
-    root_url "http://192.168.0.27:20080/bottles"
     sha256 cellar: :any_skip_relocation, arm64_ohos: "0aae5fd334fe2811c3dce5ad064221298007411aa21eb0d6f9089797fe6e7791"
   end
 
