5034451f9c6bfb89e9d5801939e8d03be907cabf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-28 07:36:21 +0800

    n8n-mcp 2.61.0

--- Formula/n/n8n-mcp.rb
+++ Formula/n/n8n-mcp.rb
@@ -1,8 +1,8 @@
 class N8nMcp < Formula
   desc "MCP for Claude Desktop, Claude Code, Windsurf, Cursor to build n8n workflows"
   homepage "https://www.n8n-mcp.com/"
-  url "https://registry.npmjs.org/n8n-mcp/-/n8n-mcp-2.59.4.tgz"
-  sha256 "64b8bf1237490d2abb41d376fb4edeed84a1a19040b9d95ccd7d1706f8656820"
+  url "https://registry.npmjs.org/n8n-mcp/-/n8n-mcp-2.61.0.tgz"
+  sha256 "158bae9c6e46f2741b42bfa13d485f57dcf6fef55de4afd939d2a3f71dfc30bc"
   license "MIT"
 
   bottle do
