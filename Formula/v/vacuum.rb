21a1e6049923726c1bea8cd8e2e64459be2d5340
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-27 20:54:43 +0800

    vacuum: update 0.29.6 bottle.

--- Formula/v/vacuum.rb
+++ Formula/v/vacuum.rb
@@ -7,7 +7,7 @@ class Vacuum < Formula
   head "https://github.com/daveshanley/vacuum.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "03a0fd8205c4a19242f2d71eb1cd452eb356e7942126fbaebffde1de830198db"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bd972b9405387877a78e0cb4ce69268ae498df8ec1b5125fc564cf9c9787b587"
   end
 
   depends_on "go" => :build
