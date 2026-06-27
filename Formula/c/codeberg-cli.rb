89aca7c59297914384e9a273c4d9e5eaae9e624d
hqzing <hqzing@outlook.com>
2026-06-27 12:22:19 +0800

    codeberg-cli: rebuild

--- Formula/c/codeberg-cli.rb
+++ Formula/c/codeberg-cli.rb
@@ -6,10 +6,6 @@ class CodebergCli < Formula
   license "AGPL-3.0-or-later"
   head "https://codeberg.org/Aviac/codeberg-cli.git", branch: "main"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cdb4449976450f6bed51c764affc8a19b4e6a965ba771cc3733f8d4f14429c88"
-  end
-
   deprecate! date: "2026-05-13", because: :repo_archived, replacement_formula: "forgejo-cli"
   disable! date: "2027-05-13", because: :repo_archived, replacement_formula: "forgejo-cli"
 
