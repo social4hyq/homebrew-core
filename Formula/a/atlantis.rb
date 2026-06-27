5aeb1e8301caf8ac1c02e53d96031f31a7d622f2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-27 20:37:07 +0800

    atlantis: update 0.44.1 bottle.

--- Formula/a/atlantis.rb
+++ Formula/a/atlantis.rb
@@ -12,7 +12,7 @@ class Atlantis < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "1b47074dc55fb440ba28ee7fff7c611684984f7eb6487424ddd66303ffcd15c9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "49726d0896ec2b05b9ae05f7826cef03fe9a858feb4a271f2f6ee36c64191c04"
   end
 
   depends_on "go" => :build
