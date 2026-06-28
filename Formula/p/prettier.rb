664a9d0dee7e839eb80b8281228fde1e2ae4cea6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-29 07:32:44 +0800

    prettier 3.9.1

--- Formula/p/prettier.rb
+++ Formula/p/prettier.rb
@@ -1,8 +1,8 @@
 class Prettier < Formula
   desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
   homepage "https://prettier.io/"
-  url "https://registry.npmjs.org/prettier/-/prettier-3.8.5.tgz"
-  sha256 "0af7ca9ff0d095913b49613a90bc4a866b86cbfbd7006ee671c3ad305cba25ac"
+  url "https://registry.npmjs.org/prettier/-/prettier-3.9.1.tgz"
+  sha256 "ff38c805812155a36e810df0468bdbb69a258dcb70a1e584ff4032e98d2d35eb"
   license "MIT"
   head "https://github.com/prettier/prettier.git", branch: "main"
 
