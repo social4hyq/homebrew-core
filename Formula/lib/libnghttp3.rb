3647a25e200ee592fa050c6fea43dd5934af5806
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-28 21:06:00 +0800

    libnghttp3: update 1.17.0 bottle.

--- Formula/lib/libnghttp3.rb
+++ Formula/lib/libnghttp3.rb
@@ -9,7 +9,7 @@ class Libnghttp3 < Formula
   head "https://github.com/ngtcp2/nghttp3.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b8880a5f4d350d911de13da952d0fefa49f0e45352dc3054059345f4394e68fd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b97eae2b6f945e7376b53b5e7a689a865f7b0af75c7cade4cb85e0b84c6be1a7"
   end
 
   depends_on "cmake" => :build
