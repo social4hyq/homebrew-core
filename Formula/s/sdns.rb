28c7dd702a372494719348a2418cdbfc5832a1b1
hqzing <hqzing@outlook.com>
2026-06-29 10:45:23 +0000

    sdns 1.7.2

--- Formula/s/sdns.rb
+++ Formula/s/sdns.rb
@@ -1,14 +1,18 @@
 class Sdns < Formula
   desc "Privacy important, fast, recursive dns resolver server with dnssec support"
   homepage "https://sdns.dev/"
-  url "https://github.com/semihalev/sdns/archive/refs/tags/v1.7.1.tar.gz"
-  sha256 "1847afad0c2fcc93d289306f48fb16050bb86040d3aef77d363c99ce3b513863"
+  url "https://github.com/semihalev/sdns/archive/refs/tags/v1.7.2.tar.gz"
+  sha256 "a611113e24dfe765b9bacb557f5cea7edde310495039f859786baea6cadaf1a9"
   license "MIT"
   head "https://github.com/semihalev/sdns.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "53499d7a6a7c7fbeb29911348d4598d2f9d8c91f1dbe21515c21f9b20f2d7d8c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "908d908a975c0f09dd913d518ecab6422a4a50a216ac7de2707f84d40ba1b219"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0f25533ed99711ccb89d10b4a50fcd6936436beb423fe022dde4bedd87324bb9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b4e0c26b048670ae58aa2249d44438e3dea44319868bac0eec75d6e362d68906"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fa9c4ce39c202fa9da244a9a3bf46888e6b53f489b8fb1d2119252c350f0d359"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0db6d0488c62254e8a32b5deafabae042d83ca7f75570b9045733d61148659a9"
+    sha256 cellar: :any,                 x86_64_linux:  "c5af80a6dd664a2ef9a6e9d4f20c1695cb7f194a7c00a2bff1cc18cc42d974c3"
   end
 
   depends_on "go" => :build
