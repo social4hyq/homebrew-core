a734e4d984ed1114c27c85c60e02f1041a32720f
hqzing <hqzing@outlook.com>
2026-06-29 10:51:55 +0000

    xdelta 3.2.0

--- Formula/x/xdelta.rb
+++ Formula/x/xdelta.rb
@@ -1,25 +1,39 @@
 class Xdelta < Formula
   desc "Binary diff, differential compression tools"
   homepage "https://github.com/jmacd/xdelta"
-  url "https://github.com/jmacd/xdelta/archive/refs/tags/v3.1.0.tar.gz"
-  sha256 "7515cf5378fca287a57f4e2fee1094aabc79569cfe60d91e06021a8fd7bae29d"
+  url "https://github.com/jmacd/xdelta/archive/refs/tags/v3.2.0.tar.gz"
+  sha256 "ba2c9676b325f1958e504a60a20340145b8073d5f8664092de17389e15a93199"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cbb4eaf5096793e5e328c161753f0fe643e0db557ff1923d7979bc71e884aa0f"
+    sha256 cellar: :any, arm64_tahoe:   "59e9b3265d1ed3eaa7acb45f2ae8db76c706fbe49f849e92fbd426d13e6182e2"
+    sha256 cellar: :any, arm64_sequoia: "be26e483322fdec244a96911ed8bace807f1da8b4a1275a604192264ae6b4e26"
+    sha256 cellar: :any, arm64_sonoma:  "e9ba02aa6039dac4e32b2198684c6fd7a88e3a3ba8beff1de70841769bdfba9d"
+    sha256 cellar: :any, sonoma:        "a2328d725a6ef8abcc7e475957fc0972e42fcb65a56826e7b7af52a2bb9ce059"
+    sha256 cellar: :any, arm64_linux:   "87b7623b5ed5bbdf3a09ce88a6a221ffe9b45dca659f2dc50911f7cf0678647d"
+    sha256 cellar: :any, x86_64_linux:  "e5631b4554f80646caef4ab1a36ed8ccc08aed7ea133ca41433f445842e2f0df"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "cmake" => :build
+  depends_on "blake3"
   depends_on "xz"
 
   def install
-    cd "xdelta3" do
-      system "autoreconf", "--force", "--install", "--verbose"
-      system "./configure", "--with-liblzma", *std_configure_args
-      system "make", "install"
-    end
+    # Fix library target to the same as `blake3` formula.
+    inreplace "xdelta3/CMakeLists.txt",
+              "set(XD3_ARMOR_LIBRARIES blake3)",
+              "set(XD3_ARMOR_LIBRARIES BLAKE3::blake3)"
+
+    args = %w[
+      -DXD3_BUILD_TESTS=OFF
+      -DXD3_LZMA_MODE=on
+      -DHOMEBREW_ALLOW_FETCHCONTENT=ON
+      -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+      -DFETCHCONTENT_TRY_FIND_PACKAGE_MODE=ALWAYS
+    ]
+    system "cmake", "-S", "xdelta3", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
