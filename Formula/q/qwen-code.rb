dd61e109222a9bee7eb8df1e2e0b30108f0d946a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-29 15:10:33 +0800

    qwen-code: update 0.19.2 bottle.

--- Formula/q/qwen-code.rb
+++ Formula/q/qwen-code.rb
@@ -6,12 +6,7 @@ class QwenCode < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "6f124d941273d425c98051175980f1b87cd5020263a1ef8399edbc2f7d5273f1"
-    sha256 cellar: :any,                 arm64_sequoia: "99f99dd7107fc00a209b38b0ce4c18d64b961d7b645f5d93345383da6181fd3f"
-    sha256 cellar: :any,                 arm64_sonoma:  "99f99dd7107fc00a209b38b0ce4c18d64b961d7b645f5d93345383da6181fd3f"
-    sha256 cellar: :any,                 sonoma:        "5a3eec1fa6c8ade01f01dc89ba8a340d460fb9098d3a70df21ae2444a9300f8a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e7c021dd033315f2076945a9455ceadac5944be7583552677cc0219c6f82de35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80928fdf9f22909b8fcf2749a378d1217d45a541e0a9bfa42d4781a1fb6caadd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0802bd24603e55a5ce4e6072dc4f4c905b71838aedc68e3e10355b41aca1f057"
   end
 
   depends_on "node"
