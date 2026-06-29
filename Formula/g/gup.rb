a7e161ddaf1ae946362caf209052d4631980dbeb
hqzing <hqzing@outlook.com>
2026-06-29 09:59:49 +0000

    gup 1.7.1

--- Formula/g/gup.rb
+++ Formula/g/gup.rb
@@ -7,7 +7,12 @@ class Gup < Formula
   head "https://github.com/nao1215/gup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cbd288ea4eb2440c2b5a6091dfcf99e469285f002ed3a761d8d9aac5a31a2f4e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "65fb815f7f2512c44a7c9d56fe17219f519b4372627ca0670cd4bd1c85d0a834"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65fb815f7f2512c44a7c9d56fe17219f519b4372627ca0670cd4bd1c85d0a834"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "65fb815f7f2512c44a7c9d56fe17219f519b4372627ca0670cd4bd1c85d0a834"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d9cb12ef30c0d4d9c0f46f97c9511fbf8f1abdb55362f25b84e9926c2aa735d9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "65251903aac664759225fdc0a9f364724052ed0ebfa13931a597ea9445e0a838"
+    sha256 cellar: :any,                 x86_64_linux:  "fa940584d0455c94a5c580a5bcaef191e8c1edc6a3ef6dfc43cb4307a6a4556f"
   end
 
   depends_on "go"
@@ -29,15 +34,15 @@ class Gup < Formula
     (testpath/"bin").mkpath
 
     (testpath/"hello").mkpath
-    (testpath/"hello/go.mod").write <<~EOS
+    (testpath/"hello/go.mod").write <<~MOD
       module example.com/hello
       go 1.22
-    EOS
-    (testpath/"hello/main.go").write <<~EOS
+    MOD
+    (testpath/"hello/main.go").write <<~GO
       package main
       import "fmt"
       func main() { fmt.Println("hello") }
-    EOS
+    GO
 
     cd testpath/"hello" do
       system "go", "install", "."
