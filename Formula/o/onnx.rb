d9632c95151277825b88316f49b13027f1846c20
hqzing <hqzing@outlook.com>
2026-06-29 10:53:39 +0000

    onnx 1.22.0

--- Formula/o/onnx.rb
+++ Formula/o/onnx.rb
@@ -1,13 +1,18 @@
 class Onnx < Formula
   desc "Open standard for machine learning interoperability"
   homepage "https://onnx.ai/"
-  url "https://github.com/onnx/onnx/archive/refs/tags/v1.21.0.tar.gz"
-  sha256 "42ffedcd8c9b6363694300c6ffec1ada77f9620176465719acb27b13a4d6f2de"
+  url "https://github.com/onnx/onnx/archive/refs/tags/v1.22.0.tar.gz"
+  sha256 "70bb8b25cf31ea9b1d9f94baacfdc8c4fa27a760f9a10f5d93881bc9eede5fbc"
   license "Apache-2.0"
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "51ff83b0105373fb63a92b6fec7910245e20490d4b014917ce4f16dedf39d8cd"
+    sha256 cellar: :any, arm64_tahoe:   "fe20a16aee1e14330375f59273bd80ac4c8d4c128a84ce83c7a6b30c22824e41"
+    sha256 cellar: :any, arm64_sequoia: "4cc11ec406bb840dca78a08c65fd29bc1101751a2a446b6cb887d5b79a55d889"
+    sha256 cellar: :any, arm64_sonoma:  "633691c29d813e7cf8c4c20a859870d733be18dac720ef6906fbf295284ddaa2"
+    sha256 cellar: :any, sonoma:        "65fc51dfa759407ddde0ed56c7690e9cef84100ab8dc622786b3615d226ed76c"
+    sha256 cellar: :any, arm64_linux:   "bc53b47a774fc25230118a7ec0c4732724554b0ddcb9b3266666175de7d229f9"
+    sha256 cellar: :any, x86_64_linux:  "115c6b1c438fb6d7a675a257704583ab3ff4f9b5bac489dcfa0f20c542e98616"
   end
 
   depends_on "cmake" => [:build, :test]
@@ -27,6 +32,10 @@ class Onnx < Formula
   end
 
   def install
+    # FIXME: From v1.22+, onnx internals are hidden by default, which breaks
+    # onnxruntime's usage of onnx as a dependency. We need to make them visible again.
+    inreplace "CMakeLists.txt", "CXX_VISIBILITY_PRESET hidden", "CXX_VISIBILITY_PRESET default"
+
     args = %W[
       -DBUILD_SHARED_LIBS=ON
       -DCMAKE_INSTALL_RPATH=#{rpath}
