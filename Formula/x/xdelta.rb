1de82941ab6c8c487a6d9d4c1e9052b15fa33820
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-29 19:14:16 +0800

    xdelta: update 3.2.0 bottle.

--- Formula/x/xdelta.rb
+++ Formula/x/xdelta.rb
@@ -6,12 +6,7 @@ class Xdelta < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "59e9b3265d1ed3eaa7acb45f2ae8db76c706fbe49f849e92fbd426d13e6182e2"
-    sha256 cellar: :any, arm64_sequoia: "be26e483322fdec244a96911ed8bace807f1da8b4a1275a604192264ae6b4e26"
-    sha256 cellar: :any, arm64_sonoma:  "e9ba02aa6039dac4e32b2198684c6fd7a88e3a3ba8beff1de70841769bdfba9d"
-    sha256 cellar: :any, sonoma:        "a2328d725a6ef8abcc7e475957fc0972e42fcb65a56826e7b7af52a2bb9ce059"
-    sha256 cellar: :any, arm64_linux:   "87b7623b5ed5bbdf3a09ce88a6a221ffe9b45dca659f2dc50911f7cf0678647d"
-    sha256 cellar: :any, x86_64_linux:  "e5631b4554f80646caef4ab1a36ed8ccc08aed7ea133ca41433f445842e2f0df"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "896ac3a4e92012534fa732cf0a1cd7350b138a3353860e0fd9389571bfe3237e"
   end
 
   depends_on "cmake" => :build
