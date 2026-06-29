a77f4a7fb58e9ee977661ddaa2d83d73a8eae2cf
hqzing <hqzing@outlook.com>
2026-06-29 10:44:25 +0000

    tea 0.14.2

--- Formula/t/tea.rb
+++ Formula/t/tea.rb
@@ -1,26 +1,31 @@
 class Tea < Formula
   desc "Command-line tool to interact with Gitea servers"
   homepage "https://gitea.com/gitea/tea"
-  url "https://gitea.com/gitea/tea/archive/v0.14.1.tar.gz"
-  sha256 "848b6b2fafa270fa77b4e278d521bfcc16d2f721c45ac90f08f5b16dc630c3f9"
+  url "https://gitea.com/gitea/tea.git",
+      tag:      "v0.14.2",
+      revision: "88f5cdcafadbafd992cbf6ea31f9f29512263452"
   license "MIT"
   head "https://gitea.com/gitea/tea.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "62b390e76a32507418431619541446ca9e80b9b1c0279743a22f3033ecd1a154"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "631c12e1cd0d3a577ac725b6628047c891f9f96ff8c9c211537735b403577efa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "631c12e1cd0d3a577ac725b6628047c891f9f96ff8c9c211537735b403577efa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "631c12e1cd0d3a577ac725b6628047c891f9f96ff8c9c211537735b403577efa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5deee6bf39583020b446b2aa4b5bf228a077faefaddc6d661b84126a01b7037f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5d379327eba2318746591d87221ab14b9373b845d6fec1346517af068989f58c"
+    sha256 cellar: :any,                 x86_64_linux:  "8cbfce4fd2342c7caac36823126dbdaf2ddda34b049b649c10b60ef5346c298f"
   end
 
   depends_on "go" => :build
 
   def install
     # get gittea sdk version
-    sdk = Utils.safe_popen_read("go", "list", "-f", "{{.Version}}", "-m", "code.gitea.io/sdk/gitea").to_s
+    sdk = Utils.safe_popen_read("go", "list", "-f", "{{.Version}}", "-m", "gitea.dev/sdk").to_s
 
     ldflags = %W[
       -s -w
-      -X code.gitea.io/tea/modules/version.Version=#{version}
-      -X code.gitea.io/tea/modules/version.Tags=#{tap.user}
-      -X code.gitea.io/tea/modules/version.SDK=#{sdk}
+      -X gitea.dev/tea/modules/version.Version=#{version}
+      -X gitea.dev/tea/modules/version.SDK=#{sdk}
     ]
 
     system "go", "build", *std_go_args(ldflags:)
