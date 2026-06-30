5f57bb10fa00f6f9eae2f1b04e410399821dc908
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-01 06:31:51 +0800

    docker-agent 1.93.0

--- Formula/d/docker-agent.rb
+++ Formula/d/docker-agent.rb
@@ -1,8 +1,8 @@
 class DockerAgent < Formula
   desc "Agent Builder and Runtime by Docker Engineering"
   homepage "https://docker.github.io/docker-agent/"
-  url "https://github.com/docker/docker-agent/archive/refs/tags/v1.90.0.tar.gz"
-  sha256 "f546912916315943f7eda467e1d2c31a40991968f66792e7d83294d5ebd197b2"
+  url "https://github.com/docker/docker-agent/archive/refs/tags/v1.93.0.tar.gz"
+  sha256 "ac302a220c80757e4dbc830495c95b09b6fb72924764e44a4e09db6f58ff599e"
   license "Apache-2.0"
   head "https://github.com/docker/docker-agent.git", branch: "main"
 
