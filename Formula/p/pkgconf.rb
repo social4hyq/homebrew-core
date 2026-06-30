883ed7237f631620cc19a8ef7bc819cf46b813e2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-01 07:47:48 +0800

    pkgconf 2.9.96

--- Formula/p/pkgconf.rb
+++ Formula/p/pkgconf.rb
@@ -1,10 +1,10 @@
 class Pkgconf < Formula
   desc "Package compiler and linker metadata toolkit"
   homepage "https://github.com/pkgconf/pkgconf"
-  url "https://distfiles.ariadne.space/pkgconf/pkgconf-2.9.95.tar.xz"
-  mirror "https://fossies.org/linux/misc/pkgconf-2.9.95.tar.xz"
-  mirror "http://fresh-center.net/linux/misc/pkgconf-2.9.95.tar.xz"
-  sha256 "f8f0c834a493c610d31a1a84142fdbc5f8273fb3d98c3e9bb6fc0e1d999739c6"
+  url "https://distfiles.ariadne.space/pkgconf/pkgconf-2.9.96.tar.xz"
+  mirror "https://fossies.org/linux/misc/pkgconf-2.9.96.tar.xz"
+  mirror "http://fresh-center.net/linux/misc/pkgconf-2.9.96.tar.xz"
+  sha256 "f2e7898225914d8ded87c0bea1e2d238795e14834dd3cb33e8c93e5d55b47eb5"
   license "ISC"
   compatibility_version 1
 
