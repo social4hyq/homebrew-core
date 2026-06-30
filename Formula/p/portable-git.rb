907291b77c343915173a4c544de91c827e84e0a4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-06-30 17:18:19 +0800

    portable-git: update 2.54.0 bottle.

--- Formula/p/portable-git.rb
+++ Formula/p/portable-git.rb
@@ -16,8 +16,8 @@ class PortableGit < PortableFormula
   no_autobump! because: "this is a critical package so an auto bump might break Homebrew usability."
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b7965ccc9fcdb760dbfd3a6420f53a76abe04db31553bfd1ae4380c5d8588f5a"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dc5c69f517e4f4ca0462647ce9492d679ae2b64b74379fc85ec7c2113197fef1"
   end
 
   patch do
