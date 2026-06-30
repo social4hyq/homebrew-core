eae50fbe4e8b23310eccb60c4ffbdf0dfeaa165b
hqzing <hqzing@outlook.com>
2026-06-30 04:16:16 +0000

    openrtsp 2026.06.29

--- Formula/o/openrtsp.rb
+++ Formula/o/openrtsp.rb
@@ -1,11 +1,11 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "https://download.live555.com/live.2026.06.12.tar.gz"
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2026.06.12.tar.gz"
-  mirror "https://pkg.freebsd.org/ports-distfiles/live.2026.06.12.tar.gz"
+  url "https://download.live555.com/live.2026.06.29.tar.gz"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2026.06.29.tar.gz"
+  mirror "https://pkg.freebsd.org/ports-distfiles/live.2026.06.29.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  sha256 "ee0e1387747d5fea46f4652249e13020714635fff1f507917122df86b58bd1cd"
+  sha256 "f8f9f56968e160b2864db5afcd95dc719205df71237fec63a3ff883b1d34ca78"
   license "LGPL-3.0-or-later"
 
   livecheck do
@@ -14,18 +14,15 @@ class Openrtsp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2dac849912d67d961a24fe08ea251dbab67d4daf3dc4afc5b008ba534bcdbfb1"
+    sha256 cellar: :any, arm64_tahoe:   "32ebffcca9f656f37501c84b6785b79a4f74f0a96cbce36ff7a3ed0b57f0b7b5"
+    sha256 cellar: :any, arm64_sequoia: "71d246f6f5502efcbed618b17961caf4521166bd9c44cb358851f9c743b04fa0"
+    sha256 cellar: :any, arm64_sonoma:  "435d1acc07dfd620e6b9284ae6aa21dc3675bc6e6b450a73aad2136b66e98cf6"
+    sha256 cellar: :any, sonoma:        "368529cdfb504acbaf62ab59f19d2159beb7ecf1da5ebbe50b5e312d8476bc92"
+    sha256 cellar: :any, arm64_linux:   "d990d7d6e8e8a4f311c64f6977c050574f0b56e1b4036fc4bc8a0fdf6442647b"
+    sha256 cellar: :any, x86_64_linux:  "340b996e6b32e04e327b77eb62b40f8f98befea82c4bbb9200186158a1913178"
   end
 
   depends_on "openssl@3"
-
-  # Support CXXFLAGS when building on macOS
-  # PR ref: https://github.com/rgaufman/live555/pull/46
-  # TODO: Remove once changes land in a release
-  patch do
-    url "https://github.com/rgaufman/live555/commit/16701af5486bb3a2d25a28edaab07789c8a9ce57.patch?full_index=1"
-    sha256 "2d98a782081028fe3b7daf6b2db19e99c46f0cadab2421745de907146a3595cb"
-  end
 
   def install
     # "test" was added to std::atomic_flag in C++20
@@ -34,8 +31,8 @@ class Openrtsp < Formula
 
     # Avoid linkage to system OpenSSL
     libs = [
-      Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
-      Formula["openssl@3"].opt_lib/shared_library("libssl"),
+      formula_opt_lib("openssl@3")/shared_library("libcrypto"),
+      formula_opt_lib("openssl@3")/shared_library("libssl"),
     ]
 
     os_flag = OS.mac? ? "macosx-bigsur" : "linux"
