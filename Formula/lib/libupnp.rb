1dfe4bf860c091421368765e0afa3d12271d284e
hqzing <hqzing@outlook.com>
2026-07-01 18:36:36 +0800

    libupnp 2.0.2

--- Formula/lib/libupnp.rb
+++ Formula/lib/libupnp.rb
@@ -1,8 +1,8 @@
 class Libupnp < Formula
   desc "Portable UPnP development kit"
   homepage "https://pupnp.sourceforge.io/"
-  url "https://github.com/pupnp/pupnp/releases/download/release-1.18.5/libupnp-1.18.5.tar.bz2"
-  sha256 "fe17522c605752f9f522d8cceab2a4601d75c2b701288a3bdbd9926e1bd9a9a1"
+  url "https://github.com/pupnp/pupnp/releases/download/release-2.0.2/libupnp-2.0.2.tar.bz2"
+  sha256 "4a79edb812397e38b85bb95344a7fda4a17f54fbf53fdb828cc23ddb7e695f77"
   license "BSD-3-Clause"
 
   livecheck do
@@ -15,6 +15,10 @@ class Libupnp < Formula
   end
 
   def install
+    # https://github.com/llvm/llvm-project/issues/65557
+    inreplace "upnp/src/genlib/miniserver/miniserver.c", "switch (gMServState)",
+                                                           "switch ((MiniServerState)gMServState)"
+
     system "./configure", "--enable-ipv6", *std_configure_args
     system "make", "install"
     pkgshare.install "upnp/test/test_init.c"
