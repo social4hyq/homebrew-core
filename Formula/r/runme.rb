27a5380c62e30051f6ca5ec7a674c35fde4e366c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-02 09:33:29 +0800

    runme: update 3.16.18 bottle.

--- Formula/r/runme.rb
+++ Formula/r/runme.rb
@@ -7,7 +7,7 @@ class Runme < Formula
   head "https://github.com/runmedev/runme.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2371e2a7032c8275df2944cd611a0c1b27c8f8938ab914e0ca69e9ba9dc46fb0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5a94553ddcf56847fbdb96dd2b47ef1fc9889e1fcec241823816985d3861e239"
   end
 
   depends_on "go" => :build
