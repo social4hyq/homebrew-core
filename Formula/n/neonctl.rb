76968a84516c892e3974855d4c4084de94133ccf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-02 09:47:13 +0800

    neonctl: update 2.29.1 bottle.

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -6,7 +6,7 @@ class Neonctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a23851f2dd5c452fd5b0565dd8100086b19659576e3bea299065854efd275db1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "483872a3440da6a61c53171db5b11bfcc0f97b23d347197518fb6d6871a0c74e"
   end
 
   depends_on "node"
