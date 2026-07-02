38ece8344134fb79567b1cee080728c9d9422360
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-02 08:00:06 +0800

    stripe-cli 1.43.4

--- Formula/s/stripe-cli.rb
+++ Formula/s/stripe-cli.rb
@@ -1,8 +1,8 @@
 class StripeCli < Formula
   desc "Command-line tool for Stripe"
   homepage "https://docs.stripe.com/stripe-cli"
-  url "https://github.com/stripe/stripe-cli/archive/refs/tags/v1.43.2.tar.gz"
-  sha256 "d89003b6257ff9dcb4940fa98c8f63dea504451853f833872d5f61d65580cf9f"
+  url "https://github.com/stripe/stripe-cli/archive/refs/tags/v1.43.4.tar.gz"
+  sha256 "c21a487154de44c3a898b2a7ef99201ba6524121b14c43687e17e572210dfdfb"
   license "Apache-2.0"
 
   bottle do
