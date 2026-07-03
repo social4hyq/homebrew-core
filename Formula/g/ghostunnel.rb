9a77e39dd8c41c275ec8d2b1e7d5de8968d751c0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-03 10:38:25 +0800

    ghostunnel: update 1.11.0 bottle.

--- Formula/g/ghostunnel.rb
+++ Formula/g/ghostunnel.rb
@@ -7,7 +7,7 @@ class Ghostunnel < Formula
   head "https://github.com/ghostunnel/ghostunnel.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4db54de45d7a321579f6c988ee504d70d7f08909d829f3bef11564631d443288"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d80c092063d2b296029fa4cf64c80b86cd023af568677b0c46c591b07b675399"
   end
 
   depends_on "go" => :build
