c3afc766da3960908c2508ae7f5dc90e652dff3a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-03 09:35:36 +0800

    worktrunk: update 0.65.0 bottle.

--- Formula/w/worktrunk.rb
+++ Formula/w/worktrunk.rb
@@ -7,7 +7,7 @@ class Worktrunk < Formula
   head "https://github.com/max-sixty/worktrunk.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e78832f53d50af07f5be34326dc74d8a7b2c6bf5f13aa92a749be3551457d5bd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3844bc7e91e7d370b43969bbbd840acdc5333788dc5c533ff6e28d14493cce68"
   end
 
   depends_on "rust" => :build
