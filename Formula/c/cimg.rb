6ff5e058178decf1198964cadfdabe985fba8867
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-03 10:56:56 +0800

    cimg: update 4.0.1 bottle.

--- Formula/c/cimg.rb
+++ Formula/c/cimg.rb
@@ -11,7 +11,7 @@ class Cimg < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cbd99b1346672a0ec42559c87c7dcee5c2906ce01356f4b2a72d97fc8b236715"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ce9225853538e1dbca1b584c44b472897627a427052f5bf292c90f334b164e53"
   end
 
   def install
