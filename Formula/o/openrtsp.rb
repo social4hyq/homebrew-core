61b96840e916331dbaff0e51b10926dc3a86f27f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-05 07:24:39 +0800

    openrtsp 2026.07.04

--- Formula/o/openrtsp.rb
+++ Formula/o/openrtsp.rb
@@ -1,11 +1,11 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "https://download.live555.com/live.2026.06.29.tar.gz"
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2026.06.29.tar.gz"
-  mirror "https://pkg.freebsd.org/ports-distfiles/live.2026.06.29.tar.gz"
+  url "https://download.live555.com/live.2026.07.04.tar.gz"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2026.07.04.tar.gz"
+  mirror "https://pkg.freebsd.org/ports-distfiles/live.2026.07.04.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  sha256 "f8f9f56968e160b2864db5afcd95dc719205df71237fec63a3ff883b1d34ca78"
+  sha256 "b89b425ec6fedaa4dc0b478ba9146a431b265f5352cb17fc44c14402c512e378"
   license "LGPL-3.0-or-later"
 
   livecheck do
