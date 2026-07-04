dc57cd91344b36b4df05e18d700abb7cc37e96e5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-04 17:55:04 +0800

    kubeseal: update 0.38.4 bottle.

--- Formula/k/kubeseal.rb
+++ Formula/k/kubeseal.rb
@@ -13,7 +13,7 @@ class Kubeseal < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "078d4f67383751b67c1e9a62b9c0e0c11fea04ffe6cbca76d7184e8009b9db95"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a1b42314dd3e5dd409f183d1323eb0bcd1aabcfc1c1ecdc843c45a627c057ffd"
   end
 
   depends_on "go" => :build
