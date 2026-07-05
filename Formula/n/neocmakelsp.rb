5fdc665142567d6a6223eb5fee19d93e7d96e1c9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-05 20:23:12 +0800

    neocmakelsp: update 0.10.4 bottle.

--- Formula/n/neocmakelsp.rb
+++ Formula/n/neocmakelsp.rb
@@ -7,7 +7,7 @@ class Neocmakelsp < Formula
   head "https://github.com/neocmakelsp/neocmakelsp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9b10e13fb836cd078f5bf5602dc83f007d912956afb0347a59101c2f4bbf869a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b58d2277e0b3b0f4753d4309f795779a2eaffdc74e16380c05c6ccc1422fd590"
   end
 
   depends_on "rust" => :build
