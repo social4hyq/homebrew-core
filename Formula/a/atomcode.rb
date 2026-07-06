af1db06538c1a8dc4afe1baa38314b68da1113f5
hqzing <hqzing@outlook.com>
2026-07-06 09:47:37 +0800

    atomcode 4.25.9

--- Formula/a/atomcode.rb
+++ Formula/a/atomcode.rb
@@ -1,8 +1,8 @@
 class Atomcode < Formula
   desc "Open-source alternative to Claude Code / Cursor Agent, living in your terminal"
   homepage "https://atomcode.atomgit.com/"
-  url "https://raw.atomgit.com/atomgit_atomcode/atomcode/archive/refs/heads/v4.25.8.tar.gz"
-  sha256 "a437fc3f62ea8db25dafc04606e40d824b04aacc9e6377d30d7bb0fab1d8c6b7"
+  url "https://raw.atomgit.com/atomgit_atomcode/atomcode/archive/refs/heads/v4.25.9.tar.gz"
+  sha256 "6affde9df2607f68735685aece4ad501a81322daba6b368a7c0d2a1f9887157a"
   license "MIT"
 
   livecheck do
