d337e4622791f5890073b7033b42f3ec97df4d6b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-08 07:42:36 +0800

    resterm 0.45.2

--- Formula/r/resterm.rb
+++ Formula/r/resterm.rb
@@ -1,8 +1,8 @@
 class Resterm < Formula
   desc "Terminal client for .http/.rest files with HTTP, GraphQL, and gRPC support"
   homepage "https://github.com/unkn0wn-root/resterm"
-  url "https://github.com/unkn0wn-root/resterm/archive/refs/tags/v0.44.6.tar.gz"
-  sha256 "17640f6646b799e9e7f11469f74a4af796aead0796a78ec58247dac51aafbda0"
+  url "https://github.com/unkn0wn-root/resterm/archive/refs/tags/v0.45.2.tar.gz"
+  sha256 "dfa9d4c864642d64578c29e7479a09aa0fcb24571946f9919b532c256bcef187"
   license "Apache-2.0"
   head "https://github.com/unkn0wn-root/resterm.git", branch: "main"
 
