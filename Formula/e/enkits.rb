c0a5cca46dac4ab6fb1a053c2c700b13b485fa7b
hqzing <hqzing@outlook.com>
2026-07-07 07:52:57 +0000

    enkits 1.12

--- Formula/e/enkits.rb
+++ Formula/e/enkits.rb
@@ -1,12 +1,17 @@
 class Enkits < Formula
   desc "C and C++ Task Scheduler for creating parallel programs"
   homepage "https://github.com/dougbinks/enkiTS"
-  url "https://github.com/dougbinks/enkiTS/archive/refs/tags/v1.11.tar.gz"
-  sha256 "b57a782a6a68146169d29d180d3553bfecb9f1a0e87a5159082331920e7d297e"
+  url "https://github.com/dougbinks/enkiTS/archive/refs/tags/v1.12.tar.gz"
+  sha256 "8373b6199d56816bd3ba58432eae74e2ebd5afcfdffb723073cc34730b189fd5"
   license "Zlib"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "84b41d45b88c5850f81ce06dbd63c7a1dad1a482bd71dcc737c4b532bac5ba89"
+    sha256 cellar: :any, arm64_tahoe:   "401d2242276475ba7176da65f6de0e70f767a59a1ec50239a14f9d41901e20d0"
+    sha256 cellar: :any, arm64_sequoia: "df82b8c8ce8d5c0ceb55ab5e572b6f61617ba2009469e9586d6adc2b8a6a64f4"
+    sha256 cellar: :any, arm64_sonoma:  "41a74d2427e5a8366243a5e9efb888da9cd5abacef5f2655236c1740f3103369"
+    sha256 cellar: :any, sonoma:        "013b6c4ec41f9a2c7d3523a8d6a9db43ac743db4dd0d4b311437cb60b1fb30ed"
+    sha256 cellar: :any, arm64_linux:   "c2a296ec45bee2c2794aa11ff67f3f42d06c44bb27c0a0524667f7dec957519d"
+    sha256 cellar: :any, x86_64_linux:  "3f68e3934e96a041e79efc854aec8d7e41ec7ca368963008c399bded2af261ac"
   end
 
   depends_on "cmake" => :build
@@ -17,12 +22,9 @@ class Enkits < Formula
       -DENKITS_INSTALL=ON
       -DENKITS_BUILD_SHARED=ON
     ]
-    # Workaround to build with CMake 4
-    args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    lib.install_symlink "#{lib}/enkiTS/#{shared_library("libenkiTS")}"
     pkgshare.install "example"
   end
 
