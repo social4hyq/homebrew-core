5f62c2f16456a62381a7792e68089f4ce2cc0d5b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-07 16:30:19 +0800

    openssh: update 10.3p1_6 bottle.

--- Formula/o/openssh.rb
+++ Formula/o/openssh.rb
@@ -15,7 +15,7 @@ class Openssh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6b906dbabbe28e2e2a7b52a7fa6852e5561107167970bee89777d843409fdb1c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d0d4a8e7268a6ac66e11e50882bddcb74cf742f8c530789371a7bc41a7f04745"
   end
 
   depends_on "pkgconf" => :build
