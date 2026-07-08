11f35754b2aedb5c0150211b690f2555147e7be9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-08 17:04:36 +0800

    openrtsp: update 2026.07.08 bottle.

--- Formula/o/openrtsp.rb
+++ Formula/o/openrtsp.rb
@@ -14,12 +14,7 @@ class Openrtsp < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "64f756bb22988c942972d6e2346cf3980110f18beb7d6b1f2699be5c036b079f"
-    sha256 cellar: :any, arm64_sequoia: "c72c315f1324ace45999d145f8e062e3b2eaac558df4be4d2d318eabf6516cc6"
-    sha256 cellar: :any, arm64_sonoma:  "bb348691152248a7a8a37b89437eece99c6214e55ca026af654d0ab7f1f11721"
-    sha256 cellar: :any, sonoma:        "c5ae519238738217bd6e61264a26f07ea992c3f2d16d3d7652dac297a0581073"
-    sha256 cellar: :any, arm64_linux:   "764be10ee12f9daa9c738a9b152eee86182cd50ae00c92673fc6fc78999cd9fa"
-    sha256 cellar: :any, x86_64_linux:  "807e342f98f7009a972004485e7152f69d2713b84eb4008b0ddadb311341e2a0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6782275137a2d630c9c6192b3c254548a647a60e9189d325d464580b3dea7a87"
   end
 
   depends_on "openssl@3"
