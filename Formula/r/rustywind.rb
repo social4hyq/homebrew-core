677691ed20100f2cceff1c3375907a454a4340cf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-08 17:09:39 +0800

    rustywind: update 0.25.1 bottle.

--- Formula/r/rustywind.rb
+++ Formula/r/rustywind.rb
@@ -7,12 +7,7 @@ class Rustywind < Formula
   head "https://github.com/avencera/rustywind.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b5fe7dcec93425180860e9fcbc77cf666b2d3fa563e743aa625e16904ded16b5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9201254612780d55d30de39013e041f864f5c14464904bcfcafb5e517c601581"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8674af57e825b7e3796399f05a99f6df396c534f1953f4dbafb78c77ab6374d9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "78973848d37e16c0fd9277a0df182b5ec29455300a7c5b110bd55291cdd24e55"
-    sha256 cellar: :any,                 arm64_linux:   "41cc84326665f64fe9eb95dcf4933fff12495a2a00fe52196752481abeae03e8"
-    sha256 cellar: :any,                 x86_64_linux:  "d7697ea7e079d229c69a7f0dbc272b182134fd2b75fec4685efd133b3d0212a9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cac05fd4742d2adcc0a947be927224a544309ce0446752614af81b9c9839594a"
   end
 
   depends_on "rust" => :build
