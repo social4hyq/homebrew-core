d55d0efd4341d3bbf25dfb8103fefa1979dc4478
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-08 17:00:54 +0800

    vercel-cli: update 54.21.0 bottle.

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -6,12 +6,7 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "d6e801d2e4c9ae541b001598e1d06457739dca07031876992cf7afd26e9fe0a0"
-    sha256 cellar: :any,                 arm64_sequoia: "f06c84569dedecd621ca91fe509aaf273f60b0e351b9d57bdbc7acec6e82c9bb"
-    sha256 cellar: :any,                 arm64_sonoma:  "f06c84569dedecd621ca91fe509aaf273f60b0e351b9d57bdbc7acec6e82c9bb"
-    sha256 cellar: :any,                 sonoma:        "a2ec1bee736cb831b2e18a754d039f7860a99c6482a67c19e1d6afb459c2abb1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fec0963a31cd6862cdb53b80c72ea0808b37ed403026725167070e5fc134eed6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ce344c87f7901c3450f952f598e4b1762719489fbb7097b3c33f39f3f785f09f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "961e594071ba799fdca5889cea41373f1e1a33d1647f9565f10500352b2c000a"
   end
 
   depends_on "node"
