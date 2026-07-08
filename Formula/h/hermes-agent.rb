b97fb797f789ab03296eb86bee42fd4e3e9436ea
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-09 06:48:34 +0800

    hermes-agent 2026.7.7.2

--- Formula/h/hermes-agent.rb
+++ Formula/h/hermes-agent.rb
@@ -3,8 +3,8 @@ class HermesAgent < Formula
 
   desc "Self-improving AI agent that creates skills from experience"
   homepage "https://hermes-agent.nousresearch.com"
-  url "https://github.com/NousResearch/hermes-agent/archive/refs/tags/v2026.7.1.tar.gz"
-  sha256 "ec8a380629cc2f3f2102dd92cad50c4ded706fe59c0e359a05681166a0ae2991"
+  url "https://github.com/NousResearch/hermes-agent/archive/refs/tags/v2026.7.7.2.tar.gz"
+  sha256 "f5d1022eed3763a768cf7b0f0844831f0170a35f54eb8d18223f2e93f503025e"
   license "MIT"
   head "https://github.com/NousResearch/hermes-agent.git", branch: "main"
 
