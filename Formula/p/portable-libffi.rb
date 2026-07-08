6e21d9cdf8e3b0e5e203a69b17fce0054f784259
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-09 07:33:57 +0800

    portable-libffi 3.7.0

--- Formula/p/portable-libffi.rb
+++ Formula/p/portable-libffi.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/portable-formula", __dir__)
 class PortableLibffi < PortableFormula
   desc "Portable Foreign Function Interface library"
   homepage "https://sourceware.org/libffi/"
-  url "https://github.com/libffi/libffi/releases/download/v3.6.0/libffi-3.6.0.tar.gz"
-  sha256 "31ff1fe32deaebfbb388727f32677bb254bf2a41382c51464c0b1837c9ee9828"
+  url "https://github.com/libffi/libffi/releases/download/v3.7.0/libffi-3.7.0.tar.gz"
+  sha256 "2255c5a638dfb51bf67c20a12a7bb70d17feb1e9eababac05f5573146f586436"
   license "MIT"
 
   livecheck do
