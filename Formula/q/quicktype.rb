0cb367a75b25650b9f5dc6a7bec4cf8e73156e39
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-09 07:38:22 +0800

    quicktype 23.3.23

--- Formula/q/quicktype.rb
+++ Formula/q/quicktype.rb
@@ -1,8 +1,8 @@
 class Quicktype < Formula
   desc "Generate types and converters from JSON, Schema, and GraphQL"
   homepage "https://github.com/glideapps/quicktype"
-  url "https://registry.npmjs.org/quicktype/-/quicktype-23.3.17.tgz"
-  sha256 "4543cba17ee1abbc73ac7178f2eea5efffc8392f3b16245ac4976f5cf4c485da"
+  url "https://registry.npmjs.org/quicktype/-/quicktype-23.3.23.tgz"
+  sha256 "4aabf760c74610bda39ee7c950683f4a6d2e854bc6dc2c6a7aac9c7f13cb6ec9"
   license "Apache-2.0"
 
   bottle do
