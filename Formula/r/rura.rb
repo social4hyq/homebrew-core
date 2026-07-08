0a18ced3850da930a79b4ea4eb2250d2e2d92a57
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-08 10:23:47 +0800

    rura: update 1.8.0 bottle.

--- Formula/r/rura.rb
+++ Formula/r/rura.rb
@@ -6,7 +6,7 @@ class Rura < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f5548f4dd2ba515a98c39ed31b63c1c49fbaf5f591639714f2b47c77f1c6e672"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8815d940d244d42b9a7b98caa4e06dd19de34fcf083b0b08f50cfc3a5a1dd767"
   end
 
   depends_on "rust" => :build
