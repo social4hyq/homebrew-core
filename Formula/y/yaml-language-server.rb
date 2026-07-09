8744a3e6e2de8ad727a76f098c4f7845f5a847af
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-09 09:29:36 +0800

    yaml-language-server: update 1.24.0 bottle.

--- Formula/y/yaml-language-server.rb
+++ Formula/y/yaml-language-server.rb
@@ -6,7 +6,7 @@ class YamlLanguageServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e7816c652fa740e8503ecc0d1a81fd5d0628115e536c91b6ecac169dc0999285"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bfc4b6f5f1ee02415fd24d195b14a03ac6c1440e882a02682ca58c963d9b2759"
   end
 
   depends_on "node"
