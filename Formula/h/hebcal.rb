7255ab069a1d7888bc98f0a5e43198e3625564f1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-09 10:22:32 +0800

    hebcal: update 5.14.0 bottle.

--- Formula/h/hebcal.rb
+++ Formula/h/hebcal.rb
@@ -7,7 +7,7 @@ class Hebcal < Formula
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ca1012546e3c119938b91c2a5897e62e8d0f030db798a4550f37bc67f567dfb6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d440cd57784adee3cd1180211c1c2c1fbd32d68ac8ca9e99c332d63effdfeca7"
   end
 
   depends_on "go" => :build
