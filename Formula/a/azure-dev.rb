d96e3db0f4874f4f3bc4376cdabbbead0338743b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-10 17:12:07 +0800

    azure-dev: update 1.27.1 bottle.

--- Formula/a/azure-dev.rb
+++ Formula/a/azure-dev.rb
@@ -7,7 +7,7 @@ class AzureDev < Formula
   head "https://github.com/Azure/azure-dev.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b484d683817e9ea7a0b7f2a2337425d5240cec8d53080313e2cc2589cf50366e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "76c0840c060478bc2960041061a88c42d90222efaa9f159c6dc74d7c0ae56c55"
   end
 
   depends_on "go" => :build
