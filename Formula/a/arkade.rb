9530441a5d13e598090470ca6d0c0a5fd0c6e45f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-11 06:08:21 +0800

    arkade 0.11.113

--- Formula/a/arkade.rb
+++ Formula/a/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
   desc "Open Source Kubernetes Marketplace"
   homepage "https://blog.alexellis.io/kubernetes-marketplace-two-year-update/"
-  url "https://github.com/alexellis/arkade/archive/refs/tags/0.11.110.tar.gz"
-  sha256 "4d5493aacbe11584ab0eae61fe2b97cfb370ad1f35ae8e05574c1de595abe5f7"
+  url "https://github.com/alexellis/arkade/archive/refs/tags/0.11.113.tar.gz"
+  sha256 "3adf369bf229709255aa5d4a6ff6c2959f4f2eb678e3b93a4cc4471d19de1f2f"
   license "MIT"
   head "https://github.com/alexellis/arkade.git", branch: "master"
 
