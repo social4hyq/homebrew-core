dfe283a5eb6e9c26486bedf136d5595e225da326
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-10 16:59:50 +0800

    talm: update 0.33.1 bottle.

--- Formula/t/talm.rb
+++ Formula/t/talm.rb
@@ -7,7 +7,7 @@ class Talm < Formula
   head "https://github.com/cozystack/talm.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "678df8f3521cdec42974a8148d81b5359681663b6d0fbb03f9262d6c5b98d067"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e0b2ff1d5b2d2845fdffe2bcfaa243cb113506f9407a461ffa1a2fe401e30829"
   end
 
   depends_on "go" => :build
