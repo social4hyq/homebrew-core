2f8aafc362dad18946fbf09f2412aed3ca61cf06
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-10 18:35:54 +0800

    atuin-server: update 18.17.0 bottle.

--- Formula/a/atuin-server.rb
+++ Formula/a/atuin-server.rb
@@ -7,7 +7,7 @@ class AtuinServer < Formula
   head "https://github.com/atuinsh/atuin.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b1caa2393f89c886ae3421fda50c0663fb2cef6e7ba59fa0eceb04be19f6d32d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6109d97e7c1ec94affff6dfe4712e13ce024be0bab76e1aef10e04ef4e8d9c9a"
   end
 
   depends_on "protobuf" => :build
