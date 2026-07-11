8aa72accf66d346614a5082f754ffb9de98673cb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-11 17:57:49 +0800

    prettier: update 3.9.5 bottle.

--- Formula/p/prettier.rb
+++ Formula/p/prettier.rb
@@ -7,7 +7,7 @@ class Prettier < Formula
   head "https://github.com/prettier/prettier.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "be7bc1cf85cc14fe523f1e4e8f654eeff1cc9d9837bb6d8eaf2ee4ce99e31c46"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dfabe623e171966c25cd128bddd2e45c6d656054ee02dfdc6599f740058621b5"
   end
 
   depends_on "node"
