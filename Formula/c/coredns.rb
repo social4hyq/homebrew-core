34910836ae7bf609c13ebb650e29bbcb87e841cd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-11 18:50:55 +0800

    coredns: update 1.14.6 bottle.

--- Formula/c/coredns.rb
+++ Formula/c/coredns.rb
@@ -12,7 +12,7 @@ class Coredns < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "04bd5f2be7fb9a74f889ef645e04f513f31c4528ffd1e295da50e6b751e618e0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5ed401cfc07d1361279b81140f8a80f57dc5d84fd4a29440a8044474414b827a"
   end
 
   depends_on "go" => :build
