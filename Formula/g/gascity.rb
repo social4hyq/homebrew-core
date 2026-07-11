b0360a550c438a2b35e058794db759bb926b88a6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-11 18:34:42 +0800

    gascity: update 1.3.4 bottle.

--- Formula/g/gascity.rb
+++ Formula/g/gascity.rb
@@ -7,7 +7,7 @@ class Gascity < Formula
   head "https://github.com/gastownhall/gascity.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ca3a4d7c8ac5a903a5d9af6e8982fab72b6b4b2b82c5d235f594209bd4a90ba1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a6b74ee8d3916b3097f4c98c616d63cbedb2809c934ace1a0899871346684224"
   end
 
   depends_on "go" => :build
