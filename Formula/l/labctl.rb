71ab09f6693780462d2e5c136d71e6332a368b1a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-11 18:14:33 +0800

    labctl: update 0.1.98 bottle.

--- Formula/l/labctl.rb
+++ Formula/l/labctl.rb
@@ -6,7 +6,7 @@ class Labctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3e1e1982fd7db0bc542e9324fa7f0546dfc784ba63f2a4186affcb2c2bcea967"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9d6f4b9edd679b13272d64d4397302dc3d09acffc06577d28a8e7b0eb4f62374"
   end
 
   depends_on "go" => :build
