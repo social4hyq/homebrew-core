11975c0df80957834f248288b3295625d7efbff1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-12 13:26:53 +0800

    meson: update 1.11.2 bottle.

--- Formula/m/meson.rb
+++ Formula/m/meson.rb
@@ -7,7 +7,7 @@ class Meson < Formula
   head "https://github.com/mesonbuild/meson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7a512a99942ce29bcac7b459c6d25f7b6468385bf7191a54564fc30ab76b89dc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4287c3f9b6e9cda867e03ffd5261bab3550dbee488a503c298255223cf387030"
   end
 
   depends_on "ninja"
