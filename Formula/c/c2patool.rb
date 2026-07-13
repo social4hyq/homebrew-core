e01d3b745fc9a73936f7524570fa2718660fd4c5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-14 06:15:40 +0800

    c2patool 0.26.72

--- Formula/c/c2patool.rb
+++ Formula/c/c2patool.rb
@@ -1,8 +1,8 @@
 class C2patool < Formula
   desc "CLI for working with C2PA manifests and media assets"
   homepage "https://contentauthenticity.org"
-  url "https://github.com/contentauth/c2pa-rs/archive/refs/tags/c2patool-v0.26.70.tar.gz"
-  sha256 "6b3c4e91302d356f54cdae84a2c0ee1dbb4cfec1bb1efa28377900fad98db634"
+  url "https://github.com/contentauth/c2pa-rs/archive/refs/tags/c2patool-v0.26.72.tar.gz"
+  sha256 "66e2991476e8a6962b73f5d0e8e2aa10b3625128acc40ad5d73e840a5f877594"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/contentauth/c2pa-rs.git", branch: "main"
 
