355cf0d6d50dd75c49f5c65a95afd195e742ba47
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-14 07:13:13 +0800

    mp4ff 0.54.0

--- Formula/m/mp4ff.rb
+++ Formula/m/mp4ff.rb
@@ -1,8 +1,8 @@
 class Mp4ff < Formula
   desc "Tools for parsing and manipulating MP4/ISOBMFF files"
   homepage "https://github.com/Eyevinn/mp4ff"
-  url "https://github.com/Eyevinn/mp4ff/archive/refs/tags/v0.53.0.tar.gz"
-  sha256 "b6ec3f2267f4cd201c8c68ce8ce865ca2535a84bc37440dcec0b65e8fd92b648"
+  url "https://github.com/Eyevinn/mp4ff/archive/refs/tags/v0.54.0.tar.gz"
+  sha256 "5fa5394fdd24ae98b27ac1b999c10d0e0227f34936a53e12e7cc822fbe5bc796"
   license "MIT"
 
   bottle do
