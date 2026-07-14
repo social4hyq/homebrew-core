df9977f01f952db376e457f3ee2a5c1934d2a154
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-14 09:34:36 +0800

    sesh: update 2.27.0 bottle.

--- Formula/s/sesh.rb
+++ Formula/s/sesh.rb
@@ -7,7 +7,7 @@ class Sesh < Formula
   head "https://github.com/joshmedeski/sesh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8495ffa511112e61d050c9b829d5fc9221ab53be21f5c9c72514509bac4b1e6d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e0c5f865fcbb7cd31094893ac01021c4140ff44b8c5167822fbe4d8533fe59bf"
   end
 
   depends_on "go" => :build
