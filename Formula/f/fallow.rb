f61155b0c2670803f2dd7a13c0ffba6bfb738a83
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-14 11:03:59 +0800

    fallow: update 3.4.2 bottle.

--- Formula/f/fallow.rb
+++ Formula/f/fallow.rb
@@ -7,7 +7,7 @@ class Fallow < Formula
   head "https://github.com/fallow-rs/fallow.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "939e32a4476be8425c72c4edd8cab56114e950be982cd8decaa24c5dd3d91804"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5320c25e69ee4464d1ebaffe269971d723f6f93ebec4f78db1dbf08d1a22aa3c"
   end
 
   depends_on "rust" => :build
