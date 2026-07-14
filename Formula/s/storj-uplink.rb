827e284693cbce19e2492f5853d8645cb950a800
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-15 07:50:19 +0800

    storj-uplink 1.160.2

--- Formula/s/storj-uplink.rb
+++ Formula/s/storj-uplink.rb
@@ -1,8 +1,8 @@
 class StorjUplink < Formula
   desc "Uplink CLI for the Storj network"
   homepage "https://storj.io"
-  url "https://github.com/storj/storj/archive/refs/tags/v1.158.8.tar.gz"
-  sha256 "e86a07582fbd286d815da47e64f378681834fd358721ad865db098a1e8447d6f"
+  url "https://github.com/storj/storj/archive/refs/tags/v1.160.2.tar.gz"
+  sha256 "3d9a06237dc76ae8576bddd091c57bdef22b8daf35d2ab08bb96146ed1ebd8e1"
   license "AGPL-3.0-only"
 
   # Upstream creates stable releases and marks them as "pre-release" before
