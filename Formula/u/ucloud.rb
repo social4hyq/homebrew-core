d6055a15643e82797e18d9c4f4a9258a3648b02a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-14 15:08:08 +0800

    ucloud: update 0.3.7 bottle.

--- Formula/u/ucloud.rb
+++ Formula/u/ucloud.rb
@@ -6,7 +6,7 @@ class Ucloud < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3a535bbba33fa040892a80ebdaf21e8a4905ae1c0549ba5b04aa258d1dd53366"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "13dfda5d33cbb4358b6c04d29b7cb9f3603469bf1f67560a72efef4e06954f82"
   end
 
   depends_on "go" => :build
