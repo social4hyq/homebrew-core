922d06067219ab90bc202df7165652d7419d9f8c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-14 09:37:52 +0800

    sqlite-utils: update 4.1.1 bottle.

--- Formula/s/sqlite-utils.rb
+++ Formula/s/sqlite-utils.rb
@@ -8,7 +8,7 @@ class SqliteUtils < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b23cf1a8656aa061541400eccb4a8bbad41418b6a663bbee8ada65abe8be70c8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "818a872f9324ea9cf179c5a2d36ab0c5e99c1bd54cb13cbdaea94965e01a95c7"
   end
 
   depends_on "python@3.14"
