aa48a3d1d27d93c056971b9ae28c13243194d1fc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-16 07:52:34 +0800

    termsvg 0.11.0

--- Formula/t/termsvg.rb
+++ Formula/t/termsvg.rb
@@ -1,8 +1,8 @@
 class Termsvg < Formula
   desc "Record, share and export your terminal as a animated SVG image"
   homepage "https://github.com/MrMarble/termsvg"
-  url "https://github.com/MrMarble/termsvg/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "7b9c7aae3f58c9dce7930fe2868e39d59817e143d13e5771c268f9c7f0cbdf31"
+  url "https://github.com/MrMarble/termsvg/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "035334dce273efe3ff4fcf6eecf0b9facdcb2988403eb9e473659c0c7d0d1c52"
   license "GPL-3.0-only"
   head "https://github.com/MrMarble/termsvg.git", branch: "master"
 
