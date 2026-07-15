4dc1b2ab6a81b584d9720f43bf8d7ad5b2b73b3f
lanfungcing <lanfq.26@qq.com>
2026-07-15 03:16:49 +0000

    ipbt 20260527

--- Formula/i/ipbt.rb
+++ Formula/i/ipbt.rb
@@ -1,9 +1,9 @@
 class Ipbt < Formula
   desc "Program for recording a UNIX terminal session"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/"
-  url "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20260410.a852474.tar.gz"
-  version "20260410"
-  sha256 "0713f515794643d48c88e79429dc392741e2ae15093b8d92bfc189110228406a"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20260527.fdcd2b0.tar.gz"
+  version "20260527"
+  sha256 "5303c8010addf1b2dec5375c8cf32d70e1835545d639ee2183863e09d4e80aa3"
   license "MIT"
 
   livecheck do
@@ -11,8 +11,15 @@ class Ipbt < Formula
     regex(/href=.*?ipbt[._-]v?(\d+(?:\.\d+)*)(?:[._-][\da-z]+)?\.t/i)
   end
 
+  no_autobump! because: :incompatible_version_format
+
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b43fee1082746c6d7143f2d9f81e5bf72280436bc23b7e821750a859f0b9c551"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "21d5de28b578d9bf806a56bd01182467bd2978fd69e47b51f10735f28c2b89ba"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b7af1d0d8c081628109d55aa72560b68154b4d10f23dee791f152a237fdc565"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e3d5b673fc202ac79d21019152ebe47e26c0558a33bcb2ce2a635d1124889dc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bdf1fa9fa0911eea396f3aa1f2258ace34feefef8b968a3043ba4d427c4e6858"
+    sha256 cellar: :any,                 arm64_linux:   "729b19d3141ebde1f08c3b98f8eefb68c8a9a9708208013d1f65dd2aeb5c0fbf"
+    sha256 cellar: :any,                 x86_64_linux:  "98981aa30643c7837c6cd87a6569723dd357dcaea8ea5bd4feacb25ffaf763c3"
   end
 
   depends_on "cmake" => :build
