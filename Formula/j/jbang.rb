cc8d26e421c08bdd446f6762928d028939b87c53
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-15 10:18:40 +0800

    jbang: update 0.141.0 bottle.

--- Formula/j/jbang.rb
+++ Formula/j/jbang.rb
@@ -11,7 +11,7 @@ class Jbang < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "3ec770571b36225eb2969de2f60c7c040b3751211d34056775c210309f0907c8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6ae426171c6901e5cd514d257eba4e2c3c6016eff20e6bf261b9763e4f4acc33"
   end
 
   depends_on "openjdk"
