9c8f24dce36f3d6830b7f366a279eb08798d53d1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-16 06:18:01 +0800

    c-blosc2 3.2.3

--- Formula/c/c-blosc2.rb
+++ Formula/c/c-blosc2.rb
@@ -1,8 +1,8 @@
 class CBlosc2 < Formula
   desc "Fast, compressed, persistent binary data store library for C"
   homepage "https://www.blosc.org"
-  url "https://github.com/Blosc/c-blosc2/archive/refs/tags/v3.2.1.tar.gz"
-  sha256 "945cc68d47ba2817279b5d64c0f9b5edce6849a52ac1a46ba8c3ecaedce35769"
+  url "https://github.com/Blosc/c-blosc2/archive/refs/tags/v3.2.3.tar.gz"
+  sha256 "32977709c21f3fec50befa7a031fa624b963427b69d3ffb69c91aadc9279c887"
   license "BSD-3-Clause"
   compatibility_version 1
   head "https://github.com/Blosc/c-blosc2.git", branch: "main"
