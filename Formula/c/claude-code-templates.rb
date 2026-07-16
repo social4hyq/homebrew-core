e1a9e23a964b1132cd0f14c4c3ceedf2348438f3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-17 06:22:24 +0800

    claude-code-templates 1.29.4

--- Formula/c/claude-code-templates.rb
+++ Formula/c/claude-code-templates.rb
@@ -1,8 +1,8 @@
 class ClaudeCodeTemplates < Formula
   desc "CLI tool for configuring and monitoring Claude Code"
   homepage "https://www.aitmpl.com/agents"
-  url "https://registry.npmjs.org/claude-code-templates/-/claude-code-templates-1.29.2.tgz"
-  sha256 "ed0d89293d51b7adf19033fb8268b30722afa51a215524aa698890a28bc3055f"
+  url "https://registry.npmjs.org/claude-code-templates/-/claude-code-templates-1.29.4.tgz"
+  sha256 "b5c67ea73eebb3bf6aee21556182d7aba4d58d12ac49acc5c98d351071a6babf"
   license "MIT"
 
   bottle do
