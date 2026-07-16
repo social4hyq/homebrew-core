070e76c4c11d0fbcdfd668fb934d923c14f3475f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-17 07:58:43 +0800

    pscale 0.303.0

--- Formula/p/pscale.rb
+++ Formula/p/pscale.rb
@@ -1,8 +1,8 @@
 class Pscale < Formula
   desc "CLI for PlanetScale Database"
   homepage "https://www.planetscale.com/"
-  url "https://github.com/planetscale/cli/archive/refs/tags/v0.300.0.tar.gz"
-  sha256 "9d3ca93acd84ce8af4a9a9683656d3c0ec96c852872ac6ef9a11dd838fca8b41"
+  url "https://github.com/planetscale/cli/archive/refs/tags/v0.303.0.tar.gz"
+  sha256 "0ac4b677562f01a784868a011afba62098db7e12cb702f276370dbf6d9a20d23"
   license "Apache-2.0"
   head "https://github.com/planetscale/cli.git", branch: "main"
 
