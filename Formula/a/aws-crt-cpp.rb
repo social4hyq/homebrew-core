5f1aa2e857429e523e8ff7aabb32d181f68b6b69
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-17 06:09:07 +0800

    aws-crt-cpp 0.42.1

--- Formula/a/aws-crt-cpp.rb
+++ Formula/a/aws-crt-cpp.rb
@@ -1,8 +1,8 @@
 class AwsCrtCpp < Formula
   desc "C++ wrapper around the aws-c-* libraries"
   homepage "https://github.com/awslabs/aws-crt-cpp"
-  url "https://github.com/awslabs/aws-crt-cpp/archive/refs/tags/v0.41.0.tar.gz"
-  sha256 "1c1db6636254bc2dc129cbff0d14b44a98c4a6e7cc2abcafa7326b25f88e6b34"
+  url "https://github.com/awslabs/aws-crt-cpp/archive/refs/tags/v0.42.1.tar.gz"
+  sha256 "bf24fdba415842654f26d36978061812e38d3485e8505d2fabf0a548fcf735c4"
   license "Apache-2.0"
   compatibility_version 1
 
