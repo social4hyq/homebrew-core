57af71b0821d011c24cb6dc04e8ef95706e468be
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-17 06:12:04 +0800

    awscli 2.36.1

--- Formula/a/awscli.rb
+++ Formula/a/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/refs/tags/2.35.24.tar.gz"
-  sha256 "10e47c9d3bdb78cd96108717c18f95109e6cb933978024fc49b68b94723e7868"
+  url "https://github.com/aws/aws-cli/archive/refs/tags/2.36.1.tar.gz"
+  sha256 "df9fe40f2f15d18720ae695ffd29466ba648e8e3a9bcd9cca1319382c19b1081"
   license "Apache-2.0"
   compatibility_version 1
   head "https://github.com/aws/aws-cli.git", branch: "v2"
