4b98b28367c671ca4d457e73333d9db7e703b137
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-16 12:49:02 +0800

    aws-lc: update 5.2.0 bottle.

--- Formula/a/aws-lc.rb
+++ Formula/a/aws-lc.rb
@@ -11,7 +11,7 @@ class AwsLc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "bd484487971a6958c5c8c1225a312bd3189f8a2e579f841fe63ea8fb6a71bacb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "aafa1460fce3b18430364db67ccfe1fcc6318a92f7c320afa1f3cf9091af482f"
   end
 
   keg_only "it conflicts with OpenSSL"
