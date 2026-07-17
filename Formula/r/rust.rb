b0393866b70d5231622d23f11bf990739fdae277
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-17 08:07:11 +0800

    rust 1.97.1

--- Formula/r/rust.rb
+++ Formula/r/rust.rb
@@ -1,8 +1,8 @@
 class Rust < Formula
   desc "Safe, concurrent, practical language"
   homepage "https://www.rust-lang.org/"
-  url "https://static.rust-lang.org/dist/rust-1.97.0-aarch64-unknown-linux-ohos.tar.xz"
-  sha256 "dd9dc155f797464befdf799359f45d3d699e4fb5585841f16dac4e2b35700613"
+  url "https://static.rust-lang.org/dist/rust-1.97.1-aarch64-unknown-linux-ohos.tar.xz"
+  sha256 "c0f366f6b46ade9e199a05a05ca61039b6cd03c86e0eb50c1b651b72a8af5c35"
   license any_of: ["Apache-2.0", "MIT"]
   compatibility_version 1
   head "https://github.com/rust-lang/rust.git", branch: "main"
