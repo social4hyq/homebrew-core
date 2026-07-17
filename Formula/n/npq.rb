92552a2b0e607d488bd2e04dfc45fc91b4524aa1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-18 07:35:24 +0800

    npq 3.22.0

--- Formula/n/npq.rb
+++ Formula/n/npq.rb
@@ -1,8 +1,8 @@
 class Npq < Formula
   desc "Audit npm packages before you install them"
   homepage "https://github.com/lirantal/npq"
-  url "https://registry.npmjs.org/npq/-/npq-3.21.0.tgz"
-  sha256 "101a251ce7d427ab19ea9282fa51bb69e489dc08ede9895b78c71b93e2c7b925"
+  url "https://registry.npmjs.org/npq/-/npq-3.22.0.tgz"
+  sha256 "86d768b1cc848de520937f156b266f800a48370afa96c1b99faa95c08dc0d11a"
   license "Apache-2.0"
 
   bottle do
