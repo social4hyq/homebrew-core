e78d8f385e1f7c45db03d80a1eb388d589ffe732
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-17 08:12:35 +0800

    sk 5.1.3

--- Formula/s/sk.rb
+++ Formula/s/sk.rb
@@ -1,8 +1,8 @@
 class Sk < Formula
   desc "Fuzzy Finder in rust!"
   homepage "https://github.com/skim-rs/skim"
-  url "https://github.com/skim-rs/skim/archive/refs/tags/v5.1.0.tar.gz"
-  sha256 "9f3d8226114b7f76e78b2a4b2819c7a23694528bd06a3f05e02e6c9667143d33"
+  url "https://github.com/skim-rs/skim/archive/refs/tags/v5.1.3.tar.gz"
+  sha256 "e348fa7690c9e1fe33ecf8f162324101214a658082e887e9372dd264dbea6f48"
   license "MIT"
   head "https://github.com/skim-rs/skim.git", branch: "master"
 
