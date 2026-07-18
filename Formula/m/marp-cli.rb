ea3967d46e3cff049b4d9077dd654d6e80a71eee
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-19 07:32:21 +0800

    marp-cli 4.5.0

--- Formula/m/marp-cli.rb
+++ Formula/m/marp-cli.rb
@@ -1,8 +1,8 @@
 class MarpCli < Formula
   desc "Easily convert Marp Markdown files into static HTML/CSS, PDF, PPT and images"
   homepage "https://github.com/marp-team/marp-cli"
-  url "https://registry.npmjs.org/@marp-team/marp-cli/-/marp-cli-4.4.1.tgz"
-  sha256 "7c471064870b4d591fc4e038210a415e873d534fc7ea16813c6b9f1ec61b7d1a"
+  url "https://registry.npmjs.org/@marp-team/marp-cli/-/marp-cli-4.5.0.tgz"
+  sha256 "36145f3400213afec408f8eff5dec2a3e5c5238201246d598019c5a5e4606d88"
   license "MIT"
 
   bottle do
