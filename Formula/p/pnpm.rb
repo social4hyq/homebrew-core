ec1ab312e547110d52e5b4a5827b43c1e712256e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 07:28:53 +0800

    pnpm 11.15.1

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -1,8 +1,8 @@
 class Pnpm < Formula
   desc "Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-11.13.0.tgz"
-  sha256 "865c76bd9111a45ca41f6eeed4067ff0ecdfee9eac83aad24179c83ffebe7599"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-11.15.1.tgz"
+  sha256 "27460629b10111604e7f98882753b53398986820c20e0a065f3a4a5e9e7db71f"
   license "MIT"
   compatibility_version 1
 
