3a336c39fd813d327e68c9a8ae05ef8e4ed1f77f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 06:03:35 +0800

    aliae 1.5.0

--- Formula/a/aliae.rb
+++ Formula/a/aliae.rb
@@ -1,8 +1,8 @@
 class Aliae < Formula
   desc "Cross shell and platform alias management"
   homepage "https://aliae.dev"
-  url "https://github.com/jandedobbeleer/aliae/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "18cca0f0bf38479d39c51a9f0ac8a73b4a1c903e03e09c7d73d262ee6905f082"
+  url "https://github.com/jandedobbeleer/aliae/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "3d20ffd39244e5d630b83cc6d0702276571151e94d03bfbf28b2f86e9aa25ab1"
   license "MIT"
   head "https://github.com/jandedobbeleer/aliae.git", branch: "main"
 
