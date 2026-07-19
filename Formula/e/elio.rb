b610994da62bf5444f4e7347e21e8adae33aee8c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 06:33:42 +0800

    elio 1.11.1

--- Formula/e/elio.rb
+++ Formula/e/elio.rb
@@ -1,8 +1,8 @@
 class Elio < Formula
   desc "Batteries-included terminal file manager with rich previews"
   homepage "https://github.com/elio-fm/elio"
-  url "https://github.com/elio-fm/elio/archive/refs/tags/v1.10.0.tar.gz"
-  sha256 "332ea7a7437f820ab732842f1674c9b8436e0b7c2a0dbece0a52155843a8001a"
+  url "https://github.com/elio-fm/elio/archive/refs/tags/v1.11.1.tar.gz"
+  sha256 "e99d3b8403a3aa77eda5ab75d88f47a5b39468a82df530059af73485c149b7ca"
   license "MIT"
 
   bottle do
