3137424387bc3a2f9893586951771b7c19133363
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 15:32:50 +0800

    libmatroska: update 1.7.2 bottle.

--- Formula/lib/libmatroska.rb
+++ Formula/lib/libmatroska.rb
@@ -12,12 +12,7 @@ class Libmatroska < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "551207828f6f2d1dffc9a8a66263a58370081abbaff0ca238510fd6806ddc023"
-    sha256 cellar: :any, arm64_sequoia: "f56ce932de982e4bba15a674e8fda795e1e17299aaa230e7a34b9fd6b371ea48"
-    sha256 cellar: :any, arm64_sonoma:  "c4933225e8d06c3d3f519947e1f97cb43f61ea3fd456c4f8c61d0c7cc300f482"
-    sha256 cellar: :any, sonoma:        "d54eb3f5430a538cbb46f3a407a5c8f89bad60f45d2808c16dc974e6ab197e8d"
-    sha256 cellar: :any, arm64_linux:   "b7454bda42e2d47e62d5c15e2c3671a6a3e10f7b867f0ead55b72bb83856f4e7"
-    sha256 cellar: :any, x86_64_linux:  "020d7c996af2f93fa9ecc73cf6a20a038fc8138f71feffe1760ba74670ce0efa"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "939d3a57ed549ce8ce07cb6621ae4a0d09c924b975f25efad3c62a3e9329ec31"
   end
 
   depends_on "cmake" => :build
