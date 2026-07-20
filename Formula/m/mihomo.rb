62d95834fbc1647e5585ffc1422b50586f149a39
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 11:07:46 +0800

    mihomo: update 1.19.29 bottle.

--- Formula/m/mihomo.rb
+++ Formula/m/mihomo.rb
@@ -7,7 +7,7 @@ class Mihomo < Formula
   head "https://github.com/MetaCubeX/mihomo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "dc472fc707bee5d69cb09a657b32e2d5465b943413a399d0f9c65cccf0e3dae7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "08f6215780659c74402b26cd8be0dda46b4970f1620daac2ceefcb14db6f67df"
   end
 
   depends_on "go" => :build
