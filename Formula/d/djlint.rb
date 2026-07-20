5ce183c9566679c1dfee238448e9dc4ce3c96dee
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 11:44:42 +0800

    djlint: update 1.40.8 bottle.

--- Formula/d/djlint.rb
+++ Formula/d/djlint.rb
@@ -9,7 +9,7 @@ class Djlint < Formula
   head "https://github.com/djlint/djLint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4cb8d2dd50c6c6dd5165f3935498f2c265838136557e5366b06d3166df449a7c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "23ba0ced2dc255cf24a72bd652452ebac6619d0c1e9d41325a0c2f9d462b1c13"
   end
 
   depends_on "libyaml"
