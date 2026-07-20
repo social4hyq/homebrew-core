79398778aa5cabaf2c89fde0ea271ea12c03e373
hqzing <hqzing@outlook.com>
2026-07-20 07:16:55 +0000

    rdap 0.10.1

--- Formula/r/rdap.rb
+++ Formula/r/rdap.rb
@@ -1,13 +1,18 @@
 class Rdap < Formula
   desc "Command-line client for the Registration Data Access Protocol"
   homepage "https://www.openrdap.org"
-  url "https://github.com/openrdap/rdap/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "19a6b1fe6c3335fa8bb48fb4c33ce56082e0ffdd24dd649745793613ab6c85cb"
+  url "https://github.com/openrdap/rdap/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "e2a41901fb1497412e0391338af5b7673fac24127fe5080c0e60c8bb5cae961e"
   license "MIT"
   head "https://github.com/openrdap/rdap.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "581e5aa6dce6a1896056925b22d2355cc0023de82478f37eef3ec6d869dab824"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "838b7368aa323d4403817a7a0ce1b2004cf9b570f6f49ba7cdf11bae5b676d65"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "838b7368aa323d4403817a7a0ce1b2004cf9b570f6f49ba7cdf11bae5b676d65"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "838b7368aa323d4403817a7a0ce1b2004cf9b570f6f49ba7cdf11bae5b676d65"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8c3ebbdfa3cd18f97b1f22fec61750713bf960c1cd6000b1ca14703b6e2ef6c2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f6cd3491a383d171f2bbc3652f6b78d2fb2667d014c4a6762e336d36ebf2aa49"
+    sha256 cellar: :any,                 x86_64_linux:  "76ac14d767a573a2b6afe0315a20b21eb22f1486982c5243bc454277d52f8efc"
   end
 
   depends_on "go" => :build
@@ -17,7 +22,7 @@ class Rdap < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/openrdap/rdap.version=#{version}
+      -X github.com/openrdap/rdap.releaseVersion=#{version}
     ]
     system "go", "build", *std_go_args(ldflags:), "./cmd/rdap"
   end
