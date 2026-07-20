dfb96059d0c8c7059b3705089302213ca3f92fa6
hqzing <hqzing@outlook.com>
2026-07-20 07:13:38 +0000

    libebml 1.4.6

--- Formula/lib/libebml.rb
+++ Formula/lib/libebml.rb
@@ -1,8 +1,8 @@
 class Libebml < Formula
   desc "Sort of a sbinary version of XML"
   homepage "https://www.matroska.org/"
-  url "https://dl.matroska.org/downloads/libebml/libebml-1.4.5.tar.xz"
-  sha256 "4971640b0592da29c2d426f303e137a9b0b3d07e1b81d069c1e56a2f49ab221b"
+  url "https://dl.matroska.org/downloads/libebml/libebml-1.4.6.tar.xz"
+  sha256 "d06cf1d5ad89390389eeb1eb7d50f70b55ac7538b19aeac8859eed3f2a9908dc"
   license "LGPL-2.1-or-later"
   head "https://github.com/Matroska-Org/libebml.git", branch: "master"
 
@@ -12,17 +12,28 @@ class Libebml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6ad20a85751358a0b3be4f4c95f20c317ae4f2b80f2a32e3472619384d4daf49"
+    sha256 cellar: :any, arm64_tahoe:   "bfcc8e198706779177c919876a4b3c026f018bc310b6de2d06ca903a74a2e218"
+    sha256 cellar: :any, arm64_sequoia: "4aaaf7ee5c09ff50b6e76ac806d7b28c22fb463e8a22c62c2bd03e5aca3b7f02"
+    sha256 cellar: :any, arm64_sonoma:  "c57c38560b3faef5a136b5a1769b283c4e4ffaaf5358b43a787c3dc70e496bb4"
+    sha256 cellar: :any, sonoma:        "d5441e7cdbe0349376d9eed8e73f65636db565ebeb8e7977e12e06e5ab3e98e4"
+    sha256 cellar: :any, arm64_linux:   "40007ef0838462c76a47105619dcdd81f69dfa889307e50e1ea131474b27095b"
+    sha256 cellar: :any, x86_64_linux:  "e50729ec58c7bdb08d793b54d4554ad1cbe745950829b125cfb0cbe3d8260fec"
   end
 
   depends_on "cmake" => :build
+  depends_on "utf8cpp" => :build
 
   def install
-    args = %w[
-      -DBUILD_SHARED_LIBS=ON
-    ]
-    # Workaround to build with CMake 4
-    args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+    inreplace "CMakeLists.txt" do |s|
+      # Allow to use newer utf8cpp library
+      s.gsub! "find_package(utf8cpp 3.2.0)", "find_package(utf8cpp)"
+      # https://github.com/Matroska-Org/libebml/issues/344
+      s.gsub! "target_link_libraries(ebml PRIVATE $<BUILD_INTERFACE:utf8cpp>)", ""
+    end
+
+    ENV.append_to_cflags "-I#{formula_opt_include("utf8cpp")}/utf8cpp"
+
+    args = %w[-DBUILD_SHARED_LIBS=ON]
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
