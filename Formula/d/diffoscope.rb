7afbea7f874f4f988fa52a6e114a4fb11e7b0a5d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 11:43:10 +0800

    diffoscope: update 325 bottle.

--- Formula/d/diffoscope.rb
+++ Formula/d/diffoscope.rb
@@ -8,7 +8,7 @@ class Diffoscope < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "708cbe8e11a76c574181b84befe0813484788fba5b0b1ca050982363447ffd77"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "48b05ff7d9dbb614f8d5b9e21b5bddc0b1bfc331ad825e9fe772c4f8fee3ae58"
   end
 
   depends_on "libarchive"
