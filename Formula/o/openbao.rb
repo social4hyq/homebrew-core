a96fba4e1868dd4146e0a6df905e9db2b038b82d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 15:54:27 +0800

    openbao: update 2.6.0 bottle.

--- Formula/o/openbao.rb
+++ Formula/o/openbao.rb
@@ -13,12 +13,7 @@ class Openbao < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5015828c088be74bb5cec6f01eccd021b43fe428143cd70ebd2991e7a98bda92"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59bbc4355955f80b8bcfbd42c9e3bbb19d62f76ba0f2c61835cf1773efb8f7ad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4063d3f58096d46fea7391b8d27ce7c0f8e988e62d44b17903092861cc7f3236"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a684e52f2beea84d3aec07705d3d02866f102aeb361b0f7244dfac1619dc9e4b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "744013ab41538bb2fc10750f803061800e52a23dad6fdf9ef5653258fe65b14b"
-    sha256 cellar: :any,                 x86_64_linux:  "d3b6ca31095a385607543e1914d6421fe96ddc7175348edba10a9f4153982e2b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "45a3a636dc933d4c738249112a84cb45e333b58e809efed1c63369ae0de867b6"
   end
 
   depends_on "go" => :build
