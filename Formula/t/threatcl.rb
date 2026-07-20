c739fc3b3e57c91d063b708a29cd5c79a558e999
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-20 12:06:00 +0800

    threatcl: update 0.6.3 bottle.

--- Formula/t/threatcl.rb
+++ Formula/t/threatcl.rb
@@ -7,7 +7,7 @@ class Threatcl < Formula
   head "https://github.com/threatcl/threatcl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "1f511077168ce3af8ee0cc5014b1da8b035a4b72ca848bbb1bec2f0c5d7aeec0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "25b629a8985c68cca3cdcb539eb3151965b8d8b6e9b81b4b1a927590ebaed572"
   end
 
   depends_on "go" => :build
