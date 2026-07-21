c8df2c641434c62ddb2670f0ff88d1eef8108e00
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-21 10:38:03 +0800

    wasmtime: update 47.0.1 bottle.

--- Formula/w/wasmtime.rb
+++ Formula/w/wasmtime.rb
@@ -16,7 +16,7 @@ class Wasmtime < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "1d81f321351e901647a5b9046fb15353d368981b90a0ecec17b8008457b651d5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d8681918ff5b28181a2a84c7eca203123dfb692e7dee02b8b1ca81933c7e42d9"
   end
 
   depends_on "cmake" => :build
