1958a9bf3cc63844b681fcecbccb2a127eff336d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-21 10:46:46 +0800

    kubevela: update 1.11.0 bottle.

--- Formula/k/kubevela.rb
+++ Formula/k/kubevela.rb
@@ -8,7 +8,7 @@ class Kubevela < Formula
   head "https://github.com/kubevela/kubevela.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "181e62163f0025e8216828263357d7074a484303f2637c8a7e30e6a2929cd743"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d3ac8eb3a2693ddfba887210ffe767fe4dd1b5e87939ef7fb39ec0491d947354"
   end
 
   depends_on "go" => :build
