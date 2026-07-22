3edb7bb2ae290cf5a35f5644772af8246ab084c3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-23 06:50:14 +0800

    hf-mcp-server 0.3.35

--- Formula/h/hf-mcp-server.rb
+++ Formula/h/hf-mcp-server.rb
@@ -1,8 +1,8 @@
 class HfMcpServer < Formula
   desc "MCP Server for Hugging Face"
   homepage "https://github.com/evalstate/hf-mcp-server"
-  url "https://registry.npmjs.org/@llmindset/hf-mcp-server/-/hf-mcp-server-0.3.33.tgz"
-  sha256 "cd2f97daa5a387a69e5266abb61b984fdaaadb9561ddfe2abe78cea0781914d1"
+  url "https://registry.npmjs.org/@llmindset/hf-mcp-server/-/hf-mcp-server-0.3.35.tgz"
+  sha256 "921dd8d46cfbcc0b91e5a9559864bd65ef26f4d5897c4a383d598b8426325319"
   license "MIT"
 
   bottle do
