1e238dc570f00825e4c319389b0fb43f09be88b6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-22 10:30:03 +0800

    pulsarctl: update 4.2.1.8 bottle.

--- Formula/p/pulsarctl.rb
+++ Formula/p/pulsarctl.rb
@@ -19,7 +19,7 @@ class Pulsarctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "bfec6f1966afb3ad542c99cfe9747e9cc6873870abc4a0ee81eed8cd7fb500ec"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "941e6e2ad480afce5028e04a4c8833f1540aa170479d240952de9e194df92eb6"
   end
 
   depends_on "go" => :build
