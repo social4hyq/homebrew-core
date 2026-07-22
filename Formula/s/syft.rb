899578cc0c56922c02847b25fce49ab1507fab5f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-22 09:59:10 +0800

    syft: update 1.49.0 bottle.

--- Formula/s/syft.rb
+++ Formula/s/syft.rb
@@ -7,7 +7,7 @@ class Syft < Formula
   head "https://github.com/anchore/syft.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8d562237adfa3ee56f4268949beb0369d70fb93c2c0096f12255b09fe38bf3e6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bfd9890d3b99b5f018d390ebde5ddaaa6e2ac248e0e047cba495f5e7aa95226a"
   end
 
   depends_on "go" => :build
