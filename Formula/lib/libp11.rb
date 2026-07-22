9b2636284625610c16cf76e09179a1b99f675b33
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-22 18:27:37 +0800

    libp11: update 0.4.19 bottle.

--- Formula/lib/libp11.rb
+++ Formula/lib/libp11.rb
@@ -11,12 +11,7 @@ class Libp11 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "97e58b3f8dbe3ec5427939e2e04dc4a2c8634fc5149e4d4f05b5f17e6430a7c2"
-    sha256 cellar: :any, arm64_sequoia: "028157d1edea451f6a386c5266c18f1792203792c903d1ccfac452c6bd250950"
-    sha256 cellar: :any, arm64_sonoma:  "6a8779d49170b3e092ef1a8156bda249f4aa5a74ed82cdeea9a25bb17564409f"
-    sha256 cellar: :any, sonoma:        "dc621d302ee73159730d28fc7bc12f00de381aa095238748e579890c8fe84701"
-    sha256 cellar: :any, arm64_linux:   "18f39b1aef751fd100d49a07600c2fc3ad0f22bae686206fb0bb4947dd967cea"
-    sha256 cellar: :any, x86_64_linux:  "8fd4ff7067a37019406df915e9b55354f1ad8f1b11bc379ab7dcd9ba34a2c48c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a2db39555147d5bac32bdc2b7146af98d694f88b0e280b6d51e43d07619ed825"
   end
 
   head do
