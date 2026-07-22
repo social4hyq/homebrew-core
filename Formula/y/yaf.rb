30ece5d2980bab6f4676819ea59046f205f97d74
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-22 09:42:41 +0800

    yaf: update 2.19.4 bottle.

--- Formula/y/yaf.rb
+++ Formula/y/yaf.rb
@@ -13,7 +13,7 @@ class Yaf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "13d1128c3237a5a3c3b6414845c0560f1556385f2cff40b8b337dffe0667b4b7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "27fd793a042eee8ef79ddd663ff18cf8170a803880cbd99d497a4eeefb0e759d"
   end
 
   depends_on "pkgconf" => :build
