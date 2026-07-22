5a52ada4754f3800f2313c28d42e634f44df63dc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-23 07:47:02 +0800

    pi-coding-agent 0.81.1

--- Formula/p/pi-coding-agent.rb
+++ Formula/p/pi-coding-agent.rb
@@ -1,8 +1,8 @@
 class PiCodingAgent < Formula
   desc "AI agent toolkit"
   homepage "https://pi.dev/"
-  url "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-0.80.10.tgz"
-  sha256 "9f2771711b8d4ebb8d59e3177026ab417bfc6caf0d4296a58de741b41e4d9c1c"
+  url "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-0.81.1.tgz"
+  sha256 "420113c0282160e6181656fd16cf18742f76bf9040ee3dfb9cb67e3e6ad5641c"
   license "MIT"
 
   bottle do
