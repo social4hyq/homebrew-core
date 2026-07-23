dff58038ea15fce6cbf733ac22b62e34387085cc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-23 08:02:24 +0800

    ryelang 0.2.55

--- Formula/r/ryelang.rb
+++ Formula/r/ryelang.rb
@@ -1,8 +1,8 @@
 class Ryelang < Formula
   desc "Rye is a homoiconic programming language focused on fluid expressions"
   homepage "https://ryelang.org/"
-  url "https://github.com/refaktor/rye/archive/refs/tags/v0.2.51.tar.gz"
-  sha256 "5bcc98fe8abe9a276ca932447954879c5fa40ef134ccb077ec25cfc4015eddee"
+  url "https://github.com/refaktor/rye/archive/refs/tags/v0.2.55.tar.gz"
+  sha256 "f66d813ac67a1822a5060dcc99d31f63f3e7f58c5f9fbb7fb1ca43eaacc461c0"
   license "BSD-3-Clause"
   head "https://github.com/refaktor/rye.git", branch: "main"
 
