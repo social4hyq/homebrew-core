f3cd251eaf832895781c6e94203e979033991379
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-24 06:35:33 +0800

    doctl 1.164.0

--- Formula/d/doctl.rb
+++ Formula/d/doctl.rb
@@ -1,8 +1,8 @@
 class Doctl < Formula
   desc "Command-line tool for DigitalOcean"
   homepage "https://github.com/digitalocean/doctl"
-  url "https://github.com/digitalocean/doctl/archive/refs/tags/v1.163.0.tar.gz"
-  sha256 "8cc7acd78114eea8409f86b11843d1298d9b12cc984c277f594802acbb3c972a"
+  url "https://github.com/digitalocean/doctl/archive/refs/tags/v1.164.0.tar.gz"
+  sha256 "83ad66a22c3cb8193b36d1929fc9c980e3d2e796bbc63c841ade90d35f7cefce"
   license "Apache-2.0"
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
