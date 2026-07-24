e42eea54f05095263987c9b7540dac40840de115
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-24 15:18:42 +0800

    croc: update 10.6.0 bottle.

--- Formula/c/croc.rb
+++ Formula/c/croc.rb
@@ -7,7 +7,7 @@ class Croc < Formula
   head "https://github.com/schollz/croc.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e54a0bc4a2dcaf77be0bb0344f23e3bd1af9913b5f7b4c5b2138095f932be1c2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "88a37f67ef35db700ba6c8edc43e347e1170a1013328f5dacce350ee9f46e621"
   end
 
   depends_on "go" => :build
