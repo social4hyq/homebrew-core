e3719b4e9fb4dd82b6f4ff18b2910ad8b5d87381
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-24 15:05:55 +0800

    dynaconf: update 3.3.3 bottle.

--- Formula/d/dynaconf.rb
+++ Formula/d/dynaconf.rb
@@ -8,7 +8,7 @@ class Dynaconf < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cdbf9c35dcccfa525769d176afc1d776a8e2b760f983a14da04338b8a42dc4f9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "289778c7c4632ff79044e858933d257f14de70ec00edd746bf8cac48d3ec07a3"
   end
 
   depends_on "python@3.14"
