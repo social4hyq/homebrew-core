2d8329a31807449cbecf9d13a8d2cef43e93e88d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-24 17:29:24 +0800

    dolt: update 2.2.2 bottle.

--- Formula/d/dolt.rb
+++ Formula/d/dolt.rb
@@ -13,7 +13,7 @@ class Dolt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c43d107843b9a6a9bc299a0670a7d17654f64f1dc01f35f8dd7356c6f06dd8a8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5cc1a812d2bfc426c15686d88318e85ac8a874cbbfae5dccfba556fb673f59eb"
   end
 
   depends_on "go" => :build
