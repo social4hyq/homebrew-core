b91be2cdd353cec6da018fdd9a685171ca311b3f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-25 07:08:37 +0800

    kokkos 5.2.0

--- Formula/k/kokkos.rb
+++ Formula/k/kokkos.rb
@@ -1,10 +1,9 @@
 class Kokkos < Formula
   desc "C++ Performance Portability Ecosystem for parallel execution and abstraction"
   homepage "https://kokkos.org"
-  url "https://github.com/kokkos/kokkos/releases/download/5.1.1/kokkos-5.1.1.tar.gz"
-  sha256 "8bdbee0f0ac383436743ad8a9e3e928705b34b31a25a92dc5179c52a3aa98519"
+  url "https://github.com/kokkos/kokkos/releases/download/5.2.0/kokkos-5.2.0.tar.gz"
+  sha256 "54993e0682d80b78939bbf260490f8cf31428bb883c0309961369997f15d94df"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/kokkos/kokkos.git", branch: "develop"
 
   livecheck do
