3d64fbe27ffed5bfa3ac8d9ceaf4b90fea428fea
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-25 06:51:20 +0800

    fresh-editor 0.4.5

--- Formula/f/fresh-editor.rb
+++ Formula/f/fresh-editor.rb
@@ -1,8 +1,8 @@
 class FreshEditor < Formula
   desc "Text editor for your terminal: easy, powerful and fast"
   homepage "https://sinelaw.github.io/fresh/"
-  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "2573f69ae949074b7b1debf286c5890935e9d5db2eec855f997715bbd94ac8fd"
+  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.4.5.tar.gz"
+  sha256 "6dbfebeda46b10b6c2b3b83923b80640c2d5f332072f97a2d2d50b5c10f0d796"
   license "GPL-2.0-or-later"
 
   bottle do
