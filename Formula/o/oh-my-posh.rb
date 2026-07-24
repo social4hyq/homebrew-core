d10deca052d8da804c52b1ac4c0253bcb884db96
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-25 07:55:22 +0800

    oh-my-posh 29.36.0

--- Formula/o/oh-my-posh.rb
+++ Formula/o/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/refs/tags/v29.35.2.tar.gz"
-  sha256 "1080f0126d07c99b28ce026b7838ef584e83f3f1ecfbf571d50c53d84a2803d3"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/refs/tags/v29.36.0.tar.gz"
+  sha256 "24c26c1819a4ee0a914bc5db1b42c86da569dc1cfa44c2bb5eb247870e47662c"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
