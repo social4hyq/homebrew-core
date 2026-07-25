31a79b178d1f20f20643f73bd5ae541ab639a393
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-25 23:33:05 +0800

    prqlc: update 0.13.14 bottle.

--- Formula/p/prqlc.rb
+++ Formula/p/prqlc.rb
@@ -7,7 +7,7 @@ class Prqlc < Formula
   head "https://github.com/prql/prql.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "903681e02bc86e90ca55d9819e56a2ed1a61514e9b547b1a140e373c83106fdc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "333ec70664825473f887ad54b1b02a60300da0db4119e034b0248ee414101a0a"
   end
 
   depends_on "rust" => :build
