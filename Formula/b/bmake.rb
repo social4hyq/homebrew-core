57e621bb16236831ea4a475bc676044e79d9ba2b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-26 06:13:36 +0800

    bmake 20260714

--- Formula/b/bmake.rb
+++ Formula/b/bmake.rb
@@ -1,8 +1,8 @@
 class Bmake < Formula
   desc "Portable version of NetBSD make(1)"
   homepage "https://www.crufty.net/help/sjg/bmake.html"
-  url "https://www.crufty.net/ftp/pub/sjg/bmake-20260704.tar.gz"
-  sha256 "1ad2620cb58addcfbd25d80d21bad9139e3a672b8f08c5f7e57f84119ecef3e3"
+  url "https://www.crufty.net/ftp/pub/sjg/bmake-20260714.tar.gz"
+  sha256 "0810781d24ad6efe010a8ce91c5c529dc8dd95a561d6c93b30e56b8d679cce65"
   license "BSD-3-Clause"
 
   livecheck do
