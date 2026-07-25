b7d2a7e377c15ab6a79939f4ade9add4ded7d900
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-25 23:41:42 +0800

    luajit-openresty: update 2.1-20260724 bottle.

--- Formula/l/luajit-openresty.rb
+++ Formula/l/luajit-openresty.rb
@@ -19,7 +19,7 @@ class LuajitOpenresty < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cd43cb598ce3eab5cae6cec34bdf1a2bdafe150dc2191083ae0fe9dea1692979"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cdd1c4781642b76d7e9a8bf51c90d624a714cddf703bca2cffb75c43a3c48d86"
   end
 
   keg_only "it conflicts with the LuaJIT formula"
