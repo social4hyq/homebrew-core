6c7039658686b6a241ba71a614986d4f5a801e02
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-26 00:01:44 +0800

    e2b: update 2.15.0 bottle.

--- Formula/e/e2b.rb
+++ Formula/e/e2b.rb
@@ -6,7 +6,7 @@ class E2b < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7b6ec2f3587d88593401a590c5ed197a8f82378e0cf35a42c9088f27e052d9fe"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6eabc8708d45a500a240a92dd176aad86d615f295fb402e8dce589c0eb19645c"
   end
 
   depends_on "node"
