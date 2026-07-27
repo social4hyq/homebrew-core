4f65839dde833d49d0b8e594b7fc888a13c7f2ad
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-27 09:43:44 +0800

    vnu: update 26.7.25 bottle.

--- Formula/v/vnu.rb
+++ Formula/v/vnu.rb
@@ -7,7 +7,7 @@ class Vnu < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cdb0c0b66816d3ed14f7b4ae57add6176cc1ab7da5f22f3176511194cb58701a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fb8d420ae15eb0978d721c01f351d20fca4d97e171d9b2781149f29a578fc206"
   end
 
   depends_on "openjdk"
