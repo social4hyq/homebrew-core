6f3d8e5c3a000bd85d39899da713f76aa1519fab
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-27 10:04:34 +0800

    libngtcp2: update 1.25.0 bottle.

--- Formula/lib/libngtcp2.rb
+++ Formula/lib/libngtcp2.rb
@@ -9,7 +9,7 @@ class Libngtcp2 < Formula
   head "https://github.com/ngtcp2/ngtcp2.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "909a8d60d311dcf0aa903632061cc98a67fded1d6f87629e218bce37dcc3021e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "429d6201e8da08fdbb4b6638289b1d86d314bd5b41e00e61f5491d4aeb8ffe14"
   end
 
   depends_on "pkgconf" => :build
