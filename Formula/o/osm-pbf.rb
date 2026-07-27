3530ad0d384a5182fe53ea7a95e9bdac437a94c0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-27 11:38:15 +0800

    osm-pbf: update 1.7.0 bottle.

--- Formula/o/osm-pbf.rb
+++ Formula/o/osm-pbf.rb
@@ -6,12 +6,7 @@ class OsmPbf < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "a9e4e9c59bd0e2e72e88e81cc9af4b1c3837b63cc7b5c423f0b862ff5261133d"
-    sha256 cellar: :any, arm64_sequoia: "fdc2726dbd213efb1235e96795e674b0c8c3ef1921fed19c9bfc54e03f63eabd"
-    sha256 cellar: :any, arm64_sonoma:  "dfd0ea7de5d8ea4e777425cdedbe18f7f0cb40bafcaef9991fa8451746a190ce"
-    sha256 cellar: :any, sonoma:        "1fcbcb1e505ccefbb6bb046b424bc2138074e2a42f0003fe869ed33768690251"
-    sha256               arm64_linux:   "995d16ea53c6fae586db82ce9c22962f1b54f6594f2b2f6d2cb24afcbbadbba8"
-    sha256               x86_64_linux:  "345f01e0bcc784a7cf96143030b3cf2b3fb7c629ab6080a0d75c3409034de245"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "711f97ca5644b5e30392474ea267ac92e29f5c254b86810e6f05ca0d401f1f82"
   end
 
   depends_on "cmake" => :build
