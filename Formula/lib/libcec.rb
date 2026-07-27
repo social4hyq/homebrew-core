acb110ca7348908f958f9d5474565329abb69244
hqzing <hqzing@outlook.com>
2026-07-27 03:32:18 +0000

    libcec 8.1.0

--- Formula/lib/libcec.rb
+++ Formula/lib/libcec.rb
@@ -1,51 +1,25 @@
 class Libcec < Formula
   desc "Control devices with TV remote control and HDMI cabling"
   homepage "https://libcec.pulse-eight.com/"
-  url "https://github.com/Pulse-Eight/libcec/archive/refs/tags/libcec-7.1.1.tar.gz"
-  sha256 "7f7da95a4c1e7160d42ca37a3ac80cf6f389b317e14816949e0fa5e2edf4cc64"
+  url "https://github.com/Pulse-Eight/libcec/archive/refs/tags/libcec-8.1.0.tar.gz"
+  sha256 "11fef68e5a77adf30519096c333d4cdb3f4cfd1d9e0a6d679636899c5807f585"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "da6a7cf9e1046de56d397877c61e86d6d8e84a46d655d065fcf9546b643e1920"
+    sha256 cellar: :any, arm64_tahoe:   "8df50d2b4ae218661eb297d51dd67494dcbab5f0c79ff0c628ca27615bed2a35"
+    sha256 cellar: :any, arm64_sequoia: "0fe31286e1ee21ad365817c96f52abdfc8b10ab94d4725fc55d194f0205982fd"
+    sha256 cellar: :any, arm64_sonoma:  "38e3ff08f1cc8532e1b1d8d9118fcf67b8709cc09f666f7902842468a0436660"
+    sha256 cellar: :any, sonoma:        "49601b83a4e2b63eece3c49b643d0cf54c7f1b16ca95f45c7bba5ccce6994269"
+    sha256 cellar: :any, arm64_linux:   "e29af667948ce5aeaa077f3176a305df56996c64c87ac843abf2042665b35b33"
+    sha256 cellar: :any, x86_64_linux:  "c4df4c9c35a1a88627ed35be53fd3ba6dd3c813491d5205b744ae43c1438d909"
   end
 
   depends_on "cmake" => :build
 
   uses_from_macos "ncurses"
 
-  resource "p8-platform" do
-    url "https://github.com/Pulse-Eight/platform/archive/refs/tags/p8-platform-2.1.0.1.tar.gz"
-    sha256 "064f8d2c358895c7e0bea9ae956f8d46f3f057772cb97f2743a11d478a0f68a0"
-
-    livecheck do
-      url "https://github.com/Pulse-Eight/platform.git"
-      regex(/^p8-platform[._-]v?(\d+(?:\.\d+)+)$/i)
-    end
-  end
-
   def install
-    ENV.cxx11
-
-    # The CMake scripts don't work well with some common LIBDIR values:
-    # - `CMAKE_INSTALL_LIBDIR=lib` is interpreted as path relative to build dir
-    # - `CMAKE_INSTALL_LIBDIR=#{lib}` breaks pkg-config and cmake config files
-    # - Setting no value uses UseMultiArch.cmake to set platform-specific paths
-    # To avoid these issues, we can specify the type of input as STRING
-    cmake_args = std_cmake_args.map do |s|
-      s.gsub "-DCMAKE_INSTALL_LIBDIR=", "-DCMAKE_INSTALL_LIBDIR:STRING="
-    end
-
-    resource("p8-platform").stage do
-      # upstream commit, https://github.com/Pulse-Eight/platform/commit/d7faed1c696b1a6a67f114a63a0f4c085f0f9195
-      ENV["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
-      odie "remove CMAKE_POLICY_VERSION_MINIMUM env" if resource("p8-platform").version > "2.1.0.1"
-
-      system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *cmake_args
-      system "cmake", "--build", "build"
-      system "cmake", "--install", "build"
-    end
-
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
