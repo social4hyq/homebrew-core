e3406ea6da8cfbda972239e93425c0714cf566d1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-28 10:33:29 +0800

    dockcheck: update 0.8.2 bottle.

--- Formula/d/dockcheck.rb
+++ Formula/d/dockcheck.rb
@@ -7,7 +7,7 @@ class Dockcheck < Formula
   head "https://github.com/mag37/dockcheck.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5ffc694f53905225f2d08fcde2d7d1f33e206efa34fbcbda1767fb1495c4e6f0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d4e6da29f575436a71119528e7f464fc8127bceb69f06cea50c6641ff61e796a"
   end
 
   depends_on "regclient"
