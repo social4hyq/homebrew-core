5346e8884ffaaf78a3f17c880030753c4f5493a4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-29 06:54:33 +0800

    ldcli 3.6.0

--- Formula/l/ldcli.rb
+++ Formula/l/ldcli.rb
@@ -1,8 +1,8 @@
 class Ldcli < Formula
   desc "CLI for managing LaunchDarkly feature flags"
   homepage "https://launchdarkly.com/docs/home/getting-started/ldcli"
-  url "https://github.com/launchdarkly/ldcli/archive/refs/tags/v3.3.0.tar.gz"
-  sha256 "cb4619bb8377d3b873a30681d01a3fec590c847dc359661b8ef9c3c98155be8e"
+  url "https://github.com/launchdarkly/ldcli/archive/refs/tags/v3.6.0.tar.gz"
+  sha256 "f16b53912eb32d94164032d391a52ea3f847334df25985121d6a53162d4c6a0f"
   license "Apache-2.0"
   head "https://github.com/launchdarkly/ldcli.git", branch: "main"
 
