dcc2520f884b720a037b9f1cbbb7d87b6f6d29ef
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-28 10:07:10 +0800

    melange: update 0.56.4 bottle.

--- Formula/m/melange.rb
+++ Formula/m/melange.rb
@@ -12,7 +12,7 @@ class Melange < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "06d392f1f4df42b4a4e3d98a27cd8046abc466a69ce23c5fa01121ebc6909c89"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "401fdc3d1c66431f10956f879e79a9f66d42aded599d6046747afdd6f4d39436"
   end
 
   depends_on "go" => :build
