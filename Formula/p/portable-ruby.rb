dda8c80fd42daa53a6576c1d60794f2890c28b9d
hqzing <hqzing@outlook.com>
2026-07-29 17:15:19 +0800

    portable-ruby: remove specific 'install' absolute path

--- Formula/p/portable-ruby.rb
+++ Formula/p/portable-ruby.rb
@@ -6,6 +6,7 @@ class PortableRuby < PortableFormula
   url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.5.tar.gz"
   sha256 "7d6149079a63f8ae1d326c9fa65c6019ba2dc3155eae7b39159817911c88958e"
   license "Ruby"
+  revision 1
 
   # This regex restricts matching to versions other than X.Y.0.
   livecheck do
@@ -174,6 +175,7 @@ class PortableRuby < PortableFormula
       inreplace lib/"ruby/#{abi_version}/#{abi_arch}/rbconfig.rb" do |s|
         s.gsub! ENV.cxx, "c++"
         s.gsub! ENV.cc, "cc"
+        s.gsub! "/usr/bin/install", "install"
       end
 
       # Ship libcrypt.a so that building native gems doesn't need system libcrypt installed.
