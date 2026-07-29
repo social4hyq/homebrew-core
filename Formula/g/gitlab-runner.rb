ef02d3b49b982234ddd8198ec66ffffe145b6d84
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-29 16:29:33 +0800

    gitlab-runner: update 19.2.1 bottle.

--- Formula/g/gitlab-runner.rb
+++ Formula/g/gitlab-runner.rb
@@ -13,7 +13,7 @@ class GitlabRunner < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "89ad3e530888c0fc9d4bdf20ccfda176a1333ad7c1c362da12b8354754199885"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c3c63292a655e1d5e9f54ce9fb8acf02452f035ae0d4e900f313216402529280"
   end
 
   depends_on "go" => :build
