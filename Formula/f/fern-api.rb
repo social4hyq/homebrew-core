0eca4b44da5b6b2712e4bfa567c2f34eb24ee668
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-29 15:34:55 +0800

    fern-api: update 5.82.0 bottle.

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -10,7 +10,7 @@ class FernApi < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6f317338bfc42ffbd66f81dd2748327f86106254cd89cb7bcc3c57015cc8fc4b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "627fb66be6409d59419cee743e862c9053368339b6fe2030302c6fa435d5a001"
   end
 
   depends_on "node"
