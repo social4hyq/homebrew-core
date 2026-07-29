6095e90c4410df840696dbfc31451564df38c2d1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-29 16:08:17 +0800

    oh-my-agent: update 11.1.1 bottle.

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -6,7 +6,7 @@ class OhMyAgent < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8430585bbf0abf5792aa96045035ca892d1f38e1c0f162e7886bcc584fb99824"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "137e977d173326cc37166a234dd82f47faad7376a9908d7aeedf7bc39579f835"
   end
 
   depends_on "node"
