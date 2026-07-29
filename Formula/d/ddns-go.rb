5591f09f44e8c813904a0e56cb322a5c150f1bbb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-30 06:29:49 +0800

    ddns-go 6.17.3

--- Formula/d/ddns-go.rb
+++ Formula/d/ddns-go.rb
@@ -1,8 +1,8 @@
 class DdnsGo < Formula
   desc "Simple and easy-to-use DDNS"
   homepage "https://github.com/jeessy2/ddns-go"
-  url "https://github.com/jeessy2/ddns-go/archive/refs/tags/v6.17.2.tar.gz"
-  sha256 "d7a8b098797171e715a20a1581b3f44c5c3a8514d93fe64e52ccde92f129bca3"
+  url "https://github.com/jeessy2/ddns-go/archive/refs/tags/v6.17.3.tar.gz"
+  sha256 "8d12594cd7a8b77effb068181b1a6d0a378e6321b5e4c3ec438e415de5fe4039"
   license "MIT"
   head "https://github.com/jeessy2/ddns-go.git", branch: "master"
 
