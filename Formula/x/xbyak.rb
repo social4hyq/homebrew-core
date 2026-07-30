d470c57c5bfcf7c722ab2bc34cd591d88fa64f8c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-30 14:55:45 +0800

    xbyak: update 7.37.6 bottle.

--- Formula/x/xbyak.rb
+++ Formula/x/xbyak.rb
@@ -12,7 +12,7 @@ class Xbyak < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6e669548457c6fe47c1e9cc8b83ca4648673f54be5cdfa955685452150f56207"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b9971ab0acec0d62c1845668e4a08025dbedf28699bbce999df44cc6f49bb94f"
   end
 
   depends_on "cmake" => :build
