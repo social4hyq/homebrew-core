47a5badef029b974a15ee93fd19f10642c9bf421
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-07-30 15:03:15 +0800

    vet: update 1.18.0 bottle.

--- Formula/v/vet.rb
+++ Formula/v/vet.rb
@@ -6,7 +6,7 @@ class Vet < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "38dcc8461707c749e2595ca3e03ffe48fd2e241b66dcdbbbf04968b4e1100040"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d04655a1ec3d60d29be86881a49425505cc9729ebc5455cbafba4ed5c0ee3893"
   end
 
   depends_on "go"
