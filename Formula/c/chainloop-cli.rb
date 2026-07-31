9d157d2b5971461ea44aab840b690dda6bdc3459
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-01 06:22:49 +0800

    chainloop-cli 1.105.2

--- Formula/c/chainloop-cli.rb
+++ Formula/c/chainloop-cli.rb
@@ -1,8 +1,8 @@
 class ChainloopCli < Formula
   desc "CLI for interacting with Chainloop"
   homepage "https://docs.chainloop.dev"
-  url "https://github.com/chainloop-dev/chainloop/archive/refs/tags/v1.105.0.tar.gz"
-  sha256 "5ec15478d6de582aa8daa3df303e20cf57d638acd0059e63b764ae856f977f09"
+  url "https://github.com/chainloop-dev/chainloop/archive/refs/tags/v1.105.2.tar.gz"
+  sha256 "7e2bfb5d0cdf00a4f625e5e07ce25fbd4fee43e2434037e590799bfb9e99c41f"
   license "Apache-2.0"
   head "https://github.com/chainloop-dev/chainloop.git", branch: "main"
 
