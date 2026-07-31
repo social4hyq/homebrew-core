c8628d7fd9e38d8f2b8b12350e65bf4f2d1ddcb2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-01 06:40:42 +0800

    fabric-ai 1.4.467

--- Formula/f/fabric-ai.rb
+++ Formula/f/fabric-ai.rb
@@ -1,8 +1,8 @@
 class FabricAi < Formula
   desc "Open-source framework for augmenting humans using AI"
   homepage "https://github.com/danielmiessler/fabric"
-  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.464.tar.gz"
-  sha256 "7323b73db863a446768df2c0067d05c9011b8eabddfe7999c7af8565b61fb578"
+  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.467.tar.gz"
+  sha256 "f25b93c4d9b6cd612806b91d64fd0c0c764edfedbea3e14e280aea8d15a1a38b"
   license "MIT"
   head "https://github.com/danielmiessler/fabric.git", branch: "main"
 
