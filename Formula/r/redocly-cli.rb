b0d88b57f4cab378993d9fabad9bb6b3a3a97d4b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-02 07:46:00 +0800

    redocly-cli 2.43.2

--- Formula/r/redocly-cli.rb
+++ Formula/r/redocly-cli.rb
@@ -1,8 +1,8 @@
 class RedoclyCli < Formula
   desc "Your all-in-one OpenAPI utility"
   homepage "https://redocly.com/docs/cli"
-  url "https://registry.npmjs.org/@redocly/cli/-/cli-2.41.2.tgz"
-  sha256 "1945fb2f17e90b19d2e22e2c83d9aad96814514092a2b3076334da8766b3084f"
+  url "https://registry.npmjs.org/@redocly/cli/-/cli-2.43.2.tgz"
+  sha256 "0616037c5f8c6af71d2632edce6d45cf04d923c274e2d6767ed5d852988224e5"
   license "MIT"
 
   bottle do
