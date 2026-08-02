6c0203f384b3e87e03871d138bcb08fa92a5c315
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 07:29:16 +0800

    nanomsg 1.2.4

--- Formula/n/nanomsg.rb
+++ Formula/n/nanomsg.rb
@@ -1,8 +1,8 @@
 class Nanomsg < Formula
   desc "Socket library in C"
   homepage "https://nanomsg.org/"
-  url "https://github.com/nanomsg/nanomsg/archive/refs/tags/1.2.2.tar.gz"
-  sha256 "3ffeafa9335245a23a99827e8d389bfce5100610f44ebbe4bfaf47e8192d5939"
+  url "https://github.com/nanomsg/nanomsg/archive/refs/tags/1.2.4.tar.gz"
+  sha256 "be255a26452400a6ff79039e1c76592694bc602e7b1e0c40a64b841ba0e434ed"
   license "MIT"
   head "https://github.com/nanomsg/nanomsg.git", branch: "master"
 
