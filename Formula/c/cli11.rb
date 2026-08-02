0e0dfc264e302c7b693e357afc885087b6e38be5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 06:21:11 +0800

    cli11 2.7.2

--- Formula/c/cli11.rb
+++ Formula/c/cli11.rb
@@ -1,8 +1,8 @@
 class Cli11 < Formula
   desc "Simple and intuitive command-line parser for C++11"
   homepage "https://cliutils.github.io/CLI11/book/"
-  url "https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.6.2.tar.gz"
-  sha256 "c6ea6b2e5608b3ea8617999bd5f47420c71b2ebdb8dc4767c1034d1da5785711"
+  url "https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.7.2.tar.gz"
+  sha256 "46eef3101da70852ec7af026e09d485ccee81813331c8c6052d39344443b83da"
   license "BSD-3-Clause"
   head "https://github.com/CLIUtils/CLI11.git", branch: "main"
 
