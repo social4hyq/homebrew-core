221222672313ddd0e5f49f8e0156a775acf7c551
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 06:04:44 +0800

    aqua 2.62.3

--- Formula/a/aqua.rb
+++ Formula/a/aqua.rb
@@ -1,8 +1,8 @@
 class Aqua < Formula
   desc "Declarative CLI Version manager"
   homepage "https://aquaproj.github.io/"
-  url "https://github.com/aquaproj/aqua/archive/refs/tags/v2.62.1.tar.gz"
-  sha256 "717a397c7d075424cbdb5256234514f5677683afa153610428f241138a363741"
+  url "https://github.com/aquaproj/aqua/archive/refs/tags/v2.62.3.tar.gz"
+  sha256 "fceb5a55a9e8dc7996ed8b6cafbb463997c82ba44e07c39ffc5f0b8fa3f67417"
   license "MIT"
   head "https://github.com/aquaproj/aqua.git", branch: "main"
 
