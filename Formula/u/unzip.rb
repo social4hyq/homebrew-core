cc65fd05e53060fc8213fec9cb9e79f2e9cc449e
lanfungcing <lanfq.26@qq.com>
2026-08-03 08:09:12 +0000

    unzip: rebuild

--- Formula/u/unzip.rb
+++ Formula/u/unzip.rb
@@ -12,11 +12,6 @@ class Unzip < Formula
     regex(%r{url=.*?(?:%20)?v?(\d+(?:\.\d+)+)/unzip\d+\.t}i)
   end
 
-  bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "898c2b94075386c797bf047636e84903f14325ac77801c6552571e3213ab3dff"
-  end
-
   keg_only :provided_by_macos
 
   uses_from_macos "zip" => :test
