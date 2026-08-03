b23c2b1df15fb014f1d19b4f769dd4140eb35c56
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 17:46:33 +0800

    harfbuzz: update 14.3.0 bottle.

--- Formula/h/harfbuzz.rb
+++ Formula/h/harfbuzz.rb
@@ -8,7 +8,7 @@ class Harfbuzz < Formula
   head "https://github.com/harfbuzz/harfbuzz.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b894cabdb75debf6fd9b2cc7756e118b4e20a8cefdfd1d793d5e7a033efb63ae"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7890889b2f840c8208e4d1c21424d331c5fa3cb55859f36150e1db09aa54e0a3"
   end
 
   depends_on "gobject-introspection" => :build
