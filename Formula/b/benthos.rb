b293db0b8d265c761493b1f656d6e4107ba2787e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-04 00:51:53 +0800

    benthos: update 4.77.0 bottle.

--- Formula/b/benthos.rb
+++ Formula/b/benthos.rb
@@ -7,7 +7,7 @@ class Benthos < Formula
   head "https://github.com/redpanda-data/benthos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4fd2b3d4d7698f115504b4f232c5fa07d875b73f0909fbb31e993b7a65ecb900"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "805dd2a65e9584755c510bed5726e42788ae70f55f5cf8445cd48deaf3ffddde"
   end
 
   depends_on "go" => :build
