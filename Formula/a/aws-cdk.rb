827f92b292dca2d1f62a150c5ac10523a6b6aa05
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 18:21:02 +0800

    aws-cdk: update 2.1134.0 bottle.

--- Formula/a/aws-cdk.rb
+++ Formula/a/aws-cdk.rb
@@ -6,7 +6,7 @@ class AwsCdk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "60387ba0b5a928e56303affa7cec9de447be1113ff826420c8086a388d4202b9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3b550c434fd73879925feb7172a11d4b4b644c687f4992ce0bf913a183aded80"
   end
 
   depends_on "node"
