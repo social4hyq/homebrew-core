70a7fc8bf3703b7c10ac80e100de9985195dde67
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 17:51:53 +0800

    flyway: update 13.1.0 bottle.

--- Formula/f/flyway.rb
+++ Formula/f/flyway.rb
@@ -6,7 +6,7 @@ class Flyway < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ac1f4865e1c0d5e32b5600043a0f61112d67a60d92a3ac7f835ed8469ab3f34c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2c223536097b7aa1c5b43af8cca4db9df519eb4508fadc41a94155db87b8de66"
   end
 
   depends_on "openjdk"
