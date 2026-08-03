cf6c78aa8f336b9154d40cb629ee196ee734d49a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 14:19:31 +0800

    rclone: update 1.75.0 bottle.

--- Formula/r/rclone.rb
+++ Formula/r/rclone.rb
@@ -8,7 +8,7 @@ class Rclone < Formula
   head "https://github.com/rclone/rclone.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6ecda7a0ea090ad564c98b6d1ec2280dcf06538547736d7827b96a06321f198d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "88b28fee3ad53136c0a1b2bd37c73cc4fd4528ce90ebabc32e2ad6095725924a"
   end
 
   depends_on "go" => :build
