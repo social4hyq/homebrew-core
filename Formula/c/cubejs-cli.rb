7c89dd3fae3c870064844a4ca133ae8d4d39e59c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 11:39:33 +0800

    cubejs-cli: update 1.7.16 bottle.

--- Formula/c/cubejs-cli.rb
+++ Formula/c/cubejs-cli.rb
@@ -6,7 +6,7 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "801fbf1b2c65eaa355420922286103564e010e30d88482b0da1111bf1c76db3b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c0033a18038b8a1f4be663f3d84d4663fc1bfbf18bb0d5095b6b26e59e971cfa"
   end
 
   depends_on "node"
