9f143868b98c884ddfde9f478a148785e7f6a1ed
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-03 18:05:09 +0800

    crane: update 0.21.8 bottle.

--- Formula/c/crane.rb
+++ Formula/c/crane.rb
@@ -7,7 +7,7 @@ class Crane < Formula
   head "https://github.com/google/go-containerregistry.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "206b8cc0227dbbd7905227b98f49bb76194e24baa937f615d9f7f7558c20f618"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2d27fd80e63d49a13bca2aaeedc5f21f02654b21ebe7cdbd7b6cc5506bb4c4f0"
   end
 
   depends_on "go" => :build
