2c1e1ed870fc3cbbece84b61e0a97dcda97e1add
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-04 12:21:18 +0800

    renovate: update 44.10.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -18,7 +18,7 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c1dfb52088d5005ca38ae82f4cc97fb9140b257a30e35c260b2a98dfe4ab3ef7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8c4b23e7b7be52cdc14757b94787b8104ec8ba9db2a756b7add07257d3c4ad40"
   end
 
   depends_on "node@24"
