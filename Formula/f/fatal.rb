4afe0998ff019ad251263b95467978f28ee361c6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-04 13:28:02 +0800

    fatal: update 2026.08.03.00 bottle.

--- Formula/f/fatal.rb
+++ Formula/f/fatal.rb
@@ -7,7 +7,7 @@ class Fatal < Formula
   head "https://github.com/facebook/fatal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b54f7569fd6939a5b6df9e1e28d8ca81905afd394092f8c1cc8d931e1dbae7f9"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "336f88012956c8db0a919434c470b0c7cbe019b1aef7db7752ff9d4966559d6a"
   end
 
   def install
