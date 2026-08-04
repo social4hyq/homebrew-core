fbea5526d38b846f62ed2be6ebf3aee24e56d89d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-05 06:31:14 +0800

    depot 2.101.78

--- Formula/d/depot.rb
+++ Formula/d/depot.rb
@@ -1,8 +1,8 @@
 class Depot < Formula
   desc "Build your Docker images in the cloud"
   homepage "https://depot.dev/"
-  url "https://github.com/depot/cli/archive/refs/tags/v2.101.76.tar.gz"
-  sha256 "aaa88514ecc449bbb5f6e3541d555d62f1ac5972ff2a9ba3e5d1ace865060edb"
+  url "https://github.com/depot/cli/archive/refs/tags/v2.101.78.tar.gz"
+  sha256 "f54c0e714e30e6149e19f04f1fa8ec8f29add9da6d80c521a20a47009a5e8cd5"
   license "MIT"
   head "https://github.com/depot/cli.git", branch: "main"
 
