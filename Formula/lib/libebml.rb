9cef323f27fdf725ff60d730f8778deb5f12883b
hqzing <hqzing@outlook.com>
2026-08-04 08:10:22 +0000

    libebml 1.4.7

--- Formula/lib/libebml.rb
+++ Formula/lib/libebml.rb
@@ -1,8 +1,8 @@
 class Libebml < Formula
   desc "Sort of a sbinary version of XML"
   homepage "https://www.matroska.org/"
-  url "https://dl.matroska.org/downloads/libebml/libebml-1.4.6.tar.xz"
-  sha256 "d06cf1d5ad89390389eeb1eb7d50f70b55ac7538b19aeac8859eed3f2a9908dc"
+  url "https://dl.matroska.org/downloads/libebml/libebml-1.4.7.tar.xz"
+  sha256 "5b08214f929ee54c6187c370f84235fc9d0f2a2258c4d320d68eae6e2bdfd3f7"
   license "LGPL-2.1-or-later"
   head "https://github.com/Matroska-Org/libebml.git", branch: "master"
 
@@ -12,22 +12,18 @@ class Libebml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a00dfbeb57ee70cf52b870f6c9d63fe56bdc18638b8c2c167c503a05b6923a10"
+    sha256 cellar: :any, arm64_tahoe:   "7d5c5e9913544130b8d5b7eeb1ed4027a62520c07bb1546e43d16707a7386a2d"
+    sha256 cellar: :any, arm64_sequoia: "5b60dc631dd96becbdd1ef9aa17711697feb3cc57c478b8a1248600a2c7fddf3"
+    sha256 cellar: :any, arm64_sonoma:  "03cf729ca8007eea4bbb7130850137a392f15c22e25fa0b1b10dd6483e2ffef2"
+    sha256 cellar: :any, sonoma:        "039207dc37ef7ba4ba3cd16e88a6c306ea503976182d5c3af90bf1790b305354"
+    sha256 cellar: :any, arm64_linux:   "0a75bcd86d089f537a3671b2d528e135896f064134cbf59fb8aedaaddcd526bd"
+    sha256 cellar: :any, x86_64_linux:  "55442b033a637e363bd95d2582320402d886ff644e1dabd70f69161b14d1517a"
   end
 
   depends_on "cmake" => :build
   depends_on "utf8cpp" => :build
 
   def install
-    inreplace "CMakeLists.txt" do |s|
-      # Allow to use newer utf8cpp library
-      s.gsub! "find_package(utf8cpp 3.2.0)", "find_package(utf8cpp)"
-      # https://github.com/Matroska-Org/libebml/issues/344
-      s.gsub! "target_link_libraries(ebml PRIVATE $<BUILD_INTERFACE:utf8cpp>)", ""
-    end
-
-    ENV.append_to_cflags "-I#{formula_opt_include("utf8cpp")}/utf8cpp"
-
     args = %w[-DBUILD_SHARED_LIBS=ON]
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
@@ -48,7 +44,7 @@ class Libebml < Formula
       }
     CPP
 
-    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}", "-L#{lib}", "-lebml"
+    system ENV.cxx, "test.cpp", "-o", "test", "-I#{include}", "-L#{lib}", "-lebml"
     system "./test"
   end
 end
