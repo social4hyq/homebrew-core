b8e7cd6722e0be9e7c9b69bbde556ec629491545
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-04 13:24:32 +0800

    flyctl: update 0.4.78 bottle.

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -18,7 +18,7 @@ class Flyctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f3d6738a092ed6255fa35941512a752fc07e379c62910b9d965392d2bb90db70"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5db991456e55320d894f173dc6be1f7f598727af82fc9ea5fc0762ebac4c9fef"
   end
 
   depends_on "go" => :build
