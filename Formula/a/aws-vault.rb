14ca251a82a11aa919d43d8c71a25edaefb44fe6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-05 16:40:27 +0800

    aws-vault: update 7.13.3 bottle.

--- Formula/a/aws-vault.rb
+++ Formula/a/aws-vault.rb
@@ -12,7 +12,7 @@ class AwsVault < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "0282e7bccff14036940703e9d3439cc8be05781d96251aadb8f3ecd68093e973"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a200a97c4e00f2cd6902edbf933cb808e4db4609d4832033bd1f81c25dc47368"
   end
 
   depends_on "go" => :build
