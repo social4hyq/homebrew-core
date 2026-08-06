5ba0af59ac59c5d4af4ee6959897c48467345a85
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-06 14:16:29 +0800

    ldcli: update 3.9.0 bottle.

--- Formula/l/ldcli.rb
+++ Formula/l/ldcli.rb
@@ -7,7 +7,7 @@ class Ldcli < Formula
   head "https://github.com/launchdarkly/ldcli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6bd8a2c71c7b69b38a34e6519de4ed7d1171e56e7ab017cf9c1c835f3c08c69f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "052b3209526f335f9d773bce12922e23fbe4b61a729c34930c9c576312043475"
   end
 
   depends_on "go" => :build
