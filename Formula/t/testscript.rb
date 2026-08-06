91b5777b9299bfce6336b353035465e4c94af04b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-06 11:02:41 +0800

    testscript: update 1.16.0 bottle.

--- Formula/t/testscript.rb
+++ Formula/t/testscript.rb
@@ -6,7 +6,7 @@ class Testscript < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8cc519b98f8364f93337a7cbbf4063060056d6d555d75ba2e45578da31ed7ae5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bc9b1fd4c6689bdbd55e84ff4d27c0ca98d1e11d55b9edb18a9f8cb660b85885"
   end
 
   depends_on "go" => :build
