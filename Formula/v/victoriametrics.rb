facbe784f75c324a2ed51de9b71a1b737ae701c3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-06 10:57:25 +0800

    victoriametrics: update 1.149.0 bottle.

--- Formula/v/victoriametrics.rb
+++ Formula/v/victoriametrics.rb
@@ -14,7 +14,7 @@ class Victoriametrics < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cb1ea66d5098112eb7177d3b8b3aabb45ca1ab74857bce1e54c4e2b78ae506f3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dfd708d6ac5e0217fd22a7fd2befb6b944d38b87e408f0db37e260ecb648028f"
   end
 
   depends_on "go" => :build
