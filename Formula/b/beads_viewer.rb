d1e72fceff0d74b02684e54cad1dfb6a9cf92da8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-06 08:41:53 +0000

    beads_viewer 0.19.0

--- Formula/b/beads_viewer.rb
+++ Formula/b/beads_viewer.rb
@@ -1,21 +1,23 @@
 class BeadsViewer < Formula
   desc "Terminal-based UI for the Beads issue tracker"
   homepage "https://github.com/Dicklesworthstone/beads_viewer"
-  url "https://github.com/Dicklesworthstone/beads_viewer/archive/refs/tags/v0.18.0.tar.gz"
-  sha256 "c40173354f84df8301121a936b6347c3958430227c53d0ea47718507a104f5da"
+  url "https://github.com/Dicklesworthstone/beads_viewer/archive/refs/tags/v0.19.0.tar.gz"
+  sha256 "7459eeaa99976be78ea0a9f85f1db98e27011772a80ca5ec05d2125e0f86ce0a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b6289a441de61dfc04c1af7aa6e89e60d6fefa431779f92257ba70023785c403"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d3e86242bd9c69febe0e94487ddec9b426a1b5108dd0ac4fe35f5046abe42cd5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3e86242bd9c69febe0e94487ddec9b426a1b5108dd0ac4fe35f5046abe42cd5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3e86242bd9c69febe0e94487ddec9b426a1b5108dd0ac4fe35f5046abe42cd5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "623f96ddcc8b1812979b902098bedb5e5e0117fbedc3d4f6fd7a7b6afe17206e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "19b67cab7f6d9fe324094b6a9bc237752c91231b28431d180a267389d2b872a0"
+    sha256 cellar: :any,                 x86_64_linux:  "db17ff6080b6d7b440efce3811fb3ef6a566b362592bb8b96899928db7cd4b14"
   end
 
   depends_on "go" => :build
 
   def install
-    ldflags = %W[
-      -s -w
-      -X github.com/Dicklesworthstone/beads_viewer/pkg/version.Version=v#{version}
-    ]
+    ldflags = %W[-X github.com/Dicklesworthstone/beads_viewer/pkg/version.version=v#{version}]
     system "go", "build", *std_go_args(ldflags:, output: bin/"bv"), "./cmd/bv"
   end
 
