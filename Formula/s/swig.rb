ccb8b267a68ef60f3da744961efa3151336ec3a1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-06 08:02:06 +0800

    swig 4.5.0

--- Formula/s/swig.rb
+++ Formula/s/swig.rb
@@ -1,8 +1,8 @@
 class Swig < Formula
   desc "Generate scripting interfaces to C/C++ code"
   homepage "https://www.swig.org/"
-  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.4.1/swig-4.4.1.tar.gz"
-  sha256 "40162a706c56f7592d08fd52ef5511cb7ac191f3593cf07306a0a554c6281fcf"
+  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.5.0/swig-4.5.0.tar.gz"
+  sha256 "22ae0e887f8cca8031a325c67d005207653200b40e71edb3f88780e28e47d0ff"
   license "GPL-3.0-or-later"
   compatibility_version 1
 
