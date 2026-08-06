1c90bbaa3b0d95fd5b686e261d7379a2aa3f69d8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-06 11:36:04 +0800

    osctrl-cli: update 0.5.5 bottle.

--- Formula/o/osctrl-cli.rb
+++ Formula/o/osctrl-cli.rb
@@ -7,7 +7,7 @@ class OsctrlCli < Formula
   head "https://github.com/jmpsec/osctrl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9e045323238f03e772324bbe1d8c344366444f1758661bca108a7e97732b6470"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d520f429606982aa603afbcda764537e8bca77bdc871a9c4f5c790847016aa58"
   end
 
   depends_on "go" => :build
