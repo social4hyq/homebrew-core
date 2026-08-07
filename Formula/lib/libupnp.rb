c72a33b726571517dafa21ec756d35d2b6aabec8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-07 20:38:59 +0800

    libupnp: update 22.0.4 bottle.

--- Formula/lib/libupnp.rb
+++ Formula/lib/libupnp.rb
@@ -11,7 +11,7 @@ class Libupnp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "683f79ce777a967461626079976192328f0e240d1181a944ac89ae6b80d45bb8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c4661a856b99e3bf95743fa4fe86edba64e5d43fb4666135e922199c2b588469"
   end
 
   depends_on "cmake" => :build
