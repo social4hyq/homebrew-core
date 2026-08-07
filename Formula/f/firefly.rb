cda4fdee936962830c06522c166d75f103d9bdf6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-07 08:40:36 +0000

    firefly 1.5.0

--- Formula/f/firefly.rb
+++ Formula/f/firefly.rb
@@ -1,10 +1,10 @@
 class Firefly < Formula
   desc "Create and manage the Hyperledger FireFly stack for blockchain interaction"
-  homepage "https://hyperledger.github.io/firefly/latest/"
-  url "https://github.com/hyperledger/firefly-cli/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "05375efa4e849695c60e70ec3e332b7a4c8dbe666f1b76b8de3f12944b85b60c"
+  homepage "https://hyperledger-firefly.github.io/firefly/latest/"
+  url "https://github.com/hyperledger-firefly/cli/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "f9c73ca146af0e9e5ed5ef68d45c5733375f211233e5093ca060c9ddf8587f0b"
   license "Apache-2.0"
-  head "https://github.com/hyperledger/firefly-cli.git", branch: "main"
+  head "https://github.com/hyperledger-firefly/cli.git", branch: "main"
 
   livecheck do
     url :stable
@@ -12,17 +12,21 @@ class Firefly < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a470016308cac24a8227af27ca07fd537f38c196e6a32fcbb2d1cb6b4325df22"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "23c8e358bda21f179ff5b0211f8bb73012c83d5816b021e2790ce46b9dc291c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "23c8e358bda21f179ff5b0211f8bb73012c83d5816b021e2790ce46b9dc291c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "23c8e358bda21f179ff5b0211f8bb73012c83d5816b021e2790ce46b9dc291c3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b98f995fef6046fcc4ed494b3f65f9b08f7b865403dd96b7d6959fe8e71ffb86"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2d1ca0dd7e52c722e28ae10497c666f041da2146f2af4874cd878ab03aaf6840"
+    sha256 cellar: :any,                 x86_64_linux:  "21ba828d9e926050090303fd2aff9d3a503d1f7775e7020b896ae73537b7bb03"
   end
 
   depends_on "go" => :build
 
   def install
     ldflags = %W[
-      -s -w
-      -X github.com/hyperledger/firefly-cli/cmd.BuildDate=#{time.iso8601}
-      -X github.com/hyperledger/firefly-cli/cmd.BuildCommit=#{tap.user}
-      -X github.com/hyperledger/firefly-cli/cmd.BuildVersionOverride=v#{version}
+      -X github.com/hyperledger-firefly/cli/cmd.BuildDate=#{time.iso8601}
+      -X github.com/hyperledger-firefly/cli/cmd.BuildCommit=#{tap.user}
+      -X github.com/hyperledger-firefly/cli/cmd.BuildVersionOverride=v#{version}
     ]
     system "go", "build", *std_go_args(ldflags:), "./ff"
 
