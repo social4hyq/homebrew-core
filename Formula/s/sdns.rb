d01d6c951e528b8ae10e7a586314a39efcee46af
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-10 16:13:30 +0000

    sdns: update 1.7.4 bottle.

--- Formula/s/sdns.rb
+++ Formula/s/sdns.rb
@@ -7,7 +7,7 @@ class Sdns < Formula
   head "https://github.com/semihalev/sdns.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "34fab8f07f4da530b3dbc66be7b5a2805eefb5583b463c84d19caf3f888eccf2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e2d0d61234c8d009ea221336d81d66146608f82c748d512af3061a72c83c02a8"
   end
 
   depends_on "go" => :build
