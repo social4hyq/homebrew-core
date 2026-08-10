7c9adbb1113a3f4fa688fbd18cd1b7553a288179
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-10 16:21:41 +0000

    rtk: update 0.45.0 bottle.

--- Formula/r/rtk.rb
+++ Formula/r/rtk.rb
@@ -7,7 +7,7 @@ class Rtk < Formula
   head "https://github.com/rtk-ai/rtk.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2c84e5eb1fd66d8aa291566d5396966b95a9b0bc5f0c428e763e906b8f10877f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8dc21801698fafd506d598505f7bb4a14a0448ba7c538e78648c8b8a272a7c4c"
   end
 
   depends_on "rust" => :build
