e89cd7f19de20ded3b6fae67a6e80ddb1fbea229
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-10 13:46:38 +0000

    libnfs 7.0.0

--- Formula/lib/libnfs.rb
+++ Formula/lib/libnfs.rb
@@ -1,8 +1,8 @@
 class Libnfs < Formula
   desc "C client library for NFS"
   homepage "https://github.com/sahlberg/libnfs"
-  url "https://github.com/sahlberg/libnfs/archive/refs/tags/libnfs-6.0.2.tar.gz"
-  sha256 "4e5459cc3e0242447879004e9ad28286d4d27daa42cbdcde423248fad911e747"
+  url "https://github.com/sahlberg/libnfs/archive/refs/tags/libnfs-7.0.0.tar.gz"
+  sha256 "d25c70537d60f1ab307b9cb5e9fb01acff71065fc9547b54dfaec109ba993003"
   license "LGPL-2.1-or-later"
 
   bottle do
