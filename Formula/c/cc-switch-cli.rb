63cf978dcefd1d6b971f743bc388140313fb6a12
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-10 08:21:22 +0000

    cc-switch-cli: update 5.10.1 bottle.

--- Formula/c/cc-switch-cli.rb
+++ Formula/c/cc-switch-cli.rb
@@ -7,12 +7,7 @@ class CcSwitchCli < Formula
   head "https://github.com/SaladDay/cc-switch-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5453e4a5640a072a6236ad4a4c6b6a3941214509f81b7b9455b40aa46531d8e0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80a7568f0ef1b8001c1f2aff3746220dad6a23d8188ad078eb3926e69c005810"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a7bbe3a62fe499898ca614db86e5a84efa938628a4851d76a4decf3e12369ef"
-    sha256 cellar: :any_skip_relocation, sonoma:        "23d626bda87c74b811016954c2284d7e299d2fa8d42701c6fc023f6c071b671d"
-    sha256 cellar: :any,                 arm64_linux:   "3febdddf872bf8cb1b24a8bf7f3fcb983ddbca19bcbcb4c72cda4f9aebe54fbd"
-    sha256 cellar: :any,                 x86_64_linux:  "a392e9b5cea61ed01128287bf8af6e053ee22784c0a367cf158763e045f5ade1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fdec8232333880081e40bffb5ec1197d6faf259ee9c31db558d0b3305f9863e3"
   end
 
   depends_on "rust" => :build
