25a76dfc61ec0883cb21712fe57e878574ffe72e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-10 16:52:02 +0000

    mill: update 1.1.8 bottle.

--- Formula/m/mill.rb
+++ Formula/m/mill.rb
@@ -11,7 +11,7 @@ class Mill < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "41cad057b3bca94d4c96739b3dd290c8b236105693e23b412802008a7961d8d4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b8d640deac64de9df2528be9c4053f4f9556e90b1a870bd5fa129987a89146b1"
   end
 
   depends_on "openjdk"
