91375e44ad7a448f826088cb71c3f1d8ea4f0e50
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-10 13:42:44 +0000

    libcec 8.1.6

--- Formula/lib/libcec.rb
+++ Formula/lib/libcec.rb
@@ -1,8 +1,8 @@
 class Libcec < Formula
   desc "Control devices with TV remote control and HDMI cabling"
   homepage "https://libcec.pulse-eight.com/"
-  url "https://github.com/Pulse-Eight/libcec/archive/refs/tags/libcec-8.1.4.tar.gz"
-  sha256 "61302836cc21c610b6fe0c751e4f130296828f191ee2650f80f73a376fe8c90d"
+  url "https://github.com/Pulse-Eight/libcec/archive/refs/tags/libcec-8.1.6.tar.gz"
+  sha256 "e1e762fee8589def3cbceb1f3e53ba06ed5b557a2705b86a225f8f30fb19c79a"
   license "GPL-2.0-or-later"
 
   bottle do
