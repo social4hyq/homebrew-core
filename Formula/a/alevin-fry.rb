69cef8e09da3d2d080785618cf6cd2f136ddacfe
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-11 01:33:18 +0000

    alevin-fry 0.17.1

--- Formula/a/alevin-fry.rb
+++ Formula/a/alevin-fry.rb
@@ -1,8 +1,8 @@
 class AlevinFry < Formula
   desc "Efficient and flexible tool for processing single-cell sequencing data"
   homepage "https://github.com/COMBINE-lab/alevin-fry"
-  url "https://github.com/COMBINE-lab/alevin-fry/archive/refs/tags/v0.16.2.tar.gz"
-  sha256 "fa1786133b95b7f011a6d4d743c07b3aea7c4f5633bc0d1d51a1758bbbc11157"
+  url "https://github.com/COMBINE-lab/alevin-fry/archive/refs/tags/v0.17.1.tar.gz"
+  sha256 "ae96ba37d0258a002cc844306e7fb2f06f29c610013c946a20e9ecf2bccf0b2d"
   license "BSD-3-Clause"
 
   bottle do
