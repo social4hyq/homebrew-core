cbd7179947a6b756df33ca490b9732909b2a7611
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-12 22:36:12 +0000

    duo_unix 2.3.0

--- Formula/d/duo_unix.rb
+++ Formula/d/duo_unix.rb
@@ -1,8 +1,8 @@
 class DuoUnix < Formula
   desc "Two-factor authentication for SSH"
   homepage "https://www.duosecurity.com/docs/duounix"
-  url "https://github.com/duosecurity/duo_unix/archive/refs/tags/duo_unix-2.2.3.tar.gz"
-  sha256 "53e57e2471978851b7d7e3d0b479ce8d8e68fff1298cb2e597106f0c57ff22ff"
+  url "https://github.com/duosecurity/duo_unix/archive/refs/tags/duo_unix-2.3.0.tar.gz"
+  sha256 "f8c53a1beb54f40765c1f5708a6cf6fd4abd94c645d5fdc52e222223d2040092"
   license "GPL-2.0-or-later"
 
   bottle do
