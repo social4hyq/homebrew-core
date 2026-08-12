740ab2ed5a68773342fb3b832a3116ed9254a0c1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-12 02:58:31 +0000

    nox: update 2026.8.10 bottle.

--- Formula/n/nox.rb
+++ Formula/n/nox.rb
@@ -8,7 +8,7 @@ class Nox < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "eda6f2ede91f9b050bbf0fb2c040a3013330da0cbd05ddd493aab859e64e2671"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7feb286b03dd08667b1841a4a97e66fda1c061f1ae2d77ba4a96f1d407c4d8b8"
   end
 
   depends_on "certifi" => :no_linkage
