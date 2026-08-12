ece83c01805fb24d908685e4016fde5260422ed0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-12 03:44:45 +0000

    aws-crt-cpp: update 0.43.5 bottle.

--- Formula/a/aws-crt-cpp.rb
+++ Formula/a/aws-crt-cpp.rb
@@ -7,7 +7,7 @@ class AwsCrtCpp < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9f35f34169a23475d33c8cbf874ced914bb308acbdccd290167a211a4c3733fc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ce2bb8a8b8f0a095762992a0c25324ee79e0466371b5beda73ced10b1afc1be3"
   end
 
   depends_on "cmake" => :build
