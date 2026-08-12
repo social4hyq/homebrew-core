7083d7f18d29cb2e84f9bad5028dfdecd7d26de5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-12 02:42:04 +0000

    qshell: update 2.19.12 bottle.

--- Formula/q/qshell.rb
+++ Formula/q/qshell.rb
@@ -7,7 +7,7 @@ class Qshell < Formula
   head "https://github.com/qiniu/qshell.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5dd5c1f466441ed63eabb6b8b4cc71c43b49fe85e1f8baf29b6f2671c89b24c0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d34ac538b3abd92f9d1e6d8b662a84560e46bd5c3f9feeeb659dbe7447674471"
   end
 
   depends_on "go" => :build
