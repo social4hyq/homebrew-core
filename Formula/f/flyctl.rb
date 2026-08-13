5975c1c02d9bbe0cbe3a05ba3de2e93b01a6e58c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-13 22:40:15 +0000

    flyctl 0.4.83

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.4.80",
-      revision: "180de61a617a9ee9936045c2b40e0d2f6461d70b"
+      tag:      "v0.4.83",
+      revision: "29e9b03d8c3a16318a3ff02d0b1911f268e9cb01"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
