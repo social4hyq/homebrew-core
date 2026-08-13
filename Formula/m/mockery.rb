ea0cfa43af4cdb60275cbb6a1e16d1bc0d7ac3ac
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-13 10:03:21 +0000

    mockery: update 3.7.3 bottle.

--- Formula/m/mockery.rb
+++ Formula/m/mockery.rb
@@ -15,7 +15,7 @@ class Mockery < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7f44b7c02a6f430dcbb0b9ca1fdffb750e5391876b3d4b46836c7c43d9371975"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3adc655ca16715fb5bb6d605676b2e45e7f9ca38dec5376cd961b6d0329f3755"
   end
 
   depends_on "go" => :build
