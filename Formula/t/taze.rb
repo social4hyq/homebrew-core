d8d8a08ff069a6098b1e61eec41f1318f5733ddf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-16 00:21:56 +0000

    taze 21.1.0

--- Formula/t/taze.rb
+++ Formula/t/taze.rb
@@ -1,8 +1,8 @@
 class Taze < Formula
   desc "Modern cli tool that keeps your deps fresh"
   homepage "https://github.com/antfu-collective/taze"
-  url "https://registry.npmjs.org/taze/-/taze-20.0.1.tgz"
-  sha256 "e80915be471506fbda2f05908393fecd2c46107be80272b1a435b516d5a5ea5d"
+  url "https://registry.npmjs.org/taze/-/taze-21.1.0.tgz"
+  sha256 "1bf46e8a0f36409a694cb22b8a01ca6506f19c8545b089eef601d952f68fa77e"
   license "MIT"
   head "https://github.com/antfu-collective/taze.git", branch: "main"
 
