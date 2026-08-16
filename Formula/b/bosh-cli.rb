ad24f881a53ab0b495ff3310f927fbbb4ee23462
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-16 04:49:10 +0000

    bosh-cli: update 7.10.9 bottle.

--- Formula/b/bosh-cli.rb
+++ Formula/b/bosh-cli.rb
@@ -7,7 +7,7 @@ class BoshCli < Formula
   head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "18f9dcd1711ee2af26fe8fdbcde4e32bd4f3662ddec41591be730cfa4633ba24"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "009e56f5db59aee05aba0e1a9ccd97a0d6f380b813a4cd68b1aae8cfa37048ac"
   end
 
   depends_on "go" => :build
