1467bfac53b3ceac0d838931fbe1e0ca967397c4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-16 04:14:55 +0000

    pyright: update 1.1.413 bottle.

--- Formula/p/pyright.rb
+++ Formula/p/pyright.rb
@@ -6,7 +6,7 @@ class Pyright < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9600a13f1d1cf2c6cc5a0bde3456ac220005fc0fe1be118657f2744c6fdf6b72"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4de58a0756f1fa451d6056bc9a8c0eb4c89d7e96f04b516ca779279bf98ceb19"
   end
 
   depends_on "node"
