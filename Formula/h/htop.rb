0dece3dda25d8f84912e14a9acac09be3ece9587
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-16 23:01:11 +0000

    htop 3.5.3

--- Formula/h/htop.rb
+++ Formula/h/htop.rb
@@ -1,8 +1,8 @@
 class Htop < Formula
   desc "Improved top (interactive process viewer)"
   homepage "https://htop.dev/"
-  url "https://github.com/htop-dev/htop/archive/refs/tags/3.5.2.tar.gz"
-  sha256 "a66a62bbd1eba59889c68f868b643e53320eea93da19f43ba13c822a826d82ba"
+  url "https://github.com/htop-dev/htop/archive/refs/tags/3.5.3.tar.gz"
+  sha256 "edf25ee020a5263ffbef9eef5a8c14392bf74e78b3d5c8bc64d9343dd9a82605"
   license "GPL-2.0-or-later"
   head "https://github.com/htop-dev/htop.git", branch: "main"
 
