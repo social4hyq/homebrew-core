e3a6ee44207b0ab5d53b73828fdf90d84cf0812d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:54:21 +0000

    libcdio: rebuild

--- Formula/lib/libcdio.rb
+++ Formula/lib/libcdio.rb
@@ -11,10 +11,6 @@ class Libcdio < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "68e4fe0d9754e9efc4de83f9ca1a8d9969f6f0264f6677de6bc429924ace4a59"
-  end
-
   depends_on "pkgconf" => :build
 
   def install
