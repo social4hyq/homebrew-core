8eafd08f6a031a45665477ab887b9f5d8e3f6e72
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:18:21 +0000

    cliproxyapi: rebuild

--- Formula/c/cliproxyapi.rb
+++ Formula/c/cliproxyapi.rb
@@ -12,10 +12,6 @@ class Cliproxyapi < Formula
     throttle 5
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b78c425747ffb9fb6bb2ad4661d10576fb25bd6b39656712c43aef50d935c398"
-  end
-
   depends_on "go" => :build
 
   def install
