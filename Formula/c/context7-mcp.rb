1e005eb093bd0d2b20ee418e257f529503bd52cf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:38:17 +0000

    context7-mcp: add 4.0.2 bottle.

--- Formula/c/context7-mcp.rb
+++ Formula/c/context7-mcp.rb
@@ -5,6 +5,11 @@ class Context7Mcp < Formula
   sha256 "da9f9793678aa332cd9a74a9f118c638764ac69c34e95c1d139e7e7c15903071"
   license "MIT"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "35020cb738f03266b21113a941616b14f3f82a530fb7a8e10d5c4a9df088e46a"
+  end
+
   depends_on "node"
 
   def install
