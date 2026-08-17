af5f7280a4b33d85ae402695f6f1a8d13711ccf9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 00:38:56 +0000

    timoni 0.32.0

--- Formula/t/timoni.rb
+++ Formula/t/timoni.rb
@@ -1,8 +1,8 @@
 class Timoni < Formula
   desc "Package manager for Kubernetes, powered by CUE and inspired by Helm"
   homepage "https://timoni.sh/"
-  url "https://github.com/stefanprodan/timoni/archive/refs/tags/v0.31.0.tar.gz"
-  sha256 "4dd2cadf9ac85fa3f8044daf00df7f5e7e5224c17cfe289dc7b8b08137c75307"
+  url "https://github.com/stefanprodan/timoni/archive/refs/tags/v0.32.0.tar.gz"
+  sha256 "0b28ffac02e9186659a96abbfebcc0394ae5e3318390872a3b42b999f50e1a14"
   license "Apache-2.0"
   head "https://github.com/stefanprodan/timoni.git", branch: "main"
 
