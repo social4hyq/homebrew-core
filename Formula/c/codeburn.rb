b90c6455c377ee272a6c04191dc29e3bbfb4db10
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:40:49 +0000

    codeburn: add 0.9.20 bottle.

--- Formula/c/codeburn.rb
+++ Formula/c/codeburn.rb
@@ -5,6 +5,11 @@ class Codeburn < Formula
   sha256 "4416d3b074047dba22b909a34347dd0d388d06679c7ca6e4d2d98420bef21d1c"
   license "MIT"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "855d8fc104071c225b6eb3968754f3757920963f204ba297f92caedc8d24a9f4"
+  end
+
   depends_on "node"
 
   def install
