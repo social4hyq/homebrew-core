3433abf19caa9952a5310bf3a3ce9296422ad744
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:55:15 +0000

    meta-package-manager: rebuild

--- Formula/m/meta-package-manager.rb
+++ Formula/m/meta-package-manager.rb
@@ -9,10 +9,6 @@ class MetaPackageManager < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/kdeldycke/meta-package-manager.git", branch: "main"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "90fb53bca02a75cfab2de7ea056cb6340ba09148cd16b8ac0eb79f5778d9d808"
-  end
-
   depends_on "rust" => :build
 
   depends_on "certifi" => :no_linkage
