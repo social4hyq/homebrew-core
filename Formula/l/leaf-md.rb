8dc2409447685404e335cac7edabc5c5be25f38a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 10:52:35 +0000

    leaf-md: update 1.28.0 bottle.

--- Formula/l/leaf-md.rb
+++ Formula/l/leaf-md.rb
@@ -7,7 +7,7 @@ class LeafMd < Formula
   head "https://github.com/RivoLink/leaf.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "262f11f76761277c1e8c4b383368230e4a1acac43c05126583618b48f3770f79"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8571b96b49a5a7ed463b7a38b23c793333840858669e37ca75de5b1997a6bef6"
   end
 
   depends_on "rust" => :build
