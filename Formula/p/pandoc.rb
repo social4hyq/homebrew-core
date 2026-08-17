c4795c1ca0b42abf82f7dd4599194a87f2ef7a79
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 13:09:05 +0000

    pandoc: add 3.10.2 bottle.

--- Formula/p/pandoc.rb
+++ Formula/p/pandoc.rb
@@ -7,6 +7,11 @@ class Pandoc < Formula
   compatibility_version 3
   head "https://github.com/jgm/pandoc.git", branch: "main"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2f7c8c124d0cc0e0acb712a22468bd6ed30dc168004d469b33c50e89d4258a8a"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
   depends_on "gmp"
