4c68fb4946fe15f30de25d40c4390ebfae35c650
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:49:31 +0000

    azcopy: add 10.32.7 bottle.

--- Formula/a/azcopy.rb
+++ Formula/a/azcopy.rb
@@ -11,6 +11,11 @@ class Azcopy < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "44c1540ee46ee9f01eff83d820c9b1a38ab36bd9a76905786c9e0314109fed99"
+  end
+
   depends_on "go" => :build
 
   def install
