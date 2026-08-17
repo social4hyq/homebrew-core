1a04f6aa267a7305c2f9f5d984b4420c60c81faf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-17 11:54:21 +0000

    libzim: rebuild

--- Formula/lib/libzim.rb
+++ Formula/lib/libzim.rb
@@ -5,10 +5,6 @@ class Libzim < Formula
   sha256 "38f8e2139a089f00196f288f52f2d0677a6becc218f380b54ca70b6f162398bd"
   license "GPL-2.0-or-later"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d73f0cc3064d0a5c41ce54a0a38c16e6b60e1c8be5f85a686fc3d3f46905b867"
-  end
-
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
