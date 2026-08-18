4b25242c1e82c214ad2e54182089336b7ce152ee
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-18 22:26:24 +0000

    codex 0.148.0

--- Formula/c/codex.rb
+++ Formula/c/codex.rb
@@ -1,8 +1,8 @@
 class Codex < Formula
   desc "OpenAI's coding agent that runs in your terminal"
   homepage "https://github.com/openai/codex"
-  url "https://github.com/openai/codex/archive/refs/tags/rust-v0.147.0.tar.gz"
-  sha256 "355bde4b40d5ba6deea2e469d36f91708315729f3e84c9c69cce6b041a5ba593"
+  url "https://github.com/openai/codex/archive/refs/tags/rust-v0.148.0.tar.gz"
+  sha256 "a45e90403eb36b7d6093b167fe1c7dba9b36063bef6d39359eed52c47a21f94a"
   license "Apache-2.0"
 
   livecheck do
