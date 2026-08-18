f9e7f9cdd0c647641f70465d8c00175fddbbf377
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-18 01:15:20 +0000

    dalfox: add 3.2.1 bottle.

--- Formula/d/dalfox.rb
+++ Formula/d/dalfox.rb
@@ -6,6 +6,11 @@ class Dalfox < Formula
   license "MIT"
   head "https://github.com/hahwul/dalfox.git", branch: "main"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7cc6bf160fb6a28175d70b0886c85dc78d3021515db602df4fb3f27ea60e2aba"
+  end
+
   depends_on "rust" => :build
   depends_on "cmake" => :build
 
