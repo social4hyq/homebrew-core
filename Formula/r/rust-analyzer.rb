23dc8c316af6fbbae00c1413fbc2b168f2047485
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-18 00:26:47 +0000

    rust-analyzer 2026-08-17.4

--- Formula/r/rust-analyzer.rb
+++ Formula/r/rust-analyzer.rb
@@ -2,8 +2,9 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-      tag:      "2026-08-10",
-      revision: "57411b3a0fb0181ed5965f6541d5ed39a9d79d4c"
+      tag:      "2026-08-17.4",
+      revision: "bb3bbbd9e4529cbf1a6392d5953f03eb01af3792"
+  version "2026-08-17.4"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
