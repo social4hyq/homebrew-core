a514a384d336ce0c1ceb468a2215aa2dc3176e51
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 13:16:28 +0000

    storj-uplink: update 1.162.3 bottle.

--- Formula/s/storj-uplink.rb
+++ Formula/s/storj-uplink.rb
@@ -18,8 +18,7 @@ class StorjUplink < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6acfaffd1268cfa5b77443fe96614e74f41ff9a0057b95a19f7d401b86a54c89"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7783ea1401e4443c7b4d56285c4be3eeb661df54a0133d3981eaed564d8fa4c1"
   end
 
   depends_on "go" => :build
