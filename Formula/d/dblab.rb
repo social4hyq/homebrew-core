882315aa664a2457831f78892690fdc5d9f81624
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 16:37:10 +0000

    dblab: update 0.48.1 bottle.

--- Formula/d/dblab.rb
+++ Formula/d/dblab.rb
@@ -7,7 +7,7 @@ class Dblab < Formula
   head "https://github.com/danvergara/dblab.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cd78cd1c2ac9c7b52df8853f231089c07b65324278ea6c26b0ad601843758b85"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "74d97381e78823561a562681537b2938408010ed441997c96ec7bd7373abaf61"
   end
 
   depends_on "go" => :build
