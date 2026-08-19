375162b800bbe5251c5f32be047f255c9a0a9958
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 14:24:28 +0000

    gdu: update 5.37.0 bottle.

--- Formula/g/gdu.rb
+++ Formula/g/gdu.rb
@@ -7,7 +7,7 @@ class Gdu < Formula
   head "https://github.com/dundee/gdu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "dfce157112ad2bbed3cf665496dcfe8238abeb05f3b5f72ef2f6692129b39b15"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bbf91400cb3b2f58b19ea06a548da81c8901047dca5409140522c3c4fa9c37e3"
   end
 
   depends_on "go" => :build
