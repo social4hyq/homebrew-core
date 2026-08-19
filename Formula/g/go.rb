9c6e232a9fd13f09a737fe6ac3206f933b4eca66
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 22:54:33 +0000

    go 1.27.0

--- Formula/g/go.rb
+++ Formula/g/go.rb
@@ -1,13 +1,12 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.26.6.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.26.6.src.tar.gz"
-  sha256 "a0721c54c688901448d77ad9b3ec7ea7c474730755ff891382e92ecb93ff2cb1"
+  url "https://go.dev/dl/go1.27.0.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.27.0.src.tar.gz"
+  sha256 "7002403d7cc44529ef6d26f69a44818263395ead7c16c05a5808ae047ebeb0e5"
   license "BSD-3-Clause"
   compatibility_version 4
   head "https://go.googlesource.com/go.git", branch: "master"
-  revision 1
 
   livecheck do
     url "https://go.dev/dl/?mode=json"
