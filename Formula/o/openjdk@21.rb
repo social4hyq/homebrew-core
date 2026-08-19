8e53ff502f231b91e3843ca6499a54c67c22e0c0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 13:46:27 +0000

    openjdk@21: update 21.0.12.1 bottle.

--- Formula/o/openjdk@21.rb
+++ Formula/o/openjdk@21.rb
@@ -12,7 +12,7 @@ class OpenjdkAT21 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d3116b7ec981f5aadc198015cd0163194157b9c2349c89193c09375a1d3ec608"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fcc7d987d1f32901ac87f2d6593a412350caba176e12f6aa4ddf38f6e77ec5a8"
   end
 
   keg_only :versioned_formula
