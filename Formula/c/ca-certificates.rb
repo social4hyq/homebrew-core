3c8b777587ba4f9f46baaf4ead34f835106d0879
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 06:43:31 +0000

    ca-certificates: add 2026-08-13 bottle.

--- Formula/c/ca-certificates.rb
+++ Formula/c/ca-certificates.rb
@@ -11,6 +11,11 @@ class CaCertificates < Formula
     regex(/href=.*?cacert[._-](\d{4}-\d{2}-\d{2})\.pem/i)
   end
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "087d3a3d9a7a274b07406177ad707f3796a78159b8a2c109c0e8d36f28c7310f"
+  end
+
   def install
     pkgshare.install "cacert-#{version}.pem" => "cacert.pem"
   end
