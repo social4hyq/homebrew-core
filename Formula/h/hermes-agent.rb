fba1434261dafe9aa3ac25204f4dd9ecc0c1cb76
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-19 14:47:23 +0000

    hermes-agent: update 2026.8.18 bottle.

--- Formula/h/hermes-agent.rb
+++ Formula/h/hermes-agent.rb
@@ -14,7 +14,7 @@ class HermesAgent < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a26fcb31db1f55db64a892bf94200045923ec24928a398b4d46ac61bfc613e13"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f327adf461ec0d54257615d94847d7cd556d59b48350058168cde52edea3102d"
   end
 
   depends_on "pkgconf" => :build
