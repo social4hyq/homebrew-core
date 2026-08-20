127a399f369a3f02c85dc0b02457c9be3faf772d
hqzing <hqzing@outlook.com>
2026-08-20 15:43:22 +0800

    node@24: disable sve2 in bundled openssl

--- Formula/n/node@24.rb
+++ Formula/n/node@24.rb
@@ -5,7 +5,7 @@ class NodeAT24 < Formula
   sha256 "f6d95e10a0431ee1067fc6aabe9f762908b4716dd35324e1ddb4b1466b76659f"
   license "MIT"
   compatibility_version 1
-  revision 1
+  revision 2
 
   livecheck do
     url "https://nodejs.org/dist/"
@@ -14,6 +14,10 @@ class NodeAT24 < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "1f9c1a59efbefe8eda7293a98170ccfb2a98b30da7d32d53f3108b6a09493e68"
+  end
+
+  patch do
+    file "Patches/node@24/0001-openssl-armcap-disable-sve2.patch"
   end
 
   keg_only :versioned_formula
@@ -66,8 +70,7 @@ class NodeAT24 < Formula
       ./configure \
         --prefix=#{prefix} \
         --dest-os=openharmony \
-        --partly-static \
-        --openssl-no-asm
+        --partly-static
 
       make -j$(nproc)
       mkdir -p /dest
