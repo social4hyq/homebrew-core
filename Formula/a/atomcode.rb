40b9c00a0e9e473e004cb73e117405769a331254
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-20 22:08:46 +0000

    atomcode 5.0.8

--- Formula/a/atomcode.rb
+++ Formula/a/atomcode.rb
@@ -1,8 +1,8 @@
 class Atomcode < Formula
   desc "Open-source alternative to Claude Code / Cursor Agent, living in your terminal"
   homepage "https://atomcode.atomgit.com/"
-  url "https://github.com/atomgit-atomcode/atomcode/archive/refs/tags/v5.0.7.tar.gz"
-  sha256 "1481ed69a50e534a7b3f471a15ebcfb4cf37fc2c5aca6ffe06e0938f7923e6ed"
+  url "https://github.com/atomgit-atomcode/atomcode/archive/refs/tags/v5.0.8.tar.gz"
+  sha256 "82fe57d7873c7193994c7fb424c3199ae9485f03f2b9eff4a11b78fbe58f92e1"
   license "MIT"
 
   livecheck do
