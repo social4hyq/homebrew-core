9af658f9531bb913244b74bf60cdf015a3beaebd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-20 22:49:08 +0000

    fern-api 5.100.0

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -1,8 +1,8 @@
 class FernApi < Formula
   desc "Stripe-level SDKs and Docs for your API"
   homepage "https://buildwithfern.com/"
-  url "https://registry.npmjs.org/fern-api/-/fern-api-5.96.0.tgz"
-  sha256 "0fe3a7a35083432a957b5157d25899a8de412b3d60fd3da42150bdc429b56f5a"
+  url "https://registry.npmjs.org/fern-api/-/fern-api-5.100.0.tgz"
+  sha256 "e490eed7045f0ef809b95cd12bf498c6b31eba71220d3db2da7958cbec933b6c"
   license "Apache-2.0"
 
   livecheck do
