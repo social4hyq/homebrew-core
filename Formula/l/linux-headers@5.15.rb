a0affe3b6593ce071b448e99a11f2eaf6c78c360
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-20 14:52:07 +0000

    linux-headers@5.15: update 5.15.216 bottle.

--- Formula/l/linux-headers@5.15.rb
+++ Formula/l/linux-headers@5.15.rb
@@ -12,7 +12,7 @@ class LinuxHeadersAT515 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5758149d57a08639c7446b11e198d577ef844252c00c11e4c633b74a065bae24"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5857fec87824e16eee845dc00d67866b137db1c4797f0066ab982c690c8e0263"
   end
 
   keg_only :versioned_formula
