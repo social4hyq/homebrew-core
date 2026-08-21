1c4801481e0e362fbcc36ab21b773381c48534f0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-21 14:20:45 +0000

    crowdin: update 4.15.1 bottle.

--- Formula/c/crowdin.rb
+++ Formula/c/crowdin.rb
@@ -11,7 +11,7 @@ class Crowdin < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "476a1ebae29b07d57c730a19a1a68a6ac4f243ae8f5fed87da0cf8d973814b00"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d6453bcf02a85678ce5077bbc4aa40e73b0ad35f5bb8e13175adc24a815ab8fb"
   end
 
   depends_on "openjdk"
