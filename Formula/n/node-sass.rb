02911edc15079d6da354afc8594f5c7649dc4849
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-21 13:30:24 +0000

    node-sass: update 1.103.0 bottle.

--- Formula/n/node-sass.rb
+++ Formula/n/node-sass.rb
@@ -6,7 +6,7 @@ class NodeSass < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "58f587fcf9be626d9dfeb1356f8a110b8db65a4822200b51c8d11de8c235da5f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3df0a2518214f94f8f0633b4215c2df63bb813645e2a3ab5bd8565ec9128a1c7"
   end
 
   depends_on "node"
