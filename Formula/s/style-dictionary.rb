997ed83804adf6b04acf8525f0cba57f6de5b6eb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-21 12:55:28 +0000

    style-dictionary: update 5.5.2 bottle.

--- Formula/s/style-dictionary.rb
+++ Formula/s/style-dictionary.rb
@@ -6,7 +6,7 @@ class StyleDictionary < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a7b3010e236ea99987540e44e0cf8ea5ced552c5e7017bc59b3aee88dd1d26b1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9c98a057373548db1b65f22fcae69ec0f88b4022e738f3fc68db483d1ebc3672"
   end
 
   depends_on "node"
