9003264a9360d3ff1860b5e1a1ce107c7c07141e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-21 12:42:56 +0000

    yq: update 4.53.6 bottle.

--- Formula/y/yq.rb
+++ Formula/y/yq.rb
@@ -13,7 +13,7 @@ class Yq < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "edf602041bdfc99d211f9c197040b785454922cf39edd4e8d3421b53c3185fe0"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1892274f848a930a9977d759d54acfa611f0870d1737f6bcd9a662169cf46cfe"
   end
 
   depends_on "go" => :build
