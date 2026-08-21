e3542ae54a64ff8968ae2481a609bdc5e381f135
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-21 14:00:34 +0000

    imath: update 3.2.3 bottle.

--- Formula/i/imath.rb
+++ Formula/i/imath.rb
@@ -7,7 +7,7 @@ class Imath < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "174c8e452dd535d34dd2e730946cef447a54e184422969bc78614d1912c3499f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "abd7fe6f7339734c7a1b061fe03028ff2b44a4f4a4e4574e2440e56fa1ba2d36"
   end
 
   depends_on "cmake" => :build
