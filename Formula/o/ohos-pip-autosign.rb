f431da3c8d231c31270f89957b0c3d4216c027ba
hqzing <hqzing@outlook.com>
2026-08-22 13:22:49 +0800

    ohos-pip-autosign: optimize error info

--- Formula/o/ohos-pip-autosign.rb
+++ Formula/o/ohos-pip-autosign.rb
@@ -4,7 +4,7 @@ class OhosPipAutosign < Formula
   url "https://atomgit.com/Harmonybrew/ohos-pip-autosign/releases/download/v1.0.0/ohos-pip-autosign-1.0.0.tar.gz"
   sha256 "c19145f6f52585746c0a75c2bdcd140c0ea732b904cce4e3a8d2a6265321381d"
   license "BSD-2-Clause"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "9d1178f40ce04e7645ff9addf53c996b9f5f23d3b54e194eefa6d02f60c0950d"
@@ -13,10 +13,11 @@ class OhosPipAutosign < Formula
   def install
     (bin/"ohos-pip-autosign").write <<~'EOS'
       #!/bin/sh
-      echo 'ohos-pip-autosign is no longer needed: the bundled pip' >&2
-      echo 'of python@3.14, python@3.13 and python@3.12 now ships' >&2
-      echo 'auto-signing. Run `brew upgrade` to upgrade your python' >&2
-      echo 'and enjoy out-of-the-box auto-signing.' >&2
+      echo 'Error:' >&2
+      echo '  ohos-pip-autosign is no longer needed: the bundled pip' >&2
+      echo '  of python@3.14, python@3.13 and python@3.12 now ships' >&2
+      echo '  auto-signing. Run `brew upgrade` to upgrade your python' >&2
+      echo '  and enjoy out-of-the-box auto-signing.' >&2
       exit 1
     EOS
     chmod 0755, bin/"ohos-pip-autosign"
