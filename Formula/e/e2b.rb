ac0296c2ec26e8d75db861f47607e9c6416394b5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-22 22:32:41 +0000

    e2b 2.17.1

--- Formula/e/e2b.rb
+++ Formula/e/e2b.rb
@@ -1,8 +1,8 @@
 class E2b < Formula
   desc "CLI to manage E2B sandboxes and templates"
   homepage "https://e2b.dev"
-  url "https://registry.npmjs.org/@e2b/cli/-/cli-2.16.3.tgz"
-  sha256 "1d2ae6f11e479c7cb3de11267fab2d19aff3e6fd91c040d48161930b3b17af2c"
+  url "https://registry.npmjs.org/@e2b/cli/-/cli-2.17.1.tgz"
+  sha256 "c24f5e83293dcde24385610b4f332806a4483271133dd804e753d5d5183712ba"
   license "Apache-2.0"
 
   bottle do
