bbdd35166ed99424b5548bbdb10dbb03bd9d79f0
hqzing <hqzing@outlook.com>
2026-08-23 02:00:25 +0800

    python@3.13: update patches

--- Formula/p/python@3.13.rb
+++ Formula/p/python@3.13.rb
@@ -5,7 +5,7 @@ class PythonAT313 < Formula
   sha256 "c28d9d213c09b5b5ab2c29812950e12f746999e099b82894231be954b26baed9"
   license "Python-2.0"
   compatibility_version 1
-  revision 4
+  revision 5
 
   livecheck do
     url "https://www.python.org/downloads/source/"
@@ -58,7 +58,7 @@ class PythonAT313 < Formula
     sha256 "f6ad667e89a1fe78046c8f13232b247200f5258d7828f3f7883d660878e0813f"
 
     patch do
-      file "Patches/python@3.14/0001-add-self-sign-py.patch"
+      file "Patches/python@3.14/0001-add-selfsign-py.patch"
     end
 
     patch do
