84244496a2a7f06c76c9173b3c4c4ad71f62d303
hqzing <hqzing@outlook.com>
2026-08-23 17:54:49 +0800

    nvm: hardcode platform

--- Formula/n/nvm.rb
+++ Formula/n/nvm.rb
@@ -5,12 +5,12 @@ class Nvm < Formula
   sha256 "d2fb84dba9914b02cd69b97df35dfca8695b8f22df6128667034d85b69b52d57"
   license "MIT"
   head "https://github.com/nvm-sh/nvm.git", branch: "master"
-  revision 1
+  revision 2
 
-  # TODO: drop this patch once
-  # https://github.com/nvm-sh/nvm/pull/3898 is merged upstream.
+  # Hardcode the platform identifier to ensure this nvm can run
+  # on any minimalist system environment (e.g., DockerHarmony).
   patch do
-    file "Patches/nvm/0001-add-ohos-support.patch"
+    file "Patches/nvm/0001-hardcode-platform.patch"
   end
 
   bottle do
