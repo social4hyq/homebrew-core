89be5b8bd29eaa42282f96e45b905b419b4b8da9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-23 15:57:02 +0000

    oxker: update 0.13.4 bottle.

--- Formula/o/oxker.rb
+++ Formula/o/oxker.rb
@@ -7,7 +7,7 @@ class Oxker < Formula
   head "https://github.com/mrjackwills/oxker.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "cd592c679cb6459ab1afa1f9570eaa1b8b8bd238310371f7767f24c188a795f6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ee4de15c42ed274c91606550094800d42022f8e5f318e3c112417d93a399930b"
   end
 
   depends_on "rust" => :build
