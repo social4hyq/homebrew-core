7da99a1517675c597aa88c988ab659469bd1b052
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-23 16:33:26 +0000

    aws-iam-authenticator: update 0.7.19 bottle.

--- Formula/a/aws-iam-authenticator.rb
+++ Formula/a/aws-iam-authenticator.rb
@@ -16,7 +16,7 @@ class AwsIamAuthenticator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "43becd305df2e25ab965a85f1cc260f2b793eb14661898917e0c467aa29b59c3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b5456a0d27ca8f6e217046317e6eec802b0c34490f7a4dab4f8ea7b8f9d03cfd"
   end
 
   depends_on "go" => :build
