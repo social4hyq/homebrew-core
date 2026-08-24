70794a9d15232d4282f7d0ab324973563245ddda
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-24 03:12:35 +0000

    cython: update 3.3.0 bottle.

--- Formula/c/cython.rb
+++ Formula/c/cython.rb
@@ -6,7 +6,7 @@ class Cython < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "389af3cf8de4f17df176d47f7b914cb8c41faa82a241efd94016fe70509ab458"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e393efc31abe654cc0ca7b59425a43d6e044ab74d02240b598dab36d54a24678"
   end
 
   keg_only <<~EOS
