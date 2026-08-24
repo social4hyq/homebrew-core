9f01d1807e1ef379168c76c1793cd7b00c642dcf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-24 03:20:41 +0000

    asc: update 4.9.0 bottle.

--- Formula/a/asc.rb
+++ Formula/a/asc.rb
@@ -7,7 +7,7 @@ class Asc < Formula
   head "https://github.com/rorkai/App-Store-Connect-CLI.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "80395608d8b3e4fa3ec41d3b97da16d275528f5f1142e42946cc5d0689f34221"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "06f6a4e4f3bcd3d11ad68483ae508afa02e782e72975724e9af6aecdb8b49ab6"
   end
 
   depends_on "go" => :build
