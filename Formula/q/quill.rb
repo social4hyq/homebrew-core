df9a0bed282d57ce9276cc84c9dad131a1d57e8d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-25 00:05:40 +0000

    quill 12.2.2

--- Formula/q/quill.rb
+++ Formula/q/quill.rb
@@ -1,8 +1,8 @@
 class Quill < Formula
   desc "C++17 Asynchronous Low Latency Logging Library"
   homepage "https://github.com/odygrd/quill"
-  url "https://github.com/odygrd/quill/archive/refs/tags/v12.2.0.tar.gz"
-  sha256 "7bd6d49813551dd337cebe41c38b92c49431f014107d5cba2141dd7629a30b54"
+  url "https://github.com/odygrd/quill/archive/refs/tags/v12.2.2.tar.gz"
+  sha256 "0daa25874ad15fa17a5cd8964ddb184ee120bed6090575c686217be152a5d7cd"
   license "MIT"
   head "https://github.com/odygrd/quill.git", branch: "master"
 
