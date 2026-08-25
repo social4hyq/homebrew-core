cc6c9ec570c896ab17b74116a4496a8e10823761
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-25 23:29:58 +0000

    openssl@3.5 3.5.8

--- Formula/o/openssl@3.5.rb
+++ Formula/o/openssl@3.5.rb
@@ -1,11 +1,10 @@
 class OpensslAT35 < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl-library.org"
-  url "https://github.com/openssl/openssl/releases/download/openssl-3.5.7/openssl-3.5.7.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/openssl-3.5.7.tar.gz"
-  sha256 "a8c0d28a529ca480f9f36cf5792e2cd21984552a3c8e4aa11a24aa31aeac98e8"
+  url "https://github.com/openssl/openssl/releases/download/openssl-3.5.8/openssl-3.5.8.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/openssl-3.5.8.tar.gz"
+  sha256 "a8f84a39918ec6415ce765d9b429d313ba97b8143169c172e734b9514464f5b2"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url "https://openssl-library.org/source/"
