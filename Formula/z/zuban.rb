f1ceaab36084409fbdbba0a3d05c4fbcfbf86981
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-26 09:42:25 +0000

    zuban: update 0.9.2 bottle.

--- Formula/z/zuban.rb
+++ Formula/z/zuban.rb
@@ -9,7 +9,7 @@ class Zuban < Formula
   head "https://github.com/zubanls/zuban.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7867c342076aaed54f2a0fdb661669f8f244b69c98ab89b94a58c5064807a0c2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2e117d5aa3070c803fcd2edcd3f08f66c8517c7dd62f5296c4c641e4230816ba"
   end
 
   depends_on "rust" => :build
