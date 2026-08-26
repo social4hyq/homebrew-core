6c71597d227e6c34b5124ab72462bb5206eea641
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-26 11:20:06 +0000

    charmcraft: update 4.4.1 bottle.

--- Formula/c/charmcraft.rb
+++ Formula/c/charmcraft.rb
@@ -9,7 +9,7 @@ class Charmcraft < Formula
   head "https://github.com/canonical/charmcraft.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5bf50c7791d5044b1bbb62842171bd031245d651050ac706214eb86903e9a8d5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "17504230a979240442afd9e19504e07448027b00da651ffdf7a9536b5228e3bd"
   end
 
   depends_on "certifi" => :no_linkage
