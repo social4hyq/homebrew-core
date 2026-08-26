ef402c8ad976701c168cf843c1b231b033f1e8ae
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-26 08:50:00 +0000

    go: update 1.27.0_2 bottle.

--- Formula/g/go.rb
+++ Formula/g/go.rb
@@ -23,7 +23,8 @@ class Go < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4253b1dc8f0a7ce6c48a412a04e9187eaa884449bb9f3a8b5c70096c082be724"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "db00551faa6d17b8c204cc9ae236c7941ccb27b6218223d026bf18eef85c08d0"
   end
 
   depends_on macos: :monterey
