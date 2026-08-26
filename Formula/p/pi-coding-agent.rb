aef63d3ed94987f96bf2d10d3cb7f088f5d2f378
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-26 09:58:08 +0000

    pi-coding-agent: update 0.84.3 bottle.

--- Formula/p/pi-coding-agent.rb
+++ Formula/p/pi-coding-agent.rb
@@ -6,7 +6,7 @@ class PiCodingAgent < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a07856d2762adebbd8d3dc988a2fe56145ea4116c4cde101151f117da9fea280"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "79e65f5b2e6ba33623aee1cb6432a04305201b36c74912008933380e6e0184e0"
   end
 
   depends_on "node"
