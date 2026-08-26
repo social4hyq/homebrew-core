7ff570fa9819d058a28976883073f069fb4e84f6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-26 10:04:59 +0000

    rainfrog: update 0.4.5 bottle.

--- Formula/r/rainfrog.rb
+++ Formula/r/rainfrog.rb
@@ -7,7 +7,7 @@ class Rainfrog < Formula
   head "https://github.com/achristmascarl/rainfrog.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "86194c29b52220f77daf0c9dbfd281f3a79dafdcbbb619d6867c5ffde1132266"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ac26f7bf3c05a48b08a0d66ca7eebc9eaf24ce7cc2e535d9aeb457330f286a70"
   end
 
   depends_on "rust" => :build
