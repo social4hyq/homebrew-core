b6812babbf34197bfc40a88301ed05bebf42adb4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-27 23:19:56 +0000

    mcp-remote 0.3.3

--- Formula/m/mcp-remote.rb
+++ Formula/m/mcp-remote.rb
@@ -1,8 +1,8 @@
 class McpRemote < Formula
   desc "Remote proxy for Model Context Protocol with OAuth support"
   homepage "https://github.com/geelen/mcp-remote"
-  url "https://registry.npmjs.org/mcp-remote/-/mcp-remote-0.2.4.tgz"
-  sha256 "401593b18cfb6cb478c0b452e75aa1e38f6320d9a9eda390d68ad92c4f8d4522"
+  url "https://registry.npmjs.org/mcp-remote/-/mcp-remote-0.3.3.tgz"
+  sha256 "dd6c13944eef4cdf6d42119761b8b822a2e34dc8b9ba944f768aa3e920814286"
   license "MIT"
 
   bottle do
