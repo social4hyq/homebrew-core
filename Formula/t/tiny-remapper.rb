4ca95e464405d57526dd7eed3d47c5dc7740f5a7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-27 01:43:12 +0000

    tiny-remapper: update 0.14.1 bottle.

--- Formula/t/tiny-remapper.rb
+++ Formula/t/tiny-remapper.rb
@@ -12,7 +12,7 @@ class TinyRemapper < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e80eaa38c36cae194c99e0ee6d5378d53941b2501d87f01b242533abf2221223"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2a67feef894f0a6c0376c50245a01806ae2719fa4d32de886fdb2b0016acfdca"
   end
 
   depends_on "openjdk@21"
