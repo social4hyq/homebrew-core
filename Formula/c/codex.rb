b8da6eec3f46fa785af2e8056f43ff7f50159acb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-27 04:30:08 +0000

    codex: update 0.150.0 bottle.

--- Formula/c/codex.rb
+++ Formula/c/codex.rb
@@ -11,7 +11,7 @@ class Codex < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2ae5b9624c4a9034579749cf108241d4920b8b4377a1931ad0e9db6041c679ac"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1a08fa65026236c53abbf1693917c6861aa8b3b74349318b85bae0500f7c8f1a"
   end
 
   depends_on "cmake" => :build
