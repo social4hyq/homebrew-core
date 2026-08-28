7c24511cea109ac399e6ca3c9e3a7df6389b7041
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-28 22:21:56 +0000

    croc 11.3.4

--- Formula/c/croc.rb
+++ Formula/c/croc.rb
@@ -1,8 +1,8 @@
 class Croc < Formula
   desc "Securely send things from one computer to another"
   homepage "https://github.com/schollz/croc"
-  url "https://github.com/schollz/croc/archive/refs/tags/v11.3.2.tar.gz"
-  sha256 "441f751cc45d0a66312a54bf5374a6f30e95992e0d71c808d5aaadb62f74cd67"
+  url "https://github.com/schollz/croc/archive/refs/tags/v11.3.4.tar.gz"
+  sha256 "f851e1e85fa04be3fbe9c1aef11b1da7c6c90fc59986cc498169569abbbd33bc"
   license "MIT"
   head "https://github.com/schollz/croc.git", branch: "main"
 
