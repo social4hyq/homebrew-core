f785892fe64a584ffcdc2c2c8938b96e3ca540a9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-28 07:26:33 +0000

    hf-mcp-server: update 0.4.15 bottle.

--- Formula/h/hf-mcp-server.rb
+++ Formula/h/hf-mcp-server.rb
@@ -6,7 +6,7 @@ class HfMcpServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "faa1b731c9f1b89db9f313310fbfa5b5f9050262654a030bcd6883549b036817"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b61c7b28d69efc41472a7e7cafe848cec5255da61c7a97c4826ba2dc1e442b20"
   end
 
   depends_on "node"
