8f15e5c3d5be4b66441f8c692bb5af8672e9b6b5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-28 08:15:04 +0000

    bitrise: update 2.43.2 bottle.

--- Formula/b/bitrise.rb
+++ Formula/b/bitrise.rb
@@ -12,7 +12,7 @@ class Bitrise < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ecd2dc4e276b0ed3ac03c174091dbdac42e3c99da16b15af53dd4f4387ca50ee"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7dc106a7a5e4aa4ac798392440b8835aa75bda9061f23cbbffbe63305a48ae56"
   end
 
   depends_on "go" => [:build, :test]
