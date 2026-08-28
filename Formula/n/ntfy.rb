857bd6fcc7defba21333f4038d969f5e21b2487b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-28 07:11:14 +0000

    ntfy: update 2.28.0 bottle.

--- Formula/n/ntfy.rb
+++ Formula/n/ntfy.rb
@@ -7,7 +7,7 @@ class Ntfy < Formula
   head "https://github.com/binwiederhier/ntfy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2e26ac2bba8d947422297fa22d3f72ab4719983d6c915c54fae04ec96d852a9a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "300aab725ac36b7e34817a706069adba057099428682fd7ff0d77f7120c118a6"
   end
 
   depends_on "go" => :build
