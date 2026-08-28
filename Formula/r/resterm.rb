d39fc3103635e74fdfe91ce01f75cd4b5bea1692
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-28 06:42:57 +0000

    resterm: update 1.4.1 bottle.

--- Formula/r/resterm.rb
+++ Formula/r/resterm.rb
@@ -7,7 +7,7 @@ class Resterm < Formula
   head "https://github.com/unkn0wn-root/resterm.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c9c81fa925b284e95cff40f3e2190a409fa4f3b6fd2d6898a8d5bdb62dc8fec5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "18c43ac8455cac1e8c248207e0d0293ce01e740a1c7ec1c9e95f1bb04a702e63"
   end
 
   depends_on "go" => :build
