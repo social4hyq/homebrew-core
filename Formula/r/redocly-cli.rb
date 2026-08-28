fd1b2d21ea62ac888702e9a7d6b2ab846a8d3761
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-28 06:47:36 +0000

    redocly-cli: update 2.48.0 bottle.

--- Formula/r/redocly-cli.rb
+++ Formula/r/redocly-cli.rb
@@ -6,7 +6,7 @@ class RedoclyCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "ff282ac57afd4f222d77f0ecdb8576c4a3c31bc225aa5f55ed27881019de8051"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "32490cb7a87a5d7a5d8f0c0c1790c7de7149d11c4d4c847591168a80e1942e3a"
   end
 
   depends_on "node"
