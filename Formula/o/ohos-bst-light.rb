edf171e29f24723e3871a0e2e2c195eeca595c3b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-29 04:01:14 +0000

    ohos-bst-light: add 2.1.2 bottle.

--- Formula/o/ohos-bst-light.rb
+++ Formula/o/ohos-bst-light.rb
@@ -5,6 +5,10 @@ class OhosBstLight < Formula
   sha256 "707296b7263a5be14d412db689be20c32c20483f0760268ebd95b7306e4b95be"
   license "0BSD"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8fbf9c1f2c9a2617d2131cf37265ec3f7d04dfabb4a7fd793450452e1ce5f5fd"
+  end
+
   def install
     system ENV.cc, "selfsign.c", "-o", "selfsign"
     bin.install "selfsign"
