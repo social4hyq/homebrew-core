e9e20e5682e8f6667611669ba1338db6332b5c22
hu60cn <hu60.cn@qq.com>
2026-08-29 02:00:16 +0000

    httpd 2.4.68 (new formula)

---
+++ Formula/h/httpd.rb
@@ -0,0 +1,194 @@
+class Httpd < Formula
+  desc "Apache HTTP server"
+  homepage "https://httpd.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=httpd/httpd-2.4.68.tar.bz2"
+  mirror "https://downloads.apache.org/httpd/httpd-2.4.68.tar.bz2"
+  sha256 "68c74d4df38c26bed4dfbdb8f3baf1eb532f3872357becc1bba5d136f6b63c06"
+  license "Apache-2.0"
+  compatibility_version 1
+
+  bottle do
+    sha256 arm64_tahoe:   "e021f36bba83fcceb2d9b7e182f81d6a5616ec23c2ce0bb4a3435bf7fa53f61c"
+    sha256 arm64_sequoia: "0549386596be75710f999a1f5e7b45804c020bec2c2347613f416e67d4cc0ba4"
+    sha256 arm64_sonoma:  "5835c1181a511b8c0eb5729dc27734669387a1c2d0dd95322ec6ae6b2a3f0bfc"
+    sha256 sonoma:        "50b619ada5467134fbd92ee54c1fed89f4f8c3b4598ae6c9c197993e40e447aa"
+    sha256 arm64_linux:   "356a06f03b55c39098fc33df6a098785a9097253afe40251f6300cb93a82a372"
+    sha256 x86_64_linux:  "a0e54338a4716e465093269159fc55bc979608755a7a09c49f3d9d5d7fcce755"
+  end
+
+  depends_on "apr"
+  depends_on "apr-util"
+  depends_on "brotli"
+  depends_on "libnghttp2"
+  depends_on "openssl@3"
+  depends_on "pcre2"
+
+  uses_from_macos "libxcrypt"
+  uses_from_macos "libxml2"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  def install
+    # fixup prefix references in favour of opt_prefix references
+    inreplace "Makefile.in",
+      '#@@ServerRoot@@#$(prefix)#', "\#@@ServerRoot@@##{opt_prefix}#"
+    inreplace "docs/conf/extra/httpd-autoindex.conf.in",
+      "@exp_iconsdir@", "#{opt_pkgshare}/icons"
+    inreplace "docs/conf/extra/httpd-multilang-errordoc.conf.in",
+      "@exp_errordir@", "#{opt_pkgshare}/error"
+
+    # fix default user/group when running as root
+    inreplace "docs/conf/httpd.conf.in", /(User|Group) daemon/, "\\1 _www"
+
+    # use Slackware-FHS layout as it's closest to what we want.
+    # these values cannot be passed directly to configure, unfortunately.
+    inreplace "config.layout" do |s|
+      s.gsub! "${datadir}/htdocs", "${datadir}"
+      s.gsub! "${htdocsdir}/manual", "#{pkgshare}/manual"
+      s.gsub! "${datadir}/error",   "#{pkgshare}/error"
+      s.gsub! "${datadir}/icons",   "#{pkgshare}/icons"
+    end
+
+    if OS.mac?
+      libxml2 = "#{MacOS.sdk_for_formula(self).path}/usr"
+      zlib = "#{MacOS.sdk_for_formula(self).path}/usr"
+    else
+      libxml2 = formula_opt_prefix("libxml2")
+      zlib = formula_opt_prefix("zlib-ng-compat")
+    end
+
+    system "./configure", "--enable-layout=Slackware-FHS",
+                          "--prefix=#{prefix}",
+                          "--sbindir=#{bin}",
+                          "--mandir=#{man}",
+                          "--sysconfdir=#{etc}/httpd",
+                          "--datadir=#{var}/www",
+                          "--localstatedir=#{var}",
+                          "--enable-mpms-shared=all",
+                          "--enable-mods-shared=all",
+                          "--enable-authnz-fcgi",
+                          "--enable-cgi",
+                          "--enable-pie",
+                          "--enable-suexec",
+                          "--with-suexec-bin=#{opt_bin}/suexec",
+                          "--with-suexec-caller=_www",
+                          "--with-port=8080",
+                          "--with-sslport=8443",
+                          "--with-apr=#{formula_opt_prefix("apr")}",
+                          "--with-apr-util=#{formula_opt_prefix("apr-util")}",
+                          "--with-brotli=#{formula_opt_prefix("brotli")}",
+                          "--with-libxml2=#{libxml2}",
+                          "--with-mpm=prefork",
+                          "--with-nghttp2=#{formula_opt_prefix("libnghttp2")}",
+                          "--with-ssl=#{formula_opt_prefix("openssl@3")}",
+                          "--with-pcre=#{formula_opt_prefix("pcre2")}/bin/pcre2-config",
+                          "--with-z=#{zlib}",
+                          "--disable-lua",
+                          "--disable-luajit"
+
+    # The Apache build system links shared modules without APR libraries
+    # (`SH_LDFLAGS` is only defaulted for AIX/OS390), relying on the dlopen
+    # global symbol scope to resolve `apr_*` symbols at runtime. This works on
+    # glibc but fails on the musl-based OHOS loader ("Error relocating
+    # mod_*.so: apr_palloc: symbol not found"). Link APR into modules directly
+    # so each module records a NEEDED dependency on libapr-1/libaprutil-1.
+    inreplace "build/config_vars.mk",
+              /^SH_LDFLAGS =.*$/,
+              "SH_LDFLAGS = -L#{formula_opt_lib("apr")} -lapr-1 " \
+              "-L#{formula_opt_lib("apr-util")} -laprutil-1"
+    system "make"
+    ENV.deparallelize if OS.linux?
+    system "make", "install"
+
+    # suexec does not install without root
+    bin.install "support/suexec"
+
+    # remove non-executable files in bin dir (for brew audit)
+    rm bin/"envvars"
+    rm bin/"envvars-std"
+
+    # avoid using Cellar paths
+    inreplace %W[
+      #{include}/httpd/ap_config_layout.h
+      #{lib}/httpd/build/config_vars.mk
+    ] do |s|
+      s.gsub! lib/"httpd/modules", HOMEBREW_PREFIX/"lib/httpd/modules"
+    end
+
+    inreplace %W[
+      #{bin}/apachectl
+      #{bin}/apxs
+      #{include}/httpd/ap_config_auto.h
+      #{include}/httpd/ap_config_layout.h
+      #{lib}/httpd/build/config_vars.mk
+      #{lib}/httpd/build/config.nice
+    ] do |s|
+      s.gsub! prefix, opt_prefix
+    end
+
+    inreplace "#{lib}/httpd/build/config_vars.mk" do |s|
+      pcre = Formula["pcre2"]
+      s.gsub! pcre.prefix.realpath, pcre.opt_prefix
+      s.gsub! "${prefix}/lib/httpd/modules", HOMEBREW_PREFIX/"lib/httpd/modules"
+      s.gsub! Superenv.shims_path, HOMEBREW_PREFIX/"bin"
+    end
+
+    (var/"cache/httpd").mkpath
+    (var/"www").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      DocumentRoot is #{var}/www.
+
+      The default ports have been set in #{etc}/httpd/httpd.conf to 8080 and in
+      #{etc}/httpd/extra/httpd-ssl.conf to 8443 so that httpd can run without sudo.
+    EOS
+  end
+
+  service do
+    run [opt_bin/"httpd", "-D", "FOREGROUND"]
+    environment_variables PATH: std_service_path_env
+    run_type :immediate
+  end
+
+  test do
+    # Ensure modules depending on zlib and xml2 have been compiled
+    assert_path_exists lib/"httpd/modules/mod_deflate.so"
+    assert_path_exists lib/"httpd/modules/mod_proxy_html.so"
+    assert_path_exists lib/"httpd/modules/mod_xml2enc.so"
+
+    begin
+      port = free_port
+
+      expected_output = "Hello world!"
+      (testpath/"index.html").write expected_output
+      (testpath/"httpd.conf").write <<~EOS
+        Listen #{port}
+        ServerName localhost:#{port}
+        DocumentRoot "#{testpath}"
+        ErrorLog "#{testpath}/httpd-error.log"
+        PidFile "#{testpath}/httpd.pid"
+        LoadModule authz_core_module #{lib}/httpd/modules/mod_authz_core.so
+        LoadModule unixd_module #{lib}/httpd/modules/mod_unixd.so
+        LoadModule dir_module #{lib}/httpd/modules/mod_dir.so
+        LoadModule mpm_prefork_module #{lib}/httpd/modules/mod_mpm_prefork.so
+      EOS
+
+      pid = spawn bin/"httpd", "-X", "-f", testpath/"httpd.conf"
+
+      sleep 3
+      sleep 2 if OS.mac? && Hardware::CPU.intel?
+
+      assert_match expected_output, shell_output("curl -s 127.0.0.1:#{port}")
+
+      # Check that `apxs` can find `apu-1-config`.
+      system bin/"apxs", "-q", "APU_CONFIG"
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
