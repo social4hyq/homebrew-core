b61fb16cac8cab5c258fe8de7c3d536dc3f293b3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-30 05:02:07 +0000

    age: update 1.3.2 bottle.

--- Formula/a/age.rb
+++ Formula/a/age.rb
@@ -7,7 +7,7 @@ class Age < Formula
   head "https://github.com/FiloSottile/age.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e7225f9d6257409bfb6905d4a9f0cfb1cb37c9f4d61d91adc94f31311ecc3c5d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "80ba22fc32099a8a2fcf286c88ef42483cb9e066dbef2d680cbb29306fc64c81"
   end
 
   depends_on "go" => :build
