5e152abcaf07ad78d6e2430cac7a617362d7d05b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-30 04:45:20 +0000

    fabric-ai: update 1.4.473 bottle.

--- Formula/f/fabric-ai.rb
+++ Formula/f/fabric-ai.rb
@@ -7,7 +7,7 @@ class FabricAi < Formula
   head "https://github.com/danielmiessler/fabric.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2c6f459146780cb5c4d30e24d6fdda4ddb146965200fe396d6224feb9b0f3356"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b4209b8654fbd058ce3f3c361fa0aa4e1581ee281085f1dbcee139ba246cb450"
   end
 
   depends_on "go" => :build
