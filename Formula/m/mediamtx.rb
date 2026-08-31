8338a118073fd2d88ce0d9f2164f189a86d67628
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 07:09:23 +0000

    mediamtx 1.20.1

--- Formula/m/mediamtx.rb
+++ Formula/m/mediamtx.rb
@@ -1,27 +1,30 @@
 class Mediamtx < Formula
   desc "Zero-dependency real-time media server and media proxy"
-  homepage "https://github.com/bluenviron/mediamtx"
+  homepage "https://mediamtx.org"
   # need to use the tag to generate the version info
   url "https://github.com/bluenviron/mediamtx.git",
-      tag:      "v1.20.0",
-      revision: "1b943637a4b5778bb929a7af7687b048fecaa03f"
+      tag:      "v1.20.1",
+      revision: "883194a19b7244355c9bc975c0574c9842733637"
   license "MIT"
   head "https://github.com/bluenviron/mediamtx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "20de9489e0c8e2bdf8c2c53a8fafb29ba4a14e55314f187f133b7d99cbd82799"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7a527f2842c0da5dcb41cbb24970df3aece1fdec09fdd4555b366231c9a20a23"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7a527f2842c0da5dcb41cbb24970df3aece1fdec09fdd4555b366231c9a20a23"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7a527f2842c0da5dcb41cbb24970df3aece1fdec09fdd4555b366231c9a20a23"
+    sha256 cellar: :any_skip_relocation, sonoma:        "04ce2887317af1e1a490ced085864c9bb796431fc4c54af3623ea09787681cac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bc6ee206b19b7d5ddf7c33fb32709847163d41e38e634cba08d3ab2e25a51d22"
+    sha256 cellar: :any,                 x86_64_linux:  "db3bf3e86e5738d7f752348f3fb0c4f24a35c6ce2167eec9a82a81071e299f76"
   end
 
   depends_on "go" => :build
 
   def install
     system "go", "generate", "./..."
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    system "go", "build", *std_go_args
 
     # Install default config
     pkgetc.install "mediamtx.yml"
-
-    (var/"log/mediamtx").mkpath
   end
 
   service do
