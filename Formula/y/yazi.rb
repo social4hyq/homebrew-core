24cfc94950464bd2c32c2957d57b228a3caef898
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 08:13:44 +0000

    yazi: update 26.8.15 bottle.

--- Formula/y/yazi.rb
+++ Formula/y/yazi.rb
@@ -15,12 +15,7 @@ class Yazi < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1accaa6b61bf2e20ba634717d232aad110a1944fedf5507dd28e2272ccf0d281"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3c2128b8c4afdce28330ee6836a5ff990ff3d738fa9d30d07cd3add420d4dd4e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c2522e2775b0b82566b0dfa92f8c5c589f7ad57c9bdaf586405d06530bff554"
-    sha256 cellar: :any_skip_relocation, sonoma:        "49473a75a0abe662e73c635bd1e20e120ad9d6d19f66d6f8bb2fa3c75bf1c996"
-    sha256 cellar: :any,                 arm64_linux:   "f28f3ce185e19f3bce528bab520fa492edf7c0f9182c3c48ff3a1b75708cbbcb"
-    sha256 cellar: :any,                 x86_64_linux:  "38cb464d751072da57a10995cbe0881e48c2ebd4c5e65217195463be6d38024b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fb3b724ce1338da498d7b62590b73ecab5427a0c66ed867bb3440bd39f9e4aa7"
   end
 
   depends_on "rust" => :build
