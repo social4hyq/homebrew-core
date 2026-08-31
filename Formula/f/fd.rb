049323ae14b7e31946488eef32cb5b13a1546ca1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 08:14:41 +0000

    fd 10.5.0

--- Formula/f/fd.rb
+++ Formula/f/fd.rb
@@ -1,22 +1,23 @@
 class Fd < Formula
   desc "Simple, fast and user-friendly alternative to find"
   homepage "https://github.com/sharkdp/fd"
-  url "https://github.com/sharkdp/fd/archive/refs/tags/v10.4.2.tar.gz"
-  sha256 "3a7e027af8c8e91c196ac259c703d78cd55c364706ddafbc66d02c326e57a456"
+  url "https://github.com/sharkdp/fd/archive/refs/tags/v10.5.0.tar.gz"
+  sha256 "e6d9e90730bf316101691e49d59cc02565278dc3779d33a77423801569484851"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/fd.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e34e02ad5f6c3c90a7a6bfd6e2f665b932eecff14664f3c85c9bbcb377372895"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f8ccfce40967859000e4512fd37b8f0ee178572f3f131d6d6fa37faac4205f16"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72ac30ac960c40f8bf5746148d94a21f3a14dcb78374108fc5416f29a7a9b146"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d08e1b33c3b39c4ad55a0213c65074ea2c75a7af4559ddfe26f50de361d3f0ac"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c57f68964c5f724c61ab92d691e84d82123fe258e69d416bca3d58d403eeb73a"
+    sha256 cellar: :any,                 arm64_linux:   "fb686c0ddb7761d127084974ca01d9db3a6e14a2e8db50feb709434eebaf6f59"
+    sha256 cellar: :any,                 x86_64_linux:  "651c4799f3f39ff04634016107683fdc333332594a926bdd5623b853855901fd"
   end
 
   depends_on "rust" => :build
 
   conflicts_with "fdclone", because: "both install `fd` binaries"
-
-  patch do
-    file "Patches/fd/0001-support-ohos.patch"
-  end
 
   def install
     ENV["JEMALLOC_SYS_WITH_LG_PAGE"] = "16" if Hardware::CPU.arm? && OS.linux?
