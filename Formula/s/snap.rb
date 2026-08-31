f15f5626bfa8c5a821c08ec06773d37eb525df89
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 07:24:16 +0000

    snap: update 2.76.3.vendor bottle.

--- Formula/s/snap.rb
+++ Formula/s/snap.rb
@@ -11,12 +11,7 @@ class Snap < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9070e427b88542d4bc15f54b2bbf90f3fd58c3b19fb6893d366c136abb03f7d1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9070e427b88542d4bc15f54b2bbf90f3fd58c3b19fb6893d366c136abb03f7d1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9070e427b88542d4bc15f54b2bbf90f3fd58c3b19fb6893d366c136abb03f7d1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c271c99e09e448331af6c14c13b1a525bf950c6ff9c0bbcd8df7dfe8b72d3e57"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ee050afade2c78d723b418761e753ef0d7b137619d9f49279d6aab9a7a573428"
-    sha256 cellar: :any,                 x86_64_linux:  "bad918ec23b37d9c3599a2c943e60abc09b20af8ebdb4ea1a4c46f99a30992b8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "bdfffe3401a3b04b5dd16f7c495f6ad92119adfd57595ba51fb8303a5f36b7f7"
   end
 
   depends_on "go" => :build
