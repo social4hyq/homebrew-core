5ed036fd2bae15773c21a5ee6bcba3ebad3f1dc7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 07:10:46 +0000

    zsync 0.8.0

--- Formula/z/zsync.rb
+++ Formula/z/zsync.rb
@@ -1,8 +1,8 @@
 class Zsync < Formula
   desc "File transfer program"
   homepage "https://zsync.moria.org.uk/"
-  url "https://zsync.moria.org.uk/download/zsync-0.7.2.tar.gz"
-  sha256 "51a54a2bcf60311f108924b5f8795fb7a8eeeedd0b52f4f634842ea3470978a2"
+  url "https://zsync.moria.org.uk/download/zsync-0.8.0.tar.gz"
+  sha256 "58b02f27e14326b62b7fdd6ed431a3e243b1c5a3ea9e3c1678e136dbf00c238d"
   license "Artistic-2.0"
   head "https://github.com/cph6/zsync.git", branch: "master"
 
@@ -12,14 +12,19 @@ class Zsync < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c3c3dfbfa62b845f971675efd71c5b47e7cece8f88a511e883d197407bea528b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a4acdec8c2fc88425b1389ac2839c04af8f182b5c19885ddf9dd6525074cd21e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a4acdec8c2fc88425b1389ac2839c04af8f182b5c19885ddf9dd6525074cd21e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4acdec8c2fc88425b1389ac2839c04af8f182b5c19885ddf9dd6525074cd21e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "237ea6250c443d6e9f7fac829d9b320f1ddf5d1f0a84cf51eeca8b23e609a93e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f516760650bc9a934de177a23391b4c7c9758f6ae64c0d5881ebeb6655149241"
+    sha256 cellar: :any,                 x86_64_linux:  "bd673f47540a39f84abbf844762e25920da491c01d6c792aaad07778568c80a5"
   end
 
   depends_on "go" => :build
 
   def install
     (buildpath/"cmd").each_child(false) do |cmd|
-      system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/cmd), "./cmd/#{cmd}"
+      system "go", "build", *std_go_args(output: bin/cmd), "./cmd/#{cmd}"
       man1.install "man/#{cmd}.1"
     end
   end
@@ -27,7 +32,7 @@ class Zsync < Formula
   test do
     touch testpath/"foo"
     system bin/"zsyncmake", "foo"
-    sha1 = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
-    assert_match "SHA-1: #{sha1}", (testpath/"foo.zsync").read
+    sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    assert_match "File-Hash: SHA-256:#{sha256}", (testpath/"foo.zsync").read
   end
 end
