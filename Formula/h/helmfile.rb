aedd516e25bcbe070f6db8bf0e07350e95d5683d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 07:12:07 +0000

    helmfile 1.7.4

--- Formula/h/helmfile.rb
+++ Formula/h/helmfile.rb
@@ -1,14 +1,19 @@
 class Helmfile < Formula
   desc "Deploy Kubernetes Helm Charts"
   homepage "https://github.com/helmfile/helmfile"
-  url "https://github.com/helmfile/helmfile/archive/refs/tags/v1.7.3.tar.gz"
-  sha256 "0e6ef2a99edefb0ca2b3b3f22d08a912b2860baa7baac82978e1662cf77409ed"
+  url "https://github.com/helmfile/helmfile/archive/refs/tags/v1.7.4.tar.gz"
+  sha256 "87c3926308dea3f48036abe6d75b4b83e749c23080f7adf3c24d92495b81a771"
   license "MIT"
   version_scheme 1
   head "https://github.com/helmfile/helmfile.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c9005c8a3927d6467b5958f424a5803694aa3b153630084a91fd2c51f6d23f20"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c7edd7bdd588781ae916bf7e7819891f6337a6707f93af65fc0101e872be6c3a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1bbabb64300c8989862ab3a2eaf89ecb19471ab0e9ec2b29894505c1c73afbb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "65fe58a1a403630c16cf270265365ffa88499ffc9cddf97f938f1d90d9ce34e1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "05b1db65b3174f1803332d7c67adbef7583fa12c7a83a69c517e5599e5085f2e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "20b898306ba40cd022fb69ef500394004da373fe0f36a45517596d21554a2f4b"
+    sha256 cellar: :any,                 x86_64_linux:  "1182ba0abc6bd4bd780040127c45a540e64d8d5e72865f30d7d18482e85484f4"
   end
 
   depends_on "go" => :build
@@ -16,7 +21,6 @@ class Helmfile < Formula
 
   def install
     ldflags = %W[
-      -s -w
       -X go.szostok.io/version.version=v#{version}
       -X go.szostok.io/version.buildDate=#{time.iso8601}
       -X go.szostok.io/version.commit="brew"
