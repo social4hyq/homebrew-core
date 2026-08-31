bb5d9806a9669eb923f3349ce7c365186f873fca
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 08:25:32 +0000

    wasm3 0.9.0

--- Formula/w/wasm3.rb
+++ Formula/w/wasm3.rb
@@ -1,25 +1,27 @@
 class Wasm3 < Formula
   desc "High performance WebAssembly interpreter"
-  homepage "https://github.com/wasm3/wasm3"
-  url "https://github.com/wasm3/wasm3/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "b778dd72ee2251f4fe9e2666ee3fe1c26f06f517c3ffce572416db067546536c"
+  homepage "https://twitter.com/wasm3_engine"
+  url "https://github.com/wasm3/wasm3/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "cab79ce74bcac25bbf80b5ebe14af9795b9bac30b05ee8f620a3bc8002f3b8e6"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d120772037702c7be3e88c8a850e3fbca56a312d54f6093f7d99bebb6c3ec0ff"
+    sha256 cellar: :any, arm64_tahoe:   "535442b0baca3955b8b1a2a863a3c1435ea6f95905c4d65bd986190a5c47d0fd"
+    sha256 cellar: :any, arm64_sequoia: "d7852e9d45aa2640f29069800eda68c8100238a6d09d89562e9a30b162d14a55"
+    sha256 cellar: :any, arm64_sonoma:  "333a6c7876562076b43d00bd81ac1f33589d87ba4fab58b35091e3411052c94d"
+    sha256 cellar: :any, sonoma:        "8de67257d87de88ccd2f8fb154e7115730bc9944b392e9639d7be967f4b80432"
+    sha256 cellar: :any, arm64_linux:   "6e341d6fed466a7ad6f4f0f2aff871ee29a5caa972336152b65f9da6cdb799ce"
+    sha256 cellar: :any, x86_64_linux:  "0fd6fcd632856e0c0c728afe74cbd7c4d87f1a243b0a1bedec5fb78ffc433b67"
   end
 
   depends_on "cmake" => :build
   depends_on "uvwasi"
 
   def install
-    # Unbundled uvwasi and link to shared library
-    inreplace "CMakeLists.txt" do |s|
-      s.gsub! "FetchContent_GetProperties(uvwasi)",
-              "FetchContent_MakeAvailable(uvwasi)"
-      s.gsub! "target_link_libraries(${OUT_FILE} uvwasi_a uv_a)",
+    # Unbundle uvwasi and link to shared library
+    inreplace "CMakeLists.txt",
+              "target_link_libraries(${OUT_FILE} uvwasi_a uv_a)",
               "target_link_libraries(${OUT_FILE} uvwasi::uvwasi)"
-    end
 
     # We bypass brew's dependency provider to set `FETCHCONTENT_TRY_FIND_PACKAGE_MODE`
     # which redirects FetchContent_Declare() to find_package() and helps find our `uvwasi`.
