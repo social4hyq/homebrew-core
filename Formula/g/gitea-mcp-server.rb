e5c74f8f06f803d8d038e2b5eb1296cb20cd84c5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 08:21:16 +0000

    gitea-mcp-server: update 1.7.0 bottle.

--- Formula/g/gitea-mcp-server.rb
+++ Formula/g/gitea-mcp-server.rb
@@ -7,11 +7,7 @@ class GiteaMcpServer < Formula
   head "https://gitea.com/gitea/gitea-mcp.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0bd199a4112f405777cb0370ed4ac91199d540f1d3499513a4d03d501e686e9b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0bd199a4112f405777cb0370ed4ac91199d540f1d3499513a4d03d501e686e9b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0bd199a4112f405777cb0370ed4ac91199d540f1d3499513a4d03d501e686e9b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3993b89763ddeaf62c8254d9d15bce009d954924ee57e46f616cd262c3c66bc3"
-    sha256 cellar: :any,                 x86_64_linux:  "9d82a9e7bdd64016c1a5f7e2fd4ad70825576e84661f891ff7c33f908f02d5dd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5dc10ee0ab8bbef3af7913236e2b0d8646cc24554ce7c466b33db527da7b56a9"
   end
 
   depends_on "go" => :build
