f136365438b3a398ee96e68becc3f2b8e13fdbd9
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 01:38:59 +0000

    solargraph: update 0.60.4 bottle.

--- Formula/s/solargraph.rb
+++ Formula/s/solargraph.rb
@@ -8,7 +8,7 @@ class Solargraph < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "da40cc9edf6bebc40ed00fe80a11f6a6d715d802b9d9ce171e1ad09e3b4b7a38"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fa5cd5b73f8fa5bd2ea520abbc757de6f997ed553921f32c6a3a68e39e16a044"
   end
 
   depends_on "ruby"
