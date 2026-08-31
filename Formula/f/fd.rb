29866390c9a16e92bb206f1fca94a5630340d5b6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 08:32:49 +0000

    fd: update 10.5.0 bottle.

--- Formula/f/fd.rb
+++ Formula/f/fd.rb
@@ -7,12 +7,7 @@ class Fd < Formula
   head "https://github.com/sharkdp/fd.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f8ccfce40967859000e4512fd37b8f0ee178572f3f131d6d6fa37faac4205f16"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72ac30ac960c40f8bf5746148d94a21f3a14dcb78374108fc5416f29a7a9b146"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d08e1b33c3b39c4ad55a0213c65074ea2c75a7af4559ddfe26f50de361d3f0ac"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c57f68964c5f724c61ab92d691e84d82123fe258e69d416bca3d58d403eeb73a"
-    sha256 cellar: :any,                 arm64_linux:   "fb686c0ddb7761d127084974ca01d9db3a6e14a2e8db50feb709434eebaf6f59"
-    sha256 cellar: :any,                 x86_64_linux:  "651c4799f3f39ff04634016107683fdc333332594a926bdd5623b853855901fd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "adfe00e014a4e32d99f9b806db5b9ccb6ab3c84cae45e24f635596c277edb2e5"
   end
 
   depends_on "rust" => :build
