a6267f4c5ce1b0a37cf0071e8a54e708247a857f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 08:22:44 +0000

    tetra 1.7.1

--- Formula/t/tetra.rb
+++ Formula/t/tetra.rb
@@ -1,8 +1,8 @@
 class Tetra < Formula
   desc "Tetragon CLI to observe, manage and troubleshoot Tetragon instances"
   homepage "https://tetragon.io/"
-  url "https://github.com/cilium/tetragon/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "ccccbe870b880212a86f959ce6f3f9f2d378646ad314100862ea6db5cb5f0a2b"
+  url "https://github.com/cilium/tetragon/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "d4de499f97899855329b5ab8d7fc9fed5be349abd6e45b673c70554e4918bd2a"
   license "Apache-2.0"
 
   livecheck do
@@ -11,19 +11,18 @@ class Tetra < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "aae7e8657ca41de0c55da185ab49ba0a01abd51a7d4f436894066b560aabc143"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "19de6f1783d1d9b2e33860c172d033a5abd4329969ad880633816687e08f1403"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8362b1a39aed776aceac2caeedd170eed45931c4ee281e5c37499357afece3a0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d3eaa89db45b7578beeb8045fe2368089f945f8bce6878c7d0f2c300df8f138"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6ed24fa9479f2265d4b1f735193b67d4ceb378621b48ad776a7e121fe8a6f0db"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "40bdd8a674cf61b2775599e494f8d1a04f7ba8b5daf4649e764940868599a971"
+    sha256 cellar: :any,                 x86_64_linux:  "4b293c3e5579cf7eb19f4b44b5c34e60d894bc6d98a142d26e919ddaae65be5c"
   end
 
   depends_on "go" => :build
 
-  # Add missing darwin stub for bpfProbes function, upstream pr ref, https://github.com/cilium/tetragon/pull/4933
-  patch do
-    url "https://github.com/cilium/tetragon/commit/c99f6bc0b8bebb40cf3cdaf1216e62c8717d85cc.patch?full_index=1"
-    sha256 "1a09f1f9324394a117a3f09a995ef56a5d7d4633169b9a8fe103425fabc840f3"
-  end
-
   def install
-    ldflags = "-s -w -X github.com/cilium/tetragon/pkg/version.Version=#{version}"
+    ldflags = "-X github.com/cilium/tetragon/pkg/version.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:, output: bin/"tetra"), "./cmd/tetra"
 
     generate_completions_from_executable(bin/"tetra", shell_parameter_format: :cobra)
