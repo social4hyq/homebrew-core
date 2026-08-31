8547c6701262d6ad78197026fcccb5abd6cb55e6
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-08-31 09:20:07 +0000

    aws-c-auth 1.0.0

--- Formula/a/aws-c-auth.rb
+++ Formula/a/aws-c-auth.rb
@@ -1,13 +1,17 @@
 class AwsCAuth < Formula
   desc "C99 library implementation of AWS client-side authentication"
   homepage "https://github.com/awslabs/aws-c-auth"
-  url "https://github.com/awslabs/aws-c-auth/archive/refs/tags/v0.10.5.tar.gz"
-  sha256 "1ddd1dc476debdbf9ff083e254396fdc3ea0846dccf7d5f983a6571303abcb35"
+  url "https://github.com/awslabs/aws-c-auth/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "12a29eb62c61cef4b38c90d4f0dd2657dc585a15c138d60941d6f20c1ad3b12d"
   license "Apache-2.0"
-  compatibility_version 1
+  compatibility_version 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9c1a650f999f12c715db376d83c6d1b6b382c6dfda27564f8cea7ac49fcbf3bc"
+    sha256 cellar: :any, arm64_tahoe:   "347f11c6e772a94298aff3463f3044e7234474018dca30a1d97eb0f2f8fdcde4"
+    sha256 cellar: :any, arm64_sequoia: "a9d57dbf4d39e117ab299795f3fc36e6e57c094630a371817fcdc1031f3dfc4c"
+    sha256 cellar: :any, arm64_sonoma:  "ab507e68ea1ced21d87eb8d321628f357bc1b5399d6956b6d12f98b0ae1ad15c"
+    sha256 cellar: :any, arm64_linux:   "3e6e970fc23afd5a33355ba4e77350852c89daee59de3594b2e8b7a0e50026ec"
+    sha256 cellar: :any, x86_64_linux:  "5b8291ff7e841d525a8ee1b2ea16b15f6dd793f42aadf6ac7ba170a15e82d6a8"
   end
 
   depends_on "cmake" => :build
@@ -48,7 +52,7 @@ class AwsCAuth < Formula
       }
     C
     system ENV.cc, "test.c", "-o", "test", "-L#{lib}", "-laws-c-auth",
-                   "-L#{Formula["aws-c-common"].opt_lib}", "-laws-c-common"
+                   "-L#{formula_opt_lib("aws-c-common")}", "-laws-c-common"
     system "./test"
   end
 end
