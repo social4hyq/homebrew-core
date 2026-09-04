cc4cc1f39e120542850ee3010ad43dfc48fd7c62
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-04 23:23:32 +0000

    grpcui 1.5.4

--- Formula/g/grpcui.rb
+++ Formula/g/grpcui.rb
@@ -1,8 +1,8 @@
 class Grpcui < Formula
   desc "Interactive web UI for gRPC, along the lines of postman"
   homepage "https://github.com/fullstorydev/grpcui"
-  url "https://github.com/fullstorydev/grpcui/archive/refs/tags/v1.5.2.tar.gz"
-  sha256 "43e127082396b1ea11f4687a6f69555579b34501538e7ca361c9db35e486139d"
+  url "https://github.com/fullstorydev/grpcui/archive/refs/tags/v1.5.4.tar.gz"
+  sha256 "664137b2982cad4bc5e8a8e5963fb46fe3686b6aff0f2f925172fd98d2f8a12f"
   license "MIT"
   head "https://github.com/fullstorydev/grpcui.git", branch: "master"
 
