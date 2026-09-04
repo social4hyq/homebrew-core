9ca9ece8559c60a5e12047eecc1dc3f6fcf7ff88
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-04 23:01:04 +0000

    fallow 3.22.0

--- Formula/f/fallow.rb
+++ Formula/f/fallow.rb
@@ -1,8 +1,8 @@
 class Fallow < Formula
   desc "Codebase intelligence for TypeScript and JavaScript"
   homepage "https://docs.fallow.tools"
-  url "https://github.com/fallow-rs/fallow/archive/refs/tags/v3.20.0.tar.gz"
-  sha256 "48d3d664f2f7c4a2ee24efd13e57f2bffa299efcbda2ba1186af4f2aa9811613"
+  url "https://github.com/fallow-rs/fallow/archive/refs/tags/v3.22.0.tar.gz"
+  sha256 "d9772b23c4023df2e15607a2e5b2437617bf2c49d241e1d54a808acc6903106a"
   license "MIT"
   head "https://github.com/fallow-rs/fallow.git", branch: "main"
 
