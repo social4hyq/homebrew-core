875cc95a00ad44854058499dec3b055746be5d5d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-04 22:08:41 +0000

    atf 0.25

--- Formula/a/atf.rb
+++ Formula/a/atf.rb
@@ -1,8 +1,8 @@
 class Atf < Formula
   desc "Automated testing framework"
   homepage "https://github.com/freebsd/atf"
-  url "https://github.com/freebsd/atf/releases/download/atf-0.23/atf-0.23.tar.gz"
-  sha256 "a64e2427d021297f25b3f2e1798f8ec4dc3061ffb01a1cd3f66cc4cee486b10f"
+  url "https://github.com/freebsd/atf/releases/download/atf-0.25/atf-0.25.tar.gz"
+  sha256 "a52be96b5565733e71df8d0ecc8a4255a495e45183de7e3657491e0a8069423f"
   license "BSD-2-Clause"
   head "https://github.com/freebsd/atf.git", branch: "master"
 
