35672ed48ca59ef5eb8aab994c9d3b18849688e2
lanfungcing <lanfq.26@qq.com>
2026-09-04 02:12:08 +0000

    qpdf: no_autobump

--- Formula/q/qpdf.rb
+++ Formula/q/qpdf.rb
@@ -6,6 +6,8 @@ class Qpdf < Formula
   license "Apache-2.0"
   compatibility_version 1
 
+  no_autobump! because: "newer version requires c++20 support"
+
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "4cb2b47aa9416e4956d226a0a3ea810baf2f0babafc72b8d8d619c80904ed30c"
   end
