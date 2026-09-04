817d0b65c72fd45146c3de6899126c637af24ebe
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-04 23:39:28 +0000

    kargo 1.11.4

--- Formula/k/kargo.rb
+++ Formula/k/kargo.rb
@@ -1,8 +1,8 @@
 class Kargo < Formula
   desc "Multi-Stage GitOps Continuous Promotion"
   homepage "https://kargo.io/"
-  url "https://github.com/akuity/kargo/archive/refs/tags/v1.11.2.tar.gz"
-  sha256 "9cdc85accb9e5b9f780b2e526545093fd2f5983acba3577b7287e26c229f7e5a"
+  url "https://github.com/akuity/kargo/archive/refs/tags/v1.11.4.tar.gz"
+  sha256 "06af0413397fbf482b311e11e016dde82626fcc7f35f39b933c20a45b214825c"
   license "Apache-2.0"
   head "https://github.com/akuity/kargo.git", branch: "main"
 
