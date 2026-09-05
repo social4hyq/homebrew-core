9b3978d6d95dd591ea94c8165ad0c89bb89a77a1
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 04:31:01 +0000

    vercel-cli: update 59.11.2 bottle.

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -6,7 +6,7 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "dfd630d59a349254c022d2904e00eb823d7ded3aeb7c8e76655c7be314794db5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1355428d1d1bac1d82a46bfd8869ee383fbea4a62a516504c748ddf4b908fc38"
   end
 
   depends_on "node"
