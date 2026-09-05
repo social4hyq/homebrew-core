079d0ff6378829c46bf2c5abb4cc753125fb70a4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 07:08:39 +0000

    kimi-code: update 0.40.1 bottle.

--- Formula/k/kimi-code.rb
+++ Formula/k/kimi-code.rb
@@ -6,7 +6,7 @@ class KimiCode < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "081fbf89ee6fb727a62e2bdf5a6c9827f2ede16ee96e36ac8e11a3ba6e674e86"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e070401bdca516fc9a924cbb4932f9df6b3dc251b5a979ef46741e4fdf21b3e1"
   end
 
   depends_on "node"
