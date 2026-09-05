4633d1d9aecedc676ea5aa74bac60d6fa64b5c5c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 05:45:16 +0000

    rqlite: update 10.3.1 bottle.

--- Formula/r/rqlite.rb
+++ Formula/r/rqlite.rb
@@ -7,7 +7,7 @@ class Rqlite < Formula
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "18ca80dcf25b231b6373d1fcd369b30e8f45e231eef3798d9daa1119350a47e5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6cf0d6dd6c048b6a82304124bbcb366d44a98e08534674094114d08ec40fd913"
   end
 
   depends_on "go" => :build
