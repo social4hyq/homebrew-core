58b16dce974cedf1f0f370691137d02a9c7866fb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 00:30:36 +0000

    node-red 5.0.6

--- Formula/n/node-red.rb
+++ Formula/n/node-red.rb
@@ -1,8 +1,8 @@
 class NodeRed < Formula
   desc "Low-code programming for event-driven applications"
   homepage "https://nodered.org/"
-  url "https://registry.npmjs.org/node-red/-/node-red-5.0.4.tgz"
-  sha256 "1acc0ed42023c9170100e2d5596b9db012d0de11f5457f52bed343b67eefbe4a"
+  url "https://registry.npmjs.org/node-red/-/node-red-5.0.6.tgz"
+  sha256 "bc37ccced0441d6d4b24e76dcdabef89f02c5a10a96cf8f623e5b0eb5db47352"
   license "Apache-2.0"
 
   bottle do
