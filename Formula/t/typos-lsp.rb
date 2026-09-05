a93f9a70acfd07137f0b5f7a9c37c2add31e1781
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 04:46:39 +0000

    typos-lsp: update 0.1.56 bottle.

--- Formula/t/typos-lsp.rb
+++ Formula/t/typos-lsp.rb
@@ -11,7 +11,7 @@ class TyposLsp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6c535af2f0bb529970426705e78c5e0a1e6973bfc9616651c8e295010cbf2349"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ccfc0c812fb8956c6fe014a97873a98734007c0a0021e5ec5458de9acfd13b05"
   end
 
   depends_on "rust" => :build
