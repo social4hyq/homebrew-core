c2d9f6d37ff7f6853fdbdd6cc8c9edd5bf130a58
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 01:48:41 +0000

    trufflehog 3.97.4

--- Formula/t/trufflehog.rb
+++ Formula/t/trufflehog.rb
@@ -1,8 +1,8 @@
 class Trufflehog < Formula
   desc "Find and verify credentials"
   homepage "https://trufflesecurity.com/"
-  url "https://github.com/trufflesecurity/trufflehog/archive/refs/tags/v3.97.1.tar.gz"
-  sha256 "d54e4d055c59b9bf50d1cf2177f638cdfa0672b2d58fd4fc49c87219c54c38e8"
+  url "https://github.com/trufflesecurity/trufflehog/archive/refs/tags/v3.97.4.tar.gz"
+  sha256 "b1346ded49f6283005284c56b98b5c3603c0f5077c0d4fcd083beda9dfba3268"
   # upstream license ask, https://github.com/trufflesecurity/trufflehog/issues/1446
   license "AGPL-3.0-only"
   head "https://github.com/trufflesecurity/trufflehog.git", branch: "main"
