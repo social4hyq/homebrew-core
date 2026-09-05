b50b3817b095766b30d2d9f691a7239a30dd6344
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 09:17:30 +0000

    bstring: update 1.1.1 bottle.

--- Formula/b/bstring.rb
+++ Formula/b/bstring.rb
@@ -7,7 +7,7 @@ class Bstring < Formula
   head "https://github.com/msteinert/bstring.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "08381d08ce19a4db3d91a1fd8869507a9fb43730ec5f2ca5cfb6e114beb86452"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5fa3cf66aa717a2c781ff84db52b1c5949c2786825fb11eac647446b456d6f2e"
   end
 
   depends_on "meson" => :build
