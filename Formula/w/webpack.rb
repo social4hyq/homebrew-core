c2a6198f33514e874af1dc7b3c15d92885aaa59e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 02:06:13 +0000

    webpack 5.110.3

--- Formula/w/webpack.rb
+++ Formula/w/webpack.rb
@@ -3,8 +3,8 @@ require "json"
 class Webpack < Formula
   desc "Bundler for JavaScript and friends"
   homepage "https://webpack.js.org/"
-  url "https://registry.npmjs.org/webpack/-/webpack-5.110.1.tgz"
-  sha256 "ad646d10798485aac196f918447de7dba6ff3c969fc40a3ed930a0f2dc14957d"
+  url "https://registry.npmjs.org/webpack/-/webpack-5.110.3.tgz"
+  sha256 "623e6256da3d33e52a6108a15af8bd36f0911834437a8ee51d27c2e799d64809"
   license "MIT"
   head "https://github.com/webpack/webpack.git", branch: "main"
 
