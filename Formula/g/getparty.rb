1ef4d5e13ac693c559f7f61e6da14292280ebb0b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 07:41:47 +0000

    getparty: update 1.28.1 bottle.

--- Formula/g/getparty.rb
+++ Formula/g/getparty.rb
@@ -7,7 +7,7 @@ class Getparty < Formula
   head "https://github.com/vbauerster/getparty.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "dc3825e6c5de10ea1deaaa22f97d4250bc667143c337eb3b23cb17c0384baacd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "76cda04a04fab31f94ee1826420c24ca6d51b78c2724fa9b009e150d6789575e"
   end
 
   depends_on "go" => :build
