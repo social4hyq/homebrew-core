f4045a9c0539deea23b617b09ceb175dedc7065b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 08:13:46 +0000

    doctl: update 1.168.0 bottle.

--- Formula/d/doctl.rb
+++ Formula/d/doctl.rb
@@ -7,7 +7,7 @@ class Doctl < Formula
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "1cd18b0b718d44f4daf8a6b36e97506f041611c3e1097313164ca0583c570840"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ddf0d30f5b114dd4dca124af9e793e85a5d9d9f9ce04620e7a5d30a008fabe7e"
   end
 
   depends_on "go" => :build
