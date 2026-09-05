a82bcf911c477870187a7bec70d97cdc50d8c229
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 08:48:21 +0000

    copier: update 9.18.1 bottle.

--- Formula/c/copier.rb
+++ Formula/c/copier.rb
@@ -8,7 +8,7 @@ class Copier < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "84709a36ba13f33b2cf0fe657e0cb7a2e82e1bd5ad1e8e4b135ebcfc52034f1b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fcac183ff7dceefc009bb1c4d580886042943e26639216662665f29e82575dc5"
   end
 
   depends_on "libyaml"
