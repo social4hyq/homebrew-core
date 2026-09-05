d3ba04baccae3ac0278aacb3cea30b3e1ae59d67
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 23:09:43 +0000

    libpcap 1.10.7

--- Formula/lib/libpcap.rb
+++ Formula/lib/libpcap.rb
@@ -1,8 +1,8 @@
 class Libpcap < Formula
   desc "Portable library for network traffic capture"
   homepage "https://www.tcpdump.org/"
-  url "https://www.tcpdump.org/release/libpcap-1.10.6.tar.gz"
-  sha256 "872dd11337fe1ab02ad9d4fee047c9da244d695c6ddf34e2ebb733efd4ed8aa9"
+  url "https://www.tcpdump.org/release/libpcap-1.10.7.tar.gz"
+  sha256 "0b394ac90dbc0a9838ff97468e05c9c9a3e873dec2514cd58db65d859d296e31"
   license "BSD-3-Clause"
   compatibility_version 1
   head "https://github.com/the-tcpdump-group/libpcap.git", branch: "master"
