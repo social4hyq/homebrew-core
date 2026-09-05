157d7c80cf275dc2ea7b194371c818f7f840e2b5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 04:41:58 +0000

    usage: update 6.6.1 bottle.

--- Formula/u/usage.rb
+++ Formula/u/usage.rb
@@ -8,7 +8,7 @@ class Usage < Formula
   head "https://github.com/jdx/usage.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "bffeb4f3a107e5093588a4a0515c2ae72d72be600af56c34eaeb7c3ff87a377f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6445b04513277752c985362944c25c09486f95ebe432c734618f9c4185be8f73"
   end
 
   depends_on "rust" => :build
