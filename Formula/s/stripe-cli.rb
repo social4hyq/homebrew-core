2ea9384172ef6863cbc95b83f7ce670e4b09a453
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 05:23:07 +0000

    stripe-cli: update 1.50.10 bottle.

--- Formula/s/stripe-cli.rb
+++ Formula/s/stripe-cli.rb
@@ -6,7 +6,7 @@ class StripeCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "55acf3a6d98c2313c66e893d3f3ddfd0dd3c5e34ee6d034ad06f5d3038689af6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4b0486fb2652e6b78a3151cea53b6fee5f6fb15eea6e1e340a425cab021b98a8"
   end
 
   depends_on "go" => :build
