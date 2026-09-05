e3303d7e352757b728e631880fe77bfdad0b4a89
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-05 04:48:04 +0000

    trufflehog: update 3.97.4 bottle.

--- Formula/t/trufflehog.rb
+++ Formula/t/trufflehog.rb
@@ -8,7 +8,7 @@ class Trufflehog < Formula
   head "https://github.com/trufflesecurity/trufflehog.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9f8ad88313aea0ed720e4480f71c2888eb8bd4e30d7e333a7a1e17201426e889"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "9cef02ec68fbdbb52f28729de89097698d957e9a1553374ea13f648aa7641fc8"
   end
 
   depends_on "go" => :build
