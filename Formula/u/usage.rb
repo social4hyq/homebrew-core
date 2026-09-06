62d966aca7c45a5d63c512bc00719c9f72b61657
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-06 23:57:27 +0000

    usage 6.8.0

--- Formula/u/usage.rb
+++ Formula/u/usage.rb
@@ -1,8 +1,8 @@
 class Usage < Formula
   desc "Tool for working with usage-spec CLIs"
   homepage "https://usage.jdx.dev/"
-  url "https://github.com/jdx/usage/archive/refs/tags/v6.7.1.tar.gz"
-  sha256 "cc0acd11a75634cf1fb110c1d93b9236a9f9187eb083856396d7f9cb2c19ac17"
+  url "https://github.com/jdx/usage/archive/refs/tags/v6.8.0.tar.gz"
+  sha256 "032842fa48ccfcd0c2ca854105c25184cf3ed17250f596f21c4448b0148100de"
   license "MIT"
   compatibility_version 1
   head "https://github.com/jdx/usage.git", branch: "main"
