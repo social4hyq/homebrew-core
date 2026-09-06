2f2f585868c40d3ccc9c667ced6380ce6fb2b24a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-06 22:57:19 +0000

    libheif 1.23.4

--- Formula/lib/libheif.rb
+++ Formula/lib/libheif.rb
@@ -1,8 +1,8 @@
 class Libheif < Formula
   desc "ISO/IEC 23008-12:2017 HEIF file format decoder and encoder"
   homepage "https://www.libde265.org/"
-  url "https://github.com/strukturag/libheif/releases/download/v1.23.3/libheif-1.23.3.tar.gz"
-  sha256 "11c1179e0e4bec33624b87f22ec42c1e993a40d946d44d26f9c431cf1456a863"
+  url "https://github.com/strukturag/libheif/releases/download/v1.23.4/libheif-1.23.4.tar.gz"
+  sha256 "d0c02b4b0e978f34a1974b6f3eea7975a537bf7a9195ffeea38e7242ff316fdd"
   license "LGPL-3.0-or-later"
   compatibility_version 1
 
