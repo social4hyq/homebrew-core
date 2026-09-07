8c2fa709d1a0e9e4db4218232feb3a042fbf57cf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-07 07:47:26 +0000

    ant: update 1.10.18 bottle.

--- Formula/a/ant.rb
+++ Formula/a/ant.rb
@@ -8,7 +8,7 @@ class Ant < Formula
   head "https://git-wip-us.apache.org/repos/asf/ant.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5862987706093073a55b527932a59d6b13eab17259940b5c7970bed35a62a48c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e99f258bbb38694a2a3e1ff3d0404e24da37a473b16472fca277fc9ac7079ca6"
   end
 
   depends_on "openjdk"
