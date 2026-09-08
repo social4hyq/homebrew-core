23875073ec6ecf6afc61c5171ccdb713b1f69d0b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 05:22:43 +0000

    stackql 0.11.669

--- Formula/s/stackql.rb
+++ Formula/s/stackql.rb
@@ -1,8 +1,8 @@
 class Stackql < Formula
   desc "SQL interface for arbitrary resources with full CRUD support"
   homepage "https://stackql.io/"
-  url "https://github.com/stackql/stackql/archive/refs/tags/v0.10.605.tar.gz"
-  sha256 "1f32717086c6d1291057f0aaa93f4d6c0846dbac4195bffd898e1bf90a23648d"
+  url "https://github.com/stackql/stackql/archive/refs/tags/v0.11.669.tar.gz"
+  sha256 "c2d514e25fa0c7813f6905a0cac3d412a28bcc6823db834dbfeb24ad1f845d45"
   license "MIT"
   head "https://github.com/stackql/stackql.git", branch: "main"
 
