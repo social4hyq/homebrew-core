d4cf3f2f1417fec84e94e083218599671d763f7a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 22:44:18 +0000

    govulncheck 1.8.0

--- Formula/g/govulncheck.rb
+++ Formula/g/govulncheck.rb
@@ -1,8 +1,8 @@
 class Govulncheck < Formula
   desc "Database client and tools for the Go vulnerability database"
   homepage "https://github.com/golang/vuln"
-  url "https://github.com/golang/vuln/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "4fb7f0204b7e039f550d8938b714c5218d870694895585e0e19b2c0c4700e4c7"
+  url "https://github.com/golang/vuln/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "f02847e4ab2d18159fd28d22a7001f2c1bbc2eb62ba884831ae35ea89f81b882"
   license "BSD-3-Clause"
   head "https://github.com/golang/vuln.git", branch: "master"
 
