11ad2a065a88025ee6c509d522931a467564546a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 01:24:36 +0000

    azqr 4.1.0

--- Formula/a/azqr.rb
+++ Formula/a/azqr.rb
@@ -3,8 +3,8 @@ class Azqr < Formula
   homepage "https://azure.github.io/azqr/"
   # pull from git tag to get submodules
   url "https://github.com/Azure/azqr.git",
-      tag:      "v.4.0.1",
-      revision: "ffda262cbccc33bf4f472c07f81758839b165b1a"
+      tag:      "v.4.1.0",
+      revision: "4edf82d545d63815b577a50f80eff4a93ca88f62"
   license "MIT"
   head "https://github.com/Azure/azqr.git", branch: "main"
 
