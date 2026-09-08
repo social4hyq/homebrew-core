e09eab48bf227b12bdf74e87c487292d4bed4c00
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 02:21:47 +0000

    gabo 1.20.0

--- Formula/g/gabo.rb
+++ Formula/g/gabo.rb
@@ -1,8 +1,8 @@
 class Gabo < Formula
   desc "Generates GitHub Actions boilerplate"
   homepage "https://github.com/ashishb/gabo"
-  url "https://github.com/ashishb/gabo/archive/refs/tags/v1.19.0.tar.gz"
-  sha256 "4e915becb142044d0f4de66ca691bb7b7de8aeb66f010f179e1be2bd5faff133"
+  url "https://github.com/ashishb/gabo/archive/refs/tags/v1.20.0.tar.gz"
+  sha256 "ba19f20fbc4e1bce153949cab06f9d0fd2a371ff6c11eea24e989634f72c53a9"
   license "Apache-2.0"
   head "https://github.com/ashishb/gabo.git", branch: "master"
 
