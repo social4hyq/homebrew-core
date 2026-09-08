33dc8c874f20cf281a29a3c010313ea9312c6c00
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 22:21:56 +0000

    cubejs-cli 1.7.35

--- Formula/c/cubejs-cli.rb
+++ Formula/c/cubejs-cli.rb
@@ -1,8 +1,8 @@
 class CubejsCli < Formula
   desc "Cube.js command-line interface"
   homepage "https://cube.dev/"
-  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-1.7.34.tgz"
-  sha256 "14bba4cdb9e76cd3c5fb29008eb055896c2e4e538fb3952cd9f7fc2f9429e269"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-1.7.35.tgz"
+  sha256 "7ed13ad00105ab7cf4d52b62548a85a1b3518b47f567e567059b4007f98cdc8a"
   license "Apache-2.0"
 
   bottle do
