e75318067bfaa152a8382258758f8e8556e317bc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 22:37:34 +0000

    gitea-runner 3.4.1

--- Formula/g/gitea-runner.rb
+++ Formula/g/gitea-runner.rb
@@ -1,8 +1,8 @@
 class GiteaRunner < Formula
   desc "Official Actions runner for Gitea"
   homepage "https://gitea.com/gitea/runner"
-  url "https://gitea.com/gitea/runner/archive/v3.3.2.tar.gz"
-  sha256 "8f08d06598e729a6e3e97bc63b470c66ffd6e620c7e154d1bbad4644b30b06f1"
+  url "https://gitea.com/gitea/runner/archive/v3.4.1.tar.gz"
+  sha256 "a3e56d09a9d2d208711a39c4ea9b2d02e17feaf9ed188fa26b9a0977b51c4859"
   license "MIT"
 
   bottle do
