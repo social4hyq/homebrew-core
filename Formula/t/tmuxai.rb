cfd56dc96658ff537d8768748d839a1350dd5e08
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 05:33:36 +0000

    tmuxai 2.3.2

--- Formula/t/tmuxai.rb
+++ Formula/t/tmuxai.rb
@@ -1,8 +1,8 @@
 class Tmuxai < Formula
   desc "AI-powered, non-intrusive terminal assistant"
   homepage "https://tmuxai.dev/"
-  url "https://github.com/BoringDystopiaDevelopment/tmuxai/archive/refs/tags/v2.3.1.tar.gz"
-  sha256 "0ccb8881c5af169eaf2c9d171791742e8580311e12582adfb73988ea9fd2ee28"
+  url "https://github.com/BoringDystopiaDevelopment/tmuxai/archive/refs/tags/v2.3.2.tar.gz"
+  sha256 "5bac370f71aa03735d42f4f5b41f53bb96132a207b9db6836719603f6132b5f8"
   license "Apache-2.0"
 
   bottle do
