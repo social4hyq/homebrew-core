e26c09002efb3451f9731f17152044844e7272f8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 01:34:00 +0000

    bzip3 1.5.4

--- Formula/b/bzip3.rb
+++ Formula/b/bzip3.rb
@@ -1,8 +1,8 @@
 class Bzip3 < Formula
   desc "Better and stronger spiritual successor to BZip2"
   homepage "https://github.com/kspalaiologos/bzip3"
-  url "https://github.com/kspalaiologos/bzip3/releases/download/1.5.3/bzip3-1.5.3.tar.gz"
-  sha256 "c48823353084df2a5a0dba44fd5295abd078e40b49f09700d08af4d9b1e31d67"
+  url "https://github.com/kspalaiologos/bzip3/releases/download/1.5.4/bzip3-1.5.4.tar.gz"
+  sha256 "89a5e4bf29e4aae98b29bb1ef275addfa2d0806ba1ef60bf8a87263cdb21f581"
   license "LGPL-3.0-only"
 
   bottle do
