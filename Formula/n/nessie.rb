0172843802ec8d1cc71d58098e78b56e4a77335e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 23:27:48 +0000

    nessie 0.108.5

--- Formula/n/nessie.rb
+++ Formula/n/nessie.rb
@@ -1,8 +1,8 @@
 class Nessie < Formula
   desc "Transactional Catalog for Data Lakes with Git-like semantics"
   homepage "https://projectnessie.org"
-  url "https://github.com/projectnessie/nessie/archive/refs/tags/nessie-0.108.4.tar.gz"
-  sha256 "af1501a93443d57fbe0c01d6882eecf0a279fe9741b21f7500e2f49e1c2dc113"
+  url "https://github.com/projectnessie/nessie/archive/refs/tags/nessie-0.108.5.tar.gz"
+  sha256 "6f430bc1530e63ee107b3dac3006eaf19dc8b7e4241e697a8ded2db04469a182"
   license "Apache-2.0"
 
   bottle do
