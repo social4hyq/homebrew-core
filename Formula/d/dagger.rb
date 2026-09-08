45de0ddb77b891482ccb03b7f6dac1c875d23a0f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 11:48:40 +0000

    dagger 0.21.9

--- Formula/d/dagger.rb
+++ Formula/d/dagger.rb
@@ -1,8 +1,8 @@
 class Dagger < Formula
   desc "Portable devkit for CI/CD pipelines"
   homepage "https://dagger.io"
-  url "https://github.com/dagger/dagger/archive/refs/tags/v0.21.8.tar.gz"
-  sha256 "2e02b66958913e773d59e344a701d479e252457dcdc6be341e98428322c91565"
+  url "https://github.com/dagger/dagger/archive/refs/tags/v0.21.9.tar.gz"
+  sha256 "652fffab225340df36ea4f65eded61628a9cf5f1dd2ac2f667aadaa5d84eb430"
   license "Apache-2.0"
   head "https://github.com/dagger/dagger.git", branch: "main"
 
@@ -12,16 +12,21 @@ class Dagger < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8cfa0dc1360da4c677c64dc532d3698059a445d190dc07ffabdaa35fd0440a88"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c006567c0c30ac15fa2ae409dc9038e775193e180d1ae9ce2c5c72b2f7376723"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c006567c0c30ac15fa2ae409dc9038e775193e180d1ae9ce2c5c72b2f7376723"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c006567c0c30ac15fa2ae409dc9038e775193e180d1ae9ce2c5c72b2f7376723"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8af26bb659202fc0f636a248128080ae58d507f5eb82bc47e7146494cb2dc8ad"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "152fde54f74bfba2f50cf583263e309839d8fd73d6074a2a2a73dfbd7bc3f6b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "010f907d51bbaa26e7d1ac8f1e10bb117532cb7f58c4406952f79ca795b23a47"
   end
 
-  depends_on "go" => :build
+  # TODO: switch back to `go` when x/net is bumped past v0.54.0 (broken with Go 1.27)
+  depends_on "go@1.26" => :build
   depends_on "docker" => :test
 
   def install
     ENV["CGO_ENABLED"] = OS.mac? ? "1" : "0"
     ldflags = %W[
-      -s -w
       -X github.com/dagger/dagger/engine.Version=v#{version}
       -X github.com/dagger/dagger/engine.Tag=v#{version}
     ]
