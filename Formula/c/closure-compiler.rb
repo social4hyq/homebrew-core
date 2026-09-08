abb7bb4cab764ac101b04f983c375e505f050829
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 22:14:40 +0000

    closure-compiler 20260907

--- Formula/c/closure-compiler.rb
+++ Formula/c/closure-compiler.rb
@@ -1,8 +1,8 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://developers.google.com/closure/compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20260831/closure-compiler-v20260831.jar"
-  sha256 "6149d9a5e7ddd85f04a437f4ce2379cb284bb5e276c1c590549faea2e25d0200"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20260907/closure-compiler-v20260907.jar"
+  sha256 "1689d45ccaa3aabae4a290e0a357d08ce56b08713a90b30e8a305c9bb21ef6eb"
   license "Apache-2.0"
 
   livecheck do
