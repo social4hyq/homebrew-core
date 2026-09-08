52854c46dcaee853a2e679462ae0a553cdd926d8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 22:36:49 +0000

    git-codereview 1.21.0

--- Formula/g/git-codereview.rb
+++ Formula/g/git-codereview.rb
@@ -1,8 +1,8 @@
 class GitCodereview < Formula
   desc "Tool for working with Gerrit code reviews"
   homepage "https://pkg.go.dev/golang.org/x/review/git-codereview"
-  url "https://github.com/golang/review/archive/refs/tags/v1.20.0.tar.gz"
-  sha256 "0361d7c03b773b64f03650948dec1643f0a755b77445e16b849f7bda290ba4e0"
+  url "https://github.com/golang/review/archive/refs/tags/v1.21.0.tar.gz"
+  sha256 "d1215854ba15b8f7d457bb00637f8f137f775004e5a6568dab82d27755dbd587"
   license "BSD-3-Clause"
   head "https://github.com/golang/review.git", branch: "master"
 
