cb9fe307133a6917b27fc0946d0b373e7929b97a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 22:51:13 +0000

    ittapi 3.28.3

--- Formula/i/ittapi.rb
+++ Formula/i/ittapi.rb
@@ -1,8 +1,8 @@
 class Ittapi < Formula
   desc "Intel Instrumentation and Tracing Technology (ITT) and Just-In-Time (JIT) API"
   homepage "https://github.com/intel/ittapi"
-  url "https://github.com/intel/ittapi/archive/refs/tags/v3.28.2.tar.gz"
-  sha256 "04d9f7e8f7f217b732f5fa2eb5a7e7e8419525afebe5ba086430e489f8261280"
+  url "https://github.com/intel/ittapi/archive/refs/tags/v3.28.3.tar.gz"
+  sha256 "b97bde2b1f4448675955dd93bd132c7eaae2bd98ee8392498f1780caa2d9b849"
   license "GPL-2.0-only"
   head "https://github.com/intel/ittapi.git", branch: "master"
 
