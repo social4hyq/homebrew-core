b97b28621089459125d6ec4694b755d1e3b8773e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-08 03:32:04 +0000

    luit 20260907

--- Formula/l/luit.rb
+++ Formula/l/luit.rb
@@ -1,8 +1,8 @@
 class Luit < Formula
   desc "Filter run between arbitrary application and UTF-8 terminal emulator"
   homepage "https://invisible-island.net/luit/"
-  url "https://invisible-mirror.net/archives/luit/luit-20250912.tgz"
-  sha256 "46958060e66f35bcb8a51ba22da1c13d726d28a86c1cf520511bcf7914bef39e"
+  url "https://invisible-mirror.net/archives/luit/luit-20260907.tgz"
+  sha256 "97bd13da3e3aa59785d64504d37389612a327953bb7581508c5fea5182f9f561"
   license "MIT"
 
   livecheck do
