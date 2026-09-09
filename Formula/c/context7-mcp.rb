7e825cf1bef13243ccefa5a4a756c0f5f82f4fa2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 22:19:23 +0000

    context7-mcp 4.0.6

--- Formula/c/context7-mcp.rb
+++ Formula/c/context7-mcp.rb
@@ -1,8 +1,8 @@
 class Context7Mcp < Formula
   desc "Up-to-date code documentation for LLMs and AI code editors"
   homepage "https://github.com/upstash/context7"
-  url "https://registry.npmjs.org/@upstash/context7-mcp/-/context7-mcp-4.0.5.tgz"
-  sha256 "c6ed2aaac7f67cfb94eb35e72a48c4c1204a815f54e15ae6e96ed3465b9d58b4"
+  url "https://registry.npmjs.org/@upstash/context7-mcp/-/context7-mcp-4.0.6.tgz"
+  sha256 "5932210e45dcc4988e31244051f61f031491b41f0b8f10003ad0b2704ce304a3"
   license "MIT"
 
   bottle do
