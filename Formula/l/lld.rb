05782350ec40e141e7fd5694328f24c8e2126469
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 01:30:09 +0000

    lld: update 23.1.1 bottle.

--- Formula/l/lld.rb
+++ Formula/l/lld.rb
@@ -13,7 +13,7 @@ class Lld < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "59eee32b60d0863257122a1925716253be8139f919402cedb7a4bb33b0c45fac"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3a8c0b2e1e360f162856417f80411538d7eca17ee39d489cdc0c10964fe9bd12"
   end
 
   # Not `:versioned_formula`: the fork auto-links versioned kegs on direct
