77b3ac29fb8a62984a33a3a5f13db64fd793ce37
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 01:13:06 +0000

    railway: update 5.49.6 bottle.

--- Formula/r/railway.rb
+++ Formula/r/railway.rb
@@ -7,7 +7,7 @@ class Railway < Formula
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7c360147bc092b47762b49b08fc961d4a4500773db01d6073f7c64506b2e8623"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "0e80ca720feb83c54fee4f069623f2d25ea552bf17c994625f4fd8ef3533e04f"
   end
 
   depends_on "rust" => :build
