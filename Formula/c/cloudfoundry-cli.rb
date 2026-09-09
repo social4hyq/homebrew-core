e6b2aa458bbf090e8edb6b9f5e4b38d329db4eb5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 01:25:21 +0000

    cloudfoundry-cli 8.19.0

--- Formula/c/cloudfoundry-cli.rb
+++ Formula/c/cloudfoundry-cli.rb
@@ -1,8 +1,8 @@
 class CloudfoundryCli < Formula
   desc "Official command-line client for Cloud Foundry"
   homepage "https://docs.cloudfoundry.org/cf-cli"
-  url "https://github.com/cloudfoundry/cli/archive/refs/tags/v8.18.4.tar.gz"
-  sha256 "1371bedf6c3929d018edb18608634888c0e7a89f6d27faad24ce8bde25e91106"
+  url "https://github.com/cloudfoundry/cli/archive/refs/tags/v8.19.0.tar.gz"
+  sha256 "bfbbb833c2727432e48d7b383ba749f6ca7e5c11c1377cc6cd22ddc802057d23"
   license "Apache-2.0"
   head "https://github.com/cloudfoundry/cli.git", branch: "main"
 
@@ -12,16 +12,20 @@ class CloudfoundryCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d10ba4b1a15e6433969059ead78b81b280a867822d831e1380029bc4e61dbb2b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6a89bbbc6fd61c7f5c1f791eab969b24325deefe1cfcce67d53251a3527593bd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6a89bbbc6fd61c7f5c1f791eab969b24325deefe1cfcce67d53251a3527593bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6a89bbbc6fd61c7f5c1f791eab969b24325deefe1cfcce67d53251a3527593bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7e4fd211e34340695a52e528d7444c5877b17785a84cd03ba0c766ca32f5b36e"
+    sha256 cellar: :any,                 x86_64_linux:  "5584469a2e82862f3b7dcec5f5b7f3ce282532c60a6ded55ff48e5617ca950ce"
   end
 
-  depends_on "go" => :build
+  # `SermoDigital/jose` registers `crypto.Hash(0)`, which Go 1.27 `RegisterHash` panics on
+  depends_on "go@1.26" => :build
 
   conflicts_with "cf", because: "both install `cf` binaries"
 
   def install
     ldflags = %W[
-      -s -w
       -X code.cloudfoundry.org/cli/v8/version.binaryVersion=#{version}
       -X code.cloudfoundry.org/cli/v8/version.binarySHA=#{tap.user}
       -X code.cloudfoundry.org/cli/v8/version.binaryBuildDate=#{time.iso8601}
