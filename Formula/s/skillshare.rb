b4656b1e542c33cb8ae7544b2142a31ae769e059
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 23:51:32 +0000

    skillshare 0.20.28

--- Formula/s/skillshare.rb
+++ Formula/s/skillshare.rb
@@ -1,8 +1,8 @@
 class Skillshare < Formula
   desc "Sync skills across AI CLI tools"
   homepage "https://skillshare.runkids.cc"
-  url "https://github.com/runkids/skillshare/archive/refs/tags/v0.20.27.tar.gz"
-  sha256 "8949741f312e019b8223103017b0efdef3e0b6a1485fd5d1b4ba5966308112d9"
+  url "https://github.com/runkids/skillshare/archive/refs/tags/v0.20.28.tar.gz"
+  sha256 "8cac0854f0cff8207af788b13f9a5fb69b8d93230c7b2d29ca8c1080d5807369"
   license "MIT"
   head "https://github.com/runkids/skillshare.git", branch: "main"
 
