dcfc7fa3586baf1ec7c122114da24b80aa8dca8f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 06:37:48 +0000

    starship: update 1.26.0_1 bottle.

--- Formula/s/starship.rb
+++ Formula/s/starship.rb
@@ -8,7 +8,7 @@ class Starship < Formula
   head "https://github.com/starship/starship.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "45cbf2ee3cf2b809979071044c0a20f441a138c16615436ec99b7c5d83727270"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "02835bc9e4ac978dec08444742efd2c6a0b9e6c0f5300e7663d21194e985e603"
   end
 
   depends_on "cmake" => :build
