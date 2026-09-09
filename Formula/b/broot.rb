25d6b5302b4c158dbda2dbd79bee4c6ac53d2e55
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 10:06:10 +0000

    broot 1.60.1

--- Formula/b/broot.rb
+++ Formula/b/broot.rb
@@ -1,13 +1,17 @@
 class Broot < Formula
   desc "New way to see and navigate directory trees"
   homepage "https://dystroy.org/broot/"
-  url "https://github.com/Canop/broot/archive/refs/tags/v1.59.0.tar.gz"
-  sha256 "61cb29922ef3953bae7f696b9f33fef51d85b5a4d85075c3612fcc6824663c37"
+  url "https://github.com/Canop/broot/archive/refs/tags/v1.60.1.tar.gz"
+  sha256 "23f6c5caed90400b4a7a277501c3c6fb46bacd4be5250da9ea357822e5a504ca"
   license "MIT"
   head "https://github.com/Canop/broot.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "dabebf494735a2cb605d4d08639914fbab5eb9c68c3fd3a11ace2dde1ca85e7f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4467571c47751245b74b6676f67d4d4a23106c0945cba9b88e0d6f492258cc19"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a3eaae5c0bdd3a8391757efe3a1b47fa77b576e50a65b2f7ebd8d54a62dac926"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5aec28fb13dbff56d359ad58e6e5d9856d6c71f611fe0c690524a6463653d7cf"
+    sha256 cellar: :any,                 arm64_linux:   "75e56a5a61195bf057527b8b19ee6b2f58353de44695de4b4e2d23402cf2b52f"
+    sha256 cellar: :any,                 x86_64_linux:  "3d9e7571b383b792fbfe08483d5c65e22b1c5777bdeea4ff5082008c2e8b8777"
   end
 
   depends_on "rust" => :build
@@ -47,18 +51,20 @@ class Broot < Formula
     assert_match "lets you explore file hierarchies with a tree-like view", output
     assert_match version.to_s, shell_output("#{bin}/broot --version")
 
+    (testpath/"conf.hjson").write "enable_kitty_keyboard: false\n"
+    (testpath/"test.txt").write "Homebrew\n"
+
     require "pty"
     require "io/console"
-    PTY.spawn(bin/"broot", "-c", ":print_tree", "--color", "no", "--outcmd", testpath/"output.txt") do |r, w, pid|
+    PTY.spawn(bin/"broot", "--conf", testpath/"conf.hjson", "-c", ":print_tree", "--color", "no") do |r, _w, pid|
       r.winsize = [20, 80] # broot dependency terminal requires width > 2
-      w.write "n\r\n"
       output = ""
       begin
         r.each { |line| output += line }
       rescue Errno::EIO
         # GNU/Linux raises EIO when read is done on closed pty
       end
-      assert_match "New Configuration files written in", output
+      assert_match "test.txt", output
       assert_predicate Process::Status.wait(pid), :success?
     end
   end
