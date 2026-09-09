95049893622faeb1b5f9d2c3c1362d8490af270c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 22:16:18 +0000

    chrome-devtools-mcp 1.9.0

--- Formula/c/chrome-devtools-mcp.rb
+++ Formula/c/chrome-devtools-mcp.rb
@@ -1,8 +1,8 @@
 class ChromeDevtoolsMcp < Formula
   desc "Chrome DevTools for coding agents"
   homepage "https://github.com/chromedevtools/chrome-devtools-mcp"
-  url "https://registry.npmjs.org/chrome-devtools-mcp/-/chrome-devtools-mcp-1.8.0.tgz"
-  sha256 "ac0334410cea75d11a5ebb555fbb0139a4a394de35d98cc2a3bc0a959f004cdd"
+  url "https://registry.npmjs.org/chrome-devtools-mcp/-/chrome-devtools-mcp-1.9.0.tgz"
+  sha256 "5f753b1cbf5775c8e437113522cd10c1f0cefd16521e0572540d57860556c133"
   license "Apache-2.0"
 
   bottle do
