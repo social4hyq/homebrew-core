30b0a80d8981e0d8c2dc2e0ed26448a853140451
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 23:49:03 +0000

    sentry-native 0.16.6

--- Formula/s/sentry-native.rb
+++ Formula/s/sentry-native.rb
@@ -1,8 +1,8 @@
 class SentryNative < Formula
   desc "Sentry SDK for C, C++ and native applications"
   homepage "https://docs.sentry.io/platforms/native/"
-  url "https://github.com/getsentry/sentry-native/archive/refs/tags/0.16.5.tar.gz"
-  sha256 "8d3f63f092ab24ab7f5d30cd8f0e80dc78670a3b3be3f1237948667907cdc3a4"
+  url "https://github.com/getsentry/sentry-native/archive/refs/tags/0.16.6.tar.gz"
+  sha256 "a194ac434da1534723556c5628256752208b0b6989fee7bf5ed6c5b3c84773ab"
   license "MIT"
 
   bottle do
