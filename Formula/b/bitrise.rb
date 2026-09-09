b2a9a4bbc9d1f84bb3153fdafc54c88fc0a6c324
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 22:11:31 +0000

    bitrise 2.43.4

--- Formula/b/bitrise.rb
+++ Formula/b/bitrise.rb
@@ -1,8 +1,8 @@
 class Bitrise < Formula
   desc "Command-line automation tool"
   homepage "https://github.com/bitrise-io/bitrise"
-  url "https://github.com/bitrise-io/bitrise/archive/refs/tags/v2.43.3.tar.gz"
-  sha256 "e58c06dea561b206f4bc0e7677a6e1d8baed6997765c3542f22dc73fbcfb8dd5"
+  url "https://github.com/bitrise-io/bitrise/archive/refs/tags/v2.43.4.tar.gz"
+  sha256 "5f1432c3c160878f56735d02f2f0c06c89fd0fa4223a75e90ec8aa71f390efca"
   license "MIT"
   head "https://github.com/bitrise-io/bitrise.git", branch: "master"
 
