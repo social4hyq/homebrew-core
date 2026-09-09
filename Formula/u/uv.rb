48736cb9615f5d55c8607b69d582867743301702
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 01:04:20 +0000

    uv: update 0.12.11 bottle.

--- Formula/u/uv.rb
+++ Formula/u/uv.rb
@@ -8,7 +8,7 @@ class Uv < Formula
   head "https://github.com/astral-sh/uv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "7d39ce4db35b94ca7a3731ef544a9dbeefd1eeb05e4f6dc555c23bda3562f903"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "a4f581666fe1adf5aeef6f42f9db2e850a5d545850e1a22bcb7b8b217c93d0e5"
   end
 
   depends_on "cmake" => :build
