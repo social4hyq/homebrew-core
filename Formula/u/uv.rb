dd99c43962ee98c60f7ca0f7544e4d74ebe93d43
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-09 00:04:28 +0000

    uv 0.12.11

--- Formula/u/uv.rb
+++ Formula/u/uv.rb
@@ -1,8 +1,8 @@
 class Uv < Formula
   desc "Extremely fast Python package installer and resolver, written in Rust"
   homepage "https://docs.astral.sh/uv/"
-  url "https://github.com/astral-sh/uv/archive/refs/tags/0.12.10.tar.gz"
-  sha256 "92f0f678f111cb34535fcdfffe98ecc75c98f1e34908cc1f80f4c213fbcb537c"
+  url "https://github.com/astral-sh/uv/archive/refs/tags/0.12.11.tar.gz"
+  sha256 "9f353551174941af14a56860d4365d281fb99967236ac84229a3fe57bdd5f504"
   license any_of: ["Apache-2.0", "MIT"]
   compatibility_version 1
   head "https://github.com/astral-sh/uv.git", branch: "main"
