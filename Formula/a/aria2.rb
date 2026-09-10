eb5ccff05b77222c1b79e27a4873cd26b01dcd2b
hqzing <hqzing@outlook.com>
2026-09-10 12:15:55 +0000

    aria2: rebuild

--- Formula/a/aria2.rb
+++ Formula/a/aria2.rb
@@ -4,7 +4,7 @@ class Aria2 < Formula
   url "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0.tar.xz"
   sha256 "60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "355058bbba8b199b460f2ff702ff4485ca73d59a978a88b0320e7be464bfb32a"
