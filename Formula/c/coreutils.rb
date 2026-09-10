b8c70e66e752d0481e323905973de288093913a9
hqzing <hqzing@outlook.com>
2026-09-10 12:09:39 +0000

    coreutils: rebuild

--- Formula/c/coreutils.rb
+++ Formula/c/coreutils.rb
@@ -5,7 +5,7 @@ class Coreutils < Formula
   mirror "https://ftp.gnu.org/gnu/coreutils/coreutils-9.11.tar.xz"
   sha256 "394024eda0a5955217ceda9cd1201e65dc8fa3aa29c2951135a49521d57c3cc3"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
   compatibility_version 1
 
   bottle do
