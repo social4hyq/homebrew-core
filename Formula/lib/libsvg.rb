91581b2c856f570e086d0b29e6253230d2ce1dd2
hqzing <hqzing@outlook.com>
2026-09-10 12:21:13 +0000

    libsvg: rebuild

--- Formula/lib/libsvg.rb
+++ Formula/lib/libsvg.rb
@@ -4,7 +4,7 @@ class Libsvg < Formula
   url "https://cairographics.org/snapshots/libsvg-0.1.4.tar.gz"
   sha256 "4c3bf9292e676a72b12338691be64d0f38cd7f2ea5e8b67fbbf45f1ed404bc8f"
   license "LGPL-2.1-or-later"
-  revision 3
+  revision 4
   compatibility_version 1
 
   livecheck do
