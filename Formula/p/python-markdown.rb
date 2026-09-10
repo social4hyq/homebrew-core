2263220a2e0f90ca5b829baf9532bf2e124eccdd
hqzing <hqzing@outlook.com>
2026-09-10 12:47:02 +0000

    python-markdown: rebuild

--- Formula/p/python-markdown.rb
+++ Formula/p/python-markdown.rb
@@ -6,6 +6,7 @@ class PythonMarkdown < Formula
   url "https://files.pythonhosted.org/packages/29/6f/da4c6aea59b3001f2e8c0ec7497475aadaf3b021c10cab5b2858f0f32b26/markdown-3.10.3.tar.gz"
   sha256 "3589362618f743188b4d955b874402bc814f4f83f544dc207719f4baa7d9c45f"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/Python-Markdown/markdown.git", branch: "master"
 
   bottle do
