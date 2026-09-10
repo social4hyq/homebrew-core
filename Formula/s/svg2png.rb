f0188c272a371fb7b3d86c6c6957709ed971ad38
hqzing <hqzing@outlook.com>
2026-09-10 14:55:05 +0000

    svg2png: rebuild

--- Formula/s/svg2png.rb
+++ Formula/s/svg2png.rb
@@ -4,7 +4,7 @@ class Svg2png < Formula
   url "https://cairographics.org/snapshots/svg2png-0.1.3.tar.gz"
   sha256 "e658fde141eb7ce981ad63d319339be5fa6d15e495d1315ee310079cbacae52b"
   license "LGPL-2.1-only"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://cairographics.org/snapshots/"
