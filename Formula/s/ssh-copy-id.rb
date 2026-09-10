ff254ac1342c0dd3b869f8d458fe44b9267721ef
hqzing <hqzing@outlook.com>
2026-09-10 08:55:51 +0000

    ssh-copy-id: rebuild

--- Formula/s/ssh-copy-id.rb
+++ Formula/s/ssh-copy-id.rb
@@ -6,6 +6,7 @@ class SshCopyId < Formula
   version "10.5p1"
   sha256 "d44d28a839ea9daf969cc69150fde59910b2b39361dad81a3bd6cbd19218db11"
   license "SSH-OpenSSH"
+  revision 1
   head "https://github.com/openssh/openssh-portable.git", branch: "master"
 
   livecheck do
