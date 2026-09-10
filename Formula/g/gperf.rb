a5ea916b1ff3a1fcedd06e9e132ad46e143f669a
hqzing <hqzing@outlook.com>
2026-09-10 08:49:21 +0000

    gperf: rebuild

--- Formula/g/gperf.rb
+++ Formula/g/gperf.rb
@@ -5,6 +5,7 @@ class Gperf < Formula
   mirror "https://ftp.gnu.org/gnu/gperf/gperf-3.3.tar.gz"
   sha256 "fd87e0aba7e43ae054837afd6cd4db03a3f2693deb3619085e6ed9d8d9604ad8"
   license "GPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "874090a3a960a5330a03c6f14560d6b7c5ea1870ffaff2c6e1fc8890dcc34c00"
