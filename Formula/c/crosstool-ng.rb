843db72fd14c17b4571a539d1c6650198329a093
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 18:32:32 +0000

    crosstool-ng: update 1.29.0_1 bottle.

--- Formula/c/crosstool-ng.rb
+++ Formula/c/crosstool-ng.rb
@@ -13,7 +13,7 @@ class CrosstoolNg < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "08b83f60c85f4bb5bc94f80fcf13404b8a9ba8f9510a3d9c1eceebd959db86ac"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f14c3b076ea7c5176954fedfd79d850a3a39af72ba5f13320fa59b2f873dd46c"
   end
 
   depends_on "help2man" => :build
