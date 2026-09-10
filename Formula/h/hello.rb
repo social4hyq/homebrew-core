d05da036f1e24928e27158f381f53c2c132b6bd7
hqzing <hqzing@outlook.com>
2026-09-10 18:01:37 +0800

    hello: rebuild

--- Formula/h/hello.rb
+++ Formula/h/hello.rb
@@ -1,9 +1,10 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftpmirror.gnu.org/gnu/hello/hello-2.12.3.tar.gz"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.12.3.tar.gz"
   sha256 "0d5f60154382fee10b114a1c34e785d8b1f492073ae2d3a6f7b147687b366aa0"
   license "GPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "895b21c57ce40a73ad02896b90ed7a4f6653fcdb5a804e8927e94a9823929af9"
