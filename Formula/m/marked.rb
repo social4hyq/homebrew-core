e3d2a5bed4e24fe41e55215532dff3f96242f887
hqzing <hqzing@outlook.com>
2026-09-10 14:32:33 +0000

    marked: rebuild

--- Formula/m/marked.rb
+++ Formula/m/marked.rb
@@ -4,6 +4,7 @@ class Marked < Formula
   url "https://registry.npmjs.org/marked/-/marked-18.0.12.tgz"
   sha256 "50f7134d0f110b3db6137ac269e6a45a79b3f8cf2b902c363f4c7dd5e2afece7"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "ac8f43424c1a1fcd8ab4fd89dc48bb92e42a1cfe86f997440e10f6672af59fb0"
