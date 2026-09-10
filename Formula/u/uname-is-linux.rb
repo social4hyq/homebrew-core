0108eb797fd753226b7a88bd86bb31c72dc86de4
hqzing <hqzing@outlook.com>
2026-09-10 08:51:26 +0000

    uname-is-linux: rebuild

--- Formula/u/uname-is-linux.rb
+++ Formula/u/uname-is-linux.rb
@@ -4,6 +4,7 @@ class UnameIsLinux < Formula
   url "https://atomgit.com/Harmonybrew/uname-is-linux/releases/download/v1.0.0/uname-is-linux-1.0.0.tar.gz"
   sha256 "e2adc9dedd9d15d45515a1bfce9969c92bb471ec8415ad6f85d292a2ce4fbd3d"
   license "BSD-2-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "4be05bee0fafd83372348487f3a0f4bb9ecb4945d2772572949fb034757c6c6b"
