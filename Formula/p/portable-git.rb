ae34d5cbfafb6bab208a6142bb29e993729e615a
hqzing <hqzing@outlook.com>
2026-09-10 11:53:25 +0000

    portable-git: rebuild

--- Formula/p/portable-git.rb
+++ Formula/p/portable-git.rb
@@ -6,6 +6,7 @@ class PortableGit < PortableFormula
   url "https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.55.0.tar.xz"
   sha256 "457fdb04dc8728e007d4688695e6912e6f680727920f2a40bf11eacc17505357"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/git/git.git", branch: "master"
 
   livecheck do
