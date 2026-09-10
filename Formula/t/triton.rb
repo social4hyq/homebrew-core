cc8024cb7ac1385f93f3acf8b658198bd5fb1c65
hqzing <hqzing@outlook.com>
2026-09-10 14:31:06 +0000

    triton: rebuild

--- Formula/t/triton.rb
+++ Formula/t/triton.rb
@@ -4,6 +4,7 @@ class Triton < Formula
   url "https://registry.npmjs.org/triton/-/triton-7.18.1.tgz"
   sha256 "36804146e9df26ac633524c7d800500f02d9c0ba6b76eb9bd4b5738ca34cbc09"
   license "MPL-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "b28e6b934dd3fb1e780831804568b2c703f5113cbe6be5b02604840fcd8b064b"
