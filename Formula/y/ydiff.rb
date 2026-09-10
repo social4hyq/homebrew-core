dc5529ff18ecbeb5a11be15e7478ec4dfdf6146e
hqzing <hqzing@outlook.com>
2026-09-10 12:45:17 +0000

    ydiff: rebuild

--- Formula/y/ydiff.rb
+++ Formula/y/ydiff.rb
@@ -6,6 +6,7 @@ class Ydiff < Formula
   url "https://files.pythonhosted.org/packages/25/08/93b7e744645d94ae2f5dcddbbd2cfd5ec1420768994987a21a10b226be86/ydiff-1.5.tar.gz"
   sha256 "dd5272afd7409902678c1ac6d1945d73cf85104ac6cda1e67684a67617918a20"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "5bb13d91b349f0bd2a9751e378b431fc56d97d172220fcbe4f6a7d35ec6e7190"
