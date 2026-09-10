a5865eda1b84b5a719a60544eb2b5e7690b61b74
hqzing <hqzing@outlook.com>
2026-09-10 13:57:30 +0000

    vercel-cli: rebuild

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -4,6 +4,7 @@ class VercelCli < Formula
   url "https://registry.npmjs.org/vercel/-/vercel-59.12.0.tgz"
   sha256 "ab22d674f49873c8475d0ec2dd5aeef6e3306abdbc1f9a296d89fec3a11d1b52"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "7c7d0024349003954a8d78bc99fe1ad12a510e6200fecd3fe353c4b262e7cdb5"
