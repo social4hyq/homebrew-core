ae61a48781b0a72b3f47a431323edb2e16fcc295
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 19:12:29 +0000

    rst-lint: update 2.0.2_1 bottle.

--- Formula/r/rst-lint.rb
+++ Formula/r/rst-lint.rb
@@ -9,7 +9,7 @@ class RstLint < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "28a113048af09222b76a6839db107c2311351386c5ec174c9cafe5ac549c17bd"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "47e7416ff942adc84ef6bcd795acc08e96144c4d59ea37a67599643ad761b806"
   end
 
   depends_on "python@3.14"
