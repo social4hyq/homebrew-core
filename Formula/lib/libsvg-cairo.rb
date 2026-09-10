8273159a22e2cf38cff7a4709d449d9a1d33163f
hqzing <hqzing@outlook.com>
2026-09-10 14:49:00 +0000

    libsvg-cairo: rebuild

--- Formula/lib/libsvg-cairo.rb
+++ Formula/lib/libsvg-cairo.rb
@@ -4,7 +4,7 @@ class LibsvgCairo < Formula
   url "https://cairographics.org/snapshots/libsvg-cairo-0.1.6.tar.gz"
   sha256 "a380be6a78ec2938100ce904363815a94068fca372c666b8cc82aa8711a0215c"
   license "LGPL-2.1-or-later"
-  revision 3
+  revision 4
 
   livecheck do
     url "https://cairographics.org/snapshots/"
