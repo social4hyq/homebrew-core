f48a2599f78e921291e0d10dbfdcdf6ecf3bc654
hqzing <hqzing@outlook.com>
2026-09-10 11:52:54 +0000

    zlib-ng-compat: rebuild

--- Formula/z/zlib-ng-compat.rb
+++ Formula/z/zlib-ng-compat.rb
@@ -6,7 +6,7 @@ class ZlibNgCompat < Formula
   mirror "http://fresh-center.net/linux/misc/legacy/zlib-ng-2.3.3.tar.gz"
   sha256 "f9c65aa9c852eb8255b636fd9f07ce1c406f061ec19a2e7d508b318ca0c907d1"
   license "Zlib"
-  revision 1
+  revision 2
   compatibility_version 1
   head "https://github.com/zlib-ng/zlib-ng.git", branch: "develop"
 
