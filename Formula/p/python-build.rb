c62e76984167e49d233439556a39fccbcf17ad12
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 18:40:29 +0000

    python-build: update 1.6.0_1 bottle.

--- Formula/p/python-build.rb
+++ Formula/p/python-build.rb
@@ -10,7 +10,7 @@ class PythonBuild < Formula
   head "https://github.com/pypa/build.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6bbbeb1b773a186f21ca8f074a9c2388bf817f728a6cf79f6fe9a26ea8ae585d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6f53ce06f60196f07639834cb60b8b372a80f8337edddcef7d20ac447bd28ba0"
   end
 
   depends_on "python@3.14"
