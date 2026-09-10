6a1c24fad87ad06c590bf2f2f5d9f9e82bb2a00f
hqzing <hqzing@outlook.com>
2026-09-10 13:33:20 +0000

    libimagequant: rebuild

--- Formula/lib/libimagequant.rb
+++ Formula/lib/libimagequant.rb
@@ -4,6 +4,7 @@ class Libimagequant < Formula
   url "https://github.com/ImageOptim/libimagequant/archive/refs/tags/4.4.1.tar.gz"
   sha256 "2464a3e922b5a220b633d674062b82f0670114f8f3dd30d1935a621c95965f1b"
   license all_of: ["GPL-3.0-or-later", "HPND"]
+  revision 1
   compatibility_version 1
 
   bottle do
