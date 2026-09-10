9d149ce92251873e1f66dddc1215748dd5d485c6
hqzing <hqzing@outlook.com>
2026-09-10 14:20:25 +0000

    claude-cmd: rebuild

--- Formula/c/claude-cmd.rb
+++ Formula/c/claude-cmd.rb
@@ -4,6 +4,7 @@ class ClaudeCmd < Formula
   url "https://registry.npmjs.org/claude-cmd/-/claude-cmd-1.1.1.tgz"
   sha256 "c6b990f7c55ec1281dca603b284d55b468ca7bbdfe217fc8091f5a8f85f16367"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "3c0f01a76d63f6111fc6c2d5aa63f24d9c6fb5f1b674cb3a936d1438f5f04a54"
