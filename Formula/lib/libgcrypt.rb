943ede50e0e6994bb7c6f4f801f82f33ebe7437b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 10:29:23 +0000

    libgcrypt: update 1.12.3_1 bottle.

--- Formula/lib/libgcrypt.rb
+++ Formula/lib/libgcrypt.rb
@@ -13,7 +13,7 @@ class Libgcrypt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b12725b1e4052779ecb29276abb0563b80d8fa7a78fcdf9c59d2b68669ec6e08"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "ef337bf253ca03804c80dffde1a0bf453e6f54fc889f59a6d5cdb3b7ba9109d6"
   end
 
   depends_on "libgpg-error"
