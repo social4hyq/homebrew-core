f0e0e431dee3eae7aa4e7633b9817412978fe69f
hqzing <hqzing@outlook.com>
2026-09-10 14:04:17 +0000

    cdk8s: rebuild

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -4,6 +4,7 @@ class Cdk8s < Formula
   url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.207.58.tgz"
   sha256 "144a5033259c73a6cfe658390c4b586bc93c77b6982334dc7071aabff60143bf"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "83c5050fed7941ca9fc637ca9b18c1c5d8857fa3f2fc5324a25f5b3100a17f63"
