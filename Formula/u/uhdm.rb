5d2e585f966a2cee45e5117d3adee7874f99ce87
hqzing <hqzing@outlook.com>
2026-09-10 12:39:19 +0000

    uhdm: rebuild

--- Formula/u/uhdm.rb
+++ Formula/u/uhdm.rb
@@ -6,6 +6,7 @@ class Uhdm < Formula
   url "https://github.com/chipsalliance/UHDM/archive/refs/tags/v1.87.tar.gz"
   sha256 "877b74bf1a0ad5fc64f46df9e2af47a088e50b362469a36c5da5f96dc3926045"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/chipsalliance/UHDM.git", branch: "master"
 
   bottle do
