0c698278558961de0ac20ca6a7f79c69b028ea80
hqzing <hqzing@outlook.com>
2026-09-10 14:55:49 +0000

    lsix: rebuild

--- Formula/l/lsix.rb
+++ Formula/l/lsix.rb
@@ -5,6 +5,7 @@ class Lsix < Formula
   sha256 "310e25389da13c19a0793adcea87f7bc9aa8acc92d9534407c8fbd5227a0e05d"
   # https://github.com/hackerb9/lsix/blob/1.9.1/lsix#L333-L341
   license any_of: ["GPL-3.0-or-later", "X11"]
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "6653a3c0b730afa45dc0278912871c668a59752ba1ff1a3b5d36aa7430f8fd2f"
