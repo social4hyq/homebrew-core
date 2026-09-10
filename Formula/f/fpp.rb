d94b65c88b7e5920e09ed2b6740e04fbb9701a2f
hqzing <hqzing@outlook.com>
2026-09-10 12:51:34 +0000

    fpp: rebuild

--- Formula/f/fpp.rb
+++ Formula/f/fpp.rb
@@ -4,6 +4,7 @@ class Fpp < Formula
   url "https://github.com/facebook/PathPicker/archive/refs/tags/0.9.5.tar.gz"
   sha256 "b0142676ed791085d619d9b3d28d28cab989ffc3b260016766841c70c97c2a52"
   license "MIT"
+  revision 1
   head "https://github.com/facebook/pathpicker.git", branch: "main"
 
   bottle do
