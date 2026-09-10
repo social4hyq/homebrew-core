6c26501b6df6f7253b3d5baa08f08f07b8849c7c
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 14:00:55 +0000

    just: update 1.58.0_1 bottle.

--- Formula/j/just.rb
+++ Formula/j/just.rb
@@ -13,7 +13,7 @@ class Just < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "359102bd59ad28904a29e9fb364b06475475c2d5373e08b314409aed9aae5f8b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "afd6db9811bc2b534c800b09e9c12c83eb07e8a81369323de45c8e32338c9123"
   end
 
   depends_on "rust" => :build
