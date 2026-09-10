405f8fd3c9ddb7a7e2114cf3a3afdc42a1b15005
hqzing <hqzing@outlook.com>
2026-09-10 10:23:04 +0000

    gosec: rebuild

--- Formula/g/gosec.rb
+++ Formula/g/gosec.rb
@@ -4,6 +4,7 @@ class Gosec < Formula
   url "https://github.com/securego/gosec/archive/refs/tags/v2.29.0.tar.gz"
   sha256 "083422c2d64f311062e7fe36ff1bd22c98b029f0a4d69f3e81fd0a4724139092"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/securego/gosec.git", branch: "master"
 
   bottle do
