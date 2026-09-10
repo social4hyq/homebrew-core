a388345138c18ece0f17496cf39f1c57a1631023
hqzing <hqzing@outlook.com>
2026-09-10 12:35:09 +0000

    certifi: rebuild

--- Formula/c/certifi.rb
+++ Formula/c/certifi.rb
@@ -4,6 +4,7 @@ class Certifi < Formula
   url "https://files.pythonhosted.org/packages/a3/c2/24167ea9858356b47a87a50d39908bfdb72ceeefe0041586e704e5376b3a/certifi-2026.7.22.tar.gz"
   sha256 "741e2c3b351ddf169a738da9f2c048608ff7f2c5cc02f1ebc6b118bb090d5d55"
   license "MPL-2.0"
+  revision 1
   compatibility_version 1
 
   bottle do
