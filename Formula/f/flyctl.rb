2096c1c06a52abc4ef4dc198dc85008e750492a5
hqzing <hqzing@outlook.com>
2026-09-10 10:20:29 +0000

    flyctl: rebuild

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -5,6 +5,7 @@ class Flyctl < Formula
       tag:      "v0.4.101",
       revision: "203d7369ecb26c9adecadb501cd95682decdb527"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
   # Upstream tags versions like `v0.1.92` and `v2023.9.8` but, as of writing,
