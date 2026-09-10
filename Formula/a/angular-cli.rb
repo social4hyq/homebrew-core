a28038186a48b77eafd73e4e1862739c815dd233
hqzing <hqzing@outlook.com>
2026-09-10 14:03:51 +0000

    angular-cli: rebuild

--- Formula/a/angular-cli.rb
+++ Formula/a/angular-cli.rb
@@ -4,6 +4,7 @@ class AngularCli < Formula
   url "https://registry.npmjs.org/@angular/cli/-/cli-22.1.7.tgz"
   sha256 "4275c6252fce9e0197570bca657fbda1658d453be3aecb99d7274add86a51c56"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "da8ca4c6f66e7ee3c9d6e417a909b5b47a910b65cff3e9e6babae8b8fb8f159f"
