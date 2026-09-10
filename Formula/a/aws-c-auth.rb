b2eb943758e3db20d39f55ad8106239b4f7fbf8e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 17:19:52 +0000

    aws-c-auth: update 1.0.0_1 bottle.

--- Formula/a/aws-c-auth.rb
+++ Formula/a/aws-c-auth.rb
@@ -8,7 +8,7 @@ class AwsCAuth < Formula
   compatibility_version 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "eab9c127ff79ef588852000c975ea630420a3a44384b2389b5dad415ced8e316"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d9e86bd31dd8bb988babdc27eef07a0e1a4b0738360cb27a10176c4bdfb18efd"
   end
 
   depends_on "cmake" => :build
