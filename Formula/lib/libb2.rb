ea9eb3e32f842b24e41cb798c40a9184a47c7955
hqzing <hqzing@outlook.com>
2026-09-10 08:53:03 +0000

    libb2: rebuild

--- Formula/lib/libb2.rb
+++ Formula/lib/libb2.rb
@@ -4,6 +4,7 @@ class Libb2 < Formula
   url "https://github.com/BLAKE2/libb2/releases/download/v0.98.1/libb2-0.98.1.tar.gz"
   sha256 "53626fddce753c454a3fea581cbbc7fe9bbcf0bc70416d48fdbbf5d87ef6c72e"
   license "CC0-1.0"
+  revision 1
   compatibility_version 1
 
   bottle do
