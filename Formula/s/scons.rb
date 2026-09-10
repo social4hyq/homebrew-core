077acfed4b7146979d6c787a4330c57a075b2626
hqzing <hqzing@outlook.com>
2026-09-10 12:35:58 +0000

    scons: rebuild

--- Formula/s/scons.rb
+++ Formula/s/scons.rb
@@ -6,6 +6,7 @@ class Scons < Formula
   url "https://files.pythonhosted.org/packages/42/b9/b7a5c88f348a0c34594d88100872c55fa1cae863ccb222c1c438341b5503/scons-4.11.1.tar.gz"
   sha256 "4210d1a80a62e986029208117991b6347ccaaaab37b67463a3ff31ee065dc487"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "f5631e1dc5278156ddc80c315a4c1a3b541126b30731829e4a3bd99357bac4a5"
