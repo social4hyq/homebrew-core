da76edad5563f25a7ffabc28f3b532c75c889f02
hqzing <hqzing@outlook.com>
2026-09-10 08:51:44 +0000

    fribidi: rebuild

--- Formula/f/fribidi.rb
+++ Formula/f/fribidi.rb
@@ -4,6 +4,7 @@ class Fribidi < Formula
   url "https://github.com/fribidi/fribidi/releases/download/v1.0.16/fribidi-1.0.16.tar.xz"
   sha256 "1b1cde5b235d40479e91be2f0e88a309e3214c8ab470ec8a2744d82a5a9ea05c"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "026b48c3b630648a47d9184e85d3c5cbf3a0bad1747859fc0e4b5aa114a5f4fe"
