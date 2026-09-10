bc11d02c9e3cd7b9568cd1868e21cd632e335988
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 19:35:46 +0000

    fpp: update 0.9.5_1 bottle.

--- Formula/f/fpp.rb
+++ Formula/f/fpp.rb
@@ -8,7 +8,7 @@ class Fpp < Formula
   head "https://github.com/facebook/pathpicker.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "be11f277186a7753cd834e34b9a1b28e9bbc054527c6abc2b72f836bdee925cb"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2b228e41c635a1f42d84124a9a2b12036c117082fe305e8e569b0c77a8974625"
   end
 
   uses_from_macos "python"
