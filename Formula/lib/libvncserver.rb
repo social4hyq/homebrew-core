de68bb8ae584373d584a3419efe5d02acffacea4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 15:40:19 +0000

    libvncserver: update 0.9.15_2 bottle.

--- Formula/lib/libvncserver.rb
+++ Formula/lib/libvncserver.rb
@@ -13,7 +13,7 @@ class Libvncserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e9844d6b1ea9ca39eb4baa2f1b144ccc3a63f583572bb04c86a81d765cf16a0e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "8be6cc2bbc25649ab7a54bd70c4aa8aa046c087451a67c9e79c533bb609c7ea4"
   end
 
   depends_on "cmake" => :build
