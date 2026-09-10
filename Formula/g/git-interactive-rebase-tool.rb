a06a9fd473ff5b4dbd400bccbb0594d373c5c26f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 17:08:06 +0000

    git-interactive-rebase-tool: update 2.4.1_5 bottle.

--- Formula/g/git-interactive-rebase-tool.rb
+++ Formula/g/git-interactive-rebase-tool.rb
@@ -12,7 +12,7 @@ class GitInteractiveRebaseTool < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "9c975fc100168c0fbed1dddbe1fb98695feabbcf4fb2fd57fee477792e1df674"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "aa3786cb6107800322a62038053d0efa482cc22d470143ee3d617fc413e1f56d"
   end
 
   depends_on "pkgconf" => :build
