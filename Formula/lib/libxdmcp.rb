54d9e23068fe4cedf968f4062a0f67c5f491dfe7
hqzing <hqzing@outlook.com>
2026-09-10 11:50:40 +0000

    libxdmcp: rebuild

--- Formula/lib/libxdmcp.rb
+++ Formula/lib/libxdmcp.rb
@@ -4,6 +4,7 @@ class Libxdmcp < Formula
   url "https://www.x.org/archive/individual/lib/libXdmcp-1.1.5.tar.xz"
   sha256 "d8a5222828c3adab70adf69a5583f1d32eb5ece04304f7f8392b6a353aa2228c"
   license "MIT"
+  revision 1
   compatibility_version 1
 
   bottle do
