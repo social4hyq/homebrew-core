f8c99975baae0d7333b8ee1af8f0e3a2a151cde3
hqzing <hqzing@outlook.com>
2026-09-10 14:34:33 +0000

    stylelint: rebuild

--- Formula/s/stylelint.rb
+++ Formula/s/stylelint.rb
@@ -4,6 +4,7 @@ class Stylelint < Formula
   url "https://registry.npmjs.org/stylelint/-/stylelint-17.15.0.tgz"
   sha256 "32b4a80fd409ae20392432f5ca87fc95f32e50cb1ced7ff10c00afe2b32bc285"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "cdb8729db7f7fe600db843135ce00f9f4e9fcb2b79d4d6a1b2ebdc895cbc9d7b"
