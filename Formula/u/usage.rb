599c2b88a128fc2bc704e670b386a0232b7714db
hqzing <hqzing@outlook.com>
2026-09-10 12:23:01 +0000

    usage: rebuild

--- Formula/u/usage.rb
+++ Formula/u/usage.rb
@@ -4,6 +4,7 @@ class Usage < Formula
   url "https://github.com/jdx/usage/archive/refs/tags/v6.8.0.tar.gz"
   sha256 "032842fa48ccfcd0c2ca854105c25184cf3ed17250f596f21c4448b0148100de"
   license "MIT"
+  revision 1
   compatibility_version 1
   head "https://github.com/jdx/usage.git", branch: "main"
 
