24802544c06ea0a52f79283649228ecef3d3f544
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 09:07:24 +0000

    icu4c@78: update 78.3_2 bottle.

--- Formula/i/icu4c@78.rb
+++ Formula/i/icu4c@78.rb
@@ -19,7 +19,7 @@ class Icu4cAT78 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2586e4f192c90480af64608337673ab19a0ac7cc76f472135f84cb5a1eada595"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "7c1ac7d25081bb1c69bd6732a1cbd1591a3432cd9ce70a0f094b0306a8106693"
   end
 
   keg_only :shadowed_by_macos, "macOS provides libicucore.dylib (but nothing else)"
