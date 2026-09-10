05869f7b2920bb7afee2718691d9f971d57c5c7a
hqzing <hqzing@outlook.com>
2026-09-10 12:34:56 +0000

    apr-util: rebuild

--- Formula/a/apr-util.rb
+++ Formula/a/apr-util.rb
@@ -5,6 +5,7 @@ class AprUtil < Formula
   mirror "https://archive.apache.org/dist/apr/apr-util-1.6.5.tar.bz2"
   sha256 "96de1dd6f6a0476d2d2e7964926d8c1ddc3bb0e210e1b1812d3ba5a454a392e2"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "d70d2890d9dfaff643ffd6d000e9bc9f5b3e704fffada74ba95f606744a44bfb"
