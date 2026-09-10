a9c775fbbaec704840a4895807e14de754711fc0
hqzing <hqzing@outlook.com>
2026-09-10 13:11:46 +0000

    httpd: rebuild

--- Formula/h/httpd.rb
+++ Formula/h/httpd.rb
@@ -5,7 +5,7 @@ class Httpd < Formula
   mirror "https://downloads.apache.org/httpd/httpd-2.4.68.tar.bz2"
   sha256 "68c74d4df38c26bed4dfbdb8f3baf1eb532f3872357becc1bba5d136f6b63c06"
   license "Apache-2.0"
-  revision 1
+  revision 2
   compatibility_version 1
 
   bottle do
