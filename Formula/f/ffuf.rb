bc757c1fc6e8181ad619737428df7990fe341d55
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 11:21:38 +0000

    ffuf: update 2.3.0_1 bottle.

--- Formula/f/ffuf.rb
+++ Formula/f/ffuf.rb
@@ -8,7 +8,7 @@ class Ffuf < Formula
   head "https://github.com/ffuf/ffuf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a0cbe66e1867a55900d492df381c597f607ca78be93196a15640b816350858c3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "3094e4bfae29a91588a31154cb1eda81fd82469b20e22be97f89ef9d8443ab3f"
   end
 
   depends_on "go" => :build
