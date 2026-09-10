4567d36a2174ad06d13a44aa74929a38438cc2bd
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 17:29:03 +0000

    rbenv-vars: update 1.2.0_2 bottle.

--- Formula/r/rbenv-vars.rb
+++ Formula/r/rbenv-vars.rb
@@ -8,7 +8,7 @@ class RbenvVars < Formula
   head "https://github.com/rbenv/rbenv-vars.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f62eb64606dd9e94e5272d31a9605201148fc647c5a5e2e6a8021c05af8b192a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fbb6008b357fd93e2f3c5861db0fd14012393017385022aa2b84061dfa068ff7"
   end
 
   depends_on "rbenv"
