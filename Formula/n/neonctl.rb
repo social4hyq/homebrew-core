edba47f801c4d7c4e6e7bc27ec24efe62e53087d
hqzing <hqzing@outlook.com>
2026-09-10 14:00:11 +0000

    neonctl: rebuild

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -4,6 +4,7 @@ class Neonctl < Formula
   url "https://registry.npmjs.org/neonctl/-/neonctl-4.14.3.tgz"
   sha256 "e23a2f94dfa3e2dbb37ce10647b8bdffc3db1fbe5fae31c47957702349ef04ce"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "d3a1967c30cf3e3e5142d7cd461817acfaed3f13ce3c67be406e2d193471e532"
