ac59ecb09c85201bd65d09f65d58fd2906531924
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 22:54:51 +0000

    packetbeat: update 9.5.3_1 bottle.

--- Formula/p/packetbeat.rb
+++ Formula/p/packetbeat.rb
@@ -9,7 +9,7 @@ class Packetbeat < Formula
   head "https://github.com/elastic/beats.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d3c3fbf1823df317231fee646a2784fce214a50543e794fa858c60bdabc429fc"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1fe22c046083afdc8baf2cf5977d0e50fa5a06cbacc12478ef840d09209c4de0"
   end
 
   depends_on "go" => :build
