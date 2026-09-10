5efb25c0200b55f705f48fd40e7fd0fc18917c78
hqzing <hqzing@outlook.com>
2026-09-10 12:50:48 +0000

    isort: rebuild

--- Formula/i/isort.rb
+++ Formula/i/isort.rb
@@ -6,6 +6,7 @@ class Isort < Formula
   url "https://files.pythonhosted.org/packages/e6/43/067e17bfa10b6486b408d5294105ac894149a9abb94b338568b1f53a73c9/isort-9.0.1.tar.gz"
   sha256 "ba23db109e3e93ef1999f7209a651214994cd807801addd16ac485982eb4edd7"
   license "MIT"
+  revision 1
   head "https://github.com/PyCQA/isort.git", branch: "main"
 
   bottle do
