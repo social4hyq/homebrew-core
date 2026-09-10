3fe0d1a2f4825181b79c0778eea9cb4682c99e5c
hqzing <hqzing@outlook.com>
2026-09-10 13:03:12 +0000

    pyvim: rebuild

--- Formula/p/pyvim.rb
+++ Formula/p/pyvim.rb
@@ -6,7 +6,7 @@ class Pyvim < Formula
   url "https://files.pythonhosted.org/packages/c3/31/04e144ec3a3a0303e3ef1ef9c6c1ec8a3b5ba9e88b98d21442d9152783c1/pyvim-3.0.3.tar.gz"
   sha256 "2a3506690f73a79dd02cdc45f872d3edf20a214d4c3666d12459e2ce5b644baa"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "eb346c5ab435b62041ee62b5befc35f32b3ede39738dd962f04932893f17ec63"
