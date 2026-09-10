8ce09dd143c5c8b540a82e879ce645e9326dc85c
hqzing <hqzing@outlook.com>
2026-09-10 08:48:11 +0000

    libffi: rebuild

--- Formula/lib/libffi.rb
+++ Formula/lib/libffi.rb
@@ -4,6 +4,7 @@ class Libffi < Formula
   url "https://github.com/libffi/libffi/releases/download/v3.8.0/libffi-3.8.0.tar.gz"
   sha256 "7da3e2d9a171eb0a038f592ecad3ff2bb2550f3496d87b3b29ad0cf4430c0db4"
   license "MIT"
+  revision 1
   compatibility_version 1
 
   livecheck do
