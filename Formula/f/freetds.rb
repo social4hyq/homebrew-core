770332782206e5d1b96fdd1f7f62b6e4b5ce954a
hqzing <hqzing@outlook.com>
2026-09-10 11:57:04 +0000

    freetds: rebuild

--- Formula/f/freetds.rb
+++ Formula/f/freetds.rb
@@ -4,6 +4,7 @@ class Freetds < Formula
   url "https://www.freetds.org/files/stable/freetds-1.5.19.tar.bz2"
   sha256 "0dc2df2fea9934e3a99e00d417f3d192e9897572f6aff3905bd48f2507d16dff"
   license "GPL-2.0-or-later"
+  revision 1
   compatibility_version 1
 
   livecheck do
