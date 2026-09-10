719f523a8e1ec6dcbd8bf9ba2c6d6754ed9e5cdb
hqzing <hqzing@outlook.com>
2026-09-10 12:39:45 +0000

    spirv-tools: rebuild

--- Formula/s/spirv-tools.rb
+++ Formula/s/spirv-tools.rb
@@ -4,6 +4,7 @@ class SpirvTools < Formula
   url "https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/tags/vulkan-sdk-1.4.357.0.tar.gz"
   sha256 "d31e7109b6ef3559067e53e520870eafed7c9534d00db9728814b6df03fa4a5e"
   license "Apache-2.0"
+  revision 1
   version_scheme 1
   compatibility_version 1
   head "https://github.com/KhronosGroup/SPIRV-Tools.git", branch: "main"
