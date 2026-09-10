1132cfa5b6553426701226bfb85841c081d2d578
hqzing <hqzing@outlook.com>
2026-09-10 08:51:53 +0000

    ohos-pip-autosign: rebuild

--- Formula/o/ohos-pip-autosign.rb
+++ Formula/o/ohos-pip-autosign.rb
@@ -4,7 +4,7 @@ class OhosPipAutosign < Formula
   url "https://atomgit.com/Harmonybrew/ohos-pip-autosign/releases/download/v1.0.0/ohos-pip-autosign-1.0.0.tar.gz"
   sha256 "c19145f6f52585746c0a75c2bdcd140c0ea732b904cce4e3a8d2a6265321381d"
   license "BSD-2-Clause"
-  revision 3
+  revision 4
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "86ca990078141ca3d2d005696747a865e95ca704fa4f328e1c5a2924c36d58f5"
