0c678af485ba15d33269177c209a8ccc6e8b802a
hqzing <hqzing@outlook.com>
2026-09-10 11:53:54 +0000

    cyrus-sasl: rebuild

--- Formula/c/cyrus-sasl.rb
+++ Formula/c/cyrus-sasl.rb
@@ -4,7 +4,7 @@ class CyrusSasl < Formula
   url "https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-2.1.28/cyrus-sasl-2.1.28.tar.gz"
   sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
   license "BSD-3-Clause-Attribution"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
