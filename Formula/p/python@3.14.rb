9ed50d00e631bc217e45935e3445c16710fb4a57
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 17:10:13 +0000

    python@3.14: update 3.14.7_7 bottle.

--- Formula/p/python@3.14.rb
+++ Formula/p/python@3.14.rb
@@ -13,7 +13,7 @@ class PythonAT314 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "6e8db7e03e70ce91e8568b5844ab196189c8c0aa6ccc7dd0565072d9621b5df1"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "635c8456e481c8187a31784455e4ed3194decb6966e31fe705fa912ebfd9a17b"
   end
 
   depends_on "pkgconf" => :build
