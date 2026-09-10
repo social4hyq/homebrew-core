d6d8f1ff039808066e9bf834503f58067af0fc97
hqzing <hqzing@outlook.com>
2026-09-10 14:00:34 +0000

    codeburn: rebuild

--- Formula/c/codeburn.rb
+++ Formula/c/codeburn.rb
@@ -4,6 +4,7 @@ class Codeburn < Formula
   url "https://registry.npmjs.org/codeburn/-/codeburn-0.9.24.tgz"
   sha256 "cd6a68a665c15916f4bb507b361d08ea4a0967821e0a9d594229d4009a4b034b"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "3d26fd551d5269b842af2d903a8468d1cca64bf16ff91747dc9c44b82c86428f"
