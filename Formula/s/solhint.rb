3a6742b97e76686426a2354389e2346ae5d8b62b
hqzing <hqzing@outlook.com>
2026-09-10 14:34:10 +0000

    solhint: rebuild

--- Formula/s/solhint.rb
+++ Formula/s/solhint.rb
@@ -4,6 +4,7 @@ class Solhint < Formula
   url "https://registry.npmjs.org/solhint/-/solhint-6.2.4.tgz"
   sha256 "c3060df2c802f344bfcdde6d5181aae393baa16f0fa5e29022a0168fc4d5baf6"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "6a51e546889973ddd77a9ab4d16c3a383912d77e43c0696e3490916690d76413"
