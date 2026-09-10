933cd647eedfa61f1514355a21381c4ec51f32e0
hqzing <hqzing@outlook.com>
2026-09-10 12:44:13 +0000

    gixy: rebuild

--- Formula/g/gixy.rb
+++ Formula/g/gixy.rb
@@ -6,6 +6,7 @@ class Gixy < Formula
   url "https://files.pythonhosted.org/packages/63/ea/0a3a38b9dfb53cf18efe3fea869f5360e81b37299fe5e86855afd88b8008/gixy_ng-0.2.53.tar.gz"
   sha256 "6dcc4175f48dd1edcc7a2a1def647f2689a465a511361b77b6f00c242f17c86a"
   license "MPL-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "aa40be12812e3a033c5ab3ed473f107329a7ff6e706213ac24259e0656bffb76"
