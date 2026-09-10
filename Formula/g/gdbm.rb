ce49fa8354d0d86ec13519e3ebb0d768c4e764a4
hqzing <hqzing@outlook.com>
2026-09-10 08:49:01 +0000

    gdbm: rebuild

--- Formula/g/gdbm.rb
+++ Formula/g/gdbm.rb
@@ -5,6 +5,7 @@ class Gdbm < Formula
   mirror "https://ftp.gnu.org/gnu/gdbm/gdbm-1.26.tar.gz"
   sha256 "6a24504a14de4a744103dcb936be976df6fbe88ccff26065e54c1c47946f4a5e"
   license "GPL-3.0-or-later"
+  revision 1
   compatibility_version 1
 
   bottle do
