6227cb47c13858db971a23e73885f98ce2e25b86
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 14:42:06 +0000

    nginx: update 1.31.5_1 bottle.

--- Formula/n/nginx.rb
+++ Formula/n/nginx.rb
@@ -16,7 +16,7 @@ class Nginx < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d6a6f2040ca1b4dca6e6aeb6bafaf94bf16862d14e06d72cbd8034e16a9d395b"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "59c26644e8993b20f37bbe9748c7d508d7230b2e6bac38123c13ff54bf7cecfc"
   end
 
   depends_on "openssl@3"
