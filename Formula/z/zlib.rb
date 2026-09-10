1e9f87c7da10ba6d894389ae091d51641c80ffd5
hqzing <hqzing@outlook.com>
2026-09-10 08:50:32 +0000

    zlib: rebuild

--- Formula/z/zlib.rb
+++ Formula/z/zlib.rb
@@ -7,6 +7,7 @@ class Zlib < Formula
   mirror "http://fresh-center.net/linux/misc/legacy/zlib-1.3.2.tar.gz"
   sha256 "bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16"
   license "Zlib"
+  revision 1
   head "https://github.com/madler/zlib.git", branch: "develop"
 
   livecheck do
