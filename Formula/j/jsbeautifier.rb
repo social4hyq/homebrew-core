4d57d8c193868f5b54acdfb8c339f29a9aadcb08
hqzing <hqzing@outlook.com>
2026-09-10 12:52:05 +0000

    jsbeautifier: rebuild

--- Formula/j/jsbeautifier.rb
+++ Formula/j/jsbeautifier.rb
@@ -6,6 +6,7 @@ class Jsbeautifier < Formula
   url "https://files.pythonhosted.org/packages/2e/81/e0e11e305caa89831a0c8e555638d588c28b426d1105e734e113b00efd5d/jsbeautifier-2.0.3.tar.gz"
   sha256 "9579d4e9dbaa00383f3efdff4c98c8140bb85ba319398e8b97cdaba27abd6ba3"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "bcf82096415d748f3f144362edf0e9cb78bfa74341977a2419a9d4a4dd96e006"
