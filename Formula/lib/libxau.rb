13b31308d30255855796f399633b564e5e8ec48c
hqzing <hqzing@outlook.com>
2026-09-10 11:50:49 +0000

    libxau: rebuild

--- Formula/lib/libxau.rb
+++ Formula/lib/libxau.rb
@@ -4,6 +4,7 @@ class Libxau < Formula
   url "https://www.x.org/archive/individual/lib/libXau-1.0.12.tar.xz"
   sha256 "74d0e4dfa3d39ad8939e99bda37f5967aba528211076828464d2777d477fc0fb"
   license "MIT"
+  revision 1
   compatibility_version 1
 
   bottle do
