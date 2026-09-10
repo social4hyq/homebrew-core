24ef3ebd65d46c2ffd81dec5b44d731257a01f95
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 16:23:01 +0000

    cln: update 1.3.7_1 bottle.

--- Formula/c/cln.rb
+++ Formula/c/cln.rb
@@ -12,7 +12,7 @@ class Cln < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b11416aca186dcd8fe95aa57a45a35dce51aebce5c8b60ca6eb7f402de7a7e79"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "5f6510b6f749c72fe1cea6ac9e843ef1c8fbb5e68db4dd66893de7768e278926"
   end
 
   head do
