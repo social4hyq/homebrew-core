78dc0516d2d6e59fb7a693d180154af863b63a0a
hqzing <hqzing@outlook.com>
2026-09-10 14:07:31 +0000

    gnutls: rebuild

--- Formula/g/gnutls.rb
+++ Formula/g/gnutls.rb
@@ -5,7 +5,7 @@ class Gnutls < Formula
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.8/gnutls-3.8.13.tar.xz"
   sha256 "ffed8ec1bf09c2426d4f14aae377de4753b53e537d685e604e99a8b16ca9c97e"
   license all_of: ["LGPL-2.1-or-later", "GPL-3.0-only"]
-  revision 2
+  revision 3
   compatibility_version 2
 
   # The download page links to the directory listing pages for the "Next" and
