5004cbe78c9614b42e54aa5120bb2668ae1f051c
hqzing <hqzing@outlook.com>
2026-09-10 12:55:45 +0000

    python-tabulate: rebuild

--- Formula/p/python-tabulate.rb
+++ Formula/p/python-tabulate.rb
@@ -6,6 +6,7 @@ class PythonTabulate < Formula
   url "https://files.pythonhosted.org/packages/46/58/8c37dea7bbf769b20d58e7ace7e5edfe65b849442b00ffcdd56be88697c6/tabulate-0.10.0.tar.gz"
   sha256 "e2cfde8f79420f6deeffdeda9aaec3b6bc5abce947655d17ac662b126e48a60d"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "e48558048b9e50fae9f53b804916b345e5af491f478d5494235b5f329bd385c0"
