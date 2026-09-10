75e0baaf34936cbb085978f0bf476c4b98be5d38
hqzing <hqzing@outlook.com>
2026-09-10 10:17:52 +0000

    openssl@4: rebuild

--- Formula/o/openssl@4.rb
+++ Formula/o/openssl@4.rb
@@ -4,6 +4,7 @@ class OpensslAT4 < Formula
   url "https://github.com/openssl/openssl/releases/download/openssl-4.0.2/openssl-4.0.2.tar.gz"
   sha256 "736b467530f916737b7031310ccb21d8218c6229e61e8e160cd1d3458cd543a8"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://openssl-library.org/source/"
