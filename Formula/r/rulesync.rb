88aaadef38736fea26ac9d4c973d6edfa74c20fb
hqzing <hqzing@outlook.com>
2026-09-10 14:01:29 +0000

    rulesync: rebuild

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -4,6 +4,7 @@ class Rulesync < Formula
   url "https://registry.npmjs.org/rulesync/-/rulesync-16.26.0.tgz"
   sha256 "fd6fff0498f19d013919744f030ec689dfb997de3669b98e027843768827ef39"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "47f9a2b3d5686908415fb60369f55cac860819b08ea01a877b26a1142cb47d16"
