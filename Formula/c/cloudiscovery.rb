a1bef080c696db5fdd32f7eb28d071ecf2bf288a
hqzing <hqzing@outlook.com>
2026-09-10 12:53:09 +0000

    cloudiscovery: rebuild

--- Formula/c/cloudiscovery.rb
+++ Formula/c/cloudiscovery.rb
@@ -6,7 +6,7 @@ class Cloudiscovery < Formula
   url "https://files.pythonhosted.org/packages/d3/c2/9a5f93ac5376f83903c8550bde45e2888da3fb092b63e02e19d6c852134c/cloudiscovery-2.4.4.tar.gz"
   sha256 "1170ea352a3c7d5643652ebe96b068482734cd995b9c92dc820206f1b87407e5"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "9fddb8bd12641201cd74a83e8f8662bf12e6aaa515296ca201146eb925d0c366"
