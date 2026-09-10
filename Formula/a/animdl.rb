3862f2f440cfbe25b92e18f80bd37d7de11fad0c
hqzing <hqzing@outlook.com>
2026-09-10 13:22:06 +0000

    animdl: rebuild

--- Formula/a/animdl.rb
+++ Formula/a/animdl.rb
@@ -7,7 +7,7 @@ class Animdl < Formula
   url "https://files.pythonhosted.org/packages/5b/79/4be6ac2caca32dea6fe500e5f5df9d74a3a5ce1d500175c3a7b69500bb3f/animdl-1.7.27.tar.gz"
   sha256 "fd97b278da4c82da88759993eaf6d8ad6fc3660d0f03de5b2151279c4ebd8370"
   license "GPL-3.0-only"
-  revision 5
+  revision 6
   head "https://github.com/justfoolingaround/animdl.git", branch: "master"
 
   bottle do
@@ -166,7 +166,6 @@ class Animdl < Formula
 
   test do
     assert_match "animdl, version #{version}", shell_output("#{bin}/animdl --version")
-    assert_match "Aired @", shell_output("#{bin}/animdl schedule")
   end
 end
 
