a201bf1af052f26b2e2128560051a2143017f49c
hqzing <hqzing@outlook.com>
2026-09-10 08:38:11 +0000

    bzip2: rebuild

--- Formula/b/bzip2.rb
+++ Formula/b/bzip2.rb
@@ -5,6 +5,7 @@ class Bzip2 < Formula
   mirror "https://mirrors.kernel.org/sourceware/bzip2/bzip2-1.0.8.tar.gz"
   sha256 "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
   license "bzip2-1.0.6"
+  revision 1
 
   livecheck do
     url "https://sourceware.org/pub/bzip2/"
