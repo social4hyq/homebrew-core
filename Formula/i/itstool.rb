2409a5b73d5b7fd2f2c79bf4e735de6a35de1720
hqzing <hqzing@outlook.com>
2026-09-10 13:16:06 +0000

    itstool: rebuild

--- Formula/i/itstool.rb
+++ Formula/i/itstool.rb
@@ -4,7 +4,7 @@ class Itstool < Formula
   url "https://files.itstool.org/itstool/itstool-2.0.7.tar.bz2"
   sha256 "6b9a7cd29a12bb95598f5750e8763cee78836a1a207f85b74d8b3275b27e87ca"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://itstool.org/download.html"
