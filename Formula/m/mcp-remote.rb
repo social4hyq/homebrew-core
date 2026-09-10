bafb77faeaba6f19892e09ce7ce16a6a4ebe01ef
hqzing <hqzing@outlook.com>
2026-09-10 14:00:52 +0000

    mcp-remote: rebuild

--- Formula/m/mcp-remote.rb
+++ Formula/m/mcp-remote.rb
@@ -4,6 +4,7 @@ class McpRemote < Formula
   url "https://registry.npmjs.org/mcp-remote/-/mcp-remote-0.8.5.tgz"
   sha256 "578f3a20ef3aa75d09b94cf582208d9dbf5e1c355aee41f0ea021bde627ec255"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "7846f108417d4abe019dda905232759d53a48e2fefb672bf94cf3f11e0166a5a"
