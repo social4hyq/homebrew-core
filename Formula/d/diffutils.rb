045ffe4d7bd8b4d4ac145b0c29527cedca9881bb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 09:03:43 +0000

    diffutils: update 3.12_1 bottle.

--- Formula/d/diffutils.rb
+++ Formula/d/diffutils.rb
@@ -9,8 +9,7 @@ class Diffutils < Formula
   compatibility_version 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "93b337ca1a957759292be29593277121350b0bf313207fa8d587a1365564a7f2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cced0000899dc3034c1b9b334056aafdc0ce9fb5abd05d3a7c1ac43f988bd2c6"
   end
 
   def install
