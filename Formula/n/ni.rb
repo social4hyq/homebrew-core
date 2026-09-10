f770fb98fd041f6c9e9db4175e4348bf59f17024
hqzing <hqzing@outlook.com>
2026-09-10 14:32:54 +0000

    ni: rebuild

--- Formula/n/ni.rb
+++ Formula/n/ni.rb
@@ -4,6 +4,7 @@ class Ni < Formula
   url "https://registry.npmjs.org/@antfu/ni/-/ni-30.5.0.tgz"
   sha256 "c47d4254ab6af0de9f0e48818d0c8e37cca8e73affb7f000252661aefff4dcda"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "36e5e3a12289e4a0791aabba9e336577e9dec79fe5c07bbc94fcfd0ac910a105"
