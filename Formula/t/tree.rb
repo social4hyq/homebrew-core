d528cc55e94d8dc841bb46d607e4592d410bd969
hqzing <hqzing@outlook.com>
2026-09-10 08:55:07 +0000

    tree: rebuild

--- Formula/t/tree.rb
+++ Formula/t/tree.rb
@@ -4,6 +4,7 @@ class Tree < Formula
   url "https://github.com/Old-Man-Programmer/tree/archive/refs/tags/2.3.2.tar.gz"
   sha256 "22cf32e84e3eb508d97a9e991c2c3cc006b9dcf4afed201d96311c5c57d08fcf"
   license "GPL-2.0-or-later"
+  revision 1
   compatibility_version 1
 
   bottle do
