8dd670a043e23d2fd6206f7c3cf821e78291bb3d
hqzing <hqzing@outlook.com>
2026-09-10 12:29:15 +0000

    devel-base: rebuild

--- Formula/d/devel-base.rb
+++ Formula/d/devel-base.rb
@@ -4,6 +4,7 @@ class DevelBase < Formula
   url "https://atomgit.com/Harmonybrew/homebrew-core.git", revision: "a8784ea451ad819d27a548411b8853fca6de3124"
   version "1.0.1"
   license "BSD-2-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "357fb37802bfafda1bf719168301f3a8213ae531b024678e7e48d0e191e3dbea"
