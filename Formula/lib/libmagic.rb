e6ebb079d77a6bb989fde4a4f13804f266535446
hqzing <hqzing@outlook.com>
2026-09-10 12:03:40 +0000

    libmagic: rebuild

--- Formula/lib/libmagic.rb
+++ Formula/lib/libmagic.rb
@@ -4,6 +4,7 @@ class Libmagic < Formula
   url "https://astron.com/pub/file/file-5.48.tar.gz"
   sha256 "ed14656883b23a364b4057c05595d93252da9bc473d30106519519d0da141283"
   license all_of: ["BSD-2-Clause-Darwin", "BSD-2-Clause", :public_domain]
+  revision 1
   compatibility_version 1
 
   livecheck do
