8fc242a139de7c6a6d21c41ea29c0a08c57d5ec2
hqzing <hqzing@outlook.com>
2026-09-10 12:15:43 +0000

    nginx: rebuild

--- Formula/n/nginx.rb
+++ Formula/n/nginx.rb
@@ -6,6 +6,7 @@ class Nginx < Formula
   url "https://nginx.org/download/nginx-1.31.5.tar.gz"
   sha256 "e951607d534836624bd36b6b45a71dbfb055237deae3738da6bbf3270dada279"
   license "BSD-2-Clause"
+  revision 1
   compatibility_version 6
   head "https://github.com/nginx/nginx.git", branch: "master"
 
