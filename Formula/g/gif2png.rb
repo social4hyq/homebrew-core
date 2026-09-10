072f7b6de0fa5498d43a59dfd4eff864397d8a97
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 21:00:10 +0000

    gif2png: update 3.0.5_1 bottle.

--- Formula/g/gif2png.rb
+++ Formula/g/gif2png.rb
@@ -8,7 +8,7 @@ class Gif2png < Formula
   head "https://gitlab.com/esr/gif2png.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f772f55c5cedf41784f71036a0390c2be7b61c69e90152a354a4020433984335"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "fa10e6ae5f299fcd7fcf02ab18bcf98f41b7a3c7cc3e87dd00687b37c22b1f44"
   end
 
   depends_on "asciidoctor" => :build
