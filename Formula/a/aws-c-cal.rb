c13e94321bb81cbdf94dbad84723550e81d3f6f3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 13:04:09 +0000

    aws-c-cal: update 1.0.0_1 bottle.

--- Formula/a/aws-c-cal.rb
+++ Formula/a/aws-c-cal.rb
@@ -8,7 +8,7 @@ class AwsCCal < Formula
   compatibility_version 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "c50f34d84ce7c3ba1e63a005fd50bcac0d01824884c7e4e0d181267a3e5b457c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "98959f32850a23883a03e123a0e58dc000dc90d7dd38699c2eaa2ca11fc56402"
   end
 
   depends_on "cmake" => :build
