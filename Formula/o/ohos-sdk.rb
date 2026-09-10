30145baa9563fda8b2204d29b05614ab60f1725d
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 07:03:53 +0000

    ohos-sdk: update 26.0.0.18_1 bottle.

--- Formula/o/ohos-sdk.rb
+++ Formula/o/ohos-sdk.rb
@@ -8,7 +8,8 @@ class OhosSdk < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4a38b192a13e6de838c15ba72441feb24f9f79ec89525d23a4455d93bc980c14"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "6b9542812c4ecee08ad310e3bd48ead6b0bc2cbdbabf6863d89472c566b51439"
   end
 
   depends_on "unzip" => :build
