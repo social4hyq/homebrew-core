83b5f3f4acc692d4a485db7550355048e898a20e
hqzing <hqzing@outlook.com>
2026-09-10 08:54:38 +0000

    mecab: rebuild

--- Formula/m/mecab.rb
+++ Formula/m/mecab.rb
@@ -5,6 +5,7 @@ class Mecab < Formula
   url "https://deb.debian.org/debian/pool/main/m/mecab/mecab_0.996.orig.tar.gz"
   sha256 "e073325783135b72e666145c781bb48fada583d5224fb2490fb6c1403ba69c59"
   license any_of: ["GPL-2.0-only", "LGPL-2.1-only", "BSD-3-Clause"]
+  revision 1
 
   livecheck do
     url :homepage
