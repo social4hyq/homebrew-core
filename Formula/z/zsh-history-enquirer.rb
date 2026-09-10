fa69660958148b466f82e6928a0da693de72321b
hqzing <hqzing@outlook.com>
2026-09-10 14:20:04 +0000

    zsh-history-enquirer: rebuild

--- Formula/z/zsh-history-enquirer.rb
+++ Formula/z/zsh-history-enquirer.rb
@@ -4,6 +4,7 @@ class ZshHistoryEnquirer < Formula
   url "https://registry.npmjs.org/zsh-history-enquirer/-/zsh-history-enquirer-1.3.2.tgz"
   sha256 "eb5fc111b9122974e086f625ba6b46accb8e06ee7cad9519b0f2daec098f7ff1"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "77d56624e35975a79d09d4dc2206cb26970d60096cb38b2b6303eeb31b0075fd"
