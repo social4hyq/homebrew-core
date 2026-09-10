a4fee11d9060deba8262356caa1e290103d1c78e
hqzing <hqzing@outlook.com>
2026-09-10 13:17:28 +0000

    librdkafka: rebuild

--- Formula/lib/librdkafka.rb
+++ Formula/lib/librdkafka.rb
@@ -4,6 +4,7 @@ class Librdkafka < Formula
   url "https://github.com/confluentinc/librdkafka/archive/refs/tags/v2.15.1.tar.gz"
   sha256 "23c8575c7d1ced07246cb9cf200c11325b72201fd4134a02414ca869fbdd8ed3"
   license "BSD-2-Clause"
+  revision 1
   compatibility_version 1
   head "https://github.com/confluentinc/librdkafka.git", branch: "master"
 
