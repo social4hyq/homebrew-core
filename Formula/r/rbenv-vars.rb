216d63566be0c7c16f51a22ec705cfeb8ffc96cf
hqzing <hqzing@outlook.com>
2026-09-10 12:33:00 +0000

    rbenv-vars: rebuild

--- Formula/r/rbenv-vars.rb
+++ Formula/r/rbenv-vars.rb
@@ -4,7 +4,7 @@ class RbenvVars < Formula
   url "https://github.com/rbenv/rbenv-vars/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "9e6a5726aad13d739456d887a43c220ba9198e672b32536d41e884c0a54b4ddb"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/rbenv/rbenv-vars.git", branch: "master"
 
   bottle do
