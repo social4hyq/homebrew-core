aea7449dd1456502b2078c9ea014aa8ea736a1ba
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 12:28:19 +0000

    ollama: update 0.33.3_1 bottle.

--- Formula/o/ollama.rb
+++ Formula/o/ollama.rb
@@ -17,7 +17,7 @@ class Ollama < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "2333970ad1c1a4f3a730a65ab910198ab442ede1eb4121333489178328fb18f6"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "60ba9485b0ea50f002476bb9d110837743bb87be33fcde099a4f285581018ff7"
   end
 
   depends_on "cmake" => :build
