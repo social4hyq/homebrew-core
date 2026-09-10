971db57c11cc4ae2399471913c13df5faa36e610
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 13:02:27 +0000

    libevent: update 2.1.13_1 bottle.

--- Formula/lib/libevent.rb
+++ Formula/lib/libevent.rb
@@ -13,7 +13,7 @@ class Libevent < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8f30a9b84ea55aef0c3bbb2300920ead95b6888504bc6bd00ff45a056e5e8542"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1c852ac76e619c08f9e6ff79338e10c2a08bf8c3a9db4eb3658ab84941462dfd"
   end
 
   depends_on "autoconf" => :build
