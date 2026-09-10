d9e75ee0a09905deef76eaee5d82c0829a790998
hqzing <hqzing@outlook.com>
2026-09-10 12:02:16 +0000

    libiconv: rebuild

--- Formula/lib/libiconv.rb
+++ Formula/lib/libiconv.rb
@@ -5,6 +5,7 @@ class Libiconv < Formula
   mirror "https://ftp.gnu.org/gnu/libiconv/libiconv-1.19.tar.gz"
   sha256 "88dd96a8c0464eca144fc791ae60cd31cd8ee78321e67397e25fc095c4a19aa6"
   license all_of: ["GPL-3.0-or-later", "LGPL-2.0-or-later"]
+  revision 1
   compatibility_version 1
 
   bottle do
