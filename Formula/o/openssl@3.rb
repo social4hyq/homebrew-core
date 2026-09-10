d03a9c7b3dcf90f23e2bd19b64ee24bbb91bfc39
hqzing <hqzing@outlook.com>
2026-09-10 10:14:39 +0000

    openssl@3: rebuild

--- Formula/o/openssl@3.rb
+++ Formula/o/openssl@3.rb
@@ -5,6 +5,7 @@ class OpensslAT3 < Formula
   mirror "http://fresh-center.net/linux/misc/openssl-3.6.4.tar.gz"
   sha256 "9bffaa1ad1e07b354c21bd3324ec02fa15579f45a7d0494b3e74bc449b7333ef"
   license "Apache-2.0"
+  revision 1
   compatibility_version 1
 
   livecheck do
