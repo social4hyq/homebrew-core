7055cbe1b6bac550f09cbdf0f3e9b9800a306c09
hqzing <hqzing@outlook.com>
2026-09-10 13:58:51 +0000

    redocly-cli: rebuild

--- Formula/r/redocly-cli.rb
+++ Formula/r/redocly-cli.rb
@@ -4,6 +4,7 @@ class RedoclyCli < Formula
   url "https://registry.npmjs.org/@redocly/cli/-/cli-2.51.2.tgz"
   sha256 "4d7b8a92f66020815a3e57820feab7c8e2caa473bcc92e65139fe9f4d77afb24"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "280090f7da60d5752d9207657e917033222d1ba092a5fef733023ddc6377c925"
