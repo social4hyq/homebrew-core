96c91d1a9186375c1119885919b37e10a0bcf996
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-10 09:13:21 +0000

    libsodium: update 1.0.22_1 bottle.

--- Formula/lib/libsodium.rb
+++ Formula/lib/libsodium.rb
@@ -13,7 +13,7 @@ class Libsodium < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "0f31dda7cd1b800ec90ca599282f9092f61eb04898a553fb940649e0fef189a2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "91578ccc2b8ed232ef7287d3f6a0dc5b529ff96f439553e7074370b58642c61f"
   end
 
   head do
