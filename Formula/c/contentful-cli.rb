4b0121cc1bce21b78cdf1bbee5c65b5837fe3bb2
hqzing <hqzing@outlook.com>
2026-09-10 14:12:50 +0000

    contentful-cli: rebuild

--- Formula/c/contentful-cli.rb
+++ Formula/c/contentful-cli.rb
@@ -4,6 +4,7 @@ class ContentfulCli < Formula
   url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-4.0.10.tgz"
   sha256 "5943c8645e5b1edd2cc2599cddf19cb1097a4230373ef67c71764ece042712b8"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "fb35bae1423348e15b2c4a1c18221347fee5175747304fa7829889389d6b1740"
