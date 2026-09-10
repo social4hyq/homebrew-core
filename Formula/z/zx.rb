4256778510c272b5d26f27954870e93be841683e
hqzing <hqzing@outlook.com>
2026-09-10 14:34:57 +0000

    zx: rebuild

--- Formula/z/zx.rb
+++ Formula/z/zx.rb
@@ -4,6 +4,7 @@ class Zx < Formula
   url "https://registry.npmjs.org/zx/-/zx-8.8.5.tgz"
   sha256 "20df3c1b6160372f02bd26176898c9b791690af4599573c99a04b2624a6aaab9"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "79812604c680162fe7faf86ab99f7b6f7cc70d8d5e924bb0518f2f454741dc01"
