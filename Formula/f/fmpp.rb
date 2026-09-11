8dc1263a44cec26e25a86f45fe90923a18d94341
hqzing <hqzing@outlook.com>
2026-09-11 01:57:42 +0000

    fmpp: rebuild

--- Formula/f/fmpp.rb
+++ Formula/f/fmpp.rb
@@ -4,7 +4,7 @@ class Fmpp < Formula
   url "https://downloads.sourceforge.net/project/fmpp/fmpp/0.9.16/fmpp_0.9.16.tar.gz"
   sha256 "86561e3f3a2ccb436f5f3df88d79a7dad72549a33191901f49d12a38b53759cd"
   license "Apache-2.0"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "bc400076e04b75b7a7e370b0181b67b750350d88eff7d4a52c02943b3aefaf0c"
