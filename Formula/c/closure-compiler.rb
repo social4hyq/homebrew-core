dc6eabdde99b950c754b8eefd77e3dd6de043514
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 07:38:58 +0000

    closure-compiler: update 20260907_1 bottle.

--- Formula/c/closure-compiler.rb
+++ Formula/c/closure-compiler.rb
@@ -12,7 +12,7 @@ class ClosureCompiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "367e44d044d530ab1025e66f5d28a2564bafba6c1626a48faf1160a13f05c5f5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "81953208e06ddac0d3345b73fbdaac56350dad1d8dcba71afc2cfdfa787a25fc"
   end
 
   depends_on "openjdk"
