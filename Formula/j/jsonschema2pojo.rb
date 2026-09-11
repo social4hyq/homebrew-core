ab290d77674ccc33aa3707c1581e8bd2adc6d271
hqzing <hqzing@outlook.com>
2026-09-11 02:00:52 +0000

    jsonschema2pojo: rebuild

--- Formula/j/jsonschema2pojo.rb
+++ Formula/j/jsonschema2pojo.rb
@@ -4,6 +4,7 @@ class Jsonschema2pojo < Formula
   url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-1.3.3/jsonschema2pojo-1.3.3.tar.gz"
   sha256 "877924359f7f3faf4a95d95df1d9fd074ede64c0f982fa86408299e9442775c4"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
