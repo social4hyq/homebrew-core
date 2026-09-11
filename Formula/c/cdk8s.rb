bde3e46de23117a7553f7bfd26041a09970447c8
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 03:57:38 +0000

    cdk8s: update 2.207.58_1 bottle.

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -7,7 +7,7 @@ class Cdk8s < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "83c5050fed7941ca9fc637ca9b18c1c5d8857fa3f2fc5324a25f5b3100a17f63"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "958c9c9199df1b3652024272d91483e553f4394ee4b861d7edd63b61b8fe0622"
   end
 
   depends_on "node"
