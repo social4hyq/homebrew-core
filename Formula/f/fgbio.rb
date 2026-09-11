34986cbdb8f07a7a2c9615a1ace2f00f1ff7152e
hqzing <hqzing@outlook.com>
2026-09-11 01:47:43 +0000

    fgbio: rebuild

--- Formula/f/fgbio.rb
+++ Formula/f/fgbio.rb
@@ -4,6 +4,7 @@ class Fgbio < Formula
   url "https://github.com/fulcrumgenomics/fgbio/releases/download/4.1.1/fgbio-4.1.1.jar"
   sha256 "cf569dd9f32dcdd4a38f2af7e2763eb16dcd1d5467e9414caeed027893b4ab36"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "08d7eb247369772ff8faaf6bb68979e92563c38e2868e51ca1defdad6fae8bb1"
