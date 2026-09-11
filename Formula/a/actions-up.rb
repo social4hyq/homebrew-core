e117a041509985fe91c62273d07f0aa07d1dd193
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 04:21:58 +0000

    actions-up: update 1.19.0_1 bottle.

--- Formula/a/actions-up.rb
+++ Formula/a/actions-up.rb
@@ -7,7 +7,7 @@ class ActionsUp < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "8ec0fcccf0716de64ad402d4090953a13e5054f5ee9e925af1de09ea92e3fa4e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2e77dff389245ee0e1af18fbc8d0d574ae62aee2383cc9255ba61dd5e4461245"
   end
 
   depends_on "node"
