40682a68d7c2716a48cc6a8c7d08bde05d90bd8e
hqzing <hqzing@outlook.com>
2026-09-11 02:00:35 +0000

    galen: rebuild

--- Formula/g/galen.rb
+++ Formula/g/galen.rb
@@ -4,7 +4,7 @@ class Galen < Formula
   url "https://github.com/galenframework/galen/releases/download/galen-2.4.4/galen-bin-2.4.4.zip"
   sha256 "b89ed0ccef4e5ea310563ab3220965f72d5fc182e89e6faadf44780f1c43b88d"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "0aafa3045b20d8d6d88894a066b47168d5fb956b3ebe6f3050139fc5a7240dcc"
