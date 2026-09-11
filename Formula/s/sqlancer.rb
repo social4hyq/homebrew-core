cef8f44ea5093788332d11c12845fde9ca73a62a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 09:28:06 +0000

    sqlancer: update 2.0.0_1 bottle.

--- Formula/s/sqlancer.rb
+++ Formula/s/sqlancer.rb
@@ -8,7 +8,7 @@ class Sqlancer < Formula
   head "https://github.com/sqlancer/sqlancer.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "190246fb6f41557c8a3ee7d1cde81fe85a241269b6de911a97e2c12ba91252a5"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "dbd6334372dc301820424b96bc346dc7a832db98a6144f879c3eabcf91500b16"
   end
 
   depends_on "maven" => :build
