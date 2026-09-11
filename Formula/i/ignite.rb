d39abb6dc32bd6238f6592f69cceef7f99be9e5a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 04:32:54 +0000

    ignite: update 29.10.1_1 bottle.

--- Formula/i/ignite.rb
+++ Formula/i/ignite.rb
@@ -8,7 +8,7 @@ class Ignite < Formula
   head "https://github.com/ignite/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "e206dc9deb9bc1287e18a053c511bc807232311755b4402928c416d343d9d9aa"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2aa14d3d97b9eb3a781b712540d6abce0e37199012b4a540f3e773e30d6f56a3"
   end
 
   depends_on "go"
