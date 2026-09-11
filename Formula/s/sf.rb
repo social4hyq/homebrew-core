e055869e381c0a77396f256ad22a07518629df36
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 04:29:17 +0000

    sf: update 2.150.6_1 bottle.

--- Formula/s/sf.rb
+++ Formula/s/sf.rb
@@ -14,7 +14,7 @@ class Sf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "682d3e2652ae9dc9714bcb56571b27f7e1150ddad033ec548b19fc2cd589b786"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "89025b2ac2e6e5517ca7803eff67b5873216f3e1dc8f21c9ba3d3d2cc3d3e7d9"
   end
 
   depends_on "node"
