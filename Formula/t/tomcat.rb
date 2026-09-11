4e4d039959ead569202caf0625f1c09b3b033c7b
hqzing <hqzing@outlook.com>
2026-09-11 01:31:41 +0000

    tomcat: rebuild

--- Formula/t/tomcat.rb
+++ Formula/t/tomcat.rb
@@ -5,6 +5,7 @@ class Tomcat < Formula
   mirror "https://archive.apache.org/dist/tomcat/tomcat-11/v11.0.25/bin/apache-tomcat-11.0.25.tar.gz"
   sha256 "071ae2c6213e3c02c58b8581113b7887a96ef2756439b3c5be36fa6aec350090"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "de3bf5ba332e7efd361574a2f16489966cd72463d7b4db45d925d8d05f21ea08"
