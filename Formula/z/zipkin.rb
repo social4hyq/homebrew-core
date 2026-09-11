fbd132f77b5aa74c52004b791303554eb109200f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 08:41:02 +0000

    zipkin: update 3.6.1_1 bottle.

--- Formula/z/zipkin.rb
+++ Formula/z/zipkin.rb
@@ -12,7 +12,7 @@ class Zipkin < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d4b47936d64b1215ae70a4d0b1696731b8cd5f4daca33d277ef1a6cda92cfcf3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "63fc926c77712a498086ee03929bc65e7f075207d5109e7be755ef8bd48c8dd5"
   end
 
   depends_on "openjdk"
