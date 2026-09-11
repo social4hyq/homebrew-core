6041d9c448d9fc992476fcedd07769816ff60033
hqzing <hqzing@outlook.com>
2026-09-11 01:47:05 +0000

    tika: rebuild

--- Formula/t/tika.rb
+++ Formula/t/tika.rb
@@ -5,6 +5,7 @@ class Tika < Formula
   mirror "https://archive.apache.org/dist/tika/3.3.2/tika-app-3.3.2.jar"
   sha256 "71ca551380e5eab1add99101f4597a8a49a6a18c6143d6874ee9599ca10ae00e"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "38d2401b79370975b3d71fabf3a29c06d8f678c8dabaa1b7afa31732d9c21f85"
