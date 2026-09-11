8138e55687e3c77d9589511e31d1e7ebddec2347
hqzing <hqzing@outlook.com>
2026-09-11 01:45:16 +0000

    epubcheck: rebuild

--- Formula/e/epubcheck.rb
+++ Formula/e/epubcheck.rb
@@ -4,6 +4,7 @@ class Epubcheck < Formula
   url "https://github.com/w3c/epubcheck/releases/download/v5.3.0/epubcheck-5.3.0.zip"
   sha256 "6c07e68584b2e2ce2f89fe06e1246dfead3eb36b46b340e7d93524f29dcff6c5"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     rebuild 1
