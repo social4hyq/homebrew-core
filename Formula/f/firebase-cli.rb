aa0b6a796933986f4b939fe1f381ee26ef62f699
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 22:39:45 +0000

    firebase-cli 15.30.0

--- Formula/f/firebase-cli.rb
+++ Formula/f/firebase-cli.rb
@@ -1,10 +1,9 @@
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-15.29.0.tgz"
-  sha256 "e8cbd5e2f4fd329120e559c8288312abe1761571023a1a92aa621c3b7459b3de"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-15.30.0.tgz"
+  sha256 "251a423232414a4c150522591f48ccfa7418417bf58652555524567ac8e86d0b"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "10643f776f801ebf4f9082794b88efbbd562c9aede51021f1a6610a5a98e6d73"
