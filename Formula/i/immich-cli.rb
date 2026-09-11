6f6539ee7861bbb698eff04bb896a0a967a48922
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 22:59:58 +0000

    immich-cli 3.2.0

--- Formula/i/immich-cli.rb
+++ Formula/i/immich-cli.rb
@@ -1,8 +1,8 @@
 class ImmichCli < Formula
   desc "Command-line interface for self-hosted photo manager Immich"
   homepage "https://immich.app/docs/features/command-line-interface"
-  url "https://registry.npmjs.org/@immich/cli/-/cli-3.1.0.tgz"
-  sha256 "dd4e317eae40a9f9cdb9a8de9191c6244c1d31d3c9853170c7126b7259a24067"
+  url "https://registry.npmjs.org/@immich/cli/-/cli-3.2.0.tgz"
+  sha256 "3412f37110a694b2902f90224e51b04854a889432ee66091a8166315187df603"
   license "AGPL-3.0-only"
 
   bottle do
