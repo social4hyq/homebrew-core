d6e79f91322fc3d6a236460c3917d3cb796678df
hqzing <hqzing@outlook.com>
2026-09-11 02:04:46 +0000

    htmlcleaner: rebuild

--- Formula/h/htmlcleaner.rb
+++ Formula/h/htmlcleaner.rb
@@ -4,7 +4,7 @@ class Htmlcleaner < Formula
   url "https://downloads.sourceforge.net/project/htmlcleaner/htmlcleaner/htmlcleaner%20v2.29/htmlcleaner-src-2.29.zip"
   sha256 "9fc68d7161be6f34f781e109bf63894d260428f186d88f315b1d2e3a33495350"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "e248f5d36953bd0cf23d3140dec3fed38f35d5d9123abf6c6cc23e47522cadc5"
