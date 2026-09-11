1ba327a4d0fb587d4d1efacc624292121c418132
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 03:22:31 +0000

    libxdamage: update 1.1.7_1 bottle.

--- Formula/lib/libxdamage.rb
+++ Formula/lib/libxdamage.rb
@@ -7,7 +7,7 @@ class Libxdamage < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "b44d4c698e645a940ab6ca7f3efaa222db245550f7346343d7ab3ac21987cf55"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "07f2718e889a92e55c63e2106baddec998230c8ce51b55dd1ff064ad35b9cbd0"
   end
 
   depends_on "pkgconf" => :build
