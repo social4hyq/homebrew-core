b1f2451595d28b9da9d4c88b3ad5fa132537da65
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 06:14:03 +0000

    libxtst: update 1.2.5_1 bottle.

--- Formula/lib/libxtst.rb
+++ Formula/lib/libxtst.rb
@@ -8,7 +8,7 @@ class Libxtst < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "55186145d6a646950d1f1295e201faf8996829ffc5c35ccbb5d2125aa9816350"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "37768c4e81e22dc5761c1f7db54cb68f071c7cee9b022179115a530bd16f434a"
   end
 
   depends_on "pkgconf" => :build
