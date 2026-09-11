99ea346f1ef8724378dc6d3eae6675fa14c9e40e
hqzing <hqzing@outlook.com>
2026-09-11 01:46:48 +0000

    tailor: rebuild

--- Formula/t/tailor.rb
+++ Formula/t/tailor.rb
@@ -4,7 +4,7 @@ class Tailor < Formula
   url "https://github.com/sleekbyte/tailor/releases/download/v0.12.0/tailor-0.12.0.tar"
   sha256 "ec3810b27e9a35ecdf3a21987f17cad86918240d773172264e9abbb1a7efc415"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "16f25b81c533d93d75126c311227bec78a87fadcf9d563a6be84f4730d6b6b8a"
