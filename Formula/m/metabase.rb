6ea812b2104fda273adecfe04421e5a12a8df320
hqzing <hqzing@outlook.com>
2026-09-11 01:30:58 +0000

    metabase: rebuild

--- Formula/m/metabase.rb
+++ Formula/m/metabase.rb
@@ -4,6 +4,7 @@ class Metabase < Formula
   url "https://downloads.metabase.com/v0.63.16/metabase.jar"
   sha256 "2e171aa7d03f10b57d92775428442203e2c5e70ecdcf781f53368e5d8f59af87"
   license "AGPL-3.0-only"
+  revision 1
 
   # The first-party download page only provides an unversioned link to the
   # latest OSS jar file. We check the "latest" GitHub release, as the release
