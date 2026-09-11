554237f89ea236ef7911989775627bb0eab8f5e5
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 07:02:37 +0000

    sratom: update 0.6.22_1 bottle.

--- Formula/s/sratom.rb
+++ Formula/s/sratom.rb
@@ -12,7 +12,7 @@ class Sratom < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "60e1e3c4fdc44863e071982be7bb948ba67b8a8afe33049d97d43c7b2203f498"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "117128cbc81b43840f6efac7f41f395a8f33aed3d6b590aec33e2952c6c432be"
   end
 
   depends_on "meson" => :build
