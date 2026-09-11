bd0eceb1d406d43ed6deb28cb3add002bfbf2df3
hqzing <hqzing@outlook.com>
2026-09-11 01:58:18 +0000

    riemann: rebuild

--- Formula/r/riemann.rb
+++ Formula/r/riemann.rb
@@ -4,6 +4,7 @@ class Riemann < Formula
   url "https://github.com/riemann/riemann/releases/download/0.3.12/riemann-0.3.12.tar.bz2"
   sha256 "82c24c7cba3bce96957f25661f39c6162a262ba76aef24e986e73dbf2a79b7a6"
   license "EPL-1.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "768ceedf4b830d5cab4f30c66fde5b9deba718f672e163c09eff5e965e270668"
