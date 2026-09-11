0aa61a1992e9f8f30528664cd2606b6fa3a4cf89
hqzing <hqzing@outlook.com>
2026-09-11 01:59:21 +0000

    solr@8.11: rebuild

--- Formula/s/solr@8.11.rb
+++ Formula/s/solr@8.11.rb
@@ -5,6 +5,7 @@ class SolrAT811 < Formula
   mirror "https://archive.apache.org/dist/lucene/solr/8.11.4/solr-8.11.4.tgz"
   sha256 "163fbdf246bbd78910bc36c3257ad50cdf31ccc3329a5ef885c23c9ef69e0ebe"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "a50c8b5dfe3475e36d102fef09eb1c05cca86f1742d74092102b79ae4b42d6d8"
