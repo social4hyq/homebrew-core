f5dad70fbe175baeebe1c1fc6902ee9e4c4e4067
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 01:04:42 +0000

    node: update 26.8.1_3 bottle.

--- Formula/n/node.rb
+++ Formula/n/node.rb
@@ -13,7 +13,7 @@ class Node < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "45ef522db16a820b8722be54dd6eddd4e9afdbaa15c22a8692b405f4b04389c8"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "b43d274a6a525f3feb040a4bc509a44a7f8300444b664cadc0ebba52870c40cb"
   end
 
   # The unversioned `llvm` (LLVM 23) is too new for node, which is not yet
