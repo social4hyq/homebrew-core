012460d3f505456baa6482a2a1371c21874d0d09
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 08:19:20 +0000

    mvnvm: update 1.0.29_1 bottle.

--- Formula/m/mvnvm.rb
+++ Formula/m/mvnvm.rb
@@ -9,7 +9,7 @@ class Mvnvm < Formula
   head "https://bitbucket.org/mjensen/mvnvm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d28ab17690dc444a168afe9e5ba096577c2c8ec6f0487802b4158e131307d20c"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "e7e793436ccf8e75b3bbfc72e331a06da16d3fdc93bcf252a5c54a84e680e5e6"
   end
 
   depends_on "openjdk"
