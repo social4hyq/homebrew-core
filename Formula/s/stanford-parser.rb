60680afa1854c78a4125b2d829c8037f4c532406
hqzing <hqzing@outlook.com>
2026-09-11 01:59:39 +0000

    stanford-parser: rebuild

--- Formula/s/stanford-parser.rb
+++ Formula/s/stanford-parser.rb
@@ -4,6 +4,7 @@ class StanfordParser < Formula
   url "https://nlp.stanford.edu/software/stanford-parser-4.2.0.zip"
   sha256 "8c2110c78f7f82b66bcf91089a18e415669eda4346bbd9a6e3bc2bde63e5fed1"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
