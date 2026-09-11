600aa304636d253d782b91c1287fe30dc6be578a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 23:21:04 +0000

    libupnp 22.1.0

--- Formula/lib/libupnp.rb
+++ Formula/lib/libupnp.rb
@@ -1,8 +1,8 @@
 class Libupnp < Formula
   desc "Portable UPnP development kit"
   homepage "https://pupnp.sourceforge.io/"
-  url "https://github.com/pupnp/pupnp/releases/download/release-22.0.6/libupnp-22.0.6.tar.bz2"
-  sha256 "7f4e1eed75d904180b705570c8c55d70a47885e46f702f4b96e4fac03159f5d7"
+  url "https://github.com/pupnp/pupnp/releases/download/release-22.1.0/libupnp-22.1.0.tar.bz2"
+  sha256 "238fd87f7173c1125dedd553193983c58a91d7fb1179358523cfe4fb2731577a"
   license "BSD-3-Clause"
 
   livecheck do
