ceeaa4baa5cc565def0c655470f32efefa4b27a3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 23:42:33 +0000

    neo4j 2026.08.1

--- Formula/n/neo4j.rb
+++ Formula/n/neo4j.rb
@@ -1,10 +1,9 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "https://neo4j.com/"
-  url "https://neo4j.com/artifact.php?name=neo4j-community-2026.07.1-unix.tar.gz"
-  sha256 "fbb3a11705cdbc87953242e3667dad37cd39f8d1e1168faa642c01a2fd1d144d"
+  url "https://neo4j.com/artifact.php?name=neo4j-community-2026.08.1-unix.tar.gz"
+  sha256 "6e4bb155a4bd02a8a7a7e83a56aca70df1cd5d90ff4dd94b540f20f49f000084"
   license "GPL-3.0-or-later"
-  revision 1
 
   livecheck do
     url "https://neo4j.com/deployment-center/"
