85a930428340b0272561c24193e0962fc992a35d
hqzing <hqzing@outlook.com>
2026-09-11 01:59:01 +0000

    skinny: rebuild

--- Formula/s/skinny.rb
+++ Formula/s/skinny.rb
@@ -4,6 +4,7 @@ class Skinny < Formula
   url "https://github.com/skinny-framework/skinny-framework/releases/download/4.0.1/skinny-4.0.1.tar.gz"
   sha256 "2382ba97f799bfc772ee79b2c084c63a1278ddd89de8dacd4ba6433f41294812"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "218f30d0e973a3e7fcfddbcc4754c0602481960de0dc4e464e8aa318d8b48e7e"
