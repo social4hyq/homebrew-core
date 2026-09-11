122c057b9e0b5a6faf0fd42ae6c9647f99f23845
hqzing <hqzing@outlook.com>
2026-09-11 01:42:03 +0000

    ballerina: rebuild

--- Formula/b/ballerina.rb
+++ Formula/b/ballerina.rb
@@ -4,6 +4,7 @@ class Ballerina < Formula
   url "https://dist.ballerina.io/downloads/2201.13.6/ballerina-2201.13.6-swan-lake.zip"
   sha256 "1fbb2a607e5216b377b6c206f340a0d3070546265d5e83d3bbcbad7c610e4593"
   license "Apache-2.0"
+  revision 1
 
   # The Downloads and Installation Options pages don't include any version
   # information or download links in the HTML. This information is instead
