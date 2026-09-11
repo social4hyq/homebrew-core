0b03af621ad849e40a0c077e2e195c480b869fb4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-11 23:25:52 +0000

    luau 0.738

--- Formula/l/luau.rb
+++ Formula/l/luau.rb
@@ -1,8 +1,8 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau.org"
-  url "https://github.com/luau-lang/luau/archive/refs/tags/0.737.tar.gz"
-  sha256 "767b872f084518ca9a79b021f83b40e6a051be4c1f91dd712a949417a4e979c6"
+  url "https://github.com/luau-lang/luau/archive/refs/tags/0.738.tar.gz"
+  sha256 "e7fa8590fff16d20f4ac0da40fe79b1fe24d489f965c60e5cb455d462bd48929"
   license "MIT"
   version_scheme 1
   head "https://github.com/luau-lang/luau.git", branch: "master"
