7c298568994517bc3b5537f3ef2335d938d0583b
hqzing <hqzing@outlook.com>
2026-09-11 01:39:28 +0000

    pkl-lsp: rebuild

--- Formula/p/pkl-lsp.rb
+++ Formula/p/pkl-lsp.rb
@@ -4,6 +4,7 @@ class PklLsp < Formula
   url "https://github.com/apple/pkl-lsp/releases/download/0.8.0/pkl-lsp-0.8.0.jar"
   sha256 "06f9de45a21aa7a16bf5c052513521f98793cd9c57036b9368fcad799e96a8ff"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
