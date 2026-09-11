9dd087788d98779b11e7eecc6e2638035a0835e6
hqzing <hqzing@outlook.com>
2026-09-11 01:30:44 +0000

    apktool: rebuild

--- Formula/a/apktool.rb
+++ Formula/a/apktool.rb
@@ -4,6 +4,7 @@ class Apktool < Formula
   url "https://github.com/iBotPeaches/Apktool/releases/download/v3.0.3/apktool_3.0.3.jar"
   sha256 "dbf930b076c6b9be08d57c449cacefc3bdd6b71ebd59b3066fc0e1f5b14f9423"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "f9215810a888f5055410f02497a2a81a62398596384f549da853a86c0d7204ce"
