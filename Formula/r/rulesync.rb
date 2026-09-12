66c5099d194bbe69af09eaa1c80f40d720bb2170
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 00:06:38 +0000

    rulesync 16.27.0

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -1,10 +1,9 @@
 class Rulesync < Formula
   desc "Unified AI rules management CLI tool"
   homepage "https://github.com/dyoshikawa/rulesync"
-  url "https://registry.npmjs.org/rulesync/-/rulesync-16.26.0.tgz"
-  sha256 "fd6fff0498f19d013919744f030ec689dfb997de3669b98e027843768827ef39"
+  url "https://registry.npmjs.org/rulesync/-/rulesync-16.27.0.tgz"
+  sha256 "02515eb11f4f6680019b22bf43ff27361fc56c621e5b8e48c6ee0ac3d4c4e4f1"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "dfae358dcdc2cd997ea21806a1199e5689e4758ee4fdaebd04d40d4af94fe856"
