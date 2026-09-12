3a636ffc863f24c5dcc27fbbafbe1eb0102bf64b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 05:01:59 +0000

    tea: update 0.16.0 bottle.

--- Formula/t/tea.rb
+++ Formula/t/tea.rb
@@ -8,7 +8,7 @@ class Tea < Formula
   head "https://gitea.com/gitea/tea.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "da7838f92af0051d763cf53a3e07255f75d623a710b89254d95a67c4cb190a7f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f36ea9473a048169d391207869954bad03ff85fbadec02cf51d95ee1f08b212a"
   end
 
   depends_on "go" => :build
