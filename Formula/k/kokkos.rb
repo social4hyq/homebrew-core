0db1440d783137fa7b7476d84fbed2313e4d2dc0
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 06:44:02 +0000

    kokkos: update 5.2.2 bottle.

--- Formula/k/kokkos.rb
+++ Formula/k/kokkos.rb
@@ -12,7 +12,7 @@ class Kokkos < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5ea0bbb04933faf45d1c7acb67bb97ab1538e786a725bbee558a80ee6a9588b3"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "243032f2ebade136a085ec7cfc15ae0638327776c15f9915d07ca87c693bd5c3"
   end
 
   depends_on "cmake" => :build
