99a4827e240cc778b8f089a2e9544a8f1b832ac3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 07:09:48 +0000

    flagd: update 0.16.3 bottle.

--- Formula/f/flagd.rb
+++ Formula/f/flagd.rb
@@ -18,7 +18,7 @@ class Flagd < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "f2390eae45e43ea2e092a1ffae5f54ba243de6c38b6f66f674430e35a36b8945"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2410a99bbf1ea5c9400b875b3236b469b6df314bc6e3b17701f99153ec8a9439"
   end
 
   depends_on "go" => :build
