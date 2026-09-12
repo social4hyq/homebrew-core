e315b3d98ca499a8fe3bba9ab48c8d88819cfb8e
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 00:03:22 +0000

    render 2.28.0

--- Formula/r/render.rb
+++ Formula/r/render.rb
@@ -1,10 +1,9 @@
 class Render < Formula
   desc "Command-line interface for Render"
   homepage "https://render.com/docs/cli"
-  url "https://github.com/render-oss/cli/archive/refs/tags/v2.26.0.tar.gz"
-  sha256 "f1934ac179570a37ef2986c0b792eb8b3bfaf97ae2f2d36d112cc2282a71fa22"
+  url "https://github.com/render-oss/cli/archive/refs/tags/v2.28.0.tar.gz"
+  sha256 "5bf7d3317315b288446f22b6f6b23f2b1a576578179c26ae435068bd8864a063"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/render-oss/cli.git", branch: "main"
 
   bottle do
