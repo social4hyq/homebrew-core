cf36582bdcc1b30fa2776df5cfaac6e60fc9a63f
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 00:13:52 +0000

    simdutf 9.1.2

--- Formula/s/simdutf.rb
+++ Formula/s/simdutf.rb
@@ -1,10 +1,9 @@
 class Simdutf < Formula
   desc "Unicode conversion routines, fast"
   homepage "https://simdutf.github.io/simdutf/"
-  url "https://github.com/simdutf/simdutf/archive/refs/tags/v9.1.1.tar.gz"
-  sha256 "ec707f17e5083999efbdaf8a9a08d35e71e955b35dbf4b8307d14a7d31e9697f"
+  url "https://github.com/simdutf/simdutf/archive/refs/tags/v9.1.2.tar.gz"
+  sha256 "0992cd1bcddee10424e49d6bc3ff8da02f9abc4c48033cbb1b0b41b62c727d33"
   license any_of: ["Apache-2.0", "MIT"]
-  revision 1
   compatibility_version 3
   head "https://github.com/simdutf/simdutf.git", branch: "master"
 
