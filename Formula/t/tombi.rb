825e7f08ba4ac450c23ea1baac93639fdac56998
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-12 23:50:56 +0000

    tombi 1.5.5

--- Formula/t/tombi.rb
+++ Formula/t/tombi.rb
@@ -1,10 +1,9 @@
 class Tombi < Formula
   desc "TOML formatter, linter and language server"
   homepage "https://github.com/tombi-toml/tombi"
-  url "https://github.com/tombi-toml/tombi/archive/refs/tags/v1.5.4.tar.gz"
-  sha256 "baa1b914ce306c4b4361450fbf2101745e5411a978f68fc2d1a5c71241e24cf3"
+  url "https://github.com/tombi-toml/tombi/archive/refs/tags/v1.5.5.tar.gz"
+  sha256 "e5253c2d9a59be9940ec4d9a767395388a9820db839bc1e0ed99a75c07006f83"
   license "MIT"
-  revision 1
   head "https://github.com/tombi-toml/tombi.git", branch: "main"
 
   livecheck do
