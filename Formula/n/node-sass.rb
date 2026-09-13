657bca0acdaa41114df860f8ed34bd48ef3070ed
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-13 23:16:50 +0000

    node-sass 1.104.1

--- Formula/n/node-sass.rb
+++ Formula/n/node-sass.rb
@@ -1,10 +1,9 @@
 class NodeSass < Formula
   desc "JavaScript implementation of a Sass compiler"
   homepage "https://github.com/sass/dart-sass"
-  url "https://registry.npmjs.org/sass/-/sass-1.104.0.tgz"
-  sha256 "38d27c532071b573b75498ca7ef61738d769f40a8ad6f8564cca28334473bba7"
+  url "https://registry.npmjs.org/sass/-/sass-1.104.1.tgz"
+  sha256 "7a935a71c27e77910a61fe079641b034ac11aaf2b30071e338f9948d9fe871d7"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "af7c5ddde003e7f97ae578c37c16c61e9a023eb3395541e4607bf80d19151c54"
