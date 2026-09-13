24d9728720b217699048723f33f24f485fdfb6ab
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-13 23:07:55 +0000

    marked 18.0.13

--- Formula/m/marked.rb
+++ Formula/m/marked.rb
@@ -1,10 +1,9 @@
 class Marked < Formula
   desc "Markdown parser and compiler built for speed"
   homepage "https://marked.js.org/"
-  url "https://registry.npmjs.org/marked/-/marked-18.0.12.tgz"
-  sha256 "50f7134d0f110b3db6137ac269e6a45a79b3f8cf2b902c363f4c7dd5e2afece7"
+  url "https://registry.npmjs.org/marked/-/marked-18.0.13.tgz"
+  sha256 "a0a6d4ab6386b2babc02419c258512199a451150ed5f1bed47795d2b07f2d074"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "85e41090ccd93a6e5d2117486157f01d93089a505e8f9ea3b9aa85893e037ce3"
