4474ddfee572019db553d72517e135aab81c5dfb
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-14 22:02:53 +0000

    apko 1.4.1

--- Formula/a/apko.rb
+++ Formula/a/apko.rb
@@ -1,10 +1,9 @@
 class Apko < Formula
   desc "Build OCI images from APK packages directly without Dockerfile"
   homepage "https://github.com/chainguard-dev/apko"
-  url "https://github.com/chainguard-dev/apko/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "0f9e882489b04b3a36b2c620ab4df6ae485dd84dbed3cab742d70b2b56655ef9"
+  url "https://github.com/chainguard-dev/apko/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "9dfe9bdc3c589db30c674b90b7d92b09eca8e245f41319c8fedc831a06b646c7"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/chainguard-dev/apko.git", branch: "main"
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
