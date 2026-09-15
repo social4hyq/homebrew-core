d63601405e6b9249ea1527d01f19eb656fccc1f2
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-15 23:28:03 +0000

    oxfmt 0.68.0

--- Formula/o/oxfmt.rb
+++ Formula/o/oxfmt.rb
@@ -1,10 +1,9 @@
 class Oxfmt < Formula
   desc "High-performance formatting tool for JavaScript and TypeScript"
   homepage "https://oxc.rs/"
-  url "https://registry.npmjs.org/oxfmt/-/oxfmt-0.67.0.tgz"
-  sha256 "70d9fd9edf644a23f513a673e73a8ebe6e828e13adfaa1fc78b86fe13382ef1d"
+  url "https://registry.npmjs.org/oxfmt/-/oxfmt-0.68.0.tgz"
+  sha256 "bf2309e391725080c15667971677d8d73bfe4cce7cdea4854c70814a4ccec19c"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "b5d34d3c46434fe3732378928411806f36b50b1928d61c1669e476756baf5d2e"
