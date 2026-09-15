2e3e642587ceea4d0ab5a5013b0e6a8fe0e685cf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-15 00:11:05 +0000

    unordered_dense 5.0.1

--- Formula/u/unordered_dense.rb
+++ Formula/u/unordered_dense.rb
@@ -1,8 +1,8 @@
 class UnorderedDense < Formula
   desc "Hashmap and hashset based on robin-hood backward shift deletion"
   homepage "https://github.com/martinus/unordered_dense"
-  url "https://github.com/martinus/unordered_dense/archive/refs/tags/v4.11.0.tar.gz"
-  sha256 "a232f7433b45872d43e4dc74a25cbd58effc0be76e3d704b34e5de3c637eed77"
+  url "https://github.com/martinus/unordered_dense/archive/refs/tags/v5.0.1.tar.gz"
+  sha256 "b79f46db45fd73310211429e5d33da4a579543ac544ae7dc12f8d9a31ad0aea4"
   license "MIT"
 
   bottle do
