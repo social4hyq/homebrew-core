674c9f9c2a0442cdd7992c0f5d9e2375bbfc5b4b
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-15 23:22:43 +0000

    nelm 1.30.2

--- Formula/n/nelm.rb
+++ Formula/n/nelm.rb
@@ -1,8 +1,8 @@
 class Nelm < Formula
   desc "Kubernetes deployment tool that manages and deploys Helm Charts"
   homepage "https://github.com/werf/nelm"
-  url "https://github.com/werf/nelm/archive/refs/tags/v1.27.0.tar.gz"
-  sha256 "deb3878cbfd4889bba803cfe89ee34871b90ec4e283984972178344a7f43f514"
+  url "https://github.com/werf/nelm/archive/refs/tags/v1.30.2.tar.gz"
+  sha256 "fead91bb61e6e183e3cda03aa881bbe15273e41634283ec49d5e748084b26e87"
   license "Apache-2.0"
   head "https://github.com/werf/nelm.git", branch: "main"
 
