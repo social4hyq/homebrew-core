92cc455142b676818ca09afeab0c5e60ae151021
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-15 22:30:21 +0000

    epubcheck 5.4.0

--- Formula/e/epubcheck.rb
+++ Formula/e/epubcheck.rb
@@ -1,10 +1,9 @@
 class Epubcheck < Formula
   desc "Validate EPUB files, version 2.0 and later"
   homepage "https://github.com/w3c/epubcheck"
-  url "https://github.com/w3c/epubcheck/releases/download/v5.3.0/epubcheck-5.3.0.zip"
-  sha256 "6c07e68584b2e2ce2f89fe06e1246dfead3eb36b46b340e7d93524f29dcff6c5"
+  url "https://github.com/w3c/epubcheck/releases/download/v5.4.0/epubcheck-5.4.0.zip"
+  sha256 "33350c61038e71dfb3d45a76aed04bf5481e6d5500cb780f6e98db8bbd15a28c"
   license "BSD-3-Clause"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "7009610782ae59cb3c0bce7d7f385be428d9c9d0851e81f7172702bc99b8dad4"
