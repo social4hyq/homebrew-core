0390a13e800800ed07c5441e09ac2f11deb86d40
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-16 02:36:23 +0000

    cc-switch-cli: update 5.10.5 bottle.

--- Formula/c/cc-switch-cli.rb
+++ Formula/c/cc-switch-cli.rb
@@ -7,7 +7,7 @@ class CcSwitchCli < Formula
   head "https://github.com/SaladDay/cc-switch-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "5a5f42d4fe470726423fef314f45416f09b3b95e432301a56400acf14936fc5e"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "57e2acdfdf5553a4685d83cf74bb77f514e32d3528e13f8ee9ea7d48fd156383"
   end
 
   depends_on "rust" => :build
