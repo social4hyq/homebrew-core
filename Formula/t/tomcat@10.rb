25a373949acb40391a4e5f9acdc8cac9d2875040
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-16 00:59:17 +0000

    tomcat@10: update 10.1.60 bottle.

--- Formula/t/tomcat@10.rb
+++ Formula/t/tomcat@10.rb
@@ -11,7 +11,7 @@ class TomcatAT10 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "10aeadb5b551aa9b572a60f0386e36035ceb3f869e796034e80ded069eea64a2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "2b4f31225207f2555beaf6e74a6f597e16cca8c59207b22aa49956f03a4c97df"
   end
 
   keg_only :versioned_formula
