525a619301277f9f7c41fb66d978ef2a2994f6cf
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-16 01:46:30 +0000

    minder: update 0.3.2 bottle.

--- Formula/m/minder.rb
+++ Formula/m/minder.rb
@@ -7,7 +7,7 @@ class Minder < Formula
   head "https://github.com/mindersec/minder.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a3a5a4d1d96b03c745c2fb2f8e2bdcb2b94fa2d31cfaba728a63bdc441d86718"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "03d081ad33f11836838140eebcbcc3b8c2d0551359a46660cc87553b83ecfe67"
   end
 
   depends_on "go" => :build
