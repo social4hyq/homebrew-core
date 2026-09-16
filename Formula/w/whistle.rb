7996c20080d5af8a0f4002d8cdf7d51c76f4e0b7
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-16 00:42:40 +0000

    whistle: update 2.10.10 bottle.

--- Formula/w/whistle.rb
+++ Formula/w/whistle.rb
@@ -6,7 +6,7 @@ class Whistle < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "847603a76fa86c5509d2794b91c55019ee8716e33e5e619feb486fc0a9732922"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "c940d5102435ea751cb505efc51432905974d65c73aec9c573c8e98acbaa9dec"
   end
 
   depends_on "node"
