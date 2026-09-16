a3b67cdf0581f004a06c498cc13c1259d8886b70
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-16 23:45:50 +0000

    sentry-cli 3.8.0

--- Formula/s/sentry-cli.rb
+++ Formula/s/sentry-cli.rb
@@ -1,8 +1,8 @@
 class SentryCli < Formula
   desc "Command-line utility to interact with Sentry"
   homepage "https://docs.sentry.io/cli/"
-  url "https://github.com/getsentry/sentry-cli/archive/refs/tags/3.7.0.tar.gz"
-  sha256 "b34093b7c0741d537cef5b46821b2b38759393ba0ba99dd4691a84618c92d2e3"
+  url "https://github.com/getsentry/sentry-cli/archive/refs/tags/3.8.0.tar.gz"
+  sha256 "38bc0057faffe31e877a3b1c6afd9c68220e1e5755fe113f70a5e5cbdd1ce2ea"
   license "BSD-3-Clause"
   head "https://github.com/getsentry/sentry-cli.git", branch: "master"
 
