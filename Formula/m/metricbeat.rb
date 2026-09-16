8aa3baaa053a232f7454f897688849498d0eaaab
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-16 01:50:31 +0000

    metricbeat: update 9.5.4 bottle.

--- Formula/m/metricbeat.rb
+++ Formula/m/metricbeat.rb
@@ -8,7 +8,7 @@ class Metricbeat < Formula
   head "https://github.com/elastic/beats.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "95ba74b84caae33561b1aae3e168b323a312ed4a60d0890143cf72cb40ee090d"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "602227d7483f6528432087b7553dad01f2d065e25abb19ffe536302bbcfcc3cd"
   end
 
   depends_on "go" => :build
