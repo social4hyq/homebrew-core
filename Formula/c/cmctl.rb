8bc2ec3d60c958b97da042634cbcff0ae3ed8350
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-17 22:15:02 +0000

    cmctl 2.6.1

--- Formula/c/cmctl.rb
+++ Formula/c/cmctl.rb
@@ -1,8 +1,8 @@
 class Cmctl < Formula
   desc "Command-line tool to manage cert-manager"
   homepage "https://cert-manager.io"
-  url "https://github.com/cert-manager/cmctl/archive/refs/tags/v2.5.0.tar.gz"
-  sha256 "4e7f137c2b5411f92948749dbdb31d61911088a54d32e14aea609da02c203bb5"
+  url "https://github.com/cert-manager/cmctl/archive/refs/tags/v2.6.1.tar.gz"
+  sha256 "83226abe4516e4e39953dee0d341b26e4c6f5a7f2f62ea07074bd2f8dd55c664"
   license "Apache-2.0"
   head "https://github.com/cert-manager/cmctl.git", branch: "main"
 
