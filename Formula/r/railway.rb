fe4a3c38804b2a3df502abb6506a7b521d9def53
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-17 23:32:15 +0000

    railway 5.57.9

--- Formula/r/railway.rb
+++ Formula/r/railway.rb
@@ -1,8 +1,8 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.com/"
-  url "https://github.com/railwayapp/cli/archive/refs/tags/v5.57.6.tar.gz"
-  sha256 "5d5644ca6722607aac5f2b527200673b4f383c6b2c98ec778cad8a4aa8ce23b8"
+  url "https://github.com/railwayapp/cli/archive/refs/tags/v5.57.9.tar.gz"
+  sha256 "b38f6da300abe910c5081b89177369739e6f257bb86de444b768e8617536855a"
   license "MIT"
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
