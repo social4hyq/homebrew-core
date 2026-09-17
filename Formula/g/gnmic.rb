64b45735e5aac361605cadee99cb25b8f287f215
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-17 15:38:29 +0000

    gnmic: update 0.49.0 bottle.

--- Formula/g/gnmic.rb
+++ Formula/g/gnmic.rb
@@ -7,7 +7,7 @@ class Gnmic < Formula
   head "https://github.com/openconfig/gnmic.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "bde7250c9c84c78e88844cb1e2bcb8763b586d8ed88030a0bbe631b6a7daa3e7"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "4598b74dcf7f2f04fa6aac2b0cfbaafdcfeb8f495cb62f54086022e8c143a673"
   end
 
   depends_on "go" => :build
