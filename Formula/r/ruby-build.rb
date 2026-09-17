b625c24e40b54530d2e2705c9fa4cf61623453d4
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-17 14:52:42 +0000

    ruby-build: update 20260916 bottle.

--- Formula/r/ruby-build.rb
+++ Formula/r/ruby-build.rb
@@ -8,7 +8,7 @@ class RubyBuild < Formula
   head "https://github.com/rbenv/ruby-build.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "d2d326b89500655d5c1eecfdbbe178611aa387b8a162dd321ccc56cfe1b331c2"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "f330576750d76b7b5b6f2f319c4a2b3e7a5ad31854b6b1bd44c5a6c919d66ef4"
   end
 
   depends_on "autoconf"
