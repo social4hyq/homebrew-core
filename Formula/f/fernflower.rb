05696637c399ac8ec77222e77e539b3333fbfee3
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-17 15:49:13 +0000

    fernflower: update 262.10968.63 bottle.

--- Formula/f/fernflower.rb
+++ Formula/f/fernflower.rb
@@ -12,7 +12,7 @@ class Fernflower < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "26bb35960ea784e5fb2f41cb353b033adf4ce5104d289f4ac3665bf6f498815a"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "cf3a3efaeb4a2fef3b23f54e296361db11c7b645008f528d920c8c8678d1db4c"
   end
 
   depends_on "openjdk"
