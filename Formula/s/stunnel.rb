eaee2026f5b5c105689e69413725c2cf69d64863
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-18 17:50:23 +0000

    stunnel: update 5.82 bottle.

--- Formula/s/stunnel.rb
+++ Formula/s/stunnel.rb
@@ -11,7 +11,7 @@ class Stunnel < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "a4b40993238ac14a051ed2fbb9a4cf0d96834d66b0da9af9b29bf1772746f1a4"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "02c8e3737dd7dc3ad18f97b0c6a0a9bf844c9bc4f48526f9d19a389ae5077ff3"
   end
 
   depends_on "openssl@3"
