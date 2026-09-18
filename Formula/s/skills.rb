edc357af922ab3876231d23e0125b87539748a01
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-18 23:37:45 +0000

    skills 1.7.0

--- Formula/s/skills.rb
+++ Formula/s/skills.rb
@@ -1,8 +1,8 @@
 class Skills < Formula
   desc "Open agent skills ecosystem"
   homepage "https://skills.sh"
-  url "https://registry.npmjs.org/skills/-/skills-1.5.26.tgz"
-  sha256 "bc93cd403104ab859abdfbe692ba1fb4429fd2e47c38abb27449a4e8e6ba943c"
+  url "https://registry.npmjs.org/skills/-/skills-1.7.0.tgz"
+  sha256 "8d1466f792baaee945dae88e05ee403d6f9e78a3ae8dcbf61496035ca274418d"
   license "MIT"
 
   bottle do
