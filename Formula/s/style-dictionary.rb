47d7eef824c71341d23caaf94edaed64b62cc564
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-19 23:39:28 +0000

    style-dictionary 5.5.4

--- Formula/s/style-dictionary.rb
+++ Formula/s/style-dictionary.rb
@@ -1,10 +1,9 @@
 class StyleDictionary < Formula
   desc "Build system for creating cross-platform styles"
   homepage "https://github.com/style-dictionary/style-dictionary"
-  url "https://registry.npmjs.org/style-dictionary/-/style-dictionary-5.5.3.tgz"
-  sha256 "7abdce80029652a95b53247e2faf03c59e9bee357d07decce9d93c89b5dd784d"
+  url "https://registry.npmjs.org/style-dictionary/-/style-dictionary-5.5.4.tgz"
+  sha256 "2ac264e1061b3fbf680a7bfa0ca32b44fa0e284e3b01a547ec977a3e27b6d827"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ohos: "28e50b9931efcfd7c119e2ba5ae7878e6a300c3025e6320a80de8ff9a215823e"
