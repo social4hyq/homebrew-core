6f5337b54ec54315a40f1f2e1078acc3cb1d215a
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-19 22:20:59 +0000

    devcockpit 3.0.0

--- Formula/d/devcockpit.rb
+++ Formula/d/devcockpit.rb
@@ -1,8 +1,8 @@
 class Devcockpit < Formula
   desc "TUI system monitor for Apple Silicon"
   homepage "https://devcockpit.app/"
-  url "https://github.com/caioricciuti/dev-cockpit/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "feb16115caf94b63b71a5c86ab47b10bee5009207790c99df52443fe4cdd4873"
+  url "https://github.com/caioricciuti/dev-cockpit/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "5f0c72cd82ce06b166ad92810d096507c9575350af058734c3c010408cf0e87c"
   license "GPL-3.0-only"
   head "https://github.com/caioricciuti/dev-cockpit.git", branch: "main"
 
