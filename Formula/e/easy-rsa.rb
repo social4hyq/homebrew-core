c5bbb38bd2c0bb4c05eece2a60c832de0d1475ba
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-20 04:46:40 +0000

    easy-rsa: update 3.2.7 bottle.

--- Formula/e/easy-rsa.rb
+++ Formula/e/easy-rsa.rb
@@ -7,7 +7,7 @@ class EasyRsa < Formula
   head "https://github.com/OpenVPN/easy-rsa.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "78fc31127e68c3998a718b613514802fc50a5e371cc98dfe070b4b4cb58dd74f"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "1e098d9edb86bc81e076b5285016e07d1d6d10068c42c8f47af215b9e6784ad7"
   end
 
   depends_on "openssl@4"
