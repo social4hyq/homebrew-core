a981038337a15a140432350b443f78013dedc45e
SwimmingTiger <hu60.cn@gmail.com>
2026-09-20 14:42:21 +0800

    vulkan-tools 1.4.357.0 (new formula)

---
+++ Formula/v/vulkan-tools.rb
@@ -0,0 +1,128 @@
+class VulkanTools < Formula
+  desc "Vulkan utilities and tools"
+  homepage "https://github.com/KhronosGroup/Vulkan-Tools"
+  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/vulkan-sdk-1.4.357.0.tar.gz"
+  sha256 "6c94b86c850808aba316d999dd6742133d6197ae2135248d3a8aed9b32ebd1f7"
+  license "Apache-2.0"
+  head "https://github.com/KhronosGroup/Vulkan-Tools.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^vulkan-sdk[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256               arm64_golden_gate: "21960746fc295e7728ca53b158701df3af11de7a8372378780564e361566fac9"
+    sha256               arm64_tahoe:       "a1574f18549fb2dba537c69cb2a76b741c9cb8f7ac8aa8841b97672d4dadf469"
+    sha256               arm64_sequoia:     "d6a631644a6ad2880416feddf252900a3b1c22a63a185c411c18965a9506b6a3"
+    sha256               arm64_sonoma:      "ac13a79328cc99cfcf088267062214c65633be11979905281e0c1518c4054ebe"
+    sha256 cellar: :any, sonoma:            "9b7145d5f2096cd64919959b982d41d8d472c2711a118554f952f0dcde5868d7"
+    sha256 cellar: :any, arm64_linux:       "c12abd38a3a29e4bb3faabd34c73167d52ffcf4e64b94d4e9bf491c328236247"
+    sha256 cellar: :any, x86_64_linux:      "a3eaf6e6a9ad06dc1ff0ce7f1b5a43522f118cd74a07b90707c002a08c6d7dcb"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "glslang"
+  depends_on "vulkan-headers"
+  depends_on "vulkan-loader"
+
+  on_macos do
+    depends_on xcode: :build # for ibtool
+    depends_on "molten-vk"
+  end
+
+  on_linux do
+    depends_on "libx11"
+    depends_on "libxcb"
+    depends_on "libxkbfile"
+    depends_on "libxrandr"
+    depends_on "wayland"
+    depends_on "wayland-protocols"
+  end
+
+  def install
+    if OS.mac?
+      # account for using already-built MoltenVK instead of the source repo
+      inreplace "cube/CMakeLists.txt",
+                "${MOLTENVK_DIR}/MoltenVK/icd/MoltenVK_icd.json",
+                "${MOLTENVK_DIR}/etc/vulkan/icd.d/MoltenVK_icd.json"
+      inreplace buildpath.glob("*/macOS/*/CMakeLists.txt"),
+                "${MOLTENVK_DIR}/Package/Release/MoltenVK/dynamic/dylib/macOS/libMoltenVK.dylib",
+                "${MOLTENVK_DIR}/lib/libMoltenVK.dylib"
+    end
+
+    args = [
+      "-DBUILD_ICD=ON",
+      "-DBUILD_CUBE=ON",
+      "-DBUILD_VULKANINFO=ON",
+      "-DINSTALL_ICD=OFF", # we will manually place it in a nonconflicting location
+      "-DGLSLANG_INSTALL_DIR=#{formula_opt_prefix("glslang")}",
+      "-DVULKAN_HEADERS_INSTALL_DIR=#{formula_opt_prefix("vulkan-headers")}",
+      "-DVULKAN_LOADER_INSTALL_DIR=#{formula_opt_prefix("vulkan-loader")}",
+      "-DCMAKE_INSTALL_RPATH=#{rpath(target: formula_opt_lib("vulkan-loader"))}",
+    ]
+    # OpenHarmony toolchain: CMake cannot identify clang, so the project's
+    # CMAKE_CXX_STANDARD 17 is never translated into -std=... (clang then
+    # defaults to C++14) and vulkaninfo's generated vulkaninfo.hpp relies on
+    # C++17 class template argument deduction. Force the standard explicitly.
+    args << "-DCMAKE_CXX_FLAGS=-std=c++17" if OS.ohos?
+    args += if OS.mac?
+      ["-DMOLTENVK_REPO_ROOT=#{formula_opt_prefix("molten-vk")}"]
+    else
+      [
+        "-DBUILD_WSI_DIRECTFB_SUPPORT=OFF",
+        "-DBUILD_WSI_WAYLAND_SUPPORT=ON",
+        "-DBUILD_WSI_XCB_SUPPORT=ON",
+        "-DBUILD_WSI_XLIB_SUPPORT=ON",
+      ]
+    end
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    # OpenHarmony CMake: `cmake --install` does not run the generated preinstall
+    # step, which relinks the binaries into CMakeFiles/CMakeRelink.dir with the
+    # install RPATH; without it the install rules fail on the missing files.
+    system "cmake", "--build", "build", "--target", "preinstall" if OS.ohos?
+    system "cmake", "--install", "build"
+
+    (lib/"mock_icd").install (buildpath/"build/icd/VkICD_mock_icd.json").realpath,
+                             shared_library("build/icd/libVkICD_mock_icd")
+
+    return unless OS.mac?
+
+    targets = [
+      formula_opt_lib("molten-vk")/shared_library("libMoltenVK"),
+      formula_opt_lib("vulkan-loader")/shared_library("libvulkan", Formula["vulkan-loader"].version.to_s),
+    ]
+    prefix.glob("cube/*.app/Contents/Frameworks").each do |framework_dir|
+      ln_sf targets, framework_dir, verbose: true
+    end
+
+    (bin/"vkcube").write_env_script "/usr/bin/open", "-a #{prefix}/cube/vkcube.app", {}
+    (bin/"vkcubepp").write_env_script "/usr/bin/open", "-a #{prefix}/cube/vkcubepp.app", {}
+  end
+
+  def caveats
+    <<~EOS
+      The mock ICD files have been installed in
+        #{opt_lib}/mock_icd
+      You can use them with the Vulkan Loader by setting
+        export VK_ICD_FILENAMES=#{opt_lib}/mock_icd/VkICD_mock_icd.json
+    EOS
+  end
+
+  test do
+    with_env(VK_ICD_FILENAMES: lib/"mock_icd/VkICD_mock_icd.json") do
+      assert_match "Vulkan Mock Device", shell_output("#{bin}/vulkaninfo --summary")
+    end
+
+    return if !OS.mac? || (Hardware::CPU.intel? && ENV["HOMEBREW_GITHUB_ACTIONS"])
+
+    # Disable Metal argument buffers for macOS Sonoma on arm
+    ENV["MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS"] = "0" if MacOS.version == :sonoma
+
+    with_env(XDG_DATA_DIRS: testpath) do
+      assert_match "DRIVER_ID_MOLTENVK", shell_output("#{bin}/vulkaninfo --summary")
+    end
+  end
+end
