5511752cbe091a9053117671edb89cc9f8b1da17
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-20 23:02:58 +0000

    minio-warp 1.8.0

--- Formula/m/minio-warp.rb
+++ Formula/m/minio-warp.rb
@@ -1,8 +1,8 @@
 class MinioWarp < Formula
   desc "S3 benchmarking tool"
   homepage "https://github.com/minio/warp"
-  url "https://github.com/minio/warp/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "c99bdb158e46e96aca9092b7d5fd6483e3901093045b1c8e987094d1fec94f2d"
+  url "https://github.com/minio/warp/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "5f0f8cbffccb4685cb5d1b191337369becf8fdd65277b68b189eb1e9eba6647b"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/warp.git", branch: "master"
 
