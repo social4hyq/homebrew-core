ac8c4b541a731d3b7ad17e4ac25ecfe0a36527fc
HarmonybrewBot <HarmonybrewBot@noreply.gitcode.com>
2026-09-20 04:43:53 +0000

    firebase-cli: update 15.30.2 bottle.

--- Formula/f/firebase-cli.rb
+++ Formula/f/firebase-cli.rb
@@ -6,7 +6,7 @@ class FirebaseCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ohos: "4e4897211d3707c0a9b69ba9ced544cd993c312c0a501409005a40b9ac2a2d41"
+    sha256 cellar: :any_skip_relocation, arm64_ohos: "d5e30dda1226498023cfbddd9c46a5e529d5826365f47c7b0455301fb24e9a24"
   end
 
   depends_on "node"
